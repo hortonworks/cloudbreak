9197f44bb3b484a317a0d7b942d922104a21e2cb
Marton Sereg <martonsereg@gmail.com>
2014-05-10 18:05:00 +0200

    creating a stack with built in params

--- src/main/java/com/sequenceiq/provisioning/CloudFormationController.java
+++ src/main/java/com/sequenceiq/provisioning/CloudFormationController.java
@@ -10,17 +10,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
+import com.amazonaws.services.cloudformation.model.CreateStackRequest;
+import com.amazonaws.services.cloudformation.model.CreateStackResult;
 import com.amazonaws.services.cloudformation.model.ListStacksResult;
+import com.amazonaws.services.cloudformation.model.Parameter;
 
 @Controller
 public class CloudFormationController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/listStacks")
     @ResponseBody
-    public ResponseEntity<ListStacksResult> listInstances(@RequestBody AwsCredentialsJson awsCredentialsJson) {
+    public ResponseEntity<ListStacksResult> listStacks(@RequestBody AwsCredentialsJson awsCredentialsJson) {
         BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(awsCredentialsJson.getAccessKey(), awsCredentialsJson.getSecretKey());
         AmazonCloudFormationClient amazonCloudFormationClient = new AmazonCloudFormationClient(basicAWSCredentials);
         ListStacksResult listStacksResult = amazonCloudFormationClient.listStacks();
         return new ResponseEntity<>(listStacksResult, HttpStatus.OK);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/stack")
+    @ResponseBody
+    public ResponseEntity<CreateStackResult> createStack(@RequestBody AwsCredentialsJson awsCredentialsJson) {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(awsCredentialsJson.getAccessKey(), awsCredentialsJson.getSecretKey());
+        AmazonCloudFormationClient amazonCloudFormationClient = new AmazonCloudFormationClient(basicAWSCredentials);
+        CreateStackRequest createStackRequest = new CreateStackRequest()
+                .withStackName("testStack")
+                .withTemplateURL("https://s3.amazonaws.com/cloudformation-templates-us-east-1/VPC_With_PublicIPs_And_DNS.template")
+                .withParameters(new Parameter().withParameterKey("KeyName").withParameterValue("test-key"));
+        CreateStackResult createStackResult = amazonCloudFormationClient.createStack(createStackRequest);
+        return new ResponseEntity<>(createStackResult, HttpStatus.OK);
+    }
+
 }
