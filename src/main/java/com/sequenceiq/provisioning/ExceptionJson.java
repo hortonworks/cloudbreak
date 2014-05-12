0b1db3cb215c04743968571d3d117e8eb83d5663
Marton Sereg <martonsereg@gmail.com>
2014-05-12 10:42:18 +0200

    handle internal errors in the controllers

---
+++ src/main/java/com/sequenceiq/provisioning/ExceptionJson.java
@@ -0,0 +1,20 @@
+package com.sequenceiq.provisioning;
+
+
+public class ExceptionJson {
+
+    private String message;
+
+    public ExceptionJson(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
