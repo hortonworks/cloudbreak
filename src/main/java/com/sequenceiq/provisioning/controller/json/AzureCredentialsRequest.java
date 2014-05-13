b19dae61cb19453e42292a3f4fc69600c061f69b
doktoric <doktoricsrichard@gmail.com>
2014-05-13 16:02:38 +0200

    basic azure classes added

---
+++ src/main/java/com/sequenceiq/provisioning/controller/json/AzureCredentialsRequest.java
@@ -0,0 +1,32 @@
+package com.sequenceiq.provisioning.controller.json;
+
+public class AzureCredentialsRequest {
+
+    private String subscriptionId;
+    private String keyStorePath;
+    private String keyStorePassword;
+
+    public String getKeyStorePath() {
+        return keyStorePath;
+    }
+
+    public void setKeyStorePath(String keyStorePath) {
+        this.keyStorePath = keyStorePath;
+    }
+
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public String getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public void setKeyStorePassword(String keyStorePassword) {
+        this.keyStorePassword = keyStorePassword;
+    }
+}
