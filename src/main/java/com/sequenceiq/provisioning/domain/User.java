cfc71c38d9cacc608b9daeab8ceca437aff1487c
Marton Sereg <martonsereg@gmail.com>
2014-05-15 17:14:43 +0200

    added basic authentication

---
+++ src/main/java/com/sequenceiq/provisioning/domain/User.java
@@ -0,0 +1,82 @@
+package com.sequenceiq.provisioning.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
+    @NotEmpty
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    public User() {
+    }
+
+    public User(User user) {
+        this.id = user.id;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.password = user.password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/sequenceiq/provisioning/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sequenceiq.provisioning.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sequenceiq.provisioning.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByEmail(String email);
+}
