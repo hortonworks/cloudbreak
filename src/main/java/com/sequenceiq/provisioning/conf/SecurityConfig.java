13826d8c309874a8dc21c4785b0a04debb45a8d3
Marton Sereg <martonsereg@gmail.com>
2014-05-16 15:16:30 +0200

    h2 console setup added temporarily

--- src/main/java/com/sequenceiq/provisioning/conf/SecurityConfig.java
+++ src/main/java/com/sequenceiq/provisioning/conf/SecurityConfig.java
@@ -25,7 +25,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .headers()
+                .contentTypeOptions()
+                .and()
                 .authorizeRequests()
+                .antMatchers("/h2/**").permitAll()
                 .anyRequest().authenticated().and()
                 .httpBasic();
     }
