e1599dc466f84f609661fd9e1f3f2ab19ed25d05
Richard Doktorics <ricsi@Richards-MacBook-Pro.local>
2014-05-17 15:02:18 +0200

    review fixes

--- src/main/java/com/sequenceiq/provisioning/controller/CertificateController.java
+++ src/main/java/com/sequenceiq/provisioning/controller/CertificateController.java
@@ -10,19 +10,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sequenceiq.provisioning.controller.json.ProvisionRequest;
+import com.sequenceiq.provisioning.domain.CertificateRequest;
 import com.sequenceiq.provisioning.domain.User;
 import com.sequenceiq.provisioning.security.CurrentUser;
 import com.sequenceiq.provisioning.service.azure.AzureProvisionService;
 import com.sequenceiq.provisioning.service.azure.CertificateGeneratorService;
 
 @Controller
-public class TestController {
+public class CertificateController {
 
     @Autowired
     private CertificateGeneratorService certificateGeneratorService;
@@ -30,27 +31,14 @@ public class TestController {
     @Autowired
     private AzureProvisionService azureProvisionService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    @RequestMapping(method = RequestMethod.POST, value = "/certificate")
     @ResponseBody
-    public ResponseEntity<String> test(@CurrentUser User user) {
-        return new ResponseEntity<>("test", HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/provision")
-    @ResponseBody
-    public ResponseEntity<String> provision(@CurrentUser User user) throws Exception {
-        azureProvisionService.provisionCluster(user, new ProvisionRequest());
-        return new ResponseEntity<>("provision", HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/generate")
-    @ResponseBody
-    public ResponseEntity<String> generate(@CurrentUser User user) throws Exception {
+    public ResponseEntity<String> generate(@CurrentUser User user, @RequestBody CertificateRequest certificateRequest) throws Exception {
         certificateGeneratorService.generateCertificate(user);
         return new ResponseEntity<>("generate", HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/cerfile")
+    @RequestMapping(method = RequestMethod.GET, value = "/certificate")
     @ResponseBody
     public ModelAndView getCertificateFile(@CurrentUser User user, HttpServletResponse response) throws Exception {
         File cerFile = certificateGeneratorService.getCertificateFile(user);
