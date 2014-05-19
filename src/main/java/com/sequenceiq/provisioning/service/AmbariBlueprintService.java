b48486bc235c7b352d56c5130820bf5f5c6ea027
Marton Sereg <martonsereg@gmail.com>
2014-05-19 16:49:25 +0200

    blueprintService logic with hardcoded ambari server host and port

--- src/main/java/com/sequenceiq/provisioning/service/AmbariBlueprintService.java
+++ src/main/java/com/sequenceiq/provisioning/service/AmbariBlueprintService.java
@@ -5,13 +5,11 @@ import groovyx.net.http.HttpResponseException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,57 +23,42 @@ import com.sequenceiq.provisioning.domain.User;
 @Service
 public class AmbariBlueprintService {
 
-    private static final long TEST_CLOUD_ID = 12L;
-    private static final Logger LOGGER = LoggerFactory.getLogger(AmbariBlueprintService.class);
-
     public void addBlueprint(User user, Long cloudId, BlueprintJson blueprintJson) {
-        // TODO
+        // TODO get ambari client host and port from cloud entity
         AmbariClient ambariClient = new AmbariClient("localhost", "49163");
-        if (!ambariClient.addBlueprint(blueprintJson.getAmbariBlueprint())) {
-            throw new BadRequestException("Failed to validate Ambari blueprint.");
+        try {
+            ambariClient.addBlueprint(blueprintJson.getAmbariBlueprint());
+        } catch (HttpResponseException e) {
+            if ("Conflict".equals(e.getMessage())) {
+                throw new BadRequestException("Ambari blueprint already exists.", e);
+            } else if ("Bad Request".equals(e.getMessage())) {
+                throw new BadRequestException("Failed to validate Ambari blueprint.", e);
+            } else {
+                throw new InternalServerException("Something went wrong", e);
+            }
         }
     }
 
     public List<BlueprintJson> retrieveBlueprints(User user, Long cloudId) {
-        // TODO
-        BlueprintJson blueprintJson = new BlueprintJson();
-        blueprintJson.setCloudId(TEST_CLOUD_ID);
-        ObjectMapper mapper = new ObjectMapper();
-        JsonFactory factory = mapper.getFactory();
         try {
-            JsonParser jp = factory
-                    .createParser("{\"host_groups\":[{\"name\":\"master\",\"components\":[{\"name\":\"NAMENODE\"},"
-                            + "{\"name\":\"SECONDARY_NAMENODE\"},{\"name\":\"RESOURCEMANAGER\"},{\"name\":\"HISTORYSERVER\"},"
-                            + "{\"name\":\"NAGIOS_SERVER\"},{\"name\":\"ZOOKEEPER_SERVER\"}],\"cardinality\":\"1\"},"
-                            + "{\"name\":\"slaves\",\"components\":[{\"name\":\"DATANODE\"},{\"name\":\"HDFS_CLIENT\"},"
-                            + "{\"name\":\"NODEMANAGER\"},{\"name\":\"YARN_CLIENT\"},{\"name\":\"MAPREDUCE2_CLIENT\"},"
-                            + "{\"name\":\"ZOOKEEPER_CLIENT\"}],\"cardinality\":\"2\"}],"
-                            + "\"Blueprints\":{\"blueprint_name\":\"multi-node-hdfs-yarn\",\"stack_name\":\"HDP\",\"stack_version\":\"2.0\"}}");
-            JsonNode actualObj = mapper.readTree(jp);
-            blueprintJson.setAmbariBlueprint(actualObj);
-        } catch (JsonParseException e) {
-            LOGGER.error("Failed to parse JSON.", e);
+            List<BlueprintJson> blueprints = new ArrayList<>();
+            // TODO get ambari client host and port from cloud entity
+            AmbariClient ambariClient = new AmbariClient("localhost", "49163");
+            Set<String> blueprintNames = ambariClient.getBlueprintsMap().keySet();
+            for (String blueprintName : blueprintNames) {
+                blueprints.add(createBlueprintJsonFromString(cloudId, ambariClient.getBlueprintAsJson(blueprintName)));
+            }
+            return blueprints;
         } catch (IOException e) {
-            LOGGER.error("Failed to create parser.", e);
+            throw new InternalServerException("Jackson failed to parse blueprint JSON.", e);
         }
-        List<BlueprintJson> blueprints = new ArrayList<>();
-        blueprints.add(blueprintJson);
-        return blueprints;
     }
 
     public BlueprintJson retrieveBlueprint(User user, Long cloudId, String id) {
-        // TODO
+        // TODO get ambari client host and port from cloud entity
         AmbariClient ambariClient = new AmbariClient("localhost", "49163");
         try {
-            String blueprint = ambariClient.getBlueprintAsJson(id);
-            BlueprintJson blueprintJson = new BlueprintJson();
-            blueprintJson.setCloudId(TEST_CLOUD_ID);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonFactory factory = mapper.getFactory();
-            JsonParser jp = factory.createParser(blueprint);
-            JsonNode actualObj = mapper.readTree(jp);
-            blueprintJson.setAmbariBlueprint(actualObj);
-            return blueprintJson;
+            return createBlueprintJsonFromString(cloudId, ambariClient.getBlueprintAsJson(id));
         } catch (HttpResponseException e) {
             if ("Not Found".equals(e.getMessage())) {
                 throw new NotFoundException("Ambari blueprint not found.", e);
@@ -85,5 +68,16 @@ public class AmbariBlueprintService {
         } catch (IOException e) {
             throw new InternalServerException("Jackson failed to parse blueprint JSON.", e);
         }
+    }
+
+    private BlueprintJson createBlueprintJsonFromString(Long cloudId, String blueprint) throws IOException {
+        BlueprintJson blueprintJson = new BlueprintJson();
+        blueprintJson.setCloudId(cloudId);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonFactory factory = mapper.getFactory();
+        JsonParser jp = factory.createParser(blueprint);
+        JsonNode actualObj = mapper.readTree(jp);
+        blueprintJson.setAmbariBlueprint(actualObj);
+        return blueprintJson;
     }
 }
