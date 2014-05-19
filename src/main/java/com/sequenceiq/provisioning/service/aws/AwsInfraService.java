6aa2939f96abe6bd832cd63bfb204633814ed9f7
doktoric <doktoricsrichard@gmail.com>
2014-05-19 13:16:48 +0200

    added persist logic to services

--- src/main/java/com/sequenceiq/provisioning/service/aws/AwsInfraService.java
+++ src/main/java/com/sequenceiq/provisioning/service/aws/AwsInfraService.java
@@ -1,16 +1,36 @@
 package com.sequenceiq.provisioning.service.aws;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.sequenceiq.provisioning.controller.json.CloudInstanceResult;
 import com.sequenceiq.provisioning.controller.json.InfraRequest;
+import com.sequenceiq.provisioning.converter.AwsInfraConverter;
 import com.sequenceiq.provisioning.domain.CloudPlatform;
 import com.sequenceiq.provisioning.domain.User;
+import com.sequenceiq.provisioning.repository.AwsInfraRepository;
+import com.sequenceiq.provisioning.repository.UserRepository;
 import com.sequenceiq.provisioning.service.InfraService;
 
+@Service
 public class AwsInfraService implements InfraService {
+
+    private static final String OK_STATUS = "ok";
+
+    @Autowired
+    private AwsInfraConverter awsInfraConverter;
+
+    @Autowired
+    private AwsInfraRepository awsInfraRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public CloudInstanceResult createInfra(User user, InfraRequest infraRequest) {
-        return new CloudInstanceResult("created");
+        user.getAwsInfraList().add(awsInfraConverter.convert(infraRequest));
+        userRepository.save(user);
+        return new CloudInstanceResult(OK_STATUS);
     }
 
     @Override
