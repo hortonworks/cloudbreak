c7fe77f609c43d2dd38a63b472be15039c9d1109
doktoric <doktoricsrichard@gmail.com>
2014-05-19 13:50:54 +0200

    added correct user check

--- src/main/java/com/sequenceiq/provisioning/controller/CloudInstanceController.java
+++ src/main/java/com/sequenceiq/provisioning/controller/CloudInstanceController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import com.sequenceiq.provisioning.controller.json.CloudInstanceRequest;
 import com.sequenceiq.provisioning.controller.json.CloudInstanceResult;
 import com.sequenceiq.provisioning.domain.CloudPlatform;
 import com.sequenceiq.provisioning.domain.User;
+import com.sequenceiq.provisioning.repository.UserRepository;
 import com.sequenceiq.provisioning.security.CurrentUser;
 import com.sequenceiq.provisioning.service.CloudInstanceService;
 
@@ -26,11 +28,14 @@ public class CloudInstanceController {
     @Resource
     private Map<CloudPlatform, CloudInstanceService> cloudInstanceServices;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(method = RequestMethod.POST, value = "/cloud")
     @ResponseBody
     public ResponseEntity<CloudInstanceResult> provisionCluster(@CurrentUser User user, @RequestBody @Valid CloudInstanceRequest cloudInstanceRequest) {
         CloudInstanceService cloudInstanceService = cloudInstanceServices.get(cloudInstanceRequest.getCloudPlatform());
-        return new ResponseEntity<>(cloudInstanceService.createCloudInstance(user, cloudInstanceRequest), HttpStatus.CREATED);
+        return new ResponseEntity<>(cloudInstanceService.createCloudInstance(userRepository.findOneWithLists(user.getId()), cloudInstanceRequest), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/sequenceiq/provisioning/controller/InfraController.java
+++ src/main/java/com/sequenceiq/provisioning/controller/InfraController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ import com.sequenceiq.provisioning.controller.json.CloudInstanceResult;
 import com.sequenceiq.provisioning.controller.json.InfraRequest;
 import com.sequenceiq.provisioning.domain.CloudPlatform;
 import com.sequenceiq.provisioning.domain.User;
+import com.sequenceiq.provisioning.repository.UserRepository;
 import com.sequenceiq.provisioning.security.CurrentUser;
 import com.sequenceiq.provisioning.service.InfraService;
 
@@ -26,11 +28,14 @@ public class InfraController {
     @Resource
     private Map<CloudPlatform, InfraService> infraServices;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(method = RequestMethod.POST, value = "/infra")
     @ResponseBody
     public ResponseEntity<CloudInstanceResult> provisionCluster(@CurrentUser User user, @RequestBody @Valid InfraRequest infraRequest) {
         InfraService infraService = infraServices.get(infraRequest.getCloudPlatform());
-        return new ResponseEntity<>(infraService.createInfra(user, infraRequest), HttpStatus.CREATED);
+        return new ResponseEntity<>(infraService.createInfra(userRepository.findOneWithLists(user.getId()), infraRequest), HttpStatus.CREATED);
     }
 
 }
