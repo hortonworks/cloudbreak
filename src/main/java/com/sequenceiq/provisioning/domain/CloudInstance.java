1d2eb4fa5275379b02ead518342324c5c82bbba4
doktoric <doktoricsrichard@gmail.com>
2014-05-20 10:24:44 +0200

    added new domain classes

--- src/main/java/com/sequenceiq/provisioning/domain/CloudInstance.java
+++ src/main/java/com/sequenceiq/provisioning/domain/CloudInstance.java
@@ -1,7 +1,30 @@
 package com.sequenceiq.provisioning.domain;
 
-/**
- * Created by ricsi on 5/20/14.
- */
-public class CloudInstance {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public abstract class CloudInstance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cloudinstance_generator")
+    @SequenceGenerator(name = "cloudinstance_generator", sequenceName = "cloudsequence_table")
+    private Long id;
+
+    public CloudInstance() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public abstract CloudPlatform cloudPlatform();
 }
