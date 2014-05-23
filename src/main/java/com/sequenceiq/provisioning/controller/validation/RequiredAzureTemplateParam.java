9a68dc215c2948824fa864998cd5f5b862d047a2
doktoric <doktoricsrichard@gmail.com>
2014-05-23 18:54:17 +0200

    added template regexes

--- src/main/java/com/sequenceiq/provisioning/controller/validation/RequiredAzureTemplateParam.java
+++ src/main/java/com/sequenceiq/provisioning/controller/validation/RequiredAzureTemplateParam.java
@@ -1,31 +1,34 @@
 package com.sequenceiq.provisioning.controller.validation;
 
+import com.google.common.base.Optional;
 import com.sequenceiq.provisioning.service.azure.AzureLocation;
 import com.sequenceiq.provisioning.service.azure.AzureVmType;
 
 public enum RequiredAzureTemplateParam implements TemplateParam {
 
-    LOCATION("location", true, AzureLocation.class),
-    DESCRIPTION("description", true, String.class),
-    ADDRESSPREFIX("addressPrefix", true, String.class),
-    SUBNETADDRESSPREFIX("subnetAddressPrefix", true, String.class),
-    DEPLOYMENTSLOT("deploymentSlot", true, String.class),
-    IMAGENAME("imageName", true, String.class),
-    USERNAME("username", true, String.class),
-    VMTYPE("vmType", true, AzureVmType.class),
+    LOCATION("location", true, AzureLocation.class, Optional.<String>absent()),
+    DESCRIPTION("description", true, String.class, Optional.<String>absent()),
+    ADDRESSPREFIX("addressPrefix", true, String.class, Optional.<String>absent()),
+    SUBNETADDRESSPREFIX("subnetAddressPrefix", true, String.class, Optional.<String>absent()),
+    DEPLOYMENTSLOT("deploymentSlot", true, String.class, Optional.<String>absent()),
+    IMAGENAME("imageName", true, String.class, Optional.<String>absent()),
+    USERNAME("username", true, String.class, Optional.<String>absent()),
+    VMTYPE("vmType", true, AzureVmType.class, Optional.<String>absent()),
 
-    SSH_PUBLIC_KEY_FINGERPRINT("sshPublicKeyFingerprint", false, String.class),
-    PASSWORD("password", false, String.class),
-    SSH_PUBLIC_KEY_PATH("sshPublicKeyPath", false, String.class);
+    SSH_PUBLIC_KEY_FINGERPRINT("sshPublicKeyFingerprint", false, String.class, Optional.<String>absent()),
+    PASSWORD("password", false, String.class, Optional.<String>absent()),
+    SSH_PUBLIC_KEY_PATH("sshPublicKeyPath", false, String.class, Optional.<String>absent());
 
     private final String paramName;
     private final Class clazz;
     private final boolean required;
+    private final Optional<String> regex;
 
-    private RequiredAzureTemplateParam(String paramName, Boolean required, Class clazz) {
+    private RequiredAzureTemplateParam(String paramName, Boolean required, Class clazz, Optional<String> regex) {
         this.paramName = paramName;
         this.required = required;
         this.clazz = clazz;
+        this.regex = regex;
     }
 
     @Override
@@ -43,4 +46,8 @@ public enum RequiredAzureTemplateParam implements TemplateParam {
         return required;
     }
 
+    @Override
+    public Optional<String> getRegex() {
+        return regex;
+    }
 }
