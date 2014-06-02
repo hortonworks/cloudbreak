044480f5df2da7dc9ad40b003bc87cb95c0388da
Marton Sereg <martonsereg@gmail.com>
2014-06-02 18:05:13 +0200

    added a happy flow in curl samples

--- src/test/resources/sample-jsons.sh
+++ src/test/resources/sample-jsons.sh
@@ -6,7 +6,7 @@ curl -u user@seq.com:test123 -X PUT -H "Content-Type:application/json" -d '{"sta
 
 curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"cloudPlatform":"AZURE","parameters":{"subscriptionId":"a8b8dac0-2b84-41da-a08b-1e45297e0d2b", "jksPassword": "tet123"}}' http://localhost:8080/credential | jq .
 
-curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"name":"smap123","url":"https://gist.githubusercontent.com/doktoric/11eb550f45416e54fe60/raw/1a7d302c162ee5fb91866766ec1a1fd18fd98ed3/gistfile1.txt"}' http://localhost:8080/blueprint | jq .
+curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"name":"smap123","url":"https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/master/src/main/resources/blueprints/multi-node-hdfs-yarn"}' http://localhost:8080/blueprint | jq .
 
 curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"cloudPlatform":"AZURE","clusterSize":2, "templateId":"51", "name":"my azure", "credentialId":"51"}' http://localhost:8080/stack | jq .
 
@@ -15,10 +15,17 @@ curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"cl
 curl -u user@seq.com:test123 -X POST -H "Content-Type:application/json" -d '{"cloudPlatform":"AZURE","name":"my-azure-cluster","parameters":{"location":"NORTH_EUROPE","description":"my-azure-cluster description","addressPrefix":"172.16.0.0/16","subnetAddressPrefix":"172.16.0.0/24","deploymentSlot":"production","imageName":"c290a6b031d841e09f2da759bbabe71f__Oracle-Linux-6","username":"ricsiuser","password":"Password!@#$","disableSshPasswordAuthentication":"true","vmType":"SMALL","ports":[{"protocol":"TCP","port":"8080","name":"local","localPort":"8080"}]}}' http://localhost:8080/template | jq .
 
 
-# create AWS stack
+# create AWS stack & cluster - happy flow
 curl -u user@seq.com:test123 -sX POST -H "Content-Type:application/json" -d '{"cloudPlatform":"AWS","parameters":{"roleArn":"arn:aws:iam::755047402263:role/seq-self-cf","instanceProfileRoleArn":"arn:aws:iam::755047402263:instance-profile/readonly-role"}}' http://localhost:8080/credential | jq .
 curl -u user@seq.com:test123 -sX POST -H "Content-Type:application/json" -d '{"cloudPlatform":"AWS","name":"awsinstancestemplate","parameters":{"region":"EU_WEST_1", "amiId":"ami-2f39f458", "keyName":"sequence-eu","sshLocation":"0.0.0.0/0","instanceType":"M1Small"}}' http://localhost:8080/template | jq .
 curl -u user@seq.com:test123 -sX POST -H "Content-Type:application/json" -d '{"nodeCount":3, "templateId":"52", "name":"sundaywork2", "credentialId":"52"}' http://localhost:8080/stack | jq .
+curl -u user@seq.com:test123 -sX POST -H "Content-Type:application/json" -d '{"url":"https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/master/src/main/resources/blueprints/multi-node-hdfs-yarn"}' http://localhost:8080/blueprint | jq .
+curl -u user@seq.com:test123 -sX POST -H "Content-Type:application/json" -d '{"clusterName":"full-cloudbreak-api","blueprintId":52}' http://localhost:8080/stack/51/cluster | jq .
 
+curl -u user@seq.com:test123 -sX GET -H "Content-Type:application/json" http://localhost:8080/credential/51 | jq .
+curl -u user@seq.com:test123 -sX GET -H "Content-Type:application/json" http://localhost:8080/template/51 | jq .
 curl -u user@seq.com:test123 -sX GET -H "Content-Type:application/json" http://localhost:8080/stack/51 | jq .
+curl -u user@seq.com:test123 -sX GET -H "Content-Type:application/json" http://localhost:8080/blueprint/52 | jq .
+curl -u user@seq.com:test123 -sX GET -H "Content-Type:application/json" http://localhost:8080/stack/51/cluster | jq .
+
 curl -u user@seq.com:test123 -sX DELETE -H "Content-Type:application/json" http://localhost:8080/stack/51 | jq .
