476040b6e2397b987305b830f74e5b72c3963785
doktoric <doktoricsrichard@gmail.com>
2014-06-04 11:22:47 +0200

    link fix

--- js/controllers.js
+++ js/controllers.js
@@ -12,7 +12,7 @@ cloudbreakControllers.controller('cloudbreakController', ['$scope', '$http', 'Te
         $http.defaults.headers.common['Content-Type']= 'application/json';
 
         if($rootScope.apiUrl === null || $rootScope.apiUrl === undefined ) {
-            $rootScope.apiUrl = "http://localhost:8080";
+            $rootScope.apiUrl = "http://uluwatu.sequenceiq.com";
         }
         if($rootScope.basic_auth === null || $rootScope.basic_auth === undefined ) {
             $rootScope.basic_auth = "dXNlckBzZXEuY29tOnRlc3QxMjM=";
