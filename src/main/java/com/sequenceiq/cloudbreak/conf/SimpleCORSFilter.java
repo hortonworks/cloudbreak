1270df256927fb56871f74f43acbeba0ca846d99
Marton Sereg <martonsereg@gmail.com>
2014-06-11 11:21:05 +0200

    allow content-type header too

--- src/main/java/com/sequenceiq/cloudbreak/conf/SimpleCORSFilter.java
+++ src/main/java/com/sequenceiq/cloudbreak/conf/SimpleCORSFilter.java
@@ -25,7 +25,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Accept, Authorization, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
     }
 
