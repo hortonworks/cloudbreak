7ed196d8914a6d2200a05104465f38f59154d5b2
doktoric <doktoricsrichard@gmail.com>
2014-06-18 15:13:05 +0200

    base64 encoding added

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 var log = log4javascript.getLogger("cloudbreak-logger");
 var popUpAppender = new log4javascript.PopUpAppender();
 var layout = new log4javascript.PatternLayout("[%-5p] %m");
@@ -60,7 +60,10 @@ cloudbreakControllers.controller('cloudbreakController', ['$scope', '$http', 'Te
             if(emailFieldLogin.value === "cbuser@sequenceiq.com" && passwFieldLogin.value === "test123") {
                 localStorage.signedIn = true;
                 $rootScope.signedIn = true;
-                $rootScope.activeUser = emailFieldLogin.value;
+                localStorage.activeUser = emailFieldLogin.value;
+                $rootScope.activeUser = localStorage.activeUser;
+                localStorage.password64 = Base64.encode(emailFieldLogin.value + ":" + passwFieldLogin.value);
+                $rootScope.password64 = localStorage.password64;
                 $scope.doQuerys();
             }
         }
@@ -68,6 +71,8 @@ cloudbreakControllers.controller('cloudbreakController', ['$scope', '$http', 'Te
         $scope.signOut = function() {
             $rootScope.signedIn = false;
             localStorage.signedIn = false;
+            localStorage.removeItem('password64');
+            localStorage.removeItem('activeUser');
         }
 
         $scope.getLedStyle = function(status) {
@@ -600,7 +605,7 @@ cloudbreakControllers.controller('cloudbreakController', ['$scope', '$http', 'Te
         $scope.doQuerys = function() {
             $http.get('connection.properties').then(function (response) {
                 $rootScope.apiUrl = response.data.backend_url;
-                $rootScope.basic_auth = response.data.password64;
+                $rootScope.basic_auth = localStorage.password64;
                 $scope.getCredentials();
                 $scope.getTemplates();
                 $scope.getBluePrints();
@@ -695,9 +700,10 @@ cloudbreakControllers.controller('cloudbreakController', ['$scope', '$http', 'Te
         }
 
         if (typeof (Storage) !== "undefined") {
-            if (localStorage.signedIn === 'true') {
+            if (localStorage.signedIn === 'true' && localStorage.activeUser && localStorage.password64) {
                 $rootScope.signedIn = true;
-                $rootScope.activeUser = "cbuser@sequenceiq.com";
+                $rootScope.activeUser = localStorage.activeUser;
+                $rootScope.basic_auth = localStorage.password64;
                 $scope.doQuerys();
             }
         } else {
