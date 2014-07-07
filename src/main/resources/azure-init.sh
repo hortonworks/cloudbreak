2b6c24484305a417e43db48aa0636f7d889c6a5e
doktoric <doktoricsrichard@gmail.com>
2014-07-07 10:58:16 +0200

    CLOUD-25 added basic azure init script

--- src/main/resources/azure-init.sh
+++ src/main/resources/azure-init.sh
@@ -1,1 +1,19 @@
-echo azure-init
+: ${NODE_PREFIX=amb}
+: ${MYDOMAIN:=mycorp.kom}
+: ${IMAGE:="sequenceiq/ambari"}
+: ${INSTANCE_ID:="0"}
+
+# installs jq to process jsons easily
+curl -o /usr/bin/jq http://stedolan.github.io/jq/download/linux64/jq
+chmod +x /usr/bin/jq
+
+# jq expression that selects the json entry of the current instance from the array returned by the metadata service
+INSTANCE_SELECTOR='. | map(select(.instanceId == "'$INSTANCE_ID'"))'
+# jq expression that selects all the other json entries
+OTHER_INSTANCES_SELECTOR='. | map(select(.instanceId != "'$INSTANCE_ID'"))'
+
+# metadata service returns '204: no-content' if metadata is not ready yet and '200: ok' if it's completed
+# every other http status codes mean that something unexpected happened
+METADATA_STATUS=204
+MAX_RETRIES=60
+RETRIES=0
