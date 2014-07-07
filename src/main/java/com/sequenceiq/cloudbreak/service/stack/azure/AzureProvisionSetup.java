77deffc660b7e6c741b8daa06f3521745e288a78
doktoric <doktoricsrichard@gmail.com>
2014-07-07 10:57:38 +0200

    CLOUD-25 added AzureProvisionSetup implementation

---
+++ src/main/java/com/sequenceiq/cloudbreak/service/stack/azure/AzureProvisionSetup.java
@@ -0,0 +1,35 @@
+package com.sequenceiq.cloudbreak.service.stack.azure;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.conf.ReactorConfig;
+import com.sequenceiq.cloudbreak.domain.CloudPlatform;
+import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.service.stack.ProvisionSetup;
+import com.sequenceiq.cloudbreak.service.stack.event.ProvisionSetupComplete;
+
+import reactor.core.Reactor;
+import reactor.event.Event;
+
+@Component
+public class AzureProvisionSetup implements ProvisionSetup {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureProvisionSetup.class);
+
+    @Autowired
+    private Reactor reactor;
+
+    @Override
+    public void setupProvisioning(Stack stack) {
+        LOGGER.info("Publishing {} event [StackId: '{}']", ReactorConfig.PROVISION_SETUP_COMPLETE_EVENT, stack.getId());
+        reactor.notify(ReactorConfig.PROVISION_SETUP_COMPLETE_EVENT, Event.wrap(new ProvisionSetupComplete(getCloudPlatform(), stack.getId())));
+    }
+
+    @Override
+    public CloudPlatform getCloudPlatform() {
+        return CloudPlatform.AZURE;
+    }
+}
