7f776697cc7f6bb83e20217519f0c539d4cbcf46
doktoric <doktoricsrichard@gmail.com>
2014-07-09 15:51:36 +0200

    CLOUD-25 init script changes

--- src/main/resources/azure-init.sh
+++ src/main/resources/azure-init.sh
@@ -9,7 +9,7 @@ curl -o /usr/bin/jq http://stedolan.github.io/jq/download/linux64/jq
 chmod +x /usr/bin/jq
 
 # instance id from ec2 metadata
-INSTANCE_ID=$(sudo less waagent/ovf-env.xml |grep -oPm1 "(?<=<HostName>)[^<]+")
+INSTANCE_ID=$(sudo cat waagent/ovf-env.xml |grep -oPm1 "(?<=<HostName>)[^<]+")
 
 # jq expression that selects the json entry of the current instance from the array returned by the metadata service
 INSTANCE_SELECTOR='. | map(select(.instanceId == "'$INSTANCE_ID'"))'
@@ -65,16 +65,14 @@ sleep 5
 DOCKER_SUBNET_OF_FIRST_OTHER=$(echo $METADATA_RESULT | jq "$OTHER_INSTANCES_SELECTOR" | jq '.[0].dockerSubnet' | sed s/\"//g)
 SERF_JOIN_IP=${DOCKER_SUBNET_OF_FIRST_OTHER}.2
 
-# temporary, while AMI is created with the latest docker version and the latest sequenceiq/ambari image
-IMAGE=ambari-warmup
-
 # determines if this instance is the Ambari server or not and sets the tags accordingly
 AMBARI_SERVER=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].ambariServer' | sed s/\"//g)
 [ "$AMBARI_SERVER" == true ] && AMBARI_ROLE="--tag ambari-server=true" || AMBARI_ROLE=""
 
 INSTANCE_IDX=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].instanceIndex' | sed s/\"//g)
+MY_IP=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].privateIp' | sed s/\"//g)
 
-CMD="docker run -d -p 8080:8080 -p 8088:8088 -p 8050:8050 -p 8020:8020 -p 10020:10020 -p 19888:19888 -e SERF_JOIN_IP=$SERF_JOIN_IP --dns 127.0.0.1 --name ${NODE_PREFIX}${INSTANCE_IDX} -h ${NODE_PREFIX}${INSTANCE_IDX}.${MYDOMAIN} --entrypoint /usr/local/serf/bin/start-serf-agent.sh  $IMAGE $AMBARI_ROLE"
+CMD="docker run -d -p 8080:8080 -p 8088:8088 -p 8050:8050 -p 8020:8020 -p 10020:10020 -p 19888:19888 -e SERF_JOIN_IP=$SERF_JOIN_IP -e SERF_ADVERTISE_IP=$MY_IP --dns 127.0.0.1 --name ${NODE_PREFIX}${INSTANCE_IDX} -h ${NODE_PREFIX}${INSTANCE_IDX}.${MYDOMAIN} --entrypoint /usr/local/serf/bin/start-serf-agent.sh  $IMAGE $AMBARI_ROLE"
 
 cat << EOF
 =========================================
