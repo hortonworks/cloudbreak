5c15eb08ed3df1ca07a7d5a17b8e3b592a31220e
oleewere <oleewere@gmail.com>
2014-07-14 14:31:35 +0200

    CLOUD-11 reset/forgot pwd: use json instead of query string

--- src/main/java/com/sequenceiq/cloudbreak/controller/PasswordResetController.java
+++ src/main/java/com/sequenceiq/cloudbreak/controller/PasswordResetController.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.cloudbreak.controller;
 
+import com.sequenceiq.cloudbreak.controller.json.UserJson;
 import com.sequenceiq.cloudbreak.service.user.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/password")
@@ -21,16 +22,16 @@ public class PasswordResetController {
     private UserService userService;
 
     @RequestMapping(value = "/reset", method = RequestMethod.POST)
-    public ResponseEntity<String> forgotPassword(@RequestParam String email) {
-        LOGGER.debug("Reset password for: {} (email)", email);
-        String user = userService.generatePasswordResetToken(email);
+    public ResponseEntity<String> forgotPassword(@RequestBody UserJson userJson) {
+        LOGGER.debug("Reset password for: {} (email)", userJson.getEmail());
+        String user = userService.generatePasswordResetToken(userJson.getEmail());
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/reset/{confToken}", method = RequestMethod.POST)
-    public ResponseEntity<String> resetPassword(@PathVariable String confToken, @RequestParam String password) {
+    public ResponseEntity<String> resetPassword(@PathVariable String confToken, @RequestBody UserJson userJson) {
         LOGGER.debug("Reset password token: {}", confToken);
-        String rToken = userService.resetPassword(confToken, password);
+        String rToken = userService.resetPassword(confToken, userJson.getPassword());
         return new ResponseEntity<>(rToken, HttpStatus.OK);
     }
 }
