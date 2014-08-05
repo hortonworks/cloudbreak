3b75ed231e2811d2fc621b2030c9ce84932220d0
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-08-05 12:07:32 +0200

    PERI-3 ability to load config file from file and from URL

--- src/main/java/com/sequenceiq/periscope/config/AppConfig.java
+++ src/main/java/com/sequenceiq/periscope/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.sequenceiq.periscope.config;
 
+import java.net.MalformedURLException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
 import org.springframework.web.client.RestOperations;
@@ -15,6 +19,9 @@ import org.yaml.snakeyaml.Yaml;
 public class AppConfig {
 
     private static final String PERISCOPE_YAML = "periscope.yaml";
+
+    @Value("${periscope.config:}")
+    private String configLocation;
 
     @Bean
     ThreadPoolExecutorFactoryBean getThreadPoolExecutorFactoryBean() {
@@ -32,8 +39,12 @@ public class AppConfig {
     }
 
     @Bean
-    Resource cloudbreakSettings() {
-        return new ClassPathResource(PERISCOPE_YAML);
+    Resource cloudbreakSettings() throws MalformedURLException {
+        if (configLocation.isEmpty()) {
+            return new ClassPathResource(PERISCOPE_YAML);
+        } else {
+            return new UrlResource(configLocation);
+        }
     }
 
 }
