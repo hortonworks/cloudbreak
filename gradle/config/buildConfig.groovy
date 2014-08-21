4be2e6ccf6da033f6648baf2ccb0fb535f04a590
doktoric <doktoricsrichard@gmail.com>
2014-08-21 18:08:44 +0200

    CLOUD-196 fixed versioning file

--- gradle/config/buildConfig.groovy
+++ gradle/config/buildConfig.groovy
@@ -2,10 +2,12 @@ environments {
     jenkins {
         sonar_host_url = "http://$System.env.SONAR_PORT_9000_TCP_ADDR:9000"
         sonar_jdbc_url = "jdbc:mysql://$System.env.SONAR_DB_PORT_3306_TCP_ADDR:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
+        metadata = "https://s3-eu-west-1.amazonaws.com/seq-repo/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
     }
 
     local {
         sonar_host_url = "http://localhost:9000"
         sonar_jdbc_url = "jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
+        metadata = "https://s3-eu-west-1.amazonaws.com/seq-repo/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
     }
 }
