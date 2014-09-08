2ebe48c922b63f48060f1dd8b0a956e456b45785
Richard Doktorics <doktoricsrichard@gmail.com>
2014-09-08 12:54:01 +0200

    CLOUD-210 added public key validator

---
+++ src/main/java/com/sequenceiq/cloudbreak/service/credential/azure/AzureCredentialInitializer.java
@@ -0,0 +1,38 @@
+package com.sequenceiq.cloudbreak.service.credential.azure;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.controller.BadRequestException;
+import com.sequenceiq.cloudbreak.domain.AzureCredential;
+
+@Component
+public class AzureCredentialInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureCredentialInitializer.class);
+
+    public AzureCredential init(AzureCredential azureCredential) {
+        validateCertificateFile(azureCredential);
+        return azureCredential;
+    }
+
+    private void validateCertificateFile(AzureCredential azureCredential) {
+        try {
+            InputStream is = new ByteArrayInputStream(azureCredential.getPublicKey().getBytes(StandardCharsets.UTF_8));
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate x509Certificate = (X509Certificate) cf.generateCertificate(is);
+        } catch (Exception e) {
+            String errorMessage = String.format("Could not validate publickey certificate [credential: '%s', certificate: '%s'], detailed message: %s",
+                    azureCredential.getId(), azureCredential.getPublicKey(), e.getMessage());
+            LOGGER.error(errorMessage, e);
+            throw new BadRequestException(errorMessage, e);
+        }
+    }
+}
