d28552155b524faacafc1945f76c0151f95c4a40
Richard Doktorics <doktoricsrichard@gmail.com>
2014-09-10 18:11:19 +0200

    CLOUD-225 failed application start fix

--- src/main/java/com/sequenceiq/cloudbreak/repository/CloudbreakEventRepository.java
+++ src/main/java/com/sequenceiq/cloudbreak/repository/CloudbreakEventRepository.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -12,10 +11,8 @@ import com.sequenceiq.cloudbreak.domain.CloudbreakEvent;
 
 public interface CloudbreakEventRepository extends CrudRepository<CloudbreakEvent, Long>, JpaSpecificationExecutor {
 
-    @Query("select cbe from CloudbreakEvent cbe where cbe.userId= :userId")
     List<CloudbreakEvent> cloudbreakEvents(@Param("userId") Long userId);
 
-    @Query("select cbe from CloudbreakEvent cbe where cbe.userId= :userId and cbe.eventTimestamp > :since")
     List<CloudbreakEvent> cloudbreakEventsSince(@Param("userId") Long userId, @Param("since") Date since);
 
 }
