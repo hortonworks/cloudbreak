3c615bf94725bc69c16e9cabc8020f666544dfce
lpuskas <laszlo.puskas@sequenceiq.com>
2014-09-10 16:30:05 +0200

    CLOUD-225 entity annotated with name

--- src/main/java/com/sequenceiq/cloudbreak/domain/CloudbreakEvent.java
+++ src/main/java/com/sequenceiq/cloudbreak/domain/CloudbreakEvent.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "cloudbreakevent")
 public class CloudbreakEvent implements ProvisionEntity {
 
     @Id
