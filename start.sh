c2d234e7cfab1dd6cdcf42d5e712e28c0eb80f5c
oleewere <oleewere@gmail.com>
2014-09-29 01:34:42 +0200

    CLOUD-229 added missing env variables

--- start.sh
+++ start.sh
@@ -15,6 +15,31 @@ if [ -z "$UAA_PORT" ]; then
   MISSING_ENV_VARS=true;
 fi
 
+if [ -z "$UR_SMTP_SENDER_HOST" ]; then
+  echo UR_SMTP_SENDER_HOST must be set;
+  MISSING_ENV_VARS=true;
+fi
+
+if [ -z "$UR_SMTP_SENDER_PORT" ]; then
+  echo UR_SMTP_SENDER_PORT must be set;
+  MISSING_ENV_VARS=true;
+fi
+
+if [ -z "$UR_SMTP_SENDER_USERNAME" ]; then
+  echo UR_SMTP_SENDER_USERNAME must be set;
+  MISSING_ENV_VARS=true;
+fi
+
+if [ -z "$UR_SMTP_SENDER_PASSWORD" ]; then
+  echo UR_SMTP_SENDER_PASSWORD must be set;
+  MISSING_ENV_VARS=true;
+fi
+
+if [ -z "$UR_SMTP_SENDER_FROM" ]; then
+  echo UR_SMTP_SENDER_FROM must be set;
+  MISSING_ENV_VARS=true;
+fi
+
 if [ $MISSING_ENV_VARS ]; then
   exit 1;
 fi
