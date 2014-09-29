d094664224f9ba6b68378040de8542d9b1b14c9d
martonsereg <marton.sereg@sequenceiq.com>
2014-09-29 10:30:49 +0200

    CLOUD-216 refactored Usage/Event interfaces to match this branch

--- src/main/java/com/sequenceiq/cloudbreak/service/usages/DefaultCloudbreakUsagesRetrievalService.java
+++ src/main/java/com/sequenceiq/cloudbreak/service/usages/DefaultCloudbreakUsagesRetrievalService.java
@@ -1,9 +1,7 @@
 package com.sequenceiq.cloudbreak.service.usages;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.stereotype.Service;
 
-import com.sequenceiq.cloudbreak.domain.Account;
 import com.sequenceiq.cloudbreak.domain.CloudbreakUsage;
-import com.sequenceiq.cloudbreak.domain.User;
-import com.sequenceiq.cloudbreak.repository.AccountRepository;
 import com.sequenceiq.cloudbreak.repository.CloudbreakUsageRepository;
 import com.sequenceiq.cloudbreak.repository.CloudbreakUsageSpecifications;
 
@@ -29,7 +24,7 @@ public class DefaultCloudbreakUsagesRetrievalService implements CloudbreakUsages
     public List<CloudbreakUsage> findUsagesForUser(String user, Long since, String cloud, String zone, String vmtype, String hours) {
 
         List<CloudbreakUsage> usages = usageRepository.findAll(Specifications
-                .where(CloudbreakUsageSpecifications.usagesWithLongField("userId", user))
+                .where(CloudbreakUsageSpecifications.usagesWithStringFields("userId", user))
                 .and(CloudbreakUsageSpecifications.usagesSince(since))
                 .and(CloudbreakUsageSpecifications.usagesWithStringFields("cloud", cloud))
                 .and(CloudbreakUsageSpecifications.usagesWithStringFields("zone", zone))
@@ -40,24 +35,24 @@ public class DefaultCloudbreakUsagesRetrievalService implements CloudbreakUsages
     }
 
     @Override
-    public List<CloudbreakUsage> findUsagesForAccount(String account, String filterUserid, Long since, String cloud, String zone, String vmtype, String hours) {
+    public List<CloudbreakUsage> findUsagesForAccount(String account, String filterUser, Long since, String cloud, String zone, String vmtype, String hours) {
         List<CloudbreakUsage> usages = new ArrayList<>();
-        Set<User> accountUsers = accountRepository.accountUsers(accountId);
-        for (User user : accountUsers) {
-            usages.addAll(findUsagesForUser(user.getId(), since, cloud, zone, vmtype, hours));
-        }
+//        Set<User> accountUsers = accountRepository.accountUsers(account);
+//        for (User user : accountUsers) {
+//            usages.addAll(findUsagesForUser(user.getId(), since, cloud, zone, vmtype, hours));
+//        }
         return usages;
     }
 
     @Override
-    public List<CloudbreakUsage> findUsagesForDeployer(Long filterAccountId, Long filterUserid, Long since, String cloud, String zone,
+    public List<CloudbreakUsage> findUsagesForDeployer(String filterAccount, String filterUser, Long since, String cloud, String zone,
             String vmtype, String hours) {
         List<CloudbreakUsage> usages = new ArrayList<>();
-        Iterator<Account> accountIt = accountRepository.findAll().iterator();
-
-        while (accountIt.hasNext()) {
-            usages.addAll(findUsagesForAccount(accountIt.next().getId(), filterUserid, since, cloud, zone, vmtype, hours));
-        }
+//        Iterator<Account> accountIt = accountRepository.findAll().iterator();
+//
+//        while (accountIt.hasNext()) {
+//            usages.addAll(findUsagesForAccount(accountIt.next().getId(), filterUser, since, cloud, zone, vmtype, hours));
+//        }
 
         return usages;
     }
