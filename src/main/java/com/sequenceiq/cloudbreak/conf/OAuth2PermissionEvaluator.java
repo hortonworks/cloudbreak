e59ba869e170e4f36163f2390c63e92f48d76ca3
martonsereg <marton.sereg@sequenceiq.com>
2014-10-02 13:31:16 +0200

    CLOUD-216 fetching user details refactored from filter

---
+++ src/main/java/com/sequenceiq/cloudbreak/conf/OAuth2PermissionEvaluator.java
@@ -0,0 +1,21 @@
+package com.sequenceiq.cloudbreak.conf;
+
+import java.io.Serializable;
+
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2PermissionEvaluator implements PermissionEvaluator {
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        return true;
+    }
+}
---
+++ src/main/java/com/sequenceiq/cloudbreak/service/user/RemoteUserDetailsService.java
@@ -0,0 +1,91 @@
+package com.sequenceiq.cloudbreak.service.user;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.sequenceiq.cloudbreak.domain.CbUser;
+
+@Service
+public class RemoteUserDetailsService implements UserDetailsService {
+
+    private static final int ACCOUNT_PART = 2;
+    private static final int ROLE_PART = 3;
+
+    @Value("${cb.client.id}")
+    private String clientId;
+
+    @Value("${cb.client.secret}")
+    private String clientSecret;
+
+    @Value("${cb.identity.server.url}")
+    private String identityServerUrl;
+
+    @Override
+    public CbUser getDetails(String username) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders tokenRequestHeaders = new HttpHeaders();
+        tokenRequestHeaders.set("Authorization", getAuthorizationHeader(clientId, clientSecret));
+
+        Map<String, String> tokenResponse = restTemplate.exchange(
+                identityServerUrl + "/oauth/token?grant_type=client_credentials",
+                HttpMethod.POST,
+                new HttpEntity<>(tokenRequestHeaders),
+                Map.class).getBody();
+
+        HttpHeaders scimRequestHeaders = new HttpHeaders();
+        scimRequestHeaders.set("Authorization", "Bearer " + tokenResponse.get("access_token"));
+
+        String scimResponse = restTemplate.exchange(
+                identityServerUrl + "/Users/" + "?filter=userName eq '" + username + "'",
+                HttpMethod.GET,
+                new HttpEntity<>(scimRequestHeaders),
+                String.class).getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(scimResponse);
+            List<String> roles = new ArrayList<>();
+            String account = null;
+            for (Iterator<JsonNode> iterator = root.get("resources").get(0).get("groups").getElements(); iterator.hasNext();) {
+                JsonNode node = iterator.next();
+                String group = node.get("display").asText();
+                if (group.startsWith("cloudbreak.account")) {
+                    String[] parts = group.split("\\.");
+                    if (account != null && !account.equals(parts[2])) {
+                        throw new IllegalStateException("A user can belong to only one account.");
+                    }
+                    account = parts[ACCOUNT_PART];
+                    roles.add(parts[ROLE_PART]);
+                }
+            }
+            return new CbUser(username, account, roles);
+        } catch (IOException e) {
+            throw new UserDetailsUnavailableException("User details cannot be retrieved from identity server.", e);
+        }
+    }
+
+    private String getAuthorizationHeader(String clientId, String clientSecret) {
+        String creds = String.format("%s:%s", clientId, clientSecret);
+        try {
+            return "Basic " + new String(Base64.encode(creds.getBytes("UTF-8")));
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("Could not convert String");
+        }
+    }
+}
