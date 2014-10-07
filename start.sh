b376bffe2b0b6566e7ae7101191181de8c452534
martonsereg <marton.sereg@sequenceiq.com>
2014-10-07 10:18:15 +0200

    CLOUD-216 docker startup script

--- start.sh
+++ start.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-: ${ULU_CLOUDBREAK_HOST:=$ULU_CB_PORT_8080_TCP_ADDR}
-: ${ULU_CLOUDBREAK_PORT:=$ULU_CB_PORT_8080_TCP_PORT}
-
-
-export ULU_SERVER_PORT=
-export ULU_CLOUDBREAK_ADDRESS=
-export ULU_IDENTITY_ADDRESS=
-export ULU_OAUTH_CLIENT_ID=
-export ULU_IDENTITY_POR=
-export ULU_OAUTH_CLIENT_SECRET=
-export ULU_OAUTH_REDIRECT_URI=
-
-
-
-if [ -z "$ULU_OAUTH_CLIENT_ID" ]; then
-  echo ULU_OAUTH_CLIENT_ID must be set;
-  ULU_OAUTH_CLIENT_ID=true;
+if [ -z "$ULU_CLOUDBREAK_ADDRESS" ]; then
+  echo ULU_CLOUDBREAK_ADDRESS must be set;
+  MISSING_ENV_VARS=true;
 fi
 
 if [ -z "$ULU_IDENTITY_ADDRESS" ]; then
@@ -43,5 +29,5 @@ if [ $MISSING_ENV_VARS ]; then
   exit 1;
 fi
 
-npm install
-node server.js
+cd /uluwatu && node server.js
+
