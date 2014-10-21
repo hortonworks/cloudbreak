e3bb255050f306b4067ac8e448e0199ac04b8da2
Richard Doktorics <doktoricsrichard@gmail.com>
2014-10-21 11:59:39 +0200

    PERI-6 added versioning

--- gradle/config/buildConfig.groovy
+++ gradle/config/buildConfig.groovy
@@ -2,12 +2,20 @@ environments {
     jenkins {
         sonar_host_url = "http://$System.env.SONAR_PORT_9000_TCP_ADDR:9000"
         sonar_jdbc_url = "jdbc:mysql://$System.env.SONAR_DB_PORT_3306_TCP_ADDR:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
+<<<<<<< HEAD
         metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
+=======
+        metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/periscope/maven-metadata.xml"
+>>>>>>> 6e140e9... PERI-6 added versioning
     }
 
     local {
         sonar_host_url = "http://localhost:9000"
         sonar_jdbc_url = "jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
+<<<<<<< HEAD
         metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
+=======
+        metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/periscope/maven-metadata.xml"
+>>>>>>> 6e140e9... PERI-6 added versioning
     }
 }
