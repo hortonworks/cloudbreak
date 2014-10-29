23d4cfbc5021d0aaf1c0e795e8214dfbf57ac9f4
Richard Doktorics <doktoricsrichard@gmail.com>
2014-10-29 09:30:15 +0100

    CLOUD-265 removed zone

--- app/static/js/controllers/templateController.js
+++ app/static/js/controllers/templateController.js
@@ -51,7 +51,6 @@ angular.module('uluwatuControllers').controller('templateController', ['$scope',
             'US_CENTRAL1_A': 'us-central1-a',
             'US_CENTRAL1_B': 'us-central1-b',
             'US_CENTRAL1_F': 'us-central1-f',
-            'EUROPE_WEST1_A': 'europe-west1-a',
             'EUROPE_WEST1_B': 'europe-west1-b',
             'ASIA_EAST1_A': 'asia-east1-a',
             'ASIA_EAST1_B': 'asia-east1-b'
