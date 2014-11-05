d1915a29e1bb8b8b0def9097aea4ce0cd05ba52b
Tamas Bihari <tamas.bihari@sequenceiq.com>
2014-11-05 16:46:14 +0100

    CLOUD-106 remove unnecessary example scripts from events.sh

--- test-scripts/events.sh
+++ test-scripts/events.sh
@@ -25,10 +25,7 @@ echo "Retrieve events ..."
 curl --config curl_config_admin_user  -X GET "http://$API_HOST:$API_PORT/user/usages?vmtype=T2Micro" | jq '.'
 
 #get token from uaa
-curl -iX POST -H "accept: application/x-www-form-urlencoded" -d 'credentials={"username":"wilbur@sequenceiq.com","password":"sargunaraj"}' "http://qa.uaa.sequenceiq.com/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell" | grep Location | cut -d'=' -f 2 | cut -d'&' -f 1
-
-#add blueprint
-#curl -H "authorization:Bearer TOKEN..." -sX POST -H "Content-Type:application/json" -d '{"name":"singlenode-yarn","url":"https://raw.githubusercontent.com/sequenceiq/cloudbreak/master/src/main/resources/blueprints/single-node-hdfs-yarn.bp"}' http://localhost:8090/user/blueprints | jq .
+curl -iX POST -H "accept: application/x-www-form-urlencoded" -d 'credentials={"username":"...","password":"..."}' "http://qa.uaa.sequenceiq.com/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell" | grep Location | cut -d'=' -f 2 | cut -d'&' -f 1
 
 #get user events
 curl -H "authorization:Bearer token...." -X GET "http://localhost:8090/events" | jq '.'
@@ -39,7 +36,3 @@ curl -H "authorization:Bearer token...." -X GET "http://localhost:8090/usages/ge
 curl -X PUT -H "Content-Type:application/json" -H "Authorization: Bearer token..." -d '{"status":"STOPPED"}' http://localhost:8090/stacks/100/cluster | jq .
 #stop(start) stack
 curl -X PUT -H "Content-Type:application/json" -H "Authorization: Bearer token..." -d '{"status":"STOPPED"}' http://localhost:8090/stacks/100 | jq .
-
-
-eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjNmMyOWEzNS1iZTQxLTQ1MGQtYmE2YS02NzJjMDdjODc2NWYiLCJzdWIiOiIxM2NhN2RhNC04MDMyLTQ2NDEtODllOC0yN2JlZjhjNDRiZmUiLCJzY29wZSI6WyJjbG91ZGJyZWFrLnRlbXBsYXRlcyIsImNsb3VkYnJlYWsuY3JlZGVudGlhbHMiLCJjbG91ZGJyZWFrLnN0YWNrcyIsInBhc3N3b3JkLndyaXRlIiwib3BlbmlkIiwiY2xvdWRicmVhay5ibHVlcHJpbnRzIl0sImNsaWVudF9pZCI6ImNsb3VkYnJlYWtfc2hlbGwiLCJjaWQiOiJjbG91ZGJyZWFrX3NoZWxsIiwidXNlcl9pZCI6IjEzY2E3ZGE0LTgwMzItNDY0MS04OWU4LTI3YmVmOGM0NGJmZSIsInVzZXJfbmFtZSI6IndpbGJ1ckBzZXF1ZW5jZWlxLmNvbSIsImVtYWlsIjoid2lsYnVyQHNlcXVlbmNlaXEuY29tIiwiaWF0IjoxNDEzNTMzOTM2LCJleHAiOjE0MTM1NzcxMzYsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC91YWEvb2F1dGgvdG9rZW4iLCJhdWQiOlsiY2xvdWRicmVhayIsIm9wZW5pZCIsInBhc3N3b3JkIl19.iEDO8NIYWN2EjAD_OUzxphr468IhOgmT0pxdqmlfglg
-
