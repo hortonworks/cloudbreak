c6add751d1fb73e90e47347c9bffb459187e8833
martonsereg <marton.sereg@sequenceiq.com>
2014-11-06 17:44:28 +0100

    CLOUD-267 dummy notification sending mechanism

--- src/main/java/com/sequenceiq/cloudbreak/websocket/UptimeNotifier.java
+++ src/main/java/com/sequenceiq/cloudbreak/websocket/UptimeNotifier.java
@@ -1,10 +1,7 @@
 package com.sequenceiq.cloudbreak.websocket;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,10 +9,10 @@ import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.domain.Cluster;
 import com.sequenceiq.cloudbreak.domain.Stack;
-import com.sequenceiq.cloudbreak.domain.WebsocketEndPoint;
 import com.sequenceiq.cloudbreak.repository.ClusterRepository;
 import com.sequenceiq.cloudbreak.repository.StackRepository;
-import com.sequenceiq.cloudbreak.websocket.message.UptimeMessage;
+import com.sequenceiq.cloudbreak.service.notification.Notification;
+import com.sequenceiq.cloudbreak.service.notification.NotificationSender;
 
 @Component
 public class UptimeNotifier {
@@ -29,26 +26,22 @@ public class UptimeNotifier {
     @Autowired
     private StackRepository stackRepository;
 
+    @Autowired
+    private NotificationSender notificationSender;
+
     @Scheduled(fixedDelay = 60000)
     public void sendUptime() {
-        Map<String, List<UptimeMessage>> uptimes = new HashMap<>();
         List<Cluster> clusters = (List<Cluster>) clusterRepository.findAll();
         long now = new Date().getTime();
         for (Cluster cluster : clusters) {
             Stack stack = stackRepository.findStackForCluster(cluster.getId());
             if (stack != null) {
                 Long uptime = cluster.getCreationFinished() == null ? 0L : now - cluster.getCreationFinished();
-                if (uptimes.containsKey(stack.getOwner())) {
-                    uptimes.get(stack.getOwner()).add(new UptimeMessage(stack.getId(), uptime));
-                } else {
-                    uptimes.put(stack.getOwner(), new ArrayList<UptimeMessage>());
-                    uptimes.get(stack.getOwner()).add(new UptimeMessage(stack.getId(), uptime));
-                }
-            }
-        }
-        if (!uptimes.isEmpty()) {
-            for (Map.Entry<String, List<UptimeMessage>> longListEntry : uptimes.entrySet()) {
-                websocketService.sendToTopicUser(longListEntry.getKey(), WebsocketEndPoint.UPTIME, longListEntry.getValue());
+                Notification notification = new Notification();
+                notification.setUserName(stack.getOwner());
+                notification.setStackId(stack.getId());
+                notification.setEventMessage(String.valueOf(uptime));
+                notificationSender.send(notification);
             }
         }
     }
