cf881d6062d5c769426de8a8281d3efb93238b13
Tamas Bihari <tamas.bihari@sequenceiq.com>
2014-11-12 15:47:19 +0100

    CLOUD-282 usage filtering by blueprint name and id

--- src/main/java/com/sequenceiq/cloudbreak/facade/CloudbreakUsagesFacade.java
+++ src/main/java/com/sequenceiq/cloudbreak/facade/CloudbreakUsagesFacade.java
@@ -6,7 +6,8 @@ import com.sequenceiq.cloudbreak.controller.json.CloudbreakUsageJson;
 
 public interface CloudbreakUsagesFacade {
 
-    List<CloudbreakUsageJson> getUsagesFor(String account, String owner, Long since, String cloud, String zone, String vmtype, String hours);
+    List<CloudbreakUsageJson> getUsagesFor(String account, String owner,
+            Long since, String cloud, String zone, String vmtype, String hours, String bpName, Long bpId);
 
     void generateUserUsages();
 
