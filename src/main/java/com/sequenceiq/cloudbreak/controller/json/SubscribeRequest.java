f3d1a22ba2112943cca22c077639269997e601a2
Tamas Bihari <tamas.bihari@sequenceiq.com>
2014-11-20 10:44:51 +0100

    CLOUD-267 validate url field in SubscribeRequest

--- src/main/java/com/sequenceiq/cloudbreak/controller/json/SubscribeRequest.java
+++ src/main/java/com/sequenceiq/cloudbreak/controller/json/SubscribeRequest.java
@@ -1,7 +1,15 @@
 package com.sequenceiq.cloudbreak.controller.json;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import com.sequenceiq.cloudbreak.conf.EnvironmentValidator;
+
 public class SubscribeRequest {
 
+    @NotNull
+    @Pattern(regexp = EnvironmentValidator.URL_PATTERN,
+            message = "Must be a proper URL!")
     private String endpointUrl;
 
     public String getEndpointUrl() {
