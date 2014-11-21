bcb3658253a235cfd6a6dabefd395ffbd571a803
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-11-21 13:57:34 +0100

    CLOUD-309 configurable reactor thread pool size with default value

--- src/main/java/com/sequenceiq/cloudbreak/conf/ReactorConfig.java
+++ src/main/java/com/sequenceiq/cloudbreak/conf/ReactorConfig.java
@@ -1,11 +1,13 @@
 package com.sequenceiq.cloudbreak.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import reactor.core.Environment;
 import reactor.core.Reactor;
 import reactor.core.spec.Reactors;
+import reactor.event.dispatch.ThreadPoolExecutorDispatcher;
 
 @Configuration
 public class ReactorConfig {
@@ -40,6 +42,9 @@ public class ReactorConfig {
 
     public static final String CLOUDBREAK_EVENT = "CLOUDBREAK_EVENT";
 
+    @Value("${cb.reactor.threadpool.core.size:100}")
+    private int reactorThreadPoolSize;
+
     @Bean
     public Environment env() {
         return new Environment();
@@ -49,7 +54,11 @@ public class ReactorConfig {
     public Reactor reactor(Environment env) {
         return Reactors.reactor()
                 .env(env)
-                .dispatcher(Environment.THREAD_POOL)
+                .dispatcher(getReactorDispatcher())
                 .get();
+    }
+
+    private ThreadPoolExecutorDispatcher getReactorDispatcher() {
+        return new ThreadPoolExecutorDispatcher(reactorThreadPoolSize, reactorThreadPoolSize);
     }
 }
