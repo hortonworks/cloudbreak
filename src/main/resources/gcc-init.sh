f11c796e75162b4d3d16dcf72b7909f4afde9790
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-12-02 21:42:06 +0100

    CLOUD-306 use only consul as DNS

--- src/main/resources/gcc-init.sh
+++ src/main/resources/gcc-init.sh
@@ -1,70 +1,164 @@
-: ${NODE_PREFIX=amb}
-: ${MYDOMAIN:=mycorp.kom}
-: ${IMAGE:="sequenceiq/ambari:1.7.0-warmup"}
-: ${INSTANCE_ID:="0"}
+: ${DOCKER_TAG:=1.6.0-consul}
+: ${CONSUL_IMAGE:=sequenceiq/consul:v0.4.1.ptr}
 
-# instance id from ec2 metadata
-INSTANCE_ID=$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google")
-# jq expression that selects the json entry of the current instance from the array returned by the metadata service
-INSTANCE_SELECTOR='. | map(select(.longName == "'$INSTANCE_ID'"))'
-# jq expression to select ambariServer from metadata
-AMBARI_SERVER_SELECTOR='. | map(select(.ambariServer))'
-# jq expression that selects all the other json entries
-OTHER_INSTANCES_SELECTOR='. | map(select(.instanceId != "'$INSTANCE_ID'"))'
+set -x
 
-# metadata service returns '204: no-content' if metadata is not ready yet and '200: ok' if it's completed
-# every other http status codes mean that something unexpected happened
-METADATA_STATUS=204
-MAX_RETRIES=60
-RETRIES=0
-while [ $METADATA_STATUS -eq 204 ] || [ $METADATA_STATUS -eq 000 ] && [ $RETRIES -ne $MAX_RETRIES ]; do
-  METADATA_STATUS=$(curl -sk -o /tmp/metadata_result -w "%{http_code}" -X GET -H Content-Type:application/json $METADATA_ADDRESS/stacks/metadata/$METADATA_HASH);
-  echo "Metadata service returned status code: $METADATA_STATUS";
-  [ $METADATA_STATUS -eq 204 ] || [ $METADATA_STATUS -eq 000 ] && sleep 5 && ((RETRIES++));
-done
-
-[ $METADATA_STATUS -ne 200 ] && exit 1;
-
-METADATA_RESULT=$(cat /tmp/metadata_result)
-
-# format and mount disks
-VOLUME_COUNT=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].volumeCount' | sed s/\"//g)
-START_LABEL=61
-for (( i=1; i<=VOLUME_COUNT; i++ )); do
-  LABEL=$(printf "\x$((START_LABEL+i))")
-  mkfs -F -t ext4 /dev/sd${LABEL}
-  mkdir /mnt/fs${i}
-  mount /dev/sd${LABEL} /mnt/fs${i}
-  DOCKER_VOLUME_PARAMS="${DOCKER_VOLUME_PARAMS} -v /mnt/fs${i}:/mnt/fs${i}"
-done
-
-docker-running() {
-    if docker info &> /dev/null ; then
-        echo UP;
-    else
-        echo DOWN;
-    fi;
+get_ip() {
+  ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
 }
-: ${DOCKER_MAX_RETRIES:=60}
-local docker_retries
-docker_retries=0
-while [[ "$(docker-running)" == "DOWN" ]] && [ $docker_retries -ne $DOCKER_MAX_RETRIES ];
-do
-	service docker restart
-	sleep 8
-	((docker_retries++))
-done
 
- # determines if this instance is the Ambari server or not and sets the tags accordingly
-AMBARI_SERVER=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].ambariServer' | sed s/\"//g)
-[ "$AMBARI_SERVER" == true ] && AMBARI_ROLE="--tag ambari-server=true" || AMBARI_ROLE=""
-INSTANCE_IDX=$(echo $METADATA_RESULT | jq "$INSTANCE_SELECTOR" | jq '.[].instanceIndex' | sed s/\"//g)
-AMBARI_SERVER_IP=$(echo $METADATA_RESULT | jq "$AMBARI_SERVER_SELECTOR" | jq '.[].privateIp' | sed s/\"//g)
-AMBARI_SERVER_DOCKER_SUBNET=$(echo $METADATA_RESULT | jq "$AMBARI_SERVER_SELECTOR" | jq '.[].dockerSubnet' | sed s/\"//g)
-CMD="docker run -d $DOCKER_VOLUME_PARAMS -e SERF_JOIN_IP=$AMBARI_SERVER_IP --net=host --name ${NODE_PREFIX}${INSTANCE_IDX} --entrypoint /usr/local/serf/bin/start-serf-agent.sh  $IMAGE $AMBARI_ROLE"
-cat << EOF
-=========================================
-CMD=$CMD
-=========================================
-EOF
-$CMD
+fix_hostname() {
+  if grep -q $(get_ip) /etc/hosts ;then
+    sed -i "/$(get_ip)/d" /etc/hosts
+  else
+    echo OK
+  fi
+}
+
+get_vpc_peers() {
+  if [ -z "$METADATA_RESULT" ]; then
+    # metadata service returns '204: no-content' if metadata is not ready yet and '200: ok' if it's completed
+    # every other http status codes mean that something unexpected happened
+    METADATA_STATUS=204
+    MAX_RETRIES=60
+    RETRIES=0
+    while [ $METADATA_STATUS -eq 204 ] || [ $METADATA_STATUS -eq 504 ] && [ $RETRIES -ne $MAX_RETRIES ]; do
+      METADATA_STATUS=$(curl -sk -o /tmp/metadata_result -w "%{http_code}" -X GET -H Content-Type:application/json $METADATA_ADDRESS/stacks/metadata/$METADATA_HASH);
+      echo "Metadata service returned status code: $METADATA_STATUS";
+      [ $METADATA_STATUS -eq 204 ] && sleep 5 && ((RETRIES++));
+    done
+
+    [ $METADATA_STATUS -ne 200 ] && exit 1;
+  fi
+
+  METADATA_RESULT=$(cat /tmp/metadata_result)
+  echo $METADATA_RESULT | jq .[].privateIp -r
+}
+
+meta_order() {
+  get_vpc_peers | xargs -n 1 | sort | cat -n | sed 's/ *//;s/\t/ /'
+}
+
+my_order() {
+  local myip=$(get_ip)
+  meta_order | grep ${myip} | cut -d" " -f 1
+}
+
+consul_join_ip() {
+  meta_order | head -1 | cut -d" " -f 2
+}
+
+start_consul() {
+
+  CONSUL_OPTIONS="-advertise $(get_ip)"
+
+  if [ $(my_order) -gt 1 ]; then
+    CONSUL_OPTIONS="$CONSUL_OPTIONS -retry-join $(consul_join_ip)"
+  fi
+
+  if [ $(my_order) -le 3 ]; then
+    CONSUL_OPTIONS="$CONSUL_OPTIONS -server -bootstrap-expect 3"
+  fi
+
+  docker rm -f consul &> /dev/null
+  docker run -d \
+    --name consul \
+    --net=host \
+    --restart=always \
+    $CONSUL_IMAGE $CONSUL_OPTIONS
+}
+
+consul_leader() {
+  local leader=$(curl -s 127.0.0.1:8500/v1/status/leader|jq . -r)
+  while [ -z "$leader" ]; do
+    sleep 1
+    leader=$(curl -s 127.0.0.1:8500/v1/status/leader|jq . -r)
+  done
+  echo ${leader%:*}
+}
+
+con() {
+  declare path="$1"
+  shift
+  local consul_ip=127.0.0.1
+
+  curl ${consul_ip}:8500/v1/${path} "$@"
+}
+
+register_ambari() {
+  JSON=$(cat <<ENDOFJSON
+  {
+     "ID":"$(hostname -i):ambari:8080",
+     "Name":"ambari-8080",
+     "Port":8080,
+     "Check":null
+  }
+ENDOFJSON
+  )
+
+  con agent/service/register -X PUT -d @- <<<"$JSON"
+}
+
+start_ambari_server() {
+  docker rm -f ambari-server &>/dev/null
+  if [[ "$(consul_leader)" ==  "$(get_ip)" ]]; then
+    docker run -d \
+     --name ambari-server \
+     --net=host \
+     --restart=always \
+     -e BRIDGE_IP=$(get_ip) \
+     sequenceiq/ambari:$DOCKER_TAG /start-server
+
+    register_ambari
+  fi
+}
+
+start_ambari_agent() {
+  set_public_host_script
+  set_disk_as_volumes
+  docker run -d \
+    --name ambari-agent \
+    --net=host \
+    --restart=always \
+    -e BRIDGE_IP=$(get_ip) \
+    $VOLUMES \
+    sequenceiq/ambari:$DOCKER_TAG /start-agent
+}
+
+set_disk_as_volumes() {
+  for fn in `ls /mnt/ | grep fs`; do
+    VOLUMES="$VOLUMES -v /mnt/$fn:/mnt/$fn"
+  done
+}
+
+set_public_host_script() {
+  VOLUMES="$VOLUMES -v /usr/local/public_host_script:/etc/ambari-agent/conf/public-hostname.sh"
+}
+
+register_ambari_add() {
+  cp /usr/local/register-ambari /etc/init.d/register-ambari
+  chmod +x /etc/init.d/register-ambari
+  chown root:root /etc/init.d/register-ambari
+  update-rc.d -f register-ambari defaults
+  update-rc.d -f register-ambari enable
+}
+
+format_disks() {
+  /usr/local/disk_mount.sh
+}
+
+main() {
+  if [[ "$1" == "::" ]]; then
+    shift
+    eval "$@"
+  else
+    format_disks
+    fix_hostname
+    start_consul
+    start_ambari_server
+    start_ambari_agent
+    register_ambari_add
+  fi
+}
+
+[[ "$0" == "$BASH_SOURCE" ]] && main "$@"
