36b0606dfd6566c7c5b302bd3fda3b82455a9496
Richard Doktorics <doktoricsrichard@gmail.com>
2014-12-02 10:03:07 +0100

    CLOUD-200 fixed security issue

--- src/main/java/com/sequenceiq/cloudbreak/repository/CredentialRepository.java
+++ src/main/java/com/sequenceiq/cloudbreak/repository/CredentialRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
 
-import com.sequenceiq.cloudbreak.domain.Blueprint;
 import com.sequenceiq.cloudbreak.domain.Credential;
 
 public interface CredentialRepository extends CrudRepository<Credential, Long> {
