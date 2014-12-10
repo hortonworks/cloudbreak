c2b794da49e2002daecdd07bce5e41eda394c809
oleewere <oleewere@gmail.com>
2014-12-10 14:35:37 +0100

    CLOUD-299 fixed credential form (download)

--- app/static/js/controllers/credentialController.js
+++ app/static/js/controllers/credentialController.js
@@ -65,7 +65,7 @@ angular.module('uluwatuControllers').controller('credentialController', ['$scope
                 $scope.credentialAzure = {};
                 $scope.modifyStatusMessage($rootScope.error_msg.azure_credential_success1 + result.id + $rootScope.error_msg.azure_credential_success2);
                 $scope.modifyStatusClass("has-success");
-                $scope.getAzureCertification(result.id);
+                window.location.href = "credentials/certificate/" + result.id
                 $scope.azureCredentialForm.$setPristine();
                 collapseCreateCredentialFormPanel();
             }, function (error) {
