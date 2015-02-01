0f3ef013f843437b141fe4f644c68d31ae14d208
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-02-01 14:44:20 +0100

    CLOUD-390 use AwsInstanceType instead of  InstanceType

--- src/main/java/com/sequenceiq/cloudbreak/service/price/AwsPriceGenerator.java
+++ src/main/java/com/sequenceiq/cloudbreak/service/price/AwsPriceGenerator.java
@@ -2,7 +2,7 @@ package com.sequenceiq.cloudbreak.service.price;
 
 import org.springframework.stereotype.Component;
 
-import com.amazonaws.services.ec2.model.InstanceType;
+import com.sequenceiq.cloudbreak.domain.AwsInstanceType;
 import com.sequenceiq.cloudbreak.domain.AwsTemplate;
 import com.sequenceiq.cloudbreak.domain.CloudPlatform;
 import com.sequenceiq.cloudbreak.domain.Template;
@@ -14,7 +14,7 @@ public class AwsPriceGenerator implements PriceGenerator {
     public Double calculate(Template template, Long hours) {
         Double result = 0.0;
         AwsTemplate temp = (AwsTemplate) template;
-        InstanceType vmType = temp.getInstanceType();
+        AwsInstanceType vmType = temp.getInstanceType();
         return result;
     }
 
