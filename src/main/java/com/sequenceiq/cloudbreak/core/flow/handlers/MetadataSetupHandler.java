c92e8f401a0567d3d1be933c2c4cbde4959862ab
lpuskas <laszlo.puskas@sequenceiq.com>
2015-02-12 11:26:49 +0100

    CLOUD-446 added facade call skeletons to flow handlers

--- src/main/java/com/sequenceiq/cloudbreak/core/flow/handlers/MetadataSetupHandler.java
+++ src/main/java/com/sequenceiq/cloudbreak/core/flow/handlers/MetadataSetupHandler.java
@@ -2,11 +2,13 @@ package com.sequenceiq.cloudbreak.core.flow.handlers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.core.CloudbreakException;
 import com.sequenceiq.cloudbreak.core.flow.AbstractFlowHandler;
 import com.sequenceiq.cloudbreak.core.flow.ProvisioningContext;
+import com.sequenceiq.cloudbreak.core.flow.ProvisioningFacade;
 
 import reactor.event.Event;
 
@@ -14,16 +16,19 @@ import reactor.event.Event;
 public class MetadataSetupHandler extends AbstractFlowHandler<ProvisioningContext> {
     private static final Logger LOGGER = LoggerFactory.getLogger(MetadataSetupHandler.class);
 
+    @Autowired
+    private ProvisioningFacade provisioningFacade;
+
     @Override
     protected Object execute(Event<ProvisioningContext> event) throws CloudbreakException {
         LOGGER.info("execute() for phase: {}", event.getKey());
-        return event;
+        ProvisioningContext provisioningContext = provisioningFacade.setupMetadata(event.getData());
+        return provisioningContext;
     }
 
     @Override
     protected void handleErrorFlow(Throwable throwable, Object data) {
         LOGGER.info("handleErrorFlow() for phase: {}", ((Event) data).getKey());
-
     }
 
     @Override
@@ -32,3 +37,4 @@ public class MetadataSetupHandler extends AbstractFlowHandler<ProvisioningContex
         return serviceResult;
     }
 }
+
