988278f2fc9b1b84daa918cc6d57f2f5471ffaa1
lpuskas <laszlo.puskas@sequenceiq.com>
2015-02-16 12:05:12 +0100

    CLOUD-475 enhanced context object / interface

--- src/main/java/com/sequenceiq/cloudbreak/core/flow/SimpleProvisioningFacade.java
+++ src/main/java/com/sequenceiq/cloudbreak/core/flow/SimpleProvisioningFacade.java
@@ -1,18 +1,39 @@
 package com.sequenceiq.cloudbreak.core.flow;
 
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.core.CloudbreakException;
+import com.sequenceiq.cloudbreak.domain.CloudPlatform;
+import com.sequenceiq.cloudbreak.service.stack.StackService;
+import com.sequenceiq.cloudbreak.service.stack.connector.ProvisionSetup;
 
 @Service
 public class SimpleProvisioningFacade implements ProvisioningFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleProvisioningFacade.class);
 
+    @Resource
+    private Map<CloudPlatform, ProvisionSetup> provisionSetups;
+
+    @Autowired
+    private StackService stackService;
+
     @Override
     public ProvisioningContext setup(ProvisioningContext provisioningContext) throws CloudbreakException {
         LOGGER.debug("Set up provisioning. Context: {}", provisioningContext);
+        try {
+            provisionSetups.get(provisioningContext.getCloudPlatform()).setupProvisioning(stackService.get(provisioningContext.getStackId()));
+            LOGGER.debug("Provisioning setup done.");
+        } catch (Exception e) {
+            LOGGER.error("Exception during provisioning setup" + e.getMessage());
+            throw new CloudbreakException(e);
+        }
         return provisioningContext;
     }
 
@@ -35,7 +56,7 @@ public class SimpleProvisioningFacade implements ProvisioningFacade {
     }
 
     @Override public ProvisioningContext startAmbari(ProvisioningContext provisioningContext) throws CloudbreakException {
-        LOGGER.debug("Building ambari cluster. Context: {}", provisioningContext);
+        LOGGER.debug("Start Ambari. Context: {}", provisioningContext);
         return provisioningContext;
     }
 
