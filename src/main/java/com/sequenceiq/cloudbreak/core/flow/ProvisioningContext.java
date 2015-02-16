0cecfd84e0ba85a764648fb9fe32e815cbb2d9d9
lpuskas <laszlo.puskas@sequenceiq.com>
2015-02-16 12:05:12 +0100

    CLOUD-475 enhanced context object / interface

--- src/main/java/com/sequenceiq/cloudbreak/core/flow/ProvisioningContext.java
+++ src/main/java/com/sequenceiq/cloudbreak/core/flow/ProvisioningContext.java
@@ -4,4 +4,21 @@ import com.sequenceiq.cloudbreak.domain.CloudPlatform;
 
 public class ProvisioningContext {
     private CloudPlatform cloudPlatform;
+    private Long stackId;
+
+    public CloudPlatform getCloudPlatform() {
+        return cloudPlatform;
+    }
+
+    public void setCloudPlatform(CloudPlatform cloudPlatform) {
+        this.cloudPlatform = cloudPlatform;
+    }
+
+    public Long getStackId() {
+        return stackId;
+    }
+
+    public void setStackId(Long stackId) {
+        this.stackId = stackId;
+    }
 }
