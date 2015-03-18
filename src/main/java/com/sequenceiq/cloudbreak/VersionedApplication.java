49c6677871900140d703aea043df69b857740001
Richard Doktorics <doktoricsrichard@gmail.com>
2015-03-18 11:46:30 +0100

    CLOUD-515 fixed version check

--- src/main/java/com/sequenceiq/cloudbreak/VersionedApplication.java
+++ src/main/java/com/sequenceiq/cloudbreak/VersionedApplication.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.core.io.ClassPathResource;
 
 public class VersionedApplication {
@@ -12,7 +11,11 @@ public class VersionedApplication {
     public static final String LONG_VERSION = "--version";
     public static final String SHORT_VERSION = "-v";
 
-    protected static void start(Class clazz, String[] args) {
+    private VersionedApplication() {
+
+    }
+
+    public boolean showVersionInfo(String[] args) {
         if (checkIfParamVersion(args)) {
             try {
                 if (LONG_VERSION.equals(args[0])) {
@@ -23,16 +26,16 @@ public class VersionedApplication {
             } catch (IOException ex) {
                 System.out.println("The application.properties file not found version is undefined.");
             }
-        } else {
-            SpringApplication.run(clazz, args);
+            return true;
         }
+        return false;
     }
 
-    private static boolean checkIfParamVersion(String[] args) {
+    private boolean checkIfParamVersion(String[] args) {
         return args.length == 1 && (LONG_VERSION.equals(args[0]) || SHORT_VERSION.equals(args[0]));
     }
 
-    private static String readVersionFromClasspath(String fileName, boolean onlyVersion) throws IOException {
+    private String readVersionFromClasspath(String fileName, boolean onlyVersion) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br;
         br = new BufferedReader(new InputStreamReader(new ClassPathResource(fileName).getInputStream(), "UTF-8"));
@@ -48,5 +51,9 @@ public class VersionedApplication {
             }
         }
         return sb.toString();
+    }
+
+    public static VersionedApplication versionedApplication() {
+        return new VersionedApplication();
     }
 }
