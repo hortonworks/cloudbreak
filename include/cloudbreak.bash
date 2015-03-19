b03a3f916f992941472471265de759d44cd81808
lalyos <lalyos@yahoo.com>
2015-03-19 01:41:40 +0100

    Selfupdate and debug added

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -2,9 +2,44 @@ init() {
     echo "cloudbreak-deployer init ..." | yellow
 }
 
+debug() {
+  if [[ "DEBUG" ]]; then
+      echo "[DEBUG] $*" | gray
+  fi
+}
+
 cbd-version() {
     declare desc="Displays the version of Cloudbrek Deployer"
-    bin-version
+    bin-version | green
+
+    echo latest version: | green
+    latest-version | yellow
+}
+
+cbd-update() {
+    declare desc="Updates itself"
+
+    local binver=$(bin-version)
+    local lastver=$(latest-version)
+    local osarch=$(uname -sm | tr " " _ )
+    debug binver=$binver lastver=$lastver|gray
+
+    if [[ ${binver} != ${lastver} ]]; then
+        debug upgrade needed |yellow
+        
+        local url=https://github.com/sequenceiq/cloudbreak-deployer/releases/download/v${lastver}/cloudbreak-deployer_${lastver}_$[osarch].tgz \
+        debug "lates turl: $url"
+        curl -Ls $url | tar -zx -C /usr/local/bin/
+    else
+        debug you have the latest version | green
+    fi
+}
+
+latest-version() {
+  #curl -Ls https://raw.githubusercontent.com/sequenceiq/cloudbreak-deployer/master/VERSION
+  curl -I https://github.com/sequenceiq/cloudbreak-deployer/releases/latest 2>&1 \
+      |sed -n "s/^Location:.*tag.v//p" \
+      | dos2unix
 }
 
 main() {
@@ -13,6 +48,7 @@ main() {
 
     cmd-export cmd-help help
     cmd-export cbd-version version
+    cmd-export cbd-update update
 
 	if [[ "${!#}" == "-h" || "${!#}" == "--help" ]]; then
 		local args=("$@")
