a3b6ff208d6a655b3483e16bdd2a066f429d3dc3
Richard Doktorics <doktoricsrichard@gmail.com>
2015-03-24 09:45:28 +0100

    CLOUD-666 aws user data fix

--- src/main/resources/ec2-init.sh
+++ src/main/resources/ec2-init.sh
@@ -1,11 +1,8 @@
 : ${CONSUL_IMAGE:=sequenceiq/consul:v0.4.1.ptr}
-
 set -x
-
 get_ip() {
   ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'
 }
-
 fix_hostname() {
   if grep -q $(get_ip) /etc/hosts ;then
     sed -i "/$(get_ip)/d" /etc/hosts
@@ -13,7 +10,6 @@ fix_hostname() {
     echo OK
   fi
 }
-
 get_vpc_peers() {
   if [ -z "$METADATA_RESULT" ]; then
     METADATA_STATUS=204
@@ -21,53 +17,39 @@ get_vpc_peers() {
       METADATA_STATUS=$(curl -sk -m 10 -o /tmp/metadata_result -w "%{http_code}" -X GET -H Content-Type:application/json $METADATA_ADDRESS/stacks/metadata/$METADATA_HASH);
       [ $METADATA_STATUS -ne 200 ] && sleep 5;
     done
-
     [ $METADATA_STATUS -ne 200 ] && exit 1;
   fi
-
   METADATA_RESULT=$(cat /tmp/metadata_result)
   echo $METADATA_RESULT | jq .[].privateIp -r
 }
-
 meta_order() {
   get_vpc_peers | xargs -n 1 | sort | cat -n | sed 's/ *//;s/\t/ /'
 }
-
 my_order() {
   local myip=$(get_ip)
   meta_order | grep "\b${myip}$" | cut -d" " -f 1
 }
-
 consul_join_ip() {
   meta_order | head -1 | cut -d" " -f 2
 }
-
 start_consul() {
   get_consul_opts
   docker rm -f consul &> /dev/null
-  docker run -d \
-    --name consul \
-    --net=host \
-    --restart=always \
-    $CONSUL_IMAGE $CONSUL_OPTIONS
+  docker run -d --name consul --net=host --restart=always $CONSUL_IMAGE $CONSUL_OPTIONS
 }
-
 get_consul_opts() {
   CONSUL_OPTIONS="-advertise $(get_ip)"
-
   if does_cluster_exist; then
     CONSUL_OPTIONS="$CONSUL_OPTIONS -retry-join $leader"
   else
     if [ $(my_order) -gt 1 ]; then
       CONSUL_OPTIONS="$CONSUL_OPTIONS -retry-join $(consul_join_ip)"
     fi
-
     if [ $(my_order) -le "$CONSUL_SERVER_COUNT" ]; then
       CONSUL_OPTIONS="$CONSUL_OPTIONS -server -bootstrap-expect $CONSUL_SERVER_COUNT"
     fi
   fi
 }
-
 does_cluster_exist() {
   ip_arr=($(get_vpc_peers))
   for ip in "${ip_arr[@]}"; do
@@ -79,7 +61,6 @@ does_cluster_exist() {
   done
   return 1
 }
-
 consul_leader() {
   local leader=$(curl -s 127.0.0.1:8500/v1/status/leader|jq . -r)
   while [ -z "$leader" ]; do
@@ -88,15 +69,12 @@ consul_leader() {
   done
   echo ${leader%:*}
 }
-
 con() {
   declare path="$1"
   shift
   local consul_ip=127.0.0.1
-
   curl ${consul_ip}:8500/v1/${path} "$@"
 }
-
 register_ambari() {
   JSON=$(cat <<ENDOFJSON
   {
@@ -107,10 +85,8 @@ register_ambari() {
   }
 ENDOFJSON
   )
-
   con agent/service/register -X PUT -d @- <<<"$JSON"
 }
-
 start_ambari_server() {
   docker rm -f ambari-server &>/dev/null
   if [[ "$(consul_leader)" ==  "$(get_ip)" ]]; then
@@ -120,27 +96,26 @@ start_ambari_server() {
     register_ambari
   fi
 }
-
 start_ambari_agent() {
   set_public_host_script
   set_disk_as_volumes
   docker run -d --name=ambari-agent --privileged --net=host --restart=always -e BRIDGE_IP=$(get_ip) -e HADOOP_CLASSPATH=/data/jars/*:/usr/lib/hadoop/lib/* -v /data/jars:/data/jars $VOLUMES sequenceiq/ambari:$AMBARI_DOCKER_TAG /start-agent
 }
-
 set_disk_as_volumes() {
   for fn in `ls /mnt/ | grep fs`; do
     VOLUMES="$VOLUMES -v /mnt/$fn:/mnt/$fn"
   done
 }
-
 set_public_host_script() {
   VOLUMES="$VOLUMES -v /usr/local/public_host_script.sh:/etc/ambari-agent/conf/public-hostname.sh"
 }
-
 format_disks() {
   /usr/local/disk_mount.sh
 }
-
+fix_host_sys() {
+  sed -i "/HOSTNAME/d" /etc/sysconfig/network
+  sh -c ' echo "HOSTNAME=localhost.localdomain" >> /etc/sysconfig/network'
+}
 main() {
   if [[ "$1" == "::" ]]; then
     shift
@@ -148,10 +123,10 @@ main() {
   else
     format_disks
     fix_hostname
+    fix_host_sys
     start_consul
     start_ambari_server
     start_ambari_agent
   fi
 }
-
 [[ "$0" == "$BASH_SOURCE" ]] && main "$@"
