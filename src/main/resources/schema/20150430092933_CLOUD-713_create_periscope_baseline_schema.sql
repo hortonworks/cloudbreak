6cb3e45239b66aa0e736de80e9a58a72273bb328
Attila Kanto <attila.kanto@gmail.com>
2015-04-30 11:37:56 +0200

    CLOUD-713 eliminate hibernate generated fk names

--- src/main/resources/schema/20150430092933_CLOUD-713_create_periscope_baseline_schema.sql
+++ src/main/resources/schema/20150430092933_CLOUD-713_create_periscope_baseline_schema.sql
@@ -1,43 +1,6 @@
 -- // CLOUD-713 create periscope baseline schema
 -- Migration SQL that makes the change goes here.
 
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- Name: postgres; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
 
 --
 -- Name: ambari; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -51,8 +14,6 @@ CREATE TABLE ambari (
     ambari_user character varying(255)
 );
 
-
-ALTER TABLE ambari OWNER TO postgres;
 
 --
 -- Name: cluster; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -70,8 +31,6 @@ CREATE TABLE cluster (
     user_id character varying(255)
 );
 
-
-ALTER TABLE cluster OWNER TO postgres;
 
 --
 -- Name: history; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -93,8 +52,6 @@ CREATE TABLE history (
 );
 
 
-ALTER TABLE history OWNER TO postgres;
-
 --
 -- Name: history_properties; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
 --
@@ -105,8 +62,6 @@ CREATE TABLE history_properties (
     key character varying(255) NOT NULL
 );
 
-
-ALTER TABLE history_properties OWNER TO postgres;
 
 --
 -- Name: metricalert; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -124,8 +79,6 @@ CREATE TABLE metricalert (
 );
 
 
-ALTER TABLE metricalert OWNER TO postgres;
-
 --
 -- Name: notification; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
 --
@@ -137,8 +90,6 @@ CREATE TABLE notification (
 );
 
 
-ALTER TABLE notification OWNER TO postgres;
-
 --
 -- Name: periscope_user; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
 --
@@ -149,8 +100,6 @@ CREATE TABLE periscope_user (
     email character varying(255)
 );
 
-
-ALTER TABLE periscope_user OWNER TO postgres;
 
 --
 -- Name: scalingpolicy; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -165,7 +114,6 @@ CREATE TABLE scalingpolicy (
 );
 
 
-ALTER TABLE scalingpolicy OWNER TO postgres;
 
 --
 -- Name: sequence_table; Type: SEQUENCE; Schema: public; Owner: postgres
@@ -178,8 +126,6 @@ CREATE SEQUENCE sequence_table
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE sequence_table OWNER TO postgres;
 
 --
 -- Name: timealert; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
@@ -195,8 +141,6 @@ CREATE TABLE timealert (
     cluster_id bigint
 );
 
-
-ALTER TABLE timealert OWNER TO postgres;
 
 --
 -- Name: ambari_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
@@ -271,74 +215,62 @@ ALTER TABLE ONLY timealert
 
 
 --
--- Name: fk_3v03tedcm5hbjtcxo1ri6ecbr; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_timealert_cluster_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY timealert
-    ADD CONSTRAINT fk_3v03tedcm5hbjtcxo1ri6ecbr FOREIGN KEY (cluster_id) REFERENCES cluster(id);
+    ADD CONSTRAINT fk_timealert_cluster_id FOREIGN KEY (cluster_id) REFERENCES cluster(id);
 
 
 --
--- Name: fk_c22ixbmoqm2xeoqisctiwtdhb; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_timealert_scalingpolicy_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY timealert
-    ADD CONSTRAINT fk_c22ixbmoqm2xeoqisctiwtdhb FOREIGN KEY (scalingpolicy_id) REFERENCES scalingpolicy(id);
+    ADD CONSTRAINT fk_timealert_scalingpolicy_id FOREIGN KEY (scalingpolicy_id) REFERENCES scalingpolicy(id);
 
 
 --
--- Name: fk_fbrkapmhprkr0lcscre4ym2ap; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_metricalert_scalingpolicy_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY metricalert
-    ADD CONSTRAINT fk_fbrkapmhprkr0lcscre4ym2ap FOREIGN KEY (scalingpolicy_id) REFERENCES scalingpolicy(id);
+    ADD CONSTRAINT fk_metricalert_scalingpolicy_id FOREIGN KEY (scalingpolicy_id) REFERENCES scalingpolicy(id);
 
 
 --
--- Name: fk_ng61bjx78yaahvwkhdco7yxgi; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_cluster_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY cluster
-    ADD CONSTRAINT fk_ng61bjx78yaahvwkhdco7yxgi FOREIGN KEY (user_id) REFERENCES periscope_user(id);
+    ADD CONSTRAINT fk_cluster_user_id FOREIGN KEY (user_id) REFERENCES periscope_user(id);
 
 
 --
--- Name: fk_sbni0w74jud722u3m5p3djspl; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_history_propertiesl_history_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY history_properties
-    ADD CONSTRAINT fk_sbni0w74jud722u3m5p3djspl FOREIGN KEY (history_id) REFERENCES history(id);
+    ADD CONSTRAINT fk_history_propertiesl_history_id FOREIGN KEY (history_id) REFERENCES history(id);
 
 
 --
--- Name: fk_t36ceslxpwdtcag3y1lq7wf6b; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_metricalert_cluster_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY metricalert
-    ADD CONSTRAINT fk_t36ceslxpwdtcag3y1lq7wf6b FOREIGN KEY (cluster_id) REFERENCES cluster(id);
+    ADD CONSTRAINT fk_metricalert_cluster_id FOREIGN KEY (cluster_id) REFERENCES cluster(id);
 
 
 --
--- Name: fk_tq6aoyelc0pf02o5vy6d5qi5p; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: fk_cluster_ambari_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY cluster
-    ADD CONSTRAINT fk_tq6aoyelc0pf02o5vy6d5qi5p FOREIGN KEY (ambari_id) REFERENCES ambari(id);
+    ADD CONSTRAINT fk_cluster_ambari_id FOREIGN KEY (ambari_id) REFERENCES ambari(id);
 
 
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
 
 -- //@UNDO
 -- SQL to undo the change goes here.
