715c5b3b94ede3caa7bac0303d0f0f00705d0284
Attila Kanto <attila.kanto@gmail.com>
2015-05-13 23:08:19 +0200

    Update versions

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -18,13 +18,13 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_CONSUL v0.5.0-v3
     env-import DOCKER_TAG_REGISTRATOR v5
     env-import DOCKER_TAG_POSTGRES 9.4.1
-    env-import DOCKER_TAG_CLOUDBREAK 0.5.34
-    env-import DOCKER_TAG_CBDB 0.5.34
+    env-import DOCKER_TAG_CLOUDBREAK 0.5.39
+    env-import DOCKER_TAG_CBDB 0.5.39
     env-import DOCKER_TAG_PERISCOPE 0.5.3
-    env-import DOCKER_TAG_PCDB 0.5.2
+    env-import DOCKER_TAG_PCDB 0.5.3
     env-import DOCKER_TAG_UAA 1.8.1-v2
     env-import DOCKER_TAG_CBSHELL 0.2.47
-    env-import DOCKER_TAG_ULUWATU 0.5.10
+    env-import DOCKER_TAG_ULUWATU 0.5.11
     env-import DOCKER_TAG_SULTANS 0.5.1
     env-import DOCKER_TAG_AMBASSADOR latest
     env-import DOCKER_TAG_CLOUDBREAK_SHELL 0.4.4
