ca5405a7e2b1e438221cbf0e95acbd462da62e62
lalyos <lalyos@yahoo.com>
2015-05-13 08:54:56 +0200

    Update uaa to 1.8.1-v2

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -22,7 +22,7 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_CBDB 0.5.34
     env-import DOCKER_TAG_PERISCOPE 0.5.3
     env-import DOCKER_TAG_PCDB 0.5.2
-    env-import DOCKER_TAG_UAA 1.8.1-v1
+    env-import DOCKER_TAG_UAA 1.8.1-v2
     env-import DOCKER_TAG_CBSHELL 0.2.47
     env-import DOCKER_TAG_ULUWATU 0.5.10
     env-import DOCKER_TAG_SULTANS 0.5.1
