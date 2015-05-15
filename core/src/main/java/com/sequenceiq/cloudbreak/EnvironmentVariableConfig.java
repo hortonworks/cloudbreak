974e87eeb4e40c31371be54ed8fd13fa8c0234f2
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-05-15 13:54:23 +0200

    CLOUD-720 use warmup ambari image

--- core/src/main/java/com/sequenceiq/cloudbreak/EnvironmentVariableConfig.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/EnvironmentVariableConfig.java
@@ -38,15 +38,15 @@ public class EnvironmentVariableConfig {
     public static final String CB_FAILED_CLUSTER_INSTALLER_MAIL_TEMPLATE_PATH = "templates/cluster-installer-mail-fail.ftl";
 
     public static final String CB_CONTAINER_ORCHESTRATOR = "SWARM";
-    public static final String CB_DOCKER_CONTAINER_AMBARI = "sequenceiq/ambari:2.0.0-consul";
+    public static final String CB_DOCKER_CONTAINER_AMBARI = "sequenceiq/ambari-warmup:2.0.0-consul";
     public static final String CB_DOCKER_CONTAINER_REGISTRATOR = "sequenceiq/registrator:v5.2";
     public static final String CB_DOCKER_CONTAINER_DOCKER_CONSUL_WATCH_PLUGN = "sequenceiq/docker-consul-watch-plugn:2.0.0-consul";
     public static final String CB_DOCKER_CONTAINER_AMBARI_DB = "postgres:9.4.1";
 
-    public static final String CB_AZURE_IMAGE_URI = "https://102589fae040d8westeurope.blob.core.windows.net/images/packer-cloudbreak-2015-05-11-centos6_2015-May-11_14-57-os-2015-05-11.vhd";
-    public static final String CB_AWS_AMI_MAP = "ap-northeast-1:ami-9adc159a,ap-southeast-1:ami-ba6856e8,ap-southeast-2:ami-5592ed6f,eu-west-1:ami-71d0bb06,sa-east-1:ami-ef0186f2,us-east-1:ami-3499885c,us-west-1:ami-87ce20c3,us-west-2:ami-5b794a6b";
-    public static final String CB_OPENSTACK_IMAGE = "cb-centos66-amb200-2015-05-11";
-    public static final String CB_GCP_SOURCE_IMAGE_PATH = "sequenceiqimage/cb-centos66-amb200-2015-05-11-1550.image.tar.gz";
+    public static final String CB_AZURE_IMAGE_URI = "https://102589fae040d8westeurope.blob.core.windows.net/images/packer-cloudbreak-2015-05-15-centos6_2015-May-15_12-38-os-2015-05-15.vhd";
+    public static final String CB_AWS_AMI_MAP = "ap-northeast-1:ami-cad318ca,ap-southeast-1:ami-d4dfe186,ap-southeast-2:ami-9d146aa7,eu-west-1:ami-4384f134,sa-east-1:ami-6deb6d70,us-east-1:ami-16f7ea7e,us-west-1:ami-67aa4323,us-west-2:ami-69526059";
+    public static final String CB_OPENSTACK_IMAGE = "cb-centos66-amb200-2015-05-15";
+    public static final String CB_GCP_SOURCE_IMAGE_PATH = "sequenceiqimage/cb-centos66-amb200-2015-05-15-1403.image.tar.gz";
 
     public static final String CB_GCP_AND_AZURE_USER_NAME = "cloudbreak";
 
