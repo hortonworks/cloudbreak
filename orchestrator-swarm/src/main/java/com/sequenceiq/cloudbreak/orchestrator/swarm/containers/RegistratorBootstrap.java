52e06b342584d53bfdc2bf22a6245318f2857793
doktoric <rdoktorics@hortonworks.com>
2015-05-28 09:52:56 +0200

    CLOUD-820 fixed swarm response problems

--- orchestrator-swarm/src/main/java/com/sequenceiq/cloudbreak/orchestrator/swarm/containers/RegistratorBootstrap.java
+++ orchestrator-swarm/src/main/java/com/sequenceiq/cloudbreak/orchestrator/swarm/containers/RegistratorBootstrap.java
@@ -5,6 +5,7 @@ import static com.sequenceiq.cloudbreak.orchestrator.DockerContainer.REGISTRATOR
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.dockerjava.api.ConflictException;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Bind;
 import com.github.dockerjava.api.model.ExposedPort;
@@ -34,23 +35,27 @@ public class RegistratorBootstrap implements ContainerBootstrap {
 
     @Override
     public Boolean call() throws Exception {
-        LOGGER.info(String.format("Registrator starting on %s node with %s private ip", nodeName, privateIp));
-        HostConfig hostConfig = new HostConfig();
-        hostConfig.setPrivileged(true);
-        hostConfig.setRestartPolicy(RestartPolicy.alwaysRestart());
-        Ports ports = new Ports();
-        ports.add(new PortBinding(new Ports.Binding(PORT), new ExposedPort(PORT)));
-        hostConfig.setPortBindings(ports);
-        String containerId = DockerClientUtil.createContainer(docker, docker.createContainerCmd(imageName)
-                .withEnv(String.format("constraint:node==%s", nodeName))
-                .withHostConfig(hostConfig)
-                .withName(REGISTRATOR.getName())
-                .withCmd(String.format("consul://%s:8500", privateIp)));
-        DockerClientUtil.startContainer(docker, docker.startContainerCmd(containerId)
-                .withBinds(new Bind("/var/run/docker.sock", new Volume("/tmp/docker.sock")))
-                .withRestartPolicy(RestartPolicy.alwaysRestart())
-                .withPortBindings(new PortBinding(new Ports.Binding("0.0.0.0", PORT), new ExposedPort(PORT))));
-        LOGGER.info(String.format("Registrator started successfully on node %s.", nodeName));
+        try {
+            LOGGER.info(String.format("Registrator starting on %s node with %s private ip", nodeName, privateIp));
+            HostConfig hostConfig = new HostConfig();
+            hostConfig.setPrivileged(true);
+            hostConfig.setRestartPolicy(RestartPolicy.alwaysRestart());
+            Ports ports = new Ports();
+            ports.add(new PortBinding(new Ports.Binding(PORT), new ExposedPort(PORT)));
+            hostConfig.setPortBindings(ports);
+            String containerId = DockerClientUtil.createContainer(docker, docker.createContainerCmd(imageName)
+                    .withEnv(String.format("constraint:node==%s", nodeName))
+                    .withHostConfig(hostConfig)
+                    .withName(REGISTRATOR.getName())
+                    .withCmd(String.format("consul://%s:8500", privateIp)));
+            DockerClientUtil.startContainer(docker, docker.startContainerCmd(containerId)
+                    .withBinds(new Bind("/var/run/docker.sock", new Volume("/tmp/docker.sock")))
+                    .withRestartPolicy(RestartPolicy.alwaysRestart())
+                    .withPortBindings(new PortBinding(new Ports.Binding("0.0.0.0", PORT), new ExposedPort(PORT))));
+            LOGGER.info(String.format("Registrator started successfully on node %s.", nodeName));
+        } catch (ConflictException ex) {
+            LOGGER.warn("Swarm api tried to create a container with the same name: {}", ex.getMessage());
+        }
         return true;
     }
 }
