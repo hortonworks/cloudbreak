a387904c12960e149b42a75468d6da3008cb53b9
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-06-04 20:16:43 +0200

    prepare for release 0.5.4

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -20,13 +20,13 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_CONSUL v0.5.0-v3
     env-import DOCKER_TAG_REGISTRATOR v5
     env-import DOCKER_TAG_POSTGRES 9.4.1
-    env-import DOCKER_TAG_CLOUDBREAK 0.5.93
-    env-import DOCKER_TAG_CBDB 0.5.92
+    env-import DOCKER_TAG_CLOUDBREAK 0.5.105
+    env-import DOCKER_TAG_CBDB 0.5.103
     env-import DOCKER_TAG_PERISCOPE 0.5.5
     env-import DOCKER_TAG_PCDB 0.5.5
     env-import DOCKER_TAG_UAA 1.8.1-v2
-    env-import DOCKER_TAG_ULUWATU 0.5.28
-    env-import DOCKER_TAG_SULTANS 0.5.3
+    env-import DOCKER_TAG_ULUWATU 0.5.33
+    env-import DOCKER_TAG_SULTANS 0.5.4
     env-import DOCKER_TAG_AMBASSADOR latest
     env-import DOCKER_TAG_CLOUDBREAK_SHELL 0.4.8
 }
