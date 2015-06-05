fdd2b00f586f698f0382a2bfbe2958a5793e3391
Marton Sereg <msereg@hortonworks.com>
2015-06-05 13:42:55 +0200

    print fingerprint first

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/gcp/GcpConnector.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/gcp/GcpConnector.java
@@ -4,6 +4,8 @@ import static com.sequenceiq.cloudbreak.domain.ResourceType.GCP_INSTANCE;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -123,8 +125,10 @@ public class GcpConnector implements CloudPlatformConnector {
                     .split("cb: -----BEGIN SSH HOST KEY FINGERPRINTS-----|cb: -----END SSH HOST KEY FINGERPRINTS-----")[2]
                     .split("\n");
             for (String fingerprint : fingerprints) {
-                if (fingerprint.contains("(RSA)") && fingerprint.trim().startsWith("cb:")) {
-                    result = fingerprint.split(" ")[10];
+                Pattern fingerprintPattern = Pattern.compile("([a-f0-9]{2}:){15,}[a-f0-9]{2}");
+                Matcher m = fingerprintPattern.matcher(fingerprint);
+                if (m.find()) {
+                    result = m.group(0);
                     break;
                 }
             }
