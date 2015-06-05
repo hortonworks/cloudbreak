8b2d938a12dbf189cb57971042ff15fb1f7e7ec3
lpuskas <laszlo.puskas@sequenceiq.com>
2015-06-05 16:34:52 +0200

    CLOUD-841 fixed transformation. Stack termination tested.

--- cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/notification/ResourcePersistenceNotifier.java
+++ cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/notification/ResourcePersistenceNotifier.java
@@ -3,12 +3,11 @@ package com.sequenceiq.cloudbreak.cloud.notification;
 
 import com.sequenceiq.cloudbreak.cloud.event.context.StackContext;
 import com.sequenceiq.cloudbreak.cloud.model.CloudResource;
-import com.sequenceiq.cloudbreak.cloud.notification.model.ResourceAllocationPersisted;
 
 import reactor.rx.Promise;
 
-public interface ResourcePersistenceNotifier {
+public interface ResourcePersistenceNotifier<T> {
 
-    Promise<ResourceAllocationPersisted> notifyResourceAllocation(CloudResource cloudResource, StackContext stackContext);
+    Promise<T> notifyResourceAllocation(CloudResource cloudResource, StackContext stackContext);
 
 }
