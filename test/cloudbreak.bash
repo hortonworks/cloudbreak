2bca15404b9f11f5208cb9ea8b501d9c5cdb7bf1
lalyos <lalyos@yahoo.com>
2015-09-01 21:07:18 +0200

    sed version test added

--- test/cloudbreak.bash
+++ test/cloudbreak.bash
@@ -1,4 +1,6 @@
-
+T_checkSedVersion() {
+    (sed --version|grep GNU | grep -q '4.2') &>/dev/null || $T_fail "GNU sed 4.2 not found on PATH."
+}
 
 T_consulRecursorOneValid() {
     result=$(consul-recursors <(cat <<EOF
