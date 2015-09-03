21a21a3544bfce1f5b7b4eb08ac72bc0d2a29b35
Attila Kanto <attila.kanto@gmail.com>
2015-09-03 17:08:21 +0200

    CLOUD-43355 version shall be updated after release

--- release-next-ver.sh
+++ release-next-ver.sh
@@ -2,20 +2,22 @@ git checkout master
 git fetch
 git reset --hard origin/master
 
-export OLD_VER=$(cat VERSION)
-export VER="${OLD_VER%.*}.$((${OLD_VER##*.}+1))"
+export VER=$(cat VERSION)
 export REL_DATE="[v${VER}] - $(date +%Y-%m-%d)"
-echo $VER > VERSION
 
 CHANGE_FILE=docs/changelog.md
-# edit changelog 
+# edit changelog
 sed -i "s/## Unreleased/## $REL_DATE/" CHANGELOG.md
 printf '## Unreleased\n\n### Fixed\n\n### Added\n\n### Removed\n\n### Changed\n'| cat - $CHANGE_FILE | tee $CHANGE_FILE
 
-git commit -m "prepare for release $VER" VERSION $CHANGE_FILE
-git push origin master
 
 git checkout -b release-${VER} origin/master
 git push origin release-$VER
 hub pull-request -b release -m "release $VER"
 git checkout master
+
+
+export NEW_VER="${VER%.*}.$((${VER##*.}+1))"
+echo $NEW_VER > VERSION
+git commit -m "Update version to $NEW_VER" VERSION $CHANGE_FILE
+git push origin master
