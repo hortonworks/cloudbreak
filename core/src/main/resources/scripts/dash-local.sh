0316aec130fa9217cf9e052212edb4f6b41ad441
Marton Sereg <msereg@hortonworks.com>
2015-09-15 17:15:56 +0200

    CLOUD-42393 regex, change local mapreduce.tar.gz, find mapred dir based on local hdp version

--- core/src/main/resources/scripts/dash-local.sh
+++ core/src/main/resources/scripts/dash-local.sh
@@ -4,6 +4,7 @@
 
 : ${SOURCE_DIR:=/data/jars}
 : ${STORAGE_JAR:=dash-azure-storage-2.2.0.jar}
+: ${MR_TAR_NAME:=mapreduce.tar.gz}
 
 main(){
   SOURCE_JAR="$SOURCE_DIR/$STORAGE_JAR"
@@ -15,6 +16,29 @@ main(){
 
   echo "Replacing azure-storage.jar with $STORAGE_JAR"
   find / -name "azure-storage*.jar" | while read line; do echo "Replacing $line"; \cp -f "$SOURCE_JAR" "${line%azure*}"; rm -f $line; done
+
+  mapred_tars=$(find / -name "$MR_TAR_NAME")
+  for mapred_tar in $mapred_tars; do
+    if [ -f $mapred_tar ]; then
+      cd ${mapred_tar%$MR_TAR_NAME}
+
+      echo "Extracting $mapred_tar."
+      tar -xzf $mapred_tar
+
+      echo "Replacing azure-storage.jar with $STORAGE_JAR."
+      find "hadoop/" -name "azure-storage*.jar" | while read line; do echo "Replacing $line"; \cp -f $SOURCE_JAR ${line%azure*}; rm -f $line; done
+
+      echo "Removing $MR_TAR_NAME."
+      rm -f "$mapred_tar"
+
+      echo "Creating new $MR_TAR_NAME with the replaced libs."
+      tar -zcf "$mapred_tar" "hadoop/"
+
+      echo "Cleaning up extracted directory."
+      rm -rf "hadoop/"
+    fi
+  done
+
 }
 
 exec &> "$LOGFILE"
