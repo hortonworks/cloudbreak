514939a077060357ee44969c06e480fee0bcf4fa
lalyos <lalyos@yahoo.com>
2015-09-23 15:05:21 +0200

    Migration specific debug and error is fixed

--- include/migrate.bash
+++ include/migrate.bash
@@ -19,19 +19,28 @@ migrate-startdb() {
     compose-up --no-recreate cbdb pcdb
 }
 
+migrateDebug() {
+    declare desc="prints to migrate log file and to stderr"
+    echo "[MIGRATE] $*" | tee -a "$DB_MIGRATION_LOG" | debug-cat
+}
+
+migrateError() {
+    echo "[ERROR] $*" | tee -a "$DB_MIGRATION_LOG" | red 1>&2
+}
+
 migrate-execute-mybatis-migrations() {
 
     local docker_image_name=$1 && shift
     local service_name=$1 && shift
     local container_name=$(compose-get-container $service_name)
-    info "Migration command on $service_name with \"$@\" params will be executed on container: $container_name" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+        migrateDebug "Migration command on $service_name with \"$@\" params will be executed on container: $container_name"
     if [ -z "$container_name" ]
     then
-        error "DB container with matching name is not running. Expected name: .*$service_name.*" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+        migrateError "DB container with matching name is not running. Expected name: .*$service_name.*"
         return 1
     fi
     local scripts_location=$1 && shift
-    debug "Scripts location:  $scripts_location" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+    migrateDebug "Scripts location:  $scripts_location"
     if [ "$service_name" = "uaadb" ]; then
         flag=false
         while [ "$flag" != "true" ]; do
@@ -43,13 +52,13 @@ migrate-execute-mybatis-migrations() {
         done
     fi
     if [ "$scripts_location" = "container" ]; then
-        info "Schema will be extracted from image:  $docker_image_name" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+        migrateDebug "Schema will be extracted from image:  $docker_image_name"
         local scripts_location=$(pwd)/.schema/$service_name
         rm -rf $scripts_location
         mkdir -p $scripts_location
         docker run --rm --entrypoint bash -v $scripts_location:/migrate/scripts $docker_image_name -c "cp /schema/* /migrate/scripts/"
     fi
-    info "Scripts location:  $scripts_location" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+    migrateDebug "Scripts location:  $scripts_location"
     docker run --rm --link $container_name:db -v $scripts_location:/migrate/scripts sequenceiq/mybatis-migrations:$DOCKER_TAG_MIGRATION $@ \
       | tee -a "$DB_MIGRATION_LOG" | grep "Applying\|MyBatis Migrations SUCCESS\|MyBatis Migrations FAILURE" 2>/dev/null
 }
@@ -85,13 +94,13 @@ migrate-one-db() {
             local docker_image_name=sequenceiq/sultans-bin:${DOCKER_TAG_SULTANS}
             ;;
         *)
-            error "Invalid database service name: $service_name. Supported databases: cbdb and pcdb" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+            migrateError "Invalid database service name: $service_name. Supported databases: cbdb and pcdb"
             return 1
             ;;
     esac
 
-    debug "Script location: $scripts_location" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
-    debug "Docker image name: $docker_image_name" 2> >(tee -a "$DB_MIGRATION_LOG" >&2)
+    migrateDebug "Script location: $scripts_location"
+    migrateDebug "Docker image name: $docker_image_name"
     migrate-execute-mybatis-migrations $docker_image_name $service_name $scripts_location "$@"
 }
 
