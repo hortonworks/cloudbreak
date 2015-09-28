72199bc2286cf361339059228eb7698877c1bef3
Marton Sereg <msereg@hortonworks.com>
2015-09-28 11:31:43 +0200

    CLOUD-47753 simple wasb fs implementation

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/filesystem/FileSystemType.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/filesystem/FileSystemType.java
@@ -2,12 +2,14 @@ package com.sequenceiq.cloudbreak.service.cluster.flow.filesystem;
 
 import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.dash.DashFileSystemConfiguration;
 import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.gcs.GcsFileSystemConfiguration;
+import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.wasb.WasbFileSystemConfiguration;
 import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.wasbintegrated.WasbIntegratedFileSystemConfiguration;
 
 public enum FileSystemType {
     DASH(DashFileSystemConfiguration.class),
     WASB_INTEGRATED(WasbIntegratedFileSystemConfiguration.class),
-    GCS(GcsFileSystemConfiguration.class);
+    GCS(GcsFileSystemConfiguration.class),
+    WASB(WasbFileSystemConfiguration.class);
 
     private Class clazz;
 
---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/filesystem/wasb/WasbFileSystemConfigurator.java
@@ -0,0 +1,45 @@
+package com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.wasb;
+
+import static com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.FileSystemType.WASB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.service.cluster.flow.BlueprintConfigurationEntry;
+import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.AbstractFileSystemConfigurator;
+import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.FileSystemScriptConfig;
+import com.sequenceiq.cloudbreak.service.cluster.flow.filesystem.FileSystemType;
+
+@Component
+public class WasbFileSystemConfigurator extends AbstractFileSystemConfigurator<WasbFileSystemConfiguration> {
+
+    @Override
+    public List<BlueprintConfigurationEntry> getBlueprintProperties(WasbFileSystemConfiguration fsConfig) {
+        List<BlueprintConfigurationEntry> bpConfigs = new ArrayList<>();
+        String accountName = fsConfig.getAccountName();
+        String accountKey = fsConfig.getAccountKey();
+        bpConfigs.add(new BlueprintConfigurationEntry("core-site", "fs.AbstractFileSystem.wasb.impl", "org.apache.hadoop.fs.azure.Wasb"));
+        bpConfigs.add(new BlueprintConfigurationEntry("core-site", "fs.azure.account.key." + accountName + ".blob.core.windows.net", accountKey));
+        bpConfigs.add(new BlueprintConfigurationEntry("core-site", "fs.azure.selfthrottling.read.factor", "1.0"));
+        bpConfigs.add(new BlueprintConfigurationEntry("core-site", "fs.azure.selfthrottling.write.factor", "1.0"));
+        return bpConfigs;
+    }
+
+    @Override
+    public String getDefaultFsValue(WasbFileSystemConfiguration fsConfig) {
+        return "wasb://cloudbreak@" + fsConfig.getAccountName() + ".blob.core.windows.net";
+    }
+
+    @Override
+    public FileSystemType getFileSystemType() {
+        return WASB;
+    }
+
+    @Override
+    protected List<FileSystemScriptConfig> getScriptConfigs() {
+        return new ArrayList<>();
+    }
+
+}
