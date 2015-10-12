9ed61bd2c85123af48a6e8db760b6879c40af24b
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-10-12 18:15:32 +0200

    CLOUD-45976 introduce V2 instance types for Azure

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/JsonToAzureTemplateConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/JsonToAzureTemplateConverter.java
@@ -1,6 +1,9 @@
 package com.sequenceiq.cloudbreak.converter;
 
+import static java.lang.String.format;
+
 import com.sequenceiq.cloudbreak.controller.json.TemplateRequest;
+
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.controller.BadRequestException;
@@ -24,11 +27,8 @@ public class JsonToAzureTemplateConverter extends AbstractConversionServiceAware
         azureTemplate.setVolumeCount((source.getVolumeCount() == null) ? 0 : source.getVolumeCount());
         azureTemplate.setVolumeSize((source.getVolumeSize() == null) ? 0 : source.getVolumeSize());
         AzureVmType azureVmType = AzureVmType.valueOf(source.getParameters().get(AzureTemplateParam.VMTYPE.getName()).toString());
-        if (azureVmType.maxDiskSize() < azureTemplate.getVolumeCount()) {
-            throw new BadRequestException(
-                    String.format("Azure not support this volumesize on the %s. The max suppported size is: %s",
-                            azureVmType.vmType(),
-                            azureVmType.maxDiskSize()));
+        if (azureVmType.maxDiskCount() < azureTemplate.getVolumeCount()) {
+            throw new BadRequestException(format("Max allowed number of disks for vm type: %s is: %s", azureVmType.vmType(), azureVmType.maxDiskCount()));
         }
         return azureTemplate;
     }
