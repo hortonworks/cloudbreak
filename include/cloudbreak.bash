b3d228ae7b538a41d960fbee8c59c2a9dec4145b
lalyos <lalyos@yahoo.com>
2015-10-13 23:54:30 +0200

    fix command: cbd util token should use PUBLIC_IP instead of PRIVATE_IP

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -356,7 +356,7 @@ util-token() {
     local TOKEN=$(curl -siX POST \
         -H "accept: application/x-www-form-urlencoded" \
         -d credentials='{"username":"'${UAA_DEFAULT_USER_EMAIL}'","password":"'${UAA_DEFAULT_USER_PW}'"}' \
-        "${PRIVATE_IP}:8089/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell" \
+        "${PUBLIC_IP}:8089/oauth/authorize?response_type=token&client_id=cloudbreak_shell&scope.0=openid&source=login&redirect_uri=http://cloudbreak.shell" \
            | grep Location | cut -d'=' -f 2 | cut -d'&' -f 1)
     info TOKEN=$TOKEN
 }
