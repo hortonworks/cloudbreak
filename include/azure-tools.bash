fa79cb6661eee00558a1fa37acf7fe6737617f67
Marton Sereg <msereg@hortonworks.com>
2015-10-26 15:04:57 +0100

    azure-cli-tools version upgraded

--- include/azure-tools.bash
+++ include/azure-tools.bash
@@ -1,11 +1,11 @@
 azure-configure-arm() {
     declare desc="Configure new ARM application"
 
-    docker run -it sequenceiq/azure-cli-tools:1.2 configure-arm "$@"
+    docker run -it sequenceiq/azure-cli-tools:1.3 configure-arm "$@"
 }
 
 azure-deploy-dash() {
     declare desc="Deploy the MicrosoftDX/DASH project to a cloud service and create its storage accounts "
 
-    docker run -it sequenceiq/azure-cli-tools:1.1 deploy_dash "$@"
+    docker run -it sequenceiq/azure-cli-tools:1.3 deploy_dash "$@"
 }
