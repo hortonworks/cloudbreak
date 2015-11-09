1b17299b5cd914af98dc610326f6d5db5a5b1671
Attila Kanto <attila.kanto@gmail.com>
2015-11-09 13:43:10 +0100

    BUG-47575 Eliminate sonar blocker from 1.1 codebase

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/SshCheckerTask.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/SshCheckerTask.java
@@ -2,6 +2,8 @@ package com.sequenceiq.cloudbreak.service.stack.flow;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.service.CloudbreakServiceException;
@@ -12,9 +14,12 @@ import net.schmizz.sshj.SSHClient;
 @Component
 public class SshCheckerTask extends StackBasedStatusCheckerTask<SshCheckerTaskContext> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SshCheckerTask.class);
+
     @Override
     public boolean checkStatus(SshCheckerTaskContext sshCheckerTaskContext) {
         final SSHClient ssh = new SSHClient();
+        boolean ret = false;
         try {
             ssh.addHostKeyVerifier(sshCheckerTaskContext.getHostKeyVerifier());
             ssh.connect(sshCheckerTaskContext.getPublicIp(), TlsSetupService.SSH_PORT);
@@ -23,16 +28,17 @@ public class SshCheckerTask extends StackBasedStatusCheckerTask<SshCheckerTaskCo
             } else {
                 ssh.authPublickey(sshCheckerTaskContext.getUser(), sshCheckerTaskContext.getSshPrivateFileLocation());
             }
-            return true;
+            ret = true;
         } catch (Exception e) {
-            return false;
+            LOGGER.info("Failed to connect ssh: {}", e.getMessage());
         } finally {
             try {
                 ssh.disconnect();
             } catch (IOException e) {
-                return false;
+                LOGGER.info("Failed to disconnect from ssh: {}", e.getMessage());
             }
         }
+        return ret;
     }
 
     @Override
