487a83a251bbe7562276a0815909d4e21457d939
lalyos <lalyos@yahoo.com>
2015-11-10 11:37:18 +0100

    Update to latest cloudbreak rc-23 and uaadb 2.7.1

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -24,13 +24,13 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_ALPINE 3.1
     env-import DOCKER_TAG_CONSUL 0.5
     env-import DOCKER_TAG_REGISTRATOR v5
-    env-import DOCKER_TAG_CLOUDBREAK 1.1.0-rc.18
+    env-import DOCKER_TAG_CLOUDBREAK 1.1.0-rc.23
     env-import DOCKER_TAG_CBDB 1.0.0
     env-import DOCKER_TAG_POSTGRES 9.4.1
     env-import DOCKER_TAG_PERISCOPE 1.0.0-rc.2
     env-import DOCKER_TAG_PCDB 0.5.6
     env-import DOCKER_TAG_UAA 2.7.1
-    env-import DOCKER_TAG_UAADB 1.0.1
+    env-import DOCKER_TAG_UAADB v2.7.1
     env-import DOCKER_TAG_ULUWATU 1.1.0-rc.7
     env-import DOCKER_TAG_SULTANS 1.1.0-rc.3
     env-import DOCKER_TAG_AMBASSADOR 0.5.0
