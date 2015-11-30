fd86206c8f0adb3011abb825d8c8f61f84d7db5f
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-11-30 18:09:46 +0100

    CLOUD-48457 runtime database migration with mybatis

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/conf/DatabaseMigrationConfig.java
@@ -0,0 +1,67 @@
+package com.sequenceiq.cloudbreak.conf;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import com.sequenceiq.cloudbreak.logger.InfoLoggingOutputStream;
+import org.apache.commons.io.Charsets;
+import org.apache.ibatis.migration.DataSourceConnectionProvider;
+import org.apache.ibatis.migration.FileMigrationLoader;
+import org.apache.ibatis.migration.operations.UpOperation;
+import org.apache.ibatis.migration.options.DatabaseOperationOption;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class DatabaseMigrationConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseMigrationConfig.class);
+    private static final String DEFAULT_SCHEMA_LOCATION = "/schema";
+    private static final String SCHEMA_IN_CONTAINER = "container";
+    private static final String DEFAULT_SCHEMA_LOCATION_IN_SOURCE = "core/src/main/resources/schema";
+
+    @Value("${cb.schema.scripts.location:" + DEFAULT_SCHEMA_LOCATION_IN_SOURCE + "}")
+    private String schemaLocation;
+
+    @Value("${cb.schema.migration.auto:true}")
+    private boolean schemaMigrationEnabled;
+
+    @Inject
+    private DataSource dataSource;
+
+    @Bean
+    @DependsOn("dataSource")
+    public UpOperation databaseUpMigration() {
+        UpOperation upOperation = new UpOperation();
+        if (schemaMigrationEnabled) {
+            DataSourceConnectionProvider dataSourceConnectionProvider = new DataSourceConnectionProvider(dataSource);
+            DatabaseOperationOption operationOption = new DatabaseOperationOption();
+            operationOption.setRemoveCRs(true);
+            PrintStream streamOut = new PrintStream(new InfoLoggingOutputStream(LOGGER));
+            FileMigrationLoader migrationsLoader = fileMigrationLoader();
+            LOGGER.info("Applying the necessary database migration scripts from location: '{}'....", schemaLocation);
+            upOperation = upOperation.operate(dataSourceConnectionProvider, migrationsLoader, operationOption, streamOut);
+        }
+        return upOperation;
+    }
+
+
+    @Bean
+    public FileMigrationLoader fileMigrationLoader() {
+        if (SCHEMA_IN_CONTAINER.equals(schemaLocation)) {
+            schemaLocation = DEFAULT_SCHEMA_LOCATION;
+        }
+        File scriptsDir = new File(schemaLocation);
+        Properties emptyProperties = new Properties();
+        String charset = Charsets.UTF_8.displayName();
+        return new FileMigrationLoader(scriptsDir, charset, emptyProperties);
+    }
+
+}
--- core/src/main/resources/schema/20150421140021_create_changelog.sql
+++ core/src/main/resources/schema/20150421140021_create_changelog.sql
@@ -9,16 +9,16 @@
 -- Be sure that ID and DESCRIPTION fields exist in
 -- BigInteger and String compatible fields respectively.
 
-CREATE TABLE ${changelog} (
+CREATE TABLE changelog (
 ID NUMERIC(20,0) NOT NULL,
 APPLIED_AT VARCHAR(25) NOT NULL,
 DESCRIPTION VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE ${changelog}
-ADD CONSTRAINT PK_${changelog}
+ALTER TABLE changelog
+ADD CONSTRAINT PK_changelog
 PRIMARY KEY (id);
 
 -- //@UNDO
 
-DROP TABLE ${changelog};
+DROP TABLE changelog;
