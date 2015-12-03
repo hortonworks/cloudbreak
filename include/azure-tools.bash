1afc67c578c0b0eeadc1eaced058acd2b75d85bf
doktoric <rdoktorics@hortonworks.com>
2015-12-03 10:06:40 +0100

    CLOUD-49151 fixed azure log location

--- include/azure-tools.bash
+++ include/azure-tools.bash
@@ -1,9 +1,9 @@
 azure-configure-arm() {
     declare desc="Configure new ARM application"
-    docker run -v "$PWD/azure-cli-log:/root/.azure" -it sequenceiq/azure-cli-tools:1.4 configure-arm "$@"
+    docker run -v "$PWD/.azure:/root/.azure" -it sequenceiq/azure-cli-tools:1.4 configure-arm "$@"
 }
 
 azure-deploy-dash() {
     declare desc="Deploy the MicrosoftDX/DASH project to a cloud service and create its storage accounts "
-    docker run -v "$PWD/azure-cli-log:/root/.azure" -it sequenceiq/azure-cli-tools:1.4 deploy_dash "$@"
+    docker run -v "$PWD/.azure:/root/.azure" -it sequenceiq/azure-cli-tools:1.4 deploy_dash "$@"
 }
