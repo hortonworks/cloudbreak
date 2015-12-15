f63dabff44e11bb7831dec944fcb7170b80c4545
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-12-15 11:41:27 +0100

    CLOUD-48457 modify database migration to run pending scripts too

--- core/src/main/java/com/sequenceiq/cloudbreak/conf/DatabaseMigrationConfig.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/conf/DatabaseMigrationConfig.java
@@ -1,16 +1,17 @@
 package com.sequenceiq.cloudbreak.conf;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.Properties;
 
-import javax.inject.Inject;
-import javax.sql.DataSource;
-
 import org.apache.commons.io.Charsets;
 import org.apache.ibatis.migration.DataSourceConnectionProvider;
 import org.apache.ibatis.migration.FileMigrationLoader;
+import org.apache.ibatis.migration.operations.PendingOperation;
 import org.apache.ibatis.migration.operations.UpOperation;
 import org.apache.ibatis.migration.options.DatabaseOperationOption;
 import org.slf4j.Logger;
@@ -26,6 +27,7 @@ public class DatabaseMigrationConfig {
     private static final String DEFAULT_SCHEMA_LOCATION = "/schema";
     private static final String SCHEMA_IN_CONTAINER = "container";
     private static final String DEFAULT_SCHEMA_LOCATION_IN_SOURCE = "core/src/main/resources/schema";
+    private static final String PENDING_OPERATION_WARNING_MSG = "WARNING: Running pending migrations out of order can create unexpected results.";
 
     @Value("${cb.schema.scripts.location:" + DEFAULT_SCHEMA_LOCATION_IN_SOURCE + "}")
     private String schemaLocation;
@@ -40,24 +42,28 @@ public class DatabaseMigrationConfig {
     @DependsOn("dataSource")
     public UpOperation databaseUpMigration() {
         UpOperation upOperation = new UpOperation();
+        PendingOperation pendingOperation = new PendingOperation();
         if (schemaMigrationEnabled) {
             DataSourceConnectionProvider dataSourceConnectionProvider = new DataSourceConnectionProvider(dataSource);
             DatabaseOperationOption operationOption = new DatabaseOperationOption();
             operationOption.setRemoveCRs(true);
-            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream upOutStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream pendingOutStream = new ByteArrayOutputStream();
             FileMigrationLoader migrationsLoader = fileMigrationLoader();
             LOGGER.info("Applying the necessary database migration scripts from location: '{}'....", schemaLocation);
-            upOperation = upOperation.operate(dataSourceConnectionProvider, migrationsLoader, operationOption, new PrintStream(outStream));
-            String migrationResult = outStream.toString().trim();
-            if (migrationResult.isEmpty()) {
+            upOperation = upOperation.operate(dataSourceConnectionProvider, migrationsLoader, operationOption, new PrintStream(upOutStream));
+            pendingOperation.operate(dataSourceConnectionProvider, migrationsLoader, operationOption, new PrintStream(pendingOutStream));
+            String upMigrationResult = upOutStream.toString().trim();
+            String pendingMigrationResult = pendingOutStream.toString().trim();
+            if (upMigrationResult.isEmpty() && pendingMigrationResult.equals(PENDING_OPERATION_WARNING_MSG)) {
                 LOGGER.info("Schema is up to date. No migration necessary.");
             } else {
-                LOGGER.warn("Migration result:\n{}", migrationResult);
+                logMigrationResult(upMigrationResult, "up");
+                logMigrationResult(pendingMigrationResult, "pending");
             }
         }
         return upOperation;
     }
-
 
     @Bean
     public FileMigrationLoader fileMigrationLoader() {
@@ -70,4 +76,9 @@ public class DatabaseMigrationConfig {
         return new FileMigrationLoader(scriptsDir, charset, emptyProperties);
     }
 
+    private void logMigrationResult(String migrationResult, String operation) {
+        if (!migrationResult.isEmpty()) {
+            LOGGER.warn("Migration result of '{}' operation:\n{}", operation, migrationResult);
+        }
+    }
 }
