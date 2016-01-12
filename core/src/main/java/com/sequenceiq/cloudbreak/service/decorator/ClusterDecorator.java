2ae694c1e7519acd2e68d1e683418bf1ebf21fc4
doktoric <rdoktorics@hortonworks.com>
2016-01-12 13:28:20 +0100

    CLOUD-46290 fixed decoratpr

--- core/src/main/java/com/sequenceiq/cloudbreak/service/decorator/ClusterDecorator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/decorator/ClusterDecorator.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.domain.Stack;
 import com.sequenceiq.cloudbreak.model.HostGroupJson;
 import com.sequenceiq.cloudbreak.controller.validation.blueprint.BlueprintValidator;
 import com.sequenceiq.cloudbreak.domain.Blueprint;
@@ -54,7 +55,8 @@ public class ClusterDecorator implements Decorator<Cluster> {
         boolean validate = (boolean) data[DecorationData.VALIDATE_BLUEPRINT.ordinal()];
         if (validate) {
             Blueprint blueprint = blueprintService.get(blueprintId);
-            blueprintValidator.validateBlueprintForStack(blueprint, subject.getHostGroups(), stackService.get(stackId).getInstanceGroups());
+            Stack stack = stackService.getById(stackId);
+            blueprintValidator.validateBlueprintForStack(blueprint, subject.getHostGroups(), stack.getInstanceGroups());
         }
         return subject;
     }
