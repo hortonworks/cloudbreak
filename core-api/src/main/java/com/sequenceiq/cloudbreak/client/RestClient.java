ad9504889e8e19ceec5677ffc26c789ec79370e0
Attila Kanto <attila.kanto@gmail.com>
2016-01-22 15:36:22 +0100

    CLOUD-51308 workaround for sporadic http url connection concurrency issue regarding self signed certificates

--- core-api/src/main/java/com/sequenceiq/cloudbreak/client/RestClient.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/client/RestClient.java
@@ -3,11 +3,14 @@ package com.sequenceiq.cloudbreak.client;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,16 +41,22 @@ public class RestClient {
     private static Client constructClient(ConfigKey configKey) {
         LOGGER.info("Contructing jax rs client: {}", configKey);
         ClientConfig config = new ClientConfig().property(ClientProperties.FOLLOW_REDIRECTS, "false");
-        ClientBuilder builder = ClientBuilder.newBuilder().withConfig(config);
+
+        ClientBuilder builder = JerseyClientBuilder.newBuilder().withConfig(config);
         if (!configKey.isSecure()) {
-            builder = builder.sslContext(CertificateTrustManager.sslContext()).
-                    hostnameVerifier(CertificateTrustManager.hostnameVerifier());
+            SSLContext sc = CertificateTrustManager.sslContext();
+            //FIXME this is required because of Jersey + HTTP url connection issue, it behaves totally differnly if you are using Java7 or Java8
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            builder = builder.sslContext(sc);
+            builder = builder.hostnameVerifier(CertificateTrustManager.hostnameVerifier());
         }
         if (configKey.isDebug()) {
             builder = builder.register(new LoggingFilter(java.util.logging.Logger.getLogger(RestClient.class.getName()), true));
         }
-        LOGGER.warn("RestClient has been constructed: {}", configKey);
-        return builder.build();
+        Client client = builder.build();
+        SSLContext sslContext = client.getSslContext();
+        LOGGER.warn("RestClient has been constructed: {}, client: {}, sslContext: {}", configKey, client, sslContext);
+        return client;
     }
 
 }
