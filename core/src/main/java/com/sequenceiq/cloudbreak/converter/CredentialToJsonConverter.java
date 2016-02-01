b2c96b2313995d4ef5524e9cc13771dbfb6cc0fb
bbihari <bbihari@hortonworks.com>
2016-02-01 14:38:15 +0100

    CLOUD-51626 Display all parameters in Credantials; decode params, mask password

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/CredentialToJsonConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/CredentialToJsonConverter.java
@@ -1,17 +1,25 @@
 package com.sequenceiq.cloudbreak.converter;
 
+import static com.sequenceiq.cloudbreak.cloud.model.Platform.platform;
+
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Component;
 
-import com.sequenceiq.cloudbreak.domain.Credential;
-import com.sequenceiq.cloudbreak.domain.json.Json;
 import com.sequenceiq.cloudbreak.api.model.CredentialResponse;
+import com.sequenceiq.cloudbreak.domain.Credential;
+import com.sequenceiq.cloudbreak.service.stack.resource.definition.credential.CredentialDefinitionService;
 import com.sequenceiq.cloudbreak.service.topology.TopologyService;
 
 @Component
 public class CredentialToJsonConverter extends AbstractConversionServiceAwareConverter<Credential, CredentialResponse> {
+    private static final String PASSWORD_FIELD = "password";
+    private static final String PASSWORD_PLACEHOLDER = "********";
 
+    @Inject
+    private CredentialDefinitionService credentialDefinitionService;
     @Inject
     private TopologyService topologyService;
 
@@ -22,9 +30,9 @@ public class CredentialToJsonConverter extends AbstractConversionServiceAwareCon
         credentialJson.setCloudPlatform(source.cloudPlatform());
         credentialJson.setName(source.getName());
         credentialJson.setPublicInAccount(source.isPublicInAccount());
-        Json attributes = source.getAttributes();
-        if (attributes != null) {
-            credentialJson.setParameters(attributes.getMap());
+        if (source.getAttributes() != null) {
+            Map<String, Object> parameters = credentialDefinitionService.revertProperties(platform(source.cloudPlatform()), source.getAttributes().getMap());
+            credentialJson.setParameters(parameters);
         }
         credentialJson.setDescription(source.getDescription() == null ? "" : source.getDescription());
         credentialJson.setPublicKey(source.getPublicKey());
@@ -32,6 +40,13 @@ public class CredentialToJsonConverter extends AbstractConversionServiceAwareCon
         if (source.getTopology() != null) {
             credentialJson.setTopologyId(source.getTopology().getId());
         }
+        clearPasswordField(credentialJson);
         return credentialJson;
+    }
+
+    private void clearPasswordField(CredentialResponse response) {
+        if (response.getParameters().get(PASSWORD_FIELD) != null) {
+            response.getParameters().put(PASSWORD_FIELD, PASSWORD_PLACEHOLDER);
+        }
     }
 }
