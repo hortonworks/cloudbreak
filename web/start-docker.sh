f82aea6a41c7f1de44f678b60f67d64393f1712c
Attila Kanto <attila.kanto@gmail.com>
2016-02-23 16:18:12 +0100

    CLOUD-48264 remove misleading new relic config

--- web/start-docker.sh
+++ web/start-docker.sh
@@ -32,23 +32,9 @@ if [ -z "$ULU_HOST_ADDRESS" ]; then
   MISSING_ENV_VARS=true;
 fi
 
-if [ -z "$ULU_PRODUCTION" ]; then
-  export ULU_PRODUCTION=false;
-fi
-
 if [ $MISSING_ENV_VARS ]; then
   exit 1;
 fi
 
 cd /uluwatu
-if [ "$ULU_PRODUCTION" = true ]; then
-  npm install newrelic
-  cp node_modules/newrelic/newrelic.js .
-  sed -i -E "s/license key here/$ULU_NEW_RELIC_KEY/g" newrelic.js
-  sed -i -E "s/My Application/$ULU_NEW_RELIC_APP/g" newrelic.js
-  echo "require('newrelic');" > new_server.js
-  cat server.js >> new_server.js
-  rm server.js
-  mv new_server.js server.js
-fi;
 node server.js
