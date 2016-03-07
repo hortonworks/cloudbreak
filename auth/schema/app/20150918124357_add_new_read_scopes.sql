4c9e65821d4aeeb77d02599bf36fc9a851d0a9ee
mhmxs <kovacsricsi@gmail.com>
2016-03-07 12:17:58 +0100

    CLOUD-52739 Disable old uaa migration scripts

--- auth/schema/app/20150918124357_add_new_read_scopes.sql
+++ auth/schema/app/20150918124357_add_new_read_scopes.sql
@@ -1,37 +1,37 @@
 -- // add new read scopes
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO groups (id, displayname) VALUES ('2a4f837e-2ab0-4663-8644-de23047d040d', 'cloudbreak.blueprints.read');
-INSERT into groups (id, displayname) VALUES ('9fc82f95-0318-4dda-af65-ea32e2f00497', 'cloudbreak.templates.read');
-INSERT into groups (id, displayname) VALUES ('8cbffdab-560d-4e14-9d1e-d5ff3f43bec6', 'cloudbreak.credentials.read');
-INSERT into groups (id, displayname) VALUES ('7f85eaad-4dac-4f74-9238-aef0e585df07', 'cloudbreak.recipes.read');
-INSERT into groups (id, displayname) VALUES ('aac2dad1-6a7b-4ae2-9c58-e71d367d0e2d', 'cloudbreak.networks.read');
-INSERT into groups (id, displayname) VALUES ('6674545b-295c-4f23-b1b4-953447dc5f40', 'cloudbreak.securitygroups.read');
-INSERT into groups (id, displayname) VALUES ('f07b098a-2700-471a-9f0d-f7dd730db95d', 'cloudbreak.stacks.read');
+--INSERT INTO groups (id, displayname) VALUES ('2a4f837e-2ab0-4663-8644-de23047d040d', 'cloudbreak.blueprints.read');
+--INSERT into groups (id, displayname) VALUES ('9fc82f95-0318-4dda-af65-ea32e2f00497', 'cloudbreak.templates.read');
+--INSERT into groups (id, displayname) VALUES ('8cbffdab-560d-4e14-9d1e-d5ff3f43bec6', 'cloudbreak.credentials.read');
+--INSERT into groups (id, displayname) VALUES ('7f85eaad-4dac-4f74-9238-aef0e585df07', 'cloudbreak.recipes.read');
+--INSERT into groups (id, displayname) VALUES ('aac2dad1-6a7b-4ae2-9c58-e71d367d0e2d', 'cloudbreak.networks.read');
+--INSERT into groups (id, displayname) VALUES ('6674545b-295c-4f23-b1b4-953447dc5f40', 'cloudbreak.securitygroups.read');
+--INSERT into groups (id, displayname) VALUES ('f07b098a-2700-471a-9f0d-f7dd730db95d', 'cloudbreak.stacks.read');
 
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.blueprints.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.templates.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.credentials.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.recipes.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.networks.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.securitygroups.read';
-INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.stacks.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.blueprints.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.templates.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.credentials.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.recipes.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.networks.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.securitygroups.read';
+--INSERT into group_membership SELECT gr.id AS group_id, usr.id AS member_id, 'USER', 'MEMBER' FROM users usr, groups gr WHERE gr.displayname='cloudbreak.stacks.read';
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-DELETE FROM groups WHERE id='2a4f837e-2ab0-4663-8644-de23047d040d';
-DELETE FROM groups WHERE id='9fc82f95-0318-4dda-af65-ea32e2f00497';
-DELETE FROM groups WHERE id='8cbffdab-560d-4e14-9d1e-d5ff3f43bec6';
-DELETE FROM groups WHERE id='7f85eaad-4dac-4f74-9238-aef0e585df07';
-DELETE FROM groups WHERE id='aac2dad1-6a7b-4ae2-9c58-e71d367d0e2d';
-DELETE FROM groups WHERE id='6674545b-295c-4f23-b1b4-953447dc5f40';
-DELETE FROM groups WHERE id='f07b098a-2700-471a-9f0d-f7dd730db95d';
+--DELETE FROM groups WHERE id='2a4f837e-2ab0-4663-8644-de23047d040d';
+--DELETE FROM groups WHERE id='9fc82f95-0318-4dda-af65-ea32e2f00497';
+--DELETE FROM groups WHERE id='8cbffdab-560d-4e14-9d1e-d5ff3f43bec6';
+--DELETE FROM groups WHERE id='7f85eaad-4dac-4f74-9238-aef0e585df07';
+--DELETE FROM groups WHERE id='aac2dad1-6a7b-4ae2-9c58-e71d367d0e2d';
+--DELETE FROM groups WHERE id='6674545b-295c-4f23-b1b4-953447dc5f40';
+--DELETE FROM groups WHERE id='f07b098a-2700-471a-9f0d-f7dd730db95d';
 
-DELETE FROM group_membership WHERE group_id='2a4f837e-2ab0-4663-8644-de23047d040d';
-DELETE FROM group_membership WHERE group_id='9fc82f95-0318-4dda-af65-ea32e2f00497';
-DELETE FROM group_membership WHERE group_id='8cbffdab-560d-4e14-9d1e-d5ff3f43bec6';
-DELETE FROM group_membership WHERE group_id='7f85eaad-4dac-4f74-9238-aef0e585df07';
-DELETE FROM group_membership WHERE group_id='aac2dad1-6a7b-4ae2-9c58-e71d367d0e2d';
-DELETE FROM group_membership WHERE group_id='6674545b-295c-4f23-b1b4-953447dc5f40';
-DELETE FROM group_membership WHERE group_id='f07b098a-2700-471a-9f0d-f7dd730db95d';
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.blueprints.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.templates.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.credentials.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.recipes.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.networks.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.securitygroups.read');
+--DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.stacks.read');
--- auth/schema/app/20160306145372_add_sssdconfig_scopes.sql
+++ auth/schema/app/20160306145372_add_sssdconfig_scopes.sql
@@ -7,17 +7,17 @@ INSERT INTO groups (id, displayname) VALUES ('a1eb419b-0a1b-4bb7-b900-218a641dda
 INSERT INTO group_membership SELECT '1e0696ea-9250-405a-aaac-7596a8c15076' AS group_id, id, 'USER' AS member_type, 'MEMBER' as authorities from users;
 INSERT INTO group_membership SELECT 'a1eb419b-0a1b-4bb7-b900-218a641ddac6' AS group_id, id, 'USER' AS member_type, 'MEMBER' as authorities from users;
 
-UPDATE oauth_client_details SET scope = CONCAT(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read') WHERE client_id='uluwatu';
-UPDATE oauth_client_details SET scope = CONCAT(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read') WHERE client_id='cloudbreak_shell';
+--UPDATE oauth_client_details SET scope = CONCAT(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read') WHERE client_id='uluwatu';
+--UPDATE oauth_client_details SET scope = CONCAT(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read') WHERE client_id='cloudbreak_shell';
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-DELETE FROM group_membership WHERE group_id='1e0696ea-9250-405a-aaac-7596a8c15076';
-DELETE FROM group_membership WHERE group_id='a1eb419b-0a1b-4bb7-b900-218a641ddac6';
+DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.sssdconfigs');
+DELETE FROM group_membership WHERE group_id=(SELECT id FROM groups where displayName = 'cloudbreak.sssdconfigs.read');
 
 DELETE FROM groups WHERE id='1e0696ea-9250-405a-aaac-7596a8c15076';
 DELETE FROM groups WHERE id='a1eb419b-0a1b-4bb7-b900-218a641ddac6';
 
-UPDATE oauth_client_details SET scope = REPLACE(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read', '') WHERE client_id='uluwatu';
-UPDATE oauth_client_details SET scope = REPLACE(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read', '') WHERE client_id='cloudbreak_shell';
+--UPDATE oauth_client_details SET scope = REPLACE(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read', '') WHERE client_id='uluwatu';
+--UPDATE oauth_client_details SET scope = REPLACE(scope, ',cloudbreak.sssdconfigs,cloudbreak.sssdconfigs.read', '') WHERE client_id='cloudbreak_shell';
