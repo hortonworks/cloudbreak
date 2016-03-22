f03f15a3907d9ea20c97923d2fcb864e18b5bff8
lalyos <lalyos@yahoo.com>
2016-03-22 03:37:26 +0100

    Revert "update regardless of version"
    This reverts commit ed8e23b38b3adf3078a24f7b53e266c01bc0d4c9.

--- include/deployer.bash
+++ include/deployer.bash
@@ -78,11 +78,18 @@ cbd-update-release() {
     local osarch=$(uname -sm | tr " " _ )
     debug $desc
     debug binver=$binver lastver=$lastver osarch=$osarch | gray
-    local url=https://github.com/sequenceiq/cloudbreak-deployer/releases/download/v${lastver}/cloudbreak-deployer_${lastver}_${osarch}.tgz
-    info "Updating $SELF_EXECUTABLE from url: $url"
-    curl -Ls $url | tar -zx -C $TEMP_DIR
-    mv $TEMP_DIR/cbd $SELF_EXECUTABLE
-    debug $SELF_EXECUTABLE is updated
+
+    if [[ ${binver} != ${lastver} ]]; then
+        debug upgrade needed |yellow
+
+        local url=https://github.com/sequenceiq/cloudbreak-deployer/releases/download/v${lastver}/cloudbreak-deployer_${lastver}_${osarch}.tgz
+        info "Updating $SELF_EXECUTABLE from url: $url"
+        curl -Ls $url | tar -zx -C $TEMP_DIR
+        mv $TEMP_DIR/cbd $SELF_EXECUTABLE
+        debug $SELF_EXECUTABLE is updated
+    else
+        debug you have the latest version | green
+    fi
 }
 
 cbd-update-snap() {
