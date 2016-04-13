e142cc6648cf2db85537ad3b6c561c3ac4adb099
Richard Doktorics <rdoktorics@hortonworks.com>
2016-04-13 11:19:22 +0200

    CLOUD-47839 fixed show the real error (#1479)

--- shell/src/main/java/com/sequenceiq/cloudbreak/shell/transformer/ExceptionTransformer.java
+++ shell/src/main/java/com/sequenceiq/cloudbreak/shell/transformer/ExceptionTransformer.java
@@ -1,11 +1,33 @@
 package com.sequenceiq.cloudbreak.shell.transformer;
 
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.ws.rs.ClientErrorException;
+
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class ExceptionTransformer {
 
+    @Inject
+    private ObjectMapper objectMapper;
+
     public RuntimeException transformToRuntimeException(Exception e) {
+        if (e instanceof ClientErrorException) {
+            if (((ClientErrorException) e).getResponse() != null && ((ClientErrorException) e).getResponse().hasEntity()) {
+                String response = ((ClientErrorException) e).getResponse().readEntity(String.class);
+                try {
+                    JsonNode message = objectMapper.readTree(response).get("message");
+                    return new RuntimeException(message.asText());
+                } catch (IOException ex) {
+                    return new RuntimeException(response);
+                }
+            }
+        }
         return new RuntimeException(e.getMessage());
     }
 
