25129ae2ba5cc3e3de1c6992b781fc14f4bee0f6
Tamas Bihari <tamas.bihari@sequenceiq.com>
2016-06-07 20:02:17 +0200

    CLOUD-59184 create individual root path for cloudbreak and periscope

--- autoscale/src/main/java/com/sequenceiq/periscope/config/ApiDocConfig.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/config/ApiDocConfig.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.sequenceiq.periscope.api.AutoscaleApi;
 import com.sequenceiq.periscope.utils.FileReaderUtils;
 
 import io.swagger.jaxrs.config.BeanConfig;
@@ -19,7 +20,7 @@ public class ApiDocConfig {
         beanConfig.setDescription(FileReaderUtils.readFileFromClasspath("swagger/auto-scaling-introduction"));
         beanConfig.setVersion("1.2.0");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setBasePath("/api/v1");
+        beanConfig.setBasePath(AutoscaleApi.API_ROOT_CONTEXT);
         beanConfig.setResourcePackage("com.sequenceiq.periscope.api");
         beanConfig.setScan(true);
         return beanConfig;
