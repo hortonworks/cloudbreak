257a80e69fd71b97d349ec97ba1b52cf4b1b1889
sodre90 <erdos.peter.bme@gmail.com>
2016-06-27 14:01:05 +0200

    CLOUD-60625 Refactor certification generation and naming

--- core/src/main/resources/init/host/tls-setup.sh
+++ core/src/main/resources/init/host/tls-setup.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 setup_cbclient_cert() {
-  ${sudopre} sudo ${sudocheck} mkdir -p /etc/certs
-  ${sudopre} sudo ${sudocheck} cp /tmp/cb-client.pem /etc/certs
+  sudo mkdir -p /etc/certs
+  sudo cp /tmp/cb-client.pem /etc/certs
 }
 
 create_certificates() {
-  ${sudopre} sudo ${sudocheck} cert-tool -d=/etc/certs -o=gateway -s localhost -s 127.0.0.1 -s ${publicIp}
-  ${sudopre} sudo ${sudocheck} mv /etc/certs/server-key.pem /etc/certs/server.key
-  ${sudopre} sudo ${sudocheck} sh -c 'cat /etc/certs/ca.pem >> /etc/certs/server.pem'
-  ${sudopre} sudo ${sudocheck} rm /etc/certs/client-key.pem /etc/certs/client.pem /etc/certs/ca-key.pem
-  ${sudopre} sudo ${sudocheck} cp /etc/certs/server.pem /tmp/server.pem
+  sudo cert-tool -d=/etc/certs -o=gateway -s localhost -s 127.0.0.1 -s ${publicIp}
+  sudo rm /etc/certs/client-key.pem /etc/certs/client.pem /etc/certs/ca-key.pem
+  sudo cp /etc/certs/server.pem /tmp/server.pem
 }
 
 start_nginx() {
-  ${sudopre} sudo ${sudocheck} mv /tmp/nginx.conf /etc/nginx/nginx.conf
-  ${sudopre} sudo ${sudocheck} mkdir -p /usr/share/nginx/json/
-  ${sudopre} sudo ${sudocheck} mv /tmp/50x.json /usr/share/nginx/json/50x.json
-  ${sudopre} sudo ${sudocheck} service nginx start
-  ${sudopre} sudo ${sudocheck} chkconfig nginx on
+  sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf
+  sudo mkdir -p /usr/share/nginx/json/
+  sudo mv /tmp/50x.json /usr/share/nginx/json/50x.json
+  sudo service nginx start
+  sudo chkconfig nginx on
 }
 
 setup_tls() {
