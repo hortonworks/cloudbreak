1ce47c56bfead0acde1204653d6833a86d309f76
Marton Sereg <msereg@hortonworks.com>
2016-08-29 14:38:59 +0200

    CLOUD-61470 small modifications in build-scripts

--- scripts/build-rc-patch.sh
+++ scripts/build-rc-patch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 : ${WORKSPACE=.}
+: ${BRANCH=rc-$(git describe --abbrev=0 --tags | cut -d '-' -f 1 | cut -d '.' -f 1,2)}
 
-./gradlew -Penv=jenkins -b build.gradle clean build release uploadArchives -Prelease.scope=patch -Prelease.stage=rc --info --stacktrace
+MINOR_VERSION=$(echo "$BRANCH" | cut -d'-' -f 2)
+
+if [ "$(git tag -l "$MINOR_VERSION.0-rc.1")" == '' ]; then
+    ./gradlew -Penv=jenkins -b build.gradle clean build release -Prelease.scope=minor -Prelease.stage=rc --info --stacktrace
+else
+    ./gradlew -Penv=jenkins -b build.gradle clean build release -Prelease.scope=patch -Prelease.stage=rc --info --stacktrace
+fi
 
 echo VERSION=$(git describe --abbrev=0 --tags) > $WORKSPACE/version
