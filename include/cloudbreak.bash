90ba9fa79654105c1de4085f6f71fbfdbf7e27ef
Tamas Bihari <tamas.bihari@sequenceiq.com>
2016-08-29 17:54:46 +0200

    CLOUD-64897 upgrade UAA to 3.6.0 (#257)

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -29,8 +29,8 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_REGISTRATOR v5
     env-import DOCKER_TAG_POSTGRES 9.4.1
     env-import DOCKER_TAG_POSTFIX latest
-    env-import DOCKER_TAG_UAA 2.7.1
-    env-import DOCKER_TAG_UAADB 2.7.1
+    env-import DOCKER_TAG_UAA 3.6.0
+    env-import DOCKER_TAG_UAADB v3.6.0
     env-import DOCKER_TAG_AMBASSADOR 0.5.0
     env-import DOCKER_TAG_CERT_TOOL 0.0.3
     env-import DOCKER_TAG_CBDB 1.2.0
