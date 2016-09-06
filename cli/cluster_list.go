9225b7dec4cced8f7d304e479b9178b20082b531
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2016-09-06 10:18:37 +0200

    move cluster node list to cluster_list

--- cli/cluster_list.go
+++ cli/cluster_list.go
@@ -61,3 +61,40 @@ func ListClusters(c *cli.Context) error {
 
 	return nil
 }
+
+func ListClusterNodes(c *cli.Context) error {
+	oAuth2Client := NewOAuth2HTTPClient(c.String(FlCBServer.Name), c.String(FlCBUsername.Name), c.String(FlCBPassword.Name))
+
+	clusterName := c.String(FlCBClusterName.Name)
+	if len(clusterName) == 0 {
+		log.Error("[ListClusterNodes] There are missing parameters.\n")
+		cli.ShowSubcommandHelp(c)
+		newExitReturnError()
+	}
+
+	respStack, err := oAuth2Client.Cloudbreak.Stacks.GetStacksUserName(&stacks.GetStacksUserNameParams{Name: clusterName})
+	if err != nil {
+		log.Errorf("[ListClusterNodes] %s", err)
+		newExitReturnError()
+	}
+
+	var tableRows []Row
+	for _, instanceGroup := range respStack.Payload.InstanceGroups {
+		metadataArray := instanceGroup.Metadata
+		for _, metadata := range metadataArray {
+			data := *metadata
+			if data.DiscoveryFQDN == nil {
+				continue
+			}
+			nodeType := *data.InstanceGroup
+			if nodeType == "master" {
+				nodeType = "master - ambari server"
+			}
+			row := &GenericRow{Data: []string{*data.InstanceID, *data.DiscoveryFQDN, *data.PublicIP, *data.PrivateIP, nodeType}}
+			tableRows = append(tableRows, row)
+		}
+	}
+
+	WriteTable([]string{"Instance ID", "Hostname", "Public IP", "Private IP", "Type"}, tableRows)
+	return nil
+}
