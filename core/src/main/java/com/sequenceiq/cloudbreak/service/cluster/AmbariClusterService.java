3ea7f97f2fa16031e5a3fea868154617d10638e4
Attila Kanto <attila.kanto@gmail.com>
2016-09-26 09:25:03 +0200

    CLOUD-EAR-4623 invalid hostmetadata count after sync when a node from master hg died

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/AmbariClusterService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/AmbariClusterService.java
@@ -158,23 +158,6 @@ public class AmbariClusterService implements ClusterService {
     @Inject
     private ComponentConfigProvider componentConfigProvider;
 
-    private enum Msg {
-        AMBARI_CLUSTER_START_IGNORED("ambari.cluster.start.ignored"),
-        AMBARI_CLUSTER_STOP_IGNORED("ambari.cluster.stop.ignored"),
-        AMBARI_CLUSTER_HOST_STATUS_UPDATED("ambari.cluster.host.status.updated"),
-        AMBARI_CLUSTER_START_REQUESTED("ambari.cluster.start.requested");
-
-        private String code;
-
-        Msg(String msgCode) {
-            code = msgCode;
-        }
-
-        public String code() {
-            return code;
-        }
-    }
-
     @Override
     @Transactional(Transactional.TxType.NEVER)
     public Cluster create(CbUser user, Long stackId, Cluster cluster, List<Component> components) {
@@ -443,10 +426,14 @@ public class AmbariClusterService implements ClusterService {
             HttpClientConfig clientConfig = tlsSecurityService.buildTLSClientConfig(stackId, cluster.getAmbariIp());
             AmbariClient ambariClient = ambariClientProvider.getAmbariClient(clientConfig, stack.getGatewayPort(), stack.getCluster().getUserName(),
                     stack.getCluster().getPassword());
+            Map<String, Integer> hostGroupCounter = new HashMap<>();
             Set<HostMetadata> hosts = hostMetadataRepository.findHostsInCluster(stack.getCluster().getId());
             Map<String, String> hostStatuses = ambariClient.getHostStatuses();
             for (HostMetadata host : hosts) {
                 if (hostStatuses.containsKey(host.getHostName())) {
+                    String hgName = host.getHostGroup().getName();
+                    Integer hgCounter = hostGroupCounter.getOrDefault(hgName, 0) + 1;
+                    hostGroupCounter.put(hgName, hgCounter);
                     HostMetadataState newState = HostMetadataState.HEALTHY.name().equals(hostStatuses.get(host.getHostName()))
                             ? HostMetadataState.HEALTHY : HostMetadataState.UNHEALTHY;
                     boolean stateChanged = updateHostMetadataByHostState(stack, host.getHostName(), newState);
@@ -455,10 +442,24 @@ public class AmbariClusterService implements ClusterService {
                     }
                 }
             }
+            hostGroupCounter(cluster.getId(), hostGroupCounter);
         } catch (CloudbreakSecuritySetupException e) {
             throw new CloudbreakServiceException(e);
         }
         return cluster;
+    }
+
+    private void hostGroupCounter(Long clusterId, Map<String, Integer> hostGroupCounter) {
+        LOGGER.info("Counted hostgroups: {}", hostGroupCounter);
+        Set<HostGroup> hostGroups = hostGroupService.getByCluster(clusterId);
+        for (HostGroup hostGroup : hostGroups) {
+            Integer hgCounter = hostGroupCounter.getOrDefault(hostGroup.getName(), 0);
+            if (!hgCounter.equals(hostGroup.getConstraint().getHostCount())) {
+                hostGroup.getConstraint().setHostCount(hgCounter);
+                constraintRepository.save(hostGroup.getConstraint());
+                LOGGER.info("Updated HostCount for hostgroup: {}, counter: {}", hostGroup.getName(), hgCounter);
+            }
+        }
     }
 
     private void updateInstanceMetadataStateToRegistered(Long stackId, HostMetadata host) {
@@ -675,7 +676,7 @@ public class AmbariClusterService implements ClusterService {
     private void prepareResults(Set<BlueprintParameterJson> requests, Cluster cluster, Map<String, String> bpI, Map<String, String> results) {
         if (cluster.getBlueprintInputs().getValue() != null) {
             if (bpI != null) {
-                for (Map.Entry<String, String> stringStringEntry  : bpI.entrySet()) {
+                for (Map.Entry<String, String> stringStringEntry : bpI.entrySet()) {
                     if (!results.keySet().contains(stringStringEntry.getKey())) {
                         results.put(stringStringEntry.getKey(), stringStringEntry.getValue());
                     }
@@ -695,7 +696,7 @@ public class AmbariClusterService implements ClusterService {
         for (BlueprintParameterJson request : requests) {
             if (bpI != null) {
                 boolean contains = false;
-                for (Map.Entry<String, String> stringStringEntry  : bpI.entrySet()) {
+                for (Map.Entry<String, String> stringStringEntry : bpI.entrySet()) {
                     if (stringStringEntry.getKey().equals(request.getName())) {
                         contains = true;
                     }
@@ -711,6 +712,23 @@ public class AmbariClusterService implements ClusterService {
 
     private void prepareAdditionalInputParameters(Map<String, String> results, Cluster cluster) {
         results.put("REMOTE_CLUSTER_NAME", cluster.getName());
+    }
+
+    private enum Msg {
+        AMBARI_CLUSTER_START_IGNORED("ambari.cluster.start.ignored"),
+        AMBARI_CLUSTER_STOP_IGNORED("ambari.cluster.stop.ignored"),
+        AMBARI_CLUSTER_HOST_STATUS_UPDATED("ambari.cluster.host.status.updated"),
+        AMBARI_CLUSTER_START_REQUESTED("ambari.cluster.start.requested");
+
+        private String code;
+
+        Msg(String msgCode) {
+            code = msgCode;
+        }
+
+        public String code() {
+            return code;
+        }
     }
 
 }
