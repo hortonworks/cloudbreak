beba325c55c28235ec297535437766bdebfcdbb0
jenkins-sequenceiq <jenkins-sequenceiq@sequenceiq.com>
2016-10-07 11:09:25 +0000

    Updated container versions to 1.8.0-dev.48

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -35,11 +35,11 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_CERT_TOOL 0.0.3
     env-import DOCKER_TAG_CBDB 1.2.0
     env-import DOCKER_TAG_PCDB 1.2.0
-    env-import DOCKER_TAG_PERISCOPE 1.8.0-dev.47
-    env-import DOCKER_TAG_CLOUDBREAK 1.8.0-dev.47
-    env-import DOCKER_TAG_ULUWATU 1.8.0-dev.47
-    env-import DOCKER_TAG_SULTANS 1.8.0-dev.47
-    env-import DOCKER_TAG_CLOUDBREAK_SHELL 1.8.0-dev.47
+    env-import DOCKER_TAG_PERISCOPE 1.8.0-dev.48
+    env-import DOCKER_TAG_CLOUDBREAK 1.8.0-dev.48
+    env-import DOCKER_TAG_ULUWATU 1.8.0-dev.48
+    env-import DOCKER_TAG_SULTANS 1.8.0-dev.48
+    env-import DOCKER_TAG_CLOUDBREAK_SHELL 1.8.0-dev.48
 
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.1.0
 
