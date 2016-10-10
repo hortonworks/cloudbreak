58690e8809324fbe2ac9eacf76c9531496cc93a5
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2016-10-10 10:54:23 +0200

    disable spinner on windows

--- cli/hdc_spinner.go
+++ cli/hdc_spinner.go
@@ -6,14 +6,16 @@ import (
 
 	"fmt"
 	"github.com/briandowns/spinner"
+	"runtime"
 )
 
 var Spinner *spinner.Spinner
 
 func init() {
-	spinner.CharSets[37] = []string{"H", "o", "r", "t", "o", "n", "w", "o", "r", "k", "s"}
-	Spinner = spinner.New(spinner.CharSets[9], 200*time.Millisecond)
-	Spinner.Writer = os.Stderr
+	if runtime.GOOS != "windows" {
+		Spinner = spinner.New(spinner.CharSets[9], 200*time.Millisecond)
+		Spinner.Writer = os.Stderr
+	}
 }
 
 func StartSpinner() {
