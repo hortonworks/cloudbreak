53e9b3bf32ba23a53a0afe6cbc108b2e2913baf9
doktoric <rdoktorics@hortonworks.com>
2016-10-11 11:12:36 +0200

    fixed variable name

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -238,7 +238,7 @@ cloudbreak-conf-ui() {
     env-import ULU_HWX_CLOUD_DEFAULT_SUBNET_ID ""
     env-import ULU_HWX_CLOUD_REGISTRATION_URL ""
     env-import HWX_DOC_LINK ""
-    env-import ULU_HWX_CLOUD_REGISTRATION_URL "true"
+    env-import ULU_SUBSCRIBE_TO_NOTIFICATIONS "true"
     env-import HWX_CLOUD_ENABLE_GOVERNANCE_AND_SECURITY "false"
 }
 
