09ab1021bf14b4c0cde32742f63b851973d56003
mhmxs <kovacsricsi@gmail.com>
2016-11-10 12:23:48 +0100

    Hello world example flow

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/helloworld/HelloWorldEvent.java
@@ -0,0 +1,17 @@
+package com.sequenceiq.cloudbreak.core.flow2.helloworld;
+
+import com.sequenceiq.cloudbreak.core.flow2.FlowEvent;
+
+public enum HelloWorldEvent implements FlowEvent {
+    START_HELLO_WORLD_EVENT,
+    HELLO_WORLD_FINISHED_EVENT,
+    FINALIZE_HELLO_WORLD_EVENT,
+
+    HELLO_WORLD_SOMETHING_WENT_WRONG,
+    HELLO_WORLD_FAIL_HANDLED_EVENT;
+
+    @Override
+    public String stringRepresentation() {
+        return name();
+    }
+}
