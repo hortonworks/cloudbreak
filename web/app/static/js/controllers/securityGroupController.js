cdac69ace513977cd0258d3395c119556ed0b8fd
doktoric <rdoktorics@hortonworks.com>
2016-11-17 15:06:56 +0100

    Secuirty group duplication cause exception on ui

--- web/app/static/js/controllers/securityGroupController.js
+++ web/app/static/js/controllers/securityGroupController.js
@@ -90,9 +90,17 @@ angular.module('uluwatuControllers').controller('securityGroupController', ['$sc
 
         $rootScope.changeRule = function(rules) {
             angular.forEach(rules, function(rule) {
-                rule.portarray = rule.ports.toString().split(",");
+                rule.portarray = remove_duplicates(rule.ports.toString().split(","));
             });
             return rules;
+        }
+
+        function remove_duplicates(arr) {
+            var obj = {};
+            var ret_arr = [];
+            for (var i = 0; i < arr.length; i++) { obj[arr[i]] = true; }
+            for (var key in obj) { ret_arr.push(key); }
+            return ret_arr;
         }
 
         $scope.deleteSecurityGroup = function(securityGroup) {
