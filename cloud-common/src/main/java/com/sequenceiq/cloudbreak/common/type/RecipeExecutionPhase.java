1506c3ee3b0a5134cba446bddc3bb5bc285c79e7
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2016-11-24 16:12:17 +0100

    CLOUD-69532 save the URL along with the recipe

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/common/type/RecipeExecutionPhase.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/common/type/RecipeExecutionPhase.java
@@ -1,16 +1,21 @@
-package com.sequenceiq.cloudbreak.orchestrator.model;
+package com.sequenceiq.cloudbreak.common.type;
 
 public enum RecipeExecutionPhase {
     PRE("pre"), POST("post");
 
     private String value;
+    private String url;
 
     RecipeExecutionPhase(String value) {
         this.value = value;
+        this.url = value + "-url";
     }
 
     public String value() {
         return value;
     }
 
+    public String url() {
+        return url;
+    }
 }
