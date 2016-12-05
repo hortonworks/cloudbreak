59ecab6d5eed387a6099c92c03a8d13cb9d95ae1
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2016-12-05 15:44:56 +0100

    CLOUD-70000 OS parameter is optional when providing the HDP repo

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/JsonToHDPRepoConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/JsonToHDPRepoConverter.java
@@ -9,7 +9,12 @@ import com.sequenceiq.cloudbreak.api.model.AmbariStackDetailsJson;
 import com.sequenceiq.cloudbreak.cloud.model.HDPRepo;
 
 @Component
-public class JsonToHDPRepoConverter extends AbstractConversionServiceAwareConverter<AmbariStackDetailsJson, HDPRepo>  {
+public class JsonToHDPRepoConverter extends AbstractConversionServiceAwareConverter<AmbariStackDetailsJson, HDPRepo> {
+
+    private static final String REDHAT_6 = "redhat6";
+
+    private static final String REDHAT_7 = "redhat7";
+
     @Override
     public HDPRepo convert(AmbariStackDetailsJson source) {
         HDPRepo repo = new HDPRepo();
@@ -17,9 +22,19 @@ public class JsonToHDPRepoConverter extends AbstractConversionServiceAwareConver
         Map<String, String> util = new HashMap<>();
 
         stack.put("repoid", source.getStackRepoId());
-        stack.put(source.getOs(), source.getStackBaseURL());
         util.put("repoid", source.getUtilsRepoId());
-        util.put(source.getOs(), source.getUtilsBaseURL());
+
+        String stackBaseURL = source.getStackBaseURL();
+        String utilsBaseURL = source.getUtilsBaseURL();
+        if (source.getOs() == null) {
+            stack.put(REDHAT_6, stackBaseURL);
+            stack.put(REDHAT_7, stackBaseURL);
+            util.put(REDHAT_6, utilsBaseURL);
+            util.put(REDHAT_7, utilsBaseURL);
+        } else {
+            stack.put(source.getOs(), stackBaseURL);
+            util.put(source.getOs(), utilsBaseURL);
+        }
 
         repo.setStack(stack);
         repo.setUtil(util);
