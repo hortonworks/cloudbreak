f6c72c418ce7688156cce2eea05147a8cf31caa8
mhalmy <mhalmy@hortonworks.com>
2016-12-15 17:22:40 +0100

    CLOUD-70657 Create new API automated test suite for Recipes

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recipes/CountRecipeResultsTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recipes/CountRecipeResultsTest.java
@@ -1,11 +1,12 @@
-package com.sequenceiq.it.cloudbreak;
+package com.sequenceiq.it.cloudbreak.recipes;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
@@ -13,6 +14,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
@@ -27,23 +30,30 @@ import com.sequenceiq.cloudbreak.api.model.InstanceGroupResponse;
 import com.sequenceiq.cloudbreak.api.model.InstanceMetaDataJson;
 import com.sequenceiq.cloudbreak.client.CloudbreakClient;
 import com.sequenceiq.it.IntegrationTestContext;
+import com.sequenceiq.it.cloudbreak.AbstractCloudbreakIntegrationTest;
+import com.sequenceiq.it.cloudbreak.CloudbreakITContextConstants;
+import com.sequenceiq.it.cloudbreak.StopSshServerTest;
 
 public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StopSshServerTest.class);
+
+    private List<String> activeIPs = Collections.synchronizedList(new ArrayList<String>());
 
     @Value("${integrationtest.defaultPrivateKeyFile}")
     private String defaultPrivateKeyFile;
 
-    @Value("${integrationtest.ambariContainer}")
-    private String ambariContainer;
-
     @Test
     @Parameters({ "searchRecipesOnHosts", "lookingFor", "require" })
     public void testFetchRecipeResult(String searchRecipesOnHosts, String lookingFor, Integer require) throws Exception {
+        //GIVEN
         Assert.assertEquals(new File(defaultPrivateKeyFile).exists(), true, "Private cert file not found: " + defaultPrivateKeyFile);
         Assert.assertFalse(lookingFor.isEmpty());
 
         IntegrationTestContext itContext = getItContext();
+
         String stackId = itContext.getContextParam(CloudbreakITContextConstants.STACK_ID);
+
         StackEndpoint stackEndpoint = itContext.getContextParam(CloudbreakITContextConstants.CLOUDBREAK_CLIENT, CloudbreakClient.class).stackEndpoint();
         List<InstanceGroupResponse> instanceGroups = stackEndpoint.get(Long.valueOf(stackId)).getInstanceGroups();
         String[] files = lookingFor.split(",");
@@ -51,13 +61,20 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
         List<Future> futures = new ArrayList<>(publicIps.size() * files.length);
         ExecutorService executorService = Executors.newFixedThreadPool(publicIps.size());
         final AtomicInteger count = new AtomicInteger(0);
-
+        //WHEN
         try {
             for (final String file : files) {
                 for (final String ip : publicIps) {
                     futures.add(executorService.submit(() -> {
-                        if (findFile(ip, file)) {
-                            count.incrementAndGet();
+                        try {
+                            while (activeIPs.contains(ip)) {
+                                Thread.sleep(1000);
+                            }
+                            if (findFile(ip, file)) {
+                                count.incrementAndGet();
+                            }
+                        } catch (InterruptedException exception) {
+                            LOGGER.info("Error in thread sleep.");
                         }
                     }));
                 }
@@ -69,7 +86,7 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
         } finally {
             executorService.shutdown();
         }
-
+        //THEN
         Assert.assertEquals(count.get(), require.intValue(), "The number of existing files is different than required.");
     }
 
@@ -85,10 +102,12 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
         return ips;
     }
 
-    private Boolean findFile(String host, String file) {
+    private Boolean findFile(String host, String file) throws InterruptedException {
         Boolean resp = false;
         Session session = null;
         ChannelExec executor = null;
+        String readLine;
+        activeIPs.add(host);
         try {
             JSch jsch = new JSch();
             jsch.addIdentity(defaultPrivateKeyFile);
@@ -101,17 +120,33 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
             session.connect(10000);
 
             executor = (ChannelExec) session.openChannel("exec");
-            executor.setCommand("sudo docker exec -it $(sudo docker ps |grep " + ambariContainer + " |cut -d\" \" -f1) file " + file);
+            executor.setCommand("sudo ls " + file);
             executor.setPty(true);
+            InputStream in = executor.getInputStream();
+            OutputStream out = executor.getOutputStream();
             executor.connect(10000);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(executor.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith(file + ": empty")) {
-                    resp = true;
+            out.write("\n".getBytes());
+            out.flush();
+            Thread.sleep(1000);
+            readLine = "";
+            byte[] tmp = new byte[1024];
+            while (true) {
+                while  (in.available() > 0) {
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) {
+                        break;
+                    }
+                    readLine += new String(tmp, 0, i);
                 }
+                if (executor.isClosed()) {
+                    break;
+                }
+                Thread.sleep(1000);
             }
+            if (readLine.startsWith(file)) {
+                resp = true;
+            }
+
         } catch (JSchException | IOException e) {
             Assert.fail(e.getMessage());
         } finally {
@@ -121,6 +156,7 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
             if (session != null) {
                 session.disconnect();
             }
+            activeIPs.remove(host);
         }
         return resp;
     }
