d33c990fd470f71997a20da842ed7d0bc0a82b49
lalyos <lalyos@yahoo.com>
2016-12-16 14:17:51 +0100

    general db restore funtion

--- include/db.bash
+++ include/db.bash
@@ -1,6 +1,5 @@
 db-init() {
     env-import DB_DUMP_VOLUME cbreak_dump
-    env-import DB_DONE_FILE .cbreak.init.done
     env-import REMOVE_CONTAINER "--rm"
     cloudbreak-conf-tags
     cloudbreak-conf-db
@@ -34,9 +33,9 @@ db-dump() {
         _exit 1
     fi
 
-    local dumpContainer=cbreak_dump
+    local contName=cbreak_dump
     docker run -d \
-        --name $dumpContainer \
+        --name $contName \
         -v $dbVolume:/var/lib/postgresql/data \
         -v $DB_DUMP_VOLUME:/dump \
         postgres:$(db-getpostgres-image-version $dbVolume)
@@ -47,49 +46,72 @@ db-dump() {
     local latestDir="$volDir/latest"
     
     debug "create dump dir: $dumpDir"
-    docker exec $dumpContainer sh -c "mkdir -p $dumpDir"
+    docker exec $contName sh -c "mkdir -p $dumpDir"
 
-    while ! docker exec $dumpContainer psql -U postgres -c "select 1" &> /dev/null; do
-        debug "waiting for DB: $dumpContainer to start ..."
-        sleep 1;
-    done
+    db-wait-for-db-cont $contName
 
-    docker exec $dumpContainer sh -c 'pg_dump -U postgres > '$dumpDir'/dump.sql'
+    docker exec $contName sh -c 'pg_dump -U postgres > '$dumpDir'/dump.sql'
 
     debug "create latest simlink $latestDir -> $dumpDir"
-    docker exec $dumpContainer sh -xc "rm -f $latestDir && ln -s $dumpDir $latestDir"
+    docker exec $contName sh -xc "rm -f $latestDir && ln -s $dumpDir $latestDir"
 
-    docker stop $dumpContainer
-    [[ "$REMOVE_CONTAINER" ]] && docker rm -f $dumpContainer
+    docker stop $contName
+    [[ "$REMOVE_CONTAINER" ]] && docker rm -f $contName
 }
 
 db-init-volume-from-dump() {
     declare desc="initialize the DB if needed, from a previous dump"
     declare volName=${1:? required: volName}
+    declare dbName=${2:? required dbName}
+    # call TODO
+}
 
-    local initContainer=cbreak_initdb
-    debug "test if $DB_DONE_FILE exists ..."
-    if docker run $REMOVE_CONTAINER -v $volName:/data alpine:$DOCKER_TAG_ALPINE test -e /data/$DB_DONE_FILE &>/dev/null; then
+db-wait-for-db-cont() {
+    declare desc="wait for db container readiness"
+    declare contName=${1:? required db container name}
+
+    # todo max time
+    while ! docker exec $contName pg_isready &> /dev/null; do
+        debug "waiting for DB: $contName to start ..."
+        sleep 1;
+    done
+}
+
+db-restore-volume-from-dump() {
+    declare desc="initialize the DB if needed, from a previous dump"
+    declare volName=${1:? required: volName}
+    declare dumpDbName=${2:? required dumpDbName}
+    declare newDbName=${3:-$dumpDbName}
+
+    debug "db: $newDbName in volume: $volName will be restore from: $dumpDbName dump"
+    local dbDoneFile=".${newDbName}.done"
+    local contName=cbreak_initdb
+    debug "test if $dbDoneFile exists ..."
+    if docker run $REMOVE_CONTAINER -v $volName:/data alpine:$DOCKER_TAG_ALPINE test -e /data/$dbDoneFile &>/dev/null; then
         debug "db initaliazation is already DONE"
     else
-        debug "db initaliazation needed, search for latest backup"
+        debug "db initaliazation needed ..."
+
+        local contName=cbreak_restore
         docker run -d \
-            --name $initContainer \
+            --name $contName \
             -v $volName:/var/lib/postgresql/data \
             -v $DB_DUMP_VOLUME:/dump \
-            --entrypoint=bash \
-            postgres:$DOCKER_TAG_POSTGRES \
-              -c "echo 'touch /var/lib/postgresql/data/$DB_DONE_FILE && kill -INT 1' > /docker-entrypoint-initdb.d/99.sh && ln -s /dump/$volName/latest/dump.sql /docker-entrypoint-initdb.d/00.sql && /docker-entrypoint.sh postgres"
-            # sed -i '3 i\set -x ' /docker-entrypoint.sh ;
+            postgres:$DOCKER_TAG_POSTGRES
 
-        debug "waiting for container: $initContainer to FINISH ..."
-        while ! docker exec $initContainer test -e /var/lib/postgresql/data/$DB_DONE_FILE &>/dev/null; do
-            debug "waiting for file: /var/lib/postgresql/data/$DB_DONE_FILE"
-            sleep 1;
-        done
+        db-wait-for-db-cont $contName
+        sleep 5
+        db-wait-for-db-cont $contName
 
-        docker stop $initContainer
-        [[ "$REMOVE_CONTAINER" ]] && docker rm -f $initContainer
+        if [[ $newDbName != "postgres" ]]; then
+            debug "create new database: $dbName"
+            docker exec $contName psql -U postgres -c "create database $newDbName"
+        fi
+
+        docker exec $contName psql -U postgres $newDbName -f /dump/$dumpDbName/latest/dump.sql
+
+        docker stop $contName
+        [[ "$REMOVE_CONTAINER" ]] && docker rm -f $contName
     fi
 
 }
