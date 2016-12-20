6ffc427669d76571a198ebf4e2e204d0c906331b
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2016-12-20 12:25:46 +0100

    CLOUD-70657 Using sshj instead of jsch for ssh command execution

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recipes/CountRecipeResultsTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recipes/CountRecipeResultsTest.java
@@ -2,18 +2,16 @@ package com.sequenceiq.it.cloudbreak.recipes;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,10 +19,6 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
 import com.sequenceiq.cloudbreak.api.endpoint.StackEndpoint;
 import com.sequenceiq.cloudbreak.api.model.InstanceGroupResponse;
 import com.sequenceiq.cloudbreak.api.model.InstanceMetaDataJson;
@@ -32,19 +26,20 @@ import com.sequenceiq.cloudbreak.client.CloudbreakClient;
 import com.sequenceiq.it.IntegrationTestContext;
 import com.sequenceiq.it.cloudbreak.AbstractCloudbreakIntegrationTest;
 import com.sequenceiq.it.cloudbreak.CloudbreakITContextConstants;
-import com.sequenceiq.it.cloudbreak.StopSshServerTest;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 
 public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(StopSshServerTest.class);
-
-    private List<String> activeIPs = Collections.synchronizedList(new ArrayList<String>());
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountRecipeResultsTest.class);
 
     @Value("${integrationtest.defaultPrivateKeyFile}")
     private String defaultPrivateKeyFile;
 
     @Test
-    @Parameters({ "searchRecipesOnHosts", "lookingFor", "require" })
+    @Parameters({"searchRecipesOnHosts", "lookingFor", "require"})
     public void testFetchRecipeResult(String searchRecipesOnHosts, String lookingFor, Integer require) throws Exception {
         //GIVEN
         Assert.assertEquals(new File(defaultPrivateKeyFile).exists(), true, "Private cert file not found: " + defaultPrivateKeyFile);
@@ -66,15 +61,8 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
             for (final String file : files) {
                 for (final String ip : publicIps) {
                     futures.add(executorService.submit(() -> {
-                        try {
-                            while (activeIPs.contains(ip)) {
-                                Thread.sleep(1000);
-                            }
-                            if (findFile(ip, file)) {
-                                count.incrementAndGet();
-                            }
-                        } catch (InterruptedException exception) {
-                            LOGGER.info("Error in thread sleep.");
+                        if (findFile(ip, file)) {
+                            count.incrementAndGet();
                         }
                     }));
                 }
@@ -102,62 +90,57 @@ public class CountRecipeResultsTest extends AbstractCloudbreakIntegrationTest {
         return ips;
     }
 
-    private Boolean findFile(String host, String file) throws InterruptedException {
-        Boolean resp = false;
-        Session session = null;
-        ChannelExec executor = null;
-        String readLine;
-        activeIPs.add(host);
+    private Boolean findFile(String host, String file) {
+        SSHClient sshClient = null;
+        boolean result = false;
         try {
-            JSch jsch = new JSch();
-            jsch.addIdentity(defaultPrivateKeyFile);
-
-            Properties config = new Properties();
-            config.put("StrictHostKeyChecking", "no");
-
-            session = jsch.getSession("cloudbreak", host, 22);
-            session.setConfig(config);
-            session.connect(10000);
-
-            executor = (ChannelExec) session.openChannel("exec");
-            executor.setCommand("sudo ls " + file);
-            executor.setPty(true);
-            InputStream in = executor.getInputStream();
-            OutputStream out = executor.getOutputStream();
-            executor.connect(10000);
-            out.write("\n".getBytes());
-            out.flush();
-            Thread.sleep(1000);
-            readLine = "";
-            byte[] tmp = new byte[1024];
-            while (true) {
-                while  (in.available() > 0) {
-                    int i = in.read(tmp, 0, 1024);
-                    if (i < 0) {
-                        break;
-                    }
-                    readLine += new String(tmp, 0, i);
-                }
-                if (executor.isClosed()) {
-                    break;
-                }
-                Thread.sleep(1000);
-            }
-            if (readLine.startsWith(file)) {
-                resp = true;
-            }
-
-        } catch (JSchException | IOException e) {
-            Assert.fail(e.getMessage());
+            sshClient = createSSHClient(host, 22, "cloudbreak", defaultPrivateKeyFile);
+            Pair<Integer, String> cmdOut = executeSshCommand(sshClient, "sudo ls " + file);
+            result = cmdOut.getLeft() == 0 && cmdOut.getRight().startsWith(file);
+        } catch (Exception ex) {
+            LOGGER.error("Error during remote command execution", ex);
         } finally {
-            if (executor != null) {
-                executor.disconnect();
+            try {
+                if (sshClient != null) {
+                    sshClient.disconnect();
+                }
+            } catch (Exception ex) {
+                LOGGER.error("Error during ssh disconnect", ex);
+            }
+        }
+        return result;
+    }
+
+    private SSHClient createSSHClient(String host, int port, String user, String privateKeyFile) throws IOException {
+        SSHClient sshClient = new SSHClient();
+        sshClient.addHostKeyVerifier(new PromiscuousVerifier());
+        sshClient.connect(host, port);
+        sshClient.authPublickey(user, privateKeyFile);
+        return sshClient;
+    }
+
+    private Session startSshSession(SSHClient ssh) throws IOException {
+        Session sshSession = ssh.startSession();
+        sshSession.allocateDefaultPTY();
+        return sshSession;
+    }
+
+    private Pair<Integer, String> executeSshCommand(SSHClient ssh, String command) throws IOException {
+        Session session = null;
+        Session.Command cmd = null;
+        try {
+            session = startSshSession(ssh);
+            cmd = session.exec(command);
+            String stdout = IOUtils.readFully(cmd.getInputStream()).toString();
+            cmd.join(10, TimeUnit.SECONDS);
+            return Pair.of(cmd.getExitStatus(), stdout);
+        } finally {
+            if (cmd != null) {
+                cmd.close();
             }
             if (session != null) {
-                session.disconnect();
+                session.close();
             }
-            activeIPs.remove(host);
         }
-        return resp;
     }
 }
