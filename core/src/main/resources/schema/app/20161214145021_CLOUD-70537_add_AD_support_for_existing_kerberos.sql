016ab1d588ca3ae70f598bd2614d62f31032566a
doktoric <rdoktorics@hortonworks.com>
2016-12-27 10:37:00 +0100

    CLOUD-70540 fixed undo script

--- core/src/main/resources/schema/app/20161214145021_CLOUD-70537_add_AD_support_for_existing_kerberos.sql
+++ core/src/main/resources/schema/app/20161214145021_CLOUD-70537_add_AD_support_for_existing_kerberos.sql
@@ -7,5 +7,5 @@ ALTER TABLE kerberosconfig ADD COLUMN kerberoscontainerdn VARCHAR(255);
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-ALTER TABLE kerberosconfig DROP COLUMN kerberosldapurl VARCHAR(255);
-ALTER TABLE kerberosconfig DROP COLUMN kerberoscontainerdn VARCHAR(255);
+ALTER TABLE kerberosconfig DROP COLUMN kerberosldapurl;
+ALTER TABLE kerberosconfig DROP COLUMN kerberoscontainerdn;
