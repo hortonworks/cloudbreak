49b581dd45a800af798849dd9fc7631f058af3dc
Attila Kanto <attila.kanto@gmail.com>
2017-01-03 14:00:53 +0100

    CLOUD-71471 autocreate default security groups

--- core/src/main/resources/schema/app/20161206195934_GIT-2064_add_columns_securityGroup_tbl.sql
+++ core/src/main/resources/schema/app/20161206195934_GIT-2064_add_columns_securityGroup_tbl.sql
@@ -4,7 +4,20 @@
 ALTER TABLE securitygroup ADD COLUMN securitygroupid CHARACTER VARYING (255);
 ALTER TABLE securitygroup ADD COLUMN cloudplatform CHARACTER VARYING (255);
 
-UPDATE securitygroup SET cloudplatform='AWS' where cloudplatform is NULL;
+UPDATE securitygroup SET name = 'default-aws-all-services-port' WHERE name = 'all-services-port';
+UPDATE securitygroup SET name = 'default-aws-only-ssh-and-ssl' WHERE name = 'only-ssh-and-ssl';
+
+UPDATE securitygroup SET cloudplatform='AWS' WHERE name = 'default-aws-all-services-port';
+UPDATE securitygroup SET cloudplatform='AWS' WHERE name = 'default-aws-only-ssh-and-ssl';
+UPDATE securitygroup SET cloudplatform='AZURE_RM' WHERE name = 'default-azure_rm-all-services-port';
+UPDATE securitygroup SET cloudplatform='AZURE_RM' WHERE name = 'default-azure_rm-only-ssh-and-ssl';
+UPDATE securitygroup SET cloudplatform='GCP' WHERE name = 'default-gcp-all-services-port';
+UPDATE securitygroup SET cloudplatform='GCP' WHERE name = 'default-gcp-only-ssh-and-ssl';
+UPDATE securitygroup SET cloudplatform='OPENSTACK' WHERE name = 'default-openstack-all-services-port';
+UPDATE securitygroup SET cloudplatform='OPENSTACK' WHERE name = 'default-openstack-only-ssh-and-ssl';
+
+UPDATE securitygroup SET cloudplatform='AWS' WHERE cloudplatform is NULL;
+
 -- //@UNDO
 -- SQL to undo the change goes here.
 
