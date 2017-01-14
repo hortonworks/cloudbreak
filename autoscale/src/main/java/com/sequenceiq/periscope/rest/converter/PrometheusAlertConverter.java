22c9db5eab5fc2416a2a0bb790b33fb4b0f101d1
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2017-01-14 14:35:14 +0100

    CLOUD-70541 introduce service-registration app

--- autoscale/src/main/java/com/sequenceiq/periscope/rest/converter/PrometheusAlertConverter.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/rest/converter/PrometheusAlertConverter.java
@@ -2,19 +2,28 @@ package com.sequenceiq.periscope.rest.converter;
 
 import static com.sequenceiq.periscope.api.model.AlertState.CRITICAL;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sequenceiq.periscope.api.model.AlertOperator;
 import com.sequenceiq.periscope.api.model.PrometheusAlertJson;
 import com.sequenceiq.periscope.domain.PrometheusAlert;
+import com.sequenceiq.periscope.model.json.Json;
 import com.sequenceiq.periscope.service.PrometheusAlertTemplateService;
 
 @Component
 public class PrometheusAlertConverter extends AbstractConverter<PrometheusAlertJson, PrometheusAlert> {
+
+    private static final String THRESHOLD_PARAM_KEY = "threshold";
+
+    private static final String OPERATOR_PARAM_KEY = "operator";
 
     @Inject
     private PrometheusAlertTemplateService templateService;
@@ -29,9 +38,11 @@ public class PrometheusAlertConverter extends AbstractConverter<PrometheusAlertJ
         double threshold = source.getThreshold();
         String alertRuleName = source.getAlertRuleName();
         try {
-            String operator = source.getAlertOperator() != null ? source.getAlertOperator().getOperator() : AlertOperator.MORE_THAN.getOperator();
+            AlertOperator alertOperator = source.getAlertOperator() != null ? source.getAlertOperator() : AlertOperator.MORE_THAN;
+            String operator = alertOperator.getOperator();
             String alertRule = templateService.createAlert(alertRuleName, alert.getName(), String.valueOf(threshold), alert.getPeriod(), operator);
             alert.setAlertRule(alertRule);
+            alert.setParameters(createParametersFrom(threshold, alertOperator));
         } catch (Exception e) {
             throw new ConversionFailedException(
                     TypeDescriptor.valueOf(PrometheusAlertJson.class),
@@ -52,6 +63,17 @@ public class PrometheusAlertConverter extends AbstractConverter<PrometheusAlertJ
         json.setPeriod(source.getPeriod());
         json.setAlertRuleName(source.getAlertRule());
         json.setAlertState(source.getAlertState());
+
+        Map<String, Object> parameters = source.getParameters().getMap();
+        json.setAlertOperator(AlertOperator.valueOf(String.valueOf(parameters.get(OPERATOR_PARAM_KEY))));
+        json.setThreshold(Double.valueOf(String.valueOf(parameters.get(THRESHOLD_PARAM_KEY))));
         return json;
+    }
+
+    private Json createParametersFrom(double threshold, AlertOperator operator) throws JsonProcessingException {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(THRESHOLD_PARAM_KEY, String.valueOf(threshold));
+        parameters.put(OPERATOR_PARAM_KEY, operator.name());
+        return new Json(parameters);
     }
 }
---
+++ orchestrator-salt/src/main/resources/salt/salt/service-registration/init.sls
@@ -0,0 +1,33 @@
+download_service_registration:
+  file.managed:
+    - name: /tmp/service-registration.tgz
+    - source: https://github.com/sequenceiq/service-registration/releases/download/v0.2/service-registration_0.2_Linux_x86_64.tgz
+    - skip_verify: True
+    - unless: ls -1 /tmp/service-registration.tgz
+
+unpack_service_registration:
+  cmd.run:
+    - name: cd /tmp && tar -zxf service-registration.tgz && chmod +x service-registration && mv service-registration /usr/local/bin/
+    - unless: ls -1 /usr/local/bin/service-registration
+
+{% if salt['grains.get']('init') == 'systemd' %}
+
+service_registration_service:
+  file.managed:
+    - name: /etc/systemd/system/service-registration.service
+    - source: salt://service-registration/systemd/service-registration.service
+
+{% else %}
+
+/etc/init.d/service-registration:
+  file.managed:
+    - makedirs: True
+    - source: salt://service-registration/init.d/service-registration
+    - mode: 755
+
+{% endif %}
+
+run_service_registration:
+  service.running:
+    - name: service-registration
+    - enable: True
