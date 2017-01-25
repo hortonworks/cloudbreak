6fb801cc126fe339b75f0e7462eeadfb147110cb
mhmxs <kovacsricsi@gmail.com>
2017-01-25 15:56:57 +0100

    CLOUD-70476 Manual repar flow restart

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/stack/repair/ManualStackRepairTriggerState.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/stack/repair/ManualStackRepairTriggerState.java
@@ -1,11 +1,20 @@
 package com.sequenceiq.cloudbreak.core.flow2.stack.repair;
 
 import com.sequenceiq.cloudbreak.core.flow2.FlowState;
+import com.sequenceiq.cloudbreak.core.flow2.RestartAction;
+import com.sequenceiq.cloudbreak.core.flow2.restart.WaitForSyncRestartAction;
 
 public enum ManualStackRepairTriggerState implements FlowState {
     INIT_STATE,
     UNHEALTHY_INSTANCES_DETECTION_STATE,
     NOTIFY_STACK_REPAIR_SERVICE_STATE,
     MANUAL_STACK_REPAIR_TRIGGER_FAILED_STATE,
-    FINAL_STATE
+    FINAL_STATE;
+
+    private Class<? extends RestartAction> restartAction = WaitForSyncRestartAction.class;
+
+    @Override
+    public Class<? extends RestartAction> restartAction() {
+        return restartAction;
+    }
 }
