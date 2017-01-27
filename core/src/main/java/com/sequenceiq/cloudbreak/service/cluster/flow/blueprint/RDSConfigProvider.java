6871fb1f3703633d5679a144f5dc8bd9310727d8
Slim Bouguerra <slim.bouguerra@gmail.com>
2017-01-26 18:26:53 -0800

    [BUG-71190] adding druid as part of the rdsConfig

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/RDSConfigProvider.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/RDSConfigProvider.java
@@ -4,6 +4,7 @@ package com.sequenceiq.cloudbreak.service.cluster.flow.blueprint;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sequenceiq.cloudbreak.common.type.RdsType;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.domain.RDSConfig;
@@ -13,6 +14,13 @@ public class RDSConfigProvider {
 
     public List<BlueprintConfigurationEntry> getConfigs(RDSConfig rdsConfig) {
         List<BlueprintConfigurationEntry> bpConfigs = new ArrayList<>();
+        if (rdsConfig.getType().equals(RdsType.DRUID)) {
+            bpConfigs.add(new BlueprintConfigurationEntry("druid-common", "druid.metadata.storage.type", "postgresql"));
+            bpConfigs.add(new BlueprintConfigurationEntry("druid-common", "druid.metadata.storage.connector.connectURI", rdsConfig.getConnectionURL()));
+            bpConfigs.add(new BlueprintConfigurationEntry("druid-common", "druid.metadata.storage.connector.user", rdsConfig.getConnectionUserName()));
+            bpConfigs.add(new BlueprintConfigurationEntry("druid-common", "druid.metadata.storage.connector.password", rdsConfig.getConnectionPassword()));
+            return bpConfigs;
+        }
         bpConfigs.add(new BlueprintConfigurationEntry("hive-site", "javax.jdo.option.ConnectionURL", rdsConfig.getConnectionURL()));
         bpConfigs.add(new BlueprintConfigurationEntry("hive-site", "javax.jdo.option.ConnectionDriverName", rdsConfig.getDatabaseType().getDbDriver()));
         bpConfigs.add(new BlueprintConfigurationEntry("hive-site", "javax.jdo.option.ConnectionUserName", rdsConfig.getConnectionUserName()));
