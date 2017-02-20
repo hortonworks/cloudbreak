27760f266e768715c1d27a8229c5644d942c9cfc
lalyos <lalyos@yahoo.com>
2017-02-20 22:13:06 +0100

    CLOUD-75338 Uprade traefik docker image to v1.1.2

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -25,7 +25,7 @@ cloudbreak-conf-tags() {
 
     env-import DOCKER_TAG_ALPINE 3.1
     env-import DOCKER_TAG_HAVEGED 1.1.0
-    env-import DOCKER_TAG_TRAEFIK v1.0.0
+    env-import DOCKER_TAG_TRAEFIK v1.1.2
     env-import DOCKER_TAG_CONSUL 0.5
     env-import DOCKER_TAG_REGISTRATOR v5
     env-import DOCKER_TAG_POSTFIX latest
