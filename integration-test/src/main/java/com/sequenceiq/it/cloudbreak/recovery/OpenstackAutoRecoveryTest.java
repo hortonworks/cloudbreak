57ddc41eaa1e02d068a5e8a0bace97de5b65f3fa
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2017-02-21 00:20:38 +0100

    CLOUD-71221 Status checking refactor

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recovery/OpenstackAutoRecoveryTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recovery/OpenstackAutoRecoveryTest.java
@@ -1,31 +1,15 @@
 package com.sequenceiq.it.cloudbreak.recovery;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.sequenceiq.cloudbreak.api.endpoint.StackEndpoint;
-import com.sequenceiq.cloudbreak.api.model.StackResponse;
-import com.sequenceiq.it.IntegrationTestContext;
-import com.sequenceiq.it.cloudbreak.AbstractCloudbreakIntegrationTest;
-import com.sequenceiq.it.cloudbreak.CloudbreakITContextConstants;
-import com.sequenceiq.it.cloudbreak.CloudbreakUtil;
-import com.sequenceiq.it.cloudbreak.WaitResult;
-import com.sequenceiq.it.cloudbreak.scaling.ScalingUtil;
 
 
-public class OpenstackAutoRecoveryTest extends AbstractCloudbreakIntegrationTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(com.sequenceiq.it.cloudbreak.recovery.OpenstackAutoRecoveryTest.class);
-
+public class OpenstackAutoRecoveryTest extends AbstractAutoRecoveryTest {
     @Value("${integrationtest.openstackcredential.tenantName}")
     private String defaultTenantName;
 
@@ -39,49 +23,19 @@ public class OpenstackAutoRecoveryTest extends AbstractCloudbreakIntegrationTest
     private String defaultEndpoint;
 
     @BeforeMethod
-    public void setContextParameters() {
-        Assert.assertNotNull(getItContext().getContextParam(CloudbreakITContextConstants.STACK_ID), "Stack id is mandatory.");
-        Assert.assertNotNull(getItContext().getContextParam(CloudbreakITContextConstants.AMBARI_USER_ID), "Ambari user id is mandatory.");
-        Assert.assertNotNull(getItContext().getContextParam(CloudbreakITContextConstants.AMBARI_PASSWORD_ID), "Ambari password id is mandatory.");
-        Assert.assertNotNull(getItContext().getContextParam(CloudbreakITContextConstants.AMBARI_PORT_ID), "Ambari port id is mandatory.");
-    }
-
-    @Test
-    @Parameters({ "hostGroup", "removedInstanceCount", "tenantName", "userName", "password", "endpoint" })
-    public void testAutoRecovery(String hostGroup, @Optional("0") Integer removedInstanceCount, @Optional("") String tenantName, @Optional("") String userName,
-            @Optional("") String password, @Optional("") String endpoint) throws Exception {
-
-        //GIVEN
+    @Parameters({ "tenantName", "userName", "password", "endpoint" })
+    public void setCloudProviderParameters(@Optional("") String tenantName,
+            @Optional("") String userName, @Optional("") String password, @Optional("") String endpoint) {
         tenantName = StringUtils.hasLength(tenantName) ? tenantName : defaultTenantName;
         userName = StringUtils.hasLength(userName) ? userName : defaultUserName;
         password = StringUtils.hasLength(password) ? password : defaultPassword;
         endpoint = StringUtils.hasLength(endpoint) ? endpoint : defaultEndpoint;
-        IntegrationTestContext itContext = getItContext();
-        String stackId = itContext.getContextParam(CloudbreakITContextConstants.STACK_ID);
-        String ambariUser = itContext.getContextParam(CloudbreakITContextConstants.AMBARI_USER_ID);
-        String ambariPassword = itContext.getContextParam(CloudbreakITContextConstants.AMBARI_PASSWORD_ID);
-        String ambariPort = itContext.getContextParam(CloudbreakITContextConstants.AMBARI_PORT_ID);
 
-        StackEndpoint stackEndpoint = getCloudbreakClient().stackEndpoint();
-        StackResponse stackResponse = stackEndpoint.get(Long.valueOf(stackId));
-
-        RecoveryUtil.deleteOpenstackInstance(endpoint, userName, password, tenantName, RecoveryUtil.getInstanceId(stackResponse, hostGroup));
-
-        Integer expectedNodeCountAmbari = ScalingUtil.getNodeCountAmbari(stackEndpoint, ambariPort, stackId, ambariUser, ambariPassword, itContext)
-                - removedInstanceCount;
-
-        WaitResult waitResult = CloudbreakUtil.waitForHostStatusStack(stackEndpoint, stackId, hostGroup, "WAITING_FOR_REPAIR");
-
-        if (waitResult == WaitResult.TIMEOUT) {
-            Assert.fail("Timeout happened when waiting for the desired host state");
-        }
-        //WHEN
-        //THEN
-        List<String> desiredStatusList = new ArrayList<>();
-        desiredStatusList.add("AVAILABLE");
-        desiredStatusList.add("AVAILABLE");
-        CloudbreakUtil.waitAndCheckStatuses(getCloudbreakClient(), stackId, desiredStatusList);
-        Integer actualNodeCountAmbari = ScalingUtil.getNodeCountAmbari(stackEndpoint, ambariPort, stackId, ambariUser, ambariPassword, itContext);
-        Assert.assertEquals(expectedNodeCountAmbari, actualNodeCountAmbari);
+        Map<String, String> cpd = getCloudProviderParams();
+        cpd.put("cloudProvider", "OPENSTACK");
+        cpd.put("tenantName", tenantName);
+        cpd.put("userName", userName);
+        cpd.put("password", password);
+        cpd.put("endpoint", endpoint);
     }
 }
