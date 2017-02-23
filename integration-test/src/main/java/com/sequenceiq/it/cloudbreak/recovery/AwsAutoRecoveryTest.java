79bbe5d1ff5fb5b49258c01c7283127dc4a68186
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2017-02-23 16:09:11 +0100

    CLOUD-71221 region parameter fix

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recovery/AwsAutoRecoveryTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/recovery/AwsAutoRecoveryTest.java
@@ -4,13 +4,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-import com.amazonaws.regions.Regions;
-
 public class AwsAutoRecoveryTest extends AbstractAutoRecoveryTest {
     @BeforeMethod
     @Parameters("region")
-    public void setCloudProviderParameters(@Optional Regions region) {
+    public void setCloudProviderParameters(@Optional String region) {
         getCloudProviderParams().put("cloudProvider", "AWS");
-        getCloudProviderParams().put("region", region.getName());
+        getCloudProviderParams().put("region", region);
     }
 }
