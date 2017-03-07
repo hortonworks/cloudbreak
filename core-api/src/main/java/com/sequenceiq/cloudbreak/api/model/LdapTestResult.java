2c895001912a87b136f772f6a5cacad6bc07151f
doktoric <rdoktorics@hortonworks.com>
2017-03-07 09:56:44 +0100

    CLOUD-76661 added enpoint for ldap connection test

---
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/LdapTestResult.java
@@ -0,0 +1,29 @@
+package com.sequenceiq.cloudbreak.api.model;
+
+import com.sequenceiq.cloudbreak.doc.ModelDescriptions;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("RdsTestResult")
+public class LdapTestResult implements JsonEntity {
+
+    @ApiModelProperty(value = ModelDescriptions.ClusterModelDescription.LDAP_CONNECTION_RESULT, required = true)
+    private String connectionResult;
+
+    public LdapTestResult() {
+
+    }
+
+    public LdapTestResult(String connectionResult) {
+        this.connectionResult = connectionResult;
+    }
+
+    public String getConnectionResult() {
+        return connectionResult;
+    }
+
+    public void setConnectionResult(String connectionResult) {
+        this.connectionResult = connectionResult;
+    }
+}
