1878195a820a748c0ff616a9b28469e2ae3b9371
Tamas Bihari <tamas.bihari@sequenceiq.com>
2017-03-17 10:25:18 +0100

    CLOUD-77200 do not switch off autorecovery when autoscaling disabled manually

--- autoscale/src/main/java/com/sequenceiq/periscope/monitor/AmbariAgentHealthMonitor.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/monitor/AmbariAgentHealthMonitor.java
@@ -1,13 +1,18 @@
 package com.sequenceiq.periscope.monitor;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.periscope.api.model.ClusterState;
 import com.sequenceiq.periscope.domain.Cluster;
 import com.sequenceiq.periscope.monitor.evaluator.AmbariAgentHealthEvaluator;
 import com.sequenceiq.periscope.monitor.evaluator.EvaluatorContext;
+import com.sequenceiq.periscope.monitor.evaluator.EvaluatorExecutor;
 
 @Component
 public class AmbariAgentHealthMonitor extends AbstractMonitor implements Monitor {
@@ -30,5 +35,17 @@ public class AmbariAgentHealthMonitor extends AbstractMonitor implements Monitor
     @Override
     public Map<String, Object> getContext(Cluster cluster) {
         return Collections.singletonMap(EvaluatorContext.CLUSTER_ID.name(), cluster.getId());
+    }
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        evalContext(context);
+        List<Cluster> clustersToRecover = getClusterService().findAll(ClusterState.RUNNING);
+        clustersToRecover.addAll(getClusterService().findAll(ClusterState.DISABLED));
+        for (Cluster cluster : clustersToRecover) {
+            EvaluatorExecutor evaluatorExecutor = getApplicationContext().getBean(getEvaluatorType().getSimpleName(), EvaluatorExecutor.class);
+            evaluatorExecutor.setContext(getContext(cluster));
+            getExecutorService().submit(evaluatorExecutor);
+        }
     }
 }
