74ed1c16bf1785855d8af96709e15d5b79ae2dd5
jenkins-sequenceiq <jenkins-sequenceiq@sequenceiq.com>
2017-04-10 15:33:10 +0200

    Updated container versions to 1.15.0-dev.144

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -32,11 +32,11 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_UAA 3.6.5
     env-import DOCKER_TAG_AMBASSADOR 0.5.0
     env-import DOCKER_TAG_CERT_TOOL 0.0.3
-    env-import DOCKER_TAG_PERISCOPE 1.15.0-dev.143
-    env-import DOCKER_TAG_CLOUDBREAK 1.15.0-dev.143
-    env-import DOCKER_TAG_ULUWATU 1.15.0-dev.143
-    env-import DOCKER_TAG_SULTANS 1.15.0-dev.143
-    env-import DOCKER_TAG_CLOUDBREAK_SHELL 1.15.0-dev.143
+    env-import DOCKER_TAG_PERISCOPE 1.15.0-dev.144
+    env-import DOCKER_TAG_CLOUDBREAK 1.15.0-dev.144
+    env-import DOCKER_TAG_ULUWATU 1.15.0-dev.144
+    env-import DOCKER_TAG_SULTANS 1.15.0-dev.144
+    env-import DOCKER_TAG_CLOUDBREAK_SHELL 1.15.0-dev.144
     env-import DOCKER_TAG_POSTGRES 9.6.1-alpine
 
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.1.0
