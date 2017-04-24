04939f692d79694531566e707bfe21f9b41a3172
doktoric <rdoktorics@hortonworks.com>
2017-04-24 09:46:24 +0200

    CLOUD-79430 fixed hdp and ambari version

--- cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/model/DefaultHDPInfo.java
+++ cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/model/DefaultHDPInfo.java
@@ -1,13 +1,8 @@
 package com.sequenceiq.cloudbreak.cloud.model;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@ConfigurationProperties("cb.hdp")
-@Component
 public class DefaultHDPInfo extends HDPInfo {
 
 
