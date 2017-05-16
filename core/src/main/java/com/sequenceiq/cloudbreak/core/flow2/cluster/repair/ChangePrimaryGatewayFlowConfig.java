40b1fb24569fef8be63d2374c6018a73022aed5c
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2017-05-16 15:34:59 +0200

    CLOUD-80485 change gateway address in traefik

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/repair/ChangePrimaryGatewayFlowConfig.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/repair/ChangePrimaryGatewayFlowConfig.java
@@ -7,11 +7,14 @@ import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryG
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayEvent.CHANGE_PRIMARY_GATEWAY_FINISHED;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayEvent.CHANGE_PRIMARY_GATEWAY_FLOW_FINISHED;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayEvent.CHANGE_PRIMARY_GATEWAY_TRIGGER_EVENT;
-import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.FINAL_STATE;
-import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.INIT_STATE;
+import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayEvent.REGISTER_PROXY_FAILED_EVENT;
+import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayEvent.REGISTER_PROXY_FINISHED_EVENT;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.CHANGE_PRIMARY_GATEWAY_FAILED_STATE;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.CHANGE_PRIMARY_GATEWAY_FINISHED_STATE;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.CHANGE_PRIMARY_GATEWAY_STATE;
+import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.FINAL_STATE;
+import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.INIT_STATE;
+import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.UPDATE_PROXY_STATE;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.repair.ChangePrimaryGatewayState.WAITING_FOR_AMBARI_SERVER_STATE;
 
 import java.util.List;
@@ -22,13 +25,15 @@ import com.sequenceiq.cloudbreak.core.flow2.config.AbstractFlowConfiguration;
 
 @Component
 public class ChangePrimaryGatewayFlowConfig extends AbstractFlowConfiguration<ChangePrimaryGatewayState, ChangePrimaryGatewayEvent> {
+
     private static final List<Transition<ChangePrimaryGatewayState, ChangePrimaryGatewayEvent>> TRANSITIONS =
             new Transition.Builder<ChangePrimaryGatewayState, ChangePrimaryGatewayEvent>()
                     .defaultFailureEvent(CHANGE_PRIMARY_GATEWAY_FAILED)
                     .from(INIT_STATE).to(CHANGE_PRIMARY_GATEWAY_STATE).event(CHANGE_PRIMARY_GATEWAY_TRIGGER_EVENT).noFailureEvent()
                     .from(CHANGE_PRIMARY_GATEWAY_STATE).to(WAITING_FOR_AMBARI_SERVER_STATE).event(CHANGE_PRIMARY_GATEWAY_FINISHED).defaultFailureEvent()
-                    .from(WAITING_FOR_AMBARI_SERVER_STATE).to(CHANGE_PRIMARY_GATEWAY_FINISHED_STATE).event(AMBARI_SERVER_STARTED)
-                        .failureEvent(AMBARI_SERVER_START_FAILED)
+                    .from(WAITING_FOR_AMBARI_SERVER_STATE).to(UPDATE_PROXY_STATE).event(AMBARI_SERVER_STARTED).failureEvent(AMBARI_SERVER_START_FAILED)
+                    .from(UPDATE_PROXY_STATE).to(CHANGE_PRIMARY_GATEWAY_FINISHED_STATE).event(REGISTER_PROXY_FINISHED_EVENT)
+                    .failureEvent(REGISTER_PROXY_FAILED_EVENT)
                     .from(CHANGE_PRIMARY_GATEWAY_FINISHED_STATE).to(FINAL_STATE).event(CHANGE_PRIMARY_GATEWAY_FLOW_FINISHED).defaultFailureEvent()
                     .build();
 
@@ -56,6 +61,6 @@ public class ChangePrimaryGatewayFlowConfig extends AbstractFlowConfiguration<Ch
 
     @Override
     public ChangePrimaryGatewayEvent[] getInitEvents() {
-        return new ChangePrimaryGatewayEvent[] { CHANGE_PRIMARY_GATEWAY_TRIGGER_EVENT};
+        return new ChangePrimaryGatewayEvent[]{CHANGE_PRIMARY_GATEWAY_TRIGGER_EVENT};
     }
 }
