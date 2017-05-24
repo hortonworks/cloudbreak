7c65e2f94024c11cc07872f610dab46e692815b6
Tamas Bihari <tamas.bihari@sequenceiq.com>
2017-05-24 18:52:42 +0200

    CLOUD-81291 add SmartSense metadata to the provisioned clusters through Blueprint

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/SmartSenseConfigProvider.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/SmartSenseConfigProvider.java
@@ -21,9 +21,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sequenceiq.cloudbreak.domain.Credential;
 import com.sequenceiq.cloudbreak.domain.HostGroup;
 import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.domain.json.Json;
 import com.sequenceiq.cloudbreak.service.hostgroup.HostGroupService;
 
 @Component
@@ -38,8 +41,24 @@ public class SmartSenseConfigProvider {
 
     private static final String NAMENODE_COMPONENT = "NAMENODE";
 
+    private static final int SMART_SENSE_CLUSTER_NAME_MAX_LENGTH = 64;
+
+    private static final String SMART_SENSE_PRODUCT_INFO_FILE = "product-info";
+
     @Value("${cb.smartsense.configure:false}")
     private boolean configureSmartSense;
+
+    @Value("${cb.instance.uuid:}")
+    private String parentUuid;
+
+    @Value("${cb.product.id:cloudbreak}")
+    private String productId;
+
+    @Value("${cb.component.cluster.id:cloudbreak-hdp}")
+    private String clustersComponentId;
+
+    @Value("${info.app.version:}")
+    private String cbVersion;
 
     @Inject
     private BlueprintProcessor blueprintProcessor;
@@ -62,12 +81,15 @@ public class SmartSenseConfigProvider {
                 Set<String> hostGroupNames = hostGroups.stream().map(getHostGroupNameMapper()).collect(Collectors.toSet());
                 blueprintText = addSmartSenseServerToBp(blueprintText, hostGroups, hostGroupNames);
                 blueprintText = blueprintProcessor.addComponentToHostgroups(HST_AGENT_COMPONENT, hostGroupNames, blueprintText);
-                configs.addAll(getSmartSenseServerConfigs());
-                configs.add(new BlueprintConfigurationEntry(SMART_SENSE_SERVER_CONFIG_FILE, "customer.smartsense.id", smartSenseId));
+                configs.addAll(getSmartSenseServerConfigs(stack, smartSenseId));
                 blueprintText = blueprintProcessor.addConfigEntries(blueprintText, configs, true);
             }
         }
         return blueprintText;
+    }
+
+    private Function<HostGroup, String> getHostGroupNameMapper() {
+        return HostGroup::getName;
     }
 
     private String addSmartSenseServerToBp(String blueprintText, Set<HostGroup> hostGroups, Set<String> hostGroupNames) {
@@ -99,14 +121,122 @@ public class SmartSenseConfigProvider {
         return blueprintText;
     }
 
-    private Function<HostGroup, String> getHostGroupNameMapper() {
-        return HostGroup::getName;
-    }
-
-    private Collection<? extends BlueprintConfigurationEntry> getSmartSenseServerConfigs() {
+    private Collection<? extends BlueprintConfigurationEntry> getSmartSenseServerConfigs(Stack stack, String smartSenseId) {
         List<BlueprintConfigurationEntry> configs = new ArrayList<>();
         configs.add(new BlueprintConfigurationEntry(SMART_SENSE_SERVER_CONFIG_FILE, "customer.account.name", "Hortonworks_Cloud_HDP"));
         configs.add(new BlueprintConfigurationEntry(SMART_SENSE_SERVER_CONFIG_FILE, "customer.notification.email", "aws-marketplace@hortonworks.com"));
+        String clusterName = getClusterName(stack);
+        configs.add(new BlueprintConfigurationEntry(SMART_SENSE_SERVER_CONFIG_FILE, "cluster.name", clusterName));
+
+        configs.add(new BlueprintConfigurationEntry(SMART_SENSE_SERVER_CONFIG_FILE, "customer.smartsense.id", smartSenseId));
+
+        HSTMetadataInstanceInfoJson instanceInfoJson = new HSTMetadataInstanceInfoJson(
+                stack.getFlexSubscription().getSubscriptionId(),
+                stack.getUuid(),
+                clusterName,
+                parentUuid);
+        HSTMetadataJson productInfo = new HSTMetadataJson(clustersComponentId, instanceInfoJson, productId, cbVersion);
+        try {
+            Json productInfoJson = new Json(productInfo);
+            configs.add(new BlueprintConfigurationEntry(SMART_SENSE_PRODUCT_INFO_FILE, "product-info-content", productInfoJson.getValue()));
+        } catch (JsonProcessingException ex) {
+            LOGGER.error("The 'product-info-content' SmartSense config could not be added to the Blueprint.");
+        }
         return configs;
+    }
+
+    private String getClusterName(Stack stack) {
+        String ssClusterNamePattern = "cbc--%s--%s";
+        String clusterName = stack.getCluster().getName();
+        String ssClusterName = String.format(ssClusterNamePattern, clusterName, stack.getUuid());
+        if (ssClusterName.length() > SMART_SENSE_CLUSTER_NAME_MAX_LENGTH) {
+            int charsOverTheLimit = ssClusterName.length() - SMART_SENSE_CLUSTER_NAME_MAX_LENGTH;
+            if (charsOverTheLimit < clusterName.length()) {
+                ssClusterName = String.format(ssClusterNamePattern, clusterName.substring(0, clusterName.length() - charsOverTheLimit), stack.getUuid());
+            } else {
+                ssClusterName = ssClusterName.substring(0, SMART_SENSE_CLUSTER_NAME_MAX_LENGTH);
+            }
+        }
+        return ssClusterName;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class HSTMetadataInstanceInfoJson {
+        private String flexPlanId;
+
+        private String guid;
+
+        private String name;
+
+        private String parentGuid;
+
+        HSTMetadataInstanceInfoJson(String flexPlanId, String guid, String name, String parentGuid) {
+            this.flexPlanId = flexPlanId;
+            this.guid = guid;
+            this.name = name;
+            this.parentGuid = parentGuid;
+        }
+
+        public String getFlexPlanId() {
+            return flexPlanId;
+        }
+
+        public String getGuid() {
+            return guid;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getParentGuid() {
+            return parentGuid;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class HSTMetadataJson {
+        private String componentId;
+
+        private HSTMetadataInstanceInfoJson instanceInfo;
+
+        private String productId;
+
+        private String productVersion;
+
+        private String schemaVersion = "1.0.0";
+
+        private String type = "cluster";
+
+        HSTMetadataJson(String componentId, HSTMetadataInstanceInfoJson instanceInfo, String productId, String productVersion) {
+            this.componentId = componentId;
+            this.instanceInfo = instanceInfo;
+            this.productId = productId;
+            this.productVersion = productVersion;
+        }
+
+        public String getComponentId() {
+            return componentId;
+        }
+
+        public HSTMetadataInstanceInfoJson getInstanceInfo() {
+            return instanceInfo;
+        }
+
+        public String getProductId() {
+            return productId;
+        }
+
+        public String getProductVersion() {
+            return productVersion;
+        }
+
+        public String getSchemaVersion() {
+            return schemaVersion;
+        }
+
+        public String getType() {
+            return type;
+        }
     }
 }
--- orchestrator-salt/src/main/resources/salt/salt/smartsense/init.sls
+++ orchestrator-salt/src/main/resources/salt/salt/smartsense/init.sls
@@ -1,6 +1,19 @@
 smartsense-hst:
   pkg.installed: []
 
+update-smartsense:
+  cmd.run:
+    - name: yum -q -y update http://s3.amazonaws.com/dev.hortonworks.com/hst/centos7/smartsense-hst-1.4.1.2.5.0.1-1814.x86_64.rpm
+    - unless: test -f /var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE/configuration/product-info.xml
+
+upgrade-smartsense-ambari-service:
+    file.copy:
+    - name:  /var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE
+    - force: True
+    - mode: 755
+    - source: /usr/hdp/share/hst/ambari-service/SMARTSENSE
+    - unless: test -f /var/lib/ambari-server/resources/stacks/HDP/2.1/services/SMARTSENSE/configuration/product-info.xml
+
 /etc/hst/conf/hst-gateway.ini:
   file.managed:
     - makedirs: True
