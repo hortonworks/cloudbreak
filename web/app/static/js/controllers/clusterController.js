19e35a2cd27e9e3933a729f2a4b80ea7739f81d8
sodre90 <erdos.peter.bme@gmail.com>
2017-06-01 15:38:30 +0200

    CLOUD-81822 fix avaibility set and wasb account key bug (ability to proceed with cluster creation is broken)

--- web/app/static/js/controllers/clusterController.js
+++ web/app/static/js/controllers/clusterController.js
@@ -89,6 +89,10 @@ angular.module('uluwatuControllers').controller('clusterController', ['$scope', 
             $scope.configureReview = false;
         }
 
+        $scope.clearWasb = function () {
+            $scope.cluster.fileSystem.properties.accountKey = "";
+        };
+
         $scope.showWizardActualElement = function(element) {
             $scope.configureHostGroups = false;
             $scope.configureFailureAction = false;
@@ -1379,6 +1383,12 @@ angular.module('uluwatuControllers').controller('clusterController', ['$scope', 
             var index = $scope.cluster.userDefinedTags.indexOf(tag);
             if (index > -1) {
                 $scope.cluster.userDefinedTags.splice(index, 1);
+            }
+        }
+
+        $scope.removeAvailabilitySetsIfDisabled = function () {
+            if (!$scope.cluster.parameters.azureAvailabilitySetsEnabled) {
+                $scope.cluster.azureAvailabilitySets = [];
             }
         }
 
