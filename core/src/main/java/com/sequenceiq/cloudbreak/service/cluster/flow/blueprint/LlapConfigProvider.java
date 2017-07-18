7b63cd01961e69b76f0cb3fcf64e2424febae82d
Marton Sereg <msereg@hortonworks.com>
2017-07-18 10:57:45 +0200

    let SA take care of LLAP confs

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/LlapConfigProvider.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/blueprint/LlapConfigProvider.java
@@ -15,15 +15,13 @@ import com.sequenceiq.cloudbreak.domain.Stack;
 public class LlapConfigProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(LlapConfigProvider.class);
 
-    private static final double PERCENTAGE_OF_LLAP_NODES = 0.75;
-
     @Inject
     private BlueprintProcessor blueprintProcessor;
 
     public String addToBlueprint(Stack stack, String blueprintText) {
         if (blueprintProcessor.componentExistsInBlueprint("HIVE_SERVER_INTERACTIVE", blueprintText)) {
             LOGGER.info("Hive server interactive exists in Blueprint");
-            List<BlueprintConfigurationEntry> configs = getConfigs((int) (stack.getFullNodeCount() * PERCENTAGE_OF_LLAP_NODES));
+            List<BlueprintConfigurationEntry> configs = getConfigs(stack.getFullNodeCount() - 1);
             blueprintText = blueprintProcessor.addConfigEntries(blueprintText, configs, false);
         }
         return blueprintText;
@@ -32,7 +30,6 @@ public class LlapConfigProvider {
     private List<BlueprintConfigurationEntry> getConfigs(Integer nodeCount) {
         List<BlueprintConfigurationEntry> configs = new ArrayList<>();
         configs.add(new BlueprintConfigurationEntry("hive-interactive-env", "num_llap_nodes", nodeCount.toString()));
-        configs.add(new BlueprintConfigurationEntry("hive-interactive-env", "num_llap_nodes_for_llap_daemons", nodeCount.toString()));
         return configs;
     }
 }
