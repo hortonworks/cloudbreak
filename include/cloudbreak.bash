7f3eee23892064e4352c1f50b714544ef5d693fb
Richard Doktorics <rdoktorics@hortonworks.com>
2017-08-04 14:05:59 +0200

    CLOUD-85581 configured identity for https usage

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -287,6 +287,8 @@ cloudbreak-conf-ui() {
     env-import ULU_HWX_CLOUD_DEFAULT_SUBNET_ID ""
     env-import ULU_HWX_CLOUD_REGISTRATION_URL ""
     env-import HWX_DOC_LINK ""
+    env-import ULU_NODE_TLS_REJECT_UNAUTHORIZED "0"
+    env-import SL_NODE_TLS_REJECT_UNAUTHORIZED "0"
     env-import ULU_SUBSCRIBE_TO_NOTIFICATIONS "false"
     env-import HWX_CLOUD_ENABLE_GOVERNANCE_AND_SECURITY "false"
 }
