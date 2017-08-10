3c54fe7f6616eb9e51b37295a78776c4a29e1316
Richard Doktorics <rdoktorics@hortonworks.com>
2017-08-10 08:41:55 +0200

    CLOUD-85973 cloudbreak logback config updated

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/logger/UniqueIdDiscriminator.java
@@ -0,0 +1,49 @@
+package com.sequenceiq.cloudbreak.logger;
+
+import com.google.common.base.Strings;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.sift.Discriminator;
+
+public class UniqueIdDiscriminator implements Discriminator<ILoggingEvent> {
+
+    private static final String KEY = "nodeid";
+
+    private final String value;
+
+    private boolean started;
+
+    public UniqueIdDiscriminator() {
+        String nodeUuidEnvVar = System.getenv("CB_INSTANCE_NODE_ID");
+        String nodeUuidEnvProperty = System.getProperty("cb.instance.node.id");
+        if (!Strings.isNullOrEmpty(nodeUuidEnvVar)) {
+            value = nodeUuidEnvVar;
+        } else if (!Strings.isNullOrEmpty(nodeUuidEnvProperty)) {
+            value = nodeUuidEnvProperty;
+        } else {
+            value = "default";
+        }
+    }
+
+    @Override
+    public String getDiscriminatingValue(ILoggingEvent iLoggingEvent) {
+        return value;
+    }
+
+    @Override
+    public String getKey() {
+        return KEY;
+    }
+
+    public void start() {
+        started = true;
+    }
+
+    public void stop() {
+        started = false;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+}
