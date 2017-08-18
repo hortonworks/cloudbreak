2b597af6d9134f7307652186e15c244385862889
mhalmy <mhalmy@hortonworks.com>
2017-08-18 11:25:36 +0200

    BUG-63867 Extend Cloudbreak API E2E testcases

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/SshUtil.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/SshUtil.java
@@ -17,83 +17,49 @@ public class SshUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(com.sequenceiq.it.cloudbreak.SshUtil.class);
 
-    public Boolean ssh(String host, String defaultPrivateKeyFile, String sshCommand, String checkType, String value) {
-        SSHClient sshClient = null;
-        boolean result = false;
-        try {
-            sshClient = createSSHClient(host, 22, "cloudbreak", defaultPrivateKeyFile);
-            Pair<Integer, String> cmdOut = executeSshCommand(sshClient, sshCommand);
+    private SshUtil() {
+    }
+
+    public static boolean executeCommand(String host, String defaultPrivateKeyFile, String sshCommand, String checkType, String value) throws IOException {
+        try (SSHClient sshClient = new SSHClient()) {
+            sshClient.addHostKeyVerifier(new PromiscuousVerifier());
+            sshClient.connect(host, 22);
+            sshClient.authPublickey("cloudbreak", defaultPrivateKeyFile);
+            Pair<Integer, String> cmdOut = execute(sshClient, sshCommand);
             LOGGER.info("Ssh command status code and output: " + cmdOut.toString());
-            result = cmdOut.getLeft() == 0 && checkCommandOutput(cmdOut, checkType, value);
-        } catch (Exception ex) {
-            LOGGER.error("Error during remote command execution", ex);
-        } finally {
-            try {
-                if (sshClient != null) {
-                    sshClient.disconnect();
-                }
-            } catch (Exception ex) {
-                LOGGER.error("Error during ssh disconnect", ex);
-            }
+            return cmdOut.getLeft() == 0 && checkCommandOutput(cmdOut, checkType, value);
         }
-        return result;
     }
 
-    private SSHClient createSSHClient(String host, int port, String user, String privateKeyFile) throws IOException {
-        SSHClient sshClient = new SSHClient();
-        sshClient.addHostKeyVerifier(new PromiscuousVerifier());
-        sshClient.connect(host, port);
-        sshClient.authPublickey(user, privateKeyFile);
-        return sshClient;
-    }
-
-    private Session startSshSession(SSHClient ssh) throws IOException {
+    private static Session startSshSession(SSHClient ssh) throws IOException {
         Session sshSession = ssh.startSession();
         sshSession.allocateDefaultPTY();
         return sshSession;
     }
 
-    private Pair<Integer, String> executeSshCommand(SSHClient ssh, String command) throws IOException {
-        Session session = null;
-        Session.Command cmd = null;
+    private static Pair<Integer, String> execute(SSHClient ssh, String command) throws IOException {
         LOGGER.info("Waiting to SSH command to be executed...");
-        try {
-            session = startSshSession(ssh);
-            cmd = session.exec(command);
-            String stdout = IOUtils.readFully(cmd.getInputStream()).toString();
-            cmd.join(10, TimeUnit.SECONDS);
-            return Pair.of(cmd.getExitStatus(), stdout);
-        } finally {
-            if (cmd != null) {
-                cmd.close();
-            }
-            if (session != null) {
-                session.close();
+        try (Session session = startSshSession(ssh)) {
+            try (Session.Command cmd = session.exec(command)) {
+                String stdout = IOUtils.readFully(cmd.getInputStream()).toString();
+                cmd.join(10, TimeUnit.SECONDS);
+                return Pair.of(cmd.getExitStatus(), stdout);
             }
         }
     }
 
-    private Boolean checkCommandOutput(Pair<Integer, String> cmdOut, String checkType, String value) throws IOException {
+    private static boolean checkCommandOutput(Pair<Integer, String> cmdOut, String checkType, String value) throws IOException {
         switch (checkType) {
             case "contains":
-                if (cmdOut.getRight().contains(value)) {
-                    return Boolean.TRUE;
-                }
-                break;
+                return cmdOut.getRight().contains(value);
             case "notContains":
-                if (!(cmdOut.getRight().contains(value))) {
-                    return Boolean.TRUE;
-                }
-                break;
+                return !(cmdOut.getRight().contains(value));
             case "beginsWith":
-                if (cmdOut.getRight().startsWith(value)) {
-                    return Boolean.TRUE;
-                }
-                break;
+                return cmdOut.getRight().startsWith(value);
             default:
                 LOGGER.info("Check type {} is not exist!", checkType);
                 break;
         }
-    return false;
+        return false;
     }
 }
