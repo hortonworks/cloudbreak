426606dbbcfdcd75fff1d3125f8c1dd4dc880117
Richard Kovacs <kovacsricsi@gmail.com>
2017-09-25 14:29:04 +0200

    BUG-88665 Make database encryptor prototyped scope

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/json/EncryptedJsonToString.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/json/EncryptedJsonToString.java
@@ -7,30 +7,35 @@ import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 
 public class EncryptedJsonToString extends JsonToString {
 
-    private static final StandardPBEStringEncryptor ENCRYPTOR = new StandardPBEStringEncryptor();
-
-    private static final StandardPBEStringEncryptor LEGACY_ENCRYPTOR = new StandardPBEStringEncryptor();
+    private static final String SECRET;
 
     static {
-        String secret = Optional.ofNullable(System.getProperty("cb.client.secret")).orElse(System.getenv("CB_CLIENT_SECRET"));
-        ENCRYPTOR.setPassword(secret);
-        LEGACY_ENCRYPTOR.setPassword("cbsecret2015");
+        SECRET = Optional.ofNullable(System.getProperty("cb.client.secret")).orElse(System.getenv("CB_CLIENT_SECRET"));
+    }
+
+    private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+
+    private final StandardPBEStringEncryptor legacyEncryptor = new StandardPBEStringEncryptor();
+
+    public EncryptedJsonToString() {
+        encryptor.setPassword(SECRET);
+        legacyEncryptor.setPassword("cbsecret2015");
     }
 
     @Override
     public String convertToDatabaseColumn(Json attribute) {
         String json = super.convertToDatabaseColumn(attribute);
-        return ENCRYPTOR.encrypt(json);
+        return encryptor.encrypt(json);
     }
 
     @Override
     public Json convertToEntityAttribute(String dbData) {
         String json;
         try {
-            json = ENCRYPTOR.decrypt(dbData);
+            json = encryptor.decrypt(dbData);
         } catch (EncryptionOperationNotPossibleException e) {
             try {
-                json = LEGACY_ENCRYPTOR.decrypt(dbData);
+                json = legacyEncryptor.decrypt(dbData);
             } catch (EncryptionOperationNotPossibleException ignored) {
                 json = dbData;
             }
