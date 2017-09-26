ad6c8daf4603d39a4ed341ca91f063bb4c079998
sodre90 <erdos.peter.bme@gmail.com>
2017-09-26 09:53:16 +0200

    CLOUD-84181 fix drop and fix revert-db.sh

--- integration-test/scripts/revert-db.sh
+++ integration-test/scripts/revert-db.sh
@@ -6,16 +6,16 @@ set +x
 
 cd $INTEGCB_LOCATION
 
-./cbd regenerate
-./cbd start cbdb
+sudo ./cbd regenerate
+sudo ./cbd start cbdb
 sudo rm -rf .schema
-./cbd migrate cbdb pending
+sudo ./cbd migrate cbdb pending
 sudo rm -rf .schema
-./cbd migrate cbdb down 10 > revert.result 2>&1
+sudo ./cbd migrate cbdb down 10 > revert.result 2>&1
 
 cat revert.result
 
-if grep -q ERROR "revert.result";
+if grep -q ERROR "revert.result" || grep -q 'Permission denied' "revert.result";
     then echo -e "\033[0;91m--- !!! REVERT DB FAILED !!! ---\n"; exit 1;
     else echo -e "\n\033[0;92m+++ !!! REVERT DB SUCCESSFULLY FINISHED !!! +++\n";
 fi
