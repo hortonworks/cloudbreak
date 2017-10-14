6ec56b9655204159273b40b10b2b24a547fda9b4
Krisztian Horvath <keyki.kk@gmail.com>
2017-10-14 21:55:45 +0200

    CLOUD-90212 fix AbstractStackFailureAction stack query

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/stack/termination/StackTerminationFailureAction.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/stack/termination/StackTerminationFailureAction.java
@@ -6,14 +6,19 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.statemachine.StateContext;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.cloud.event.Selectable;
 import com.sequenceiq.cloudbreak.common.type.MetricType;
+import com.sequenceiq.cloudbreak.core.flow2.Flow;
 import com.sequenceiq.cloudbreak.core.flow2.stack.AbstractStackFailureAction;
 import com.sequenceiq.cloudbreak.core.flow2.stack.StackFailureContext;
+import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.logger.MDCBuilder;
 import com.sequenceiq.cloudbreak.reactor.api.event.StackEvent;
 import com.sequenceiq.cloudbreak.reactor.api.event.StackFailureEvent;
+import com.sequenceiq.cloudbreak.service.stack.StackService;
 
 @Component("StackTerminationFailureAction")
 public class StackTerminationFailureAction extends AbstractStackFailureAction<StackTerminationState, StackTerminationEvent> {
@@ -22,11 +27,24 @@ public class StackTerminationFailureAction extends AbstractStackFailureAction<St
     @Inject
     private StackTerminationService stackTerminationService;
 
+    @Inject
+    private StackService stackService;
+
+    @Override
+    protected StackFailureContext createFlowContext(
+        String flowId, StateContext<StackTerminationState, StackTerminationEvent> stateContext, StackFailureEvent payload) {
+        Flow flow = getFlow(flowId);
+        Stack stack = stackService.getByIdWithLists(payload.getStackId());
+        MDCBuilder.buildMdcContext(stack);
+        flow.setFlowFailed(payload.getException());
+        return new StackFailureContext(flowId, stack);
+    }
+
     @Override
     protected void doExecute(StackFailureContext context, StackFailureEvent payload, Map<Object, Object> variables) {
         Boolean deleteDependencies = Boolean.valueOf(String.valueOf(variables.get("DELETEDEPENDENCIES")));
         stackTerminationService.handleStackTerminationError(context.getStack(), payload,
-                variables.get("FORCEDTERMINATION") != null, deleteDependencies);
+            variables.get("FORCEDTERMINATION") != null, deleteDependencies);
         metricService.incrementMetricCounter(MetricType.STACK_TERMINATION_FAILED, context.getStack());
         sendEvent(context);
     }
