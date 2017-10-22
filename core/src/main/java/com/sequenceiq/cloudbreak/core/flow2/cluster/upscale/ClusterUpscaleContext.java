b81c6ba128c45d106e3a0f0ca928b1f5be5c1ad9
Krisztian Horvath <keyki.kk@gmail.com>
2017-10-22 12:55:45 +0200

    CLOUD-90212 use a minimal stack and cluster object on cluster upscale

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/upscale/ClusterUpscaleContext.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/upscale/ClusterUpscaleContext.java
@@ -1,15 +1,15 @@
 package com.sequenceiq.cloudbreak.core.flow2.cluster.upscale;
 
-import com.sequenceiq.cloudbreak.core.flow2.cluster.ClusterContext;
-import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.core.flow2.cluster.ClusterMinimalContext;
+import com.sequenceiq.cloudbreak.domain.StackMinimal;
 
-public class ClusterUpscaleContext extends ClusterContext {
+public class ClusterUpscaleContext extends ClusterMinimalContext {
     private final String hostGroupName;
 
     private final Integer adjustment;
 
-    public ClusterUpscaleContext(String flowId, Stack stack, String hostGroupName, Integer adjustment) {
-        super(flowId, stack, stack.getCluster());
+    public ClusterUpscaleContext(String flowId, StackMinimal stack, String hostGroupName, Integer adjustment) {
+        super(flowId, stack);
         this.hostGroupName = hostGroupName;
         this.adjustment = adjustment;
     }
