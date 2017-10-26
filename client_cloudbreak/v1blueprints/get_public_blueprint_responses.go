hortonworks/cb-cli

Use Cloudbreak API v2

client_cloudbreak/v1blueprints/get_public_blueprint_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1blueprints
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// GetPublicBlueprintReader is a Reader for the GetPublicBlueprint structure.
+type GetPublicBlueprintReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPublicBlueprintReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPublicBlueprintOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPublicBlueprintOK creates a GetPublicBlueprintOK with default headers values
+func NewGetPublicBlueprintOK() *GetPublicBlueprintOK {
+	return &GetPublicBlueprintOK{}
+}
+
+/*GetPublicBlueprintOK handles this case with default header values.
+
+successful operation
+*/
+type GetPublicBlueprintOK struct {
+	Payload *models_cloudbreak.BlueprintResponse
+}
+
+func (o *GetPublicBlueprintOK) Error() string {
+	return fmt.Sprintf("[GET /v1/blueprints/account/{name}][%d] getPublicBlueprintOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPublicBlueprintOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.BlueprintResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1connectors/get_platform_security_groups_responses.go
@@ -1,0 +1,90 @@
+package v1connectors
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/swag"
+	"github.com/go-openapi/validate"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+type GetPlatformSecurityGroupsReader struct {
+	formats strfmt.Registry
+}
+
+func (o *GetPlatformSecurityGroupsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPlatformSecurityGroupsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+func NewGetPlatformSecurityGroupsOK() *GetPlatformSecurityGroupsOK {
+	return &GetPlatformSecurityGroupsOK{}
+}
+
+type GetPlatformSecurityGroupsOK struct {
+	Payload GetPlatformSecurityGroupsOKBody
+}
+
+func (o *GetPlatformSecurityGroupsOK) Error() string {
+	return fmt.Sprintf("[POST /v1/connectors/securitygroups][%d] getPlatformSecurityGroupsOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPlatformSecurityGroupsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+type GetPlatformSecurityGroupsOKBody map[string][]models_cloudbreak.PlatformSecurityGroupResponse
+
+func (o GetPlatformSecurityGroupsOKBody) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := validate.Required("getPlatformSecurityGroupsOK", "body", o); err != nil {
+		return err
+	}
+
+	for k := range o {
+
+		if err := validate.Required("getPlatformSecurityGroupsOK"+"."+k, "body", o[k]); err != nil {
+			return err
+		}
+
+		if err := validate.UniqueItems("getPlatformSecurityGroupsOK"+"."+k, "body", o[k]); err != nil {
+			return err
+		}
+
+		for i := 0; i < len(o[k]); i++ {
+
+			if swag.IsZero(o[k][i]) {
+				continue
+			}
+
+		}
+
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

client_cloudbreak/v1credentials/get_public_credential_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1credentials
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// GetPublicCredentialReader is a Reader for the GetPublicCredential structure.
+type GetPublicCredentialReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPublicCredentialReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPublicCredentialOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPublicCredentialOK creates a GetPublicCredentialOK with default headers values
+func NewGetPublicCredentialOK() *GetPublicCredentialOK {
+	return &GetPublicCredentialOK{}
+}
+
+/*GetPublicCredentialOK handles this case with default header values.
+
+successful operation
+*/
+type GetPublicCredentialOK struct {
+	Payload *models_cloudbreak.CredentialResponse
+}
+
+func (o *GetPublicCredentialOK) Error() string {
+	return fmt.Sprintf("[GET /v1/credentials/account/{name}][%d] getPublicCredentialOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPublicCredentialOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.CredentialResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1rdsconfigs/post_private_rds_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1rdsconfigs
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// PostPrivateRdsReader is a Reader for the PostPrivateRds structure.
+type PostPrivateRdsReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *PostPrivateRdsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewPostPrivateRdsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewPostPrivateRdsOK creates a PostPrivateRdsOK with default headers values
+func NewPostPrivateRdsOK() *PostPrivateRdsOK {
+	return &PostPrivateRdsOK{}
+}
+
+/*PostPrivateRdsOK handles this case with default header values.
+
+successful operation
+*/
+type PostPrivateRdsOK struct {
+	Payload *models_cloudbreak.RDSConfigResponse
+}
+
+func (o *PostPrivateRdsOK) Error() string {
+	return fmt.Sprintf("[POST /v1/rdsconfigs/user][%d] postPrivateRdsOK  %+v", 200, o.Payload)
+}
+
+func (o *PostPrivateRdsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.RDSConfigResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1smartsensesubscriptions/get_private_smart_sense_subscriptions_responses.go
@@ -1,0 +1,65 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1smartsensesubscriptions
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// GetPrivateSmartSenseSubscriptionsReader is a Reader for the GetPrivateSmartSenseSubscriptions structure.
+type GetPrivateSmartSenseSubscriptionsReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPrivateSmartSenseSubscriptionsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPrivateSmartSenseSubscriptionsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPrivateSmartSenseSubscriptionsOK creates a GetPrivateSmartSenseSubscriptionsOK with default headers values
+func NewGetPrivateSmartSenseSubscriptionsOK() *GetPrivateSmartSenseSubscriptionsOK {
+	return &GetPrivateSmartSenseSubscriptionsOK{}
+}
+
+/*GetPrivateSmartSenseSubscriptionsOK handles this case with default header values.
+
+successful operation
+*/
+type GetPrivateSmartSenseSubscriptionsOK struct {
+	Payload []*models_cloudbreak.SmartSenseSubscriptionJSON
+}
+
+func (o *GetPrivateSmartSenseSubscriptionsOK) Error() string {
+	return fmt.Sprintf("[GET /v1/smartsensesubscriptions/user][%d] getPrivateSmartSenseSubscriptionsOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPrivateSmartSenseSubscriptionsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1util/test_ldap_connection_util_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1util
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// TestLdapConnectionUtilReader is a Reader for the TestLdapConnectionUtil structure.
+type TestLdapConnectionUtilReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *TestLdapConnectionUtilReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewTestLdapConnectionUtilOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewTestLdapConnectionUtilOK creates a TestLdapConnectionUtilOK with default headers values
+func NewTestLdapConnectionUtilOK() *TestLdapConnectionUtilOK {
+	return &TestLdapConnectionUtilOK{}
+}
+
+/*TestLdapConnectionUtilOK handles this case with default header values.
+
+successful operation
+*/
+type TestLdapConnectionUtilOK struct {
+	Payload *models_cloudbreak.RdsTestResult
+}
+
+func (o *TestLdapConnectionUtilOK) Error() string {
+	return fmt.Sprintf("[POST /v1/util/ldap][%d] testLdapConnectionUtilOK  %+v", 200, o.Payload)
+}
+
+func (o *TestLdapConnectionUtilOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.RdsTestResult)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v2stacks/post_private_stack_v2_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v2stacks
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/hdc-cli/models_cloudbreak"
+)
+
+// PostPrivateStackV2Reader is a Reader for the PostPrivateStackV2 structure.
+type PostPrivateStackV2Reader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *PostPrivateStackV2Reader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewPostPrivateStackV2OK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewPostPrivateStackV2OK creates a PostPrivateStackV2OK with default headers values
+func NewPostPrivateStackV2OK() *PostPrivateStackV2OK {
+	return &PostPrivateStackV2OK{}
+}
+
+/*PostPrivateStackV2OK handles this case with default header values.
+
+successful operation
+*/
+type PostPrivateStackV2OK struct {
+	Payload *models_cloudbreak.StackResponse
+}
+
+func (o *PostPrivateStackV2OK) Error() string {
+	return fmt.Sprintf("[POST /v2/stacks/user][%d] postPrivateStackV2OK  %+v", 200, o.Payload)
+}
+
+func (o *PostPrivateStackV2OK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.StackResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

hortonworks/cloudbreak

client_cloudbreak/v1blueprints/get_public_blueprint_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1blueprints
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// GetPublicBlueprintReader is a Reader for the GetPublicBlueprint structure.
+type GetPublicBlueprintReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPublicBlueprintReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPublicBlueprintOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPublicBlueprintOK creates a GetPublicBlueprintOK with default headers values
+func NewGetPublicBlueprintOK() *GetPublicBlueprintOK {
+	return &GetPublicBlueprintOK{}
+}
+
+/*GetPublicBlueprintOK handles this case with default header values.
+
+successful operation
+*/
+type GetPublicBlueprintOK struct {
+	Payload *models_cloudbreak.BlueprintResponse
+}
+
+func (o *GetPublicBlueprintOK) Error() string {
+	return fmt.Sprintf("[GET /v1/blueprints/account/{name}][%d] getPublicBlueprintOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPublicBlueprintOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.BlueprintResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1connectors/get_platform_security_groups_responses.go
@@ -1,0 +1,90 @@
+package v1connectors
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/swag"
+	"github.com/go-openapi/validate"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+type GetPlatformSecurityGroupsReader struct {
+	formats strfmt.Registry
+}
+
+func (o *GetPlatformSecurityGroupsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPlatformSecurityGroupsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+func NewGetPlatformSecurityGroupsOK() *GetPlatformSecurityGroupsOK {
+	return &GetPlatformSecurityGroupsOK{}
+}
+
+type GetPlatformSecurityGroupsOK struct {
+	Payload GetPlatformSecurityGroupsOKBody
+}
+
+func (o *GetPlatformSecurityGroupsOK) Error() string {
+	return fmt.Sprintf("[POST /v1/connectors/securitygroups][%d] getPlatformSecurityGroupsOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPlatformSecurityGroupsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+type GetPlatformSecurityGroupsOKBody map[string][]models_cloudbreak.PlatformSecurityGroupResponse
+
+func (o GetPlatformSecurityGroupsOKBody) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := validate.Required("getPlatformSecurityGroupsOK", "body", o); err != nil {
+		return err
+	}
+
+	for k := range o {
+
+		if err := validate.Required("getPlatformSecurityGroupsOK"+"."+k, "body", o[k]); err != nil {
+			return err
+		}
+
+		if err := validate.UniqueItems("getPlatformSecurityGroupsOK"+"."+k, "body", o[k]); err != nil {
+			return err
+		}
+
+		for i := 0; i < len(o[k]); i++ {
+
+			if swag.IsZero(o[k][i]) {
+				continue
+			}
+
+		}
+
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}

client_cloudbreak/v1credentials/get_public_credential_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1credentials
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// GetPublicCredentialReader is a Reader for the GetPublicCredential structure.
+type GetPublicCredentialReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPublicCredentialReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPublicCredentialOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPublicCredentialOK creates a GetPublicCredentialOK with default headers values
+func NewGetPublicCredentialOK() *GetPublicCredentialOK {
+	return &GetPublicCredentialOK{}
+}
+
+/*GetPublicCredentialOK handles this case with default header values.
+
+successful operation
+*/
+type GetPublicCredentialOK struct {
+	Payload *models_cloudbreak.CredentialResponse
+}
+
+func (o *GetPublicCredentialOK) Error() string {
+	return fmt.Sprintf("[GET /v1/credentials/account/{name}][%d] getPublicCredentialOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPublicCredentialOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.CredentialResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1rdsconfigs/post_private_rds_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1rdsconfigs
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// PostPrivateRdsReader is a Reader for the PostPrivateRds structure.
+type PostPrivateRdsReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *PostPrivateRdsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewPostPrivateRdsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewPostPrivateRdsOK creates a PostPrivateRdsOK with default headers values
+func NewPostPrivateRdsOK() *PostPrivateRdsOK {
+	return &PostPrivateRdsOK{}
+}
+
+/*PostPrivateRdsOK handles this case with default header values.
+
+successful operation
+*/
+type PostPrivateRdsOK struct {
+	Payload *models_cloudbreak.RDSConfigResponse
+}
+
+func (o *PostPrivateRdsOK) Error() string {
+	return fmt.Sprintf("[POST /v1/rdsconfigs/user][%d] postPrivateRdsOK  %+v", 200, o.Payload)
+}
+
+func (o *PostPrivateRdsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.RDSConfigResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1smartsensesubscriptions/get_private_smart_sense_subscriptions_responses.go
@@ -1,0 +1,65 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1smartsensesubscriptions
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// GetPrivateSmartSenseSubscriptionsReader is a Reader for the GetPrivateSmartSenseSubscriptions structure.
+type GetPrivateSmartSenseSubscriptionsReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *GetPrivateSmartSenseSubscriptionsReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewGetPrivateSmartSenseSubscriptionsOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewGetPrivateSmartSenseSubscriptionsOK creates a GetPrivateSmartSenseSubscriptionsOK with default headers values
+func NewGetPrivateSmartSenseSubscriptionsOK() *GetPrivateSmartSenseSubscriptionsOK {
+	return &GetPrivateSmartSenseSubscriptionsOK{}
+}
+
+/*GetPrivateSmartSenseSubscriptionsOK handles this case with default header values.
+
+successful operation
+*/
+type GetPrivateSmartSenseSubscriptionsOK struct {
+	Payload []*models_cloudbreak.SmartSenseSubscriptionJSON
+}
+
+func (o *GetPrivateSmartSenseSubscriptionsOK) Error() string {
+	return fmt.Sprintf("[GET /v1/smartsensesubscriptions/user][%d] getPrivateSmartSenseSubscriptionsOK  %+v", 200, o.Payload)
+}
+
+func (o *GetPrivateSmartSenseSubscriptionsOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v1util/test_ldap_connection_util_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v1util
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// TestLdapConnectionUtilReader is a Reader for the TestLdapConnectionUtil structure.
+type TestLdapConnectionUtilReader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *TestLdapConnectionUtilReader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewTestLdapConnectionUtilOK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewTestLdapConnectionUtilOK creates a TestLdapConnectionUtilOK with default headers values
+func NewTestLdapConnectionUtilOK() *TestLdapConnectionUtilOK {
+	return &TestLdapConnectionUtilOK{}
+}
+
+/*TestLdapConnectionUtilOK handles this case with default header values.
+
+successful operation
+*/
+type TestLdapConnectionUtilOK struct {
+	Payload *models_cloudbreak.RdsTestResult
+}
+
+func (o *TestLdapConnectionUtilOK) Error() string {
+	return fmt.Sprintf("[POST /v1/util/ldap][%d] testLdapConnectionUtilOK  %+v", 200, o.Payload)
+}
+
+func (o *TestLdapConnectionUtilOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.RdsTestResult)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}

client_cloudbreak/v2stacks/post_private_stack_v2_responses.go
@@ -1,0 +1,67 @@
+// Code generated by go-swagger; DO NOT EDIT.
+
+package v2stacks
+
+// This file was generated by the swagger tool.
+// Editing this file might prove futile when you re-run the swagger generate command
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-openapi/runtime"
+
+	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/hortonworks/cb-cli/models_cloudbreak"
+)
+
+// PostPrivateStackV2Reader is a Reader for the PostPrivateStackV2 structure.
+type PostPrivateStackV2Reader struct {
+	formats strfmt.Registry
+}
+
+// ReadResponse reads a server response into the received o.
+func (o *PostPrivateStackV2Reader) ReadResponse(response runtime.ClientResponse, consumer runtime.Consumer) (interface{}, error) {
+	switch response.Code() {
+
+	case 200:
+		result := NewPostPrivateStackV2OK()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return result, nil
+
+	default:
+		return nil, runtime.NewAPIError("unknown error", response, response.Code())
+	}
+}
+
+// NewPostPrivateStackV2OK creates a PostPrivateStackV2OK with default headers values
+func NewPostPrivateStackV2OK() *PostPrivateStackV2OK {
+	return &PostPrivateStackV2OK{}
+}
+
+/*PostPrivateStackV2OK handles this case with default header values.
+
+successful operation
+*/
+type PostPrivateStackV2OK struct {
+	Payload *models_cloudbreak.StackResponse
+}
+
+func (o *PostPrivateStackV2OK) Error() string {
+	return fmt.Sprintf("[POST /v2/stacks/user][%d] postPrivateStackV2OK  %+v", 200, o.Payload)
+}
+
+func (o *PostPrivateStackV2OK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	o.Payload = new(models_cloudbreak.StackResponse)
+
+	// response payload
+	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}