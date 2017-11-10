e637df37b815c80419eca0a339a2ec63494104c3
Krisztian Horvath <keyki.kk@gmail.com>
2017-11-10 11:18:51 +0100

    CLOUD-91578 remove spring consul and enable class proxying

--- core/src/main/java/com/sequenceiq/cloudbreak/CloudbreakApplication.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/CloudbreakApplication.java
@@ -5,12 +5,14 @@ import static com.sequenceiq.cloudbreak.VersionedApplication.versionedApplicatio
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAutoConfiguration
 @EnableSwagger2
 @ComponentScan(basePackages = "com.sequenceiq.cloudbreak")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class CloudbreakApplication {
     public static void main(String[] args) {
         if (!versionedApplication().showVersionInfo(args)) {
