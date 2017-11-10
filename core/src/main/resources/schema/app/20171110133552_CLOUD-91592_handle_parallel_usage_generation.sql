bfde6cf254af638ea109295eaac23daecb9393ba
Krisztian Horvath <keyki.kk@gmail.com>
2017-11-10 15:40:56 +0100

    CLOUD-91592 handle concurrent usage generation

---
+++ core/src/main/resources/schema/app/20171110133552_CLOUD-91592_handle_parallel_usage_generation.sql
@@ -0,0 +1,9 @@
+-- // CLOUD-91592 handle parallel usage generation
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE ONLY cloudbreakusage ADD CONSTRAINT uk_stack_instancegroup_day UNIQUE (stackid, instancegroup, day);
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE ONLY cloudbreakusage DROP CONSTRAINT IF EXISTS uk_stack_instancegroup_day;
