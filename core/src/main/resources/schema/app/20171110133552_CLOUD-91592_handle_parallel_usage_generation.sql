29e534dc1d049096233989fbcb7fad6b32061e87
Krisztian Horvath <keyki.kk@gmail.com>
2017-11-14 07:07:57 +0100

    CLOUD-91592 cleanup table before addings unique constraint

--- core/src/main/resources/schema/app/20171110133552_CLOUD-91592_handle_parallel_usage_generation.sql
+++ core/src/main/resources/schema/app/20171110133552_CLOUD-91592_handle_parallel_usage_generation.sql
@@ -1,6 +1,7 @@
 -- // CLOUD-91592 handle parallel usage generation
 -- Migration SQL that makes the change goes here.
 
+DELETE FROM cloudbreakusage;
 ALTER TABLE ONLY cloudbreakusage ADD CONSTRAINT uk_stack_instancegroup_day UNIQUE (stackid, instancegroup, day);
 
 -- //@UNDO
