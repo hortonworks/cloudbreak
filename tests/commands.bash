7763f25993feaaba698031ac5831626d1adefa10
afarsang <afarsang@gmail.com>
2017-11-27 15:13:49 +0100

    Update commands.bash

--- tests/commands.bash
+++ tests/commands.bash
@@ -10,121 +10,120 @@ GCP_ARGS="--name cli-gcp --project-id testprojet --service-account-id testuser@s
 
 CB_BIN="/usr/local/bin/cb"
 
-
 function configure-cb() {
     $CB_BIN configure "$@" $COMMON_ARGS_WO_CLUSTER
 }
 
 function describe-cluster() {
-    $CB_BIN describe-cluster "$@"
+    $CB_BIN cluster describe "$@"
 }
 
 function start-cluster() {
-    $CB_BIN start-cluster "$@"
+    $CB_BIN cluster start "$@"
 }
 
 function stop-cluster() {
-    $CB_BIN stop-cluster "$@"
+    $CB_BIN cluster stop "$@"
 }
 
 function list-clusters() {
-    $CB_BIN list-clusters
+    $CB_BIN cluster list
 }
 
 function list-blueprints() {
-     $CB_BIN list-blueprints
+     $CB_BIN blueprint list
  }
 
  function create-blueprint {
-     $CB_BIN create-blueprint "$@"
+     $CB_BIN blueprint  create "$@"
  }
 
   function delete-blueprint() {
-     $CB_BIN delete-blueprint --name "$@"
+     $CB_BIN blueprint delete --name "$@"
  }
 
  function describe-blueprint() {
-    $CB_BIN describe-blueprint --name "$@"
+    $CB_BIN blueprint describe --name "$@"
  }
 
 
 function create-credential-aws-key() {
-        $CB_BIN create-credential aws key-based "$@" $AWS_ARGS_KEY
+        $CB_BIN credential create aws key-based "$@" $AWS_ARGS_KEY
    }
 
 function create-credential-aws-role() {
-        $CB_BIN create-credential aws role-based "$@" $AWS_ARGS_ROLE
+        $CB_BIN credential create aws role-based "$@" $AWS_ARGS_ROLE
    }
 
 function create-credential-openstack-v2() {
-        $CB_BIN create-credential openstack keystone-v2 "$@" $OPENSTACK_ARGS
+        $CB_BIN credential create openstack keystone-v2 "$@" $OPENSTACK_ARGS
    }
 
 function create-credential-openstack-v3() {
-        $CB_BIN create-credential openstack keystone-v3 "$@" $OPENSTACK_ARGS
+        $CB_BIN credential create openstack keystone-v3 "$@" $OPENSTACK_ARGS
    }
 
 function create-credential-azure() {
-        $CB_BIN create-credential azure app-based "$@" $AZURE_ARGS
+        $CB_BIN credential create azure app-based "$@" $AZURE_ARGS
    }
 
 function create-credential-gcp() {
-        $CB_BIN create-credential gcp "$@" $GCP_ARGS
+        $CB_BIN credential create gcp "$@" $GCP_ARGS
    }
 
 function list-credentials() {
-        $CB_BIN list-credentials
+        $CB_BIN credential  list
    }
 
 function describe-credential() {
-        $CB_BIN describe-credential --name openstack
+        $CB_BIN credential describe --name openstack
    }
 function delete-credential() {
-        $CB_BIN delete-credential --name cli-aws-role
+        $CB_BIN credential delete --name cli-aws-role
    }
 
 function create-cluster() {
-    $CB_BIN create-cluster "$@"
+    $CB_BIN cluster create "$@"
 }
 
 function delete-cluster() {
-    $CB_BIN delete-cluster "$@"
+    $CB_BIN cluster delete "$@"
 }
 
 function create-recipe-pre-ambari-start-url() {
-    $CB_BIN  create-recipe from-url  --name recipe-post-cli --execution-type pre-ambari-start --url ${RECIPE_URL}
+    $CB_BIN recipe  create from-url  --name recipe-post-cli --execution-type pre-ambari-start --url ${RECIPE_URL}
 }
 
 function create-recipe-post-ambari-start-url() {
-    $CB_BIN  create-recipe from-url  --name recipe-pre-cli --execution-type post-ambari-start --url ${RECIPE_URL}
+    $CB_BIN  recipe create from-url  --name recipe-pre-cli --execution-type post-ambari-start --url ${RECIPE_URL}
 }
 
 function create-recipe-post-cluster-install-url() {
-    $CB_BIN  create-recipe from-url  --name recipe-pre-cli --execution-type post-cluster-install --url ${RECIPE_URL}
+    $CB_BIN  recipe create from-url  --name recipe-pre-cli --execution-type post-cluster-install --url ${RECIPE_URL}
 }
 
 function list-recipes() {
-    $CB_BIN  list-recipes
+    $CB_BIN  recipe list
 }
 
 function delete-recipe() {
-    $CB_BIN  delete-recipe --name recipe-pre-cli
+    $CB_BIN recipe  delete --name recipe-pre-cli
 }
 
 function describe-recipe() {
-    $CB_BIN  describe-recipe --name recipe-pre-cli
+    $CB_BIN  recipe describe --name recipe-pre-cli
 }
 
 function scale-cluster() {
-    $CB_BIN  scale-cluster  "$@"
+    $CB_BIN  cluster  scale "$@"
 }
 
 function repair-cluster() {
-    $CB_BIN  repair-cluster  --name ${CLUSTER_NAME}
+    $CB_BIN  cluster  repair --name ${CLUSTER_NAME}
 }
 
 function sync-cluster() {
-    $CB_BIN  sync-cluster --nanme ${CLUSTER_NAME}
+    $CB_BIN cluster  sync --nanme ${CLUSTER_NAME}
 
 }
 
