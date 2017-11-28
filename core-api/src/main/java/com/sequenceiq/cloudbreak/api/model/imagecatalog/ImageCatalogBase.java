7daa2a6fbe65f04781621c53a264d29192539670
Gergely Topolyai <gtopolyai@hortonworks.com>
2017-11-28 15:24:57 +0100

    CLOUD-92343 Image catalog default problem

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/imagecatalog/ImageCatalogBase.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/imagecatalog/ImageCatalogBase.java
@@ -1,6 +1,8 @@
 package com.sequenceiq.cloudbreak.api.model.imagecatalog;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions;
 
@@ -10,6 +12,9 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel
 public class ImageCatalogBase {
 
+    @Size(max = 100, min = 5, message = "The length of the credential's name has to be in range of 5 to 100")
+    @Pattern(regexp = "([a-z][-a-z0-9]*[a-z0-9])",
+            message = "The name of the credential can only contain lowercase alphanumeric characters and hyphens and has start with an alphanumeric character")
     @NotNull
     @ApiModelProperty(value = ModelDescriptions.NAME, required = true)
     private String name;
