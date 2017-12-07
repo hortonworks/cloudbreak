5979f201068654ecd4f0f9e6e69f3a03e56b6e93
Gergely Topolyai <gtopolyai@hortonworks.com>
2017-12-07 10:36:09 +0100

    CLOUD-91835 Port 9443 should not be wide open

--- core/src/main/java/com/sequenceiq/cloudbreak/service/securityrule/SecurityRuleService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/securityrule/SecurityRuleService.java
@@ -17,56 +17,42 @@ public class SecurityRuleService {
 
     private static final String TCP_PROTOCOL = "tcp";
 
-    private static final String CIDR_IP_FORMAT = "%s/32";
-
-    @Value("#{'${cb.publicip:}'.split(',')}")
-    private Set<String> cloudbreakPublicIps;
-
     @Value("${cb.nginx.port:9443}")
     private String gatewayPort;
-
-    @Value("${cb.knox.port:8443}")
-    private String knoxPort;
-
-    @Value("${cb.https.port:443}")
-    private String httpsPort;
 
     @Value("${cb.ssh.port:22}")
     private String sshPort;
 
-    @Value("${cb.default.gateway.cidr:}")
-    private String defaultGatewayCidr;
+    @Value("#{'${cb.default.gateway.cidr:}'.split(',')}")
+    private Set<String> defaultGatewayCidr;
 
     public SecurityRulesResponse getPublicIps() {
 
         SecurityRulesResponse ret = new SecurityRulesResponse();
 
-        Set<String> defaultPublicIps = cloudbreakPublicIps.stream().filter(StringUtils::isNotBlank).collect(Collectors.toSet());
+        Set<String> defaultGatewayCidrs = defaultGatewayCidr.stream().filter(StringUtils::isNotBlank).collect(Collectors.toSet());
 
-        if (!defaultPublicIps.isEmpty()) {
-            defaultPublicIps.forEach(ip -> addCidrAndPort(ret, String.format(CIDR_IP_FORMAT, ip)));
-        }
-
-        if (StringUtils.isNotBlank(defaultGatewayCidr)) {
-            addCidrAndPort(ret, defaultGatewayCidr);
+        if (!defaultGatewayCidrs.isEmpty()) {
+            defaultGatewayCidr.forEach(cidr -> addCidrAndPort(ret, cidr));
         }
 
         return ret;
     }
 
     private void addCidrAndPort(SecurityRulesResponse ret, String cidr) {
-        ret.getCore().addAll(createSecurityRuleResponse(cidr, sshPort));
-        ret.getGateway().addAll(createSecurityRuleResponse(cidr, sshPort, gatewayPort, knoxPort, httpsPort));
+        ret.getGateway().addAll(createSecurityRuleResponse(cidr, sshPort, gatewayPort));
     }
 
     private List<SecurityRuleResponse> createSecurityRuleResponse(String cidr, String... ports) {
         List<SecurityRuleResponse> rules = new ArrayList<>();
         for (String port : ports) {
-            SecurityRuleResponse securityRuleResponse = new SecurityRuleResponse();
-            securityRuleResponse.setPorts(port);
-            securityRuleResponse.setProtocol(TCP_PROTOCOL);
-            securityRuleResponse.setSubnet(cidr);
-            rules.add(securityRuleResponse);
+            if (StringUtils.isNotBlank(port)) {
+                SecurityRuleResponse securityRuleResponse = new SecurityRuleResponse();
+                securityRuleResponse.setPorts(port);
+                securityRuleResponse.setProtocol(TCP_PROTOCOL);
+                securityRuleResponse.setSubnet(cidr);
+                rules.add(securityRuleResponse);
+            }
         }
         return rules;
     }
