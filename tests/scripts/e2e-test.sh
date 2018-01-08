0e11708689c037fa2b6e5ebdca39602b391062ff
mhalmy <mhalmy@hortonworks.com>
2018-01-08 15:47:09 +0100

    update parameter names

--- tests/scripts/e2e-test.sh
+++ tests/scripts/e2e-test.sh
@@ -1,8 +1,8 @@
 echo Hello world!
 
 export CLOUD_URL=$BASE_URL
-export EMAIL=$USERNAME
+export EMAIL=$USERNAME_CLI
 
-cb configure --server $CLOUD_URL --username $EMAIL --password $PASSWORD
+cb configure --server $CLOUD_URL --username $EMAIL --password $PASSWORD_CLI
 bats -t e2e/*.bats > report.tap
 cat report.tap
