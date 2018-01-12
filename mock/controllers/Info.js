fe2294eb7c9b005b4d60ca646d245249ba8a4b78
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-01-12 16:10:11 +0100

    BUG-94365 Introduce new Info Service for Mock API

---
+++ mock/controllers/Info.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var url = require('url');
+
+var Info = require('./InfoService');
+
+module.exports.getCloudbreakInfo = function getCloudbreakInfo (req, res, next) {
+    Info.getCloudbreakInfo(req.swagger.params, res, next);
+};
+
+module.exports.getCloudbreakHealth = function getCloudbreakHealth (req, res, next) {
+    Info.getCloudbreakHealth(req.swagger.params, res, next);
+};
---
+++ mock/controllers/InfoService.js
@@ -0,0 +1,43 @@
+'use strict';
+
+exports.getCloudbreakInfo = function(args, res, next) {
+    /**
+     * retrieve Cloudbreak information for admin user
+     *
+     * returns Info
+     **/
+    var examples = {};
+    examples['application/json'] =
+    {
+        "app":
+        {
+            "name":"cloudbreak",
+            "version":"2.4.0-dev.49"
+        }
+    };
+    if (Object.keys(examples).length > 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+    } else {
+        res.end();
+    }
+}
+
+exports.getCloudbreakHealth = function(args, res, next) {
+    /**
+     * retrieve Cloudbreak information for admin user
+     *
+     * returns Health
+     **/
+    var examples = {};
+    examples['application/json'] =
+    {
+        "status":"UP"
+    };
+    if (Object.keys(examples).length > 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+    } else {
+        res.end();
+    }
+}
