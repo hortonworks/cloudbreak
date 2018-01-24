3d633ff8a5172bda4d9134fee9ffec00e180140c
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-01-24 23:18:51 +0100

    BUG-94371 Fixing CB IP

--- tests/scripts/integration-test.sh
+++ tests/scripts/integration-test.sh
@@ -3,11 +3,11 @@
 echo Hello world!
 
 date
-curl 'https://192.168.99.100/cb/api/v1/blueprints/account' -H 'accept: application/json, text/plain, */*' --compressed --insecure
+curl 'https://127.0.0.1/cb/api/v1/blueprints/account' -H 'accept: application/json, text/plain, */*' --compressed --insecure
 
 curl -sX POST --insecure  -H "Accept: application/x-www-form-urlencoded" --data-urlencode 'credentials={"username":"admin@example.com","password":"cloudbreak"}' "https://127.0.0.1/identity/oauth/authorize?response_type=token&client_id=cloudbreak_shell" -w '%{redirect_url}'
 
-export BASE_URL=https://192.168.99.100
+export BASE_URL=https://127.0.0.1
 export USERNAME_CLI=admin@example.com
 export PASSWORD_CLI=cloudbreak
 #could be a bug:
