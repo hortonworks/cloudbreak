97576b6c8008670daeccbc0994e5dcf79de1de8f
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-01-24 18:46:59 +0100

    BUG-94371 refactor for commands round.2

--- tests/commands.bash
+++ tests/commands.bash
@@ -1,172 +1,172 @@
 CB_BIN="DEBUG=1 cb"
 
 function configure-cb() {
-  $CB_BIN configure "$@"
+  "$CB_BIN" configure "$@"
 }
 
 function generate-cluster-template() {
-  $CB_BIN cluster generate-template "$@"
+  "$CB_BIN" cluster generate-template "$@"
 }
 
 function list-blueprints() {
-  $CB_BIN blueprint list
+  "$CB_BIN" blueprint list
  }
 
  function create-blueprint {
-   $CB_BIN blueprint  create "$@"
+   "$CB_BIN" blueprint  create "$@"
  }
 
   function delete-blueprint() {
-   $CB_BIN blueprint delete "$@"
+   "$CB_BIN" blueprint delete "$@"
  }
 
  function describe-blueprint() {
-   $CB_BIN blueprint describe "$@"
+   "$CB_BIN" blueprint describe "$@"
  }
 
  function create-credential-aws-key() {
-   $CB_BIN credential create aws key-based "$@"
+   "$CB_BIN" credential create aws key-based "$@"
  }
 
  function create-credential-aws-role() {
-  $CB_BIN credential create aws role-based "$@"
+  "$CB_BIN" credential create aws role-based "$@"
  }
 
 function create-credential-openstack-v2() {
- $CB_BIN credential create openstack keystone-v2 "$@"
+ "$CB_BIN" credential create openstack keystone-v2 "$@"
    }
 
 function create-credential-openstack-v3() {
-  $CB_BIN credential create openstack keystone-v3 "$@"
+  "$CB_BIN" credential create openstack keystone-v3 "$@"
    }
 
 function create-credential-azure() {
-  $CB_BIN credential create azure app-based "$@"
+  "$CB_BIN" credential create azure app-based "$@"
    }
 
 function create-credential-gcp() {
- $CB_BIN credential create gcp "$@"
+ "$CB_BIN" credential create gcp "$@"
    }
 
 function list-credentials() {
- $CB_BIN credential list
+ "$CB_BIN" credential list
    }
 
 function describe-credential() {
-  $CB_BIN credential describe "$@"
+  "$CB_BIN" credential describe "$@"
    }
 function delete-credential() {
-  $CB_BIN credential delete "$@"
+  "$CB_BIN" credential delete "$@"
    }
 
 function create-recipe(){
-  $CB_BIN recipe create "$@"
+  "$CB_BIN" recipe create "$@"
 }
 
 function list-recipes() {
- $CB_BIN recipe list
+ "$CB_BIN" recipe list
 }
 
 function delete-recipe() {
- $CB_BIN recipe delete "$@"
+ "$CB_BIN" recipe delete "$@"
 }
 
 function describe-recipe() {
- $CB_BIN recipe describe "$@"
+ "$CB_BIN" recipe describe "$@"
 }
 
 function create-cluster() {
- $CB_BIN cluster create "$@"
+ "$CB_BIN" cluster create "$@"
 }
 
 function start-cluster() {
- $CB_BIN cluster start "$@"
+ "$CB_BIN" cluster start "$@"
 }
 
 function stop-cluster() {
- $CB_BIN cluster stop "$@"
+ "$CB_BIN" cluster stop "$@"
 }
 
 function list-clusters() {
- $CB_BIN cluster list
+ "$CB_BIN" cluster list
 }
 
 function delete-cluster() {
- $CB_BIN cluster delete "$@"
+ "$CB_BIN" cluster delete "$@"
 }
 
 function describe-cluster() {
- $CB_BIN cluster describe "$@"
+ "$CB_BIN" cluster describe "$@"
 }
 
 function scale-cluster() {
- $CB_BIN cluster scale "$@"
+ "$CB_BIN" cluster scale "$@"
 }
 
 function repair-cluster() {
- $CB_BIN cluster repair "$@"
+ "$CB_BIN" cluster repair "$@"
 }
 
 function sync-cluster() {
- $CB_BIN cluster sync "$@"
+ "$CB_BIN" cluster sync "$@"
 }
 
 function change-ambari-password() {
- $CB_BIN cluster change-ambari-password "$@"
+ "$CB_BIN" cluster change-ambari-password "$@"
 }
 
 function reinstall-cluster() {
- $CB_BIN cluster reinstall "$@"
+ "$CB_BIN" cluster reinstall "$@"
 }
 
 function generate-reinstall-template() {
- $CB_BIN cluster generate-reinstall-template "$@"
+ "$CB_BIN" cluster generate-reinstall-template "$@"
 }
 
 function list-ldaps() {
- $CB_BIN ldap list "$@"
+ "$CB_BIN" ldap list "$@"
 }
 
 function create-ldap() {
- $CB_BIN ldap create "$@"
+ "$CB_BIN" ldap create "$@"
 }
 
 function delete-ldap() {
- $CB_BIN  ldap delete "$@"
+ "$CB_BIN"  ldap delete "$@"
 }
 
 function availability-zone-list() {
- $CB_BIN cloud availability-zones "$@"
+ "$CB_BIN" cloud availability-zones "$@"
 }
 
 function region-list() {
- $CB_BIN cloud regions "$@"
+ "$CB_BIN" cloud regions "$@"
 }
 
 function instance-list() {
- $CB_BIN cloud instances "$@"
+ "$CB_BIN" cloud instances "$@"
 }
 
 function volume-list() {
- $CB_BIN cloud volumes "$@"
+ "$CB_BIN" cloud volumes "$@"
 }
 
 function list-image-catalog() {
- $CB_BIN imagecatalog list "$@"
+ "$CB_BIN" imagecatalog list "$@"
 }
 
 function create-image-catalog() {
- $CB_BIN imagecatalog create "$@"
+ "$CB_BIN" imagecatalog create "$@"
 }
 
 function get-images() {
- $CB_BIN imagecatalog images "$@"
+ "$CB_BIN" imagecatalog images "$@"
 }
 
 function delete-image-catalog() {
- $CB_BIN imagecatalog delete "$@"
+ "$CB_BIN" imagecatalog delete "$@"
 }
 
 function set-default-image-catalog() {
- $CB_BIN imagecatalog set-default "$@"
+ "$CB_BIN" imagecatalog set-default "$@"
 }
