319b224e3761ea213e671e2c59fb495607ec0aa0
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-02-05 19:30:48 +0100

    BUG-95894 Fixing BATS commands

--- tests/scripts/e2e-test.sh
+++ tests/scripts/e2e-test.sh
@@ -1,5 +1,5 @@
 echo Hello world!
 
 cb configure --server $BASE_URL --username $USERNAME_CLI --password $PASSWORD_CLI
-bats --tap -d e2e/*.bats | tee report.tap
+bats --tap e2e/*.bats | tee report.tap
 cat report.tap
