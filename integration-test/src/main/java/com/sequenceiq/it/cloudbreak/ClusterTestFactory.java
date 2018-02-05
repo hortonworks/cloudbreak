6432256d800ff7ce347e807a38f21e6706481a71
Arpad Farsang <afarsang@HW12482.local>
2018-02-05 15:04:29 +0100

    improvements based on github comments

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/ClusterTestFactory.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/ClusterTestFactory.java
@@ -8,16 +8,16 @@ import org.testng.annotations.Factory;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class ClusterTestFactory extends CloudbreakTest {
     @Factory
     @Parameters({"providers"})
     public Object[] clusterTestFactory(@Optional(OpenstackCloudProvider.OPENSTACK) String providers) {
         CloudProvider[] cloudProviders = CloudProviderHelper.providerFactory(providers, getTestParameter());
-        Object[] results = new Object[cloudProviders.length];
-        IntStream.range(0, results.length).forEach(index -> results[index] = new ClusterTests(cloudProviders[index],
-                getTestParameter()));
+        Object[] results = Arrays.stream(cloudProviders)
+                .map(provider->new ClusterTests(provider, getTestParameter()))
+                .toArray();
         return results;
     }
 
