246d350a401c19a8813db08dab3ce38a59d0ecff
Gergely Topolyai <gtopolyai@hortonworks.com>
2018-02-07 09:22:52 +0100

    CLOUD-95998 Filter stackBaseURL and utilBaseURL if null

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/converter/AmbariStackDetailsJsonToStackRepoDetailsConverterTest.java
@@ -0,0 +1,49 @@
+package com.sequenceiq.cloudbreak.converter;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sequenceiq.cloudbreak.api.model.AmbariStackDetailsJson;
+import com.sequenceiq.cloudbreak.cloud.model.component.StackRepoDetails;
+import com.sequenceiq.cloudbreak.controller.BadRequestException;
+
+public class AmbariStackDetailsJsonToStackRepoDetailsConverterTest extends AbstractJsonConverterTest<AmbariStackDetailsJson> {
+
+    private AmbariStackDetailsJsonToStackRepoDetailsConverter underTest;
+
+    @Before
+    public void setUp() {
+        underTest = new AmbariStackDetailsJsonToStackRepoDetailsConverter();
+    }
+
+    @Test
+    public void testConvertWhenBaseImage() {
+        StackRepoDetails result = underTest.convert(getRequest("stack/ambari-stack-details-base-image.json"));
+
+        assertAllFieldsNotNull(result, Collections.singletonList("knox"));
+
+        Assert.assertFalse(result.getStack().containsKey(StackRepoDetails.CUSTOM_VDF_REPO_KEY));
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testConvertWhenVDFAndBaseURLNotProvided() {
+        underTest.convert(getRequest("stack/ambari-stack-details-empty.json"));
+    }
+
+    @Test
+    public void testConvertWhenVDFProvided() {
+
+        AmbariStackDetailsJson request = getRequest("stack/ambari-stack-details-vdf.json");
+        StackRepoDetails result = underTest.convert(request);
+
+        Assert.assertFalse(result.getStack().containsKey(request.getOs()));
+    }
+
+    @Override
+    public Class<AmbariStackDetailsJson> getRequestClass() {
+        return AmbariStackDetailsJson.class;
+    }
+}
