ab0f86b3f199e708f3ccf94b6b64e95cb959a637
Krisztian Horvath <keyki.kk@gmail.com>
2018-02-07 09:02:56 +0100

    initialize the stack and cluster related metrics for proper prometheus alerts

--- core/src/main/java/com/sequenceiq/cloudbreak/service/metrics/MetricService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/metrics/MetricService.java
@@ -1,11 +1,15 @@
 package com.sequenceiq.cloudbreak.service.metrics;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.boot.actuate.metrics.writer.Delta;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.stereotype.Service;
 
+import com.sequenceiq.cloudbreak.common.type.CloudConstants;
 import com.sequenceiq.cloudbreak.common.type.MetricType;
 import com.sequenceiq.cloudbreak.domain.Stack;
 import com.sequenceiq.cloudbreak.domain.view.StackView;
@@ -15,11 +19,30 @@ public class MetricService {
 
     private static final String METRIC_PREFIX = "cloudbreak.";
 
+    private static final String JMX_COUNTER_TYPE = "counter.";
+
+    @Inject
+    private MetricWriter metricWriter;
+
     @Inject
     private CounterService counterService;
 
     @Inject
     private GaugeService gaugeService;
+
+    @PostConstruct
+    public void init() {
+        for (MetricType metricType : MetricType.values()) {
+            String metricName = metricType.getMetricName();
+            if (metricName.startsWith("stack") || metricName.startsWith("cluster")) {
+                initJmxMetricCounter(metricName, CloudConstants.AWS);
+                initJmxMetricCounter(metricName, CloudConstants.AZURE);
+                initJmxMetricCounter(metricName, CloudConstants.GCP);
+                initJmxMetricCounter(metricName, CloudConstants.OPENSTACK);
+                initJmxMetricCounter(metricName, CloudConstants.YARN);
+            }
+        }
+    }
 
     /**
      * Increment a counter based metric.
@@ -72,6 +95,10 @@ public class MetricService {
 
     private void incrementMetricCounter(String metric) {
         counterService.increment(METRIC_PREFIX + metric.toLowerCase());
+    }
+
+    private void initJmxMetricCounter(String metric, String cloudPlatform) {
+        metricWriter.increment(new Delta<>(JMX_COUNTER_TYPE + METRIC_PREFIX + getMetricNameWithPlatform(metric, cloudPlatform), 0));
     }
 
     private String getMetricNameWithPlatform(MetricType metric, String cloudPlatform) {
