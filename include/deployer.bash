bec37e3cf0ad8eb7ba48a44d97068ea7749f9032
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2018-02-08 14:35:23 +0100

    CLOUD-93508 add timeout to cbd delete confirmation

--- include/deployer.bash
+++ include/deployer.bash
@@ -374,7 +374,7 @@ is-sub-path() {
 deployer-delete() {
     declare desc="Deletes yaml files, and all dbs. You can use '--force' to avoid confirm dialog"
     if [[ "$1" != "--force" ]] ; then
-        read -r -p "Are you sure you would like to wipe all Cloudbreak related data? [y/N] " response
+        read -r -t 10 -p "Are you sure you would like to wipe all Cloudbreak related data? [y/N] " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
             debug "Delete confirmed"
         else
