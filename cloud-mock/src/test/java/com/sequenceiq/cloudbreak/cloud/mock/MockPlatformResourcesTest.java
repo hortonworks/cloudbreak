7ccb792d731034d72fe8dc5085ebeeff14df7e6b
Richard Doktorics <rdoktorics@hortonworks.com>
2018-02-12 16:57:22 +0100

    BUG-93367 fixed default region on mock + tests

---
+++ cloud-mock/src/test/java/com/sequenceiq/cloudbreak/cloud/mock/MockPlatformResourcesTest.java
@@ -0,0 +1,36 @@
+package com.sequenceiq.cloudbreak.cloud.mock;
+
+import static com.sequenceiq.cloudbreak.cloud.model.Region.region;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
+import com.sequenceiq.cloudbreak.cloud.model.CloudRegions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockPlatformResourcesTest {
+
+    private MockPlatformResources underTest = new MockPlatformResources();
+
+    @Test
+    public void getDefaultRegionWhenNoDefaultFoundForMockProviderThenShouldReturnWithTheFirstElement() throws Exception {
+        ReflectionTestUtils.setField(underTest, "defaultRegions", "AWS:eu-west-1");
+        underTest.init();
+        CloudRegions regions = underTest.regions(new CloudCredential(1L, "mock"), region("mock"), new HashMap<>());
+        Assert.assertEquals("USA", regions.getDefaultRegion());
+    }
+
+    @Test
+    public void getDefaultRegionWhenDefaultFoundForMockProviderThenShouldReturnWithDefaultElement() throws Exception {
+        ReflectionTestUtils.setField(underTest, "defaultRegions", "AWS:eu-west-1,MOCK:Europe");
+        underTest.init();
+        CloudRegions regions = underTest.regions(new CloudCredential(1L, "mock"), region("mock"), new HashMap<>());
+        Assert.assertEquals("Europe", regions.getDefaultRegion());
+    }
+}
