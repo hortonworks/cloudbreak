8e90d1d4b4bdbd4591579e3aa6b7094b56a2da51
Arpad Farsang <afarsang@HW12482.local>
2018-02-16 14:54:10 +0100

    BUG-96794 add verbose log message to test report

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/Log.java
@@ -0,0 +1,20 @@
+package com.sequenceiq.it.cloudbreak.newway;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.Reporter;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class Log {
+    private Log() {
+    }
+
+    public static void logJSON(String message, Object jsonObject) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer, jsonObject);
+
+        Reporter.log(message + writer.toString());
+    }
+}
