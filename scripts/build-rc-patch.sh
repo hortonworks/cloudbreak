eb677177380c6db4c73230a4a7457946d6eac3b7
sodre90 <perdos@hortonworks.com>
2018-02-23 14:52:40 +0100

    BUG-97298 Update grgit plugin because Github removes algorithms

--- scripts/build-rc-patch.sh
+++ scripts/build-rc-patch.sh
@@ -5,9 +5,16 @@
 MINOR_VERSION=$(echo "$BRANCH" | cut -d'-' -f 2)
 
 if [ "$(git tag -l "$MINOR_VERSION.0-rc.1")" == '' ]; then
-    ./gradlew -Penv=jenkins -b build.gradle clean build release uploadArchives -Prelease.scope=minor -Prelease.stage=rc --info --stacktrace --parallel
+    ./gradlew -Penv=jenkins -b build.gradle clean build uploadArchives -Preckon.scope=minor -Preckon.stage=rc --info --stacktrace --parallel
+    RECKONED_VERSION=$(./gradlew -Penv=jenkins -b build.gradle buildInfo -Preckon.scope=minor -Preckon.stage=rc | grep Reckoned)
 else
-    ./gradlew -Penv=jenkins -b build.gradle clean build release uploadArchives -Prelease.scope=patch -Prelease.stage=rc --info --stacktrace --parallel
+    ./gradlew -Penv=jenkins -b build.gradle clean build uploadArchives -Preckon.scope=patch -Preckon.stage=rc --info --stacktrace --parallel
+    RECKONED_VERSION=$(./gradlew -Penv=jenkins -b build.gradle buildInfo -Preckon.scope=patch -Preckon.stage=rc | grep Reckoned)
 fi
 
-echo VERSION=$(git describe --abbrev=0 --tags) > $WORKSPACE/version
+VERSION=${RECKONED_VERSION#Reckoned version: }
+
+git tag $VERSION
+git push origin $VERSION
+
+echo VERSION=$VERSION > $WORKSPACE/version
