835570b6cd9fb7c93f226fab604421044a07e110
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-02-26 18:49:50 +0100

    BUG-97053 V2 Connectors Regions test cases fixes

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/RegionTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/RegionTests.java
@@ -20,18 +20,18 @@ import org.testng.annotations.Test;
 import javax.ws.rs.ForbiddenException;
 import java.util.Collection;
 
-public class ConnectorsTests extends CloudbreakTest {
+public class RegionTests extends CloudbreakTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectorsTests.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegionTests.class);
 
     private String errorMessage = "";
 
     private CloudProvider cloudProvider;
 
-    public ConnectorsTests() {
+    public RegionTests() {
     }
 
-    public ConnectorsTests(CloudProvider cp, TestParameter tp) {
+    public RegionTests(CloudProvider cp, TestParameter tp) {
         this.cloudProvider = cp;
         setTestParameter(tp);
     }
@@ -39,9 +39,9 @@ public class ConnectorsTests extends CloudbreakTest {
     @BeforeTest
     @Parameters({ "provider" })
     public void beforeTest(@Optional(OpenstackCloudProvider.OPENSTACK) String provider) {
-        LOGGER.info("before cluster test set provider: " + provider);
+        LOGGER.info("Provider: {} Region test setup has been started.", provider);
         if (this.cloudProvider != null) {
-            LOGGER.info("cloud provider already set - running from factory test");
+            LOGGER.info("{} provider already set - running from factory test", this.cloudProvider);
             return;
         }
         this.cloudProvider = CloudProviderHelper.providerFactory(provider, getTestParameter())[0];
@@ -49,7 +49,7 @@ public class ConnectorsTests extends CloudbreakTest {
 
     @AfterTest
     public void cleanUp() throws Exception {
-        LOGGER.info("Delete credential: \'{}\'", cloudProvider.getCredentialName());
+        LOGGER.info("Delete credential with name: {}", cloudProvider.getCredentialName());
 
         try {
             given(CloudbreakClient.isCreated());
@@ -59,21 +59,25 @@ public class ConnectorsTests extends CloudbreakTest {
         } catch (ForbiddenException e) {
             String exceptionMessage = e.getResponse().readEntity(String.class);
             this.errorMessage = exceptionMessage.substring(exceptionMessage.lastIndexOf(":") + 1);
-            LOGGER.info("ForbiddenException message ::: " + this.errorMessage);
+            LOGGER.info("ForbiddenException message ::: {}", this.errorMessage);
         }
     }
 
     @Test(priority = 0, groups = { "regions" })
     public void testDefaultRegionForCredential() throws Exception {
         given(CloudbreakClient.isCreated());
-        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform() + " credential is created");
-        given(Region.request(), cloudProvider.getPlatform() + " region request");
+        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform()
+                .concat(" credential is created"));
+        given(Region.request(), cloudProvider.getPlatform()
+                .concat(" region request"));
         when(Region.getPlatformRegions(), "Default region is requested to "
-                + cloudProvider.getPlatform() + " credential");
+                .concat(cloudProvider.getPlatform())
+                .concat(" credential"));
         then(Region.assertThis(
                 (region, t) -> {
-                    LOGGER.info(cloudProvider.getPlatform() + " Default Region is ::: "
-                            + region.getRegionResponse().getDefaultRegion());
+                    LOGGER.info("{} Default Region is ::: {}",
+                            cloudProvider.getPlatform(),
+                            region.getRegionResponse().getDefaultRegion());
                     Assert.assertTrue(region.getRegionResponse().getDefaultRegion().contains(cloudProvider.region()),
                             "[" + cloudProvider.region() + "] region is not the default one!");
                 }), "[" + cloudProvider.region() + "] region should be the default one."
@@ -83,14 +87,18 @@ public class ConnectorsTests extends CloudbreakTest {
     @Test(priority = 1, groups = { "regions" })
     public void testListRegionsForCredential() throws Exception {
         given(CloudbreakClient.isCreated());
-        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform() + " credential is created");
-        given(Region.request(), cloudProvider.getPlatform() + " region request");
+        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform()
+                .concat(" credential is created"));
+        given(Region.request(), cloudProvider.getPlatform()
+                .concat(" region request"));
         when(Region.getPlatformRegions(), "Regions are requested to "
-                + cloudProvider.getPlatform() + " credential");
+                .concat(cloudProvider.getPlatform())
+                .concat(" credential"));
         then(Region.assertThis(
                 (region, t) -> {
-                    LOGGER.info(cloudProvider.getPlatform() + " number of Regions ::: "
-                            + Integer.toString(region.getRegionResponse().getRegions().size()));
+                    LOGGER.info("{} number of Regions ::: {}",
+                            cloudProvider.getPlatform(),
+                            Integer.toString(region.getRegionResponse().getRegions().size()));
                     Assert.assertNotNull(region.getRegionResponse().getRegions().size());
                 }), "Regions should be part of the response."
         );
@@ -99,14 +107,18 @@ public class ConnectorsTests extends CloudbreakTest {
     @Test(priority = 2, groups = { "regions" })
     public void testListDisplayNamesForCredential() throws Exception {
         given(CloudbreakClient.isCreated());
-        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform() + " credential is created");
-        given(Region.request(), cloudProvider.getPlatform() + " region request");
+        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform()
+                .concat(" credential is created"));
+        given(Region.request(), cloudProvider.getPlatform()
+                .concat(" region request"));
         when(Region.getPlatformRegions(), "Regions are requested to "
-                + cloudProvider.getPlatform() + " credential");
+                .concat(cloudProvider.getPlatform())
+                .concat(" credential"));
         then(Region.assertThis(
                 (region, t) -> {
-                    LOGGER.info(cloudProvider.getPlatform() + " number of Display Names ::: "
-                            + Integer.toString(region.getRegionResponse().getDisplayNames().size()));
+                    LOGGER.info("{} number of Display Names ::: {}",
+                            cloudProvider.getPlatform(),
+                            Integer.toString(region.getRegionResponse().getDisplayNames().size()));
                     Assert.assertNotNull(region.getRegionResponse().getDisplayNames().size());
                 }), "Display Names should be part of the response."
         );
@@ -115,14 +127,18 @@ public class ConnectorsTests extends CloudbreakTest {
     @Test(priority = 3, groups = { "regions" })
     public void testListAvailabilityZonesForCredential() throws Exception {
         given(CloudbreakClient.isCreated());
-        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform() + " credential is created");
-        given(Region.request(), cloudProvider.getPlatform() + " region request");
+        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform()
+                .concat(" credential is created"));
+        given(Region.request(), cloudProvider.getPlatform()
+                .concat(" region request"));
         when(Region.getPlatformRegions(), "Regions are requested to "
-                + cloudProvider.getPlatform() + " credential");
+                .concat(cloudProvider.getPlatform())
+                .concat(" credential"));
         then(Region.assertThis(
                 (region, t) -> {
-                    LOGGER.info(cloudProvider.getPlatform() + " number of Availibility Zones ::: "
-                            + Integer.toString(region.getRegionResponse().getAvailabilityZones().size()));
+                    LOGGER.info("{} number of Availibility Zones ::: {}",
+                            cloudProvider.getPlatform(),
+                            Integer.toString(region.getRegionResponse().getAvailabilityZones().size()));
                     Assert.assertNotNull(region.getRegionResponse().getAvailabilityZones().size());
                 }), "Availibility Zones should be part of the response."
         );
@@ -131,23 +147,26 @@ public class ConnectorsTests extends CloudbreakTest {
     @Test(priority = 4, groups = { "regions" })
     public void testListAvailabilityZonesForDefaultRegion() throws Exception {
         given(CloudbreakClient.isCreated());
-        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform() + " credential is created");
-        given(Region.request(), cloudProvider.getPlatform() + " region request");
+        given(cloudProvider.aValidCredential(), cloudProvider.getPlatform()
+                .concat(" credential is created"));
+        given(Region.request(), cloudProvider.getPlatform()
+                .concat(" region request"));
         when(Region.getPlatformRegions(), "Regions are requested to "
-                + cloudProvider.getPlatform() + " credential");
+                .concat(cloudProvider.getPlatform())
+                .concat(" credential"));
         then(Region.assertThis(
                 (region, t) -> {
                     Collection<String> availibilityZones = region.getRegionResponse().getAvailabilityZones()
                             .get(cloudProvider.region());
 
                     if (cloudProvider.getPlatform().equalsIgnoreCase("AZURE")) {
-                        LOGGER.info(cloudProvider.getPlatform()
-                                + " Default Region's Availibility Zone is not available.");
+                        LOGGER.info("{} Default Region's Availibility Zone is not available.",
+                                cloudProvider.getPlatform());
                         Assert.assertTrue(availibilityZones.isEmpty());
                     } else {
                         availibilityZones.forEach(zone ->
-                            LOGGER.info(cloudProvider.getPlatform()
-                                    + " Default Region's Availibility Zone is ::: " + zone));
+                            LOGGER.info("{} Default Region's Availibility Zone is ::: {}",
+                                    cloudProvider.getPlatform(), zone));
                         Assert.assertFalse(availibilityZones.isEmpty());
                     }
                 }), "Default Region Availibility Zones should be part of the response."
