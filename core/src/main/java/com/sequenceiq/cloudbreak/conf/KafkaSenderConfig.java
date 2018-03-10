52d61e72f043e7c50b064f14584fa932716d3f3d
sodre90 <perdos@hortonworks.com>
2018-03-10 17:30:04 +0100

    DPAAS-301 send structured events to kafka

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/conf/KafkaSenderConfig.java
@@ -0,0 +1,49 @@
+package com.sequenceiq.cloudbreak.conf;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaSenderConfig {
+
+    @Value("${cb.kafka.bootstrap.servers:}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        // list of host:port pairs used for establishing the initial connections to the Kakfa cluster
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+
+        return props;
+    }
+
+    public boolean isKafkaConfigured() {
+        return StringUtils.isNotEmpty(bootstrapServers);
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- core/src/main/java/com/sequenceiq/cloudbreak/structuredevent/DefaultStructuredEventClient.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/structuredevent/DefaultStructuredEventClient.java
@@ -1,18 +1,33 @@
 package com.sequenceiq.cloudbreak.structuredevent;
 
-import javax.inject.Inject;
-
+import com.sequenceiq.cloudbreak.conf.KafkaSenderConfig;
+import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEvent;
+import com.sequenceiq.cloudbreak.structuredevent.reactor.KafkaStructuredEventAsyncNotifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEvent;
+import javax.inject.Inject;
 
 @Service
 public class DefaultStructuredEventClient implements StructuredEventClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultStructuredEventClient.class);
+
+    @Inject
+    private KafkaSenderConfig kafkaSenderConfig;
+
     @Inject
     private StructuredEventService structuredEventService;
+
+    @Inject
+    private KafkaStructuredEventAsyncNotifier kafkaAsyncSender;
 
     @Override
     public void sendStructuredEvent(StructuredEvent structuredEvent) {
         structuredEventService.storeStructuredEvent(structuredEvent);
+        if (kafkaSenderConfig.isKafkaConfigured()) {
+            kafkaAsyncSender.sendEventLogMessage(structuredEvent);
+        }
     }
 }
