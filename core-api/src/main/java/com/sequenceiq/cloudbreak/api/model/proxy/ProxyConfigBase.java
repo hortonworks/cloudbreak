54207643299f6f0d7b8e229267668aab4cd79149
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2018-03-14 11:43:36 +0100

    BUG-98434 add more validation to proxy request

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/proxy/ProxyConfigBase.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/proxy/ProxyConfigBase.java
@@ -1,7 +1,10 @@
 package com.sequenceiq.cloudbreak.api.model.proxy;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sequenceiq.cloudbreak.api.model.JsonEntity;
@@ -15,6 +18,9 @@ public abstract class ProxyConfigBase implements JsonEntity {
 
     @NotNull
     @ApiModelProperty(value = ProxyConfigModelDescription.NAME, required = true)
+    @Size(max = 100, min = 4, message = "The length of the name has to be in range of 4 to 100")
+    @Pattern(regexp = "(^[a-z][-a-z0-9]*[a-z0-9]$)",
+            message = "The name can only contain lowercase alphanumeric characters and hyphens and has start with an alphanumeric character")
     private String name;
 
     @ApiModelProperty(ModelDescriptions.DESCRIPTION)
@@ -22,10 +28,15 @@ public abstract class ProxyConfigBase implements JsonEntity {
 
     @NotNull
     @ApiModelProperty(value = ProxyConfigModelDescription.SERVER_HOST, required = true)
+    @Size(max = 255, min = 1, message = "The length of the server host has to be in range of 1 to 255")
+    @Pattern(regexp = "(^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$)",
+            message = "The server host can only contain lowercase alphanumeric characters and hyphens and dots")
     private String serverHost;
 
-    @NotNull
+    @NotNull(message = "Server port is required")
     @ApiModelProperty(value = ProxyConfigModelDescription.SERVER_PORT, required = true)
+    @Min(value = 1, message = "Port value must be greater than 0")
+    @Max(value = 65535, message = "Port value cannot be greater than 65535")
     private Integer serverPort;
 
     @NotNull
