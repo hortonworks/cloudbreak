fbfcfd39c5511bb3fab683b6c17cc5a8cf32146a
sodre90 <perdos@hortonworks.com>
2018-03-22 16:17:02 +0100

    BUG-97298 fix reckon #3 (dev build)
    (cherry picked from commit caa1fe4)

--- scripts/build-rc-patch.sh
+++ scripts/build-rc-patch.sh
@@ -1,24 +1,20 @@
 #!/bin/bash -e
-set -x
 : ${WORKSPACE=.}
 : ${BRANCH=rc-$(git describe --abbrev=0 --tags | cut -d '-' -f 1 | cut -d '.' -f 1,2)}
 
 MINOR_VERSION=$(echo "$BRANCH" | cut -d'-' -f 2)
-LATEST_TAG_ON_BRANCH=$(git tag -l --sort=-v:refname | grep $MINOR_VERSION | head -n 1)
 
-if [[ $LATEST_TAG_ON_BRANCH != *"-rc"* ]]; then
-    echo "released tag found: $LATEST_TAG_ON_BRANCH"
-    echo "need to add '-rc.1'"
-    VERSION=$LATEST_TAG_ON_BRANCH-rc.1
+if [ "$(git tag -l "$MINOR_VERSION.0")" == '' ]; then
+    ./gradlew -Penv=jenkins -b build.gradle clean build uploadArchives -Preckon -Preckon.scope=minor -Preckon.stage=rc --info --stacktrace --parallel
+    RECKONED_VERSION=$(./gradlew -Penv=jenkins -b build.gradle buildInfo -Preckon -Preckon.scope=minor -Preckon.stage=rc | grep Reckoned)
 else
-    LATEST_RC_NUMBER=$(echo $LATEST_TAG_ON_BRANCH | cut -d'.' -f 4)
-    VERSION=$(echo $LATEST_TAG_ON_BRANCH | cut -d'-' -f 1)-rc.$((LATEST_RC_NUMBER+1))
-fi;
+    ./gradlew -Penv=jenkins -b build.gradle clean build uploadArchives -Preckon -Preckon.scope=patch -Preckon.stage=rc --info --stacktrace --parallel
+    RECKONED_VERSION=$(./gradlew -Penv=jenkins -b build.gradle buildInfo -Preckon -Preckon.scope=patch -Preckon.stage=rc | grep Reckoned)
+fi
+
+VERSION=${RECKONED_VERSION#Reckoned version: }
 
 git tag -a $VERSION -m "$VERSION"
 git push origin $VERSION
 
-./gradlew -Penv=jenkins -b build.gradle clean build uploadArchives -Preckon.scope=patch -Preckon.stage=rc --info --stacktrace --parallel
-
-echo "Computed next rc version: $VERSION"
 echo VERSION=$VERSION > $WORKSPACE/version
