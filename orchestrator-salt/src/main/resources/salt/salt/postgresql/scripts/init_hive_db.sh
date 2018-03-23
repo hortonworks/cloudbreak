2f40c998d230223f41962af6fc0aa33a12648a6d
Attila Kanto <attila.kanto@gmail.com>
2018-03-23 12:08:08 +0100

    BUG-99162 cannot install cluster on non prewarmed image

--- orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/init_hive_db.sh
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/init_hive_db.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -f "/var/init_hive_db_executed" ]; then
+DATADIR=$(psql -c "show data_directory;" | grep "/data")
+echo "Datadir: $DATADIR"
+
+if [ ! -f "$DATADIR/init_hive_db_executed" ]; then
     set -e
     echo "Create Hive database"
     echo "CREATE DATABASE $DATABASE;" | psql -U postgres
@@ -8,10 +11,8 @@ if [ ! -f "/var/init_hive_db_executed" ]; then
     echo "GRANT ALL PRIVILEGES ON DATABASE $USER TO $DATABASE;" | psql -U postgres
 
     echo "Add access to pg_hba.conf"
-    DATADIR=$(psql -c "show data_directory;" | grep "/data")
-    echo "Datadir: $DATADIR"
     echo "host $DATABASE $USER 0.0.0.0/0 md5" >> $DATADIR/pg_hba.conf
     echo "local $DATABASE $USER md5" >> $DATADIR/pg_hba.conf
-    echo $(date +%Y-%m-%d:%H:%M:%S) >> /var/init_hive_db_executed
+    echo $(date +%Y-%m-%d:%H:%M:%S) >> $DATADIR/init_hive_db_executed
     set +e
 fi
