1215393e4334a877cb2de56f58928558cd5b5185
Gergely Topolyai <gtopolyai@hortonworks.com>
2018-03-26 15:20:54 +0200

    BUG-97206 in cluster create and credentials shows enabled platforms only

--- core/src/test/java/com/sequenceiq/cloudbreak/service/credential/CredentialServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/credential/CredentialServiceTest.java
@@ -5,24 +5,34 @@ import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.google.common.collect.Sets;
 import com.sequenceiq.cloudbreak.common.model.user.IdentityUser;
+import com.sequenceiq.cloudbreak.common.model.user.IdentityUserRole;
 import com.sequenceiq.cloudbreak.controller.BadRequestException;
 import com.sequenceiq.cloudbreak.domain.Credential;
 import com.sequenceiq.cloudbreak.domain.Topology;
 import com.sequenceiq.cloudbreak.domain.json.Json;
 import com.sequenceiq.cloudbreak.repository.CredentialRepository;
 import com.sequenceiq.cloudbreak.service.AuthorizationService;
+import com.sequenceiq.cloudbreak.service.account.AccountPreferencesService;
 import com.sequenceiq.cloudbreak.service.notification.NotificationSender;
 import com.sequenceiq.cloudbreak.service.stack.connector.adapter.ServiceProviderCredentialAdapter;
 import com.sequenceiq.cloudbreak.service.user.UserProfileCredentialHandler;
@@ -31,6 +41,9 @@ import com.sequenceiq.cloudbreak.service.user.UserProfileCredentialHandler;
 public class CredentialServiceTest {
 
     private static final String PLATFORM = "OPENSTACK";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Mock
     private CredentialRepository credentialRepository;
@@ -46,6 +59,9 @@ public class CredentialServiceTest {
 
     @Mock
     private NotificationSender notificationSender;
+
+    @Mock
+    private AccountPreferencesService accountPreferencesService;
 
     @InjectMocks
     private CredentialService credentialService = new CredentialService();
@@ -112,11 +128,56 @@ public class CredentialServiceTest {
         assertEquals(credential.cloudPlatform(), modify.cloudPlatform());
     }
 
-    @Test(expected = BadRequestException.class)
+    @Test
     public void testModifyDifferentPlatform() {
         Credential credential = new Credential();
         credential.setCloudPlatform("BAD");
+
+        thrown.expect(BadRequestException.class);
+        thrown.expectMessage("Modifying credential platform is forbidden");
+
         credentialService.modify(user, credential);
+    }
+
+    @Test
+    public void testRetrieveAccountCredentialsWhenUserIsAdmin() {
+
+        IdentityUser user = mock(IdentityUser.class);
+        Set<String> platforms = Sets.newHashSet("AWS");
+        Credential credential = new Credential();
+        credential.setCloudPlatform("AWS");
+
+        when(user.getRoles()).thenReturn(Collections.singleton(IdentityUserRole.fromString("ADMIN")));
+        when(user.getAccount()).thenReturn("account");
+        when(accountPreferencesService.enabledPlatforms()).thenReturn(platforms);
+        when(credentialRepository.findAllInAccountAndFilterByPlatforms(user.getAccount(), platforms)).thenReturn(Sets.newHashSet(credential));
+
+        Set<Credential> actual = credentialService.retrieveAccountCredentials(user);
+
+        assertEquals("AWS", actual.stream().findFirst().get().cloudPlatform());
+
+        verify(credentialRepository, times(1)).findAllInAccountAndFilterByPlatforms("account", platforms);
+    }
+
+    @Test
+    public void testRetrieveAccountCredentialsWhenUserIsNotAdmin() {
+
+        IdentityUser user = mock(IdentityUser.class);
+        Set<String> platforms = Sets.newHashSet("AWS");
+        Credential credential = new Credential();
+        credential.setCloudPlatform("AWS");
+
+        when(user.getRoles()).thenReturn(Collections.singleton(IdentityUserRole.fromString("USER")));
+        when(user.getAccount()).thenReturn("account");
+        when(user.getUserId()).thenReturn("userId");
+        when(accountPreferencesService.enabledPlatforms()).thenReturn(platforms);
+        when(credentialRepository.findPublicInAccountForUserFilterByPlatforms("userId", "account", platforms)).thenReturn(Sets.newHashSet(credential));
+
+        Set<Credential> actual = credentialService.retrieveAccountCredentials(user);
+
+        assertEquals("AWS", actual.stream().findFirst().get().cloudPlatform());
+
+        verify(credentialRepository, times(1)).findPublicInAccountForUserFilterByPlatforms("userId", "account", platforms);
     }
 
 }
