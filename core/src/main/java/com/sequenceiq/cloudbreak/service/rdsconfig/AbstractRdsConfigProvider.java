e450097a4693beac7af09fd59ea68d03871ebbd4
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2018-04-03 17:36:28 +0200

    BUG-99602 Implement postgres db creation for ranger in case of mysql_server not represented in the blueprint

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/rdsconfig/AbstractRdsConfigProvider.java
@@ -0,0 +1,114 @@
+package com.sequenceiq.cloudbreak.service.rdsconfig;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.sequenceiq.cloudbreak.api.model.ResourceStatus;
+import com.sequenceiq.cloudbreak.api.model.rds.RDSDatabase;
+import com.sequenceiq.cloudbreak.api.model.rds.RdsType;
+import com.sequenceiq.cloudbreak.domain.Blueprint;
+import com.sequenceiq.cloudbreak.domain.Cluster;
+import com.sequenceiq.cloudbreak.domain.RDSConfig;
+import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.repository.ClusterRepository;
+import com.sequenceiq.cloudbreak.repository.RdsConfigRepository;
+import com.sequenceiq.cloudbreak.util.PasswordUtil;
+
+public abstract class AbstractRdsConfigProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractRdsConfigProvider.class);
+
+    @Inject
+    private RdsConfigRepository rdsConfigRepository;
+
+    @Inject
+    private RdsConfigService rdsConfigService;
+
+    @Inject
+    private ClusterRepository clusterRepository;
+
+    public Map<String, Object> createServicePillarConfigMapIfNeeded(Stack stack, Cluster cluster) {
+        if (isRdsConfigNeeded(cluster.getBlueprint())) {
+            Set<RDSConfig> rdsConfigs = createPostgresRdsConfigIfNeeded(stack, cluster);
+            RDSConfig rdsConfig = rdsConfigs.stream().filter(rdsConfig1 -> rdsConfig1.getType().equalsIgnoreCase(getRdsType().name())).findFirst().get();
+            Map<String, Object> postgres = new HashMap<>();
+            postgres.put("database", getDb());
+            postgres.put("user", getDbUser());
+            postgres.put("password", rdsConfig.getConnectionPassword());
+            return Collections.singletonMap(getPillarKey(), postgres);
+        }
+        return Collections.emptyMap();
+    }
+
+    public Set<RDSConfig> createPostgresRdsConfigIfNeeded(Stack stack, Cluster cluster) {
+        Set<RDSConfig> rdsConfigs = rdsConfigRepository.findByClusterId(stack.getOwner(), stack.getAccount(), cluster.getId());
+        if (isRdsConfigNeeded(cluster.getBlueprint())
+                && rdsConfigs.stream().noneMatch(rdsConfig -> rdsConfig.getType().equalsIgnoreCase(getRdsType().name()))) {
+            LOGGER.info("Creating postgres RDSConfig for {}", getRdsType().name());
+            rdsConfigs = createPostgresRdsConf(stack, cluster, rdsConfigs, getDbUser(), getDbPort(), getDb());
+        }
+        return rdsConfigs;
+    }
+
+    private Set<RDSConfig> createPostgresRdsConf(Stack stack, Cluster cluster, Set<RDSConfig> rdsConfigs, String dbUserName, String dbPort, String dbName) {
+        RDSConfig rdsConfig = new RDSConfig();
+        rdsConfig.setName(getRdsType().name() + '_' + stack.getName() + stack.getId());
+        rdsConfig.setConnectionUserName(dbUserName);
+        rdsConfig.setConnectionPassword(PasswordUtil.generatePassword());
+        String primaryGatewayIp = stack.getPrimaryGatewayInstance().getPrivateIp();
+        rdsConfig.setConnectionURL("jdbc:postgresql://" + primaryGatewayIp + ":" + dbPort + "/" + dbName);
+        rdsConfig.setDatabaseEngine(RDSDatabase.POSTGRES.name());
+        rdsConfig.setType(getRdsType().name());
+        rdsConfig.setConnectionDriver(RDSDatabase.POSTGRES.getDbDriver());
+        rdsConfig.setStatus(ResourceStatus.DEFAULT);
+        rdsConfig.setCreationDate(new Date().getTime());
+        rdsConfig.setOwner(stack.getOwner());
+        rdsConfig.setAccount(stack.getAccount());
+        rdsConfig.setClusters(Collections.singleton(cluster));
+        rdsConfig = rdsConfigService.create(rdsConfig);
+
+        if (rdsConfigs == null) {
+            rdsConfigs = new HashSet<>();
+        }
+        rdsConfigs.add(rdsConfig);
+        cluster.setRdsConfigs(rdsConfigs);
+        clusterRepository.save(cluster);
+        return rdsConfigs;
+    }
+
+    protected List<String[]> createPathListFromConfingurations(String[] path, String[] configurations) {
+        List<String[]> pathList = new ArrayList<>();
+        Arrays.stream(configurations).forEach(configuration -> {
+            List<String> pathWithConfig = Lists.newArrayList(path);
+            pathWithConfig.add(configuration);
+            pathList.add(pathWithConfig.toArray(new String [pathWithConfig.size()]));
+        });
+        return pathList;
+    }
+
+    protected abstract String getDbUser();
+
+    protected abstract String getDb();
+
+    protected abstract String getDbPort();
+
+    protected abstract String getPillarKey();
+
+    protected abstract RdsType getRdsType();
+
+    protected abstract boolean isRdsConfigNeeded(Blueprint blueprint);
+
+}
