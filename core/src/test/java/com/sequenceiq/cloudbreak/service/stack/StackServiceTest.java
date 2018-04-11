0f705802748d0502ffdc01908622e2c805d62a18
David Szabo <dszabo@hortonworks.com>
2018-04-11 08:09:19 +0200

    BUG-100176 Update stack state if creation failed with image not found

--- core/src/test/java/com/sequenceiq/cloudbreak/service/stack/StackServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stack/StackServiceTest.java
@@ -4,12 +4,16 @@ import static com.sequenceiq.cloudbreak.api.model.InstanceMetadataType.CORE;
 import static com.sequenceiq.cloudbreak.api.model.InstanceMetadataType.GATEWAY;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,15 +24,30 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.access.AccessDeniedException;
 
+import com.sequenceiq.cloudbreak.api.model.DetailedStackStatus;
+import com.sequenceiq.cloudbreak.cloud.PlatformParameters;
+import com.sequenceiq.cloudbreak.cloud.model.Variant;
 import com.sequenceiq.cloudbreak.common.model.user.IdentityUser;
 import com.sequenceiq.cloudbreak.controller.BadRequestException;
+import com.sequenceiq.cloudbreak.controller.CloudbreakApiException;
 import com.sequenceiq.cloudbreak.controller.NotFoundException;
+import com.sequenceiq.cloudbreak.core.CloudbreakImageCatalogException;
+import com.sequenceiq.cloudbreak.core.CloudbreakImageNotFoundException;
 import com.sequenceiq.cloudbreak.core.flow2.service.ReactorFlowManager;
 import com.sequenceiq.cloudbreak.domain.InstanceMetaData;
+import com.sequenceiq.cloudbreak.domain.SecurityConfig;
 import com.sequenceiq.cloudbreak.domain.Stack;
+import com.sequenceiq.cloudbreak.domain.StackAuthentication;
+import com.sequenceiq.cloudbreak.repository.InstanceGroupRepository;
 import com.sequenceiq.cloudbreak.repository.InstanceMetaDataRepository;
+import com.sequenceiq.cloudbreak.repository.SecurityConfigRepository;
 import com.sequenceiq.cloudbreak.repository.StackRepository;
+import com.sequenceiq.cloudbreak.repository.StackUpdater;
 import com.sequenceiq.cloudbreak.service.AuthorizationService;
+import com.sequenceiq.cloudbreak.service.ComponentConfigProvider;
+import com.sequenceiq.cloudbreak.service.TlsSecurityService;
+import com.sequenceiq.cloudbreak.service.image.ImageService;
+import com.sequenceiq.cloudbreak.service.stack.connector.adapter.ServiceProviderConnectorAdapter;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StackServiceTest {
@@ -42,6 +61,10 @@ public class StackServiceTest {
     private static final String OWNER = "1234567";
 
     private static final String USER_ID = OWNER;
+
+    private static final String VARIANT_VALUE = "VARIANT_VALUE";
+
+    private static final String IMAGE_CATALOG = "IMAGE_CATALOG";
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -72,6 +95,39 @@ public class StackServiceTest {
 
     @Mock
     private IdentityUser user;
+
+    @Mock
+    private ServiceProviderConnectorAdapter connector;
+
+    @Mock
+    private Variant variant;
+
+    @Mock
+    private StackAuthentication stackAuthentication;
+
+    @Mock
+    private ComponentConfigProvider componentConfigProvider;
+
+    @Mock
+    private InstanceGroupRepository instanceGroupRepository;
+
+    @Mock
+    private TlsSecurityService tlsSecurityService;
+
+    @Mock
+    private SecurityConfig securityConfig;
+
+    @Mock
+    private SecurityConfigRepository securityConfigRepository;
+
+    @Mock
+    private ImageService imageService;
+
+    @Mock
+    private PlatformParameters parameters;
+
+    @Mock
+    private StackUpdater stackUpdater;
 
     @Test
     public void testRemoveInstanceWhenTheInstanceIsCoreTypeAndUserHasRightToTerminateThenThenProcessWouldBeSuccessful() {
@@ -161,4 +217,62 @@ public class StackServiceTest {
         underTest.get(STACK_ID);
     }
 
+    @Test(expected = CloudbreakApiException.class)
+    public void testCreateFailsWithInvalidImageId() throws CloudbreakImageNotFoundException, CloudbreakImageCatalogException {
+        when(connector.checkAndGetPlatformVariant(stack)).thenReturn(variant);
+        when(variant.value()).thenReturn(VARIANT_VALUE);
+
+        when(stack.getStackAuthentication()).thenReturn(stackAuthentication);
+        when(stackAuthentication.passwordAuthenticationRequired()).thenReturn(false);
+
+        when(stackRepository.save(stack)).thenReturn(stack);
+
+        when(tlsSecurityService.storeSSHKeys()).thenReturn(securityConfig);
+        when(connector.getPlatformParameters(stack)).thenReturn(parameters);
+
+        Optional<String> imageId = Optional.of("IMAGE_ID");
+        doThrow(new CloudbreakImageNotFoundException("Image not found")).when(imageService).create(stack, parameters, IMAGE_CATALOG, imageId, Optional.empty());
+
+        when(stack.getId()).thenReturn(Long.MAX_VALUE);
+        try {
+            stack = underTest.create(user, stack, IMAGE_CATALOG, imageId, Optional.empty());
+        } finally {
+            verify(stack, times(1)).setPlatformVariant(eq(VARIANT_VALUE));
+            verify(securityConfig, times(1)).setSaltPassword(anyObject());
+            verify(securityConfig, times(1)).setSaltBootPassword(anyObject());
+            verify(securityConfig, times(1)).setKnoxMasterSecret(anyObject());
+            verify(securityConfig, times(1)).setStack(stack);
+            verify(securityConfigRepository, times(1)).save(securityConfig);
+
+            verify(stackUpdater, times(1)).updateStackStatus(eq(Long.MAX_VALUE), eq(DetailedStackStatus.PROVISION_FAILED), anyString());
+        }
+    }
+
+    @Test
+    public void testCreateImageFoundNoStackStatusUpdate() {
+        when(connector.checkAndGetPlatformVariant(stack)).thenReturn(variant);
+        when(variant.value()).thenReturn(VARIANT_VALUE);
+
+        when(stack.getStackAuthentication()).thenReturn(stackAuthentication);
+        when(stackAuthentication.passwordAuthenticationRequired()).thenReturn(false);
+
+        when(stackRepository.save(stack)).thenReturn(stack);
+
+        when(tlsSecurityService.storeSSHKeys()).thenReturn(securityConfig);
+        when(connector.getPlatformParameters(stack)).thenReturn(parameters);
+
+        try {
+            Optional<String> imageId = Optional.of("IMAGE_ID");
+            stack = underTest.create(user, stack, IMAGE_CATALOG, imageId, Optional.empty());
+        } finally {
+            verify(stack, times(1)).setPlatformVariant(eq(VARIANT_VALUE));
+            verify(securityConfig, times(1)).setSaltPassword(anyObject());
+            verify(securityConfig, times(1)).setSaltBootPassword(anyObject());
+            verify(securityConfig, times(1)).setKnoxMasterSecret(anyObject());
+            verify(securityConfig, times(1)).setStack(stack);
+            verify(securityConfigRepository, times(1)).save(securityConfig);
+
+            verify(stackUpdater, times(0)).updateStackStatus(eq(Long.MAX_VALUE), eq(DetailedStackStatus.PROVISION_FAILED), anyString());
+        }
+    }
 }
