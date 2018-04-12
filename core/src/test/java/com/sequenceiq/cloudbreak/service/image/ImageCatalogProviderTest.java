46b1f8d08f0004f62f0cebe5f3959f04ab6e5c3d
David Szabo <dszabo@hortonworks.com>
2018-04-12 09:22:11 +0200

    Imports organized, removed duplication from core gradle

--- core/src/test/java/com/sequenceiq/cloudbreak/service/image/ImageCatalogProviderTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/image/ImageCatalogProviderTest.java
@@ -23,13 +23,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sequenceiq.cloudbreak.TestUtil;
