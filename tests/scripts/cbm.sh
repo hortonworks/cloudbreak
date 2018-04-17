c733b1531764bc29004122d556e87e04ee17285d
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-04-17 13:24:31 +0200

    Add versioning for Integration test Mock

--- tests/scripts/cbm.sh
+++ tests/scripts/cbm.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+: ${GIT_VERSION:=latest}
+: ${GIT_TAG:=latest}
+
+set-mock-image-tag() {
+    echo "GitHub First Parent Tag is: ${GIT_VERSION}"
+    echo "GitHub Tag is: ${GIT_TAG}"
+    echo "CircleCI Branch is: ${CIRCLE_BRANCH}"
+    echo "CircleCI Tag is: ${CIRCLE_TAG}"
+
+    if [[ "${GIT_VERSION}" != *"-rc."* ]]; then
+        export GIT_VERSION=latest
+    fi
+}
+
+set-mock-image-tag
 docker-compose up -d
+
 sleep 30s
