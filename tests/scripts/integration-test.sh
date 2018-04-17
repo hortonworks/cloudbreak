81b6a777d0fea28572e5e9b0371f18f7bee05047
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-04-17 20:46:23 +0200

    Fixing Mock Integration test environment issues

--- tests/scripts/integration-test.sh
+++ tests/scripts/integration-test.sh
@@ -3,7 +3,13 @@
 export BASE_URL=https://127.0.0.1
 export USERNAME_CLI=admin@example.com
 export PASSWORD_CLI=cloudbreak
-#could be a bug:
+
+echo "Configure CB CLI to " $BASE_URL $USERNAME_CLI $PASSWORD_CLI
 cb configure --server $BASE_URL --username $USERNAME_CLI --password $PASSWORD_CLI
 
+echo "Path is inside Docker container: " $(pwd)
+echo "Docker container Home is: " "${HOME}"
+
+echo "Launch Bats tests from " /work/integration/ " container folder"
+cd work
 bats --tap integration/*.bats | tee test-result.tap
