651b93e2e7dd404db663d61a05895ed5488b1dc7
Ferenc Schneider <fschneider@hortonworks.com>
2018-04-20 13:46:21 +0200

    BUG-99294 parameter substitution in install mpack script

--- orchestrator-salt/src/main/resources/salt/salt/ambari/scripts/install-mpack.sh
+++ orchestrator-salt/src/main/resources/salt/salt/ambari/scripts/install-mpack.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -ex
 
-echo yes | ambari-server install-mpack --mpack={{ mpack.mpackUrl }} --verbose
+ARGS=""
+{% if mpack.purge %}
+ARGS+="--purge"
+{% endif %}
+{% if mpack.force %}
+ARGS+=" --force"
+{% endif %}
+{% if mpack.purgeList %}
+ARGS+=" --purge-list {{ mpack.purgeList|join(',') }}"
+{% endif %}
+echo yes | ambari-server install-mpack --mpack={{ mpack.mpackUrl }} ${ARGS} --verbose
 echo "$(date +%Y-%m-%d:%H:%M:%S) {{ mpack.mpackUrl }}" >> /var/mpack_installed
