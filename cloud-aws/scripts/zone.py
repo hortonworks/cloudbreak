3a7cd5f65b1e1d3d8cae59c7b239847798d771b2
Tamas Bihari <tbihari@hortonworks.com>
2018-04-25 15:01:56 +0200

    BUG-100027 fix AWS instance type updater to allow only defaultVM types that exists in the region

--- cloud-aws/scripts/zone.py
+++ cloud-aws/scripts/zone.py
@@ -7,10 +7,19 @@ with open('./src/main/resources/definitions/zones.json') as data_file:
 with open(sys.argv[1]) as data_file:
     zoneVmTypes = json.load(data_file)
 
+#Filter zones that are supported by Cloudbreak
+zoneVmTypes["items"] = [zone for zone in zoneVmTypes["items"] if zone["zone"] in zoneData]
+
 for zone in zoneVmTypes["items"]:
     zoneName = zone["zone"]
+    print zoneName
     zone["zone"] = zoneData[zoneName]["zoneid"]
-    zone["defaultVmType"] = zoneData[zoneName]["defaultvm"]
+    defaultVm = zoneData[zoneName]["defaultvm"]
+    if defaultVm in zone["vmTypes"]:
+        zone["defaultVmType"] = defaultVm
+    else:
+        print "Default VM type '%s' doesn't exist as available VM type in zone '%s'." % (defaultVm, zoneName)
+        sys.exit(1)
 
 with open(sys.argv[2], 'w') as outfile:
     json.dump(zoneVmTypes, outfile, indent=2)
