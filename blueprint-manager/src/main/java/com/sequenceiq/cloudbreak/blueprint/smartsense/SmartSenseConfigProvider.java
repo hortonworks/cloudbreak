6c8cd6190f432f3d41e5be4561816f610f34a934
Richard Doktorics <rdoktorics@hortonworks.com>
2018-05-03 14:57:50 +0200

    BUG-102110 When adding SmartSense server to blueprint, host group is selected randomly

--- blueprint-manager/src/main/java/com/sequenceiq/cloudbreak/blueprint/smartsense/SmartSenseConfigProvider.java
+++ blueprint-manager/src/main/java/com/sequenceiq/cloudbreak/blueprint/smartsense/SmartSenseConfigProvider.java
@@ -88,7 +88,7 @@ public class SmartSenseConfigProvider implements BlueprintComponentConfigProvide
     private String addSmartSenseServerToBp(BlueprintTextProcessor blueprintProcessor, Iterable<HostgroupView> hostgroupViews,
             Collection<String> hostGroupNames) {
         if (!blueprintProcessor.componentExistsInBlueprint(HST_SERVER_COMPONENT)) {
-            String aHostGroupName = hostGroupNames.stream().findFirst().get();
+            String aHostGroupName = hostGroupNames.stream().sorted(String::compareTo).findFirst().get();
             boolean singleNodeGatewayFound = false;
             for (HostgroupView hostGroup : hostgroupViews) {
                 if (hostGroup.isInstanceGroupConfigured() && GATEWAY.equals(hostGroup.getInstanceGroupType()) && hostGroup.getNodeCount().equals(1)) {
