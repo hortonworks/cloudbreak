058a94979697f22bd843bea16e0c7f5c6324873e
sodre90 <perdos@hortonworks.com>
2018-05-03 13:43:00 +0200

    BUG-99779 remove unused optional

--- core/src/test/java/com/sequenceiq/cloudbreak/service/stack/StackServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stack/StackServiceTest.java
@@ -15,8 +15,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
