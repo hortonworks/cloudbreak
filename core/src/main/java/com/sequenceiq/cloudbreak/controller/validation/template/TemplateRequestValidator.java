1c73e9c09ece211086d09fc90c5ec749828b3bd7
Attila Palfi <apalfi@hortonworks.com>
2018-05-10 13:26:23 +0200

    BUG-101026 InstaceGroup - HostGroup mapping changed in request time

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/template/TemplateRequestValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/template/TemplateRequestValidator.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.cloudbreak.controller.validation.template;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.model.TemplateRequest;
@@ -13,8 +15,12 @@ public class TemplateRequestValidator implements Validator<TemplateRequest> {
     @Override
     public ValidationResult validate(TemplateRequest request) {
         ValidationResultBuilder resultBuilder = ValidationResult.builder();
-        resultBuilder.ifError(() -> request.getRootVolumeSize() != null && request.getRootVolumeSize() < 1,
-                "Root volume size cannot be smaller than 1 gigabyte.");
+        if (Objects.isNull(request)) {
+            resultBuilder.error("Template request cannot be null in the instance group request.");
+        } else {
+            resultBuilder.ifError(() -> request.getRootVolumeSize() != null && request.getRootVolumeSize() < 1,
+                    "Root volume size cannot be smaller than 1 gigabyte.");
+        }
         return resultBuilder.build();
     }
 }
--- core/src/test/java/com/sequenceiq/cloudbreak/controller/validation/stack/StackRequestValidatorTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/controller/validation/stack/StackRequestValidatorTest.java
@@ -1,11 +1,18 @@
 package com.sequenceiq.cloudbreak.controller.validation.stack;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
+import com.google.common.collect.Sets;
+import com.sequenceiq.cloudbreak.api.model.ClusterRequest;
+import com.sequenceiq.cloudbreak.api.model.HostGroupRequest;
 import com.sequenceiq.cloudbreak.api.model.InstanceGroupRequest;
 import com.sequenceiq.cloudbreak.api.model.StackRequest;
 import com.sequenceiq.cloudbreak.api.model.TemplateRequest;
@@ -47,13 +54,90 @@ public class StackRequestValidatorTest {
         assertEquals(State.VALID, validationResult.getState());
     }
 
+    @Test
+    public void testWithLargerInstanceGroupSetThanHostGroups() {
+        String plusOne = "very master";
+        StackRequest stackRequest = stackRequestWithInstanceAndHostGroups(
+                Sets.newHashSet(plusOne, "master", "worker", "compute"),
+                Sets.newHashSet("master", "worker", "compute")
+        );
+
+        ValidationResult validationResult = underTest.validate(stackRequest);
+        assertEquals(State.ERROR, validationResult.getState());
+        assertTrue(validationResult.getErrors().get(0).startsWith("There are instance groups in the request that do not have a corresponding host group: "
+                + plusOne));
+    }
+
+    @Test
+    public void testWithLargerHostGroupSetThanInstanceGroups() {
+        StackRequest stackRequest = stackRequestWithInstanceAndHostGroups(
+                Sets.newHashSet("master", "worker", "compute"),
+                Sets.newHashSet("super master", "master", "worker", "compute")
+        );
+
+        ValidationResult validationResult = underTest.validate(stackRequest);
+        assertEquals(State.ERROR, validationResult.getState());
+        assertTrue(validationResult.getErrors().get(0).startsWith("There are host groups in the request that do not have a corresponding instance group"));
+    }
+
+    @Test
+    public void testWithBothGroupContainsDifferentValues() {
+        StackRequest stackRequest = stackRequestWithInstanceAndHostGroups(
+                Sets.newHashSet("worker", "compute"),
+                Sets.newHashSet("master", "worker")
+        );
+
+        ValidationResult validationResult = underTest.validate(stackRequest);
+        assertEquals(State.ERROR, validationResult.getState());
+        assertEquals(2, validationResult.getErrors().size());
+    }
+
+    private StackRequest stackRequestWithInstanceAndHostGroups(Set<String> instanceGroups, Set<String> hostGroups) {
+        List<InstanceGroupRequest> instanceGroupList = instanceGroups.stream()
+                .map(ig -> getInstanceGroupRequest(new TemplateRequest(), ig))
+                .collect(Collectors.toList());
+
+        Set<HostGroupRequest> hostGroupSet = hostGroups.stream()
+                .map(hg -> {
+                    HostGroupRequest hostGroupRequest = new HostGroupRequest();
+                    hostGroupRequest.setName(hg);
+                    return hostGroupRequest;
+                })
+                .collect(Collectors.toSet());
+
+        ClusterRequest clusterRequest = new ClusterRequest();
+        clusterRequest.setHostGroups(hostGroupSet);
+
+        return getStackRequest(instanceGroupList, clusterRequest);
+    }
+
     private StackRequest stackRequestWithRootVolumeSize(Integer rootVolumeSize) {
         TemplateRequest templateRequest = new TemplateRequest();
         templateRequest.setRootVolumeSize(rootVolumeSize);
+        InstanceGroupRequest instanceGroupRequest = getInstanceGroupRequest(templateRequest, "master");
+        ClusterRequest clusterRequest = getClusterRequest();
+        return getStackRequest(Collections.singletonList(instanceGroupRequest), clusterRequest);
+    }
+
+    private InstanceGroupRequest getInstanceGroupRequest(TemplateRequest templateRequest, String master) {
         InstanceGroupRequest instanceGroupRequest = new InstanceGroupRequest();
+        instanceGroupRequest.setGroup(master);
         instanceGroupRequest.setTemplate(templateRequest);
+        return instanceGroupRequest;
+    }
+
+    private ClusterRequest getClusterRequest() {
+        HostGroupRequest hostGroupRequest = new HostGroupRequest();
+        ClusterRequest clusterRequest = new ClusterRequest();
+        hostGroupRequest.setName("master");
+        clusterRequest.setHostGroups(Sets.newHashSet(hostGroupRequest));
+        return clusterRequest;
+    }
+
+    private StackRequest getStackRequest(List<InstanceGroupRequest> instanceGroupRequests, ClusterRequest clusterRequest) {
         StackRequest stackRequest = new StackRequest();
-        stackRequest.setInstanceGroups(Arrays.asList(instanceGroupRequest));
+        stackRequest.setClusterRequest(clusterRequest);
+        stackRequest.setInstanceGroups(instanceGroupRequests);
         return stackRequest;
     }
 
