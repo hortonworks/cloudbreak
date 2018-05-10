0ccc89139e33a123872fc81fcb509e0822a5b2ec
Attila Palfi <apalfi@hortonworks.com>
2018-05-10 13:26:23 +0200

    BUG-101026 InstaceGroup - HostGroup mapping changed in request time

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/stack/StackRequestValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/stack/StackRequestValidator.java
@@ -1,10 +1,16 @@
 package com.sequenceiq.cloudbreak.controller.validation.stack;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import com.google.common.collect.Sets;
+import com.sequenceiq.cloudbreak.api.model.HostGroupBase;
+import com.sequenceiq.cloudbreak.api.model.InstanceGroupBase;
 import com.sequenceiq.cloudbreak.api.model.StackRequest;
 import com.sequenceiq.cloudbreak.api.model.TemplateRequest;
 import com.sequenceiq.cloudbreak.controller.validation.ValidationResult;
@@ -24,12 +30,39 @@ public class StackRequestValidator implements Validator<StackRequest> {
 
     @Override
     public ValidationResult validate(StackRequest stackRequest) {
-        ValidationResultBuilder resultBuilder = ValidationResult.builder();
+        ValidationResultBuilder validationBuilder = ValidationResult.builder();
         if (CollectionUtils.isEmpty(stackRequest.getInstanceGroups())) {
-            resultBuilder.error("Stack request must contain instance groups.");
+            validationBuilder.error("Stack request must contain instance groups.");
         }
-        resultBuilder = validateTemplates(stackRequest, resultBuilder);
-        return resultBuilder.build();
+        validationBuilder = validateHostgroupInstanceGroupMapping(stackRequest, validationBuilder);
+        validationBuilder = validateTemplates(stackRequest, validationBuilder);
+        return validationBuilder.build();
+    }
+
+    private ValidationResultBuilder validateHostgroupInstanceGroupMapping(StackRequest stackRequest, ValidationResultBuilder validationBuilder) {
+        Set<String> instanceGroupSet = stackRequest.getInstanceGroups()
+                .stream()
+                .map(InstanceGroupBase::getGroup)
+                .collect(Collectors.toSet());
+        Set<String> hostGroupSet = stackRequest.getClusterRequest().getHostGroups()
+                .stream()
+                .map(HostGroupBase::getName)
+                .collect(Collectors.toSet());
+
+        if (!instanceGroupSet.containsAll(hostGroupSet)) {
+            Set<String> newHostGroupSet = Sets.newHashSet(hostGroupSet);
+            newHostGroupSet.removeAll(instanceGroupSet);
+            validationBuilder.error("There are host groups in the request that do not have a corresponding instance group: "
+                    + newHostGroupSet.stream().collect(Collectors.joining(", ")));
+        }
+
+        if (!hostGroupSet.containsAll(instanceGroupSet)) {
+            instanceGroupSet.removeAll(hostGroupSet);
+            validationBuilder.error("There are instance groups in the request that do not have a corresponding host group: "
+                    + instanceGroupSet.stream().collect(Collectors.joining(", ")));
+        }
+
+        return validationBuilder;
     }
 
     private ValidationResultBuilder validateTemplates(StackRequest stackRequest, ValidationResultBuilder resultBuilder) {
