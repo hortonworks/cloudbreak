a88112ff00940cda5d309ad145ee916e3a854da5
Arpad Farsang <afarsang@HW12482.local>
2018-05-30 13:47:46 +0200

    BUG-104104 Add stack response to the test report in failing case

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/StackPostStrategy.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/StackPostStrategy.java
@@ -1,11 +1,15 @@
 package com.sequenceiq.it.cloudbreak.newway;
 
 import static com.sequenceiq.it.cloudbreak.newway.CloudbreakClient.getTestContextCloudbreakClient;
+import static com.sequenceiq.it.cloudbreak.newway.log.Log.log;
 import static com.sequenceiq.it.cloudbreak.newway.log.Log.logJSON;
 
-import com.sequenceiq.it.IntegrationTestContext;
+import javax.ws.rs.WebApplicationException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.sequenceiq.it.IntegrationTestContext;
 
 public class StackPostStrategy implements Strategy {
     private static final Logger LOGGER = LoggerFactory.getLogger(StackPostStrategy.class);
@@ -27,11 +31,18 @@ public class StackPostStrategy implements Strategy {
             stackEntity.getRequest().setCluster(cluster.getRequest());
         }
 
-        logJSON("Stack post request: ", stackEntity.getRequest());
-        stackEntity.setResponse(
-                client.getCloudbreakClient()
-                        .stackV2Endpoint()
-                        .postPrivate(stackEntity.getRequest()));
-        logJSON("Stack post response: ", stackEntity.getResponse());
+        log(" Name:\n" + stackEntity.getRequest().getGeneral().getName());
+        logJSON(" Stack post request:\n", stackEntity.getRequest());
+        try {
+            stackEntity.setResponse(
+                    client.getCloudbreakClient()
+                            .stackV2Endpoint()
+                            .postPrivate(stackEntity.getRequest()));
+        } catch (WebApplicationException e) {
+            logJSON(" Stack post response:\n", e.getResponse().readEntity(String.class));
+            throw e;
+        }
+        logJSON(" Stack post response:\n", stackEntity.getResponse());
+        log(" ID:\n" + stackEntity.getResponse().getId());
     }
 }
--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/log/ReportListener.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/log/ReportListener.java
@@ -3,12 +3,10 @@ package com.sequenceiq.it.cloudbreak.newway.log;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-import java.util.Arrays;
-
 public class ReportListener extends TestListenerAdapter {
     @Override
     public void onTestFailure(ITestResult tr) {
-        Log.log(tr.getThrowable().getMessage());
-        Log.log(Arrays.toString(tr.getThrowable().getStackTrace()));
+        Throwable throwable = tr.getThrowable();
+        Log.log(throwable.getMessage());
     }
 }
