188fc3eff66dcfa9341041f0f970efa572020238
Arpad Farsang <afarsang@HW12482.local>
2018-05-30 16:00:18 +0200

    BUG-102525 mock redesign

---
+++ tests/responses/responses.js
@@ -0,0 +1,224 @@
+var responses={};
+
+var acc = require('./accountpreferences/accountpreferences.json');
+var platforms = require('./accountpreferences/platforms.json');
+var defaultblueprint = require('./blueprints/default-blueprint.json');
+var qablueprint = require('./blueprints/qa-blueprints.json');
+var disktypes = require('./connectors/disktypes.json');
+var gateways = require('./connectors/gateways.json');
+var ippools = require('./connectors/ippools.json');
+var connetworks = require('./connectors/networks.json');
+var recommendations = require('./connectors/recommendations.json');
+var regions = require('./connectors/regions.json');
+var sshkeys = require('./connectors/sshkeys.json');
+var securitygroups = require('./connectors/securitygroups.json');
+var credentialaws = require('./credentials/aws.json');
+var credentialazure = require('./credentials/azure.json');
+var credentialgcp = require('./credentials/gcp.json');
+var credentialopenstack = require('./credentials/openstack.json');
+var cbdefimagec = require('./imagecatalogs/cloudbreak-default.json');
+var defimagec = require('./imagecatalogs/default-imagecatalog.json');
+var qaimages = require('./imagecatalogs/qa-images.json');
+var ldap = require('./ldapconfig/default-ldap.json');
+var networks = require('./networks/networks.json');
+var proxy = require('./proxyconfig/default-proxy.json');
+var rds = require('./rdsconfig/default-rds.json');
+var recipes = require('./recipes/recipes.json');
+var responses = require('./responses.js');
+var secgr = require('./securitygroups/securitygroups.json');
+var secrules = require('./securityrules/securityrules.json');
+var aws = require('./stacks/aws.json');
+var azure = require('./stacks/azure.json');
+var gcp = require('./stacks/gcp.json');
+var openstack = require('./stacks/openstack.json');
+var templates = require('./templates/qa-templates.json');
+var profile = require('./users/default-profile.json');
+var matrix = require('./utilsmatrix.json');
+
+const OK = 200;
+
+var responseObject = function(payload, code) {
+	return { responses : [ { response: payload , statusCode: code } ] };
+}
+
+var addResponseObject = function(responses, payload, code, conditionText) {
+	var newElement = { response: payload, statusCode: code, condition: conditionText };
+	responses.responses.push(newElement);
+	return responses;
+}
+
+var responseObjectWithCondition = function(payload, code, conditionText) {
+	return { responses : [ { response: payload , statusCode: code , condition: conditionText} ] };
+}
+
+var stackResponses = responseObjectWithCondition({"message":"Stack 'az404' not found"}, 404, "return params['name'].value === 'az404';");
+stackResponses = addResponseObject(stackResponses, openstack, OK, "return params['name'].value !== 'az404';");
+
+var stackOperationResponses = responseObjectWithCondition({"message":"Stack 'azstatus' not found"}, 404, "return params['name'].value === 'azstatus';");
+stackOperationResponses = addResponseObject(stackOperationResponses,null, OK , "return params['name'].value !== 'azstatus';");
+
+var stackReinstallResponses = responseObjectWithCondition({"message":"Stack 'aaaaa' not found"}, 404, "return params['name'].value === 'aaaaa';");
+stackReinstallResponses = addResponseObject(stackReinstallResponses,null, OK , "return params['name'].value !== 'aaaaa';");
+
+responses.getCloudbreakInfo= responseObject({ "app": { "name":"cloudbreak", "version":"MOCK" } }, OK);
+responses.getCloudbreakHealth= responseObject({ "status":"UP" }, OK);
+responses.getAccountPreferencesEndpoint= responseObject(acc, OK);
+responses.isPlatformSelectionDisabled= responseObject(platforms, OK);
+responses.platformEnablement= responseObject(platforms, OK);
+responses.getPublicsBlueprint= responseObject(qablueprint, OK);
+responses.getPublicBlueprint= responseObject(defaultblueprint, OK);
+responses.getPrivatesBlueprint= responseObject(qablueprint, OK);
+responses.getPrivateBlueprint= responseObject(defaultblueprint, OK);
+responses.getBlueprint= responseObject(defaultblueprint, OK);
+responses.postPublicBlueprint= responseObject(defaultblueprint, OK);
+responses.postPrivateBlueprint= responseObject(defaultblueprint, OK);
+responses.getBlueprintRequestFromId= responseObject(defaultblueprint, OK);
+responses.getDisktypes= responseObject(disktypes, OK);
+responses.getGatewaysCredentialId= responseObject(gateways, OK);
+responses.getIpPoolsCredentialId= responseObject(ippools, OK);
+responses.getPlatformNetworks= responseObject(connetworks, OK);
+responses.getPublicsNetwork= responseObject(networks, OK);
+responses.createRecommendation= responseObject(recommendations, OK);
+responses.getRegions= responseObject(regions, OK);
+responses.getPlatformSShKeys= responseObject(sshkeys, OK);
+responses.getCredential = responseObject(credentialopenstack, OK);
+responses.postPrivateCredential = responseObject(credentialopenstack, OK);
+responses.getPublicsCredential= responseObject([credentialopenstack,credentialgcp,credentialazure,credentialaws], OK);
+responses.getPublicCredential= responseObject(credentialopenstack, OK);
+responses.getPrivatesCredential= responseObject([credentialopenstack,credentialgcp,credentialazure,credentialaws], OK);
+responses.getPrivateCredential= responseObject(credentialopenstack, OK);
+responses.getPublicsImageCatalogs= responseObject(defimagec, OK);
+responses.postPublicImageCatalog= responseObject(defimagec, OK);
+responses.putPublicImageCatalog= responseObject(defimagec, OK);
+responses.getPublicImageCatalogsByName= responseObject(defimagec, OK);
+responses.getPublicImagesByProviderAndCustomImageCatalog= responseObject(qaimages, OK);
+responses.putSetDefaultImageCatalogByName= responseObject(defimagec, OK);
+responses.postPrivateImageCatalog= responseObject(defimagec, OK);
+responses.getImageCatalogRequestFromName= responseObject(defimagec, OK);
+responses.getPublicsLdap= responseObject(ldap, OK);
+responses.postPublicLdap= responseObject(ldap, OK);
+responses.getPublicLdap= responseObject(ldap, OK);
+responses.postLdapConnectionTest= responseObject({ "connectionResult":"Failed to connect to LDAP server: hwxad-1a2bcd3e45678f90.elb.eu-west-1.amazonaws.com:123" }, OK);
+responses.getPrivatesLdap= responseObject(ldap, OK);
+responses.postPrivateLdap= responseObject(ldap, OK);
+responses.getPrivateLdap= responseObject(ldap, OK);
+responses.getLdap= responseObject(ldap, OK);
+responses.getPublicsProxyConfig= responseObject(proxy, OK);
+responses.postPublicProxyConfig= responseObject(proxy, OK);
+responses.getPublicProxyConfig= responseObject(proxy, OK);
+responses.getPrivatesProxyConfig= responseObject(proxy, OK);
+responses.postPrivateProxyConfig= responseObject(proxy, OK);
+responses.getPrivateProxyConfig= responseObject(proxy, OK);
+responses.getProxyConfig= responseObject(proxy, OK);
+responses.getPublicsRds= responseObject(rds, OK);
+responses.postPublicRds= responseObject(rds, OK);
+responses.getPublicRds= responseObject(rds, OK);
+responses.testRdsConnection= responseObject({ "connectionResult":"Failed to connect to RDS: The connection attempt failed." }, OK);
+responses.getPrivatesRds= responseObject(rds, OK);
+responses.postPrivateRds= responseObject(rds, OK);
+responses.getPrivateRds= responseObject(rds, OK);
+responses.getPublicsRecipe= responseObject(recipes, OK);
+responses.getPublicRecipe= responseObject(recipes[0], OK);
+responses.postPublicRecipe= responseObject(recipes[0], OK);
+responses.postPrivateRecipe= responseObject(recipes[0], OK);
+responses.getRecipeRequestFromName= responseObject(recipes, OK);
+responses.getPublicsSecurityGroup= responseObject(secgr, OK);
+responses.getDefaultSecurityRules= responseObject(secrules, OK);
+responses.getPublicsStack= responseObject([openstack,aws,azure,gcp], OK);
+responses.getPublicStack= stackResponses;
+responses.getPublicsTemplate= responseObject(templates, OK);
+responses.getUserProfile= responseObject(profile, OK);
+responses.getRegionsByCredentialId= responseObject(regions, OK);
+responses.getPublicsStackV2= responseObject([openstack, aws, azure, gcp], OK);
+responses.getPublicStackV2= responseObject(stackResponses, OK);
+responses.postPrivateStackV2= responseObject(openstack, OK);
+responses.getPrivateStackV2= responseObject(openstack, OK);
+responses.getStackV2= responseObject(openstack, OK);
+responses.getPlatformSecurityGroups= responseObject(securitygroups, OK);
+responses.postRepositoryConfigsValidation= responseObject({
+  "utilsBaseURL" : true,
+  "ambariGpgKeyUrl" : true,
+  "mpackUrl" : true,
+  "stackBaseURL" : true,
+  "versionDefinitionFileUrl" : true,
+  "ambariBaseUrl" : true
+}, OK);
+responses.getStackMatrixUtil= responseObject(matrix , OK);
+responses.getSmartSenseSubscription= responseObject({
+  "owner" : "owner",
+  "publicInAccount" : false,
+  "id" : 1,
+  "autoGenerated" : false,
+  "subscriptionId" : "subscriptionId",
+  "account" : "account"
+}, OK);
+responses.getTagSpecifications = responseObject({ "specifications" : { "key" : { "key" : "{}" } } }, OK);
+responses.getPublicManagementPacks = responseObject([ {
+  "public" : false,
+  "name" : "name",
+  "description" : "description",
+  "purge" : false,
+  "force" : false,
+  "id" : 0,
+  "mpackUrl" : "mpackUrl",
+  "purgeList" : [ "purgeList", "purgeList" ]
+}, {
+  "public" : false,
+  "name" : "name",
+  "description" : "description",
+  "purge" : false,
+  "force" : false,
+  "id" : 0,
+  "mpackUrl" : "mpackUrl",
+  "purgeList" : [ "purgeList", "purgeList" ]
+} ],OK);
+responses.getPublicFlexSubscriptions = responseObject([ {
+  "owner" : "owner",
+  "publicInAccount" : false,
+  "smartSenseSubscriptionId" : 0,
+  "usedForController" : false,
+  "name" : "name",
+  "smartSenseSubscription" : {
+    "owner" : "owner",
+    "publicInAccount" : false,
+    "id" : 1,
+    "autoGenerated" : false,
+    "subscriptionId" : "subscriptionId",
+    "account" : "account"
+  },
+  "id" : 6,
+  "subscriptionId" : "subscriptionId",
+  "account" : "account",
+  "usedAsDefault" : false
+}, {
+  "owner" : "owner",
+  "publicInAccount" : false,
+  "smartSenseSubscriptionId" : 0,
+  "usedForController" : false,
+  "name" : "name",
+  "smartSenseSubscription" : {
+    "owner" : "owner",
+    "publicInAccount" : false,
+    "id" : 1,
+    "autoGenerated" : false,
+    "subscriptionId" : "subscriptionId",
+    "account" : "account"
+  },
+  "id" : 6,
+  "subscriptionId" : "subscriptionId",
+  "account" : "account",
+  "usedAsDefault" : false
+} ], OK);
+
+responses.putstartStackV2 = stackOperationResponses;
+responses.putscalingStackV2 = stackOperationResponses;
+responses.putstopStackV2 = stackOperationResponses;
+responses.putrepairStackV2 = stackOperationResponses;
+responses.putsyncStackV2 = stackOperationResponses;
+responses.putreinstallStackV2 = stackReinstallResponses;
+
+responses.checkClientVersion = responseObject({ "versionCheckOk" : true, "message" : "message" }, OK);
+ 
+module.exports= {
+  responses:responses
+}
--- tests/scripts/docker-integration-test.sh
+++ tests/scripts/docker-integration-test.sh
@@ -18,10 +18,9 @@ docker run -it \
     --rm \
     --name $TEST_CONTAINER_NAME \
     --net=host \
-    -v "${HOME}/project/build/Linux":/usr/local/bin \
+    -v $(pwd)/../build/Linux:/usr/local/bin \
     -v $(pwd):/work \
     afarsang/docker-bats-jq /work/scripts/integration-test.sh
-RESULT=$?
 
 echo
 echo "Failed tests:"
@@ -29,4 +28,4 @@ echo "============="
 grep "not ok" test-result.tap || { echo " none " ; exit ; }
 echo
 
-exit $RESULT
+exit 1
