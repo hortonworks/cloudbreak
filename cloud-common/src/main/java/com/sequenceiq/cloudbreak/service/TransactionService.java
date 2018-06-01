4a90f381d347b0596800cff239b39037026dc8ad
Richard Kovacs <kovacsricsi@gmail.com>
2018-06-01 12:37:01 +0200

    BUG-102728 Log long during transactions

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/service/TransactionService.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/service/TransactionService.java
@@ -1,62 +1,105 @@
 package com.sequenceiq.cloudbreak.service;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionService.class);
+
+    private static final long TX_DURATION_OK = 1000L;
+
+    private static final long TX_DURATION_ERROR = 3000L;
+
     @Inject
     private TransactionExecutorService transactionExecutorService;
 
+    @Inject
+    private Clock clock;
+
     public <T> T required(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.required(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
         }
     }
 
     public <T> T requiresNew(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.requiresNew(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
         }
     }
 
     public <T> T mandatory(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.mandatory(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
         }
     }
 
     public <T> T supports(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.supports(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
         }
     }
 
     public <T> T notSupported(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.notSupported(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
         }
     }
 
     public <T> T never(TransactionCallback<T> callback) throws TransactionExecutionException {
+        long start = clock.getCurrentTime();
         try {
             return transactionExecutorService.never(callback);
         } catch (RuntimeException e) {
             throw new TransactionExecutionException("Transaction went fail", e);
+        } finally {
+            processTransactionDuration(start);
+        }
+    }
+
+    private void processTransactionDuration(long start) {
+        long duration = clock.getCurrentTime() - start;
+        if (TX_DURATION_ERROR < duration) {
+            LOGGER.error("Transaction duration was critical, took {}ms  at: {}", duration,
+                    String.join("\n\t", Arrays.stream(Thread.currentThread().getStackTrace()).map(StackTraceElement::toString).collect(Collectors.toSet())));
+        } else if (TX_DURATION_OK < duration) {
+            LOGGER.warn("Transaction duration was high, took {}ms at: {}", duration,
+                    String.join("\n\t", Arrays.stream(Thread.currentThread().getStackTrace()).map(StackTraceElement::toString).collect(Collectors.toSet())));
         }
     }
 
