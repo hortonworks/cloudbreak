9cf6502d5b3f9718a8ccdc232183d6fd480fb64a
Richard Doktorics <rdoktorics@hortonworks.com>
2018-06-04 16:07:37 +0200

    RMP-11415 fix for adls storage link

--- blueprint-manager/src/test/java/com/sequenceiq/cloudbreak/blueprint/filesystem/query/FileSystemConfigQueryServiceTest.java
+++ blueprint-manager/src/test/java/com/sequenceiq/cloudbreak/blueprint/filesystem/query/FileSystemConfigQueryServiceTest.java
@@ -75,8 +75,8 @@ public class FileSystemConfigQueryServiceTest {
         Assert.assertTrue(hiveMetastore.isPresent());
         Assert.assertTrue(rangerAdmin.isPresent());
 
-        Assert.assertEquals("default-account-name.azuredatalakestore.net/apps/hive/warehouse", hiveMetastore.get().getDefaultPath());
-        Assert.assertEquals("default-account-name.azuredatalakestore.net/apps/ranger/audit/bigCluster", rangerAdmin.get().getDefaultPath());
+        Assert.assertEquals("default-account-name.azuredatalakestore.net/hwx-remote/apps/hive/warehouse", hiveMetastore.get().getDefaultPath());
+        Assert.assertEquals("default-account-name.azuredatalakestore.net/hwx-remote/apps/ranger/audit/bigCluster", rangerAdmin.get().getDefaultPath());
     }
 
     @Test
@@ -97,7 +97,7 @@ public class FileSystemConfigQueryServiceTest {
 
         Assert.assertFalse(hiveMetastore.isPresent());
         Assert.assertTrue(rangerAdmin.isPresent());
-        Assert.assertEquals("default-account-name.azuredatalakestore.net/apps/ranger/audit/bigCluster", rangerAdmin.get().getDefaultPath());
+        Assert.assertEquals("default-account-name.azuredatalakestore.net/hwx-remote/apps/ranger/audit/bigCluster", rangerAdmin.get().getDefaultPath());
     }
 
     @Test
@@ -118,7 +118,7 @@ public class FileSystemConfigQueryServiceTest {
 
         Assert.assertTrue(hiveMetastore.isPresent());
         Assert.assertFalse(rangerAdmin.isPresent());
-        Assert.assertEquals("default-account-name.azuredatalakestore.net/apps/hive/warehouse", hiveMetastore.get().getDefaultPath());
+        Assert.assertEquals("default-account-name.azuredatalakestore.net/hwx-remote/apps/hive/warehouse", hiveMetastore.get().getDefaultPath());
     }
 
     @Test
