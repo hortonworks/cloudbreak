7766852b1fd8e1687f23c0ecdc30e3c0336e4e06
Tamas Bihari <tbihari@hortonworks.com>
2018-06-11 19:29:53 +0200

    BUG-104950 use file based database dump and restore mechanism

--- include/deployer.bash
+++ include/deployer.bash
@@ -175,14 +175,14 @@ cbd-update-release() {
     if [[ ${binver} != ${lastver} ]]; then
         debug upgrade needed |yellow
 
-        if docker volume inspect common &>/dev/null; then
+        if docker volume inspect $COMMON_DB_VOL &>/dev/null; then
             info "Backing up databases"
-            debug "Backup $CB_DB_ENV_DB on volume $COMMON_DB_VOL"
-            db-dump $COMMON_DB_VOL $CB_DB_ENV_DB
-            debug "Backup $PERISCOPE_DB_ENV_DB on volume $COMMON_DB_VOL"
-            db-dump $COMMON_DB_VOL $PERISCOPE_DB_ENV_DB
-            debug "Backup $IDENTITY_DB_NAME on volume $COMMON_DB_VOL"
-            db-dump $COMMON_DB_VOL $IDENTITY_DB_NAME
+            debug "Backing up $CB_DB_ENV_DB"
+            db-dump $CB_DB_ENV_DB
+            debug "Backing up $PERISCOPE_DB_ENV_DB"
+            db-dump $PERISCOPE_DB_ENV_DB
+            debug "Backing up $IDENTITY_DB_NAME"
+            db-dump $IDENTITY_DB_NAME
         fi
 
         local url=https://github.com/hortonworks/cloudbreak-deployer/releases/download/v${lastver}/cloudbreak-deployer_${lastver}_${osarch}.tgz
@@ -554,8 +554,8 @@ start-wait-cmd() {
 start-requested-services() {
     declare services="$@"
 
-    db-initialize-databases
     deployer-generate
+    db-initialize-databases
 
     if ! [[ "$services" ]]; then
         debug "All services must be started"
@@ -683,9 +683,7 @@ main() {
 
     cmd-export-ns db "Db operations namespace"
     cmd-export db-dump
-    cmd-export db-list-dumps
-    cmd-export db-set-dump
-    cmd-export db-restore-volume-from-dump
+    cmd-export db-restore
 
     cmd-export cbd-update update
 
