87a4662a575c990387d5fccaa151ec60535b0663
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2018-06-27 11:47:31 +0200

    BUG-106231 Autoscale doesn't work if cloudstorage is enabled

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/v2/filesystem/CloudStorageParameters.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/v2/filesystem/CloudStorageParameters.java
@@ -1,8 +1,10 @@
 package com.sequenceiq.cloudbreak.api.model.v2.filesystem;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sequenceiq.cloudbreak.api.model.JsonEntity;
 import com.sequenceiq.cloudbreak.api.model.filesystem.FileSystemType;
 
+@JsonIgnoreProperties(ignoreUnknown = true, value = "type")
 public interface CloudStorageParameters extends JsonEntity {
 
     FileSystemType getType();
