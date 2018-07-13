37c03720288fd8e3644f19ecbbece23634fd64b1
Richard Doktorics <rdoktorics@hortonworks.com>
2018-07-13 13:43:57 +0200

    BUG-107482 adding ttl to every cache

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/CloudResourceRegionCache.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/CloudResourceRegionCache.java
@@ -1,6 +1,9 @@
 package com.sequenceiq.cloudbreak.cache.common;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.cache.CacheDefinition;
@@ -12,12 +15,16 @@ public class CloudResourceRegionCache implements CacheDefinition {
 
     private static final long MAX_ENTRIES = 1000L;
 
+    @Value("${cb.cloud.region.cache.ttl:15}")
+    private long ttlMinutes;
+
     @Override
     public CacheConfiguration cacheConfiguration() {
         CacheConfiguration cacheConfiguration = new CacheConfiguration();
         cacheConfiguration.setName("cloudResourceRegionCache");
         cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
         cacheConfiguration.setMaxEntriesLocalHeap(MAX_ENTRIES);
+        cacheConfiguration.setTimeToLiveSeconds(ttlMinutes == 0L ? 1 : TimeUnit.MINUTES.toSeconds(ttlMinutes));
         return cacheConfiguration;
     }
 }
