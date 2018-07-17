c3005ecf37f2eae08f295016087f574cced40b0a
bbihari <bbihari@hortonworks.com>
2018-07-17 11:10:03 +0200

    BUG-107554 Cloudbreak History throws 'Internal server error: HasPermission should be added' error

--- core/src/main/java/com/sequenceiq/cloudbreak/repository/CloudbreakUsageRepository.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/repository/CloudbreakUsageRepository.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.beust.jcommander.internal.Nullable;
 import com.sequenceiq.cloudbreak.aspect.BaseRepository;
-import com.sequenceiq.cloudbreak.domain.CloudbreakUsage;
 import com.sequenceiq.cloudbreak.aspect.HasPermission;
+import com.sequenceiq.cloudbreak.domain.CloudbreakUsage;
 import com.sequenceiq.cloudbreak.service.EntityType;
 
 @EntityType(entityClass = CloudbreakUsage.class)
 @Transactional(Transactional.TxType.REQUIRED)
 @HasPermission
-public interface CloudbreakUsageRepository extends BaseRepository<CloudbreakUsage, Long>, JpaSpecificationExecutor {
+public interface CloudbreakUsageRepository extends BaseRepository<CloudbreakUsage, Long>, JpaSpecificationExecutor<CloudbreakUsage> {
 
     @Query("SELECT u FROM CloudbreakUsage u WHERE u.stackId = :stackId AND u.status = 'OPEN'")
     List<CloudbreakUsage> findOpensForStack(@Param("stackId") Long stackId);
@@ -30,4 +32,7 @@ public interface CloudbreakUsageRepository extends BaseRepository<CloudbreakUsag
 
     @Query("SELECT u FROM CloudbreakUsage u WHERE (u.status = 'STOPPED' OR u.status = 'OPEN')AND u.day < :today")
     List<CloudbreakUsage> findAllOpenAndStopped(@Param("today") Date today);
+
+    @Override
+    List<CloudbreakUsage> findAll(@Nullable Specification<CloudbreakUsage> spec);
 }
