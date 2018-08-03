0e1f1342671e5eebcfea29e6c5462b82635c9c17
Meszaros Gergely <grigorijat9026@gmail.com>
2018-08-03 18:21:44 +0200

    BUG-108343 – Tests for support python shebangs in recipe

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/GeneralRecipeClusterTests.java
@@ -0,0 +1,153 @@
+package com.sequenceiq.it.cloudbreak;
+
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.sequenceiq.cloudbreak.api.model.RecipeType;
+import com.sequenceiq.it.cloudbreak.newway.CloudbreakClient;
+import com.sequenceiq.it.cloudbreak.newway.CloudbreakTest;
+import com.sequenceiq.it.cloudbreak.newway.Cluster;
+import com.sequenceiq.it.cloudbreak.newway.Recipe;
+import com.sequenceiq.it.cloudbreak.newway.Stack;
+import com.sequenceiq.it.cloudbreak.newway.TestParameter;
+import com.sequenceiq.it.cloudbreak.newway.cloud.CloudProvider;
+import com.sequenceiq.it.cloudbreak.newway.cloud.CloudProviderHelper;
+import com.sequenceiq.it.cloudbreak.newway.cloud.OpenstackCloudProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GeneralRecipeClusterTests extends CloudbreakTest {
+
+    private static final String BLUEPRINT_HDP26_NAME = "Data Science: Apache Spark 2, Apache Zeppelin";
+
+    private static final String VALID_RECIPE_DESCRIPTION = "python recipe for API E2E tests";
+
+    private static final Set<String> BASH_RECIPE_NAMES = new HashSet<>(Arrays.asList("post-ambari-start", "pre-ambari-start",
+            "post-cluster-install", "pre-termination"));
+
+    private static final Set<String> PYTHON_RECIPE_NAMES = new HashSet<>(Arrays.asList("post-ambari-start-py", "pre-ambari-start-py",
+            "post-cluster-install-py", "pre-termination-py"));
+
+    private static final String[] FILES_PATH = {"/var/log/recipes/post-ambari-start", "/var/log/recipes/pre-ambari-start",
+            "/var/log/recipes/post-cluster-install"};
+
+    private static final String[] HOSTGROUPS = {"master", "worker", "compute"};
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeneralRecipeClusterTests.class);
+
+    private static final String PYTHON_SCRIPT = String.format("#!/usr/bin/python%nprint(\"An example of a python script\")%nimport sys%n"
+            + "print(sys.version_info)");
+
+    private static final String BASH_SCRIPT = String.format("#!/bin/bash%nCOUNTER=0%nwhile [ ! -f /tmp/pre-termination ] && [ $COUNTER -lt 15 ]%ndo%n  "
+            + "sleep 2%n  COUNTER=$((COUNTER+1))%ndone");
+
+    @Value("${integrationtest.defaultPrivateKeyFile}")
+    private String defaultPrivateKeyFile;
+
+    private CloudProvider cloudProvider;
+
+    public GeneralRecipeClusterTests() {
+    }
+
+    public GeneralRecipeClusterTests(CloudProvider cp, TestParameter tp) {
+        cloudProvider = cp;
+        setTestParameter(tp);
+    }
+
+    @BeforeTest
+    @Parameters("provider")
+    public void beforeTest(@org.testng.annotations.Optional(OpenstackCloudProvider.OPENSTACK) String provider) {
+        LOGGER.info("before cluster test set provider: " + provider);
+        if (cloudProvider != null) {
+            LOGGER.info(cloudProvider + " cloud provider already set - running from factory test");
+            return;
+        }
+        cloudProvider = CloudProviderHelper.providerFactory(provider, getTestParameter());
+    }
+
+    @BeforeTest
+    public void setupRecipes() throws Exception {
+        given(CloudbreakClient.isCreated());
+        for (String recipe : BASH_RECIPE_NAMES) {
+            given(Recipe.isCreated()
+                    .withName(recipe)
+                    .withDescription(VALID_RECIPE_DESCRIPTION)
+                    .withRecipeType(RecipeType.valueOf(recipe.replace("-", "_").toUpperCase()))
+                    .withContent(Base64.encodeBase64String(BASH_SCRIPT.getBytes()))
+            );
+        }
+        for (String pythonRecipe : PYTHON_RECIPE_NAMES) {
+            given(Recipe.isCreated()
+                    .withName(pythonRecipe)
+                    .withDescription(VALID_RECIPE_DESCRIPTION)
+                    .withRecipeType(RecipeType.valueOf(pythonRecipe.substring(0, pythonRecipe.indexOf("-py")).replace("-", "_").toUpperCase()))
+                    .withContent(Base64.encodeBase64String(PYTHON_SCRIPT.getBytes()))
+            );
+        }
+    }
+
+    @Test
+    public void testCreateClusterWithRecipes() throws Exception {
+        given(cloudProvider.aValidCredential());
+        given(Cluster.request()
+                        .withAmbariRequest(cloudProvider.ambariRequestWithBlueprintName(BLUEPRINT_HDP26_NAME)),
+                "a cluster request");
+        given(cloudProvider
+                .aValidStackRequest()
+                .withInstanceGroups(cloudProvider.instanceGroups(Stream.concat(
+                        BASH_RECIPE_NAMES.stream(), PYTHON_RECIPE_NAMES.stream()).collect(Collectors.toSet())))
+                .withName(getTestParameter().getWithDefault("openstackClusterName", "e2e-cluster-py-recipe")),
+                "a stack request");
+        when(Stack.post(), "post the stack request with recipes");
+        then(Stack.waitAndCheckClusterAndStackAvailabilityStatus(),
+                "wait and check availability");
+        then(Stack.checkClusterHasAmbariRunning(
+                getTestParameter().get(CloudProviderHelper.DEFAULT_AMBARI_PORT),
+                getTestParameter().get(CloudProviderHelper.DEFAULT_AMBARI_USER),
+                getTestParameter().get(CloudProviderHelper.DEFAULT_AMBARI_PASSWORD)),
+                "check ambari is running and components available");
+    }
+
+    @Test(priority = 1)
+    public void testCheckRecipesOnNodes() throws Exception {
+        given(cloudProvider.aValidCredential());
+        given(cloudProvider.aValidStackIsCreated(), "a stack is created");
+        when(Stack.get());
+        then(Stack.checkRecipes(HOSTGROUPS, FILES_PATH, getTestParameter().getRequired("integrationtest.defaultPrivateKeyFile"),
+                getRequiredRecipeAmountForRunningCluster()), "check recipes are ran on all nodes");
+    }
+
+    @Test(priority = 2)
+    public void testTerminateClusterCheckRecipePreTerm() throws Exception {
+        given(cloudProvider.aValidCredential());
+        given(cloudProvider.aValidStackIsCreated(), "a stack is created");
+        when(Stack.delete());
+        then(Stack.waitAndCheckClusterDeleted(), "stack has been deleted");
+    }
+
+    @AfterTest
+    public void cleanUpdeleteValidRecipes() throws Exception {
+        for (String recipe : Stream.concat(BASH_RECIPE_NAMES.stream(), PYTHON_RECIPE_NAMES.stream()).collect(Collectors.toSet())) {
+            given(Recipe.request()
+                    .withName(recipe)
+            );
+            when(Recipe.delete());
+        }
+    }
+
+    private int getRequiredRecipeAmountForRunningCluster() {
+        // * 2 since we post both bash and python script
+        // + 1 since the hdfs-home (default) recipe/script is only available on the master node
+        return FILES_PATH.length * 2 * HOSTGROUPS.length + 1;
+    }
+
+}
--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/SshService.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/SshService.java
@@ -1,91 +1,65 @@
 package com.sequenceiq.it.cloudbreak;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.apache.commons.lang3.tuple.Pair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.userauth.UserAuthException;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class SshService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SshService.class);
 
-    public void executeCommand(List<String> publicIps, String [] files, String pirvateKey, String sshCommand, String sshUser, int sshTimeout, Integer require,
-            Map<String,
-                    List<String>> sshCheckMap) throws Exception {
-        Collection<Future<?>> futures = new ArrayList<>(publicIps.size() * files.length);
-        ExecutorService executorService = Executors.newFixedThreadPool(publicIps.size());
-        AtomicInteger count = new AtomicInteger(0);
-        try {
-            for (String file : files) {
-                if (sshCheckMap.containsKey("beginsWith")) {
-                    sshCheckMap.put("beginsWith", Collections.singletonList(file));
-                }
-                for (String ip : publicIps) {
-                    futures.add(executorService.submit(() -> {
-                        try {
-                            if (isExecutedCommand(ip, pirvateKey, sshCommand + file, sshUser, sshTimeout, sshCheckMap)) {
-                                count.incrementAndGet();
-                            }
-                        } catch (IOException e) {
-                            LOGGER.error("Error occurred during ssh execution: " + e);
-                        }
-                    }));
-                }
-                if (sshCheckMap.containsKey("beginsWith")) {
-                    sshCheckMap.put("beginsWith", Collections.singletonList(""));
+    public int countFilesOnHostByExtensionAndPath(@Nonnull List<String> publicIps, @Nonnull String[] paths, Optional<String> defaultCommandChain,
+                    String privateKey, String extension, String sshUser, int sshTimeout, int require) throws IOException {
+        int quantity = 0;
+        for (String publicIp : publicIps) {
+            for (String path : paths) {
+                String fileListCommand = String.format("find %s -type f -name *.%s", path, extension);
+                String formatTextForFullCommand = !defaultCommandChain.isPresent() || defaultCommandChain.get().endsWith(";") ? "%s%s" : "%s;%s";
+                String command = String.format(formatTextForFullCommand, defaultCommandChain.isPresent() ? defaultCommandChain : "", fileListCommand);
+                try (SSHClient sshClient = createSshClient(publicIp, sshTimeout, sshUser, privateKey)) {
+                    Pair<Integer, String> cmdOut = execute(sshClient, command);
+                    quantity += Stream.of(cmdOut.getValue().split(System.getProperty("line.separator"))).filter(s -> s != null && s.startsWith("/")).count();
+                } catch (UserAuthException | TransportException ignore) {
+                    LOGGER.warn("Whoops! SSH fail on {} while getting info for location: {}", publicIp, path);
                 }
             }
-            for (Future<?> future : futures) {
-                future.get();
-            }
-        } finally {
-            executorService.shutdown();
         }
-        org.testng.Assert.assertEquals(count.get(), require.intValue(), "The number of existing files is different than required.");
+        return quantity;
     }
 
-    public static boolean isExecutedCommand(String host, String defaultPrivateKeyFile, String sshCommand, String sshUser, int sshTimeout,  Map<String,
-            List<String>> sshCheckMap)
-            throws IOException {
-        if (sshCheckMap.isEmpty()) {
-            return false;
+    private static SSHClient createSshClient(String host, int sshTimeout, String sshUser, String defaultPrivateKeyFile) throws IOException {
+        SSHClient client = new SSHClient();
+        client.addHostKeyVerifier(new PromiscuousVerifier());
+        client.connect(host, 22);
+        client.setConnectTimeout(sshTimeout);
+        client.authPublickey(sshUser, defaultPrivateKeyFile);
+        return client;
+    }
+
+    private static Pair<Integer, String> execute(SSHClient ssh, String command) throws IOException {
+        LOGGER.info("Waiting to SSH command to be executed...");
+        try (Session session = startSshSession(ssh);
+                    Command cmd = session.exec(command);
+                    OutputStream os = IOUtils.readFully(cmd.getInputStream())) {
+            LOGGER.info("The following SSH command is going to be executed on host {}: {}", ssh.getConnection().getTransport().getRemoteHost(), command);
+            cmd.join(10L, TimeUnit.SECONDS);
+            return Pair.of(cmd.getExitStatus(), os.toString());
         }
-        try (SSHClient sshClient = new SSHClient()) {
-            sshClient.addHostKeyVerifier(new PromiscuousVerifier());
-            sshClient.connect(host, 22);
-            sshClient.setConnectTimeout(sshTimeout);
-            sshClient.authPublickey(sshUser, defaultPrivateKeyFile);
-            Pair<Integer, String> cmdOut = execute(sshClient, sshCommand);
-            LOGGER.info("Ssh command status code and output: " + cmdOut + " on host: " + host);
-            for (Entry<String, List<String>> entry : sshCheckMap.entrySet()) {
-                for (String listValue: entry.getValue()) {
-                    if (cmdOut.getLeft() != 0 || !checkCommandOutput(cmdOut, entry.getKey(), listValue)) {
-                        LOGGER.error("Ssh command output is not proper: " + entry.getKey() + ' ' + listValue);
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
     }
 
     private static Session startSshSession(SSHClient ssh) throws IOException {
@@ -94,30 +68,4 @@ public class SshService {
         return sshSession;
     }
 
-    private static Pair<Integer, String> execute(SSHClient ssh, String command) throws IOException {
-        LOGGER.info("Waiting to SSH command to be executed...");
-        try (Session session = startSshSession(ssh)) {
-            try (Command cmd = session.exec(command)) {
-                try (OutputStream os = IOUtils.readFully(cmd.getInputStream())) {
-                    cmd.join(10L, TimeUnit.SECONDS);
-                    return Pair.of(cmd.getExitStatus(), os.toString());
-                }
-            }
-        }
-    }
-
-    private static boolean checkCommandOutput(Pair<Integer, String> cmdOut, String checkType, String value) {
-        switch (checkType) {
-            case "contains":
-                return cmdOut.getRight().contains(value);
-            case "notContains":
-                return !(cmdOut.getRight().contains(value));
-            case "beginsWith":
-                return cmdOut.getRight().startsWith(value);
-            default:
-                LOGGER.info("Check type {} is not exist!", checkType);
-                break;
-        }
-        return false;
-    }
 }
