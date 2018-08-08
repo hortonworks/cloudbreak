5a53ae27ff1513ccb656063f7488ea78425bfb42
Erdős Péter <perdos@hortonworks.com>
2018-08-08 13:41:49 +0200

    fix CB_KAFKA_BOOTSTRAP_SERVERS default value

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -233,7 +233,7 @@ cloudbreak-conf-defaults() {
         env-import CB_DEFAULT_GATEWAY_CIDR
     fi;
     env-import CB_AUDIT_FILE_ENABLED false
-    env-import CB_KAFKA_BOOTSTRAP_SERVERS
+    env-import CB_KAFKA_BOOTSTRAP_SERVERS ""
     env-import CB_LOCAL_DEV_BIND_ADDR "192.168.64.1"
     env-import ADDRESS_RESOLVING_TIMEOUT 120000
     env-import CB_UI_MAX_WAIT 400
