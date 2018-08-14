05cc478100f98ad74dcf2948c9912657e2cbc95e
Attila Palfi <apalfi@hortonworks.com>
2018-08-14 17:21:29 +0200

    BUG-108475 - stack v3 endpoints

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v3/StackV3Endpoint.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v3/StackV3Endpoint.java
@@ -5,14 +5,22 @@ import java.util.Set;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.sequenceiq.cloudbreak.api.model.GeneratedBlueprintResponse;
+import com.sequenceiq.cloudbreak.api.model.stack.StackImageChangeRequest;
 import com.sequenceiq.cloudbreak.api.model.stack.StackResponse;
+import com.sequenceiq.cloudbreak.api.model.stack.StackScaleRequestV2;
+import com.sequenceiq.cloudbreak.api.model.stack.cluster.ClusterRepairRequest;
 import com.sequenceiq.cloudbreak.api.model.v2.StackV2Request;
 import com.sequenceiq.cloudbreak.doc.ContentType;
 import com.sequenceiq.cloudbreak.doc.ControllerDescription;
@@ -48,11 +56,109 @@ public interface StackV3Endpoint {
             nickname = "createStackInOrganization")
     StackResponse createInOrganization(@PathParam("organizationId") Long organizationId, @Valid StackV2Request request);
 
+    // deleteStackV2
     @DELETE
     @Path("{name}")
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = StackOpDescription.DELETE_BY_NAME_IN_ORG, produces = ContentType.JSON, notes = Notes.PROXY_CONFIG_NOTES,
             nickname = "deleteStackInOrganization")
-    StackResponse deleteInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+    StackResponse deleteInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @QueryParam("forced") @DefaultValue("false") Boolean forced,
+            @QueryParam("deleteDependencies") @DefaultValue("false") Boolean deleteDependencies);
 
+    // putsyncStackV2
+    @PUT
+    @Path("{name}/sync")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.SYNC_BY_NAME_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "putsyncStackV3")
+    StackResponse putSyncInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+
+    // retryStack
+    @POST
+    @Path("{name}/retry")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.RETRY_BY_NAME_IN_ORG,
+            produces = ContentType.JSON, notes = Notes.RETRY_STACK_NOTES, nickname = "retryStackV3")
+    void retryInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+
+    // putstopStackV2
+    @PUT
+    @Path("{name}/stop")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.STOP_BY_NAME_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "putstopStackV3")
+    StackResponse putStopInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+
+    // putstartStackV2
+    @PUT
+    @Path("{name}/start")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.START_BY_NAME_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "putstartStackV3")
+    StackResponse putStartInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+
+    // putscalingStackV2
+    @PUT
+    @Path("{name}/scaling")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.SCALE_BY_NAME_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "putscalingStackV3")
+    StackResponse putScalingInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @Valid StackScaleRequestV2 updateRequest);
+
+    // repairCluster // v1
+    @POST
+    @Path("{name}/manualrepair")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.REPAIR_CLUSTER_IN_ORG, produces = ContentType.JSON, notes = Notes.CLUSTER_REPAIR_NOTES,
+            nickname = "repairClusterV3")
+    StackResponse repairClusterInOrganization(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            ClusterRepairRequest clusterRepairRequest);
+
+    // deleteCluster // v1
+    @DELETE
+    @Path("{name}/cluster")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.DELETE_WITH_KERBEROS_IN_ORG, produces = ContentType.JSON, notes = Notes.CLUSTER_NOTES,
+            nickname = "deleteClusterWithKerberosV3")
+    void deleteWithKerberosInOrg(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @QueryParam("withStackDelete") @DefaultValue("false") Boolean withStackDelete,
+            @QueryParam("deleteDependencies") @DefaultValue("false") Boolean deleteDependencies);
+
+    // getClusterRequestFromName
+    @GET
+    @Path("{name}/request")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.GET_STACK_REQUEST_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "getStackRequestFromNameV3")
+    StackV2Request getRequestfromName(@PathParam("organizationId") Long organizationId, @PathParam("name") String name);
+
+    // postPublicStackV2ForBlueprint
+    @POST
+    @Path("{name}/blueprint")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.POST_STACK_FOR_BLUEPRINT_IN_ORG, produces = ContentType.JSON,
+            notes = Notes.STACK_NOTES, nickname = "postStackForBlueprintV3")
+    GeneratedBlueprintResponse postStackForBlueprint(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @Valid StackV2Request stackRequest) throws Exception;
+
+    // deleteInstanceStackV2
+    @DELETE
+    @Path("{name}/instance/{instanceId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.DELETE_INSTANCE_BY_ID_IN_ORG, produces = ContentType.JSON, notes = Notes.STACK_NOTES,
+            nickname = "deleteInstanceStackV3")
+    StackResponse deleteInstance(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @PathParam("stackId") Long stackId, @PathParam("instanceId") String instanceId,
+            @QueryParam("forced") @DefaultValue("false") boolean forced);
+
+    // changeImage
+    @PUT
+    @Path("{name}/changeImage")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = StackOpDescription.CHECK_IMAGE_IN_ORG, produces = ContentType.JSON,
+            notes = Notes.STACK_NOTES, nickname = "changeImageV3")
+    Response changeImage(@PathParam("organizationId") Long organizationId, @PathParam("name") String name,
+            @Valid StackImageChangeRequest stackImageChangeRequest);
 }
--- core/src/main/java/com/sequenceiq/cloudbreak/controller/StackV3Controller.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/StackV3Controller.java
@@ -4,11 +4,17 @@ import java.util.Set;
 
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
+import javax.validation.Valid;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v3.StackV3Endpoint;
+import com.sequenceiq.cloudbreak.api.model.GeneratedBlueprintResponse;
+import com.sequenceiq.cloudbreak.api.model.stack.StackImageChangeRequest;
 import com.sequenceiq.cloudbreak.api.model.stack.StackResponse;
+import com.sequenceiq.cloudbreak.api.model.stack.StackScaleRequestV2;
+import com.sequenceiq.cloudbreak.api.model.stack.cluster.ClusterRepairRequest;
 import com.sequenceiq.cloudbreak.api.model.v2.StackV2Request;
 
 @Component
@@ -31,7 +37,62 @@ public class StackV3Controller extends NotificationController implements StackV3
     }
 
     @Override
-    public StackResponse deleteInOrganization(Long organizationId, String name) {
+    public StackResponse deleteInOrganization(Long organizationId, String name, Boolean forced, Boolean deleteDependencies) {
+        return null;
+    }
+
+    @Override
+    public StackResponse putSyncInOrganization(Long organizationId, String name) {
+        return null;
+    }
+
+    @Override
+    public void retryInOrganization(Long organizationId, String name) {
+
+    }
+
+    @Override
+    public StackResponse putStopInOrganization(Long organizationId, String name) {
+        return null;
+    }
+
+    @Override
+    public StackResponse putStartInOrganization(Long organizationId, String name) {
+        return null;
+    }
+
+    @Override
+    public StackResponse putScalingInOrganization(Long organizationId, String name, @Valid StackScaleRequestV2 updateRequest) {
+        return null;
+    }
+
+    @Override
+    public StackResponse repairClusterInOrganization(Long organizationId, String name, ClusterRepairRequest clusterRepairRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteWithKerberosInOrg(Long organizationId, String name, Boolean withStackDelete, Boolean deleteDependencies) {
+
+    }
+
+    @Override
+    public StackV2Request getRequestfromName(Long organizationId, String name) {
+        return null;
+    }
+
+    @Override
+    public GeneratedBlueprintResponse postStackForBlueprint(Long organizationId, String name, @Valid StackV2Request stackRequest) {
+        return null;
+    }
+
+    @Override
+    public StackResponse deleteInstance(Long organizationId, String name, Long stackId, String instanceId, boolean forced) {
+        return null;
+    }
+
+    @Override
+    public Response changeImage(Long organizationId, String name, @Valid StackImageChangeRequest stackImageChangeRequest) {
         return null;
     }
 }
