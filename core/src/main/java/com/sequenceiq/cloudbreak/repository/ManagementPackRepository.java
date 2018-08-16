681b768311b8ffc5d10121ee43a370471fc64a94
Gergely Topolyai <gtopolyai@hortonworks.com>
2018-08-16 17:09:41 +0200

    BUG-109340 V1 to V3, Management packs

--- core/src/main/java/com/sequenceiq/cloudbreak/repository/ManagementPackRepository.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/repository/ManagementPackRepository.java
@@ -1,12 +1,7 @@
 package com.sequenceiq.cloudbreak.repository;
 
-import java.util.Set;
-
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.sequenceiq.cloudbreak.aspect.DisableHasPermission;
 import com.sequenceiq.cloudbreak.aspect.organization.OrganizationResourceType;
@@ -20,21 +15,4 @@ import com.sequenceiq.cloudbreak.service.EntityType;
 @OrganizationResourceType(resource = OrganizationResource.MPACK)
 public interface ManagementPackRepository extends OrganizationResourceRepository<ManagementPack, Long> {
 
-    ManagementPack findOneById(Long id);
-
-    Set<ManagementPack> findByOwner(String owner);
-
-    Set<ManagementPack> findByAccount(String account);
-
-    ManagementPack findOneByNameAndOwner(String name, String owner);
-
-    @Query("SELECT m FROM ManagementPack m WHERE  m.name= :name and ((m.publicInAccount=true and m.account= :account) or m.owner= :owner)")
-    ManagementPack findOneByNameBasedOnAccount(@Param("name") String name, @Param("account") String account, @Param("owner") String owner);
-
-    ManagementPack findOneByNameAndAccount(String name, String account);
-
-    ManagementPack findOneByIdAndAccount(Long id, String account);
-
-    @Query("SELECT m FROM ManagementPack m WHERE ((m.account= :account AND m.publicInAccount= true) OR m.owner= :user)")
-    Set<ManagementPack> findPublicInAccountForUser(@Param("user") String user, @Param("account") String account);
 }
