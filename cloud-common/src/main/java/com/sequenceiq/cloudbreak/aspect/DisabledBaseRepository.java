a8ef1b8b46af3f0de95d295b04e77f5eda0c117a
Gergely Topolyai <gtopolyai@hortonworks.com>
2018-09-04 08:35:18 +0200

    BUG-110299 other user than creator in the same org cannot modify the cluster who has all the permission

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/aspect/DisabledBaseRepository.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/aspect/DisabledBaseRepository.java
@@ -1,45 +1,64 @@
 package com.sequenceiq.cloudbreak.aspect;
 
+import static com.sequenceiq.cloudbreak.authorization.OrganizationPermissions.Action.READ;
+import static com.sequenceiq.cloudbreak.authorization.OrganizationPermissions.Action.WRITE;
+
 import java.io.Serializable;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import com.sequenceiq.cloudbreak.aspect.organization.CheckPermissionsByReturnValue;
+import com.sequenceiq.cloudbreak.aspect.organization.CheckPermissionsByTarget;
+import com.sequenceiq.cloudbreak.aspect.organization.CheckPermissionsByTargetId;
+import com.sequenceiq.cloudbreak.aspect.organization.DisableCheckPermissions;
+
 @NoRepositoryBean
 @DisableHasPermission
 public interface DisabledBaseRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
 
     @Override
+    @CheckPermissionsByTarget(action = WRITE, targetIndex = 0)
     <S extends T> S save(S entity);
 
     @Override
+    @CheckPermissionsByTarget(action = WRITE, targetIndex = 0)
     <S extends T> Iterable<S> saveAll(Iterable<S> entities);
 
     @Override
+    @CheckPermissionsByReturnValue(action = READ)
     Optional<T> findById(ID id);
 
     @Override
+    @CheckPermissionsByReturnValue(action = READ)
     boolean existsById(ID id);
 
     @Override
+    @CheckPermissionsByReturnValue(action = READ)
     Iterable<T> findAll();
 
     @Override
+    @CheckPermissionsByReturnValue(action = READ)
     Iterable<T> findAllById(Iterable<ID> ids);
 
     @Override
+    @DisableCheckPermissions
     long count();
 
     @Override
+    @CheckPermissionsByTargetId(action = READ)
     void deleteById(ID id);
 
     @Override
+    @CheckPermissionsByTarget(action = WRITE, targetIndex = 0)
     void delete(T entity);
 
     @Override
+    @CheckPermissionsByTarget(action = WRITE, targetIndex = 0)
     void deleteAll(Iterable<? extends T> entities);
 
     @Override
+    @DisableCheckPermissions
     void deleteAll();
 }
