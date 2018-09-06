b16f5d60b5962e5dc3413562b110691b5f430f2e
Richard Kovacs <kovacsricsi@gmail.com>
2018-09-06 11:57:56 +0200

    BUG-87983 Make performance test great again

--- performance-test/perftest.sh
+++ performance-test/perftest.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 : ${CB_PERFTEST_HOST:=192.168.99.100}
+: ${CB_MOCK_HOST:="mockhosts.service.consul"}
 : ${CB_NUMBER_OF_USERS:=1}
 : ${CB_RAMPUP_SECONDS:=3}
 : ${CB_DELAY_BEFORE_TERM:=60}
+
+echo "[!!] Make sure you spinned up the mock server: CB_SERVER_ADDRESS=$CB_PERFTEST_HOST MOCK_SERVER_ADDRESS=$CB_MOCK_HOST gradle :integration-test:runMockServer"
+echo "[!!] $CB_MOCK_HOST must be resolvable from the running Cloudbreak backend"
+echo "[!!] User admin@example.com must exists in uaa database"
 
 docker run -it --rm \
 -v `pwd`/conf:/opt/gatling/conf \
 -v `pwd`/user-files:/opt/gatling/user-files \
 -v `pwd`/results:/opt/gatling/results \
 -e CB_PERFTEST_HOST=$CB_PERFTEST_HOST \
+-e CB_MOCK_HOST=$CB_MOCK_HOST \
 -e CB_NUMBER_OF_USERS=$CB_NUMBER_OF_USERS \
 -e CB_RAMPUP_SECONDS=$CB_RAMPUP_SECONDS \
 -e CB_DELAY_BEFORE_TERM=$CB_DELAY_BEFORE_TERM \
