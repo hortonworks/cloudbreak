f32512aedbc39e4974c4027dde47f06541565213
Richard Kovacs <kovacsricsi@gmail.com>
2018-09-06 15:58:28 +0200

    BUG-109924 Add max depth to user cache

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/CloudResourceRegionCache.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/CloudResourceRegionCache.java
@@ -6,12 +6,8 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.sequenceiq.cloudbreak.cache.CacheDefinition;
-
-import net.sf.ehcache.config.CacheConfiguration;
-
 @Service
-public class CloudResourceRegionCache implements CacheDefinition {
+public class CloudResourceRegionCache extends AbstractCacheDefinition {
 
     private static final long MAX_ENTRIES = 1000L;
 
@@ -19,12 +15,27 @@ public class CloudResourceRegionCache implements CacheDefinition {
     private long ttlMinutes;
 
     @Override
-    public CacheConfiguration cacheConfiguration() {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration();
-        cacheConfiguration.setName("cloudResourceRegionCache");
-        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
-        cacheConfiguration.setMaxEntriesLocalHeap(MAX_ENTRIES);
-        cacheConfiguration.setTimeToLiveSeconds(ttlMinutes == 0L ? 1 : TimeUnit.MINUTES.toSeconds(ttlMinutes));
-        return cacheConfiguration;
+    protected String getName() {
+        return "cloudResourceRegionCache";
+    }
+
+    @Override
+    protected String getMemoryStoreEvictionPolicy() {
+        return "LRU";
+    }
+
+    @Override
+    protected long getMaxEntriesLocalHeap() {
+        return MAX_ENTRIES;
+    }
+
+    @Override
+    protected long getMaxBytesLocalHeap() {
+        return 0L;
+    }
+
+    @Override
+    protected long getTimeToLiveSeconds() {
+        return ttlMinutes == 0L ? 1 : TimeUnit.MINUTES.toSeconds(ttlMinutes);
     }
 }
--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/UserCache.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/cache/common/UserCache.java
@@ -1,26 +1,43 @@
 package com.sequenceiq.cloudbreak.cache.common;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
 
-import com.sequenceiq.cloudbreak.cache.CacheDefinition;
-
-import net.sf.ehcache.config.CacheConfiguration;
-
 @Service
-public class UserCache implements CacheDefinition {
+public class UserCache extends AbstractCacheDefinition {
 
-    private static final long MAX_ENTRIES = 1000L;
+    private static final long MAX_BYTES =  10 * 1024L;
 
     private static final long TTL_IN_SECONDS = 5L * 60;
 
-    @Override
-    public CacheConfiguration cacheConfiguration() {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration();
-        cacheConfiguration.setName("userCache");
-        cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
-        cacheConfiguration.setMaxEntriesLocalHeap(MAX_ENTRIES);
-        cacheConfiguration.setTimeToLiveSeconds(TTL_IN_SECONDS);
-        return cacheConfiguration;
+    @PostConstruct
+    public void init() {
+        System.setProperty("net.sf.ehcache.sizeofengine.default.userCache", UserCacheSizeOfEngine.class.getCanonicalName());
     }
 
+    @Override
+    protected String getName() {
+        return "userCache";
+    }
+
+    @Override
+    protected String getMemoryStoreEvictionPolicy() {
+        return "LRU";
+    }
+
+    @Override
+    protected long getMaxEntriesLocalHeap() {
+        return 0L;
+    }
+
+    @Override
+    protected long getMaxBytesLocalHeap() {
+        return MAX_BYTES;
+    }
+
+    @Override
+    protected long getTimeToLiveSeconds() {
+        return TTL_IN_SECONDS;
+    }
 }
