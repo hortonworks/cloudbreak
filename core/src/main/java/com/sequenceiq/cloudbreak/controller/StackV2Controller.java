514f79e45601ccda4d28c2a98523b93e8d663433
Attila Palfi <apalfi@hortonworks.com>
2018-09-13 15:55:51 +0200

    throwing not found when stack is not found

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/StackV2Controller.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/StackV2Controller.java
@@ -28,17 +28,17 @@ import com.sequenceiq.cloudbreak.api.model.stack.cluster.ClusterRepairRequest;
 import com.sequenceiq.cloudbreak.api.model.users.UserNamePasswordJson;
 import com.sequenceiq.cloudbreak.api.model.v2.StackV2Request;
 import com.sequenceiq.cloudbreak.common.model.user.IdentityUser;
-import com.sequenceiq.cloudbreak.domain.workspace.Workspace;
-import com.sequenceiq.cloudbreak.domain.workspace.User;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.domain.workspace.User;
+import com.sequenceiq.cloudbreak.domain.workspace.Workspace;
 import com.sequenceiq.cloudbreak.service.ClusterCommonService;
 import com.sequenceiq.cloudbreak.service.RestRequestThreadLocalService;
 import com.sequenceiq.cloudbreak.service.StackCommonService;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterService;
-import com.sequenceiq.cloudbreak.service.workspace.WorkspaceService;
 import com.sequenceiq.cloudbreak.service.stack.CloudParameterCache;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.cloudbreak.service.user.UserService;
+import com.sequenceiq.cloudbreak.service.workspace.WorkspaceService;
 
 @Component
 @Transactional(TxType.NEVER)
@@ -194,7 +194,7 @@ public class StackV2Controller extends NotificationController implements StackV2
     public StackV2Request getRequestfromName(String name) {
         User user = userService.getOrCreate(restRequestThreadLocalService.getIdentityUser());
         Workspace workspace = workspaceService.get(restRequestThreadLocalService.getRequestedWorkspaceId(), user);
-        return stackService.getStackRequestByNameInDefaultWorkspaceForUser(name, user, workspace);
+        return stackService.getStackRequestByNameInWorkspace(name, workspace);
     }
 
     @Override
