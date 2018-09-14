300022a4a249ca414c6ca2502b67aeb0d1a9db83
Ferenc Schneider <fschneider@hortonworks.com>
2018-09-14 08:27:00 +0200

    BUG-111030 Handle forbidden exception during e2e test cleanup

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/CloudbreakClusterTestConfiguration.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/CloudbreakClusterTestConfiguration.java
@@ -2,9 +2,13 @@ package com.sequenceiq.it.cloudbreak.newway;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.ws.rs.ForbiddenException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
+
+import com.sequenceiq.cloudbreak.api.model.v2.StackV2Request;
 
 public class CloudbreakClusterTestConfiguration extends CloudbreakTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(CloudbreakClusterTestConfiguration.class);
@@ -28,10 +32,19 @@ public class CloudbreakClusterTestConfiguration extends CloudbreakTest {
         int retryCount = 0;
         Long workspaceId = getItContext().getContextParam(CloudbreakTest.WORKSPACE_ID, Long.class);
 
-        while (CloudbreakClient.getTestContextCloudbreakClient().apply(getItContext()).getCloudbreakClient()
-                .stackV3Endpoint().getRequestfromName(workspaceId, clusterName) != null
-                && retryCount++ < MAX_RETRY) {
+        while (getRequestFromName(workspaceId, clusterName) != null && retryCount++ < MAX_RETRY) {
             TimeUnit.SECONDS.sleep(SLEEP_TIME);
         }
+    }
+
+    private StackV2Request getRequestFromName(Long workspaceId, String clusterName) {
+        StackV2Request result;
+        try {
+            result = CloudbreakClient.getTestContextCloudbreakClient().apply(getItContext()).getCloudbreakClient()
+                    .stackV3Endpoint().getRequestfromName(workspaceId, clusterName);
+        } catch (ForbiddenException ex) {
+            result = null;
+        }
+        return result;
     }
 }
