68c61d24c7436bd3d03ddff4ef1b89925f489dd8
Yolanda Davis <yolanda.m.davis@gmail.com>
2018-09-27 06:04:53 -0400

    RMP-11186 - add new HDF Messaging blueprint that includes Kafka, and Schema Registry

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/rds/RdsType.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/rds/RdsType.java
@@ -2,5 +2,5 @@ package com.sequenceiq.cloudbreak.api.model.rds;
 
 public enum RdsType {
 
-    HIVE, RANGER, DRUID, SUPERSET, OOZIE, AMBARI, BEACON
+    HIVE, RANGER, DRUID, SUPERSET, OOZIE, AMBARI, BEACON, REGISTRY
 }
