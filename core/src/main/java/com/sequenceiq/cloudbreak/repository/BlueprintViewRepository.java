0eba3c3e5869f63c57e4666b51ab946d55c746a5
Krisztian Horvath <keyki.kk@gmail.com>
2018-10-08 14:12:16 +0200

    BUG-110897 do not return deleted blueprints

--- core/src/main/java/com/sequenceiq/cloudbreak/repository/BlueprintViewRepository.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/repository/BlueprintViewRepository.java
@@ -1,9 +1,15 @@
 package com.sequenceiq.cloudbreak.repository;
 
+import java.util.Set;
+
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.sequenceiq.cloudbreak.aspect.DisableHasPermission;
+import com.sequenceiq.cloudbreak.aspect.workspace.CheckPermissionsByReturnValue;
 import com.sequenceiq.cloudbreak.aspect.workspace.WorkspaceResourceType;
 import com.sequenceiq.cloudbreak.authorization.WorkspaceResource;
 import com.sequenceiq.cloudbreak.domain.view.BlueprintView;
@@ -15,4 +21,8 @@ import com.sequenceiq.cloudbreak.service.EntityType;
 @Transactional(TxType.REQUIRED)
 @WorkspaceResourceType(resource = WorkspaceResource.BLUEPRINT)
 public interface BlueprintViewRepository extends WorkspaceResourceRepository<BlueprintView, Long> {
+
+    @Query("SELECT b FROM BlueprintView b WHERE b.workspace.id= :workspaceId AND b.status <> 'DEFAULT_DELETED'")
+    @CheckPermissionsByReturnValue
+    Set<BlueprintView> findAllByNotDeletedInWorkspace(@Param("workspaceId") Long workspaceId);
 }
