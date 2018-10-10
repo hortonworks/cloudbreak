8dce7fc65755e92ed0bd9cbf1cc706cc7bfe32bf
bbihari <bbihari@hortonworks.com>
2018-10-10 14:40:58 +0200

    BUG-112047 The show blueprint and show cli command could display sensitive information among the custom properties

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/AccountPreferencesResponse.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/AccountPreferencesResponse.java
@@ -10,11 +10,21 @@ public class AccountPreferencesResponse extends AccountPreferencesBase {
 
     private Set<SupportedExternalDatabaseServiceEntryResponse> supportedExternalDatabases = new HashSet<>();
 
+    private Set<FeatureSwitch> featureSwitches = new HashSet<>();
+
     public Set<SupportedExternalDatabaseServiceEntryResponse> getSupportedExternalDatabases() {
         return supportedExternalDatabases;
     }
 
     public void setSupportedExternalDatabases(Set<SupportedExternalDatabaseServiceEntryResponse> supportedExternalDatabases) {
         this.supportedExternalDatabases = supportedExternalDatabases;
+    }
+
+    public Set<FeatureSwitch> getFeatureSwitches() {
+        return featureSwitches;
+    }
+
+    public void setFeatureSwitches(Set<FeatureSwitch> featureSwitches) {
+        this.featureSwitches = featureSwitches;
     }
 }
