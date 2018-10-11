05abc4dcbb8adf38923a928cfe40403172cefe6d
Attila Palfi <apalfi@hortonworks.com>
2018-10-11 16:55:46 +0200

    BUG-112149 - environment GET and LIST finished

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/EnvironmentV3Controller.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/EnvironmentV3Controller.java
@@ -1,12 +1,15 @@
 package com.sequenceiq.cloudbreak.controller;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 import javax.validation.Valid;
 
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v3.EnvironmentV3Endpoint;
@@ -35,22 +38,25 @@ public class EnvironmentV3Controller implements EnvironmentV3Endpoint {
     @Inject
     private WorkspaceService workspaceService;
 
+    @Inject
+    @Named("conversionService")
+    private ConversionService conversionService;
+
     @Override
     public SimpleEnvironmentResponse create(Long workspaceId, @Valid EnvironmentRequest request) {
         return environmentService.createForLoggedInUser(request, workspaceId);
     }
 
-    // TODO: finish
-    @Override
-    public SimpleEnvironmentResponse get(Long workspaceId, String environmentName) {
-
-        return null;
-    }
-
-    // TODO: finish
     @Override
     public Set<SimpleEnvironmentResponse> list(Long workspaceId) {
-        return null;
+        return environmentService.findAllByWorkspaceId(workspaceId).stream()
+                .map(env -> conversionService.convert(env, SimpleEnvironmentResponse.class))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public SimpleEnvironmentResponse get(Long workspaceId, String environmentName) {
+        return conversionService.convert(environmentService.getByNameForWorkspaceId(environmentName, workspaceId), SimpleEnvironmentResponse.class);
     }
 
     // TODO: finish
