669a779bbf9738a4173a6a1c516fc1c48d00028d
Richard Doktorics <rdoktorics@hortonworks.com>
2018-10-15 16:47:31 +0200

    BUG-112147 added id to cluster template response

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/template/ClusterTemplateResponse.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/model/template/ClusterTemplateResponse.java
@@ -3,13 +3,19 @@ package com.sequenceiq.cloudbreak.api.model.template;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sequenceiq.cloudbreak.api.model.ResourceStatus;
+import com.sequenceiq.cloudbreak.doc.ModelDescriptions;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ClusterTemplateResponse extends ClusterTemplateBase {
+
+    @ApiModelProperty(ModelDescriptions.ID)
+    private Long id;
+
     private ResourceStatus status;
 
     public ResourceStatus getStatus() {
@@ -18,5 +24,13 @@ public class ClusterTemplateResponse extends ClusterTemplateBase {
 
     public void setStatus(ResourceStatus status) {
         this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
