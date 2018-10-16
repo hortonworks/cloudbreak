a14eabb31ca0f1ca2ef60f0d1cf986adde0f2bd5
Richard Doktorics <rdoktorics@hortonworks.com>
2018-10-16 09:07:04 +0200

    BUG-112147 added id to cluster template response

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/ClusterTemplateV3Controller.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/ClusterTemplateV3Controller.java
@@ -9,15 +9,21 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v3.ClusterTemplateV3EndPoint;
+import com.sequenceiq.cloudbreak.api.model.stack.StackResponse;
 import com.sequenceiq.cloudbreak.api.model.template.ClusterTemplateRequest;
 import com.sequenceiq.cloudbreak.api.model.template.ClusterTemplateResponse;
+import com.sequenceiq.cloudbreak.api.model.v2.StackFromTemplateRequest;
+import com.sequenceiq.cloudbreak.common.model.user.CloudbreakUser;
 import com.sequenceiq.cloudbreak.controller.validation.credential.CredentialValidator;
 import com.sequenceiq.cloudbreak.converter.mapper.ClusterTemplateMapper;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.ClusterTemplate;
 import com.sequenceiq.cloudbreak.domain.workspace.User;
+import com.sequenceiq.cloudbreak.domain.workspace.Workspace;
 import com.sequenceiq.cloudbreak.service.RestRequestThreadLocalService;
+import com.sequenceiq.cloudbreak.service.StackCommonService;
 import com.sequenceiq.cloudbreak.service.template.ClusterTemplateService;
 import com.sequenceiq.cloudbreak.service.user.UserService;
+import com.sequenceiq.cloudbreak.service.workspace.WorkspaceService;
 import com.sequenceiq.cloudbreak.util.WorkspaceEntityType;
 
 @Controller
@@ -30,6 +36,12 @@ public class ClusterTemplateV3Controller extends NotificationController implemen
 
     @Inject
     private UserService userService;
+
+    @Inject
+    private WorkspaceService workspaceService;
+
+    @Inject
+    private StackCommonService stackCommonService;
 
     @Inject
     private RestRequestThreadLocalService restRequestThreadLocalService;
@@ -47,6 +59,14 @@ public class ClusterTemplateV3Controller extends NotificationController implemen
         User user = userService.getOrCreate(restRequestThreadLocalService.getCloudbreakUser());
         clusterTemplate = clusterTemplateService.create(clusterTemplate, workspaceId, user);
         return clusterTemplateMapper.mapEntityToResponse(clusterTemplate);
+    }
+
+    @Override
+    public StackResponse createStackInWorkspaceFromTemplate(Long workspaceId, String name, @Valid StackFromTemplateRequest request) {
+        CloudbreakUser cloudbreakUser = restRequestThreadLocalService.getCloudbreakUser();
+        User user = userService.getOrCreate(cloudbreakUser);
+        Workspace workspace = workspaceService.get(workspaceId, user);
+        return stackCommonService.createInWorkspaceFromTemplate(name, request, cloudbreakUser, user, workspace);
     }
 
     @Override
