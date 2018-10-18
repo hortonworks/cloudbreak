551abebe3bb41a3178c618d0baee7d341f79a9a5
Ferenc Schneider <fschneider@hortonworks.com>
2018-10-18 13:18:46 +0200

    BUG-112656 list environment resources filtered by environment

--- gradle/config/buildConfig.groovy
+++ gradle/config/buildConfig.groovy
@@ -1,14 +1,14 @@
 environments {
   jenkins {
+    metadata = "$System.env.NEXUS_URL/com/sequenceiq/cloudbreak/maven-metadata.xml"
     sonar_host_url = "http://$System.env.SONAR_HOST:9000"
-    metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
     sonar_host_user = "$System.env.SONAR_USER"
     sonar_host_password = "$System.env.SONAR_PASSWORD"
   }
 
   local {
+    metadata = "$System.env.NEXUS_URL/com/sequenceiq/cloudbreak/maven-metadata.xml"
     sonar_host_url = "http://localhost:9000"
-    metadata = "https://s3-eu-west-1.amazonaws.com/maven.sequenceiq.com/releases/com/sequenceiq/cloudbreak/maven-metadata.xml"
     sonar_host_user = "$System.env.SONAR_USER"
     sonar_host_password = "$System.env.SONAR_PASSWORD"
   }
