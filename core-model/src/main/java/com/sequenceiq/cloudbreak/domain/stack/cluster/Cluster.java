cc679799876d3dbd6ed34a801cf3b30fa8496d92
lnardai <laci.nardai@gmail.com>
2018-10-19 16:34:13 +0200

    Added VaultValue annotation and Aspect for writing in to the Vault database. Many mocked data.

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/Cluster.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/Cluster.java
@@ -40,6 +40,8 @@ import javax.persistence.UniqueConstraint;
 import com.sequenceiq.cloudbreak.api.model.ConfigStrategy;
 import com.sequenceiq.cloudbreak.api.model.ExecutorType;
 import com.sequenceiq.cloudbreak.api.model.Status;
+import com.sequenceiq.cloudbreak.aspect.vault.VaultIdentifier;
+import com.sequenceiq.cloudbreak.aspect.vault.VaultValue;
 import com.sequenceiq.cloudbreak.authorization.WorkspaceResource;
 import com.sequenceiq.cloudbreak.domain.Blueprint;
 import com.sequenceiq.cloudbreak.domain.Container;
@@ -63,7 +65,7 @@ import com.sequenceiq.cloudbreak.type.KerberosType;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"workspace_id", "name"}))
-public class Cluster implements ProvisionEntity, WorkspaceAwareResource {
+public class Cluster implements ProvisionEntity, WorkspaceAwareResource, VaultIdentifier {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "cluster_generator")
@@ -112,9 +114,10 @@ public class Cluster implements ProvisionEntity, WorkspaceAwareResource {
     @Column(nullable = false)
     private String password;
 
+    @VaultValue
     private String cloudbreakAmbariUser;
 
-    @Convert(converter = EncryptionConverter.class)
+    @VaultValue
     private String cloudbreakAmbariPassword;
 
     @Column(nullable = false)
@@ -165,7 +168,7 @@ public class Cluster implements ProvisionEntity, WorkspaceAwareResource {
 
     private String uptime;
 
-    @Convert(converter = EncryptionConverter.class)
+    @VaultValue
     private String ambariSecurityMasterKey;
 
     @ManyToOne
@@ -449,6 +452,10 @@ public class Cluster implements ProvisionEntity, WorkspaceAwareResource {
         return cloudbreakAmbariUser;
     }
 
+    public String getOriginalCloudbreakAmbariUser() {
+        return cloudbreakAmbariUser;
+    }
+
     public void setCloudbreakAmbariUser(String cloudbreakAmbariUser) {
         this.cloudbreakAmbariUser = cloudbreakAmbariUser;
     }
@@ -567,5 +574,10 @@ public class Cluster implements ProvisionEntity, WorkspaceAwareResource {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String getUniqueIdentifier() {
+        return getStack().getUuid();
     }
 }
