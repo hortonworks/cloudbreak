dd87f5e372e0f0c064f78062ca94558c0062b271
Krisztian Horvath <keyki.kk@gmail.com>
2018-10-25 19:21:49 +0200

    include attribute name into the vault path

--- core/src/main/java/com/sequenceiq/cloudbreak/service/vault/VaultService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/vault/VaultService.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.core.VaultTemplate;
@@ -13,23 +15,30 @@ import org.springframework.vault.support.VaultResponse;
 @Service
 public class VaultService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(VaultService.class);
+
     @Inject
     private VaultOperations operations;
 
     @Inject
     private VaultTemplate template;
 
-    public String addFieldToSecret(String path, String value) throws Exception {
+    public void addFieldToSecret(String path, String value) throws Exception {
+        long start = System.currentTimeMillis();
         VaultResponse response = operations.read(path);
+        LOGGER.debug("Vault read took {} ms", System.currentTimeMillis() - start);
         if (response != null && response.getData() != null) {
             throw new InvalidKeyException(String.format("Path: %s already exists!", path));
         }
+        start = System.currentTimeMillis();
         operations.write(path, Collections.singletonMap("secret", value));
-        return path;
+        LOGGER.debug("Vault write took {} ms", System.currentTimeMillis() - start);
     }
 
     public String resolveSingleValue(String path) {
+        long start = System.currentTimeMillis();
         VaultResponse response = template.read(path);
+        LOGGER.debug("Vault read took {} ms", System.currentTimeMillis() - start);
         if (response != null && response.getData() != null) {
             return String.valueOf(response.getData().get("secret"));
         }
@@ -37,7 +46,9 @@ public class VaultService {
     }
 
     public void deleteSecret(String path) {
+        long start = System.currentTimeMillis();
         operations.delete(path);
+        LOGGER.debug("Vault delete took {} ms", System.currentTimeMillis() - start);
     }
 
 }
