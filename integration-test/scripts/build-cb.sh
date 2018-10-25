ae62fab2fce044acb97e6dd332e23f4bf895e23a
Richard Doktorics <rdoktorics@hortonworks.com>
2018-10-25 14:45:43 +0200

    nexus added

--- integration-test/scripts/build-cb.sh
+++ integration-test/scripts/build-cb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo -e "\n\033[1;96m--- build cloudbreak\033[0m\n"
-../gradlew -p ../ clean build -x test
+../gradlew -p ../ build -x test -x checkstyleMain -x checkstyleTest -x spotbugsMain -x spotbugsTest 
