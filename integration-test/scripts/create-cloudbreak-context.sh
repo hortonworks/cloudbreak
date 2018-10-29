4aaaf6684b182c7c1a993ff7dd005fddbe2fbbcb
Krisztian Horvath <keyki.kk@gmail.com>
2018-10-29 08:43:53 +0100

    include vault into the integration test

--- integration-test/scripts/create-cloudbreak-context.sh
+++ integration-test/scripts/create-cloudbreak-context.sh
@@ -8,4 +8,6 @@ cp docker-compose_template.yml docker-compose.yml
 ip_address=$(docker run --label cbreak.sidekick=true alpine:3.7 sh -c 'ip ro | grep default | cut -d" " -f 3')
 sed -i.bak "s/dns:/dns: $ip_address/g" docker-compose.yml
 ./scripts/fill_public_ip.sh
+echo -e "\n" >> integcb/Profile
+echo "export VAULT_AUTO_UNSEAL=true" >> integcb/Profile
 cd $INTEGCB_LOCATION
