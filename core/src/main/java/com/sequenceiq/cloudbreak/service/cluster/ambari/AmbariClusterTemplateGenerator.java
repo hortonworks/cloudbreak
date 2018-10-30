7a3775d3565a3a289e060d59f188de838fc7c161
Krisztian Horvath <keyki.kk@gmail.com>
2018-10-30 16:39:25 +0100

    fix kerberos related issues

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/ambari/AmbariClusterTemplateGenerator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/ambari/AmbariClusterTemplateGenerator.java
@@ -12,6 +12,7 @@ import com.sequenceiq.cloudbreak.blueprint.kerberos.KerberosDetailService;
 import com.sequenceiq.cloudbreak.domain.KerberosConfig;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.service.CloudbreakException;
+import com.sequenceiq.cloudbreak.service.VaultService;
 
 @Service
 public class AmbariClusterTemplateGenerator {
@@ -27,6 +28,9 @@ public class AmbariClusterTemplateGenerator {
     @Inject
     private AmbariRepositoryVersionService ambariRepositoryVersionService;
 
+    @Inject
+    private VaultService vaultService;
+
     String generateClusterTemplate(Cluster cluster, Map<String, List<Map<String, String>>> hostGroupMappings,
             ClusterService ambariClient) throws CloudbreakException {
         String blueprintName = cluster.getBlueprint().getAmbariName();
@@ -39,7 +43,7 @@ public class AmbariClusterTemplateGenerator {
             String principal = kerberosDetailService.resolvePrincipalForKerberos(kerberosConfig);
             clusterTemplate = ambariClient.createClusterJson(blueprintName, hostGroupMappings,
                     ambariSecurityConfigProvider.getAmbariUserProvidedPassword(cluster), configStrategy,
-                    principal, kerberosConfig.getPassword(), KEY_TYPE, false, repositoryVersion);
+                    principal, vaultService.resolveSingleValue(kerberosConfig.getPassword()), KEY_TYPE, false, repositoryVersion);
         } else {
             clusterTemplate = ambariClient.createClusterJson(blueprintName, hostGroupMappings,
                     ambariSecurityConfigProvider.getAmbariUserProvidedPassword(cluster), configStrategy,
