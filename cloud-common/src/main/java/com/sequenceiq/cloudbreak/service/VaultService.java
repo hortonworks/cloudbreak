f746ae111c86d088d86dc7bc201236b7e3c4238e
Krisztian Horvath <keyki.kk@gmail.com>
2018-10-31 17:28:02 +0100

    Revert "fix recipe vault resolution issue"
    This reverts commit ba1ba1431c5c0532c39009269279e45a600b303b.

--- cloud-common/src/main/java/com/sequenceiq/cloudbreak/service/VaultService.java
+++ cloud-common/src/main/java/com/sequenceiq/cloudbreak/service/VaultService.java
@@ -39,17 +39,11 @@ public class VaultService {
         if (path == null) {
             return null;
         }
-        try {
-            long start = System.currentTimeMillis();
-            VaultResponse response = template.read(path);
-            LOGGER.debug("Vault read took {} ms", System.currentTimeMillis() - start);
-            if (response != null && response.getData() != null) {
-                return String.valueOf(response.getData().get("secret"));
-            }
-        } catch (IllegalArgumentException ie) {
-            LOGGER.info("Failed to fetch the secret, because it's an invalid path. Return the path.");
-        } catch (Exception e) {
-            LOGGER.error("Failed to fetch the secret", e);
+        long start = System.currentTimeMillis();
+        VaultResponse response = template.read(path);
+        LOGGER.debug("Vault read took {} ms", System.currentTimeMillis() - start);
+        if (response != null && response.getData() != null) {
+            return String.valueOf(response.getData().get("secret"));
         }
         return path;
     }
