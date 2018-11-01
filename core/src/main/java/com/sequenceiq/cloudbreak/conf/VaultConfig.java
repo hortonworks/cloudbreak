25817ca1f1c101e9b9da60eea7c249b8102820bf
Krisztian Horvath <keyki.kk@gmail.com>
2018-11-01 13:54:17 +0100

    fix periscope cannot start issue

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/conf/VaultConfig.java
@@ -0,0 +1,33 @@
+package com.sequenceiq.cloudbreak.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+
+@Configuration
+public class VaultConfig extends AbstractVaultConfiguration {
+
+    @Value("${cb.vault.addr:vault.service.consul}")
+    private String address;
+
+    @Value("${cb.vault.port:8200}")
+    private int port;
+
+    @Value("${cb.vault.root.token:}")
+    private String rootToken;
+
+    @Override
+    public VaultEndpoint vaultEndpoint() {
+        VaultEndpoint endpoint = VaultEndpoint.create(address, port);
+        endpoint.setScheme("http");
+        return endpoint;
+    }
+
+    @Override
+    public ClientAuthentication clientAuthentication() {
+        return new TokenAuthentication(rootToken);
+    }
+}
