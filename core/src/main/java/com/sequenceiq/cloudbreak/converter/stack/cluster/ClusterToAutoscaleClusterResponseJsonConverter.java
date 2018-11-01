3e1e2d94a620941ff5db69bdb9f2457f71cf20c3
Krisztian Horvath <keyki.kk@gmail.com>
2018-11-01 12:38:06 +0100

    handle user provided ambari user/pass as secret

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/stack/cluster/ClusterToAutoscaleClusterResponseJsonConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/stack/cluster/ClusterToAutoscaleClusterResponseJsonConverter.java
@@ -1,18 +1,25 @@
 package com.sequenceiq.cloudbreak.converter.stack.cluster;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.model.AutoscaleClusterResponse;
 import com.sequenceiq.cloudbreak.converter.AbstractConversionServiceAwareConverter;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
+import com.sequenceiq.cloudbreak.service.VaultService;
 
 @Component
 public class ClusterToAutoscaleClusterResponseJsonConverter extends AbstractConversionServiceAwareConverter<Cluster, AutoscaleClusterResponse> {
 
+    @Inject
+    private VaultService vaultService;
+
     @Override
     public AutoscaleClusterResponse convert(Cluster source) {
         AutoscaleClusterResponse response = getConversionService().convert(source, AutoscaleClusterResponse.class);
-        response.setPassword(source.getPassword());
+        response.setPassword(vaultService.resolveSingleValue(source.getCloudbreakAmbariPassword()));
+        response.setUserName(vaultService.resolveSingleValue(source.getCloudbreakAmbariUser()));
         return response;
     }
 }
