e065cb02043a853f1e143bb1cdd9ae55c34833f2
Krisztian Horvath <keyki.kk@gmail.com>
2018-11-05 12:55:07 +0100

    BUG-113477 move generated recipe text to vault

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/v2/cli/RecipeToRecipeRequestConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/v2/cli/RecipeToRecipeRequestConverter.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.cloudbreak.converter.v2.cli;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.cloudbreak.api.model.RecipeRequest;
 import com.sequenceiq.cloudbreak.converter.AbstractConversionServiceAwareConverter;
 import com.sequenceiq.cloudbreak.domain.Recipe;
+import com.sequenceiq.cloudbreak.service.VaultService;
 
 @Component
 public class RecipeToRecipeRequestConverter
@@ -14,13 +17,16 @@ public class RecipeToRecipeRequestConverter
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RecipeToRecipeRequestConverter.class);
 
+    @Inject
+    private VaultService vaultService;
+
     @Override
     public RecipeRequest convert(Recipe source) {
         RecipeRequest recipeRequest = new RecipeRequest();
         recipeRequest.setName("");
         recipeRequest.setDescription(source.getDescription());
         recipeRequest.setRecipeType(source.getRecipeType());
-        recipeRequest.setContent(source.getContent());
+        recipeRequest.setContent(vaultService.resolveSingleValue(source.getContent()));
         return recipeRequest;
     }
 
