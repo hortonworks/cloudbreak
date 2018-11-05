2f3f3804b0d8be33c89efb25c22d873cde3ca0e4
Krisztian Horvath <keyki.kk@gmail.com>
2018-11-05 12:55:07 +0100

    move generated recipe text to vault

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/host/GeneratedRecipe.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/host/GeneratedRecipe.java
@@ -1,7 +1,6 @@
 package com.sequenceiq.cloudbreak.domain.stack.cluster.host;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,8 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
+import com.sequenceiq.cloudbreak.aspect.vault.VaultValue;
 import com.sequenceiq.cloudbreak.domain.ProvisionEntity;
-import com.sequenceiq.cloudbreak.domain.converter.EncryptionConverter;
 
 @Entity
 public class GeneratedRecipe implements ProvisionEntity {
@@ -20,23 +19,19 @@ public class GeneratedRecipe implements ProvisionEntity {
     @SequenceGenerator(name = "generatedrecipe_generator", sequenceName = "generatedrecipe_id_seq", allocationSize = 1)
     private Long id;
 
-    @Convert(converter = EncryptionConverter.class)
-    @Column(length = 1000000, columnDefinition = "TEXT", nullable = false)
-    private String originalRecipe;
-
-    @Convert(converter = EncryptionConverter.class)
-    @Column(length = 1000000, columnDefinition = "TEXT", nullable = false)
-    private String extendedRecipe;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    @VaultValue
+    private String extendedRecipeText;
 
     @ManyToOne
     private HostGroup hostGroup;
 
-    public String getExtendedRecipe() {
-        return extendedRecipe;
+    public String getExtendedRecipeText() {
+        return extendedRecipeText;
     }
 
-    public void setExtendedRecipe(String extendedRecipe) {
-        this.extendedRecipe = extendedRecipe;
+    public void setExtendedRecipeText(String extendedRecipeText) {
+        this.extendedRecipeText = extendedRecipeText;
     }
 
     public Long getId() {
@@ -45,14 +40,6 @@ public class GeneratedRecipe implements ProvisionEntity {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getOriginalRecipe() {
-        return originalRecipe;
-    }
-
-    public void setOriginalRecipe(String originalRecipe) {
-        this.originalRecipe = originalRecipe;
     }
 
     public HostGroup getHostGroup() {
