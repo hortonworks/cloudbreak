aeb261dd4dcbfd0c4e2ce68ca5afe6a6392cfd7a
Krisztian Horvath <keyki.kk@gmail.com>
2018-11-05 10:51:02 +0100

    BUG-113477 move clustertemplates to vault

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/ClusterTemplate.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/cluster/ClusterTemplate.java
@@ -3,7 +3,6 @@ package com.sequenceiq.cloudbreak.domain.stack.cluster;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -16,9 +15,8 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import com.sequenceiq.cloudbreak.api.model.ResourceStatus;
+import com.sequenceiq.cloudbreak.aspect.vault.VaultValue;
 import com.sequenceiq.cloudbreak.authorization.WorkspaceResource;
-import com.sequenceiq.cloudbreak.domain.json.EncryptedJsonToString;
-import com.sequenceiq.cloudbreak.domain.json.Json;
 import com.sequenceiq.cloudbreak.domain.workspace.Workspace;
 import com.sequenceiq.cloudbreak.domain.workspace.WorkspaceAwareResource;
 
@@ -37,9 +35,8 @@ public class ClusterTemplate implements WorkspaceAwareResource, Serializable {
     @Column(length = 1000000, columnDefinition = "TEXT")
     private String description;
 
-    @Convert(converter = EncryptedJsonToString.class)
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private Json template;
+    @VaultValue
+    private String template;
 
     @ManyToOne
     private Workspace workspace;
@@ -51,11 +48,11 @@ public class ClusterTemplate implements WorkspaceAwareResource, Serializable {
     @Column(nullable = false)
     private String cloudPlatform;
 
-    public Json getTemplate() {
+    public String getTemplate() {
         return template;
     }
 
-    public void setTemplate(Json template) {
+    public void setTemplate(String template) {
         this.template = template;
     }
 
