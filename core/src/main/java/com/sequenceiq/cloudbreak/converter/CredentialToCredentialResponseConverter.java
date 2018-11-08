d0baf7efcb7af72e35400ebbe396058d6dacce2f
bbihari <bbihari@hortonworks.com>
2018-11-08 15:53:43 +0100

    BUG-113477 return non sensitive properties for credentials

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/CredentialToCredentialResponseConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/CredentialToCredentialResponseConverter.java
@@ -1,8 +1,12 @@
 package com.sequenceiq.cloudbreak.converter;
 
+import static com.sequenceiq.cloudbreak.cloud.model.Platform.platform;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -12,6 +16,9 @@ import com.sequenceiq.cloudbreak.api.model.CredentialResponse;
 import com.sequenceiq.cloudbreak.api.model.users.WorkspaceResourceResponse;
 import com.sequenceiq.cloudbreak.controller.validation.credential.CredentialValidator;
 import com.sequenceiq.cloudbreak.domain.Credential;
+import com.sequenceiq.cloudbreak.domain.json.Json;
+import com.sequenceiq.cloudbreak.service.secret.SecretService;
+import com.sequenceiq.cloudbreak.service.stack.resource.definition.credential.CredentialDefinitionService;
 import com.sequenceiq.cloudbreak.service.topology.TopologyService;
 
 @Component
@@ -26,6 +33,12 @@ public class CredentialToCredentialResponseConverter extends AbstractConversionS
     @Inject
     private CredentialValidator credentialValidator;
 
+    @Inject
+    private CredentialDefinitionService credentialDefinitionService;
+
+    @Inject
+    private SecretService secretService;
+
     @Override
     public CredentialResponse convert(Credential source) {
         CredentialResponse credentialJson = new CredentialResponse();
@@ -34,7 +47,11 @@ public class CredentialToCredentialResponseConverter extends AbstractConversionS
         credentialJson.setCloudPlatform(source.cloudPlatform());
         credentialJson.setName(source.getName());
         if (source.getAttributes() != null) {
-            credentialJson.setParameters(Collections.singletonMap("value", source.getAttributes()));
+            Json secretAttributes = new Json(secretService.get(source.getAttributes()));
+            Map<String, Object> parameters = credentialDefinitionService.removeSensitives(platform(source.cloudPlatform()), secretAttributes.getMap());
+            convertValuesToBooleanIfNecessary(parameters);
+            credentialJson.setParameters(parameters);
+            credentialJson.setParametersPath(source.getAttributes());
         }
         credentialJson.setDescription(source.getDescription() == null ? "" : source.getDescription());
         if (source.getTopology() != null) {
@@ -51,6 +68,18 @@ public class CredentialToCredentialResponseConverter extends AbstractConversionS
                 response.getParameters().put(field, PLACEHOLDER);
             }
         }
+    }
+
+    private void convertValuesToBooleanIfNecessary(Map<String, Object> parameters) {
+        parameters.keySet().forEach(s -> {
+            if (isStringAndBoolean(parameters.get(s))) {
+                parameters.put(s, Boolean.parseBoolean((String) parameters.get(s)));
+            }
+        });
+    }
+
+    private boolean isStringAndBoolean(Object o) {
+        return o instanceof String && (TRUE.toString().equalsIgnoreCase((String) o) || FALSE.toString().equalsIgnoreCase((String) o));
     }
 
 }
---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/resource/definition/ResourceDefinitionService.java
@@ -0,0 +1,46 @@
+package com.sequenceiq.cloudbreak.service.stack.resource.definition;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.sequenceiq.cloudbreak.cloud.event.platform.ResourceDefinitionRequest;
+import com.sequenceiq.cloudbreak.cloud.event.platform.ResourceDefinitionResult;
+import com.sequenceiq.cloudbreak.cloud.model.CloudPlatformVariant;
+import com.sequenceiq.cloudbreak.cloud.model.Platform;
+import com.sequenceiq.cloudbreak.cloud.model.Variant;
+import com.sequenceiq.cloudbreak.cloud.reactor.ErrorHandlerAwareReactorEventFactory;
+import com.sequenceiq.cloudbreak.service.stack.connector.OperationException;
+
+import reactor.bus.EventBus;
+
+@Service
+public class ResourceDefinitionService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceDefinitionService.class);
+
+    @Inject
+    private EventBus eventBus;
+
+    @Inject
+    private ErrorHandlerAwareReactorEventFactory eventFactory;
+
+    @Cacheable("resourceDefinitionCache")
+    public String getResourceDefinition(String cloudPlatform, String resource) {
+        LOGGER.debug("Sending request for {} {} resource property definition", cloudPlatform, resource);
+        CloudPlatformVariant platformVariant = new CloudPlatformVariant(Platform.platform(cloudPlatform), Variant.EMPTY);
+        ResourceDefinitionRequest request = new ResourceDefinitionRequest(platformVariant, resource);
+        eventBus.notify(request.selector(), eventFactory.createEvent(request));
+        try {
+            ResourceDefinitionResult result = request.await();
+            LOGGER.info("Resource property definition: {}", result);
+            return result.getDefinition();
+        } catch (InterruptedException e) {
+            LOGGER.error("Error while sending resource definition request", e);
+            throw new OperationException(e);
+        }
+    }
+}
