ad785f5b783a7962fb42918212eb69c20dbe13bc
lnardai <laci.nardai@gmail.com>
2018-11-28 16:42:13 +0100

    Fix failure detection for circleCI

--- integration-test/scripts/check-results.sh
+++ integration-test/scripts/check-results.sh
@@ -6,19 +6,25 @@ status_code=0
 if [[ ! -d test-output ]]; then
     echo -e "\033[0;91m--- !!! NO test-output DIRECTORY !!! ---\n"; status_code=1;
 else
-    if grep -r '<failure ' test-output && grep -r '<skipped' test-output;
-        then echo -e "\033[0;91m--- !!! INTEGRATION TEST FAILED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n"; status_code=1;
-        else echo -e "\n\033[0;92m+++ INTEGRATION TEST SUCCESSFULLY FINISHED +++\n";
+    grep -r '<failure ' test-output
+    if [[ "$?" -eq 0 ]]; then
+      echo -e "\033[0;91m--- !!! INTEGRATION TEST FAILED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n"; status_code=1;
+    fi
+    grep -r '<skipped' test-output
+    if [[ "$?" -eq 0 ]]; then
+      echo -e "\033[0;91m--- !!! INTEGRATION TEST SKIPPED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n"; status_code=1;
     fi
 fi
 
 # Check swagger validation results
-if grep -q "is valid against swagger specification 2.0" swagger-validation-result.out;
-    then echo -e "\n\033[0;92m+++ THE SWAGGER SPECIFICATION IS VALID +++\n";
-    else echo -e "\033[0;91m--- !!! THE SWAGGER SPECIFICATION IS INVALID, CHECK \033[1;93mswagger-validation-result.out\033[0;91m FOR RESULTS !!! ---\n"; status_code=1;
+if ! grep -q "is valid against swagger specification 2.0" swagger-validation-result.out; then
+    echo -e "\033[0;91m--- !!! THE SWAGGER SPECIFICATION IS INVALID, CHECK \033[1;93mswagger-validation-result.out\033[0;91m FOR RESULTS !!! ---\n"; status_code=1;
 fi
 
 # Exit if there are failed tests
 if [[ status_code -eq 1 ]];
     then exit 1;
 fi
+
+echo -e "\n\033[0;92m+++ INTEGRATION TEST SUCCESSFULLY FINISHED +++\n";
+echo -e "\n\033[0;92m+++ THE SWAGGER SPECIFICATION IS VALID +++\n";
