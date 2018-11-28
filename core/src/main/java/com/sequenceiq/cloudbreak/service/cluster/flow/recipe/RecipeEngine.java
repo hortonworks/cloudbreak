af6525c9f88dcbff8ee5c10ba200efb11f742794
Attila Kanto <attila.kanto@gmail.com>
2018-11-28 11:44:19 +0100

    BUG-113193 Cleanup of Atlas hack since BUG-107810 has been implemented

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/recipe/RecipeEngine.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/recipe/RecipeEngine.java
@@ -4,7 +4,6 @@ import static com.sequenceiq.cloudbreak.api.model.RecipeType.POST_AMBARI_START;
 import static com.sequenceiq.cloudbreak.api.model.RecipeType.PRE_AMBARI_START;
 import static com.sequenceiq.cloudbreak.api.model.RecipeType.PRE_TERMINATION;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Set;
 
@@ -14,23 +13,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Sets;
 import com.sequenceiq.cloudbreak.api.model.RecipeType;
 import com.sequenceiq.cloudbreak.api.model.stack.instance.InstanceGroupType;
-import com.sequenceiq.cloudbreak.blueprint.BlueprintProcessorFactory;
-import com.sequenceiq.cloudbreak.blueprint.SmartsenseConfigurationLocator;
-import com.sequenceiq.cloudbreak.common.model.recipe.RecipeScript;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.OrchestratorTypeResolver;
 import com.sequenceiq.cloudbreak.domain.Orchestrator;
 import com.sequenceiq.cloudbreak.domain.Recipe;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
-import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.host.HostGroup;
-import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceGroup;
-import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
 import com.sequenceiq.cloudbreak.service.CloudbreakException;
-import com.sequenceiq.cloudbreak.service.smartsense.SmartSenseSubscriptionService;
-import com.sequenceiq.cloudbreak.util.FileReaderUtils;
 
 @Component
 public class RecipeEngine {
@@ -41,25 +31,11 @@ public class RecipeEngine {
     private OrchestratorTypeResolver orchestratorTypeResolver;
 
     @Inject
-    private RecipeBuilder recipeBuilder;
-
-    @Inject
     private OrchestratorRecipeExecutor orchestratorRecipeExecutor;
-
-    @Inject
-    private BlueprintProcessorFactory blueprintProcessorFactory;
-
-    @Inject
-    private SmartsenseConfigurationLocator smartsenseConfigurationLocator;
-
-    @Inject
-    private SmartSenseSubscriptionService smartSenseSubscriptionService;
 
     public void uploadRecipes(Stack stack, Set<HostGroup> hostGroups) throws CloudbreakException {
         Orchestrator orchestrator = stack.getOrchestrator();
         if (recipesSupportedOnOrchestrator(orchestrator)) {
-            String blueprintText = stack.getCluster().getBlueprint().getBlueprintText();
-            addS3SymlinkRecipe(stack, blueprintText, hostGroups);
             boolean recipesFound = recipesFound(hostGroups);
             if (recipesFound) {
                 orchestratorRecipeExecutor.uploadRecipes(stack, hostGroups);
@@ -121,7 +97,6 @@ public class RecipeEngine {
         return (recipesFound(hostGroups, recipeType)) && recipesSupportedOnOrchestrator(orchestrator);
     }
 
-
     private boolean recipesFound(Iterable<HostGroup> hostGroups) {
         for (HostGroup hostGroup : hostGroups) {
             if (!hostGroup.getRecipes().isEmpty()) {
@@ -137,52 +112,6 @@ public class RecipeEngine {
                 if (recipe.getRecipeType() == recipeType) {
                     return true;
                 }
-            }
-        }
-        return false;
-    }
-
-    private void addS3SymlinkRecipe(Stack stack, String blueprintText, Set<HostGroup> hostGroups) {
-        try {
-            Cluster cluster = stack.getCluster();
-            for (HostGroup hostGroup : hostGroups) {
-                if (isComponentPresent(blueprintText, "ATLAS_SERVER", hostGroup)) {
-                    String script = FileReaderUtils.readFileFromClasspath("scripts/prepare-s3-symlinks.sh")
-                            .replaceAll("\\$AMBARI_USER", cluster.getUserName())
-                            .replaceAll("\\$AMBARI_IP", getAmbariPrivateIp(stack))
-                            .replaceAll("\\$AMBARI_PASSWORD", cluster.getPassword())
-                            .replaceAll("\\$CLUSTER_NAME", cluster.getName());
-                    RecipeScript recipeScript = new RecipeScript(script, RecipeType.POST_CLUSTER_INSTALL);
-                    Recipe recipe = recipeBuilder.buildRecipes("prepare-s3-symlinks", Collections.singletonList(recipeScript)).get(0);
-                    hostGroup.addRecipe(recipe);
-                    break;
-                }
-            }
-        } catch (IOException e) {
-            LOGGER.warn("Cannot create s3 symlinks recipe", e);
-        }
-    }
-
-    private String getAmbariPrivateIp(Stack stack) {
-        String result = null;
-        for (InstanceGroup ig : stack.getInstanceGroups()) {
-            if (InstanceGroupType.isGateway(ig.getInstanceGroupType())) {
-                InstanceMetaData imd = ig.getInstanceMetaDataSet().iterator().next();
-                result = imd.getPrivateIp();
-            }
-        }
-        return result;
-    }
-
-    private boolean isComponentPresent(String blueprint, String component, HostGroup hostGroup) {
-        return isComponentPresent(blueprint, component, Sets.newHashSet(hostGroup));
-    }
-
-    private boolean isComponentPresent(String blueprint, String component, Set<HostGroup> hostGroups) {
-        for (HostGroup hostGroup : hostGroups) {
-            Set<String> components = blueprintProcessorFactory.get(blueprint).getComponentsInHostGroup(hostGroup.getName());
-            if (components.contains(component)) {
-                return true;
             }
         }
         return false;
