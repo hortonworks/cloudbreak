4458b8d8924e97444fe21e2b0c2fe2d9f92255ac
Richard Kovacs <kovacsricsi@gmail.com>
2018-12-10 16:49:33 +0100

    Fix integration test

--- tests/spec/e2e/spec_helper.rb
+++ tests/spec/e2e/spec_helper.rb
@@ -4,15 +4,16 @@ require 'json'
 require 'date'
 require 'allure-rspec'
 require_relative "../common/command_builder"
-require "oauth"
 
-RSpec.configure do |c|
-  c.include AllureRSpec::Adaptor
+RSpec.configure do |config|
+  config.include AllureRSpec::Adaptor
 
-  c.expect_with(:rspec) { |c| c.syntax = :expect }
-  c.color = true
-  c.tty = true
-  c.formatter = :documentation
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
 
   def html_print(&blk)
     puts "<pre>"
@@ -21,8 +22,8 @@ RSpec.configure do |c|
   end
 end
 
-AllureRSpec.configure do |c|
-  c.output_dir = "allure/allure-results" # default: gen/allure-results
-  c.clean_dir = true # clean the output directory first? (default: true)
-  c.logging_level = Logger::ERROR # logging level (default: DEBUG)
+AllureRSpec.configure do |config|
+  config.output_dir = "allure/allure-results" # - default: gen/allure-results
+  config.clean_dir = true # - clean the output directory first? (default: true)
+  config.logging_level = Logger::ERROR # - logging level (default: DEBUG)
 end
