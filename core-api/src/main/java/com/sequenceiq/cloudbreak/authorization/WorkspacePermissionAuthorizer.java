7644168ca6a9a203d17507d4cae809c2e8e392f6
Attila Palfi <apalfi@hortonworks.com>
2018-12-12 13:53:18 +0100

    BUG-115708 added missing files

---
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/authorization/WorkspacePermissionAuthorizer.java
@@ -0,0 +1,33 @@
+package com.sequenceiq.cloudbreak.authorization;
+
+import static com.sequenceiq.cloudbreak.authorization.WorkspacePermissions.Action.INVITE;
+import static com.sequenceiq.cloudbreak.authorization.WorkspacePermissions.Action.MANAGE;
+import static com.sequenceiq.cloudbreak.authorization.WorkspacePermissions.Action.READ;
+import static com.sequenceiq.cloudbreak.authorization.WorkspacePermissions.Action.WRITE;
+import static com.sequenceiq.cloudbreak.authorization.WorkspacePermissions.getName;
+import static com.sequenceiq.cloudbreak.authorization.WorkspaceResource.ALL;
+import static com.sequenceiq.cloudbreak.authorization.WorkspaceResource.WORKSPACE;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+@Component
+    public class WorkspacePermissionAuthorizer {
+
+    public boolean hasPermission(Set<String> permissions, WorkspaceResource resource, WorkspacePermissions.Action action) {
+        if (resource == WORKSPACE) {
+            if (action == INVITE && (permissions.contains(getName(WORKSPACE, INVITE)) || permissions.contains(getName(WORKSPACE, MANAGE)))) {
+                return true;
+            }
+            return action == MANAGE && permissions.contains(getName(WORKSPACE, MANAGE));
+        } else if (permissions.contains(getName(ALL, WRITE))) {
+            return true;
+        } else if (permissions.contains(getName(ALL, READ)) && action == READ) {
+            return true;
+        }
+        String permissionName = getName(resource, action);
+        return permissions.contains(permissionName);
+    }
+
+}
