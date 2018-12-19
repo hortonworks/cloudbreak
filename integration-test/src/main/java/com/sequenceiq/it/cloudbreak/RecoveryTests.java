9a4520a4d9991d3bc01ccf4ce65c5e92f537ae07
Attila Palfi <apalfi@hortonworks.com>
2018-12-19 12:50:15 +0100

    BUG-115951 using identity token for an autoscale endpoint in manual recovery test

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/RecoveryTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/RecoveryTests.java
@@ -3,8 +3,6 @@ package com.sequenceiq.it.cloudbreak;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,7 +14,6 @@ import com.sequenceiq.it.cloudbreak.newway.cloud.CloudProvider;
 import com.sequenceiq.it.cloudbreak.newway.cloud.HostGroupType;
 
 public class RecoveryTests extends ClusterTests {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RecoveryTests.class);
 
     @Test(dataProvider = "providernameblueprintimage", priority = 15)
     public void testManualRecovery(CloudProvider cloudProvider, String clusterName, String blueprintName, String imageId) throws Exception {
