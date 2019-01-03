1dcf5d418f60bc18e76689e7adce295182562970
Richard Kovacs <kovacsricsi@gmail.com>
2019-01-03 16:07:25 +0100

    BUG-106338 Fine tunning log levels to be more INFOrmative

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/mapper/AuthenticationCredentialsNotFoundExceptionMapper.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/mapper/AuthenticationCredentialsNotFoundExceptionMapper.java
@@ -1,12 +1,21 @@
 package com.sequenceiq.cloudbreak.controller.mapper;
 
+import static ch.qos.logback.classic.Level.INFO;
+
 import javax.ws.rs.core.Response.Status;
 
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.classic.Level;
+
 @Component
 public class AuthenticationCredentialsNotFoundExceptionMapper extends BaseExceptionMapper<AuthenticationCredentialsNotFoundException> {
+
+    @Override
+    protected Level getLogLevel() {
+        return INFO;
+    }
 
     @Override
     Status getResponseStatus() {
