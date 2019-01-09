6955c740d43446d91a604da9c63ab93c41da6df3
Krisztian Horvath <keyki.kk@gmail.com>
2019-01-09 11:23:23 +0100

    RMP-12945 use structured event instead of response

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/audit/StructuredEventEntityToAuditEventConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/audit/StructuredEventEntityToAuditEventConverter.java
@@ -5,11 +5,9 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.model.audit.AuditEvent;
-import com.sequenceiq.cloudbreak.api.model.audit.StructuredEventResponse;
 import com.sequenceiq.cloudbreak.converter.AbstractConversionServiceAwareConverter;
 import com.sequenceiq.cloudbreak.domain.StructuredEventEntity;
 import com.sequenceiq.cloudbreak.structuredevent.db.converter.StructuredEventEntityToStructuredEventConverter;
-import com.sequenceiq.cloudbreak.structuredevent.db.converter.StructuredEventToStructuredEventResponseConverter;
 import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEvent;
 
 @Component
@@ -18,13 +16,9 @@ public class StructuredEventEntityToAuditEventConverter extends AbstractConversi
     @Inject
     private StructuredEventEntityToStructuredEventConverter structuredEventEntityToStructuredEventConverter;
 
-    @Inject
-    private StructuredEventToStructuredEventResponseConverter structuredEventToStructuredEventResponseConverter;
-
     @Override
     public AuditEvent convert(StructuredEventEntity source) {
         StructuredEvent structuredEvent = structuredEventEntityToStructuredEventConverter.convert(source);
-        StructuredEventResponse structuredEventResponse = structuredEventToStructuredEventResponseConverter.convert(structuredEvent);
-        return new AuditEvent(source.getId(), structuredEventResponse);
+        return new AuditEvent(source.getId(), structuredEvent);
     }
 }
