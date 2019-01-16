3b6496661b11a20003a4e561184db57e7610c9e2
Richard Kovacs <kovacsricsi@gmail.com>
2019-01-16 14:57:23 +0100

    RMP-12882 KubernetesConfigService test

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/KubernetesConfigServiceTest.java
@@ -0,0 +1,74 @@
+package com.sequenceiq.cloudbreak.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.sequenceiq.cloudbreak.controller.exception.NotFoundException;
+import com.sequenceiq.cloudbreak.domain.KubernetesConfig;
+import com.sequenceiq.cloudbreak.domain.view.EnvironmentView;
+import com.sequenceiq.cloudbreak.domain.workspace.User;
+import com.sequenceiq.cloudbreak.domain.workspace.Workspace;
+import com.sequenceiq.cloudbreak.repository.KubernetesConfigRepository;
+import com.sequenceiq.cloudbreak.service.TransactionService.TransactionExecutionException;
+import com.sequenceiq.cloudbreak.service.secret.SecretService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KubernetesConfigServiceTest {
+
+    @Rule
+    public final ExpectedException thrown = ExpectedException.none();
+
+    @InjectMocks
+    private KubernetesConfigService underTest;
+
+    @Mock
+    private KubernetesConfigRepository kubernetesConfigRepository;
+
+    @Mock
+    private SecretService secretService;
+
+    @Before
+    public void setup() throws TransactionExecutionException {
+        doAnswer(invocation -> invocation.getArgument(0)).when(kubernetesConfigRepository).save(any());
+    }
+
+    @Test
+    public void testUpdateByWorkspaceIdNotFound() {
+        thrown.expect(NotFoundException.class);
+
+        underTest.updateByWorkspaceId(1L, new KubernetesConfig(), new User());
+    }
+
+    @Test
+    public void testUpdateByWorkspaceIdOk() {
+        KubernetesConfig originalConfig = new KubernetesConfig();
+        originalConfig.setId(1L);
+        originalConfig.setWorkspace(new Workspace());
+        originalConfig.setEnvironments(Set.of(new EnvironmentView()));
+        when(kubernetesConfigRepository.findByNameAndWorkspaceId(any(), anyLong())).thenReturn(originalConfig);
+
+        KubernetesConfig result = underTest.updateByWorkspaceId(1L, new KubernetesConfig(), new User());
+
+        verify(secretService, times(1)).delete(any());
+
+        Assert.assertEquals(originalConfig.getId(), result.getId());
+        Assert.assertEquals(originalConfig.getWorkspace(), result.getWorkspace());
+        Assert.assertEquals(originalConfig.getEnvironments(), result.getEnvironments());
+    }
+}
