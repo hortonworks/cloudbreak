a3831ecc3a43dc43c68bad69b1d24538c9650528
sodre90 <perdos@hortonworks.com>
2019-01-30 12:34:27 +0100

    BUG-116921 - fix broken unit tests

--- core/src/test/java/com/sequenceiq/cloudbreak/service/image/ImageCatalogServiceDefaultTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/image/ImageCatalogServiceDefaultTest.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -86,7 +88,7 @@ public class ImageCatalogServiceDefaultTest {
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
                 { "com/sequenceiq/cloudbreak/service/image/default-hdp-imagecatalog.json", "aws", "HDP", "2.6", "latest-hdp", "5.0.0", "" },
-                { "com/sequenceiq/cloudbreak/service/image/default-hdp-imagecatalog.json", "aws", "HDP", "2.6", "latest-hdp", "5.0.0", "centos7" },
+                { "com/sequenceiq/cloudbreak/service/image/default-hdp-imagecatalog.json", "aws", "HDP", "2.6", "latest-hdp", "5.0.0", "redhat7" },
                 { "com/sequenceiq/cloudbreak/service/image/default-hdp-imagecatalog.json", "aws", "HDP", "2.6", "latest-amazonlinux-hdp",
                     "5.0.0", "amazonlinux2" },
                 { "com/sequenceiq/cloudbreak/service/image/default-hdp-imagecatalog.json", "aws", "HDP", "missing", "third-latest-base-amazonlinux",
@@ -128,7 +130,11 @@ public class ImageCatalogServiceDefaultTest {
         ReflectionTestUtils.setField(underTest, "cbVersion", cbVersion);
         ReflectionTestUtils.setField(underTest, "defaultCatalogUrl", "");
         // WHEN
-        StatedImage statedImage = underTest.getPrewarmImageDefaultPreferred(provider, clusterType, clusterVersion, Collections.singleton(os),
+        Set<String> operatingSystems = null;
+        if (StringUtils.isNotEmpty(os)) {
+            operatingSystems = Collections.singleton(os);
+        }
+        StatedImage statedImage = underTest.getPrewarmImageDefaultPreferred(provider, clusterType, clusterVersion, operatingSystems,
                 cloudbreakUser, user);
         // THEN
         Assert.assertEquals("Wrong default image has been selected", expectedImageId, statedImage.getImage().getUuid());
