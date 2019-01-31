992d65027978551755bb8975f46472dbe27d7492
Attila Palfi <apalfi@hortonworks.com>
2019-01-31 14:08:07 +0100

    BUG-116875 - added mpack validation

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/mpack/ManagementPackCreationValidator.java
@@ -0,0 +1,56 @@
+package com.sequenceiq.cloudbreak.service.mpack;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.client.ConfigKey;
+import com.sequenceiq.cloudbreak.client.RestClientUtil;
+import com.sequenceiq.cloudbreak.controller.validation.ValidationResult;
+import com.sequenceiq.cloudbreak.controller.validation.ValidationResult.ValidationResultBuilder;
+import com.sequenceiq.cloudbreak.controller.validation.Validator;
+import com.sequenceiq.cloudbreak.domain.ManagementPack;
+
+@Component
+public class ManagementPackCreationValidator implements Validator<ManagementPack> {
+
+    private static final String MEDIA_TYPE_APPLICATION = "application";
+
+    private static final String MEDIA_SUB_TYPE_X_TAR = "x-tar";
+
+    private Client client;
+
+    @PostConstruct
+    public void init() {
+        client = RestClientUtil.get(new ConfigKey(true, false, false));
+    }
+
+    @Override
+    public ValidationResult validate(ManagementPack subject) {
+        ValidationResultBuilder resultBuilder = ValidationResult.builder();
+        WebTarget target = client.target(subject.getMpackUrl());
+        try (Response response = target.request().head()) {
+            if (response.getStatusInfo() != null) {
+                if (!response.getStatusInfo().getFamily().equals(Family.SUCCESSFUL)) {
+                    resultBuilder.error(String.format("The URL is invalid! Response code was ['%s'].", response.getStatus()));
+                }
+            }
+            if (response.getMediaType() != null) {
+                if (!isMediaTypeValid(response.getMediaType())) {
+                    resultBuilder.error(String.format("Media type ['%s'] is invalid.", response.getMediaType()));
+                }
+            }
+        }
+        return resultBuilder.build();
+    }
+
+    private boolean isMediaTypeValid(MediaType mediaType) {
+        return MEDIA_TYPE_APPLICATION.equalsIgnoreCase(mediaType.getType())
+                && MEDIA_SUB_TYPE_X_TAR.equalsIgnoreCase(mediaType.getSubtype());
+    }
+}
