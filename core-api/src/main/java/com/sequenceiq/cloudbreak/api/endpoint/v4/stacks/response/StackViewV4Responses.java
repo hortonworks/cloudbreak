da03bbf099e6b061c01943726a32b0885ff16953
Ferenc Schneider <fschneider@hortonworks.com>
2019-02-05 16:42:03 +0100

    BUG-117327 Environment tests

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/stacks/response/StackViewV4Responses.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/stacks/response/StackViewV4Responses.java
@@ -2,11 +2,16 @@ package com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response;
 
 import java.util.Set;
 
+import com.google.common.collect.Sets;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.responses.GeneralCollectionV4Response;
 
 public class StackViewV4Responses extends GeneralCollectionV4Response<StackViewV4Response> {
 
     public StackViewV4Responses(Set<StackViewV4Response> responses) {
         super(responses);
+    }
+
+    public StackViewV4Responses() {
+        super(Sets.newHashSet());
     }
 }
