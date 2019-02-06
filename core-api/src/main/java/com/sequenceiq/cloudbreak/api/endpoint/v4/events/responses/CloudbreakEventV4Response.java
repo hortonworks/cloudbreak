ffaa6ea6a96f571849aa5b78cf11c76109d764da
Tamas Bihari <tbihari@hortonworks.com>
2019-02-06 16:12:14 +0100

    CB-292 new utils endpoint to validate notification system/chain

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/events/responses/CloudbreakEventV4Response.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/events/responses/CloudbreakEventV4Response.java
@@ -1,33 +1,19 @@
 package com.sequenceiq.cloudbreak.api.endpoint.v4.events.responses;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.JsonEntity;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions.BlueprintModelDescription;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions.ClusterModelDescription;
-import com.sequenceiq.cloudbreak.doc.ModelDescriptions.EventModelDescription;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions.InstanceGroupModelDescription;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions.StackModelDescription;
-
 import com.sequenceiq.cloudbreak.structuredevent.event.LdapDetails;
 import com.sequenceiq.cloudbreak.structuredevent.event.RdsDetails;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel
-public class CloudbreakEventV4Response implements JsonEntity {
-
-    @ApiModelProperty(EventModelDescription.TYPE)
-    private String eventType;
-
-    @ApiModelProperty(EventModelDescription.TIMESTAMP)
-    private long eventTimestamp;
-
-    @ApiModelProperty(EventModelDescription.MESSAGE)
-    private String eventMessage;
-
-    @ApiModelProperty(ModelDescriptions.USER_ID)
-    private String userId;
+public class CloudbreakEventV4Response extends CloudbreakEventBaseV4 {
 
     @ApiModelProperty(ModelDescriptions.CLOUD_PLATFORM)
     private String cloud;
@@ -81,38 +67,6 @@ public class CloudbreakEventV4Response implements JsonEntity {
 
     public void setNodeCount(Integer nodeCount) {
         this.nodeCount = nodeCount;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public long getEventTimestamp() {
-        return eventTimestamp;
-    }
-
-    public void setEventTimestamp(long eventTimestamp) {
-        this.eventTimestamp = eventTimestamp;
-    }
-
-    public String getEventMessage() {
-        return eventMessage;
-    }
-
-    public void setEventMessage(String eventMessage) {
-        this.eventMessage = eventMessage;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 
     public String getCloud() {
--- core/src/main/java/com/sequenceiq/cloudbreak/service/notification/NotificationSender.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/notification/NotificationSender.java
@@ -2,4 +2,6 @@ package com.sequenceiq.cloudbreak.service.notification;
 
 public interface NotificationSender {
     <T> void send(Notification<T> notification);
+
+    void sendTestNotification(String userId);
 }
