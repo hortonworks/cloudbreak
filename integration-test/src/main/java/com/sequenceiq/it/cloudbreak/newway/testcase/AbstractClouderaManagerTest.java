83d74d439c7b32ed9b8c3799ef357128b38ec23e
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2019-02-13 13:55:08 +0100

    CB-354 Mock tests for CM

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/testcase/AbstractClouderaManagerTest.java
@@ -0,0 +1,31 @@
+package com.sequenceiq.it.cloudbreak.newway.testcase;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.sequenceiq.it.cloudbreak.newway.Blueprint;
+import com.sequenceiq.it.cloudbreak.newway.BlueprintEntity;
+import com.sequenceiq.it.cloudbreak.newway.context.TestContext;
+import com.sequenceiq.it.util.ResourceUtil;
+
+public class AbstractClouderaManagerTest extends AbstractIntegrationTest {
+    @BeforeMethod
+    public void beforeMethod(Object[] data) throws IOException {
+        TestContext testContext = (TestContext) data[0];
+        createDefaultUser(testContext);
+        createDefaultCredential(testContext);
+        createDefaultImageCatalog(testContext);
+        testContext.given(BlueprintEntity.class)
+                .withName(getNameGenerator().getRandomNameForMock())
+                .withAmbariBlueprint(ResourceUtil.readResourceAsString(applicationContext, "classpath:/blueprint/clouderamanager.bp"))
+                .when(Blueprint.postV2());
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tear(Object[] data) {
+        TestContext testContext = (TestContext) data[0];
+        testContext.cleanupTestContextEntity();
+    }
+}
---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/testcase/ClouderaManagerStartStopTest.java
@@ -0,0 +1,73 @@
+package com.sequenceiq.it.cloudbreak.newway.testcase;
+
+import static com.sequenceiq.cloudbreak.cloud.model.InstanceStatus.STARTED;
+import static com.sequenceiq.cloudbreak.cloud.model.InstanceStatus.STOPPED;
+import static com.sequenceiq.it.cloudbreak.newway.Mock.gson;
+import static com.sequenceiq.it.spark.ITResponse.MOCK_ROOT;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.Response.Status.OK;
+
+import org.testng.annotations.Test;
+
+import com.sequenceiq.it.cloudbreak.newway.BlueprintEntity;
+import com.sequenceiq.it.cloudbreak.newway.Stack;
+import com.sequenceiq.it.cloudbreak.newway.StackEntity;
+import com.sequenceiq.it.cloudbreak.newway.context.TestContext;
+import com.sequenceiq.it.cloudbreak.newway.entity.AmbariEntity;
+import com.sequenceiq.it.cloudbreak.newway.entity.ClusterEntity;
+import com.sequenceiq.it.cloudbreak.newway.mock.model.SPIMock;
+import com.sequenceiq.it.spark.StatefulRoute;
+import com.sequenceiq.it.spark.spi.CloudVmInstanceStatuses;
+
+public class ClouderaManagerStartStopTest extends AbstractClouderaManagerTest {
+
+    private static final String CLOUD_INSTANCE_STATUSES = MOCK_ROOT + SPIMock.CLOUD_INSTANCE_STATUSES;
+
+    @Test(dataProvider = "testContext")
+    public void testCreateNewRegularCluster(TestContext testContext) {
+        mockSpi(testContext);
+        String name = testContext.get(BlueprintEntity.class).getRequest().getName();
+        testContext
+                .given("cm", AmbariEntity.class).withBlueprintName(name).withValidateBlueprint(Boolean.FALSE)
+                .given("cmcluster", ClusterEntity.class).withAmbari("cm")
+                .given(StackEntity.class).withCluster("cmcluster")
+                .when(Stack.postV2())
+                .await(STACK_AVAILABLE)
+                .when(Stack.stopV2())
+                .await(STACK_STOPPED)
+                .when(Stack.startV2())
+                .await(STACK_AVAILABLE)
+                .validate();
+    }
+
+    private void mockSpi(TestContext testContext) {
+        StatefulRoute okState = (request, response, model) -> {
+            String resultJson = gson().toJson(new CloudVmInstanceStatuses(model.getInstanceMap()).createCloudVmInstanceStatuses());
+            response.body(resultJson);
+            response.type(TEXT_PLAIN);
+            response.status(OK.getStatusCode());
+            return "";
+        };
+
+        StatefulRoute stoppedStateSpi = (request, response, model) -> {
+            String resultJson = gson().toJson(new CloudVmInstanceStatuses(model.getInstanceMap()).createCloudVmInstanceStatuses());
+            response.body(resultJson.replaceAll(STARTED.name(), STOPPED.name()));
+            response.type(TEXT_PLAIN);
+            response.status(OK.getStatusCode());
+            return "";
+        };
+
+        StatefulRoute startedStateSpi = (request, response, model) -> {
+            String resultJson = gson().toJson(new CloudVmInstanceStatuses(model.getInstanceMap()).createCloudVmInstanceStatuses());
+            response.body(resultJson.replaceAll(STOPPED.name(), STARTED.name()));
+            response.type(TEXT_PLAIN);
+            response.status(OK.getStatusCode());
+            return "";
+        };
+
+        testContext.getModel().getSpiMock().getDynamicRouteStack().clearPost(CLOUD_INSTANCE_STATUSES);
+        testContext.getModel().getSpiMock().getDynamicRouteStack().post(CLOUD_INSTANCE_STATUSES, okState);
+        testContext.getModel().getSpiMock().getDynamicRouteStack().post(CLOUD_INSTANCE_STATUSES, startedStateSpi);
+        testContext.getModel().getSpiMock().getDynamicRouteStack().post(CLOUD_INSTANCE_STATUSES, stoppedStateSpi);
+    }
+}
