9268287b01f773e7361e3bff0449733a340daf5d
Michael Wilson <mike.wilson@cloudera.com>
2019-02-21 10:48:26 -0500

    CB-25. Java 11 support.
    Java 11 is now supported in Cloudbreak. This additionally modifies
    several docker images to user openjdk 11 instead of openjdk 10.

--- integration-test/scripts/create-image.sh
+++ integration-test/scripts/create-image.sh
@@ -6,5 +6,8 @@ echo -e "\n\033[1;96m--- Test will run on \033[92;5m"$TEST_ENV"\033[0m\n"
 echo -e docker-host: $DOCKER_HOST
 echo -e "\n\033[1;96m--- Copy ../core/build/libs/cloudbreak.jar to test-image directory\033[0m\n"
 cp ../core/build/libs/cloudbreak.jar test-image
+cp ../docker-cloudbreak/bootstrap/start_cloudbreak_app.sh test-image
+cp ../docker-cloudbreak/bootstrap/wait_for_cloudbreak_api.sh test-image
+cp ../docker-cloudbreak/jmx_prometheus_javaagent-0.10.jar test-image
 echo -e "\n\033[1;96m--- Build dev cloudbreak test image\033[0m\n"
 docker build -t hortonworks/cloudbreak:dev test-image
