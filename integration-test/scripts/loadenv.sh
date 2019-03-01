3404f1f73624e317bebd7136bde32dea255310f4
sodre90 <perdos@hortonworks.com>
2019-03-01 17:21:49 +0100

    CB-21 adapt integration-test to new cbd

--- integration-test/scripts/loadenv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$DOCKER_MACHINE" ]; then
-    docker-machine env $(DOCKER_MACHINE) | awk '{ gsub ("\"", ""); print}' > docker_env.tmp
-    echo TEST_ENV='docker-machine' >> docker_env.tmp
-else
-    echo TEST_ENV='boot2docker' > docker_env.tmp
-fi
