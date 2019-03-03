dccfa0eb80251525a110a9e59a45da2abdee981b
Tamas Bihari <tbihari@hortonworks.com>
2019-03-03 18:01:11 +0100

    CB-365 decorate image catalog validator with constraints and cover with tests

--- core-api/src/main/java/com/sequenceiq/cloudbreak/validation/ImageCatalogValidator.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/validation/ImageCatalogValidator.java
@@ -9,12 +9,29 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status.Family;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sequenceiq.cloudbreak.client.RestClientUtil;
 
 public class ImageCatalogValidator implements ConstraintValidator<ValidImageCatalog, String> {
+
+    public static final String INVALID_URL_MSG = "The value should be a valid URL and start with 'http(s)'!";
+
+    public static final String FAILED_TO_GET_BY_FAMILY_TYPE = "Failed to get response by the specified URL '%s' due to: '%s'!";
+
+    public static final String FAILED_TO_GET_WITH_EXCEPTION = "Failed to get response by the specified URL!";
+
+    public static final String INVALID_JSON_IN_RESPONSE = "The file on the specified URL couldn't be parsed as JSON!";
+
+    public static final String INVALID_JSON_STRUCTURE_IN_RESPONSE = "The JSON on the specified URL does not match structure expected for an Image Catalog!";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageCatalogValidator.class);
 
     private static final Client CLIENT = RestClientUtil.get();
 
@@ -27,18 +44,38 @@ public class ImageCatalogValidator implements ConstraintValidator<ValidImageCata
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || !value.startsWith("http")) {
+            context.buildConstraintViolationWithTemplate(INVALID_URL_MSG).addConstraintViolation();
             return false;
         }
         try {
             WebTarget target = CLIENT.target(value);
-            try (Response response = target.request().get()) {
-                if (response.getStatusInfo().getFamily().equals(Family.SUCCESSFUL)) {
-                    String responseContent = response.readEntity(String.class);
-                    OBJECT_MAPPER.readValue(responseContent, CloudbreakImageCatalog.class);
+            Response response = target.request().get();
+            Response.StatusType responseStatusInfo = response.getStatusInfo();
+            if (responseStatusInfo.getFamily().equals(Family.SUCCESSFUL)) {
+                String responseContent = response.readEntity(String.class);
+                if (imageCatalogParsable(context, responseContent)) {
                     return true;
                 }
+            } else {
+                String msg = String.format(FAILED_TO_GET_BY_FAMILY_TYPE, value, responseStatusInfo.getReasonPhrase());
+                context.buildConstraintViolationWithTemplate(msg).addConstraintViolation();
             }
-        } catch (Throwable ignore) {
+        } catch (Throwable throwable) {
+            context.buildConstraintViolationWithTemplate(FAILED_TO_GET_WITH_EXCEPTION).addConstraintViolation();
+            LOGGER.debug("Failed to validate the specified image catalog URL: " + value, throwable);
+        }
+        return false;
+    }
+
+    private boolean imageCatalogParsable(ConstraintValidatorContext context, String responseContent) throws java.io.IOException {
+        try {
+            OBJECT_MAPPER.readValue(responseContent, CloudbreakImageCatalog.class);
+            return true;
+        } catch (JsonParseException jPE) {
+            context.buildConstraintViolationWithTemplate(INVALID_JSON_IN_RESPONSE).addConstraintViolation();
+        } catch (JsonMappingException jME) {
+            context.buildConstraintViolationWithTemplate(INVALID_JSON_STRUCTURE_IN_RESPONSE)
+                    .addConstraintViolation();
         }
         return false;
     }
