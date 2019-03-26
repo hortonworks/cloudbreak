dbce22c25168be701af659c6afc7f5b06dc066af
David Szabo <dszabo@cloudera.com>
2019-03-26 15:34:10 +0100

    CB-723 Simplify get stacks for autoscale query

--- core/src/main/java/com/sequenceiq/cloudbreak/service/GatewayConfigService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/GatewayConfigService.java
@@ -2,6 +2,7 @@ package com.sequenceiq.cloudbreak.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -9,9 +10,10 @@ import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.client.SaltClientConfig;
 import com.sequenceiq.cloudbreak.controller.exception.NotFoundException;
-import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
 import com.sequenceiq.cloudbreak.domain.SecurityConfig;
+import com.sequenceiq.cloudbreak.domain.projection.AutoscaleStack;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
 
 @Service
@@ -43,6 +45,14 @@ public class GatewayConfigService {
     public String getPrimaryGatewayIp(Stack stack) {
         InstanceMetaData gatewayInstance = stack.getPrimaryGatewayInstance();
         return gatewayInstance == null ? null : getGatewayIp(stack, gatewayInstance);
+    }
+
+    public String getPrimaryGatewayIp(AutoscaleStack stack) {
+        String gatewayIP = Optional.ofNullable(stack.getPublicIp()).orElse(stack.getPrivateIp());
+        if (stack.getUsePrivateIpToTls()) {
+            gatewayIP = stack.getPrivateIp();
+        }
+        return gatewayIP;
     }
 
     public String getGatewayIp(Stack stack, InstanceMetaData gatewayInstance) {
