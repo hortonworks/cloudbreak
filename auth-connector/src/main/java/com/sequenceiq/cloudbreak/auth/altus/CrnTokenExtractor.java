56e39b5682467f258fe83434287141681040222a
lnardai <laci.nardai@gmail.com>
2019-04-05 17:00:15 +0200

    CB-716 make sure only extract valid CRN from CRN header.

--- auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/CrnTokenExtractor.java
+++ auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/CrnTokenExtractor.java
@@ -7,15 +7,13 @@ import org.springframework.security.oauth2.provider.authentication.BearerTokenEx
 import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
-import io.netty.util.internal.StringUtil;
-
 public class CrnTokenExtractor implements TokenExtractor {
 
-    private static final String CRN_HEADER = "x-cdp-actor-crn";
+    public static final String CRN_HEADER = "x-cdp-actor-crn";
 
     @Override
     public Authentication extract(HttpServletRequest request) {
-        if (StringUtil.isNullOrEmpty(request.getHeader(CRN_HEADER))) {
+        if (!Crn.isCrn(request.getHeader(CRN_HEADER))) {
             return new BearerTokenExtractor().extract(request);
         }
         String tokenValue = extractHeaderToken(request, CRN_HEADER);
