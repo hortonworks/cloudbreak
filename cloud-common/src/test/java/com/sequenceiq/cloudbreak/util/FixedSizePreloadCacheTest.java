a0ba030982c3f54a49107526ddf61e81334a84d5
lnardai <laci.nardai@gmail.com>
2019-04-11 20:13:36 +0200

    CB-706 unit test is unstable due too threading.

--- cloud-common/src/test/java/com/sequenceiq/cloudbreak/util/FixedSizePreloadCacheTest.java
+++ cloud-common/src/test/java/com/sequenceiq/cloudbreak/util/FixedSizePreloadCacheTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
+import org.awaitility.Awaitility;
 import org.junit.Test;
 
 public class FixedSizePreloadCacheTest {
@@ -23,7 +25,9 @@ public class FixedSizePreloadCacheTest {
         FixedSizePreloadCache<String> cache = new FixedSizePreloadCache<>(1, items::next);
 
         assertEquals("a", cache.pop());
-        Thread.sleep(5);
+        Awaitility.await()
+                .atMost(1, TimeUnit.SECONDS)
+                .until(() -> cache.size() == 1);
         assertEquals("b", cache.pop());
     }
 }
