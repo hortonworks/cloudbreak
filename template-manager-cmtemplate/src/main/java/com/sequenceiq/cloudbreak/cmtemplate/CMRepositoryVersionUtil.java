2ab82a95421061fa4c4b3b7f95e248a1002d51b8
David Szabo <dszabo@cloudera.com>
2019-04-23 11:51:19 +0200

    CB-331 Keep Host Template on CM b6.3.0 and up

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/CMRepositoryVersionUtil.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/CMRepositoryVersionUtil.java
@@ -9,19 +9,17 @@ import com.sequenceiq.cloudbreak.cloud.model.Versioned;
 public class CMRepositoryVersionUtil {
     public static final Versioned CLOUDERAMANAGER_VERSION_6_2_0 = () -> "6.2.0";
 
+    public static final Versioned CLOUDERAMANAGER_VERSION_6_3_0 = () -> "6.3.0";
+
     private CMRepositoryVersionUtil() {
     }
 
     public static boolean isEnableKerberosSupportedViaClusterDefinition(ClouderaManagerRepo clouderaManagerRepoDetails) {
-        return isClouderaManagerVersionNewOrEqualThanV620(clouderaManagerRepoDetails);
+        return isVersionNewerOrEqualThanLimited(clouderaManagerRepoDetails::getVersion, CLOUDERAMANAGER_VERSION_6_2_0);
     }
 
     public static boolean isKeepHostTemplateSupportedViaClusterDefinition(ClouderaManagerRepo clouderaManagerRepoDetails) {
-        return isClouderaManagerVersionNewOrEqualThanV620(clouderaManagerRepoDetails);
-    }
-
-    private static boolean isClouderaManagerVersionNewOrEqualThanV620(ClouderaManagerRepo clouderaManagerRepoDetails) {
-        return isVersionNewerOrEqualThanLimited(clouderaManagerRepoDetails::getVersion, CLOUDERAMANAGER_VERSION_6_2_0);
+        return isVersionNewerOrEqualThanLimited(clouderaManagerRepoDetails::getVersion, CLOUDERAMANAGER_VERSION_6_3_0);
     }
 
     public static boolean isVersionNewerOrEqualThanLimited(Versioned currentVersion, Versioned limitedAPIVersion) {
