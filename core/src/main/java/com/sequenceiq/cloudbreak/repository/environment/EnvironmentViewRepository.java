27d518065fdc0f0c36dbe0e15b8f0e8afdb57894
Ferenc Schneider <fschneider@hortonworks.com>
2019-04-30 18:12:52 +0200

    CB-1250 Sql query tunning on stack and env lists

--- core/src/main/java/com/sequenceiq/cloudbreak/repository/environment/EnvironmentViewRepository.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/repository/environment/EnvironmentViewRepository.java
@@ -8,6 +8,9 @@ import java.util.Set;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.sequenceiq.cloudbreak.aspect.DisableHasPermission;
 import com.sequenceiq.cloudbreak.aspect.workspace.CheckPermissionsByWorkspaceId;
 import com.sequenceiq.cloudbreak.aspect.workspace.DisableCheckPermissions;
@@ -22,6 +25,11 @@ import com.sequenceiq.cloudbreak.service.EntityType;
 @EntityType(entityClass = EnvironmentView.class)
 @WorkspaceResourceType(resource = WorkspaceResource.ENVIRONMENT)
 public interface EnvironmentViewRepository extends WorkspaceResourceRepository<EnvironmentView, Long> {
+
+    @CheckPermissionsByWorkspaceId(action = READ)
+    @Query("SELECT ev FROM EnvironmentView ev LEFT JOIN FETCH ev.workspace w LEFT JOIN FETCH w.tenant LEFT JOIN FETCH ev.credential "
+            + "LEFT JOIN FETCH ev.datalakeResources WHERE w.id= :id")
+    Set<EnvironmentView> findAllByWorkspaceId(@Param("id") Long workspaceId);
 
     @CheckPermissionsByWorkspaceId(action = READ, workspaceIdIndex = 1)
     Set<EnvironmentView> findAllByNameInAndWorkspaceId(Collection<String> names, Long workspaceId);
