bfbfa8a6d701a6cfd6ea33ae37275c5991022320
Santosh Maniraju <SMANIRAJU@HORTONWORKS.COM>
2019-05-06 17:40:10 +0530

    CB-1201 : Support for DB setup for Oozie, Ranger Added Support for Configuring DB Properties for Oozie, Ranger.

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/database/base/DatabaseType.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/database/base/DatabaseType.java
@@ -2,5 +2,5 @@ package com.sequenceiq.cloudbreak.api.endpoint.v4.database.base;
 
 public enum DatabaseType {
 
-    HIVE, RANGER, DRUID, SUPERSET, OOZIE, AMBARI, CLOUDERA_MANAGER, BEACON, REGISTRY, HIVE_DAS, SQOOP
+    HIVE, RANGER, DRUID, SUPERSET, OOZIE, AMBARI, CLOUDERA_MANAGER, BEACON, REGISTRY, HIVE_DAS
 }
