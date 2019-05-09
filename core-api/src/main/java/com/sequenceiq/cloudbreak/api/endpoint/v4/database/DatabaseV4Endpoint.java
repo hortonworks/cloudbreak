ad95e03fc589d4149f863a03172c2f07194e1e20
Michael Wilson <mike.wilson@cloudera.com>
2019-05-08 22:25:02 -0400

    CB-863. Add dummy redbeams CRUD endpoints.
    Dummy redbeams CRUD endpoints have been added.

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/database/DatabaseV4Endpoint.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/database/DatabaseV4Endpoint.java
@@ -38,7 +38,7 @@ public interface DatabaseV4Endpoint {
     @GET
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.LIST_BY_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.LIST, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "listDatabasesByWorkspace")
     DatabaseV4Responses list(@PathParam("workspaceId") Long workspaceId, @QueryParam("environment") String environment,
             @QueryParam("attachGlobal") @DefaultValue("false") Boolean attachGlobal);
@@ -46,35 +46,35 @@ public interface DatabaseV4Endpoint {
     @POST
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.CREATE_IN_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.CREATE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "createDatabaseInWorkspace")
     DatabaseV4Response create(@PathParam("workspaceId") Long workspaceId, @Valid DatabaseV4Request request);
 
     @GET
     @Path("{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.GET_BY_NAME_IN_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.GET_BY_NAME, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "getDatabaseInWorkspace")
     DatabaseV4Response get(@PathParam("workspaceId") Long workspaceId, @PathParam("name") String name);
 
     @DELETE
     @Path("{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.DELETE_BY_NAME_IN_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.DELETE_BY_NAME, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "deleteDatabaseInWorkspace")
     DatabaseV4Response delete(@PathParam("workspaceId") Long workspaceId, @PathParam("name") String name);
 
     @DELETE
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.DELETE_MULTIPLE_BY_NAME_IN_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.DELETE_MULTIPLE_BY_NAME, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "deleteDatabasesInWorkspace")
     DatabaseV4Responses deleteMultiple(@PathParam("workspaceId") Long workspaceId, Set<String> names);
 
     @GET
     @Path("{name}/request")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = DatabaseOpDescription.GET_REQUEST_IN_WORKSPACE, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
+    @ApiOperation(value = DatabaseOpDescription.GET_REQUEST, produces = ContentType.JSON, notes = Notes.DATABASE_NOTES,
             nickname = "getDatabaseRequestFromNameInWorkspace")
     DatabaseV4Request getRequest(@PathParam("workspaceId") Long workspaceId, @PathParam("name") String name);
 
