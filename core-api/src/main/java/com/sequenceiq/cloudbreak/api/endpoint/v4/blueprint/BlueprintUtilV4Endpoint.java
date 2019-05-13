9058a0fc57bed83b062c139153842e787ada3ad4
Arpad Farsang <afarsang@HW12482.local>
2019-05-13 14:10:09 +0200

    CB-1375 move blueprint recommendation to blueprint-util api

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/BlueprintUtilV4Endpoint.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/BlueprintUtilV4Endpoint.java
@@ -13,6 +13,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.GeneratedCmTemplateV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.RecommendationV4Response;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.ServiceDependencyMatrixV4Response;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.SupportedVersionsV4Response;
 import com.sequenceiq.cloudbreak.doc.ControllerDescription;
@@ -33,7 +34,7 @@ public interface BlueprintUtilV4Endpoint {
     @ApiOperation(value = ConnectorOpDescription.GET_RECOMMENDATION, produces = JSON, notes = Notes.CONNECTOR_NOTES,
             nickname = "getServiceAndDependencies")
     ServiceDependencyMatrixV4Response getServiceAndDependencies(@PathParam("workspaceId") Long workspaceId,
-        @QueryParam("services") Set<String> services, @QueryParam("platform") String platform);
+            @QueryParam("services") Set<String> services, @QueryParam("platform") String platform);
 
     @GET
     @Path("services")
@@ -48,6 +49,15 @@ public interface BlueprintUtilV4Endpoint {
     @ApiOperation(value = ConnectorOpDescription.GET_RECOMMENDATION, produces = JSON, notes = Notes.CONNECTOR_NOTES,
             nickname = "getGeneratedTemplate")
     GeneratedCmTemplateV4Response getGeneratedTemplate(@PathParam("workspaceId") Long workspaceId,
-        @QueryParam("services") Set<String> services, @QueryParam("platform") String platform);
+            @QueryParam("services") Set<String> services, @QueryParam("platform") String platform);
 
+    @GET
+    @Path("recommendation")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = ConnectorOpDescription.GET_RECOMMENDATION, produces = JSON, notes = Notes.CONNECTOR_NOTES,
+            nickname = "createRecommendationForWorkspace")
+    RecommendationV4Response createRecommendation(@PathParam("workspaceId") Long workspaceId,
+            @QueryParam("blueprintName") String blueprintName, @QueryParam("credentialName") String credentialName,
+            @QueryParam("region") String region, @QueryParam("platformVariant") String platformVariant,
+            @QueryParam("availabilityZone") String availabilityZone);
 }
