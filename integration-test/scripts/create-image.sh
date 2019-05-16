74fac855e77d9e3c3cda22fb4d95804002dda806
doktoric <rdoktorics@cloudera.com>
2019-05-16 11:53:08 +0200

    The image build is struggling with a sed problem

--- integration-test/scripts/create-image.sh
+++ integration-test/scripts/create-image.sh
@@ -9,7 +9,7 @@ cp -R ../docker-cloudbreak/ ./integcb/docker-containers/docker-cloudbreak/
 cp ../core/build/libs/cloudbreak.jar ./integcb/docker-containers/docker-cloudbreak
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-cloudbreak:/integcb/docker-containers/docker-cloudbreak \
- busybox /bin/sh -c "sed -i '/cloudbreak-$VERSION/c\ADD cloudbreak.jar /' /integcb/docker-containers/docker-cloudbreak/Dockerfile"
+ busybox /bin/sh -c "sed -i '/cloudbreak-\$VERSION/c\ADD cloudbreak.jar /' /integcb/docker-containers/docker-cloudbreak/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev cloudbreak test image\033[0m\n"
 docker build -t hortonworks/cloudbreak:dev ./integcb/docker-containers/docker-cloudbreak
 
@@ -18,7 +18,7 @@ cp -R ../docker-autoscale/ ./integcb/docker-containers/docker-autoscale/
 cp ../autoscale/build/libs/periscope.jar ./integcb/docker-containers/docker-autoscale
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-autoscale:/integcb/docker-containers/docker-autoscale \
- busybox /bin/sh -c "sed -i '/periscope-$VERSION/c\ADD periscope.jar /' /integcb/docker-containers/docker-autoscale/Dockerfile"
+ busybox /bin/sh -c "sed -i '/periscope-\$VERSION/c\ADD periscope.jar /' /integcb/docker-containers/docker-autoscale/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev autoscale test image\033[0m\n"
 docker build -t hortonworks/cloudbreak-autoscale:dev ./integcb/docker-containers/docker-autoscale
 
@@ -27,7 +27,7 @@ cp -R ../docker-datalake/ ./integcb/docker-containers/docker-datalake/
 cp ../datalake/build/libs/datalake.jar ./integcb/docker-containers/docker-datalake
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-datalake:/integcb/docker-containers/docker-datalake \
- busybox /bin/sh -c "sed -i '/datalake-$VERSION/c\ADD datalake.jar /' /integcb/docker-containers/docker-datalake/Dockerfile"
+ busybox /bin/sh -c "sed -i '/datalake-\$VERSION/c\ADD datalake.jar /' /integcb/docker-containers/docker-datalake/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev datalake test image\033[0m\n"
 docker build -t hortonworks/cloudbreak-datalake:dev ./integcb/docker-containers/docker-datalake
 
@@ -36,7 +36,7 @@ cp -R ../docker-freeipa/ ./integcb/docker-containers/docker-freeipa/
 cp ../freeipa/build/libs/freeipa.jar ./integcb/docker-containers/docker-freeipa
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-freeipa:/integcb/docker-containers/docker-freeipa \
- busybox /bin/sh -c "sed -i '/freeipa-$VERSION/c\ADD freeipa.jar /' /integcb/docker-containers/docker-freeipa/Dockerfile"
+ busybox /bin/sh -c "sed -i '/freeipa-\$VERSION/c\ADD freeipa.jar /' /integcb/docker-containers/docker-freeipa/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev freeipa test image\033[0m\n"
 docker build -t hortonworks/cloudbreak-freeipa:dev ./integcb/docker-containers/docker-freeipa
 
@@ -45,7 +45,7 @@ cp -R ../docker-environment/ ./integcb/docker-containers/docker-environment/
 cp ../environment/build/libs/environment.jar ./integcb/docker-containers/docker-environment
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-environment:/integcb/docker-containers/docker-environment \
- busybox /bin/sh -c "sed -i '/environment-$VERSION/c\ADD environment.jar /' /integcb/docker-containers/docker-environment/Dockerfile"
+ busybox /bin/sh -c "sed -i '/environment-\$VERSION/c\ADD environment.jar /' /integcb/docker-containers/docker-environment/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev environment test image\033[0m\n"
 docker build -t hortonworks/cloudbreak-environment:dev ./integcb/docker-containers/docker-environment
 
@@ -54,6 +54,6 @@ cp -R  ../docker-redbeams/ ./integcb/docker-containers/docker-redbeams/
 cp ../redbeams/build/libs/redbeams.jar ./integcb/docker-containers/docker-redbeams
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-redbeams:/integcb/docker-containers/docker-redbeams \
- busybox /bin/sh -c "sed -i '/redbeams-$VERSION/c\ADD redbeams.jar /' /integcb/docker-containers/docker-redbeams/Dockerfile"
+ busybox /bin/sh -c "sed -i '/redbeams-\$VERSION/c\ADD redbeams.jar /' /integcb/docker-containers/docker-redbeams/Dockerfile"
 echo -e "\n\033[1;96m--- Build dev redbeams test image\033[0m\n"
 docker build -t hortonworks/cloudbreak-redbeams:dev ./integcb/docker-containers/docker-redbeams
