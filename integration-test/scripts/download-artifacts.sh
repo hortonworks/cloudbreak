59a3fa0eca6f1ceda703895d4904aa32b2105e85
doktoric <rdoktorics@cloudera.com>
2019-05-16 09:00:59 +0200

    fixed script

--- integration-test/scripts/download-artifacts.sh
+++ integration-test/scripts/download-artifacts.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl ${REPO_URL}/com/sequenceiq/cloudbreak/$(VERSION)/cloudbreak-$(VERSION).jar -o ../core/build/libs/cloudbreak.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/environment/$(VERSION)/environment-$(VERSION).jar -o ../environment/build/libs/environment.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/redbeams/$(VERSION)/redbeams-$(VERSION).jar -o ../redbeams/build/libs/redbeams.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/datalake/$(VERSION)/datalake-$(VERSION).jar -o ../datalake/build/libs/datalake.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/freeipa/$(VERSION)/freeipa-$(VERSION).jar -o ../freeipa/build/libs/freeipa.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/periscope/$(VERSION)/periscope-$(VERSION).jar -o ../autoscale/build/libs/periscope.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/mock-caas/$(VERSION)/mock-caas-$(VERSION).jar -o ../mock-caas/build/libs/mock-caas.jar --create-dirs
-curl ${REPO_URL}/com/sequenceiq/cloudbreak-integration-test/$(VERSION)/cloudbreak-integration-test-$(VERSION).jar -o ../integration-test/build/libs/cloudbreak-integration-test.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/cloudbreak/${VERSION}/cloudbreak-${VERSION}.jar -o ../core/build/libs/cloudbreak.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/environment/${VERSION}/environment-${VERSION}.jar -o ../environment/build/libs/environment.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/redbeams/${VERSION}/redbeams-${VERSION}.jar -o ../redbeams/build/libs/redbeams.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/datalake/${VERSION}/datalake-${VERSION}.jar -o ../datalake/build/libs/datalake.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/freeipa/${VERSION}/freeipa-${VERSION}.jar -o ../freeipa/build/libs/freeipa.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/periscope/${VERSION}/periscope-${VERSION}.jar -o ../autoscale/build/libs/periscope.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/mock-caas/${VERSION}/mock-caas-${VERSION}.jar -o ../mock-caas/build/libs/mock-caas.jar --create-dirs
+curl ${REPO_URL}/com/sequenceiq/cloudbreak-integration-test/${VERSION}/cloudbreak-integration-test-${VERSION}.jar -o ../integration-test/build/libs/cloudbreak-integration-test.jar --create-dirs
