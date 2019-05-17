1951bd0f30826fc53181701e6118c9a2e5031ffd
doktoric <rdoktorics@cloudera.com>
2019-05-17 08:43:31 +0200

    CB-1351 the bp name is required on the response

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/requests/BlueprintV4Request.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/requests/BlueprintV4Request.java
@@ -3,10 +3,14 @@ package com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.requests;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.BlueprintV4Base;
+import com.sequenceiq.cloudbreak.doc.ModelDescriptions;
 import com.sequenceiq.cloudbreak.doc.ModelDescriptions.BlueprintModelDescription;
 import com.sequenceiq.cloudbreak.validation.ValidHttpContentSize;
 
@@ -18,6 +22,12 @@ import io.swagger.annotations.ApiModelProperty;
 @JsonInclude(Include.NON_NULL)
 public class BlueprintV4Request extends BlueprintV4Base {
 
+    @ApiModelProperty(value = ModelDescriptions.NAME)
+    @Size(max = 100, min = 1, message = "The length of the blueprint's name has to be in range of 1 to 100 and should not contain semicolon "
+            + "and percentage character.")
+    @Pattern(regexp = "^[^;\\/%]*$")
+    private String name;
+
     @ValidHttpContentSize
     @ApiModelProperty(BlueprintModelDescription.URL)
     private String url;
@@ -25,6 +35,14 @@ public class BlueprintV4Request extends BlueprintV4Base {
     private Set<String> services = new HashSet<>();
 
     private String platform;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getUrl() {
         return url;
