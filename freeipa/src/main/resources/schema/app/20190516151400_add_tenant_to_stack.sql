55f9026f285207dcea4bc3818854483b0975f5f4
Abhishek Purohit <apurohit@cloudera.com>
2019-05-17 08:58:03 -0700

    Adjusted uniqueness and removed one of the index

--- freeipa/src/main/resources/schema/app/20190516151400_add_tenant_to_stack.sql
+++ freeipa/src/main/resources/schema/app/20190516151400_add_tenant_to_stack.sql
@@ -12,21 +12,16 @@
 ALTER TABLE stack
     ADD tenant VARCHAR(255);
 
-CREATE UNIQUE INDEX tenant_uindex
+CREATE INDEX tenant_uindex
     ON stack (tenant);
 
-CREATE UNIQUE INDEX environment_tenant_uindex
-    ON stack (name, environment, tenant);
-
-CREATE UNIQUE INDEX stack_name_environment_tenant_uindex
-    ON stack (name, environment, tenant);
+CREATE INDEX environment_tenant_uindex
+    ON stack (environment, tenant);
 
 -- //@UNDO
 
 DROP INDEX tenant_uindex;
 
 DROP INDEX environment_tenant_uindex;
-
-DROP INDEX stack_name_environment_tenant_uindex;
 
 ALTER TABLE stack DROP COLUMN tenant;
