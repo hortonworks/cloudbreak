4b83ff4b785a9dc94abbada586cb4805885ea3cb
Abhishek Purohit <apurohit@cloudera.com>
2019-05-20 16:07:02 -0700

    removed extra methods for now, not used

--- freeipa/src/main/java/com/sequenceiq/freeipa/repository/StackRepository.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/repository/StackRepository.java
@@ -16,17 +16,9 @@ public interface StackRepository extends CrudRepository<Stack, Long> {
     @Query("SELECT s FROM Stack s LEFT JOIN FETCH s.instanceGroups ig LEFT JOIN FETCH ig.instanceMetaData WHERE s.id= :id ")
     Optional<Stack> findOneWithLists(@Param("id") Long id);
 
-    Optional<Stack> findByNameAndEnvironment(String name, String environment);
-
-    @Query("SELECT s FROM Stack s WHERE s.accountId = :accountId and s.environment = :environment ")
-    Optional<Stack> findByAccountIdAndEnvironment(String accountId, String environment);
-
     @Query("SELECT s FROM Stack s WHERE s.accountId = :accountId and s.environment = :environment and s.name = :name")
     Optional<Stack> findByAccountIdEnvironmentAndName(
             @Param("accountId") String accountId,
             @Param("environment")  String environment,
             @Param("name") String name);
-
-    @Query("SELECT s FROM Stack s WHERE s.accountId = :accountId ")
-    Optional<Stack> findByAccountId(String accountId);
 }
