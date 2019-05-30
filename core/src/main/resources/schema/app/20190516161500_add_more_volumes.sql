9e48698b3de39ecb76d9e665b62aedcc05cf3568
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2019-05-30 17:32:25 +0200

    CB-1339 volumes migration fix

--- core/src/main/resources/schema/app/20190516161500_add_more_volumes.sql
+++ core/src/main/resources/schema/app/20190516161500_add_more_volumes.sql
@@ -1,8 +1,6 @@
 -- // add more volumes
 -- Migration SQL that makes the change goes here.
 
-ALTER TABLE IF EXISTS template DROP COLUMN volumeSize, DROP COLUMN volumeCount, DROP COLUMN volumeType;
-
 CREATE TABLE volumetemplate (
     id bigint NOT NULL,
     volumecount integer,
@@ -20,11 +18,20 @@ CREATE SEQUENCE volume_template_id_seq START WITH 1
 ALTER TABLE volumetemplate
    ALTER COLUMN id SET DEFAULT nextval ('volume_template_id_seq');
 
+INSERT INTO volumetemplate (volumecount, volumesize, volumetype, template_id) SELECT volumecount, volumesize, volumetype, id FROM template;
+
+ALTER TABLE IF EXISTS template DROP COLUMN volumesize, DROP COLUMN volumecount, DROP COLUMN volumetype;
+
 -- //@UNDO
 -- SQL to undo the change goes here.
+
+ALTER TABLE IF EXISTS template ADD COLUMN volumesize integer, ADD COLUMN volumecount integer, ADD COLUMN volumetype character varying(255);
+
+UPDATE template
+SET volumesize=subquery.volumesize, volumecount=subquery.volumecount, volumetype=subquery.volumetype
+FROM (SELECT volumecount, volumetype, volumesize, template_id FROM volumetemplate) AS subquery
+WHERE id = subquery.template_id;
 
 DROP TABLE IF EXISTS volumetemplate;
 
 DROP SEQUENCE IF EXISTS volume_template_id_seq;
-
-ALTER TABLE IF EXISTS template ADD COLUMN volumeSize integer, ADD COLUMN volumeCount integer, ADD COLUMN volumeType character varying(255);
