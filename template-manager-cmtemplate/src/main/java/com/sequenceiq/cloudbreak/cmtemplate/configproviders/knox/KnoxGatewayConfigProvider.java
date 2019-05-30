7ad4c2cd5a0bb81245cf895f8a1e0f37ec4cf5dc
Krisztian Horvath <keyki.kk@gmail.com>
2019-05-30 09:03:58 +0200

    CB-1020 Knox does not require ldap parameters anymore

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/knox/KnoxGatewayConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/knox/KnoxGatewayConfigProvider.java
@@ -5,7 +5,6 @@ import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.c
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,7 +19,6 @@ import com.sequenceiq.cloudbreak.common.type.InstanceGroupType;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 import com.sequenceiq.cloudbreak.template.views.GatewayView;
 import com.sequenceiq.cloudbreak.template.views.HostgroupView;
-import com.sequenceiq.cloudbreak.template.views.LdapView;
 
 @Component
 public class KnoxGatewayConfigProvider extends AbstractRoleConfigConfigProvider {
@@ -37,44 +35,16 @@ public class KnoxGatewayConfigProvider extends AbstractRoleConfigConfigProvider 
 
     private static final String GATEWAY_PATH = "gateway_path";
 
-    // This needs to be removed once Knox CSD with PAM is merged
-    private static final String SEC_GROUP_MAPPING = "hadoop_security_group_mapping_class";
-
-    // This needs to be removed once Knox CSD with PAM is merged
-    private static final String LDAP_USR = "ldap_connection_user";
-
-    // This needs to be removed once Knox CSD with PAM is merged
-    private static final String LDAP_PWD = "ldap_connection_password";
-
-    // This needs to be removed once Knox CSD with PAM is merged
-    private static final String LDAP_URL = "ldap_connection_url";
-
     @Override
     protected List<ApiClusterTemplateConfig> getRoleConfig(String roleType, HostgroupView hostGroupView, TemplatePreparationObject source) {
         switch (roleType) {
             case "KNOX_GATEWAY":
                 List<ApiClusterTemplateConfig> config = new ArrayList<>();
-
                 GatewayView gateway = source.getGatewayView();
                 if (gateway != null) {
-                    config.add(config(SEC_GROUP_MAPPING, "org.apache.hadoop.security.ShellBasedUnixGroupsMapping"));
                     config.add(config(KNOX_MASTER_SECRET, gateway.getMasterSecret()));
                     config.add(config(GATEWAY_PATH, gateway.getPath()));
-
-                    // This needs to be removed once Knox CSD with PAM is merged
-                    Optional<LdapView> ldapConfigOpt = source.getLdapConfig();
-                    if (ldapConfigOpt.isPresent()) {
-                        LdapView ldapView = ldapConfigOpt.get();
-                        config.add(config(LDAP_URL, String.format("%s://%s:%d", ldapView.getProtocol(), ldapView.getServerHost(), ldapView.getServerPort())));
-                        config.add(config(LDAP_USR, ldapView.getBindDn()));
-                        config.add(config(LDAP_PWD, ldapView.getBindPassword()));
-                    } else {
-                        config.add(config(LDAP_URL, "ldap://localhost:3389"));
-                        config.add(config(LDAP_USR, "admin"));
-                        config.add(config(LDAP_PWD, "admin-password"));
-                    }
                 }
-
                 return config;
             default:
                 return List.of();
