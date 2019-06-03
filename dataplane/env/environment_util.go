7d7c5034ce4663ab5308d06bb8bdca6a09bda287
David Szabo <dszabo@cloudera.com>
2019-06-03 09:12:27 +0200

    CB-1466, CB-1467 Environment API and helper

---
+++ dataplane/env/environment_util.go
@@ -0,0 +1,24 @@
+package env
+
+import (
+	"fmt"
+
+	"github.com/hortonworks/cb-cli/dataplane/api-environment/client/v1env"
+	"github.com/hortonworks/cb-cli/dataplane/oauth"
+	"github.com/hortonworks/dp-cli-common/utils"
+	"github.com/urfave/cli"
+)
+
+func GetEnvirontmentCrnByName(c *cli.Context, environment string) string {
+	envClient := oauth.Environment(*oauth.NewEnvironmentClientFromContext(c)).Environment
+	resp, err := envClient.V1env.GetEnvironmentV1(v1env.NewGetEnvironmentV1Params().WithName(environment))
+	if err != nil {
+		utils.LogErrorAndExit(err)
+	}
+	environmentDetails := resp.Payload
+	if len(environmentDetails.ID) == 0 {
+		errmsg := fmt.Sprintf("Failed to get the CRN of environment: %s", environment)
+		utils.LogErrorMessageAndExit(errmsg)
+	}
+	return environmentDetails.ID
+}
