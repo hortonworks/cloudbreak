76266f18760110da5a4adda39af2ccf6c535aaf8
Mészáros Gergely <grigorij9026@gmail.com>
2019-06-04 11:22:17 +0200

    CB-1483 env edit

---
+++ environment/src/main/java/com/sequenceiq/environment/environment/dto/EnvironmentChangeCredentialDto.java
@@ -0,0 +1,41 @@
+package com.sequenceiq.environment.environment.dto;
+
+public class EnvironmentChangeCredentialDto {
+
+    private String credentialName;
+
+    public EnvironmentChangeCredentialDto(String credentialName) {
+        this.credentialName = credentialName;
+    }
+
+    public String getCredentialName() {
+        return credentialName;
+    }
+
+    public void setCredentialName(String credentialName) {
+        this.credentialName = credentialName;
+    }
+
+    public static final class EnvironmentChangeCredentialDtoBuilder {
+
+        private String credentialName;
+
+        private EnvironmentChangeCredentialDtoBuilder() {
+        }
+
+        public static EnvironmentChangeCredentialDtoBuilder anEnvironmentChangeCredentialDto() {
+            return new EnvironmentChangeCredentialDtoBuilder();
+        }
+
+        public EnvironmentChangeCredentialDtoBuilder withCredentialName(String credentialName) {
+            this.credentialName = credentialName;
+            return this;
+        }
+
+        public EnvironmentChangeCredentialDto build() {
+            return new EnvironmentChangeCredentialDto(credentialName);
+        }
+
+    }
+
+}
