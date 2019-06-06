df6b757895c536eb55bb6a98f7e89a6ee8f9faa4
Attila Palfi <apalfi@hortonworks.com>
2019-06-06 15:46:47 +0200

    CB-1717 - api client for redbeams

---
+++ redbeams-api/src/main/java/com/sequenceiq/redbeams/client/RedbeamsApiClientConfig.java
@@ -0,0 +1,48 @@
+package com.sequenceiq.redbeams.client;
+
+import javax.ws.rs.client.WebTarget;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sequenceiq.cloudbreak.client.ThreadLocalUserCrnWebTargetBuilder;
+import com.sequenceiq.cloudbreak.client.UserCrnClientRequestFilter;
+import com.sequenceiq.cloudbreak.client.WebTargetEndpointFactory;
+import com.sequenceiq.redbeams.api.RedbeamsApi;
+import com.sequenceiq.redbeams.api.endpoint.v4.database.DatabaseV4Endpoint;
+import com.sequenceiq.redbeams.api.endpoint.v4.databaseserver.DatabaseServerV4Endpoint;
+
+@Configuration
+public class RedbeamsApiClientConfig {
+
+    private final UserCrnClientRequestFilter userCrnClientRequestFilter;
+
+    public RedbeamsApiClientConfig(UserCrnClientRequestFilter userCrnClientRequestFilter) {
+        this.userCrnClientRequestFilter = userCrnClientRequestFilter;
+    }
+
+    @Bean
+    @ConditionalOnBean(RedbeamsApiClientParams.class)
+    public WebTarget redbeamsApiClientWebTarget(RedbeamsApiClientParams redbeamsApiClientParams) {
+        return new ThreadLocalUserCrnWebTargetBuilder(redbeamsApiClientParams.getServiceUrl())
+                .withCertificateValidation(redbeamsApiClientParams.isCertificateValidation())
+                .withIgnorePreValidation(redbeamsApiClientParams.isIgnorePreValidation())
+                .withDebug(redbeamsApiClientParams.isRestDebug())
+                .withClientRequestFilter(userCrnClientRequestFilter)
+                .withApiRoot(RedbeamsApi.API_ROOT_CONTEXT)
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "redbeamsApiClientWebTarget")
+    DatabaseV4Endpoint databaseV4Endpoint(WebTarget redbeamsApiClientWebTarget) {
+        return new WebTargetEndpointFactory().createEndpoint(redbeamsApiClientWebTarget, DatabaseV4Endpoint.class);
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "redbeamsApiClientWebTarget")
+    DatabaseServerV4Endpoint databaseServerV4Endpoint(WebTarget redbeamsApiClientWebTarget) {
+        return new WebTargetEndpointFactory().createEndpoint(redbeamsApiClientWebTarget, DatabaseServerV4Endpoint.class);
+    }
+}
