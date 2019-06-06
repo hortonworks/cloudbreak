480008526ff97651fc04350a2500b4273551c404
Mészáros Gergely <grigorij9026@gmail.com>
2019-06-06 15:32:06 +0200

    CB-1415 env create with existing network fix

--- environment/src/main/java/com/sequenceiq/environment/network/NetworkService.java
+++ environment/src/main/java/com/sequenceiq/environment/network/NetworkService.java
@@ -1,15 +1,19 @@
 package com.sequenceiq.environment.network;
 
 import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
 
 import org.springframework.stereotype.Service;
 
+import com.sequenceiq.cloudbreak.auth.altus.Crn;
 import com.sequenceiq.environment.CloudPlatform;
 import com.sequenceiq.environment.environment.domain.Environment;
 import com.sequenceiq.environment.network.domain.BaseNetwork;
-import com.sequenceiq.environment.network.v1.converter.EnvironmentNetworkConverter;
 import com.sequenceiq.environment.network.dto.NetworkDto;
 import com.sequenceiq.environment.network.repository.BaseNetworkRepository;
+import com.sequenceiq.environment.network.v1.converter.EnvironmentNetworkConverter;
 
 @Service
 public class NetworkService {
@@ -24,12 +28,14 @@ public class NetworkService {
         this.environmentNetworkConverterMap = environmentNetworkConverterMap;
     }
 
-    public BaseNetwork createNetworkIfPossible(Environment environment, NetworkDto networkDto, CloudPlatform cloudPlatform) {
+    public BaseNetwork createNetworkIfPossible(Environment environment, NetworkDto networkDto, CloudPlatform cloudPlatform, String accountId) {
         BaseNetwork network = null;
         if (networkDto != null) {
             EnvironmentNetworkConverter environmentNetworkConverter = environmentNetworkConverterMap.get(cloudPlatform);
             if (environmentNetworkConverter != null) {
                 BaseNetwork baseNetwork = environmentNetworkConverter.convert(networkDto, environment);
+                baseNetwork.setResourceCrn(createCRN(accountId));
+                baseNetwork.setAccountId(accountId);
                 network = save(baseNetwork);
             }
         }
@@ -37,8 +43,19 @@ public class NetworkService {
     }
 
     @SuppressWarnings("unchecked")
-    public BaseNetwork save(BaseNetwork awsNetwork) {
-        Object saved = networkRepository.save(awsNetwork);
+    public BaseNetwork save(BaseNetwork network) {
+        Object saved = networkRepository.save(network);
         return (BaseNetwork) saved;
     }
+
+    private String createCRN(@Nonnull String accountId) {
+        return Crn.builder()
+                .setService(Crn.Service.ENVIRONMENTS)
+                .setAccountId(accountId)
+                .setResourceType(Crn.ResourceType.NETWORK)
+                .setResource(UUID.randomUUID().toString())
+                .build()
+                .toString();
+    }
+
 }
