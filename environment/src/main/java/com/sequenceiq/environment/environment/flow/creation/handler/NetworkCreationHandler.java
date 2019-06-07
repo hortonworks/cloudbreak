d0ed76dea4c36bed9729e0e670f6435b37b4ac75
bbihari <bbihari@hortonworks.com>
2019-06-07 16:52:02 +0200

    CB-1765 Save availability-zone info in the environment network properties

--- environment/src/main/java/com/sequenceiq/environment/environment/flow/creation/handler/NetworkCreationHandler.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/flow/creation/handler/NetworkCreationHandler.java
@@ -3,12 +3,27 @@ package com.sequenceiq.environment.environment.flow.creation.handler;
 import static com.sequenceiq.environment.environment.flow.creation.event.EnvCreationHandlerSelectors.CREATE_NETWORK_EVENT;
 import static com.sequenceiq.environment.environment.flow.creation.event.EnvCreationStateSelectors.START_RDBMS_CREATION_EVENT;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.cloud.model.CloudNetwork;
+import com.sequenceiq.cloudbreak.cloud.model.CloudNetworks;
+import com.sequenceiq.cloudbreak.cloud.model.CloudSubnet;
 import com.sequenceiq.environment.environment.EnvironmentStatus;
 import com.sequenceiq.environment.environment.dto.EnvironmentDto;
+import com.sequenceiq.environment.environment.dto.EnvironmentDtoConverter;
 import com.sequenceiq.environment.environment.flow.creation.event.EnvCreationEvent;
 import com.sequenceiq.environment.environment.service.EnvironmentService;
+import com.sequenceiq.environment.network.NetworkService;
+import com.sequenceiq.environment.network.dto.AwsParams;
+import com.sequenceiq.environment.network.dto.NetworkDto;
+import com.sequenceiq.environment.platformresource.PlatformParameterService;
+import com.sequenceiq.environment.platformresource.PlatformResourceRequest;
 import com.sequenceiq.flow.reactor.api.event.EventSender;
 import com.sequenceiq.flow.reactor.api.handler.EventSenderAwareHandler;
 
@@ -19,9 +34,22 @@ public class NetworkCreationHandler extends EventSenderAwareHandler<EnvironmentD
 
     private final EnvironmentService environmentService;
 
-    protected NetworkCreationHandler(EventSender eventSender, EnvironmentService environmentService) {
+    private final PlatformParameterService platformParameterService;
+
+    private final NetworkService networkService;
+
+    private final EnvironmentDtoConverter environmentDtoConverter;
+
+    protected NetworkCreationHandler(EventSender eventSender,
+            EnvironmentService environmentService,
+            PlatformParameterService platformParameterService,
+            NetworkService networkService,
+            EnvironmentDtoConverter environmentDtoConverter) {
         super(eventSender);
         this.environmentService = environmentService;
+        this.platformParameterService = platformParameterService;
+        this.networkService = networkService;
+        this.environmentDtoConverter = environmentDtoConverter;
     }
 
     @Override
@@ -30,14 +58,42 @@ public class NetworkCreationHandler extends EventSenderAwareHandler<EnvironmentD
         // TODO: create network
         environmentService.findById(environmentDto.getId()).ifPresent(environment -> {
             environment.setStatus(EnvironmentStatus.NETWORK_CREATION_IN_PROGRESS);
+            networkService.decorateNetworkWithSubnetMeta(environment.getNetwork().getId(),
+                    getSubnetMetas(environmentDtoConverter.environmentToDto(environment)));
             environmentService.save(environment);
         });
         sleepForTestPurpose();
+
         EnvCreationEvent envCreationEvent = EnvCreationEvent.EnvCreationEventBuilder.anEnvCreationEvent()
                 .withResourceId(environmentDto.getResourceId())
                 .withSelector(START_RDBMS_CREATION_EVENT.selector())
                 .build();
         eventSender().sendEvent(envCreationEvent, environmentDtoEvent.getHeaders());
+    }
+
+    private Map<String, CloudSubnet> getSubnetMetas(EnvironmentDto environmentDto) {
+        String regionName = environmentDto.getRegionSet().iterator().next().getName();
+        PlatformResourceRequest prr = new PlatformResourceRequest();
+        prr.setCredential(environmentDto.getCredential());
+        prr.setCloudPlatform(environmentDto.getCloudPlatform());
+        prr.setRegion(regionName);
+        CloudNetworks cnsr = platformParameterService.getCloudNetworks(prr);
+        Set<CloudNetwork> cns = cnsr.getCloudNetworkResponses().get(regionName);
+        return cns.stream()
+                .filter(n -> n.getId().equals(getVpcId(environmentDto)))
+                .findFirst()
+                .map(CloudNetwork::getSubnetsMeta)
+                .stream()
+                .flatMap(Set::stream)
+                .filter(sn -> environmentDto.getNetwork().getSubnetIds().contains(sn.getId()))
+                .collect(Collectors.toMap(CloudSubnet::getId, Function.identity()));
+    }
+
+    private String getVpcId(EnvironmentDto environmentDto) {
+        return Optional.ofNullable(environmentDto.getNetwork())
+                .map(NetworkDto::getAws)
+                .map(AwsParams::getVpcId)
+                .orElse(null);
     }
 
     @Override
