04921bf0d26aa8c89831333a9a72b05d93a47710
doktoric <rdoktorics@cloudera.com>
2019-06-07 17:02:39 +0200

    CRB-848 Create new environment including FreeIPA instance

---
+++ environment/src/main/java/com/sequenceiq/environment/configuration/SupportedPlatfroms.java
@@ -0,0 +1,31 @@
+package com.sequenceiq.environment.configuration;
+
+import java.util.Set;
+
+public class SupportedPlatfroms {
+
+    private final Set<String> freeIpa;
+
+    private final Set<String> redbeams;
+
+    public SupportedPlatfroms(String[] freeIpa, String[] redbeams) {
+        this.freeIpa = freeIpa == null ? Set.of() : Set.of(freeIpa);
+        this.redbeams = redbeams == null ? Set.of() : Set.of(redbeams);
+    }
+
+    public Set<String> freeIpa() {
+        return freeIpa;
+    }
+
+    public Set<String> redbeams() {
+        return redbeams;
+    }
+
+    public boolean supportedPlatformForFreeIpa(String platform) {
+        return freeIpa.contains(platform);
+    }
+
+    public boolean supportedPlatformForRedBeams(String platform) {
+        return redbeams.contains(platform);
+    }
+}
