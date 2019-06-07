ef44e7ba80acbfc9433c1e9ac74f3c0e7292423c
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-06-07 12:53:31 +0200

    CB-1745 Get network from environment if it is not provided by request

--- core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/DistroXV1RequestToStackV4RequestConverter.java
+++ core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/DistroXV1RequestToStackV4RequestConverter.java
@@ -10,11 +10,14 @@ import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.StackV4Request;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.cluster.sharedservice.SharedServiceV4Request;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.environment.placement.PlacementSettingsV4Request;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.network.NetworkV4Request;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.tags.TagsV4Request;
 import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
+import com.sequenceiq.cloudbreak.exception.BadRequestException;
 import com.sequenceiq.cloudbreak.service.environment.EnvironmentClientService;
 import com.sequenceiq.cloudbreak.service.workspace.WorkspaceService;
 import com.sequenceiq.distrox.api.v1.distrox.model.DistroXV1Request;
+import com.sequenceiq.distrox.api.v1.distrox.model.network.NetworkV1Request;
 import com.sequenceiq.distrox.api.v1.distrox.model.sharedservice.SdxV1Request;
 import com.sequenceiq.distrox.api.v1.distrox.model.tags.TagsV1Request;
 import com.sequenceiq.environment.api.v1.environment.model.response.DetailedEnvironmentResponse;
@@ -65,7 +68,7 @@ public class DistroXV1RequestToStackV4RequestConverter {
         request.setImage(getIfNotNull(source.getImage(), imageConverter::convert));
         request.setCluster(getIfNotNull(source.getCluster(), clusterConverter::convert));
         request.setInstanceGroups(getIfNotNull(source.getInstanceGroups(), instanceGroupConverter::convertTo));
-        request.setNetwork(getIfNotNull(source.getNetwork(), networkConverter::convert));
+        request.setNetwork(getNetwork(source.getNetwork(), environment));
         request.setAws(getIfNotNull(source.getAws(), stackParameterConverter::convert));
         request.setAzure(getIfNotNull(source.getAzure(), stackParameterConverter::convert));
         request.setPlacement(getPlacement(environment));
@@ -79,6 +82,44 @@ public class DistroXV1RequestToStackV4RequestConverter {
         request.setOpenstack(null);
         request.setYarn(null);
         return request;
+    }
+
+    private NetworkV4Request getNetwork(NetworkV1Request networkRequest, DetailedEnvironmentResponse environment) {
+        NetworkV4Request network = getIfNotNull(networkRequest, networkConverter::convert);
+        if (network == null) {
+            network = getIfNotNull(environment.getNetwork(), networkConverter::convert);
+        }
+        validateSubnetIds(network, environment);
+        return network;
+    }
+
+    private void validateSubnetIds(NetworkV4Request network, DetailedEnvironmentResponse environment) {
+        switch (environment.getCloudPlatform()) {
+            case "AWS":
+                validateSubnet(network, environment, network.getAws().getSubnetId());
+                validateVpc(environment, environment.getNetwork().getAws().getVpcId(), network.getAws().getVpcId());
+                break;
+            case "AZURE":
+                validateSubnet(network, environment, network.getAzure().getSubnetId());
+                validateVpc(environment, environment.getNetwork().getAzure().getNetworkId(), network.getAzure().getNetworkId());
+                break;
+            default:
+        }
+
+    }
+
+    private void validateVpc(DetailedEnvironmentResponse environment, String environmentVpcId, String vpcId) {
+        if (!vpcId.equals(environmentVpcId)) {
+            throw new BadRequestException(String.format("The given vpc id (%s) is not match with the Environment vpc id (%s)",
+                    vpcId, environmentVpcId));
+        }
+    }
+
+    private void validateSubnet(NetworkV4Request network, DetailedEnvironmentResponse environment, String subnetId) {
+        if (!environment.getNetwork().getSubnetIds().contains(subnetId)) {
+            throw new BadRequestException(String.format("The given subnet id (%s) is not attached to the Environment (%s)",
+                    network.getAws().getSubnetId(), environment.getName()));
+        }
     }
 
     public DistroXV1Request convert(StackV4Request source) {
