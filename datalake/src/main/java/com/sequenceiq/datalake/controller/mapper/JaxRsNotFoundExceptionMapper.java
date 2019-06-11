96a4fc4637982a5cf852a3f1eb3b69800ea7aa71
sodre90 <perdos@hortonworks.com>
2019-06-11 10:57:24 +0200

    CB-1700 SDX uses new environment - fix rebase issue

--- datalake/src/main/java/com/sequenceiq/datalake/controller/mapper/JaxRsNotFoundExceptionMapper.java
+++ datalake/src/main/java/com/sequenceiq/datalake/controller/mapper/JaxRsNotFoundExceptionMapper.java
@@ -1,15 +1,14 @@
 package com.sequenceiq.datalake.controller.mapper;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
 import org.springframework.stereotype.Component;
 
-import com.sequenceiq.cloudbreak.exception.NotFoundException;
-
 @Provider
 @Component
-public class SpringNotFoundExceptionMapper extends BaseExceptionMapper<NotFoundException> {
+public class JaxRsNotFoundExceptionMapper extends BaseExceptionMapper<NotFoundException> {
 
     @Override
     Status getResponseStatus() {
