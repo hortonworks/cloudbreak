130f8fc3a3cb11c7cbd2cac067852deabf0e9f9d
Krisztian Horvath <keyki.kk@gmail.com>
2019-06-11 15:05:41 +0200

    Revert "CB-1161: remove subnetids column"
    This reverts commit 2584614b3d789b251c12218bf84bc2d5624ba1dc.

--- core/src/main/resources/schema/app/20190611081532_CB-1161-remove-subnetids-column.sql
+++
@@ -1,8 +0,0 @@
--- // CB-1161-remove-subnetids-column
--- Migration SQL that makes the change goes here.
-ALTER TABLE environment_network DROP COLUMN IF EXISTS subnetids;
-
--- //@UNDO
--- SQL to undo the change goes here.
-ALTER TABLE environment_network ADD COLUMN IF NOT EXISTS subnetids TEXT NOT NULL;
-
