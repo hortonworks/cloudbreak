9450dc701d9e4637f00df3b91d99541e415803d6
Doroszlai, Attila <adoroszlai@apache.org>
2019-06-11 20:42:30 +0200

    CB-1804. Inject idbroker_master_secret

--- template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/knox/KnoxGatewayConfigProviderTest.java
+++ template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/knox/KnoxGatewayConfigProviderTest.java
@@ -1,9 +1,12 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.knox;
 
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,6 +21,7 @@ import com.sequenceiq.cloudbreak.common.type.InstanceGroupType;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.gateway.Gateway;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject.Builder;
+import com.sequenceiq.cloudbreak.template.model.GeneralClusterConfigs;
 import com.sequenceiq.cloudbreak.template.views.HostgroupView;
 import com.sequenceiq.cloudbreak.util.FileReaderUtils;
 
@@ -101,6 +105,52 @@ public class KnoxGatewayConfigProviderTest {
         Map<String, ApiClusterTemplateService> additionalServices = underTest.getAdditionalServices(cmTemplateProcessor, preparationObject);
 
         assertTrue(additionalServices.isEmpty());
+    }
+
+    @Test
+    public void roleConfigsWithGateway() {
+        HostgroupView any = mock(HostgroupView.class);
+        Gateway gateway = new Gateway();
+        gateway.setKnoxMasterSecret("admin");
+        gateway.setPath("/a/b/c");
+        TemplatePreparationObject source = Builder.builder().withGateway(gateway, "key").build();
+
+        assertEquals(
+                List.of(
+                        config("idbroker_master_secret", gateway.getKnoxMasterSecret())
+                ),
+                underTest.getRoleConfig(KnoxRoles.IDBROKER, any, source)
+        );
+        assertEquals(
+                List.of(
+                        config("gateway_master_secret", gateway.getKnoxMasterSecret()),
+                        config("gateway_path", gateway.getPath())
+                ),
+                underTest.getRoleConfig(KnoxRoles.KNOX_GATEWAY, any, source)
+        );
+        assertEquals(List.of(), underTest.getRoleConfig("NAMENODE", any, source));
+    }
+
+    @Test
+    public void roleConfigsWithoutGateway() {
+        HostgroupView any = mock(HostgroupView.class);
+        GeneralClusterConfigs gcc = new GeneralClusterConfigs();
+        gcc.setPassword("secret");
+        TemplatePreparationObject source = Builder.builder().withGeneralClusterConfigs(gcc).build();
+
+        assertEquals(
+                List.of(
+                        config("idbroker_master_secret", gcc.getPassword())
+                ),
+                underTest.getRoleConfig(KnoxRoles.IDBROKER, any, source)
+        );
+        assertEquals(
+                List.of(
+                        config("gateway_master_secret", gcc.getPassword())
+                ),
+                underTest.getRoleConfig(KnoxRoles.KNOX_GATEWAY, any, source)
+        );
+        assertEquals(List.of(), underTest.getRoleConfig("NAMENODE", any, source));
     }
 
     private String getBlueprintText(String path) {
