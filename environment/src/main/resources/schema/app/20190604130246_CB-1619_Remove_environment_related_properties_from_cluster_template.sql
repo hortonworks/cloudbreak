788c87d7cea9858e0b402b75a739d736251168f1
bbihari <bbihari@hortonworks.com>
2019-06-11 15:15:59 +0200

    CB-1765 Save availability-zone info in the environment network properties, fix sql

--- environment/src/main/resources/schema/app/20190604130246_CB-1619_Remove_environment_related_properties_from_cluster_template.sql
+++ environment/src/main/resources/schema/app/20190604130246_CB-1619_Remove_environment_related_properties_from_cluster_template.sql
@@ -2,7 +2,7 @@
 -- Migration SQL that makes the change goes here.
 
 
-ALTER TABLE environment_network ADD COLUMN IF NOT EXISTS subnetmetas TEXT NOT NULL;
+ALTER TABLE environment_network ADD COLUMN IF NOT EXISTS subnetmetas TEXT NOT NULL DEFAULT '{}';
 
 -- //@UNDO
 -- SQL to undo the change goes here.
