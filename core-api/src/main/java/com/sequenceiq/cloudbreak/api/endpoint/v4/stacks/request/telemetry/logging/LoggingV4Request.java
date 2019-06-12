eab5edb96d3b6c82d145b8b1e57b390b81236176
Oliver Szabo <oleewere@gmail.com>
2019-06-12 13:47:54 +0200

    DISTX-196. Validate Logging inputs during stack request processing

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/stacks/request/telemetry/logging/LoggingV4Request.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/stacks/request/telemetry/logging/LoggingV4Request.java
@@ -2,10 +2,12 @@ package com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.telemetry.loggi
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.LoggingV4Base;
+import com.sequenceiq.cloudbreak.validation.ValidLoggingV4Request;
 
 import io.swagger.annotations.ApiModel;
 
 @ApiModel
 @JsonIgnoreProperties(ignoreUnknown = true)
+@ValidLoggingV4Request
 public class LoggingV4Request extends LoggingV4Base {
 }
