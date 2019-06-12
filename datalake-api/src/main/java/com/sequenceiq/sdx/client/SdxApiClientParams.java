2b5e2194bd35e6b6d9abfcbcdba6d43decb26712
Tamas Bihari <tbihari@hortonworks.com>
2019-06-12 16:23:17 +0200

    CB-1123 conventional API client for SDX

---
+++ datalake-api/src/main/java/com/sequenceiq/sdx/client/SdxApiClientParams.java
@@ -0,0 +1,34 @@
+package com.sequenceiq.sdx.client;
+
+public class SdxApiClientParams {
+    private boolean restDebug;
+
+    private boolean certificateValidation;
+
+    private boolean ignorePreValidation;
+
+    private String sdxServerUrl;
+
+    public SdxApiClientParams(boolean restDebug, boolean certificateValidation, boolean ignorePreValidation, String sdxServerUrl) {
+        this.restDebug = restDebug;
+        this.certificateValidation = certificateValidation;
+        this.ignorePreValidation = ignorePreValidation;
+        this.sdxServerUrl = sdxServerUrl;
+    }
+
+    public String getServiceUrl() {
+        return sdxServerUrl;
+    }
+
+    public boolean isCertificateValidation() {
+        return certificateValidation;
+    }
+
+    public boolean isIgnorePreValidation() {
+        return ignorePreValidation;
+    }
+
+    public boolean isRestDebug() {
+        return restDebug;
+    }
+}
