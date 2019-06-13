8d57725d41e2e8373d7f4662b4923b25524229fe
Krisztian Horvath <keyki.kk@gmail.com>
2019-06-13 16:44:26 +0200

    OPSAPS-50867 kerberos workaround for Hive

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hive/HiveMetastoreConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hive/HiveMetastoreConfigProvider.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Component;
 
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRoleConfigConfigProvider;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils;
 import com.sequenceiq.cloudbreak.domain.RDSConfig;
+import com.sequenceiq.cloudbreak.dto.KerberosConfig;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 import com.sequenceiq.cloudbreak.template.views.HostgroupView;
 import com.sequenceiq.cloudbreak.template.views.RdsView;
@@ -26,7 +28,8 @@ public class HiveMetastoreConfigProvider extends AbstractRoleConfigConfigProvide
         Optional<RDSConfig> rdsConfigOptional = getFirstRDSConfigOptional(templatePreparationObject);
         Preconditions.checkArgument(rdsConfigOptional.isPresent());
         RdsView hiveView = new RdsView(rdsConfigOptional.get());
-        return List.of(
+
+        List<ApiClusterTemplateConfig> configs = Lists.newArrayList(
                 config("hive_metastore_database_host", hiveView.getHost()),
                 config("hive_metastore_database_name", hiveView.getDatabaseName()),
                 config("hive_metastore_database_password", hiveView.getConnectionPassword()),
@@ -34,6 +37,16 @@ public class HiveMetastoreConfigProvider extends AbstractRoleConfigConfigProvide
                 config("hive_metastore_database_type", hiveView.getSubprotocol()),
                 config("hive_metastore_database_user", hiveView.getConnectionUserName())
         );
+
+        Optional<KerberosConfig> kerberosConfigOpt = templatePreparationObject.getKerberosConfig();
+        if (kerberosConfigOpt.isPresent()) {
+            String realm = Optional.ofNullable(kerberosConfigOpt.get().getRealm()).orElse("").toUpperCase();
+            String safetyValveValue = "<property><name>hive.server2.authentication.kerberos.principal</name><value>hive/_HOST@" + realm
+                    + "</value></property><property><name>hive.server2.authentication.kerberos.keytab</name><value>hive.keytab</value></property>";
+            configs.add(config("hive_service_config_safety_valve", safetyValveValue));
+        }
+
+        return configs;
     }
 
     @Override
