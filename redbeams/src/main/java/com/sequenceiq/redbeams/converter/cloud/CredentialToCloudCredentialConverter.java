f9aafde21a6f0ee15402bc4e56bbe583a660fb90
Tamas Bihari <tbihari@hortonworks.com>
2019-06-14 10:51:34 +0200

    fix redbeams IDEA run and cloud credential creation

--- redbeams/src/main/java/com/sequenceiq/redbeams/converter/cloud/CredentialToCloudCredentialConverter.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/converter/cloud/CredentialToCloudCredentialConverter.java
@@ -2,15 +2,15 @@ package com.sequenceiq.redbeams.converter.cloud;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
-import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
-import com.sequenceiq.cloudbreak.common.json.Json;
-import com.sequenceiq.redbeams.domain.Credential;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
+import com.sequenceiq.cloudbreak.common.json.Json;
+import com.sequenceiq.redbeams.domain.Credential;
 
 @Component
 public class CredentialToCloudCredentialConverter implements Converter<Credential, CloudCredential> {
@@ -28,7 +28,8 @@ public class CredentialToCloudCredentialConverter implements Converter<Credentia
             fields = new Json(credential.getAttributes()).getMap();
         }
         fields.put(CREDENTIAL_ID, credential.getId());
-        return new CloudCredential(credential.getId(), credential.getName(), fields);
+        //TODO replace credential id with crn
+        return new CloudCredential(credential.getId().toString(), credential.getName(), fields);
     }
 
 }
