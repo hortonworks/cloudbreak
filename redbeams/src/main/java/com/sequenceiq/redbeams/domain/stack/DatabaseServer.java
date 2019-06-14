b4da3aaa6c335ff34f0d627c8a65cb4186b1a994
Bill Havanki <bhavanki@cloudera.com>
2019-06-14 15:27:37 -0400

    CB-1837 Add redbeams DBStack and associated entities
    New entity classes represent the creation of a new database server. The
    top-level DBStack object represents one creation request. It is
    comprised of a network description, a database server description, and
    general top-level information. The lower-level Network, DatabaseServer,
    and Security entities store more detailed information. Network and
    Security are based on similar entity classes in the core Cloudbreak
    service. All of the entities might end up with more fields in the near
    future as the logic for performing database creation develops.
    
    A database migration for establishing the entities in the redbeams
    database is included.

---
+++ redbeams/src/main/java/com/sequenceiq/redbeams/domain/stack/DatabaseServer.java
@@ -0,0 +1,152 @@
+package com.sequenceiq.redbeams.domain.stack;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.common.DatabaseVendor;
+import com.sequenceiq.cloudbreak.common.json.Json;
+import com.sequenceiq.cloudbreak.common.json.JsonToString;
+import com.sequenceiq.cloudbreak.service.secret.SecretValue;
+import com.sequenceiq.cloudbreak.service.secret.domain.Secret;
+import com.sequenceiq.cloudbreak.service.secret.domain.SecretToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class DatabaseServer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "databaseserver_generator")
+    @SequenceGenerator(name = "databaseserver_generator", sequenceName = "databaseserver_id_seq", allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    @Column(length = 1000000, columnDefinition = "TEXT")
+    private String description;
+
+    private String instanceType;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DatabaseVendor databaseVendor;
+
+    private Long storageSize;
+
+    @Convert(converter = SecretToString.class)
+    @SecretValue
+    @Column(nullable = false)
+    private Secret rootUserName = Secret.EMPTY;
+
+    @Convert(converter = SecretToString.class)
+    @SecretValue
+    @Column(nullable = false)
+    private Secret rootPassword = Secret.EMPTY;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "securitygroup_id", referencedColumnName = "id")
+    private SecurityGroup securityGroup;
+
+    @Convert(converter = JsonToString.class)
+    @Column(columnDefinition = "TEXT")
+    private Json attributes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstanceType() {
+        return instanceType;
+    }
+
+    public void setInstanceType(String instanceType) {
+        this.instanceType = instanceType;
+    }
+
+    public DatabaseVendor getDatabaseVendor() {
+        return databaseVendor;
+    }
+
+    public void setDatabaseVendor(DatabaseVendor databaseVendor) {
+        this.databaseVendor = databaseVendor;
+    }
+
+    public Long getStorageSize() {
+        return storageSize;
+    }
+
+    public void setStorageSize(Long storageSize) {
+        this.storageSize = storageSize;
+    }
+
+    public String getRootUserName() {
+        return rootUserName.getRaw();
+    }
+
+    public String getRootUserNameSecret() {
+        return rootUserName.getSecret();
+    }
+
+    public void setRootUserName(String rootUserName) {
+        this.rootUserName = new Secret(rootUserName);
+    }
+
+    public String getRootPassword() {
+        return rootPassword.getRaw();
+    }
+
+    public String getRootPasswordSecret() {
+        return rootPassword.getSecret();
+    }
+
+    public void setRootPassword(String rootPassword) {
+        this.rootPassword = new Secret(rootPassword);
+    }
+
+    public SecurityGroup getSecurityGroup() {
+        return securityGroup;
+    }
+
+    public void setSecurityGroup(SecurityGroup securityGroup) {
+        this.securityGroup = securityGroup;
+    }
+
+    public Json getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Json attributes) {
+        this.attributes = attributes;
+    }
+
+}
