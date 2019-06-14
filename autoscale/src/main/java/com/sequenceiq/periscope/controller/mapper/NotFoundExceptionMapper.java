da33d618c5b1681d13d31f400ce3ccad3b4ce2af
Tamas Bihari <tbihari@hortonworks.com>
2019-06-14 09:56:31 +0200

    CB-1617 short log message for not found exceptions in CB

--- autoscale/src/main/java/com/sequenceiq/periscope/controller/mapper/NotFoundExceptionMapper.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/controller/mapper/NotFoundExceptionMapper.java
@@ -1,15 +1,33 @@
 package com.sequenceiq.periscope.controller.mapper;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sequenceiq.periscope.service.NotFoundException;
 
 @Provider
 public class NotFoundExceptionMapper extends BaseExceptionMapper<NotFoundException> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(NotFoundExceptionMapper.class);
+
     @Override
     Status getResponseStatus() {
         return Status.NOT_FOUND;
+    }
+
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        LOGGER.info("Resource not found: {}", getErrorMessage(exception));
+        return Response.status(getResponseStatus()).entity(getEntity(exception)).build();
+    }
+
+    private String getErrorMessage(NotFoundException exception) {
+        String message = exception.getMessage();
+        LOGGER.debug("Exception text has been mapped: {}", message);
+        return message;
     }
 }
