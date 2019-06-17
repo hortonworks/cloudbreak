1b5503d494e5d8fb81008cd65228738e40f089b6
sodre90 <perdos@hortonworks.com>
2019-06-17 15:11:53 +0200

    CB-1890 Create detail endpoint for SDX CRUD (StackV4Response)

---
+++ datalake-api/src/main/java/com/sequenceiq/sdx/api/model/SdxClusterDetailResponse.java
@@ -0,0 +1,25 @@
+package com.sequenceiq.sdx.api.model;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.StackV4Response;
+
+public class SdxClusterDetailResponse extends SdxClusterResponse {
+
+    private StackV4Response stackV4Response;
+
+    public SdxClusterDetailResponse() {
+    }
+
+    public SdxClusterDetailResponse(SdxClusterResponse sdxClusterResponse, StackV4Response stackV4Response) {
+        super(sdxClusterResponse.getCrn(), sdxClusterResponse.getName(), sdxClusterResponse.getStatus(),
+                sdxClusterResponse.getEnvironmentName(), sdxClusterResponse.getEnvironmentCrn());
+        this.stackV4Response = stackV4Response;
+    }
+
+    public StackV4Response getStackV4Response() {
+        return stackV4Response;
+    }
+
+    public void setStackV4Response(StackV4Response stackV4Response) {
+        this.stackV4Response = stackV4Response;
+    }
+}
