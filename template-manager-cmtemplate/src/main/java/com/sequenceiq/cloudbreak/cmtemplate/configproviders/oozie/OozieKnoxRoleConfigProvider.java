6233b3b559d5212c37a8cbe4624f093dce2a195e
Peter Darvasi <pdarvasi@hortonworks.com>
2019-06-18 17:16:29 +0200

    CB-974 - Renamed AbstractRoleConfigProvider

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/oozie/OozieKnoxRoleConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/oozie/OozieKnoxRoleConfigProvider.java
@@ -10,19 +10,18 @@ import org.springframework.stereotype.Component;
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.ExposedService;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
-import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRoleConfigConfigProvider;
+import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRoleConfigProvider;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
-import com.sequenceiq.cloudbreak.template.views.HostgroupView;
 
 @Component
-public class OozieKnoxRoleConfigProvider extends AbstractRoleConfigConfigProvider {
+public class OozieKnoxRoleConfigProvider extends AbstractRoleConfigProvider {
 
     public static final String OOZIE_PROXY_USER_SETTINGS = "<property><name>oozie.service.ProxyUserService.proxyuser.knox"
             + ".groups</name><value>*</value></property><property><name>oozie.service.ProxyUserService.proxyuser.knox"
             + ".hosts</name><value>*</value></property>";
 
     @Override
-    public List<ApiClusterTemplateConfig> getRoleConfig(String roleType, HostgroupView hostGroupView, TemplatePreparationObject source) {
+    public List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, TemplatePreparationObject source) {
         return List.of(
                 config("oozie_config_safety_valve",
                         OOZIE_PROXY_USER_SETTINGS));
