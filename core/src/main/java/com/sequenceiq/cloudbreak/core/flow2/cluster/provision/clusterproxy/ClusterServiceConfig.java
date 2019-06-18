90000e918370c0f75cb390a6e8af4fe32ad02493
Guruprasad Venkatesh <dv.guruprasad@gmail.com>
2019-06-18 23:27:18 +0530

    CDPAM-66 | Refactoring to improve tests, other minor improvements.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/provision/clusterproxy/ClusterServiceConfig.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/provision/clusterproxy/ClusterServiceConfig.java
@@ -2,13 +2,20 @@ package com.sequenceiq.cloudbreak.core.flow2.cluster.provision.clusterproxy;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 class ClusterServiceConfig {
+    @JsonProperty
     private String name;
 
+    @JsonProperty
     private List<String> endpoints;
 
+    @JsonProperty
     private List<ClusterServiceCredential> credentials;
 
+    @JsonCreator
     ClusterServiceConfig(String serviceName, List<String> endpoints, List<ClusterServiceCredential> credentials) {
         this.name = serviceName;
         this.endpoints = endpoints;
