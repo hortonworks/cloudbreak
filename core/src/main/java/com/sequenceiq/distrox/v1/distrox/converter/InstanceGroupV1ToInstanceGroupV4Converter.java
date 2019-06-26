7af4c9d2821e41d253fab84ef28609f32e47866b
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-06-26 16:34:59 +0200

    CB-2074 Ports are not opened by distrox in case of GATEWAY node

--- core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/InstanceGroupV1ToInstanceGroupV4Converter.java
+++ core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/InstanceGroupV1ToInstanceGroupV4Converter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.instancegroup.InstanceGroupV4Request;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.instancegroup.securitygroup.SecurityGroupV4Request;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.util.requests.SecurityRuleV4Request;
+import com.sequenceiq.cloudbreak.common.type.InstanceGroupType;
 import com.sequenceiq.distrox.api.v1.distrox.model.instancegroup.InstanceGroupV1Request;
 
 @Component
@@ -37,7 +38,7 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
         response.setName(source.getName());
         response.setTemplate(getIfNotNull(source.getTemplate(), instanceTemplateConverter::convert));
         response.setRecoveryMode(source.getRecoveryMode());
-        response.setSecurityGroup(getSecurityGroup(source.getName()));
+        response.setSecurityGroup(getSecurityGroup(source.getType()));
         response.setRecipeNames(source.getRecipeNames());
         response.setAws(getIfNotNull(source.getAws(), instanceGroupParameterConverter::convert));
         response.setAzure(getIfNotNull(source.getAzure(), instanceGroupParameterConverter::convert));
@@ -61,20 +62,20 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
         return response;
     }
 
-    private SecurityGroupV4Request getSecurityGroup(String name) {
+    private SecurityGroupV4Request getSecurityGroup(InstanceGroupType type) {
         SecurityGroupV4Request response = new SecurityGroupV4Request();
         SecurityRuleV4Request securityRule = new SecurityRuleV4Request();
         securityRule.setProtocol("tcp");
         securityRule.setSubnet("0.0.0.0/0");
-        securityRule.setPorts(getPorts(name));
+        securityRule.setPorts(getPorts(type));
         response.setSecurityRules(List.of(securityRule));
         return response;
     }
 
-    private List<String> getPorts(String name) {
+    private List<String> getPorts(InstanceGroupType type) {
         List<String> ret = new ArrayList<>();
         ret.add("22");
-        if ("master".equals(name)) {
+        if (type == InstanceGroupType.GATEWAY) {
             ret.addAll(List.of("9443", "8443", "443"));
         }
         return ret;
