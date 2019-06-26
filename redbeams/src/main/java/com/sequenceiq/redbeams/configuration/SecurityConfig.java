b9a3211e6c9bac3dc56923c7e8fd572b9a848346
Bill Havanki <bhavanki@cloudera.com>
2019-06-26 12:10:53 -0400

    CB-1514 Fix CRN-based authentication in redbeams

--- redbeams/src/main/java/com/sequenceiq/redbeams/configuration/SecurityConfig.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/configuration/SecurityConfig.java
@@ -2,19 +2,32 @@ package com.sequenceiq.redbeams.configuration;
 
 import static com.sequenceiq.redbeams.api.RedbeamsApi.API_ROOT_CONTEXT;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
-import com.sequenceiq.cloudbreak.auth.security.ScimAccountGroupReaderFilter;
+import com.sequenceiq.cloudbreak.auth.altus.GrpcUmsClient;
+import com.sequenceiq.cloudbreak.auth.security.CrnUserDetailsService;
 import com.sequenceiq.redbeams.service.security.TenantBasedPermissionEvaluator;
 
 @Configuration
@@ -60,17 +73,52 @@ public class SecurityConfig {
         private static final String V4_API = API_ROOT_CONTEXT + "/v4/**";
 
         @Inject
-        private ScimAccountGroupReaderFilter scimAccountGroupReaderFilter;
+        private GrpcUmsClient umsClient;
+
+        @Bean
+        public RequestHeaderAuthenticationFilter headerAuthenticationFilter() throws Exception {
+            RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter = new RequestHeaderAuthenticationFilter();
+            requestHeaderAuthenticationFilter.setPrincipalRequestHeader("x-cdp-actor-crn");
+            requestHeaderAuthenticationFilter.setAuthenticationManager(authenticationManager());
+            requestHeaderAuthenticationFilter.setExceptionIfHeaderMissing(false);
+            requestHeaderAuthenticationFilter.setContinueFilterChainOnUnsuccessfulAuthentication(true);
+            return requestHeaderAuthenticationFilter;
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return new CrnUserDetailsService(umsClient);
+        }
+
+        @Bean
+        @Override
+        protected AuthenticationManager authenticationManager() throws Exception {
+            List<AuthenticationProvider> providers = new ArrayList<>(1);
+            providers.add(preAuthAuthProvider());
+            return new ProviderManager(providers);
+        }
+
+        @Bean
+        public PreAuthenticatedAuthenticationProvider preAuthAuthProvider() throws Exception {
+            PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+            provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
+            return provider;
+        }
+
+        @Bean
+        public UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() throws Exception {
+            UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
+            wrapper.setUserDetailsService(userDetailsService());
+            return wrapper;
+        }
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.addFilterAfter(scimAccountGroupReaderFilter, AbstractPreAuthenticatedProcessingFilter.class)
+            // http.addFilterAfter(scimAccountGroupReaderFilter, AbstractPreAuthenticatedProcessingFilter.class)
+            http.addFilterAfter(headerAuthenticationFilter(), AbstractPreAuthenticatedProcessingFilter.class)
                     .authorizeRequests()
                     .antMatchers(V4_API)
-                    .access("#oauth2.isOAuth()")
-                    // .antMatchers(REDBEAMS_API)
-                    // .access("#oauth2.hasScope('cloudbreak.autoscale')")
-
+                    .authenticated()
                     .antMatchers(API_ROOT_CONTEXT + "/swagger.json").permitAll()
                     .antMatchers(API_ROOT_CONTEXT + "/api-docs/**").permitAll()
                     .antMatchers(API_ROOT_CONTEXT + "/**").denyAll();
