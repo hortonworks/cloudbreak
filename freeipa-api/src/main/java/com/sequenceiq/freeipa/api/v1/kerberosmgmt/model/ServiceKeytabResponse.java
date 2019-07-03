847816f7cd973fd486703b90e3911bb2a773e9c0
Kalyan Kumar Kalvagadda <kkalyan@cloudera.com>
2019-07-03 17:40:45 -0500

    CDPSDX-595: Replacing the sensitive strings on the response to SecretResponse

--- freeipa-api/src/main/java/com/sequenceiq/freeipa/api/v1/kerberosmgmt/model/ServiceKeytabResponse.java
+++ freeipa-api/src/main/java/com/sequenceiq/freeipa/api/v1/kerberosmgmt/model/ServiceKeytabResponse.java
@@ -2,6 +2,7 @@ package com.sequenceiq.freeipa.api.v1.kerberosmgmt.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sequenceiq.freeipa.api.v1.kerberosmgmt.doc.KeytabModelDescription;
+import com.sequenceiq.cloudbreak.service.secret.model.SecretResponse;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -11,24 +12,24 @@ import io.swagger.annotations.ApiModelProperty;
 public class ServiceKeytabResponse {
 
     @ApiModelProperty (KeytabModelDescription.PRINCIPAL)
-    private String servicePrincial;
+    private SecretResponse servicePrincial;
 
     @ApiModelProperty (KeytabModelDescription.KEYTAB)
-    private String keytab;
+    private SecretResponse keytab;
 
-    public String getServicePrincial() {
+    public SecretResponse getServicePrincial() {
         return servicePrincial;
     }
 
-    public void setServicePrincial(String servicePrincial) {
+    public void setServicePrincial(SecretResponse servicePrincial) {
         this.servicePrincial = servicePrincial;
     }
 
-    public String getKeytab() {
+    public SecretResponse getKeytab() {
         return keytab;
     }
 
-    public void setKeytab(String keytab) {
+    public void setKeytab(SecretResponse keytab) {
         this.keytab = keytab;
     }
 }
