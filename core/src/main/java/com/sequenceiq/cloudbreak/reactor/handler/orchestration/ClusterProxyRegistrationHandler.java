300092e2b52a976460608344d5354cf9c87822c0
Guruprasad Venkatesh <dv.guruprasad@gmail.com>
2019-07-05 22:19:26 +0530

    CDPAM-116 | Persisting public key cert to be used to configure knox token topology

--- core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/orchestration/ClusterProxyRegistrationHandler.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/orchestration/ClusterProxyRegistrationHandler.java
@@ -7,11 +7,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.SSOType;
 import com.sequenceiq.cloudbreak.common.event.Selectable;
 import com.sequenceiq.cloudbreak.core.flow2.cluster.provision.clusterproxy.ClusterProxyService;
+import com.sequenceiq.cloudbreak.core.flow2.cluster.provision.clusterproxy.ConfigRegistrationResponse;
+import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
+import com.sequenceiq.cloudbreak.domain.stack.cluster.gateway.Gateway;
 import com.sequenceiq.cloudbreak.reactor.api.event.orchestration.ClusterProxyRegistrationFailed;
 import com.sequenceiq.cloudbreak.reactor.api.event.orchestration.ClusterProxyRegistrationRequest;
 import com.sequenceiq.cloudbreak.reactor.api.event.orchestration.ClusterProxyRegistrationSuccess;
+import com.sequenceiq.cloudbreak.service.gateway.GatewayService;
+import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.flow.event.EventSelectorUtil;
 import com.sequenceiq.flow.reactor.api.handler.EventHandler;
 
@@ -31,6 +38,12 @@ public class ClusterProxyRegistrationHandler implements EventHandler<ClusterProx
     @Inject
     private ClusterProxyService clusterProxyService;
 
+    @Inject
+    private StackService stackService;
+
+    @Inject
+    private GatewayService gatewayService;
+
     @Override
     public String selector() {
         return EventSelectorUtil.selector(ClusterProxyRegistrationRequest.class);
@@ -39,18 +52,31 @@ public class ClusterProxyRegistrationHandler implements EventHandler<ClusterProx
     @Override
     public void accept(Event<ClusterProxyRegistrationRequest> event) {
         ClusterProxyRegistrationRequest request = event.getData();
-        Selectable response;
-        try {
-            if (clusterProxyIntegrationEnabled) {
-                clusterProxyService.registerCluster(request.getResourceId());
-                response = new ClusterProxyRegistrationSuccess(request.getResourceId());
-            } else {
-                response = new ClusterProxyRegistrationSuccess(request.getResourceId());
-            }
-        } catch (Exception e) {
-            LOGGER.error("Error occurred when registering cluster proxy config", e);
-            response = new ClusterProxyRegistrationFailed(request.getResourceId(), e);
-        }
+        Selectable response = registerCluster(request);
         eventBus.notify(response.selector(), new Event<>(event.getHeaders(), response));
+    }
+
+    private Selectable registerCluster(ClusterProxyRegistrationRequest request) {
+        Stack stack = stackService.getByIdWithListsInTransaction(request.getResourceId());
+        try {
+            if (!clusterProxyIntegrationEnabled) {
+                return new ClusterProxyRegistrationSuccess(request.getResourceId());
+            }
+            ConfigRegistrationResponse registerResponse = clusterProxyService.registerCluster(stack);
+            Cluster cluster = stack.getCluster();
+            if (cluster.hasGateway()) {
+                LOGGER.debug("Updating Gateway for cluster {} in environment {} with public key certificate retrieved from Cluster Proxy",
+                        cluster.getId(), stack.getEnvironmentCrn());
+                Gateway gateway = cluster.getGateway();
+                gateway.setTokenCert(registerResponse.getX509Unwrapped());
+                gateway.setSsoType(SSOType.SSO_PROVIDER);
+                gatewayService.save(gateway);
+            }
+            return new ClusterProxyRegistrationSuccess(request.getResourceId());
+        } catch (Exception e) {
+            LOGGER.error("Error occurred when registering cluster {} in environment {} to cluster proxy",
+                    stack.getCluster().getId(), stack.getEnvironmentCrn(), e);
+            return new ClusterProxyRegistrationFailed(request.getResourceId(), e);
+        }
     }
 }
