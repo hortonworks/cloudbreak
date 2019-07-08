f88daf2e61f277c5d4e87a88e5bfaab8dee41622
doktoric <rdoktorics@cloudera.com>
2019-07-08 13:41:39 +0200

    CB-2259 securitygroup validation

--- environment/src/main/java/com/sequenceiq/environment/environment/validation/EnvironmentValidatorService.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/validation/EnvironmentValidatorService.java
@@ -1,21 +1,15 @@
 package com.sequenceiq.environment.environment.validation;
 
-import java.util.Map;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.cloud.model.CloudRegions;
 import com.sequenceiq.cloudbreak.util.ValidationResult;
-import com.sequenceiq.environment.api.v1.environment.model.request.EnvironmentAttachRequest;
 import com.sequenceiq.environment.environment.domain.Environment;
-import com.sequenceiq.environment.environment.domain.EnvironmentAwareResource;
-import com.sequenceiq.environment.environment.domain.EnvironmentView;
 import com.sequenceiq.environment.environment.dto.EnvironmentCreationDto;
 import com.sequenceiq.environment.environment.dto.LocationDto;
-import com.sequenceiq.environment.environment.validation.validators.EnvironmentAttachValidator;
 import com.sequenceiq.environment.environment.validation.validators.EnvironmentCreationValidator;
-import com.sequenceiq.environment.environment.validation.validators.EnvironmentDetachValidator;
 import com.sequenceiq.environment.environment.validation.validators.EnvironmentRegionValidator;
 
 @Service
@@ -23,34 +17,15 @@ public class EnvironmentValidatorService {
 
     private final EnvironmentCreationValidator creationValidator;
 
-    private final EnvironmentAttachValidator attachValidator;
-
-    private final EnvironmentDetachValidator detachValidator;
-
     private final EnvironmentRegionValidator regionValidator;
 
-    public EnvironmentValidatorService(EnvironmentCreationValidator creationValidator, EnvironmentAttachValidator attachValidator,
-            EnvironmentDetachValidator detachValidator, EnvironmentRegionValidator regionValidator) {
+    public EnvironmentValidatorService(EnvironmentCreationValidator creationValidator, EnvironmentRegionValidator regionValidator) {
         this.creationValidator = creationValidator;
-        this.attachValidator = attachValidator;
-        this.detachValidator = detachValidator;
         this.regionValidator = regionValidator;
     }
 
     public ValidationResult validateCreation(Environment environment, EnvironmentCreationDto request, CloudRegions cloudRegions) {
         return creationValidator.validate(environment, request, cloudRegions);
-    }
-
-    public ValidationResult validateProxyAttach(EnvironmentAttachRequest request) {
-        return attachValidator.validate(request);
-    }
-
-    public ValidationResult validateDetachForEnvironment(Environment environment, Map<? extends EnvironmentAwareResource, Set<String>> resourcesToClusters) {
-        return detachValidator.validateForEnvironment(environment, resourcesToClusters);
-    }
-
-    public ValidationResult validateDetachForResource(EnvironmentAwareResource resource, Map<EnvironmentView, Set<String>> envsToClusters) {
-        return detachValidator.validateForResource(resource, envsToClusters);
     }
 
     public ValidationResult.ValidationResultBuilder validateRegions(Set<String> requestedRegions, CloudRegions cloudRegions,
---
+++ environment/src/test/java/com/sequenceiq/environment/environment/validation/securitygroup/AwsEnvironmentSecurityGroupValidatorTest.java
@@ -0,0 +1,191 @@
+package com.sequenceiq.environment.environment.validation.securitygroup;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.common.collect.Sets;
+import com.sequenceiq.cloudbreak.cloud.model.CloudSecurityGroup;
+import com.sequenceiq.cloudbreak.cloud.model.CloudSecurityGroups;
+import com.sequenceiq.cloudbreak.util.ValidationResult;
+import com.sequenceiq.environment.api.v1.environment.model.request.EnvironmentRequest;
+import com.sequenceiq.environment.environment.dto.EnvironmentCreationDto;
+import com.sequenceiq.environment.environment.dto.SecurityAccessDto;
+import com.sequenceiq.environment.network.dto.AwsParams;
+import com.sequenceiq.environment.network.dto.NetworkDto;
+import com.sequenceiq.environment.platformresource.PlatformParameterService;
+import com.sequenceiq.environment.platformresource.PlatformResourceRequest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AwsEnvironmentSecurityGroupValidatorTest {
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private PlatformParameterService platformParameterService;
+
+    @InjectMocks
+    private AwsEnvironmentSecurityGroupValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        underTest = new AwsEnvironmentSecurityGroupValidator(platformParameterService);
+    }
+
+    @Test
+    public void testValidationWhenGroupsInTheSameVpcReturnValid() {
+        String region = "eu-west-1";
+        String sec1 = "sec-1";
+        String sec2 = "sec-2";
+        String vpcId = "vpc-123";
+
+        when(platformParameterService.getSecurityGroups(any(PlatformResourceRequest.class)))
+                .thenReturn(cloudSecurityGroups(region, vpcId, sec1, sec2));
+
+        EnvironmentCreationDto environmentDto = EnvironmentCreationDto.Builder
+                .anEnvironmentCreationDto()
+                .withRegions(Sets.newHashSet(region))
+                .withSecurityAccess(getSecurityAccessDto(sec1, sec2))
+                .withNetwork(getNetworkDto(vpcId))
+                .withCredential(getEnvironmentRequest())
+                .build();
+
+        ValidationResult.ValidationResultBuilder builder = ValidationResult.builder();
+        underTest.validate(environmentDto, builder);
+        requestIsValid(builder);
+    }
+
+    @Test
+    public void testValidationWhenGroupsInDifferentVpcReturnInValid() {
+        String region = "eu-west-1";
+        String sec1 = "sec-1";
+        String sec2 = "sec-2";
+        String vpcId = "vpc-123";
+        String requestVpcId = "vpc-124";
+
+        when(platformParameterService.getSecurityGroups(any(PlatformResourceRequest.class)))
+                .thenReturn(cloudSecurityGroups(region, vpcId, sec1, sec2));
+
+        EnvironmentCreationDto environmentDto = EnvironmentCreationDto.Builder
+                .anEnvironmentCreationDto()
+                .withRegions(Sets.newHashSet(region))
+                .withSecurityAccess(getSecurityAccessDto(sec1, sec2))
+                .withNetwork(getNetworkDto(requestVpcId))
+                .withCredential(getEnvironmentRequest())
+                .build();
+
+        ValidationResult.ValidationResultBuilder builder = ValidationResult.builder();
+        underTest.validate(environmentDto, builder);
+        requestIsInValid(builder);
+    }
+
+    @Test
+    public void testValidationWhenGroupsDefinedButUserWantNewVpcReturnInValid() {
+        String region = "eu-west-1";
+        String sec1 = "sec-1";
+        String sec2 = "sec-2";
+        String vpcId = "vpc-123";
+
+        when(platformParameterService.getSecurityGroups(any(PlatformResourceRequest.class)))
+                .thenReturn(cloudSecurityGroups(region, vpcId, sec1, sec2));
+
+        EnvironmentCreationDto environmentDto = EnvironmentCreationDto.Builder
+                .anEnvironmentCreationDto()
+                .withRegions(Sets.newHashSet(region))
+                .withSecurityAccess(getSecurityAccessDto(sec1, sec2))
+                .withNetwork(getNewNetworkDto())
+                .withCredential(getEnvironmentRequest())
+                .build();
+
+        ValidationResult.ValidationResultBuilder builder = ValidationResult.builder();
+        underTest.validate(environmentDto, builder);
+        requestIsInValid(builder);
+    }
+
+    @Test
+    public void testValidationWhenNewGroupsRequestedAndUserWantNewVpcReturnValid() {
+        String region = "eu-west-1";
+
+        EnvironmentCreationDto environmentDto = EnvironmentCreationDto.Builder
+                .anEnvironmentCreationDto()
+                .withRegions(Sets.newHashSet(region))
+                .withSecurityAccess(getNewSecurityAccessDto())
+                .withNetwork(getNewNetworkDto())
+                .withCredential(getEnvironmentRequest())
+                .build();
+
+        ValidationResult.ValidationResultBuilder builder = ValidationResult.builder();
+        underTest.validate(environmentDto, builder);
+        requestIsValid(builder);
+    }
+
+    private void requestIsValid(ValidationResult.ValidationResultBuilder builder) {
+        assertFalse(builder.build().hasError());
+    }
+
+    private void requestIsInValid(ValidationResult.ValidationResultBuilder builder) {
+        assertTrue(builder.build().hasError());
+    }
+
+    private SecurityAccessDto getSecurityAccessDto(String sec1, String sec2) {
+        return SecurityAccessDto.builder()
+                    .withDefaultSecurityGroupId(sec1)
+                    .withSecurityGroupIdForKnox(sec2)
+                    .build();
+    }
+
+    private SecurityAccessDto getNewSecurityAccessDto() {
+        return SecurityAccessDto.builder()
+                .withCidr("0.0.0.0/0")
+                .build();
+    }
+
+    private NetworkDto getNetworkDto(String vpcId) {
+        AwsParams awsParams = new AwsParams();
+        awsParams.setVpcId(vpcId);
+
+        return NetworkDto.Builder.aNetworkDto()
+                .withAws(awsParams)
+                .build();
+    }
+
+    private NetworkDto getNewNetworkDto() {
+        return NetworkDto.Builder.aNetworkDto()
+                .withNetworkCidr("0.0.0.0/0")
+                .build();
+    }
+
+    private EnvironmentRequest getEnvironmentRequest() {
+        EnvironmentRequest environmentRequest = new EnvironmentRequest();
+        environmentRequest.setCredentialName("apple");
+        return environmentRequest;
+    }
+
+    public CloudSecurityGroups cloudSecurityGroups(String region, String vpcId, String... securityGroupIds) {
+        CloudSecurityGroups cloudSecurityGroups = new CloudSecurityGroups();
+        cloudSecurityGroups.getCloudSecurityGroupsResponses().put(region, new HashSet<>());
+
+        for (String securityGroupId : securityGroupIds) {
+            CloudSecurityGroup cloudSecurityGroup = new CloudSecurityGroup(
+                    securityGroupId + "x",
+                    securityGroupId,
+                    Map.of("vpcId", vpcId)
+            );
+            cloudSecurityGroups.getCloudSecurityGroupsResponses().get(region).add(cloudSecurityGroup);
+        }
+        return cloudSecurityGroups;
+    }
+
+}
