adb229cd86e8cad05bece04cf3ee36398df3703c
Krisztian Horvath <keyki.kk@gmail.com>
2019-07-10 17:39:26 +0200

    CB-2191 setup HUE LB with Knox

---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hue/HueKnoxRoleConfigProvider.java
@@ -0,0 +1,44 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.hue;
+
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.stereotype.Component;
+
+import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.ExposedService;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateComponentConfigProvider;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
+
+@Component
+public class HueKnoxRoleConfigProvider implements CmTemplateComponentConfigProvider {
+
+    private static final String AUTH_BACKEND = "auth_backend";
+
+    private static final String KNOX_AUTH_BACKED = "desktop.auth.backend.KnoxSpnegoDjangoBackend";
+
+    public List<ApiClusterTemplateConfig> getServiceConfigs(CmTemplateProcessor templateProcessor, TemplatePreparationObject source) {
+        return List.of(config(AUTH_BACKEND, KNOX_AUTH_BACKED));
+    }
+
+    @Override
+    public String getServiceType() {
+        return "HUE";
+    }
+
+    @Override
+    public List<String> getRoleTypes() {
+        return List.of(HueRoles.HUE_LOAD_BALANCER);
+    }
+
+    @Override
+    public boolean isConfigurationNeeded(CmTemplateProcessor cmTemplateProcessor, TemplatePreparationObject source) {
+        return Objects.nonNull(source.getGatewayView())
+                && Objects.nonNull(source.getGatewayView().getExposedServices())
+                && source.getGatewayView().getExposedServices().getValue().contains(ExposedService.HUE.getKnoxService());
+    }
+
+}
