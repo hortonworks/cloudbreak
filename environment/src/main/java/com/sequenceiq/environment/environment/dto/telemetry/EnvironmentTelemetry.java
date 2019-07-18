5dcc71c2c1e5adc8c6e65f2aa60f73e85f90c673
Oliver Szabo <oleewere@gmail.com>
2019-07-18 20:33:13 +0200

    Missing Json Creator for EnvironmentTelemetry

--- environment/src/main/java/com/sequenceiq/environment/environment/dto/telemetry/EnvironmentTelemetry.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/dto/telemetry/EnvironmentTelemetry.java
@@ -1,14 +1,24 @@
 package com.sequenceiq.environment.environment.dto.telemetry;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EnvironmentTelemetry implements Serializable {
 
     private final EnvironmentLogging logging;
 
     private final EnvironmentWorkloadAnalytics workloadAnalytics;
 
-    public EnvironmentTelemetry(EnvironmentLogging logging, EnvironmentWorkloadAnalytics workloadAnalytics) {
+    public EnvironmentTelemetry(@JsonProperty("logging") EnvironmentLogging logging,
+            @JsonProperty("workloadAnalytics") EnvironmentWorkloadAnalytics workloadAnalytics) {
         this.logging = logging;
         this.workloadAnalytics = workloadAnalytics;
     }
