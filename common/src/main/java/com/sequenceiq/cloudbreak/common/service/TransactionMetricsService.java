e64b21c0d68e98a78bff6a8e9913371ee2e32cc3
Attila Kanto <attila.kanto@gmail.com>
2019-07-24 12:06:57 +0200

    CB-2643 add identifier to be able to track the beginning and the end of the transaction

---
+++ common/src/main/java/com/sequenceiq/cloudbreak/common/service/TransactionMetricsService.java
@@ -0,0 +1,61 @@
+package com.sequenceiq.cloudbreak.common.service;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionMetricsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionMetricsService.class);
+
+    private static final long TX_DURATION_OK = 1000L;
+
+    private static final long TX_DURATION_ERROR = 3000L;
+
+    @Autowired
+    private Clock clock;
+
+    @Value("${cb.log.transaction.stacktrace:true}")
+    private boolean logTransactionStacktrace;
+
+    public TransactionMetricsContext createTransactionMetricsContext() {
+        long start = clock.getCurrentTimeMillis();
+        TransactionMetricsContext transactionMetricsContext = new TransactionMetricsContext(start);
+        LOGGER.debug("Transaction context has been created, transactionMetricsId: {}", transactionMetricsContext.getTxId());
+        return transactionMetricsContext;
+    }
+
+    public void processTransactionDuration(TransactionMetricsContext transactionMetricsContext) {
+        long duration = clock.getCurrentTimeMillis() - transactionMetricsContext.getStart();
+        if (TX_DURATION_ERROR < duration) {
+            if (logTransactionStacktrace) {
+                LOGGER.error("Transaction duration was critical, transactionMetricsId: {}, took {}ms at: {}", transactionMetricsContext.getTxId(),
+                        duration, generateStackTrace());
+            } else {
+                LOGGER.error("Transaction duration was critical, transactionMetricsId: {}, took {}ms", transactionMetricsContext.getTxId(), duration);
+            }
+        } else if (TX_DURATION_OK < duration) {
+            if (logTransactionStacktrace) {
+                LOGGER.info("Transaction duration was high, transactionMetricsId: {}, took {}ms at: {}", transactionMetricsContext.getTxId(),
+                        duration, generateStackTrace());
+            } else {
+                LOGGER.info("Transaction duration was high, transactionMetricsId: {}, took {}ms", transactionMetricsContext.getTxId(), duration);
+            }
+        } else {
+            LOGGER.debug("Transaction duration was ok, transactionMetricsId: {}, took {}ms", transactionMetricsContext.getTxId(), duration);
+        }
+    }
+
+    private String generateStackTrace() {
+        return String.join("\n\t", Arrays.stream(Thread.currentThread().getStackTrace()).map(StackTraceElement::toString)
+                .collect(Collectors.toCollection(LinkedHashSet::new)));
+    }
+
+}
