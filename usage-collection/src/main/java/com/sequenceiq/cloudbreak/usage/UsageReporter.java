656f5ff1eaa34cc80437788de425a6d5ad7301e2
ganeshk <ganeshk@cloudera.com>
2019-07-23 21:37:40 -0700

    DISTX-41 Implementing business metrics collection for Datahub and Datalake clusters
    1. The usage event schema is defined in thunderhead repo.
    2. The schema protobuf file is fetched from git in gradle and compiled using java.
    3. The usage reporter interface and implementation will be used in appropriate place in this repo.
    4. Build and checked that it works fine.
    5. This is my first PR to this repo, will add the instrumentation after this PR is merged.
    6. Add necessary steps to make sure that gradle builds properly in all environments.

---
+++ usage-collection/src/main/java/com/sequenceiq/cloudbreak/usage/UsageReporter.java
@@ -0,0 +1,54 @@
+package com.sequenceiq.cloudbreak.usage;
+
+import com.cloudera.thunderhead.service.common.usage.UsageProto;
+
+/**
+ * Interface for reporting usage.
+ */
+public interface UsageReporter {
+    /**
+     * The overall version of the usage event model. See the comments on the
+     * version field in the Event message in usage.proto for details on how this
+     * is used.
+     *
+     * Version history:
+     *   1: Initial version.
+     *   2: IAM user created and user updated events updated for GDPR compliance.
+     *   3: IAM user created events will always have one of identity provider ID
+     *        or SFDC contact ID set.
+     */
+    int USAGE_VERSION = 3;
+    /**
+     * Reports that an CDP Datahub cluster has been requested.
+     * @param timestamp the cluster creation date which will be used as the
+     * timestamp for the event
+     * @param details the event details
+     */
+    void cdpDatahubClusterRequested(
+            long timestamp,
+            UsageProto.CDPDatahubClusterRequested details);
+
+    /**
+     * Reports that an CDP Datahub cluster's status has changed.
+     * @param details the event details
+     */
+    void cdpDatahubClusterStatusChanged(
+            UsageProto.CDPDatahubClusterStatusChanged details);
+
+    /**
+     * Reports that an CDP Datalake cluster has been requested.
+     * @param timestamp the cluster creation date which will be used as the
+     * timestamp for the event
+     * @param details the event details
+     */
+    void cdpDatalakeClusterRequested(
+            long timestamp,
+            UsageProto.CDPDatalakeClusterRequested details);
+
+    /**
+     * Reports that an CDP Datalake cluster's status has changed.
+     * @param details the event details
+     */
+    void cdpDatalakeClusterStatusChanged(
+            UsageProto.CDPDatalakeClusterStatusChanged details);
+}
