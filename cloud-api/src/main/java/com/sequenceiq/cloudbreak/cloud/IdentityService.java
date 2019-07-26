324b612b46d27a8af65c253f8c094c311d64f7bb
Dénes Berger <dberger@hortonworks.com>
2019-07-26 13:13:52 +0200

    CB-2455 Add Region to AWS STS service call, Update ARN parser

--- cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/IdentityService.java
+++ cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/IdentityService.java
@@ -4,6 +4,6 @@ import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
 
 public interface IdentityService extends CloudPlatformAware {
 
-    String getAccountId(CloudCredential cloudCredential);
+    String getAccountId(String region, CloudCredential cloudCredential);
 
 }
