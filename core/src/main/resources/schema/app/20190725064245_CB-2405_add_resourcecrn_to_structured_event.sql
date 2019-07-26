2f7afb51d9bdda541565ff556a5b93792ae02c37
doktoric <rdoktorics@cloudera.com>
2019-07-26 10:38:59 +0200

    CB-2405 removed migration

--- core/src/main/resources/schema/app/20190725064245_CB-2405_add_resourcecrn_to_structured_event.sql
+++ core/src/main/resources/schema/app/20190725064245_CB-2405_add_resourcecrn_to_structured_event.sql
@@ -7,16 +7,6 @@ ALTER TABLE recipe RENAME COLUMN crn TO resourceCrn;
 ALTER TABLE imagecatalog RENAME COLUMN crn TO resourceCrn;
 ALTER TABLE blueprint RENAME COLUMN crn TO resourceCrn;
 
-UPDATE structuredevent stre
-SET resourcecrn = SQ.crn
-FROM (SELECT s.resourcecrn AS "crn", se.workspace_id AS "wpid"
-      FROM structuredevent se
-      INNER JOIN stack s ON s.id = se.resourceid) AS SQ
-WHERE SQ.wpid = stre.workspace_id
-AND stre.resourcetype = 'stacks'
-AND stre.resourceid IS NOT NULL
-AND stre.resourcecrn IS NULL;
-
 -- //@UNDO
 -- SQL to undo the change goes here.
 
