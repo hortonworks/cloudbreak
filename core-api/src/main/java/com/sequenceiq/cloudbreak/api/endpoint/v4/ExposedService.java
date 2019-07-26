7ab8402f5ceb303943f4729fdaa7ed4eb0723c5d
Krisztian Horvath <keyki.kk@gmail.com>
2019-07-26 17:34:50 +0200

    CB-2474 extend the services with the tls ports

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/ExposedService.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/ExposedService.java
@@ -12,44 +12,46 @@ import com.google.common.collect.ImmutableList;
 
 public enum ExposedService {
 
-    ALL("Every Service", "ALL", "", "", true, null, false, true),
+    ALL("Every Service", "ALL", "", "", true, null, null, false, true),
 
-    CLOUDERA_MANAGER("CM-API", "CM-API", "CM-API", "/cm-api/", true, 7180, true, true),
-    CLOUDERA_MANAGER_UI("CM-UI", "CM-UI", "CM-UI", "/cmf/home", true, 7180, false, false),
-    HUE("HUE", "HUE_LOAD_BALANCER", "HUE", "/hue/", true, 8889, false, false),
-    WEBHDFS("WebHDFS", "NAMENODE", "WEBHDFS", "/webhdfs/v1", true, 9870, true, true),
-    NAMENODE("Name Node", "NAMENODE", "HDFSUI", "/hdfs/", true, 9870, false, false),
-    RESOURCEMANAGER_WEB("Resource Manager", "RESOURCEMANAGER", "YARNUIV2", "/yarnuiv2/", true, 8088, false, true),
-    JOB_HISTORY_SERVER("Job History Server", "JOBHISTORY", "JOBHISTORYUI", "/jobhistory/", true, 19888, false, false),
-    HIVE_SERVER("Hive Server", "HIVESERVER2", "HIVE", "/hive/", false, 10001, true, true),
-    HIVE_SERVER_INTERACTIVE("Hive Server Interactive", "", "HIVE_INTERACTIVE", "/hive/", false, 10501, true, true),
-    ATLAS("Atlas", "ATLAS_SERVER", "ATLAS", "/atlas/", true, 21000, false, true),
-    SPARK_HISTORY_SERVER("Spark History Server", "SPARK_YARN_HISTORY_SERVER", "SPARKHISTORYUI", "/sparkhistory/", true, 18088, false, false),
-    ZEPPELIN("Zeppelin", "ZEPPELIN_SERVER", "ZEPPELIN", "/zeppelin/", false, 8885, false, false),
-    RANGER("Ranger", "RANGER_ADMIN", "RANGER", "/ranger/", true, 6080, false, true),
-    LIVY_SERVER("Livy Server", "LIVY_SERVER", "LIVYSERVER1", "/livy/ui", true, 8998, false, true),
-    OOZIE_UI("Oozie Server", "OOZIE_SERVER", "OOZIE", "/oozie/", true, 11000, false, true),
-    SOLR("Solr Server", "SOLR_SERVER", "SOLR", "/solr/", true, 8983, false, true),
-    HBASE_UI("HBase UI", "MASTER", "HBASEUI", "/hbase/webui/master", true, 16010, false, false),
-    HBASE_REST("HBase Rest", "HBASERESTSERVER", "WEBHBASE", "/hbase/", true, 20550, true, true);
+    CLOUDERA_MANAGER("CM-API", "CM-API", "CM-API", "/cm-api/", true, 7180, 7183, true, true),
+    CLOUDERA_MANAGER_UI("CM-UI", "CM-UI", "CM-UI", "/cmf/home", true, 7180, 7183, false, false),
+    HUE("HUE", "HUE_LOAD_BALANCER", "HUE", "/hue/", true, 8889, 8889, false, false),
+    WEBHDFS("WebHDFS", "NAMENODE", "WEBHDFS", "/webhdfs/v1", true, 9870, 9871, true, true),
+    NAMENODE("Name Node", "NAMENODE", "HDFSUI", "/hdfs/", true, 9870, 9871, false, false),
+    RESOURCEMANAGER_WEB("Resource Manager", "RESOURCEMANAGER", "YARNUIV2", "/yarnuiv2/", true, 8088, 8090, false, true),
+    JOB_HISTORY_SERVER("Job History Server", "JOBHISTORY", "JOBHISTORYUI", "/jobhistory/", true, 19888, 19890, false, false),
+    HIVE_SERVER("Hive Server", "HIVESERVER2", "HIVE", "/hive/", false, 10001, 10002, true, true),
+    HIVE_SERVER_INTERACTIVE("Hive Server Interactive", "", "HIVE_INTERACTIVE", "/hive/", false, 10501, 10502, true, true),
+    ATLAS("Atlas", "ATLAS_SERVER", "ATLAS", "/atlas/", true, 21000, 21443, false, true),
+    SPARK_HISTORY_SERVER("Spark History Server", "SPARK_YARN_HISTORY_SERVER", "SPARKHISTORYUI", "/sparkhistory/", true, 18088, 18488, false, false),
+    ZEPPELIN("Zeppelin", "ZEPPELIN_SERVER", "ZEPPELIN", "/zeppelin/", false, 8885, 8886, false, false),
+    RANGER("Ranger", "RANGER_ADMIN", "RANGER", "/ranger/", true, 6080, 6182, false, true),
+    LIVY_SERVER("Livy Server", "LIVY_SERVER", "LIVYSERVER1", "/livy/ui", true, 8998, 8998, false, true),
+    OOZIE_UI("Oozie Server", "OOZIE_SERVER", "OOZIE", "/oozie/", true, 11000, 11443, false, true),
+    SOLR("Solr Server", "SOLR_SERVER", "SOLR", "/solr/", true, 8983, 8985, false, true),
+    HBASE_UI("HBase UI", "MASTER", "HBASEUI", "/hbase/webui/master", true, 16010, 16010, false, false),
+    HBASE_REST("HBase Rest", "HBASERESTSERVER", "WEBHBASE", "/hbase/", true, 20550, 20550, true, true);
 
     private final String displayName;
     private final String serviceName;
     private final String knoxService;
     private final String knoxUrl;
     private final boolean ssoSupported;
-    private final Integer cmPort;
+    private final Integer port;
+    private final Integer tlsPort;
     private final boolean apiOnly;
     private final boolean apiIncluded;
 
     ExposedService(String displayName, String serviceName, String knoxService, String knoxUrl,
-            boolean ssoSupported, Integer cmPort, boolean apiOnly, boolean apiIncluded) {
+            boolean ssoSupported, Integer port, Integer tlsPort, boolean apiOnly, boolean apiIncluded) {
         this.displayName = displayName;
         this.serviceName = serviceName;
         this.knoxService = knoxService;
         this.knoxUrl = knoxUrl;
         this.ssoSupported = ssoSupported;
-        this.cmPort = cmPort;
+        this.port = port;
+        this.tlsPort = tlsPort;
         this.apiOnly = apiOnly;
         this.apiIncluded = apiIncluded;
     }
@@ -86,8 +88,8 @@ public enum ExposedService {
     public static Map<String, Integer> getAllServicePorts() {
         return Arrays.stream(values()).filter(x -> !Strings.isNullOrEmpty(x.serviceName))
                 .filter(x -> !Strings.isNullOrEmpty(x.knoxService))
-                .filter(x -> Objects.nonNull(x.cmPort))
-                .collect(Collectors.toMap(k -> k.knoxService, v -> v.cmPort));
+                .filter(x -> Objects.nonNull(x.port))
+                .collect(Collectors.toMap(k -> k.knoxService, v -> v.port));
     }
 
     public String getServiceName() {
@@ -110,8 +112,12 @@ public enum ExposedService {
         return ssoSupported;
     }
 
-    public Integer getCmPort() {
-        return cmPort;
+    public Integer getPort() {
+        return port;
+    }
+
+    public Integer getTlsPort() {
+        return tlsPort;
     }
 
     public boolean isUISupported() {
