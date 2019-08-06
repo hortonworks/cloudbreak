8a70aa20711cbf46de07c6785875deaa0fe1ec4b
Peter Darvasi <pdarvasi@hortonworks.com>
2019-08-06 17:51:08 +0200

    CB-2878 - Hive Needs to be in HTTP Mode for Knox to Proxy J/ODBC Access

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hive/HiveKnoxConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hive/HiveKnoxConfigProvider.java
@@ -3,15 +3,13 @@ package com.sequenceiq.cloudbreak.cmtemplate.configproviders.hive;
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
 
 import java.util.List;
-import java.util.Objects;
-
 import org.springframework.stereotype.Component;
 
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
-import com.sequenceiq.cloudbreak.api.endpoint.v4.ExposedService;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateComponentConfigProvider;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils;
+import com.sequenceiq.cloudbreak.dto.KerberosConfig;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 
 @Component
@@ -19,14 +17,15 @@ public class HiveKnoxConfigProvider implements CmTemplateComponentConfigProvider
 
     private static final String HIVE_SERVICE_CONFIG_SAFETY_VALVE = "hive_service_config_safety_valve";
 
-    private static final String VALUE = ConfigUtils.getSafetyValveProperty("hive.server2.thrift.http.port", "10001")
-            + ConfigUtils.getSafetyValveProperty("hive.server2.thrift.http.path", "cliservice")
-            + ConfigUtils.getSafetyValveProperty("hive.server2.transport.mode", "http")
-            + ConfigUtils.getSafetyValveProperty("hive.server2.allow.user.substitution", "true");
-
     @Override
     public List<ApiClusterTemplateConfig> getServiceConfigs(CmTemplateProcessor templateProcessor, TemplatePreparationObject templatePreparationObject) {
-        return List.of(config(HIVE_SERVICE_CONFIG_SAFETY_VALVE, VALUE));
+        KerberosConfig kerberosConfigOpt = templatePreparationObject.getKerberosConfig().get();
+        String realm = kerberosConfigOpt.getRealm();
+        String keytab = ConfigUtils.getSafetyValveProperty("hive.server2.authentication.spnego.keytab", "hive.keytab");
+        String principal = ConfigUtils.getSafetyValveProperty("hive.server2.authentication.spnego.principal", "HTTP/_HOST@" + realm);
+        return List.of(config(HIVE_SERVICE_CONFIG_SAFETY_VALVE,
+                principal
+                        + keytab));
     }
 
     @Override
@@ -36,14 +35,12 @@ public class HiveKnoxConfigProvider implements CmTemplateComponentConfigProvider
 
     @Override
     public List<String> getRoleTypes() {
-        return List.of(HiveRoles.HIVE, HiveRoles.HIVEMETASTORE, HiveRoles.HIVESERVER2);
+        return List.of(HiveRoles.HIVESERVER2);
     }
 
     @Override
     public boolean isConfigurationNeeded(CmTemplateProcessor cmTemplateProcessor, TemplatePreparationObject source) {
-        return Objects.nonNull(source.getGatewayView())
-                && Objects.nonNull(source.getGatewayView().getExposedServices())
-                && source.getGatewayView().getExposedServices().contains(ExposedService.NAMENODE.getKnoxService());
+        return cmTemplateProcessor.isRoleTypePresentInService(getServiceType(), getRoleTypes()) &&
+                source.getKerberosConfig().isPresent();
     }
-
 }
