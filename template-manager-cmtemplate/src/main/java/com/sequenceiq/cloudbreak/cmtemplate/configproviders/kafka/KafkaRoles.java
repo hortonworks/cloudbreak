e1d991b547615b23716fab62c25b62b75ce4cca4
Krisztian Horvath <keyki.kk@gmail.com>
2019-08-06 15:36:39 +0200

    CB-2698 configure Kafka log.dirs to use the external volumes

---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kafka/KafkaRoles.java
@@ -0,0 +1,12 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.kafka;
+
+public class KafkaRoles {
+
+    public static final String KAFKA_SERVICE = "KAFKA";
+
+    public static final String KAFKA_BROKER = "KAFKA_BROKER";
+
+    private KafkaRoles() {
+    }
+
+}
---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kafka/KafkaVolumeConfigProvider.java
@@ -0,0 +1,41 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.kafka;
+
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
+import static com.sequenceiq.cloudbreak.template.VolumeUtils.buildVolumePathStringZeroVolumeHandled;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
+import com.sequenceiq.cloudbreak.cmtemplate.CmHostGroupRoleConfigProvider;
+import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
+import com.sequenceiq.cloudbreak.template.views.HostgroupView;
+
+@Component
+public class KafkaVolumeConfigProvider implements CmHostGroupRoleConfigProvider {
+
+    @Override
+    public List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, HostgroupView hostGroupView, TemplatePreparationObject source) {
+        Integer volumeCount = Objects.nonNull(hostGroupView) ? hostGroupView.getVolumeCount() : 0;
+        switch (roleType) {
+            case KafkaRoles.KAFKA_BROKER:
+                return List.of(config("log.dirs", buildVolumePathStringZeroVolumeHandled(volumeCount, "kafka")));
+            default:
+                return List.of();
+        }
+    }
+
+    @Override
+    public String getServiceType() {
+        return KafkaRoles.KAFKA_SERVICE;
+    }
+
+    @Override
+    public Set<String> getRoleTypes() {
+        return Set.of(KafkaRoles.KAFKA_BROKER);
+    }
+
+}
