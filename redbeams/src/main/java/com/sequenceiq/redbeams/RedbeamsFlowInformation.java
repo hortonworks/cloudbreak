0c45f2f5ab9d58ec3b098e061f97ebe56ba9cfb2
Gergely Papp <gpapp@hortonworks.com>
2019-08-07 16:53:41 +0200

    CB-2886 Redbeams: make flows restartable

--- redbeams/src/main/java/com/sequenceiq/redbeams/RedbeamsFlowInformation.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/RedbeamsFlowInformation.java
@@ -8,12 +8,17 @@ import org.springframework.stereotype.Component;
 
 import com.sequenceiq.flow.core.ApplicationFlowInformation;
 import com.sequenceiq.flow.core.config.FlowConfiguration;
+import com.sequenceiq.redbeams.flow.redbeams.provision.RedbeamsProvisionFlowConfig;
+import com.sequenceiq.redbeams.flow.redbeams.termination.RedbeamsTerminationFlowConfig;
 
 @Component
 public class RedbeamsFlowInformation implements ApplicationFlowInformation {
     @Override
     public List<Class<? extends FlowConfiguration<?>>> getRestartableFlows() {
-        return List.of();
+        return List.of(
+                RedbeamsProvisionFlowConfig.class,
+                RedbeamsTerminationFlowConfig.class
+        );
     }
 
     @Override
