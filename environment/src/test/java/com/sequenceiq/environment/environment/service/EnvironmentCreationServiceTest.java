a2dfdb99a39df16c23c77a041f42b4c82173c4a2
Richard Doktorics <rdoktorics@cloudera.com>
2019-08-14 16:04:11 +0200

    Revert "CB-2967 Unit test fix"
    This reverts commit 7d00cf614f4ce80e94881a2c647a203a7734e34b.

--- environment/src/test/java/com/sequenceiq/environment/environment/service/EnvironmentCreationServiceTest.java
+++ environment/src/test/java/com/sequenceiq/environment/environment/service/EnvironmentCreationServiceTest.java
@@ -108,7 +108,6 @@ class EnvironmentCreationServiceTest {
         when(authenticationDtoConverter.dtoToAuthentication(any())).thenReturn(new EnvironmentAuthentication());
         when(environmentService.getRegionsByEnvironment(eq(environment))).thenReturn(getCloudRegions());
         when(validatorService.validateCreation(any(), any(), any())).thenReturn(ValidationResult.builder().build());
-        when(validatorService.validateTelemetryLoggingStorageLocation(any())).thenReturn(ValidationResult.builder().build());
         when(environmentService.save(any())).thenReturn(environment);
         environmentCreationServiceUnderTest
                 .create(environmentCreationDto, ACCOUNT_ID, EnvironmentTestData.USER);
@@ -139,7 +138,6 @@ class EnvironmentCreationServiceTest {
         when(authenticationDtoConverter.dtoToAuthentication(any())).thenReturn(new EnvironmentAuthentication());
         when(environmentService.getRegionsByEnvironment(eq(environment))).thenReturn(getCloudRegions());
         when(validatorService.validateCreation(any(), any(), any())).thenReturn(ValidationResult.builder().error("nogood").build());
-        when(validatorService.validateTelemetryLoggingStorageLocation(any())).thenReturn(ValidationResult.builder().build());
         when(environmentService.save(any())).thenReturn(environment);
         assertThrows(BadRequestException.class, () -> environmentCreationServiceUnderTest.create(environmentCreationDto, ACCOUNT_ID,
                 EnvironmentTestData.USER));
