0e9a63b9391fd75098c95b7c4bdb4ada224ce69a
Krisztian Horvath <keyki.kk@gmail.com>
2019-08-27 14:32:58 +0200

    CB-2868 handle the case when users are created concurrently

--- core/src/main/java/com/sequenceiq/cloudbreak/service/user/CachedUserService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/user/CachedUserService.java
@@ -9,6 +9,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.sequenceiq.cloudbreak.common.service.TransactionService.TransactionRuntimeExecutionException;
 import com.sequenceiq.cloudbreak.common.user.CloudbreakUser;
 import com.sequenceiq.cloudbreak.workspace.model.User;
 
@@ -19,8 +20,12 @@ public class CachedUserService {
 
     @Cacheable(cacheNames = "userCache", key = "#cloudbreakUser")
     public User getUser(CloudbreakUser cloudbreakUser, Function<CloudbreakUser, User> findByTenantAndUsername, Function<CloudbreakUser, User> createUser) {
-        return Optional.ofNullable(findByTenantAndUsername.apply(cloudbreakUser))
-                .orElseGet(() -> createUser.apply(cloudbreakUser));
+        try {
+            return getOrCreateUser(cloudbreakUser, findByTenantAndUsername, createUser);
+        } catch (TransactionRuntimeExecutionException e) {
+            LOGGER.debug("User is already created, possibly on another thread", e);
+            return getOrCreateUser(cloudbreakUser, findByTenantAndUsername, createUser);
+        }
     }
 
     @CacheEvict(cacheNames = "userCache", key = "#cloudbreakUser")
@@ -32,4 +37,9 @@ public class CachedUserService {
     public void evictUser(User user) {
         LOGGER.debug("Remove all from user cache");
     }
+
+    private User getOrCreateUser(CloudbreakUser cloudbreakUser, Function<CloudbreakUser, User> findUser, Function<CloudbreakUser, User> createUser) {
+        return Optional.ofNullable(findUser.apply(cloudbreakUser)).orElseGet(() -> createUser.apply(cloudbreakUser));
+    }
+
 }
