bd63ec396f752e357fc3a8bc9ce3d84567fcac3d
Dénes Berger <dberger@hortonworks.com>
2019-08-30 13:06:18 +0200

    CB-3287 Shadow Access Denied response on AWS getBucketLocation call
    See comments in AwsObjectStorageConnector class

--- datalake/src/main/java/com/sequenceiq/datalake/service/validation/cloudstorage/CloudStorageLocationValidator.java
+++ datalake/src/main/java/com/sequenceiq/datalake/service/validation/cloudstorage/CloudStorageLocationValidator.java
@@ -1,13 +1,17 @@
 package com.sequenceiq.datalake.service.validation.cloudstorage;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.providerservices.CloudProviderServicesV4Endopint;
 import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
 import com.sequenceiq.cloudbreak.cloud.model.objectstorage.ObjectStorageMetadataRequest;
 import com.sequenceiq.cloudbreak.cloud.model.objectstorage.ObjectStorageMetadataResponse;
+import com.sequenceiq.cloudbreak.cloud.model.objectstorage.ObjectStorageMetadataResponse.ResponseStatus;
 import com.sequenceiq.cloudbreak.service.secret.service.SecretService;
 import com.sequenceiq.cloudbreak.validation.ValidationResult.ValidationResultBuilder;
+import com.sequenceiq.common.api.telemetry.response.LoggingResponse;
 import com.sequenceiq.common.model.FileSystemType;
 import com.sequenceiq.datalake.entity.Credential;
 import com.sequenceiq.datalake.service.validation.converter.CredentialToCloudCredentialConverter;
@@ -30,20 +34,34 @@ public class CloudStorageLocationValidator {
     }
 
     public void validate(String storageLocation, DetailedEnvironmentResponse environment, ValidationResultBuilder resultBuilder) {
-        String bucketName = getBucketName(storageLocation);
+        Optional<FileSystemType> fileSystemType = getFileSystemType(environment);
+        String bucketName = getBucketName(fileSystemType, storageLocation);
         Credential credential = getCredential(environment);
         CloudCredential cloudCredential = credentialToCloudCredentialConverter.convert(credential);
         ObjectStorageMetadataRequest request = createObjectStorageMetadataRequest(environment.getCloudPlatform(), cloudCredential, bucketName);
         ObjectStorageMetadataResponse response = cloudProviderServicesV4Endopint.getObjectStorageMetaData(request);
-        resultBuilder.ifError(() -> !environment.getLocation().getName().equals(response.getRegion()),
+        resultBuilder.ifError(() -> response.getStatus() == ResponseStatus.OK && !environment.getLocation().getName().equals(response.getRegion()),
                 String.format("Object storage location [%s] of bucket '%s' must match environment location [%s]",
                         response.getRegion(),
                         bucketName,
-                        environment.getLocation()));
+                        environment.getLocation().getName()));
     }
 
-    private String getBucketName(String storageLocation) {
-        storageLocation = storageLocation.replace(FileSystemType.S3.getProtocol() + "://", "");
+    private Optional<FileSystemType> getFileSystemType(DetailedEnvironmentResponse environment) {
+        LoggingResponse logging = environment.getTelemetry().getLogging();
+        if (logging.getS3() != null) {
+            return Optional.of(logging.getS3().getType());
+        }
+        if (logging.getWasb() != null) {
+            return Optional.of(logging.getWasb().getType());
+        }
+        return Optional.empty();
+    }
+
+    private String getBucketName(Optional<FileSystemType> fileSystemType, String storageLocation) {
+        if (fileSystemType.isPresent()) {
+            storageLocation = storageLocation.replace(fileSystemType.get().getProtocol() + "://", "");
+        }
         return storageLocation.split("/")[0];
     }
 
