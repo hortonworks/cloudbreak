1157ca8206d20a008a1c2f3f47a3271e4ddd6f0d
Dénes Berger <dberger@hortonworks.com>
2019-09-03 15:27:41 +0200

    CB-3323 Add Retry to Ambari calls

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/AmbariHostsStatusCheckerTask.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/AmbariHostsStatusCheckerTask.java
@@ -2,11 +2,14 @@ package com.sequenceiq.cloudbreak.service.cluster.flow;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.service.ClusterBasedStatusCheckerTask;
+import com.sequenceiq.cloudbreak.service.cluster.AmbariClientRetryer;
 import com.sequenceiq.cloudbreak.service.cluster.ambari.AmbariHostsUnavailableException;
 
 @Component
@@ -14,9 +17,12 @@ public class AmbariHostsStatusCheckerTask extends ClusterBasedStatusCheckerTask<
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AmbariHostsStatusCheckerTask.class);
 
+    @Inject
+    private AmbariClientRetryer ambariClientRetryer;
+
     @Override
     public boolean checkStatus(AmbariHostsCheckerContext t) {
-        Map<String, String> hostNamesWithStatus = t.getAmbariClient().getHostStatuses();
+        Map<String, String> hostNamesWithStatus = ambariClientRetryer.getHostStatuses(t.getAmbariClient());
         int totalNodes = hostNamesWithStatus.size();
         LOGGER.info("Ambari client found {} hosts ({} needed). [Stack: '{}'] | Known hosts with status: [{}]",
                 totalNodes, t.getHostsInCluster().size(), t.getStack().getId(), hostNamesWithStatus);
@@ -25,7 +31,7 @@ public class AmbariHostsStatusCheckerTask extends ClusterBasedStatusCheckerTask<
 
     @Override
     public void handleTimeout(AmbariHostsCheckerContext t) {
-        Map<String, String> hostNamesWithStatus = t.getAmbariClient().getHostStatuses();
+        Map<String, String> hostNamesWithStatus = ambariClientRetryer.getHostStatuses(t.getAmbariClient());
         throw new AmbariHostsUnavailableException(String.format("Operation timed out. Failed to find all '%s' Ambari hosts. Stack: '%s' "
                         + "| Known hosts with status: [%s]",
                 t.getHostsInCluster().size(), t.getStack().getId(), hostNamesWithStatus));
