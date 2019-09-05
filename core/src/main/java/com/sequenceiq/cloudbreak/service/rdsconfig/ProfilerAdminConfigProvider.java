e85b8adec625097575c397bf3de79ed268dc6f94
subrat.mishra <subrat.mishra@yahoo.com>
2019-09-05 11:59:12 +0530

    CB-3374: Added RDS support for Profiler Services

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/rdsconfig/ProfilerAdminConfigProvider.java
@@ -0,0 +1,67 @@
+package com.sequenceiq.cloudbreak.service.rdsconfig;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessorFactory;
+import com.sequenceiq.cloudbreak.domain.Blueprint;
+import com.sequenceiq.cloudbreak.service.blueprint.BlueprintService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Component
+public class ProfilerAdminConfigProvider extends AbstractRdsConfigProvider {
+    private static final String PILLAR_KEY = "profiler_admin";
+
+    @Value("${cb.profiler.admin.database.user:profiler_agent}")
+    private String profilerAdminDbUser;
+
+    @Value("${cb.profiler.admin.database.db:profiler_agent}")
+    private String profilerAdminDb;
+
+    @Value("${cb.profiler.admin.database.port:5432}")
+    private String profilerAdminDbPort;
+
+    @Inject
+    private CmTemplateProcessorFactory cmTemplateProcessorFactory;
+
+    @Inject
+    private BlueprintService blueprintService;
+
+    private boolean isRdsConfigNeedForProfilerAdmin(Blueprint blueprint) {
+        String blueprintText = blueprint.getBlueprintText();
+        CmTemplateProcessor cmTemplateProcessor = cmTemplateProcessorFactory.get(blueprintText);
+        return cmTemplateProcessor.isComponentExistsInHostGroup("PROFILER_ADMIN_AGENT", "master");
+    }
+
+    @Override
+    protected String getDbUser() {
+        return profilerAdminDbUser;
+    }
+
+    @Override
+    protected String getDb() {
+        return profilerAdminDb;
+    }
+
+    @Override
+    protected String getDbPort() {
+        return profilerAdminDbPort;
+    }
+
+    @Override
+    protected String getPillarKey() {
+        return PILLAR_KEY;
+    }
+
+    @Override
+    protected DatabaseType getRdsType() {
+        return DatabaseType.PROFILER_AGENT;
+    }
+
+    @Override
+    protected boolean isRdsConfigNeeded(Blueprint blueprint) {
+        return isRdsConfigNeedForProfilerAdmin(blueprint);
+    }
+}
---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/rdsconfig/ProfilerMetricsConfigProvider.java
@@ -0,0 +1,67 @@
+package com.sequenceiq.cloudbreak.service.rdsconfig;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessorFactory;
+import com.sequenceiq.cloudbreak.domain.Blueprint;
+import com.sequenceiq.cloudbreak.service.blueprint.BlueprintService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Component
+public class ProfilerMetricsConfigProvider extends AbstractRdsConfigProvider {
+    private static final String PILLAR_KEY = "profiler_metrics";
+
+    @Value("${cb.profiler.metrics.database.user:profiler_metric}")
+    private String profilerMetricsDbUser;
+
+    @Value("${cb.profiler.metrics.database.db:profiler_metric}")
+    private String profilerMetricsDb;
+
+    @Value("${cb.profiler.metrics.database.port:5432}")
+    private String profilerMetricsDbPort;
+
+    @Inject
+    private CmTemplateProcessorFactory cmTemplateProcessorFactory;
+
+    @Inject
+    private BlueprintService blueprintService;
+
+    private boolean isRdsConfigNeedForProfilerMetrics(Blueprint blueprint) {
+        String blueprintText = blueprint.getBlueprintText();
+        CmTemplateProcessor cmTemplateProcessor = cmTemplateProcessorFactory.get(blueprintText);
+        return cmTemplateProcessor.isComponentExistsInHostGroup("PROFILER_METRICS_AGENT", "master");
+    }
+
+    @Override
+    protected String getDbUser() {
+        return profilerMetricsDbUser;
+    }
+
+    @Override
+    protected String getDb() {
+        return profilerMetricsDb;
+    }
+
+    @Override
+    protected String getDbPort() {
+        return profilerMetricsDbPort;
+    }
+
+    @Override
+    protected String getPillarKey() {
+        return PILLAR_KEY;
+    }
+
+    @Override
+    protected DatabaseType getRdsType() {
+        return DatabaseType.PROFILER_METRIC;
+    }
+
+    @Override
+    protected boolean isRdsConfigNeeded(Blueprint blueprint) {
+        return isRdsConfigNeedForProfilerMetrics(blueprint);
+    }
+}
