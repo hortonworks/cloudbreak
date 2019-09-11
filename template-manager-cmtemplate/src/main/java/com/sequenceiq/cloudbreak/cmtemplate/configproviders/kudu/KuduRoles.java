3d92b191cbc405c80a1ffd645a5c0071a6942f26
Santosh Maniraju <SMANIRAJU@HORTONWORKS.COM>
2019-09-11 10:22:03 +0530

    CB-3347 Kudu Volume Config Initialization

---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kudu/KuduRoles.java
@@ -0,0 +1,13 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.kudu;
+
+public class KuduRoles {
+
+    public static final String KUDU_SERVICE = "KUDU";
+
+    public static final String KUDU_MASTER = "KUDU_MASTER";
+
+    public static final String KUDU_TSERVER = "KUDU_TSERVER";
+
+    private KuduRoles() { }
+
+}
