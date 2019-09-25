111974a9138cce3bb51cde91e04f31bb5b79b57f
Tamas Bihari <tbihari@hortonworks.com>
2019-09-25 12:49:09 +0200

    CB-3669 disable anonymization for every logging attempts

--- common/src/main/java/com/sequenceiq/cloudbreak/logger/format/SimpleLayoutFormat.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/logger/format/SimpleLayoutFormat.java
@@ -1,16 +1,11 @@
 package com.sequenceiq.cloudbreak.logger.format;
 
-import static com.sequenceiq.cloudbreak.common.anonymizer.AnonymizerUtil.anonymize;
-
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 public class SimpleLayoutFormat implements LayoutFormat {
 
     @Override
     public String format(ILoggingEvent event, String message, String loggerNameFilter) {
-        if (loggerNameFilter != null && event.getLoggerName().startsWith(loggerNameFilter)) {
-            message = anonymize(message);
-        }
         return message;
     }
 }
