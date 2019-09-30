40d4383a291d89bb6113dbc935195bd94192a461
sodre90 <perdos@cloudera.com>
2019-09-30 15:05:12 +0200

    remove unused stack api view repository and service

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/StackApiViewService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/StackApiViewService.java
@@ -9,30 +9,20 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.api.model.stack.StackViewResponse;
 import com.sequenceiq.cloudbreak.converter.stack.StackListItemToStackViewResponseConverter;
 import com.sequenceiq.cloudbreak.domain.projection.StackInstanceCount;
 import com.sequenceiq.cloudbreak.domain.projection.StackListItem;
-import com.sequenceiq.cloudbreak.domain.view.StackApiView;
 import com.sequenceiq.cloudbreak.repository.HostMetadataRepository;
 import com.sequenceiq.cloudbreak.repository.InstanceMetaDataRepository;
-import com.sequenceiq.cloudbreak.repository.StackApiViewRepository;
 import com.sequenceiq.cloudbreak.repository.StackRepository;
-import com.sequenceiq.cloudbreak.service.TransactionService;
-import com.sequenceiq.cloudbreak.service.TransactionService.TransactionExecutionException;
-import com.sequenceiq.cloudbreak.service.TransactionService.TransactionRuntimeExecutionException;
 
 @Service
 public class StackApiViewService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StackApiViewService.class);
-
-    @Inject
-    private StackApiViewRepository stackApiViewRepository;
 
     @Inject
     private StackRepository stackRepository;
@@ -42,13 +32,6 @@ public class StackApiViewService {
 
     @Inject
     private HostMetadataRepository hostMetadataRepository;
-
-    @Inject
-    private TransactionService transactionService;
-
-    @Inject
-    @Qualifier("conversionService")
-    private ConversionService conversionService;
 
     @Inject
     private StackListItemToStackViewResponseConverter stackListItemToStackViewResponseConverter;
@@ -66,16 +49,5 @@ public class StackApiViewService {
                     return stackListItemToStackViewResponseConverter.convert(item, stackInstanceCounts, stackUnhealthyInstanceCounts, sharedClusterName);
                 })
                 .collect(Collectors.toSet());
-    }
-
-    public StackViewResponse retrieveById(Long stackId) {
-        try {
-            return transactionService.required(() -> {
-                Optional<StackApiView> byId = stackApiViewRepository.findById(stackId);
-                return conversionService.convert(byId.orElse(null), StackViewResponse.class);
-            });
-        } catch (TransactionExecutionException e) {
-            throw new TransactionRuntimeExecutionException(e);
-        }
     }
 }
