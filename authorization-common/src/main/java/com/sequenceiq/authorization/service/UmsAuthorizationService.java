4549682b415e850a53cb9f85ab49886fd96557ff
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2019-10-04 09:56:56 +0200

    CB-3196 remove unused ums roles cache and logic

--- authorization-common/src/main/java/com/sequenceiq/authorization/service/UmsAuthorizationService.java
+++ authorization-common/src/main/java/com/sequenceiq/authorization/service/UmsAuthorizationService.java
@@ -1,28 +1,19 @@
 package com.sequenceiq.authorization.service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
-import com.cloudera.thunderhead.service.usermanagement.UserManagementProto;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Maps;
 import com.sequenceiq.authorization.resource.AuthorizationResource;
 import com.sequenceiq.authorization.resource.ResourceAction;
 import com.sequenceiq.authorization.resource.RightUtils;
-import com.sequenceiq.cloudbreak.auth.altus.Crn;
 import com.sequenceiq.cloudbreak.auth.altus.GrpcUmsClient;
 import com.sequenceiq.cloudbreak.logger.LoggerContextKey;
 import com.sequenceiq.cloudbreak.logger.MDCBuilder;
@@ -72,31 +63,5 @@ public class UmsAuthorizationService {
             requestId = UUID.randomUUID().toString();
         }
         return Optional.of(requestId);
-    }
-
-    private Map<String, String> getRolesByRights(String actorCrn) {
-        List<UserManagementProto.Role> roles = umsClient.listRoles(actorCrn, Crn.fromString(actorCrn).getAccountId(), getRequestId());
-        Map<String, String> rolesMap = Maps.newHashMap();
-        Arrays.stream(AuthorizationResource.values()).forEach(authorizationResource -> {
-            Arrays.stream(ResourceAction.values()).forEach(action -> {
-                String right = RightUtils.getRight(authorizationResource, action);
-                List<String> requiredRolesForRight = roles.stream()
-                        .filter(role -> role.getPolicyList().stream()
-                                .filter(policy -> policy.getPolicyDefinition().getStatementList().stream()
-                                        .filter(policyStatement -> policyStatement.getRightList().stream()
-                                                .filter(roleRight -> StringUtils.equals(roleRight, right))
-                                                .findAny()
-                                                .isPresent())
-                                        .findAny()
-                                        .isPresent())
-                                .findAny()
-                                .isPresent())
-                        .map(role -> Crn.fromString(role.getCrn()).getResource())
-                        .collect(Collectors.toList());
-                String requiredRoles = requiredRolesForRight.isEmpty() ? "PowerUser" : Joiner.on(", ").join(requiredRolesForRight);
-                rolesMap.put(right, requiredRoles);
-            });
-        });
-        return rolesMap;
     }
 }
