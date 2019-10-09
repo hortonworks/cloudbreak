b7e5654255d1d814cd48e148fe206467103bcc2b
Attila Kanto <attila.kanto@gmail.com>
2019-10-09 13:02:02 +0200

    OPSAPS-52977 Ensure that certificates from JDK default trustore are imported into the trustore of autotls.
    JDK has its own truststore, which contains certs from different trusted authorities, CM replaces the default truststore when launching apps like HS2.

--- orchestrator-salt/src/main/resources/salt/salt/cloudera/manager/scripts/setup-autotls.sh
+++ orchestrator-salt/src/main/resources/salt/salt/cloudera/manager/scripts/setup-autotls.sh
@@ -8,6 +8,7 @@ CM_KEYTAB_FILE={{ cm_keytab.path }}
 CM_PRINCIPAL={{ cm_keytab.principal }}
 CERTMANAGER_DIR="/etc/cloudera-scm-server/certs"
 CERTMANAGER_ARGS=
+CACERTS_DIR=/opt/cacerts
 OUT_FILE=`mktemp -t signed_ca_chain.XXXXXX.pem`
 
 rm -rf $CERTMANAGER_DIR
@@ -16,9 +17,17 @@ source /bin/activate_salt_env
 
 kinit -kt $CM_KEYTAB_FILE $CM_PRINCIPAL
 
-/opt/cloudera/cm-agent/bin/certmanager --location $CERTMANAGER_DIR setup --configure-services $CERTMANAGER_ARGS --override ca_dn="CN=${HOSTNAME}" --stop-at-csr --altname DNS:${HOSTNAME}
+mkdir -p ${CACERTS_DIR}
+
+rm -f ${CACERTS_DIR}/cacerts.p12
+rm -f ${CACERTS_DIR}/cacerts.pem
+
+keytool -importkeystore -srckeystore ${JAVA_HOME}/jre/lib/security/cacerts -srcstorepass changeit -destkeystore ${CACERTS_DIR}/cacerts.p12 -deststorepass changeit -deststoretype PKCS12
+openssl pkcs12 -in ${CACERTS_DIR}/cacerts.p12 -passin pass:changeit -out ${CACERTS_DIR}/cacerts.pem
+
+/opt/cloudera/cm-agent/bin/certmanager --location $CERTMANAGER_DIR setup --configure-services $CERTMANAGER_ARGS --override ca_dn="CN=${HOSTNAME}" --stop-at-csr --altname DNS:${HOSTNAME} --trusted-ca-certs ${CACERTS_DIR}/cacerts.pem
 /opt/cloudera/cm/bin/generate_intermediate_ca_ipa.sh $CM_PRINCIPAL ${CERTMANAGER_DIR}/CMCA/private/ca_csr.pem $OUT_FILE
-/opt/cloudera/cm-agent/bin/certmanager --location $CERTMANAGER_DIR setup --configure-services $CERTMANAGER_ARGS --override ca_dn="CN=${HOSTNAME}" --signed-ca-cert=$OUT_FILE --skip-cm-init --altname DNS:${HOSTNAME} > $CERTMANAGER_DIR/auto-tls.init.txt
+/opt/cloudera/cm-agent/bin/certmanager --location $CERTMANAGER_DIR setup --configure-services $CERTMANAGER_ARGS --override ca_dn="CN=${HOSTNAME}" --signed-ca-cert=$OUT_FILE --skip-cm-init --altname DNS:${HOSTNAME} --trusted-ca-certs ${CACERTS_DIR}/cacerts.pem > $CERTMANAGER_DIR/auto-tls.init.txt
 
 rm -rf $OUT_FILE
 
