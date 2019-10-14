21efbfe17b045d80447cfebd8ca8152521115f34
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-10-14 14:32:30 +0200

    CB-3773 Retry cluster creation does not work for datahub (datamart) cluster

--- core/src/test/java/com/sequenceiq/distrox/v1/distrox/converter/SdxConverterTest.java
+++ core/src/test/java/com/sequenceiq/distrox/v1/distrox/converter/SdxConverterTest.java
@@ -1,25 +1,17 @@
 package com.sequenceiq.distrox.v1.distrox.converter;
 
-import static org.mockito.Mockito.when;
-
-import java.util.Collections;
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.cluster.sharedservice.SharedServiceV4Request;
-import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.domain.stack.StackStatus;
 import com.sequenceiq.cloudbreak.exception.BadRequestException;
-import com.sequenceiq.cloudbreak.service.stack.StackService;
-import com.sequenceiq.distrox.api.v1.distrox.model.sharedservice.SdxV1Request;
+import com.sequenceiq.sdx.api.model.SdxClusterResponse;
+import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
 
 @ExtendWith(MockitoExtension.class)
 public class SdxConverterTest {
@@ -27,69 +19,34 @@ public class SdxConverterTest {
     @InjectMocks
     private SdxConverter underTest;
 
-    @Mock
-    private StackService stackService;
-
     @Test
     public void testGetSharedServiceWhenSdxNullAndEnvironmentHasNotSdx() {
-        String environmenName = "environmenName";
-        when(stackService.getByEnvironmentCrnAndStackType(environmenName, StackType.DATALAKE)).thenReturn(Collections.emptyList());
-
-        SharedServiceV4Request sdxRequest = underTest.getSharedService(null, environmenName);
+        SharedServiceV4Request sdxRequest = underTest.getSharedService(null);
 
         Assertions.assertNull(sdxRequest);
     }
 
     @Test
-    public void testGetSharedServiceWhenSdxNullAndEnvironmentHasMoreThanOneSdx() {
-        String environmenName = "environmenName";
-        when(stackService.getByEnvironmentCrnAndStackType(environmenName, StackType.DATALAKE)).thenReturn(List.of(new Stack(), new Stack()));
+    public void testGetSharedServiceWhenSdxIsRunning() {
+        SdxClusterResponse sdxClusterResponse = new SdxClusterResponse();
+        sdxClusterResponse.setName("some-sdx");
+        sdxClusterResponse.setStatus(SdxClusterStatusResponse.RUNNING);
 
-        BadRequestException exception = Assertions.assertThrows(BadRequestException.class, () -> underTest.getSharedService(null, environmenName));
-        Assertions.assertEquals(exception.getMessage(), "More than one Datalake attached to the environment. Please specify one.");
-    }
-
-    @Test
-    public void testGetSharedServiceWhenSdxNullAndSdxInvIsRunning() {
-        String environmenName = "environmenName";
-        Stack stack = new Stack();
-        stack.setName("some-sdx");
-        stack.setStackStatus(getStatus(Status.AVAILABLE));
-        when(stackService.getByEnvironmentCrnAndStackType(environmenName, StackType.DATALAKE)).thenReturn(List.of(stack));
-
-        SharedServiceV4Request sdxRequest = underTest.getSharedService(null, environmenName);
+        SharedServiceV4Request sdxRequest = underTest.getSharedService(sdxClusterResponse);
 
         Assertions.assertEquals("some-sdx", sdxRequest.getDatalakeName());
     }
 
     @Test
-    public void testGetSharedServiceWhenSdxNotAttachedToEnvironment() {
-        String environmenName = "environmenName";
-        Stack stack = new Stack();
-        stack.setName("some-sdx");
-        stack.setStackStatus(getStatus(Status.AVAILABLE));
-        when(stackService.getByEnvironmentCrnAndStackType(environmenName, StackType.DATALAKE)).thenReturn(List.of(stack));
+    public void testGetSharedServiceWhenSdxIsNotRunning() {
+        SdxClusterResponse sdxClusterResponse = new SdxClusterResponse();
+        sdxClusterResponse.setName("some-sdx");
+        sdxClusterResponse.setStatusReason("external db creation in progress");
+        sdxClusterResponse.setStatus(SdxClusterStatusResponse.EXTERNAL_DATABASE_CREATION_IN_PROGRESS);
 
-        SdxV1Request sdxRequest = new SdxV1Request();
-        sdxRequest.setName("other-sdx-name");
-
-        BadRequestException exception = Assertions.assertThrows(BadRequestException.class, () -> underTest.getSharedService(sdxRequest, environmenName));
-        Assertions.assertEquals(exception.getMessage(), "The given Datalake not attached to the environment");
-    }
-
-    @Test
-    public void testGetSharedServiceWhenSdxInvIsNotRunning() {
-        String environmenName = "environmenName";
-        Stack stack = new Stack();
-        stack.setName("some-sdx");
-        stack.setStackStatus(getStatus(Status.DELETE_FAILED));
-        when(stackService.getByEnvironmentCrnAndStackType(environmenName, StackType.DATALAKE)).thenReturn(List.of(stack));
-
-        SdxV1Request sdxRequest = new SdxV1Request();
-        sdxRequest.setName("some-sdx");
-
-        BadRequestException exception = Assertions.assertThrows(BadRequestException.class, () -> underTest.getSharedService(sdxRequest, environmenName));
-        Assertions.assertEquals(exception.getMessage(), "Datalake status is invalid. Current state is DELETE_FAILED instead of AVAILABLE");
+        BadRequestException exception = Assertions.assertThrows(BadRequestException.class, () -> underTest.getSharedService(sdxClusterResponse));
+        Assertions.assertEquals(exception.getMessage(), "Datalake should be running state. Current state is 'external db creation in progress' "
+                + "instead of Running");
     }
 
     private StackStatus getStatus(Status status) {
