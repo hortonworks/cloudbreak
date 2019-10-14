b7db90ecca8b3e200c53d37c46dad3df0d759538
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-10-14 14:32:30 +0200

    CB-3773 Retry cluster creation does not work for datahub (datamart) cluster

--- core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/SdxConverter.java
+++ core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/SdxConverter.java
@@ -1,56 +1,29 @@
 package com.sequenceiq.distrox.v1.distrox.converter;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.cluster.sharedservice.SharedServiceV4Request;
-import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.exception.BadRequestException;
-import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.distrox.api.v1.distrox.model.sharedservice.SdxV1Request;
+import com.sequenceiq.sdx.api.model.SdxClusterResponse;
+import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
 
 @Component
 public class SdxConverter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SdxConverter.class);
 
-    @Inject
-    private StackService stackService;
-
-    public SharedServiceV4Request getSharedService(SdxV1Request sdx) {
-        return getSharedServiceV4Request(sdx.getName());
-    }
-
-    public SharedServiceV4Request getSharedService(SdxV1Request sdx, String environemntCrn) {
-        List<Stack> datalakes = stackService.getByEnvironmentCrnAndStackType(environemntCrn, StackType.DATALAKE);
-        StringBuilder name = new StringBuilder();
+    public SharedServiceV4Request getSharedService(SdxClusterResponse sdx) {
         if (sdx == null) {
-            if (datalakes.isEmpty()) {
-                LOGGER.info("We don't attach the cluster to any Datalake, because the environemnt has not SDX. So we continue the creation as simple WORKLOAD.");
-                return null;
-            } else if (datalakes.size() > 1) {
-                throw new BadRequestException("More than one Datalake attached to the environment. Please specify one.");
-            }
-            name.append(datalakes.get(0).getName());
-        } else {
-            name.append(sdx.getName());
+            LOGGER.info("We don't attach the cluster to any Datalake, because the environment has not SDX. So we continue the creation as simple WORKLOAD.");
+            return null;
         }
-        Stack datalake = datalakes.stream()
-                .filter(sr -> sr.getName().equals(name.toString()))
-                .findFirst()
-                .orElseThrow(() -> new BadRequestException("The given Datalake not attached to the environment"));
-        if (datalake.getStatus() != Status.AVAILABLE) {
-            throw new BadRequestException(String.format("Datalake status is invalid. Current state is %s instead of %s", datalake.getStatus().name(),
-                    Status.AVAILABLE));
+        if (sdx.getStatus() != SdxClusterStatusResponse.RUNNING) {
+            throw new BadRequestException(String.format("Datalake should be running state. Current state is '%s' instead of Running", sdx.getStatusReason()));
         }
-        return getSharedServiceV4Request(name.toString());
+        return getSharedServiceV4Request(sdx.getName());
     }
 
     public SdxV1Request getSdx(SharedServiceV4Request sharedServiceV4Request) {
