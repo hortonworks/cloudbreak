fb46a3dea3200051cacf78416b9240c24525d5d8
Dénes Berger <dberger@hortonworks.com>
2019-10-18 13:51:01 +0200

    fix integration test after merge conflict from rc-2.13

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
@@ -438,7 +438,7 @@ public class ClusterTemplateTest extends AbstractIntegrationTest {
         assertNotNull(entity);
         assertNotNull(entity.getResponses());
         long defaultCount = entity.getResponses().stream().filter(template -> ResourceStatus.DEFAULT.equals(template.getStatus())).count();
-        assertEquals("Should have 6 of default cluster templates", 6, defaultCount);
+        assertEquals("Should have 6 of default cluster templates", 11, defaultCount);
         return entity;
     }
 
