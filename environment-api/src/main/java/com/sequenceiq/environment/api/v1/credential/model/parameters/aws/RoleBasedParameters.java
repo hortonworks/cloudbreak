885b9d9b057f923e68529f159e584d9cef99e6ed
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-10-21 14:48:20 +0200

    CB-2321 Cannot determine the ARN associated with a role credential

--- environment-api/src/main/java/com/sequenceiq/environment/api/v1/credential/model/parameters/aws/RoleBasedParameters.java
+++ environment-api/src/main/java/com/sequenceiq/environment/api/v1/credential/model/parameters/aws/RoleBasedParameters.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.environment.api.v1.credential.model.parameters.aws;
 
+import static com.sequenceiq.environment.api.doc.credential.CredentialModelDescription.AWS_ROLE_ARN;
+
 import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
@@ -17,7 +19,7 @@ import io.swagger.annotations.ApiModelProperty;
 public class RoleBasedParameters implements Serializable {
 
     @NotNull
-    @ApiModelProperty(required = true, example = "arn:aws:iam::981628461338:role/example-role")
+    @ApiModelProperty(value = AWS_ROLE_ARN, required = true, example = "arn:aws:iam::981628461338:role/example-role")
     private String roleArn;
 
     public String getRoleArn() {
