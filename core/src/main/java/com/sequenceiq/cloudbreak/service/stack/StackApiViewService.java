db2cdd016b27ea6d75865e5609f0b56ca3e8ea64
David Szabo <dszabo@cloudera.com>
2019-10-21 10:01:21 +0200

    CB-4055 Return hostmetadata in stacks response

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/StackApiViewService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/StackApiViewService.java
@@ -1,20 +1,25 @@
 package com.sequenceiq.cloudbreak.service.stack;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.sequenceiq.cloudbreak.api.model.stack.StackViewResponse;
 import com.sequenceiq.cloudbreak.converter.stack.StackListItemToStackViewResponseConverter;
 import com.sequenceiq.cloudbreak.domain.projection.StackInstanceCount;
 import com.sequenceiq.cloudbreak.domain.projection.StackListItem;
+import com.sequenceiq.cloudbreak.domain.view.HostGroupView;
+import com.sequenceiq.cloudbreak.repository.HostGroupViewRepository;
 import com.sequenceiq.cloudbreak.repository.HostMetadataRepository;
 import com.sequenceiq.cloudbreak.repository.InstanceMetaDataRepository;
 import com.sequenceiq.cloudbreak.repository.StackRepository;
@@ -31,6 +36,9 @@ public class StackApiViewService {
     private InstanceMetaDataRepository instanceMetaDataRepository;
 
     @Inject
+    private HostGroupViewRepository hostGroupViewRepository;
+
+    @Inject
     private HostMetadataRepository hostMetadataRepository;
 
     @Inject
@@ -42,12 +50,28 @@ public class StackApiViewService {
         Map<Long, Integer> stackUnhealthyInstanceCounts = hostMetadataRepository.countUnhealthyByWorkspaceId(workspaceId).stream()
                 .collect(Collectors.toMap(StackInstanceCount::getStackId, StackInstanceCount::getInstanceCount));
         Set<StackListItem> stackList = stackRepository.findByWorkspaceId(workspaceId);
+        Set<Long> clusterIds = stackList.stream().map(StackListItem::getClusterId).collect(Collectors.toSet());
+        Map<Long, List<HostGroupView>> clusterHgMap = getClusterHostGroupMap(clusterIds);
         return stackList.stream()
                 .map(item -> {
                     String sharedClusterName = Optional.ofNullable(item.getSharedClusterId())
                             .map(sharedClusterId -> stackRepository.findNameByIdAndWorkspaceId(sharedClusterId, workspaceId)).orElse(null);
-                    return stackListItemToStackViewResponseConverter.convert(item, stackInstanceCounts, stackUnhealthyInstanceCounts, sharedClusterName);
+                    return stackListItemToStackViewResponseConverter.convert(item, stackInstanceCounts, stackUnhealthyInstanceCounts, sharedClusterName,
+                            clusterHgMap.get(item.getClusterId()));
                 })
                 .collect(Collectors.toSet());
+    }
+
+    private Map<Long, List<HostGroupView>> getClusterHostGroupMap(Set<Long> clusterIds) {
+        if (CollectionUtils.isEmpty(clusterIds)) {
+            return Map.of();
+        }
+
+        Set<HostGroupView> allHostGroups = hostGroupViewRepository.findHostGroupsInClusterList(clusterIds);
+        return allHostGroups.stream().collect(Collectors.toMap(
+                hg -> hg.getCluster().getId(),
+                List::of,
+                (left, right) -> Stream.concat(left.stream(), right.stream()).collect(Collectors.toList())
+        ));
     }
 }
