1c97b2e6b3cd4e6fe8905db8fc4155535af13240
Tamas Bihari <tbihari@hortonworks.com>
2019-10-22 18:34:08 +0200

    CB-4102 backward compatibility for service URLs when Knox is on port 8443

--- core/src/main/java/com/sequenceiq/cloudbreak/service/gateway/GatewayService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/gateway/GatewayService.java
@@ -2,6 +2,7 @@ package com.sequenceiq.cloudbreak.service.gateway;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.domain.stack.cluster.gateway.Gateway;
@@ -10,10 +11,14 @@ import com.sequenceiq.cloudbreak.repository.GatewayRepository;
 @Service
 public class GatewayService {
 
+    @Value("${cb.https.port}")
+    private String httpsPort;
+
     @Inject
     private GatewayRepository repository;
 
     public Gateway save(Gateway gateway) {
+        gateway.setGatewayPort(Integer.valueOf(httpsPort));
         return repository.save(gateway);
     }
 
