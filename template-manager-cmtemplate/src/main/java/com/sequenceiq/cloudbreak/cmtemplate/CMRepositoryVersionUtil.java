e6e4ac30f29e6bbe17cad038d4fc694c27689c22
Balazs Bence Sari <bsari@cloudera.com>
2019-11-04 17:21:40 +0100

    CB-4250 Config provider for Kafka PAM authentication
    CB-4250 Config provider for Kafka PAM authentication #1
    
    CB-4250 Config provider for Kafka PAM authentication #2
    
    CB-4250 Config provider for Kafka PAM authentication #3

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/CMRepositoryVersionUtil.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/CMRepositoryVersionUtil.java
@@ -15,6 +15,8 @@ public class CMRepositoryVersionUtil {
 
     public static final Versioned CLOUDERAMANAGER_VERSION_7_0_2 = () -> "7.0.2";
 
+    public static final Versioned CLOUDERAMANAGER_VERSION_7_1_0 = () -> "7.1.0";
+
     public static final Versioned CFM_VERSION_2_0_0_0 = () -> "2.0.0.0";
 
     private CMRepositoryVersionUtil() {
