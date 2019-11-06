408901a6537df87ae5a5f46348c72876ba27930f
lnardai <laci.nardai@gmail.com>
2019-11-06 14:22:13 +0100

    CB-4054 no strictly related, but forced termination was missing some cleanup, moved everything to finalizeTermination. (Will conflict with master)

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/TerminationService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/TerminationService.java
@@ -3,29 +3,37 @@ package com.sequenceiq.cloudbreak.service.stack.flow;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.InstanceStatus;
+import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
+import com.sequenceiq.cloudbreak.common.service.Clock;
+import com.sequenceiq.cloudbreak.common.service.TransactionService;
+import com.sequenceiq.cloudbreak.common.service.TransactionService.TransactionExecutionException;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.OrchestratorTypeResolver;
+import com.sequenceiq.cloudbreak.core.flow2.stack.termination.CleanupFreeIpaTask;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceGroup;
 import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
-import com.sequenceiq.cloudbreak.common.service.Clock;
+import com.sequenceiq.cloudbreak.logger.MDCBuilder;
 import com.sequenceiq.cloudbreak.service.CloudbreakException;
 import com.sequenceiq.cloudbreak.service.StackUpdater;
-import com.sequenceiq.cloudbreak.common.service.TransactionService;
-import com.sequenceiq.cloudbreak.common.service.TransactionService.TransactionExecutionException;
 import com.sequenceiq.cloudbreak.service.cluster.flow.ClusterTerminationService;
+import com.sequenceiq.cloudbreak.service.datalake.DatalakeResourcesService;
 import com.sequenceiq.cloudbreak.service.stack.InstanceGroupService;
 import com.sequenceiq.cloudbreak.service.stack.InstanceMetaDataService;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.FreeIpaV1Endpoint;
 
 @Service
 public class TerminationService {
@@ -56,12 +64,30 @@ public class TerminationService {
     private TransactionService transactionService;
 
     @Inject
+    private DatalakeResourcesService datalakeResourcesService;
+
+    @Inject
+    @Qualifier("cloudbreakListeningScheduledExecutorService")
+    private ExecutorService executorService;
+
+    @Inject
+    private FreeIpaV1Endpoint freeIpaV1Endpoint;
+
+    @Inject
+    private ThreadBasedUserCrnProvider threadBasedUserCrnProvider;
+
+    @Inject
     private Clock clock;
 
     public void finalizeTermination(Long stackId, boolean force) {
         Stack stack = stackService.getByIdWithListsInTransaction(stackId);
         Date now = new Date();
         String terminatedName = stack.getName() + DELIMITER + now.getTime();
+        if (stack.getType() == StackType.DATALAKE) {
+            datalakeResourcesService.deleteWithDependenciesByStackId(stack.getId());
+        }
+        executorService.execute(new CleanupFreeIpaTask(stack, freeIpaV1Endpoint, threadBasedUserCrnProvider, threadBasedUserCrnProvider.getUserCrn(),
+                MDCBuilder.getMdcContextMap()));
         Cluster cluster = stack.getCluster();
         try {
             transactionService.required(() -> {
