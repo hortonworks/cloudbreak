d62e9a50fa26342523904afb2b146f36b5c770b3
doktoric <rdoktorics@cloudera.com>
2019-11-13 11:02:05 +0100

    CB-4249 Expose Schema Registry API in CDPD

--- integration-test/scripts/create-image.sh
+++ integration-test/scripts/create-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
 rm -rf ./integcb/docker-containers
@@ -13,7 +13,7 @@ cp ../core/build/libs/cloudbreak.jar ./integcb/docker-containers/docker-cloudbre
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-cloudbreak:/integcb/docker-containers/docker-cloudbreak \
- busybox /bin/sh -c "sed -i '/cloudbreak-\$VERSION/c\ADD cloudbreak.jar /' /integcb/docker-containers/docker-cloudbreak/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/cloudbreak-\$VERSION/c\ADD cloudbreak.jar /' /integcb/docker-containers/docker-cloudbreak/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev cloudbreak test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak:dev ./integcb/docker-containers/docker-cloudbreak
@@ -25,7 +25,7 @@ cp ../autoscale/build/libs/periscope.jar ./integcb/docker-containers/docker-auto
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-autoscale:/integcb/docker-containers/docker-autoscale \
- busybox /bin/sh -c "sed -i '/periscope-\$VERSION/c\ADD periscope.jar /' /integcb/docker-containers/docker-autoscale/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/periscope-\$VERSION/c\ADD periscope.jar /' /integcb/docker-containers/docker-autoscale/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev autoscale test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-autoscale:dev ./integcb/docker-containers/docker-autoscale
@@ -37,7 +37,7 @@ cp ../datalake/build/libs/datalake.jar ./integcb/docker-containers/docker-datala
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-datalake:/integcb/docker-containers/docker-datalake \
- busybox /bin/sh -c "sed -i '/datalake-\$VERSION/c\ADD datalake.jar /' /integcb/docker-containers/docker-datalake/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/datalake-\$VERSION/c\ADD datalake.jar /' /integcb/docker-containers/docker-datalake/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev datalake test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-datalake:dev ./integcb/docker-containers/docker-datalake
@@ -49,7 +49,7 @@ cp ../freeipa/build/libs/freeipa.jar ./integcb/docker-containers/docker-freeipa
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-freeipa:/integcb/docker-containers/docker-freeipa \
- busybox /bin/sh -c "sed -i '/freeipa-\$VERSION/c\ADD freeipa.jar /' /integcb/docker-containers/docker-freeipa/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/freeipa-\$VERSION/c\ADD freeipa.jar /' /integcb/docker-containers/docker-freeipa/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev freeipa test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-freeipa:dev ./integcb/docker-containers/docker-freeipa
@@ -61,7 +61,7 @@ cp ../environment/build/libs/environment.jar ./integcb/docker-containers/docker-
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-environment:/integcb/docker-containers/docker-environment \
- busybox /bin/sh -c "sed -i '/environment-\$VERSION/c\ADD environment.jar /' /integcb/docker-containers/docker-environment/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/environment-\$VERSION/c\ADD environment.jar /' /integcb/docker-containers/docker-environment/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev environment test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-environment:dev ./integcb/docker-containers/docker-environment
@@ -73,7 +73,7 @@ cp ../redbeams/build/libs/redbeams.jar ./integcb/docker-containers/docker-redbea
 date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -d -v "$(pwd)"/integcb/docker-containers/docker-redbeams:/integcb/docker-containers/docker-redbeams \
- busybox /bin/sh -c "sed -i '/redbeams-\$VERSION/c\ADD redbeams.jar /' /integcb/docker-containers/docker-redbeams/Dockerfile"
+ busybox:1.31.1 /bin/sh -c "sed -i '/redbeams-\$VERSION/c\ADD redbeams.jar /' /integcb/docker-containers/docker-redbeams/Dockerfile"
 date
 echo -e "\n\033[1;96m--- Build dev redbeams test image\033[0m\n"
 docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-redbeams:dev ./integcb/docker-containers/docker-redbeams
