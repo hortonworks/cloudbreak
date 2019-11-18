e82d2823c50e2c669a1b7b85a82688f8d92ad5cb
bbihari <bbihari@hortonworks.com>
2019-11-18 13:10:19 +0100

    CB-4511 AnonymizerUtil takes 2-3s on REST Structured Events, regexp optimalization

--- structuredevent-model/src/main/java/com/sequenceiq/cloudbreak/structuredevent/json/AnonymizerUtil.java
+++ structuredevent-model/src/main/java/com/sequenceiq/cloudbreak/structuredevent/json/AnonymizerUtil.java
@@ -8,16 +8,16 @@ public class AnonymizerUtil {
 
     private static final ReplacePattern[] PATTERNS = {
             //common PW
-            new ReplacePattern("(?i)(?<="
+            new ReplacePattern("(?i)("
                     + "password=|password\":\"|password:|password |"
                     + "pass=|pass\":\"|pass:|pass |"
                     + "key=|key\":\"|key:|key |"
                     + "credential=|credential\":\"|credential:|credential "
-                    + ")(?=\\S*)[^\\s'\"]*", REPLACEMENT),
+                    + ")([^\\s'\"]*)", REPLACEMENT),
             //WASB
-            new ReplacePattern("(?i)(?<=\\.blob\\.core\\.windows\\.net\":\")(?=\\S*)[^\\s'\"]*", REPLACEMENT),
+            new ReplacePattern("(?i)(\\.blob\\.core\\.windows\\.net\":\")([^\\s'\"])*", REPLACEMENT),
             //CM
-            new ReplacePattern("(?<=\"name\":\\s{0,100}\"[^\"]{0,100}password\",\\s{0,100}\"value\":\\s{0,100}\")[^\\s'\"]*", REPLACEMENT)
+            new ReplacePattern("(\"name\":\\s*\"[^\"]*password\",\\s*\"value\":\\s*\")[^\\s'\"]*", REPLACEMENT)
     };
 
     private AnonymizerUtil() {
@@ -45,11 +45,8 @@ public class AnonymizerUtil {
         }
 
         public String replaceAll(String input) {
-            return pattern.matcher(input).replaceAll(replacement);
+            return pattern.matcher(input).replaceAll("$1" + replacement);
         }
     }
 
 }
-
-
-
