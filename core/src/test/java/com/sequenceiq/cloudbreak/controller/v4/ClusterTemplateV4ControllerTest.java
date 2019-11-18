cf19f4f41ed7a093dc69db381c42e7a8adc94956
Lajos Rodek <15786876+lajosrodek@users.noreply.github.com>
2019-11-18 16:40:34 +0100

    CB-4488 Hide Azure cluster definitions when Azure entitlement is missing

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/controller/v4/ClusterTemplateV4ControllerTest.java
@@ -0,0 +1,47 @@
+package com.sequenceiq.cloudbreak.controller.v4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.clustertemplate.responses.ClusterTemplateViewV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.clustertemplate.responses.ClusterTemplateViewV4Responses;
+import com.sequenceiq.cloudbreak.service.blueprint.BlueprintService;
+import com.sequenceiq.cloudbreak.service.template.ClusterTemplateService;
+
+@ExtendWith(MockitoExtension.class)
+class ClusterTemplateV4ControllerTest {
+
+    private static final Long WORKSPACE_ID = 123L;
+
+    @Mock
+    private BlueprintService blueprintService;
+
+    @Mock
+    private ClusterTemplateService clusterTemplateService;
+
+    @InjectMocks
+    private ClusterTemplateV4Controller underTest;
+
+    @Test
+    void testList() {
+        Set<ClusterTemplateViewV4Response> responses = Set.of();
+        when(clusterTemplateService.listInWorkspaceAndCleanUpInvalids(WORKSPACE_ID)).thenReturn(responses);
+
+        ClusterTemplateViewV4Responses result = underTest.list(WORKSPACE_ID);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getResponses()).isSameAs(responses);
+        verify(blueprintService).updateDefaultBlueprintCollection(WORKSPACE_ID);
+        verify(clusterTemplateService).updateDefaultClusterTemplates(WORKSPACE_ID);
+    }
+
+}
