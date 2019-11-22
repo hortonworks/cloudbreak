ed8cf37a7414c8b0b1a7c7eb657fd8b32c919d9f
Gergely Topolyai <gtopolyai@hortonworks.com>
2019-11-22 13:00:42 +0100

    CB-4575 Polling aws statuses after the start or stop requests fired

---
+++ cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/poller/CancellableAttemptMaker.java
@@ -0,0 +1,34 @@
+package com.sequenceiq.cloudbreak.cloud.aws.poller;
+
+import static com.sequenceiq.cloudbreak.cloud.scheduler.PollGroup.CANCELLED;
+
+import com.dyngr.core.AttemptMaker;
+import com.dyngr.core.AttemptResult;
+import com.dyngr.core.AttemptResults;
+import com.sequenceiq.cloudbreak.cloud.scheduler.CancellationException;
+import com.sequenceiq.cloudbreak.cloud.scheduler.PollGroup;
+import com.sequenceiq.cloudbreak.cloud.store.InMemoryStateStore;
+
+public abstract class CancellableAttemptMaker<V> implements AttemptMaker<V> {
+
+    private final Long stackId;
+
+    protected CancellableAttemptMaker(Long stackId) {
+        this.stackId = stackId;
+    }
+
+    @Override
+    public AttemptResult<V> process() throws Exception {
+        if (cancelled()) {
+            return AttemptResults.breakFor(new CancellationException("The flow is cancelled"));
+        }
+        return doProcess();
+    }
+
+    protected abstract AttemptResult<V> doProcess();
+
+    private boolean cancelled() {
+        PollGroup pollGroup = InMemoryStateStore.getStack(stackId);
+        return CANCELLED.equals(pollGroup);
+    }
+}
