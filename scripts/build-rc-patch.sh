d684c64f15e249d662ff86a8bfd089b499f4481e
doktoric <rdoktorics@cloudera.com>
2019-11-30 07:18:01 +0100

    CB-4685 correct path in swagger

--- scripts/build-rc-patch.sh
+++ scripts/build-rc-patch.sh
@@ -7,7 +7,7 @@ echo "Build version: $VERSION"
 
 aws s3 cp ./core/build/swagger/cb.json "s3://cloudbreak-swagger/swagger-${VERSION}.json" --acl public-read
 aws s3 cp ./environment/build/swagger/environment.json "s3://environment-swagger/swagger-${VERSION}.json" --acl public-read
-aws s3 cp ./datalake/build/swagger/freeipa.json "s3://freeipa-swagger/swagger-${VERSION}.json" --acl public-read
-aws s3 cp ./freeipa/build/swagger/redbeams.json "s3://redbeams-swagger/swagger-${VERSION}.json" --acl public-read
-aws s3 cp ./redbeams/build/swagger/datalake.json "s3://datalake-swagger/swagger-${VERSION}.json" --acl public-read
+aws s3 cp ./freeipa/build/swagger/freeipa.json "s3://freeipa-swagger/swagger-${VERSION}.json" --acl public-read
+aws s3 cp ./redbeams/build/swagger/redbeams.json "s3://redbeams-swagger/swagger-${VERSION}.json" --acl public-read
+aws s3 cp ./datalake/build/swagger/datalake.json "s3://datalake-swagger/swagger-${VERSION}.json" --acl public-read
 aws s3 cp ./autoscale/build/swagger/autoscale.json "s3://autoscale-swagger/swagger-${VERSION}.json" --acl public-read
