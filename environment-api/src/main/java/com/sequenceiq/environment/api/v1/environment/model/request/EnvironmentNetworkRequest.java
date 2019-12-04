74a05191d371fac289c34f95b60f39d90617464d
doktoric <rdoktorics@cloudera.com>
2019-12-04 10:27:01 +0100

    CB-4720 Subnetids and cidr should not be enabled in the env request

--- environment-api/src/main/java/com/sequenceiq/environment/api/v1/environment/model/request/EnvironmentNetworkRequest.java
+++ environment-api/src/main/java/com/sequenceiq/environment/api/v1/environment/model/request/EnvironmentNetworkRequest.java
@@ -3,6 +3,7 @@ package com.sequenceiq.environment.api.v1.environment.model.request;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.sequenceiq.cloudbreak.validation.MutuallyExclusiveNotNull;
 import com.sequenceiq.environment.api.v1.environment.model.base.EnvironmentNetworkBase;
 
 import io.swagger.annotations.ApiModel;
@@ -10,5 +11,7 @@ import io.swagger.annotations.ApiModel;
 @ApiModel(value = "EnvironmentNetworkV1Request")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
+@MutuallyExclusiveNotNull(fieldGroups = {"networkCidr", "subnetIds"},
+        message = "The network CIDR and the subnet Ids should not be defined in the same request.")
 public class EnvironmentNetworkRequest extends EnvironmentNetworkBase {
 }
