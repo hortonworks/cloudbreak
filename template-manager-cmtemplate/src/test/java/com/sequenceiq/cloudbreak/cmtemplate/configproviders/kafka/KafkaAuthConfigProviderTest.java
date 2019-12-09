b3e7178c7c8961be81ae27b7b16523f2aa26eae3
Richard Doktorics <rdoktorics@cloudera.com>
2019-12-09 08:48:35 +0100

    Revert "CB-4653, update streaming to CDH-7.2.0.2"
    This reverts commit c29c0a09dcb02bc7daa12df9a42ec4ddd4d98b67.

--- template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kafka/KafkaAuthConfigProviderTest.java
+++ template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kafka/KafkaAuthConfigProviderTest.java
@@ -1,28 +1,20 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.kafka;
 
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
-import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.kafka.KafkaConfigProviderUtilsTest.cdhParcelVersion;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
-import com.sequenceiq.cloudbreak.cloud.model.ClouderaManagerProduct;
-import com.sequenceiq.cloudbreak.cloud.model.ClouderaManagerRepo;
-import com.sequenceiq.cloudbreak.template.views.BlueprintView;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -35,11 +27,23 @@ import com.sequenceiq.cloudbreak.template.TemplatePreparationObject.Builder;
 @ExtendWith(MockitoExtension.class)
 class KafkaAuthConfigProviderTest {
 
-    private static final Set<ApiClusterTemplateConfig> NO_AUTH_EXPECTED_CONFIGS = Set.of();
+    private static final String V7_0_1 = "7.0.1";
 
-    private static final Set<ApiClusterTemplateConfig> GENERAL_AUTH_EXPECTED_CONFIGS = Set.of(
-            config("ldap.auth.url", "protocol://host:1234"),
-            config("ldap.auth.user.dn.template", "pattern"));
+    private static final String V7_0_2 = "7.0.2";
+
+    private static final String V7_0_99 = "7.0.99";
+
+    private static final String V7_1_0 = "7.1.0";
+
+    private static final String V7_X_0 = "7.x.0";
+
+    private static final Iterable<String> NO_AUTH_VERSIONS = Lists.newArrayList(null, V7_0_1);
+
+    private static final Iterable<String> LDAP_AUTH_VERSIONS = Set.of(V7_0_2, V7_0_99);
+
+    private static final Iterable<String> PAM_AUTH_VERSIONS = Set.of(V7_1_0, V7_X_0);
+
+    private static final Set<ApiClusterTemplateConfig> NO_AUTH_EXPECTED_CONFIGS = Set.of();
 
     private static final Set<ApiClusterTemplateConfig> LDAP_AUTH_EXPECTED_CONFIGS = Set.of(
             config("ldap.auth.url", "protocol://host:1234"),
@@ -56,49 +60,44 @@ class KafkaAuthConfigProviderTest {
     @Mock
     private CmTemplateProcessor cmTemplateProcessor;
 
-    @Mock
-    private BlueprintView blueprintView;
-
     @BeforeEach
     void setUp() {
         underTest = new KafkaAuthConfigProvider();
     }
 
-    private TemplatePreparationObject templatePreparationObject(String cdhVersion) {
-        Builder builder = Builder.builder()
+    private static TemplatePreparationObject createTemplatePreparationObject() {
+        return Builder.builder()
                 .withLdapConfig(LdapViewBuilder.aLdapView()
                         .withProtocol("protocol")
                         .withServerPort(1234)
                         .withServerHost("host")
                         .withUserDnPattern("pattern")
                         .build())
-                .withBlueprintView(blueprintView);
-        if (cdhVersion != null) {
-            List<ClouderaManagerProduct> products = KafkaConfigProviderUtilsTest.products("CDH=" + cdhVersion);
-            builder.withProductDetails(new ClouderaManagerRepo(), products);
+                .build();
+    }
+
+    @Test
+    void getServiceConfigsNoAuth() {
+        testGetServiceConfigs(NO_AUTH_VERSIONS, NO_AUTH_EXPECTED_CONFIGS);
+    }
+
+    @Test
+    void getServiceConfigsLdap() {
+        testGetServiceConfigs(LDAP_AUTH_VERSIONS, LDAP_AUTH_EXPECTED_CONFIGS);
+    }
+
+    @Test
+    void getServiceConfigsPam() {
+        testGetServiceConfigs(PAM_AUTH_VERSIONS, PAM_AUTH_EXPECTED_CONFIGS);
+    }
+
+    private void testGetServiceConfigs(Iterable<String> versions, Iterable<ApiClusterTemplateConfig> expectedConfigs) {
+        for (String version: versions) {
+            when(cmTemplateProcessor.getVersion()).thenReturn(Optional.ofNullable(version));
+            TemplatePreparationObject tpo = createTemplatePreparationObject();
+            List<ApiClusterTemplateConfig> serviceConfigs = underTest.getServiceConfigs(cmTemplateProcessor, tpo);
+            assertThat(serviceConfigs).as("Expected configs for cdh version: %s", version).hasSameElementsAs(expectedConfigs);
         }
-        return builder.build();
-    }
-
-    @ParameterizedTest
-    @MethodSource("testArgs")
-    void getServiceConfigs(String cdhMainVersion, String cdhParcelVersion, Collection<ApiClusterTemplateConfig> expectedConfigs) {
-        when(blueprintView.getProcessor()).thenReturn(cmTemplateProcessor);
-        when(cmTemplateProcessor.getVersion()).thenReturn(Optional.ofNullable(cdhMainVersion));
-
-        TemplatePreparationObject tpo = templatePreparationObject(cdhParcelVersion);
-        List<ApiClusterTemplateConfig> serviceConfigs = underTest.getServiceConfigs(cmTemplateProcessor, tpo);
-        assertThat(serviceConfigs).as("Expected configs for cdh version: %s / %s", cdhMainVersion, cdhParcelVersion).hasSameElementsAs(expectedConfigs);
-    }
-
-    static Stream<Arguments> testArgs() {
-        return Stream.of(
-                Arguments.of("7.0.1", cdhParcelVersion("7.0.1", 5), NO_AUTH_EXPECTED_CONFIGS),
-                Arguments.of("7.0.2", cdhParcelVersion("7.0.2", 0), LDAP_AUTH_EXPECTED_CONFIGS),
-                Arguments.of("7.0.2", cdhParcelVersion("7.0.2", 2), PAM_AUTH_EXPECTED_CONFIGS),
-                Arguments.of("7.0.2", cdhParcelVersion("7.0.2", 3), PAM_AUTH_EXPECTED_CONFIGS),
-                Arguments.of("7.0.2", "irregularCdhVersion-123", GENERAL_AUTH_EXPECTED_CONFIGS),
-                Arguments.of("7.1.0", cdhParcelVersion("7.1.0", 0), PAM_AUTH_EXPECTED_CONFIGS));
     }
 
     @Test
