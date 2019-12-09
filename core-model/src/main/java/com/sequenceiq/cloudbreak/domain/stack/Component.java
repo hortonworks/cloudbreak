0a4d883d46c33ba666a795b5fbfea7f3e458ce17
David Szabo <dszabo@cloudera.com>
2019-12-09 13:19:33 +0100

    CB-4299 Increased Component and ClusterComponent id sequence step size

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/Component.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/Component.java
@@ -11,17 +11,17 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
-import com.sequenceiq.cloudbreak.common.type.ComponentType;
-import com.sequenceiq.cloudbreak.domain.ProvisionEntity;
 import com.sequenceiq.cloudbreak.common.json.Json;
 import com.sequenceiq.cloudbreak.common.json.JsonToString;
+import com.sequenceiq.cloudbreak.common.type.ComponentType;
+import com.sequenceiq.cloudbreak.domain.ProvisionEntity;
 
 @Entity
 public class Component implements ProvisionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "component_generator")
-    @SequenceGenerator(name = "component_generator", sequenceName = "component_id_seq", allocationSize = 1)
+    @SequenceGenerator(name = "component_generator", sequenceName = "component_id_seq", allocationSize = 20)
     private Long id;
 
     @Enumerated(EnumType.STRING)
