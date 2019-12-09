9bdfb8c98540a6bfabaf4154c29f71e1474196fa
Richard Doktorics <rdoktorics@cloudera.com>
2019-12-09 17:13:38 +0100

    Revert "Revert "CB-4299 Optimized component saves, moved security config generation to flow step""
    This reverts commit 53ee129fb3cc4dde6cb25c8497677bd9d13a1440.

--- core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/Component.java
+++ core-model/src/main/java/com/sequenceiq/cloudbreak/domain/stack/Component.java
@@ -94,6 +94,7 @@ public class Component implements ProvisionEntity {
                 + "id=" + id
                 + ", componentType=" + componentType
                 + ", name='" + name + '\''
+                + ", stackId=" + stack.getId()
                 + '}';
     }
 }
