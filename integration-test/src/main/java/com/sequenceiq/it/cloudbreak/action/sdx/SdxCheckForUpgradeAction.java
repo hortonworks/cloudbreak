8cad878c4d6c378145a28cb55d7c306cf967960e
Annamaria Szegedi <aszegedi@hortonworks.com>
2019-12-12 12:24:13 +0100

    CB-3805 Create new AWS E2E tests for SDX Image Upgrade

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/sdx/SdxCheckForUpgradeAction.java
@@ -0,0 +1,30 @@
+package com.sequenceiq.it.cloudbreak.action.sdx;
+
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.UpgradeOptionV4Response;
+import com.sequenceiq.it.cloudbreak.SdxClient;
+import com.sequenceiq.it.cloudbreak.action.Action;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
+import com.sequenceiq.it.cloudbreak.log.Log;
+
+public class SdxCheckForUpgradeAction implements Action<SdxInternalTestDto, SdxClient> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SdxCheckForUpgradeAction.class);
+
+    @Override
+    public SdxInternalTestDto action(TestContext testContext, SdxInternalTestDto testDto, SdxClient client) throws Exception {
+        Log.log(LOGGER, format(" Environment: %s", testDto.getRequest().getEnvironment()));
+        Log.logJSON(LOGGER, " SDX check for upgrade request: ", testDto.getRequest());
+        UpgradeOptionV4Response upgradeResponse = client.getSdxClient()
+                .sdxEndpoint()
+                .checkForUpgradeByName(testDto.getName());
+        Log.logJSON(LOGGER, " SDX check for upgrade response: ", upgradeResponse);
+        Log.log(LOGGER, " SDX name: %s", client.getSdxClient().sdxEndpoint().get(testDto.getName()).getName());
+        return testDto;
+    }
+}
---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/sdx/SdxUpgradeAction.java
@@ -0,0 +1,28 @@
+package com.sequenceiq.it.cloudbreak.action.sdx;
+
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sequenceiq.it.cloudbreak.SdxClient;
+import com.sequenceiq.it.cloudbreak.action.Action;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
+import com.sequenceiq.it.cloudbreak.log.Log;
+
+public class SdxUpgradeAction implements Action<SdxInternalTestDto, SdxClient> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SdxUpgradeAction.class);
+
+    @Override
+    public SdxInternalTestDto action(TestContext testContext, SdxInternalTestDto testDto, SdxClient client) throws Exception {
+        Log.log(LOGGER, format(" Environment: %s", testDto.getRequest().getEnvironment()));
+        Log.logJSON(LOGGER, " SDX upgrade request: ", testDto.getRequest());
+        client.getSdxClient()
+                .sdxEndpoint()
+                .upgradeClusterByName(testDto.getName());
+        Log.log(LOGGER, " SDX name: %s", client.getSdxClient().sdxEndpoint().get(testDto.getName()).getName());
+        return testDto;
+    }
+}
