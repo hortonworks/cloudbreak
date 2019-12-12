8f6a47f0eb97fc600561b709f220d1ca778eb3ce
David Han <dhan@cloudera.com>
2019-12-12 00:20:08 -0600

    CDPCP-1212. Update usermanagement proto

---
+++ freeipa/src/test/java/com/sequenceiq/freeipa/client/FreeIpaClientTest.java
@@ -0,0 +1,8 @@
+package com.sequenceiq.freeipa.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+// (c) Copyright 2019 Cloudera, Inc. All rights reserved.
+
+class FreeIpaClientTest {
+
+}
