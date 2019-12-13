b4c4a61efc0426924b380f6c840320dd15b1ee26
Asif Arman <asif.arman@cloudera.com>
2019-12-13 11:48:17 -0800

    CDPCP-1212. Review feedback

--- freeipa/src/test/java/com/sequenceiq/freeipa/service/freeipa/user/PasswordServiceTest.java
+++ freeipa/src/test/java/com/sequenceiq/freeipa/service/freeipa/user/PasswordServiceTest.java
@@ -8,9 +8,8 @@ import static org.mockito.Mockito.when;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Supplier;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.sequenceiq.cloudbreak.common.service.Clock;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,27 +26,21 @@ class PasswordServiceTest {
 
     private static final String ACCOUNT_ID = UUID.randomUUID().toString();
 
-    private static final String ACTOR_CRN = "crn:iam:us-west-2:" + ACCOUNT_ID + ":user:" + UUID.randomUUID().toString();
-
-    private Supplier<Long> mockTimeSupplier = () -> MOCK_TIME;
-
     @Mock
     private GrpcUmsClient grpcUmsClient;
+
+    @Mock
+    private Clock clock;
 
     @InjectMocks
     private PasswordService underTest;
 
-    @BeforeEach
-    void setUp() {
-        underTest.currentTimeSupplier = mockTimeSupplier;
-    }
-
     @Test
     void testCalculateExpirationTimeNoPasswordPolicy() {
         UserManagementProto.Account account = UserManagementProto.Account.newBuilder().build();
-        when(grpcUmsClient.getAccountDetails(eq(ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
+        when(grpcUmsClient.getAccountDetails(eq(PasswordService.INTERNAL_ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
 
-        assertEquals(Optional.empty(), underTest.calculateExpirationTime(ACTOR_CRN, ACCOUNT_ID));
+        assertEquals(Optional.empty(), underTest.calculateExpirationTime(PasswordService.INTERNAL_ACTOR_CRN, ACCOUNT_ID));
     }
 
     @Test
@@ -57,9 +50,9 @@ class PasswordServiceTest {
                         .setWorkloadPasswordMaxLifetime(0)
                         .build())
                 .build();
-        when(grpcUmsClient.getAccountDetails(eq(ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
+        when(grpcUmsClient.getAccountDetails(eq(PasswordService.INTERNAL_ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
 
-        assertEquals(Optional.empty(), underTest.calculateExpirationTime(ACTOR_CRN, ACCOUNT_ID));
+        assertEquals(Optional.empty(), underTest.calculateExpirationTime(PasswordService.INTERNAL_ACTOR_CRN, ACCOUNT_ID));
     }
 
     @Test
@@ -71,8 +64,9 @@ class PasswordServiceTest {
                         .setWorkloadPasswordMaxLifetime(lifetime)
                         .build())
                 .build();
-        when(grpcUmsClient.getAccountDetails(eq(ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
+        when(clock.getCurrentInstant()).thenReturn(Instant.ofEpochMilli(MOCK_TIME));
+        when(grpcUmsClient.getAccountDetails(eq(PasswordService.INTERNAL_ACTOR_CRN), eq(ACCOUNT_ID), any())).thenReturn(account);
 
-        assertEquals(Optional.of(Instant.ofEpochMilli(MOCK_TIME + lifetime)), underTest.calculateExpirationTime(ACTOR_CRN, ACCOUNT_ID));
+        assertEquals(Optional.of(Instant.ofEpochMilli(MOCK_TIME + lifetime)), underTest.calculateExpirationTime(PasswordService.INTERNAL_ACTOR_CRN, ACCOUNT_ID));
     }
 }
