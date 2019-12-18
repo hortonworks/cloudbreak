f4c27fcb433bb73e227366a097882aefca801a51
Attila Kanto <attila.kanto@gmail.com>
2019-12-18 09:23:13 +0100

    CB-4913 Cloudbreak pods are crashing during startup. We shall not do expensive task in onApplicationStart, furthermore the removed code is not relevant anymore since we shall not do any special flow handling in onApplictionEvent (basically during start)

--- core/src/main/java/com/sequenceiq/cloudbreak/init/CloudbreakCleanupService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/init/CloudbreakCleanupService.java
@@ -1,17 +1,5 @@
 package com.sequenceiq.cloudbreak.init;
 
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.START_IN_PROGRESS;
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.STOP_IN_PROGRESS;
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_IN_PROGRESS;
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_REQUESTED;
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.WAIT_FOR_SYNC;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -19,52 +7,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus;
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
-import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.InstanceStatus;
-import com.sequenceiq.cloudbreak.core.flow2.service.ReactorFlowManager;
-import com.sequenceiq.cloudbreak.domain.projection.StackStatusView;
-import com.sequenceiq.cloudbreak.domain.stack.Stack;
-import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
-import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
-import com.sequenceiq.cloudbreak.exception.FlowNotAcceptedException;
-import com.sequenceiq.cloudbreak.exception.FlowsAlreadyRunningException;
-import com.sequenceiq.cloudbreak.service.StackUpdater;
-import com.sequenceiq.cloudbreak.service.cluster.ClusterService;
 import com.sequenceiq.cloudbreak.service.flowlog.RestartFlowService;
 import com.sequenceiq.cloudbreak.service.ha.HeartbeatService;
-import com.sequenceiq.cloudbreak.service.stack.InstanceMetaDataService;
-import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.cloudbreak.startup.MissingVolumeTemplatesMigrator;
-import com.sequenceiq.cloudbreak.structuredevent.event.CloudbreakEventService;
 
 @Component
 public class CloudbreakCleanupService implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger LOGGER = LoggerFactory.getLogger(CloudbreakCleanupService.class);
 
     @Inject
-    private StackService stackService;
-
-    @Inject
-    private StackUpdater stackUpdater;
-
-    @Inject
-    private ClusterService clusterService;
-
-    @Inject
-    private InstanceMetaDataService instanceMetaDataService;
-
-    @Inject
-    private CloudbreakEventService eventService;
-
-    @Inject
     private RestartFlowService restartFlowService;
-
-    @Inject
-    private ReactorFlowManager flowManager;
 
     @Inject
     private HeartbeatService heartbeatService;
@@ -72,16 +26,10 @@ public class CloudbreakCleanupService implements ApplicationListener<ContextRefr
     @Inject
     private MissingVolumeTemplatesMigrator missingVolumeTemplatesMigrator;
 
-    private final List<Status> syncRequiredStates = Arrays.asList(UPDATE_REQUESTED, UPDATE_IN_PROGRESS, WAIT_FOR_SYNC, START_IN_PROGRESS, STOP_IN_PROGRESS);
-
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         heartbeatService.heartbeat();
         try {
-            List<Long> stackIdsUnderOperation = restartFlowService.collectUnderOperationResources();
-            List<StackStatusView> stacksToSync = resetStackStatus(stackIdsUnderOperation);
-            List<Cluster> clustersToSync = resetClusterStatus(stacksToSync, stackIdsUnderOperation);
-            triggerSyncs(stacksToSync, clustersToSync);
             restartFlowService.purgeTerminatedResourceFlowLogs();
             missingVolumeTemplatesMigrator.run();
         } catch (Exception e) {
@@ -89,71 +37,5 @@ public class CloudbreakCleanupService implements ApplicationListener<ContextRefr
             ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) event.getApplicationContext();
             applicationContext.close();
         }
-    }
-
-    private List<StackStatusView> resetStackStatus(Collection<Long> excludeStackIds) {
-        return stackService.getByStatuses(syncRequiredStates).stream()
-                .filter(s -> !excludeStackIds.contains(s.getId()) || Status.WAIT_FOR_SYNC.equals(s.getStatus().getStatus()))
-                .peek(s -> {
-                    if (!Status.WAIT_FOR_SYNC.equals(s.getStatus().getStatus())) {
-                        loggingStatusChange("Stack", s.getId(), s.getStatus().getStatus(), Status.WAIT_FOR_SYNC);
-                        stackUpdater.updateStackStatus(s.getId(), DetailedStackStatus.WAIT_FOR_SYNC, s.getStatus().getStatusReason());
-                    }
-                    cleanInstanceMetaData(instanceMetaDataService.findAllRequestedInStack(s.getId()));
-                }).collect(Collectors.toList());
-    }
-
-    private void cleanInstanceMetaData(Iterable<InstanceMetaData> metadataSet) {
-        for (InstanceMetaData metadata : metadataSet) {
-            if (InstanceStatus.REQUESTED.equals(metadata.getInstanceStatus()) && metadata.getInstanceId() == null) {
-                LOGGER.debug("InstanceMetaData [privateId: '{}'] is deleted at CB start.", metadata.getPrivateId());
-                instanceMetaDataService.delete(metadata);
-            }
-        }
-    }
-
-    private List<Cluster> resetClusterStatus(Collection<StackStatusView> stacksToSync, Collection<Long> excludeStackIds) {
-        return clusterService.findByStatuses(syncRequiredStates).stream()
-                .filter(c -> !excludeStackIds.contains(c.getStack().getId()))
-                .peek(c -> {
-                    if (!Status.WAIT_FOR_SYNC.equals(c.getStatus())) {
-                        loggingStatusChange("Cluster", c.getId(), c.getStatus(), Status.WAIT_FOR_SYNC);
-                        c.setStatus(Status.WAIT_FOR_SYNC);
-                        clusterService.save(c);
-                    }
-                }).filter(c -> !isStackToSyncContainsCluster(stacksToSync, c)).collect(Collectors.toList());
-    }
-
-    private boolean isStackToSyncContainsCluster(Collection<StackStatusView> stacksToSync, Cluster cluster) {
-        Set<Long> stackIds = stacksToSync.stream().map(StackStatusView::getId).collect(Collectors.toSet());
-        return stackIds.contains(cluster.getStack().getId());
-    }
-
-    private void loggingStatusChange(String type, Long id, Status status, Status deleteFailed) {
-        LOGGER.debug("{} {} status is updated from {} to {} at CB start.", type, id, status, deleteFailed);
-    }
-
-    private void triggerSyncs(Iterable<StackStatusView> stacksToSync, Iterable<Cluster> clustersToSync) {
-        try {
-            for (StackStatusView stack : stacksToSync) {
-                LOGGER.debug("Triggering full sync on stack [name: {}, id: {}].", stack.getName(), stack.getId());
-                fireEvent(stack.getId());
-                flowManager.triggerFullSyncWithoutCheck(stack.getId());
-            }
-
-            for (Cluster cluster : clustersToSync) {
-                Stack stack = cluster.getStack();
-                LOGGER.debug("Triggering sync on cluster [name: {}, id: {}].", cluster.getName(), cluster.getId());
-                fireEvent(stack.getId());
-                flowManager.triggerClusterSyncWithoutCheck(stack.getId());
-            }
-        } catch (OptimisticLockingFailureException | FlowsAlreadyRunningException | FlowNotAcceptedException e) {
-            LOGGER.error("Cannot trigger sync on stacks. Maybe another node is already syncing them?", e);
-        }
-    }
-
-    private void fireEvent(Long stackId) {
-        eventService.fireCloudbreakEvent(stackId, UPDATE_IN_PROGRESS.name(),
-                "Couldn't retrieve the cluster's status, starting to sync.");
     }
 }
