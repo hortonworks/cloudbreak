b2c52b4f3ecf923450c5db4a186c309514287468
Arpad Farsang <afarsang@HW12482.local>
2020-01-06 16:17:17 +0100

    CB-4979 test to check hibernate queries how many sql statement generates

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/SqlStatementInspector.java
@@ -0,0 +1,28 @@
+package com.sequenceiq.cloudbreak;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SqlStatementInspector implements StatementInspector {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SqlStatementInspector.class);
+
+    private static AtomicInteger selectStatements = new AtomicInteger(0);
+
+    public static int getSelectCountNumberAndReset() {
+        return selectStatements.getAndSet(0);
+    }
+
+    @Override
+    public String inspect(String sql) {
+        LOGGER.info(" Inspect the statement: " + sql);
+        if (sql.startsWith("select")) {
+            selectStatements.incrementAndGet();
+        }
+
+        return null;
+    }
+}
