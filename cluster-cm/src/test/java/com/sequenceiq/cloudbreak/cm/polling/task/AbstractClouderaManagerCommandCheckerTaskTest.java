20b082f00365e6a073ff7537f82cabe40173fd51
Attila Palfi <apalfi@hortonworks.com>
2020-01-08 12:45:46 +0100

    CB-4893 - tolerating SocketExceptions in CM status checker tasks

--- cluster-cm/src/test/java/com/sequenceiq/cloudbreak/cm/polling/task/AbstractClouderaManagerCommandCheckerTaskTest.java
+++ cluster-cm/src/test/java/com/sequenceiq/cloudbreak/cm/polling/task/AbstractClouderaManagerCommandCheckerTaskTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.net.SocketException;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -13,6 +14,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 
 import com.cloudera.api.swagger.CommandsResourceApi;
 import com.cloudera.api.swagger.client.ApiClient;
@@ -71,7 +73,47 @@ public class AbstractClouderaManagerCommandCheckerTaskTest {
         when(commandsResourceApi.readCommand(id)).thenAnswer(new Http500Answer(SIX));
 
         expectedEx.expect(ClouderaManagerOperationFailedException.class);
-        expectedEx.expectMessage("internal server error");
+        expectedEx.expectMessage("Operation is considered failed.");
+
+        for (int i = 0; i < SIX; i++) {
+            boolean inProgress = underTest.checkStatus(pollerObject);
+            assertFalse(inProgress);
+        }
+        underTest.checkStatus(pollerObject);
+    }
+
+    @Test
+    public void testPollingWithFiveSocketExceptions() throws ApiException {
+        Stack stack = new Stack();
+        BigDecimal id = new BigDecimal(ID);
+        ClouderaManagerPollerObject pollerObject = new ClouderaManagerPollerObject(stack, apiClient, id);
+        SocketException socketException = new SocketException("Network is unreachable (connect failed)");
+        ApiException apiException = new ApiException(socketException);
+        when(commandsResourceApi.readCommand(id))
+                .thenAnswer(new ExceptionThrowingApiCommandAnswer(apiException, apiException, apiException, apiException, apiException));
+
+        for (int i = 0; i < FIVE; i++) {
+            boolean inProgress = underTest.checkStatus(pollerObject);
+            assertFalse(inProgress);
+        }
+        boolean result = underTest.checkStatus(pollerObject);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testPollingWithThreeInternalServerErrorAndThreeSocketExceptions() throws ApiException {
+        Stack stack = new Stack();
+        BigDecimal id = new BigDecimal(1);
+        ClouderaManagerPollerObject pollerObject = new ClouderaManagerPollerObject(stack, apiClient, id);
+        SocketException socketException = new SocketException("Network is unreachable (connect failed)");
+        ApiException socketApiException = new ApiException(socketException);
+        ApiException internalServerError = new ApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "error");
+        when(commandsResourceApi.readCommand(id)).thenAnswer(new ExceptionThrowingApiCommandAnswer(internalServerError, internalServerError,
+                internalServerError, socketApiException, socketApiException, socketApiException));
+
+        expectedEx.expect(ClouderaManagerOperationFailedException.class);
+        expectedEx.expectMessage("Operation is considered failed.");
 
         for (int i = 0; i < SIX; i++) {
             boolean inProgress = underTest.checkStatus(pollerObject);
