845f82fdc800cb461b0ad6822f272d87c5fc625a
Krisztian Horvath <keyki.kk@gmail.com>
2020-01-17 10:14:57 +0100

    OPSAPS-54280 configure missed heartbeat interval for cm, default is 30

--- core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/ClusterHostServiceRunner.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/ClusterHostServiceRunner.java
@@ -108,6 +108,9 @@ public class ClusterHostServiceRunner {
     @Value("${cb.cm.heartbeat.interval}")
     private String cmHeartbeatInterval;
 
+    @Value("${cb.cm.missed.heartbeat.interval}")
+    private String cmMissedHeartbeatInterval;
+
     @Inject
     private StackService stackService;
 
@@ -459,7 +462,9 @@ public class ClusterHostServiceRunner {
 
     private void decoratePillarWithClouderaManagerSettings(Map<String, SaltPillarProperties> servicePillar) {
         servicePillar.put("cloudera-manager-settings", new SaltPillarProperties("/cloudera-manager/settings.sls",
-                singletonMap("cloudera-manager", singletonMap("settings", singletonMap("heartbeat_interval", cmHeartbeatInterval)))));
+                singletonMap("cloudera-manager", singletonMap("settings", Map.of(
+                        "heartbeat_interval", cmHeartbeatInterval,
+                        "missed_heartbeat_interval", cmMissedHeartbeatInterval)))));
     }
 
     private void saveCustomNameservers(Stack stack, KerberosConfig kerberosConfig, Map<String, SaltPillarProperties> servicePillar) {
