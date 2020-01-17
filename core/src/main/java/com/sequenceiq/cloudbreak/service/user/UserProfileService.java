b61d52f44a8ba33247e3f23a2e8095669f64f55b
Dénes Berger <dberger@cloudbreak.com>
2020-01-17 16:37:08 +0100

    CB-5004 Fix race condition issue on UserProfile save

--- core/src/main/java/com/sequenceiq/cloudbreak/service/user/UserProfileService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/user/UserProfileService.java
@@ -16,9 +16,6 @@ import com.sequenceiq.cloudbreak.common.user.CloudbreakUser;
 import com.sequenceiq.cloudbreak.domain.UserProfile;
 import com.sequenceiq.cloudbreak.repository.UserProfileRepository;
 import com.sequenceiq.cloudbreak.service.CloudbreakRestRequestThreadLocalService;
-import com.sequenceiq.cloudbreak.service.image.ImageCatalogService;
-import com.sequenceiq.cloudbreak.service.secret.service.SecretService;
-import com.sequenceiq.cloudbreak.service.workspace.WorkspaceService;
 import com.sequenceiq.cloudbreak.workspace.model.User;
 
 @Service
@@ -26,23 +23,16 @@ public class UserProfileService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserProfileService.class);
 
+    private final Object lockObject = new Object();
+
     @Inject
     private UserProfileRepository userProfileRepository;
-
-    @Inject
-    private ImageCatalogService imageCatalogService;
-
-    @Inject
-    private SecretService secretService;
 
     @Inject
     private CloudbreakRestRequestThreadLocalService restRequestThreadLocalService;
 
     @Inject
     private UserService userService;
-
-    @Inject
-    private WorkspaceService workspaceService;
 
     public UserProfile getOrCreateForLoggedInUser() {
         CloudbreakUser cloudbreakUser = restRequestThreadLocalService.getCloudbreakUser();
@@ -53,24 +43,19 @@ public class UserProfileService {
     public UserProfile getOrCreate(User user) {
         UserProfile userProfile = getSilently(user).orElse(null);
         if (userProfile == null) {
-            synchronized (UserProfileService.class) {
-                userProfile = getSilently(user).orElse(null);
-                if (userProfile == null) {
-                    userProfile = new UserProfile();
-                    userProfile.setUserName(user.getUserName());
-                    addUiProperties(userProfile);
-                    userProfile.setUser(user);
-                    userProfile = userProfileRepository.save(userProfile);
-                }
-            }
+            userProfile = creteUserProfile(user);
         } else if (userProfile.getUserName() == null && user.getUserName() != null) {
-            userProfile.setUserName(user.getUserName());
-            userProfile = userProfileRepository.save(userProfile);
+            userProfile = updateUserProfileUserName(user);
         } else if (userProfile.getUser() == null) {
-            userProfile.setUser(user);
-            userProfile = userProfileRepository.save(userProfile);
+            userProfile = updateUserProfileUser(user);
         }
         return userProfile;
+    }
+
+    public UserProfile save(UserProfile userProfile) {
+        synchronized (lockObject) {
+            return userProfileRepository.save(userProfile);
+        }
     }
 
     private Optional<UserProfile> getSilently(User user) {
@@ -79,10 +64,6 @@ public class UserProfileService {
         } catch (AccessDeniedException ignore) {
             return Optional.empty();
         }
-    }
-
-    public UserProfile save(UserProfile userProfile) {
-        return userProfileRepository.save(userProfile);
     }
 
     public Set<UserProfile> findByImageCatalogId(Long catalogId) {
@@ -95,5 +76,55 @@ public class UserProfileService {
         } catch (IllegalArgumentException ignored) {
             userProfile.setUiProperties(null);
         }
+    }
+
+    private UserProfile creteUserProfile(User user) {
+        UserProfile userProfile;
+        LOGGER.debug("UserProfile is null for User {} ({})", user.getUserName(), user.getId());
+        synchronized (lockObject) {
+            userProfile = getSilently(user).orElse(null);
+            if (userProfile == null) {
+                LOGGER.debug("UserProfile is still null for User {} ({}). Creating new UserProfile record.", user.getUserName(), user.getId());
+                userProfile = new UserProfile();
+                userProfile.setUserName(user.getUserName());
+                addUiProperties(userProfile);
+                userProfile.setUser(user);
+                userProfile = userProfileRepository.save(userProfile);
+                LOGGER.debug("UserProfile record created. {} ({})", userProfile.getUserName(), userProfile.getId());
+            }
+        }
+        return userProfile;
+    }
+
+    private UserProfile updateUserProfileUserName(User user) {
+        UserProfile userProfile;
+        LOGGER.debug("UserProfile had empty UserName for User {} ({})", user.getUserName(), user.getId());
+        synchronized (lockObject) {
+            userProfile = getSilently(user).orElseThrow(() ->
+                    new IllegalStateException(String.format("UserProfile became NULL for user %s (%d)", user.getUserName(), user.getId())));
+            if (userProfile.getUserName() == null && user.getUserName() != null) {
+                LOGGER.debug("UserProfile still had empty UserName for User {} ({}). Setting.", user.getUserName(), user.getId());
+                userProfile.setUserName(user.getUserName());
+                userProfile = userProfileRepository.save(userProfile);
+                LOGGER.debug("UserProfile record saved. {} ({})", userProfile.getUserName(), userProfile.getId());
+            }
+        }
+        return userProfile;
+    }
+
+    private UserProfile updateUserProfileUser(User user) {
+        UserProfile userProfile;
+        LOGGER.debug("UserProfile had no User Id set for User {} ({})", user.getUserName(), user.getId());
+        synchronized (lockObject) {
+            userProfile = getSilently(user).orElseThrow(() ->
+                    new IllegalStateException(String.format("UserProfile became NULL for user %s (%d)", user.getUserName(), user.getId())));
+            if (userProfile.getUser() == null) {
+                LOGGER.debug("UserProfile still had no User Id set for User {} ({}). Setting.", user.getUserName(), user.getId());
+                userProfile.setUser(user);
+                userProfile = userProfileRepository.save(userProfile);
+                LOGGER.debug("UserProfile record saved. {} ({})", userProfile.getUserName(), userProfile.getId());
+            }
+        }
+        return userProfile;
     }
 }
