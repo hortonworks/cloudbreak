10f072e4a4fb981ed83ffa6443137e639f10459c
Attila Kristof Foldi <foldik@cloudera.com>
2020-01-22 16:19:57 +0100

    CB-5215 Add postgres traffic check to integration test

--- integration-test/scripts/check-results.sh
+++ integration-test/scripts/check-results.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+: ${INTEGRATIONTEST_MAX_PG_NETWORK_OUTPUT:="16GB"}
+
 status_code=0
 
 if [[ "$CIRCLECI" ]]; then
@@ -22,6 +24,22 @@ if [[ "$CIRCLECI" ]]; then
     # Check swagger validation results
     if ! grep -q "is valid against swagger specification 2.0" swagger-validation-result.out; then
         echo -e "\033[0;91m--- !!! THE SWAGGER SPECIFICATION IS INVALID, CHECK \033[1;93mswagger-validation-result.out\033[0;91m FOR RESULTS !!! ---\n"; status_code=1;
+    fi
+
+    if [[ -z "${INTEGRATIONTEST_YARN_QUEUE}" ]] && [[ "$AWS" != true ]]; then
+      docker run --rm \
+       -v "$(pwd)"/scripts/analyse_postgres_stat.jsh:/opt/analyse_postgres_stat.jsh \
+       -v "$(pwd)"/test-output/docker_stats/pg_stat_network_io.result:/tmp/pg_stat_network_io.result \
+       --env INTEGRATION_TEST_MAX_POSTGRES_OUTPUT=${INTEGRATIONTEST_MAX_PG_NETWORK_OUTPUT} \
+       openjdk:11-jdk \
+       jshell /opt/analyse_postgres_stat.jsh > ./test-output/docker_stats/pg_stat_network_io_analysed.result;
+
+      pg_res=$(cat ./test-output/docker_stats/pg_stat_network_io_analysed.result);
+      if [[ "$pg_res" == "POSTGRES>> OK" ]]; then
+        echo -e "\n\033[0;92m+++ Postgres traffic is below ${max_pg_network_output} limit.";
+      else
+        echo -e "\033[0;91m--- !!! Postgres trafic check failed: ${pg_res}"; status_code=1;
+      fi
     fi
 
     # Exit if there are failed tests
