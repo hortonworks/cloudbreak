14489c6671534e365a3eca545190fa497e6ff288
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-01-28 15:08:27 +0100

    CB-5302 Sometimes the external id could not be fetched by the user crn if the user does not exist

--- environment/src/main/java/com/sequenceiq/environment/user/UserPreferencesService.java
+++ environment/src/main/java/com/sequenceiq/environment/user/UserPreferencesService.java
@@ -6,9 +6,11 @@ import java.util.UUID;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
+import com.sequenceiq.cloudbreak.exception.NotFoundException;
 
 @Service
 public class UserPreferencesService {
@@ -34,12 +36,21 @@ public class UserPreferencesService {
             if (StringUtils.isEmpty(userPreferences.getExternalId())) {
                 LOGGER.debug("External id exist for current user with crn '{}'", userCrn);
                 userPreferences.setExternalId(generateExternalId());
+                userPreferences = userPreferencesRepository.save(userPreferences);
             }
         } else {
             LOGGER.debug("User preferences does not exist, creating it with crn '{}'", userCrn);
             userPreferences = new UserPreferences(generateExternalId(), userCrn);
+            try {
+                userPreferences = userPreferencesRepository.save(userPreferences);
+            } catch (AccessDeniedException e) {
+                LOGGER.debug("User exists with crn: ");
+                userPreferencesOptional = userPreferencesRepository.findByUserCrn(userCrn);
+                userPreferences = userPreferencesOptional.orElseThrow(() -> new NotFoundException("User does not exists with crn. If you see this error, " +
+                        "you've caught something big, because Duplicate exception occurred"));
+            }
         }
-        return userPreferencesRepository.save(userPreferences).getExternalId();
+        return userPreferences.getExternalId();
     }
 
     private String generateExternalId() {
