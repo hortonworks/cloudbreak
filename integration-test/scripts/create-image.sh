4dd69d7b0d225fc4e82a8b20f662a21fb845edea
Attila Kristof Foldi <foldik@cloudera.com>
2020-01-29 15:57:28 +0100

    CB-5327 Parallel docker image build during integration tests

--- integration-test/scripts/create-image.sh
+++ integration-test/scripts/create-image.sh
@@ -15,9 +15,6 @@ echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-cloudbreak:/integcb/docker-containers/docker-cloudbreak \
  busybox:1.31.1 /bin/sh -c "sed -i '/cloudbreak-\$VERSION/c\ADD cloudbreak.jar /' /integcb/docker-containers/docker-cloudbreak/Dockerfile"
 echo "StatusCode######## $?"
-date
-echo -e "\n\033[1;96m--- Build dev cloudbreak test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak:dev ./integcb/docker-containers/docker-cloudbreak
 
 date
 echo -e "\n\033[1;96m--- Copy ../autoscale/build/libs/periscope.jar to docker-autoscale directory\033[0m\n"
@@ -27,9 +24,6 @@ date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-autoscale:/integcb/docker-containers/docker-autoscale \
  busybox:1.31.1 /bin/sh -c "sed -i '/periscope-\$VERSION/c\ADD periscope.jar /' /integcb/docker-containers/docker-autoscale/Dockerfile"
-date
-echo -e "\n\033[1;96m--- Build dev autoscale test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-autoscale:dev ./integcb/docker-containers/docker-autoscale
 
 date
 echo -e "\n\033[1;96m--- Copy ../datalake/build/libs/datalake.jar to docker-datalake directory\033[0m\n"
@@ -39,9 +33,6 @@ date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-datalake:/integcb/docker-containers/docker-datalake \
  busybox:1.31.1 /bin/sh -c "sed -i '/datalake-\$VERSION/c\ADD datalake.jar /' /integcb/docker-containers/docker-datalake/Dockerfile"
-date
-echo -e "\n\033[1;96m--- Build dev datalake test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-datalake:dev ./integcb/docker-containers/docker-datalake
 
 date
 echo -e "\n\033[1;96m--- Copy ../freeipa/build/libs/freeipa.jar to docker-freeipa directory\033[0m\n"
@@ -51,9 +42,6 @@ date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-freeipa:/integcb/docker-containers/docker-freeipa \
  busybox:1.31.1 /bin/sh -c "sed -i '/freeipa-\$VERSION/c\ADD freeipa.jar /' /integcb/docker-containers/docker-freeipa/Dockerfile"
-date
-echo -e "\n\033[1;96m--- Build dev freeipa test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-freeipa:dev ./integcb/docker-containers/docker-freeipa
 
 date
 echo -e "\n\033[1;96m--- Copy ../environment/build/libs/environment.jar to docker-environment directory\033[0m\n"
@@ -63,9 +51,6 @@ date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-environment:/integcb/docker-containers/docker-environment \
  busybox:1.31.1 /bin/sh -c "sed -i '/environment-\$VERSION/c\ADD environment.jar /' /integcb/docker-containers/docker-environment/Dockerfile"
-date
-echo -e "\n\033[1;96m--- Build dev environment test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-environment:dev ./integcb/docker-containers/docker-environment
 
 date
 echo -e "\n\033[1;96m--- Copy ../redbeams/build/libs/redbeams.jar to docker-redbeams directory\033[0m\n"
@@ -75,6 +60,12 @@ date
 echo -e "\n\033[1;96m--- Change Dockerfile \033[0m\n"
 docker run -v "$(pwd)"/integcb/docker-containers/docker-redbeams:/integcb/docker-containers/docker-redbeams \
  busybox:1.31.1 /bin/sh -c "sed -i '/redbeams-\$VERSION/c\ADD redbeams.jar /' /integcb/docker-containers/docker-redbeams/Dockerfile"
-date
-echo -e "\n\033[1;96m--- Build dev redbeams test image\033[0m\n"
-docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-redbeams:dev ./integcb/docker-containers/docker-redbeams
+
+echo -e "\n\033[1;96m--- Build docker images\033[0m\n"
+docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak:dev ./integcb/docker-containers/docker-cloudbreak & \
+  docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-datalake:dev ./integcb/docker-containers/docker-datalake & \
+  docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-autoscale:dev ./integcb/docker-containers/docker-autoscale & \
+  docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-freeipa:dev ./integcb/docker-containers/docker-freeipa & \
+  docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-environment:dev ./integcb/docker-containers/docker-environment & \
+  docker build -t docker-private.infra.cloudera.com/cloudera/cloudbreak-redbeams:dev ./integcb/docker-containers/docker-redbeams & \
+  wait
