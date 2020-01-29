7a169cac37e3c5e2f31f026a0f1656f6bd52183a
doktoric <rdoktorics@cloudera.com>
2020-01-29 18:05:32 +0100

    CB-5330 Remove ambari username from yarn credential

--- environment/src/main/java/com/sequenceiq/environment/credential/attributes/yarn/YarnCredentialAttributes.java
+++ environment/src/main/java/com/sequenceiq/environment/credential/attributes/yarn/YarnCredentialAttributes.java
@@ -2,17 +2,7 @@ package com.sequenceiq.environment.credential.attributes.yarn;
 
 public class YarnCredentialAttributes {
 
-    private String ambariUser;
-
     private String endpoint;
-
-    public String getAmbariUser() {
-        return ambariUser;
-    }
-
-    public void setAmbariUser(String ambariUser) {
-        this.ambariUser = ambariUser;
-    }
 
     public String getEndpoint() {
         return endpoint;
