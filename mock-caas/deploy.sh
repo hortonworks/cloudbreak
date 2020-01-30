1d109b776156282c6d9a73a52308facffdac77d7
doktoric <rdoktorics@cloudera.com>
2020-01-30 10:44:58 +0100

    CB-5347 Disable public docker image publishing

--- mock-caas/deploy.sh
+++ mock-caas/deploy.sh
@@ -18,17 +18,13 @@ new_version() {
   debug "building docker image for version: $VERSION"
   INTERNAL_IMAGE_NAME=${INTERNAL_IMAGE_NAME:-${DOCKER_IMAGE}}
   # Build docker and push to hortonworks repo
-  docker build -t ${DOCKER_IMAGE}:${VERSION} --build-arg=REPO_URL=${NEXUS_URL} --build-arg=VERSION=${VERSION} .
-  docker push ${DOCKER_IMAGE}:${VERSION}
-  docker tag ${DOCKER_IMAGE}:${VERSION} ${INTERNAL_IMAGE_NAME}:${VERSION}
+  docker build -t ${INTERNAL_IMAGE_NAME}:${VERSION} --build-arg=REPO_URL=${NEXUS_URL} --build-arg=VERSION=${VERSION} .
   docker push ${INTERNAL_IMAGE_NAME}:${VERSION}
 }
 
 main() {
   : ${VERSION:?"required!"}
   : ${DOCKER_IMAGE:?"required!"}
-  : ${DOCKERHUB_USERNAME:?"required!"}
-  : ${DOCKERHUB_PASSWORD:?"required!"}
   : ${DEBUG:=1}
   : ${NEXUS_URL:?"required!"}
 
