c632b89b922d395f10ac6125d02f2de6046aab66
Adam Holley <git@holleyism.com>
2020-02-09 20:43:00 -0600

    CB-5486: Add instanceID to the response for the health check
    This patch adds the instanceID for the nodes to the reponse
    for the health check.

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/FreeIpaHealthDetailsService.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/FreeIpaHealthDetailsService.java
@@ -1,8 +1,10 @@
 package com.sequenceiq.freeipa.service.stack;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -21,6 +23,7 @@ import com.sequenceiq.freeipa.client.FreeIpaClientException;
 import com.sequenceiq.freeipa.client.model.RPCMessage;
 import com.sequenceiq.freeipa.client.model.RPCResponse;
 import com.sequenceiq.freeipa.entity.InstanceGroup;
+import com.sequenceiq.freeipa.entity.InstanceMetaData;
 import com.sequenceiq.freeipa.entity.Stack;
 import com.sequenceiq.freeipa.service.freeipa.FreeIpaClientFactory;
 
@@ -34,6 +37,8 @@ public class FreeIpaHealthDetailsService {
     private static final String STATUS_OK = "OK";
 
     private static final int STATUS_GROUP = 2;
+
+    private static final int NODE_GROUP = 1;
 
     private static final String MESSAGE_UNAVAILABLE = "Message Unavailable";
 
@@ -49,17 +54,17 @@ public class FreeIpaHealthDetailsService {
 
     public HealthDetailsFreeIpaResponse getHealthDetails(String environmentCrn, String accountId) {
         Stack stack = stackService.getByEnvironmentCrnAndAccountIdWithLists(environmentCrn, accountId);
-        String masterCN = findMasterCN(stack);
+        InstanceMetaData master = findMaster(stack);
         Optional<RPCResponse<Boolean>> rpcResponse = Optional.empty();
         try {
-            rpcResponse = Optional.ofNullable(checkFreeIpaHealth(stack, masterCN));
+            rpcResponse = Optional.ofNullable(checkFreeIpaHealth(stack, master));
         } catch (FreeIpaClientException e) {
             LOGGER.error("Unable to check the health of FreeIPA.", e);
         }
-        return createResponse(stack, rpcResponse);
+        return createResponse(stack, rpcResponse, master);
     }
 
-    private HealthDetailsFreeIpaResponse createResponse(Stack stack, Optional<RPCResponse<Boolean>> rpcResponse) {
+    private HealthDetailsFreeIpaResponse createResponse(Stack stack, Optional<RPCResponse<Boolean>> rpcResponse, InstanceMetaData master) {
         HealthDetailsFreeIpaResponse response = new HealthDetailsFreeIpaResponse();
         response.setEnvironmentCrn(stack.getEnvironmentCrn());
         response.setCrn(stack.getResourceCrn());
@@ -73,23 +78,36 @@ public class FreeIpaHealthDetailsService {
             }
         } else {
             response.setStatus(DetailedStackStatus.UNREACHABLE.getStatus());
-            NodeHealthDetails nodeResponse = null;
-            nodeResponse = new NodeHealthDetails();
+            NodeHealthDetails nodeResponse = new NodeHealthDetails();
             response.addNodeHealthDetailsFreeIpaResponses(nodeResponse);
+            nodeResponse.setName(master.getDiscoveryFQDN());
             nodeResponse.setStatus(Status.UNREACHABLE);
         }
+        updateResponseWithInstanceIds(response, stack);
         return response;
     }
 
-    private String findMasterCN(Stack stack) {
-        InstanceGroup masterGroup = stack.getInstanceGroups().stream()
-                .filter(instanceGroup -> InstanceGroupType.MASTER == instanceGroup.getInstanceGroupType()).findFirst().get();
-        return masterGroup.getNotDeletedInstanceMetaDataSet().stream().findFirst().get().getDiscoveryFQDN();
+    private void updateResponseWithInstanceIds(HealthDetailsFreeIpaResponse response, Stack stack) {
+        Map<String, String> nameIdMap = getNameIdMap(stack);
+        for (NodeHealthDetails node: response.getNodeHealthDetails()) {
+            node.setInstanceId(nameIdMap.get(node.getName()));
+        }
     }
 
-    private RPCResponse<Boolean> checkFreeIpaHealth(Stack stack, String masterCN) throws FreeIpaClientException {
+    private Map<String, String> getNameIdMap(Stack stack) {
+        return stack.getInstanceGroups().stream().flatMap(ig -> ig.getInstanceMetaData().stream())
+                .collect(Collectors.toMap(InstanceMetaData::getDiscoveryFQDN, InstanceMetaData::getInstanceId));
+    }
+
+    private InstanceMetaData findMaster(Stack stack) {
+        InstanceGroup masterGroup = stack.getInstanceGroups().stream()
+                .filter(instanceGroup -> InstanceGroupType.MASTER == instanceGroup.getInstanceGroupType()).findFirst().get();
+        return masterGroup.getNotDeletedInstanceMetaDataSet().stream().findFirst().get();
+    }
+
+    private RPCResponse<Boolean> checkFreeIpaHealth(Stack stack, InstanceMetaData master) throws FreeIpaClientException {
         FreeIpaClient freeIpaClient = freeIpaClientFactory.getFreeIpaClientForStack(stack);
-        return freeIpaClient.serverConnCheck(masterCN, masterCN);
+        return freeIpaClient.serverConnCheck(master.getDiscoveryFQDN(), master.getDiscoveryFQDN());
     }
 
     private boolean isOverallHealthy(HealthDetailsFreeIpaResponse response) {
@@ -110,6 +128,7 @@ public class FreeIpaHealthDetailsService {
                 nodeResponse = new NodeHealthDetails();
                 response.addNodeHealthDetailsFreeIpaResponses(nodeResponse);
                 nodeResponse.setStatus(Status.AVAILABLE);
+                nodeResponse.setName(nodeMatcher.group(NODE_GROUP));
             }
             if (nodeResponse == null) {
                 LOGGER.info("No node for message: {}" + message.getMessage());
