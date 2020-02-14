51f33ec17f2c8ef5f7941d63edb3dd4f97b855a7
David Bajzath <bajzathd@gmail.com>
2020-02-14 12:30:52 +0100

    CB-5122 Rename freeipa child environment management

--- freeipa/src/main/java/com/sequenceiq/freeipa/controller/FreeIpaV1Controller.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/controller/FreeIpaV1Controller.java
@@ -14,27 +14,27 @@ import com.sequenceiq.cloudbreak.validation.ValidationResult;
 import com.sequenceiq.cloudbreak.validation.ValidationResult.State;
 import com.sequenceiq.freeipa.api.v1.freeipa.cleanup.CleanupRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.FreeIpaV1Endpoint;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.attachchildenv.AttachChildEnvironmentRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.create.CreateFreeIpaRequest;
-import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.deregchildenv.DeregisterChildEnvironmentRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.describe.DescribeFreeIpaResponse;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.detachchildenv.DetachChildEnvironmentRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.health.HealthDetailsFreeIpaResponse;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.list.ListFreeIpaResponse;
-import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.regchildenv.RegisterChildEnvironmentRequest;
 import com.sequenceiq.freeipa.api.v1.operation.model.OperationStatus;
 import com.sequenceiq.freeipa.client.FreeIpaClientException;
 import com.sequenceiq.freeipa.controller.exception.BadRequestException;
+import com.sequenceiq.freeipa.controller.validation.AttachChildEnvironmentRequestValidator;
 import com.sequenceiq.freeipa.controller.validation.CreateFreeIpaRequestValidator;
-import com.sequenceiq.freeipa.controller.validation.RegisterChildEnvironmentRequestValidator;
 import com.sequenceiq.freeipa.service.freeipa.cleanup.CleanupService;
 import com.sequenceiq.freeipa.service.stack.ChildEnvironmentService;
 import com.sequenceiq.freeipa.service.stack.ClusterProxyService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaCreationService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaDeletionService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaDescribeService;
+import com.sequenceiq.freeipa.service.stack.FreeIpaHealthDetailsService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaListService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaRootCertificateService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaStartService;
-import com.sequenceiq.freeipa.service.stack.FreeIpaHealthDetailsService;
 import com.sequenceiq.freeipa.service.stack.FreeIpaStopService;
 import com.sequenceiq.freeipa.util.CrnService;
 
@@ -74,7 +74,7 @@ public class FreeIpaV1Controller implements FreeIpaV1Endpoint {
     private CreateFreeIpaRequestValidator createFreeIpaRequestValidator;
 
     @Inject
-    private RegisterChildEnvironmentRequestValidator registerChildEnvironmentRequestValidator;
+    private AttachChildEnvironmentRequestValidator attachChildEnvironmentRequestValidator;
 
     @Inject
     private FreeIpaStartService freeIpaStartService;
@@ -97,20 +97,20 @@ public class FreeIpaV1Controller implements FreeIpaV1Endpoint {
     }
 
     @Override
-    public void registerChildEnvironment(@Valid RegisterChildEnvironmentRequest request) {
-        ValidationResult validationResult = registerChildEnvironmentRequestValidator.validate(request);
+    public void attachChildEnvironment(@Valid AttachChildEnvironmentRequest request) {
+        ValidationResult validationResult = attachChildEnvironmentRequestValidator.validate(request);
         if (validationResult.hasError()) {
-            LOGGER.debug("FreeIPA request has validation error(s): {}.", validationResult.getFormattedErrors());
+            LOGGER.debug("AttachChildEnvironmentRequest has validation error(s): {}.", validationResult.getFormattedErrors());
             throw new BadRequestException(validationResult.getFormattedErrors());
         }
         String accountId = crnService.getCurrentAccountId();
-        childEnvironmentService.registerChildEnvironment(request, accountId);
+        childEnvironmentService.attachChildEnvironment(request, accountId);
     }
 
     @Override
-    public void deregisterChildEnvironment(@Valid DeregisterChildEnvironmentRequest request) {
+    public void detachChildEnvironment(@Valid DetachChildEnvironmentRequest request) {
         String accountId = crnService.getCurrentAccountId();
-        childEnvironmentService.deregisterChildEnvironment(request, accountId);
+        childEnvironmentService.detachChildEnvironment(request, accountId);
     }
 
     @Override
