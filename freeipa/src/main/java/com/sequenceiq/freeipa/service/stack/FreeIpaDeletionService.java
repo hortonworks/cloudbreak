90143e5ca388d3889d1dc91b44cb508afc5dec97
David Bajzath <bajzathd@gmail.com>
2020-02-17 14:28:48 +0100

    CB-5122 Validate delete freeipa request

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/FreeIpaDeletionService.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/FreeIpaDeletionService.java
@@ -3,12 +3,15 @@ package com.sequenceiq.freeipa.service.stack;
 import static com.sequenceiq.freeipa.flow.stack.termination.StackTerminationEvent.TERMINATION_EVENT;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.freeipa.controller.exception.NotFoundException;
+import com.sequenceiq.freeipa.entity.ChildEnvironment;
 import com.sequenceiq.freeipa.entity.Stack;
 import com.sequenceiq.freeipa.flow.stack.termination.event.TerminationEvent;
 import com.sequenceiq.freeipa.service.freeipa.flow.FreeIpaFlowManager;
@@ -21,6 +24,9 @@ public class FreeIpaDeletionService {
     private StackService stackService;
 
     @Inject
+    private ChildEnvironmentService childEnvironmentService;
+
+    @Inject
     private FreeIpaFlowManager flowManager;
 
     @Inject
@@ -31,6 +37,7 @@ public class FreeIpaDeletionService {
         if (stacks.isEmpty()) {
             throw new NotFoundException("No FreeIpa found in environment");
         }
+        stacks.forEach(stack -> validateDeletion(stack, accountId));
         stacks.forEach(this::unscheduleAndTriggerTerminate);
     }
 
@@ -38,5 +45,16 @@ public class FreeIpaDeletionService {
         freeipaJobService.unschedule(stack);
         flowManager.notify(TERMINATION_EVENT.event(), new TerminationEvent(TERMINATION_EVENT.event(), stack.getId(), false));
         flowManager.cancelRunningFlows(stack.getId());
+    }
+
+    private void validateDeletion(Stack stack, String accountId) {
+        List<ChildEnvironment> childEnvironments = childEnvironmentService.findChildEnvironments(stack, accountId);
+        if (!childEnvironments.isEmpty()) {
+            String childEnvironmentCrns = childEnvironments.stream()
+                    .map(ChildEnvironment::getEnvironmentCrn)
+                    .collect(Collectors.joining(", "));
+            throw new BadRequestException(String.format("FreeIpa can not be deleted while it has the following child environment(s) attached [%s]",
+                    childEnvironmentCrns));
+        }
     }
 }
