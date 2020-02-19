50788a13dee169dbb6564f1c9508ab2231c92405
Andor Molnar <andor@cloudera.com>
2020-02-19 15:31:25 +0100

    CB-5643. Enable access for internal Crns in Kerberos and LDAP endpoints

--- freeipa/src/main/java/com/sequenceiq/freeipa/kerberos/v1/KerberosConfigV1Controller.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/kerberos/v1/KerberosConfigV1Controller.java
@@ -1,21 +1,23 @@
 package com.sequenceiq.freeipa.kerberos.v1;
 
+import com.sequenceiq.cloudbreak.auth.security.internal.InternalReady;
+import com.sequenceiq.cloudbreak.auth.security.internal.ResourceCrn;
+import com.sequenceiq.freeipa.api.v1.kerberos.KerberosConfigV1Endpoint;
+import com.sequenceiq.freeipa.api.v1.kerberos.model.create.CreateKerberosConfigRequest;
+import com.sequenceiq.freeipa.api.v1.kerberos.model.describe.DescribeKerberosConfigResponse;
+import com.sequenceiq.freeipa.util.CrnService;
+import com.sequenceiq.notification.NotificationController;
+import org.springframework.stereotype.Controller;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
-import org.springframework.stereotype.Controller;
-
-import com.sequenceiq.freeipa.api.v1.kerberos.KerberosConfigV1Endpoint;
-import com.sequenceiq.freeipa.api.v1.kerberos.model.create.CreateKerberosConfigRequest;
-import com.sequenceiq.freeipa.api.v1.kerberos.model.describe.DescribeKerberosConfigResponse;
-import com.sequenceiq.freeipa.util.CrnService;
-import com.sequenceiq.notification.NotificationController;
-
 @Controller
 @Transactional(TxType.NEVER)
+@InternalReady
 public class KerberosConfigV1Controller extends NotificationController implements KerberosConfigV1Endpoint {
     @Inject
     private KerberosConfigV1Service kerberosConfigV1Service;
@@ -29,7 +31,7 @@ public class KerberosConfigV1Controller extends NotificationController implement
     }
 
     @Override
-    public DescribeKerberosConfigResponse getForCluster(@NotEmpty String environmentCrn, @NotEmpty String clusterName) throws Exception {
+    public DescribeKerberosConfigResponse getForCluster(@NotEmpty @ResourceCrn String environmentCrn, @NotEmpty String clusterName) throws Exception {
         String accountId = crnService.getCurrentAccountId();
         return kerberosConfigV1Service.getForCluster(environmentCrn, accountId, clusterName);
     }
