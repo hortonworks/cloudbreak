12fe9f3a0d20a443ceaca989f8fefed12dba2272
David Bajzath <bajzathd@gmail.com>
2020-02-21 11:25:15 +0100

    CB-5546 Add mock FreeIpa child environment delete tests

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/assertion/freeipa/FreeIpaChildEnvironmentAssertion.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/assertion/freeipa/FreeIpaChildEnvironmentAssertion.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.it.cloudbreak.assertion.freeipa;
 
+import javax.ws.rs.NotFoundException;
+
 import org.assertj.core.api.Assertions;
 
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.describe.DescribeFreeIpaResponse;
@@ -16,12 +18,28 @@ public class FreeIpaChildEnvironmentAssertion {
     /**
      * Validate that when fetching FreeIpa for the child environment it returns the parent environment's FreeIpa
      */
-    public static Assertion<FreeIPAChildEnvironmentTestDto, FreeIPAClient> validate() {
+    public static Assertion<FreeIPAChildEnvironmentTestDto, FreeIPAClient> validateChildFreeipa() {
         return (testContext, entity, client) -> {
-            DescribeFreeIpaResponse freeIpaResponse = client.getFreeIpaClient().getFreeIpaV1Endpoint()
-                    .describe(entity.getRequest().getChildEnvironmentCrn());
-            Assertions.assertThat(freeIpaResponse.getEnvironmentCrn()).isEqualTo(entity.getRequest().getParentEnvironmentCrn());
+            DescribeFreeIpaResponse freeIpaResponse = describeChildFreeipa(entity, client);
+            Assertions.assertThat(freeIpaResponse.getEnvironmentCrn())
+                    .isEqualTo(entity.getRequest().getParentEnvironmentCrn());
             return entity;
         };
+    }
+
+    /**
+     * Validate that when fetching FreeIpa for the child environment it is not found
+     */
+    public static Assertion<FreeIPAChildEnvironmentTestDto, FreeIPAClient> validateNoFreeipa() {
+        return (testContext, entity, client) -> {
+            Assertions.assertThatThrownBy(() -> describeChildFreeipa(entity, client))
+                    .isInstanceOf(NotFoundException.class);
+            return entity;
+        };
+    }
+
+    private static DescribeFreeIpaResponse describeChildFreeipa(FreeIPAChildEnvironmentTestDto entity, FreeIPAClient client) {
+        return client.getFreeIpaClient().getFreeIpaV1Endpoint()
+                .describe(entity.getRequest().getChildEnvironmentCrn());
     }
 }
