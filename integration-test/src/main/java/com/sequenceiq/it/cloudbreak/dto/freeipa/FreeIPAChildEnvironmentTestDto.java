ae18e378a1f5b061b8726040b25f7cfd29cb69eb
David Bajzath <bajzathd@gmail.com>
2020-02-21 11:37:49 +0100

    CB-5546 Clean up attach/detach child env tests

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/dto/freeipa/FreeIPAChildEnvironmentTestDto.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/dto/freeipa/FreeIPAChildEnvironmentTestDto.java
@@ -1,24 +1,16 @@
 package com.sequenceiq.it.cloudbreak.dto.freeipa;
 
-import java.util.Collection;
-
 import javax.inject.Inject;
 
-import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.detachchildenv.DetachChildEnvironmentRequest;
-import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.list.ListFreeIpaResponse;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.attachchildenv.AttachChildEnvironmentRequest;
-import com.sequenceiq.it.cloudbreak.FreeIPAClient;
 import com.sequenceiq.it.cloudbreak.Prototype;
 import com.sequenceiq.it.cloudbreak.client.FreeIPATestClient;
-import com.sequenceiq.it.cloudbreak.context.Purgable;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.AbstractFreeIPATestDto;
 import com.sequenceiq.it.cloudbreak.dto.environment.EnvironmentTestDto;
-import com.sequenceiq.it.cloudbreak.search.Searchable;
 
 @Prototype
-public class FreeIPAChildEnvironmentTestDto extends AbstractFreeIPATestDto<AttachChildEnvironmentRequest, Void, FreeIPAChildEnvironmentTestDto>
-        implements Purgable<ListFreeIpaResponse, FreeIPAClient>, Searchable {
+public class FreeIPAChildEnvironmentTestDto extends AbstractFreeIPATestDto<AttachChildEnvironmentRequest, Void, FreeIPAChildEnvironmentTestDto> {
 
     public static final String CHILD_ENVIRONMENT_KEY = "childEnv";
 
@@ -45,31 +37,4 @@ public class FreeIPAChildEnvironmentTestDto extends AbstractFreeIPATestDto<Attac
         return getTestContext().get(EnvironmentTestDto.class).getResponse().getCrn();
     }
 
-    @Override
-    public Collection<ListFreeIpaResponse> getAll(FreeIPAClient client) {
-        return client.getFreeIpaClient().getFreeIpaV1Endpoint().list();
-    }
-
-    @Override
-    public boolean deletable(ListFreeIpaResponse entity) {
-        return entity.getName().startsWith(getResourcePropertyProvider().prefix());
-    }
-
-    @Override
-    public void delete(TestContext testContext, ListFreeIpaResponse entity, FreeIPAClient client) {
-        DetachChildEnvironmentRequest request = new DetachChildEnvironmentRequest();
-        request.setParentEnvironmentCrn(getParentEnvironmentCrn());
-        request.setChildEnvironmentCrn(getChildEnvironmentCrn());
-        client.getFreeIpaClient().getFreeIpaV1Endpoint().detachChildEnvironment(request);
-    }
-
-    @Override
-    public Class<FreeIPAClient> client() {
-        return FreeIPAClient.class;
-    }
-
-    @Override
-    public String getSearchId() {
-        return getName();
-    }
 }
