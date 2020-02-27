0fb4ebe0c0738d0c8f71ff80f2a9ac0c16ccac7f
Balázs Bihari <balazs.bihari@gmail.com>
2020-02-27 14:17:29 +0100

    CB-5787 Event history is filled with invalid information and status is Running (#7405)

--- core/src/test/java/com/sequenceiq/cloudbreak/service/cluster/ClusterServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/cluster/ClusterServiceTest.java
@@ -11,7 +11,6 @@ import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.spy;
@@ -202,7 +201,7 @@ public class ClusterServiceTest {
     }
 
     @Test
-    public void shouldTriggerSync() {
+    public void shouldNotTriggerSync() {
         String hostFQDN = "host2Name.stopped";
         InstanceMetaData instanceMd = getHost(hostFQDN, "master", InstanceStatus.SERVICES_HEALTHY, InstanceGroupType.GATEWAY);
         when(hostGroupService.getByClusterIdAndName(stack.getCluster().getId(), instanceMd.getInstanceGroup().getGroupName()))
@@ -211,12 +210,10 @@ public class ClusterServiceTest {
 
         when(stackService.findByCrn(STACK_CRN)).thenReturn(stack);
         when(cloudbreakMessagesService.getMessage(any(), anyCollection())).thenReturn("failed node");
-        when(instanceMetaDataService.getPrimaryGatewayInstanceMetadata(anyLong())).thenReturn(Optional.of(instanceMd));
-        doNothing().when(flowManager).triggerStackSync(anyLong());
 
         underTest.reportHealthChange(STACK_CRN, Set.of(hostFQDN), Set.of());
 
-        verify(flowManager, times(1)).triggerStackSync(eq(stack.getId()));
+        verify(flowManager, times(0)).triggerStackSync(eq(stack.getId()));
     }
 
     @Test
