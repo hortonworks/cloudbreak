ab5ecc68e5fec5f2b0644ff910e6fe3e22913d17
Tamas Bihari <tbihari@hortonworks.com>
2020-03-04 22:16:58 +0100

    CB-5917 use parent environment virtual groups for workloads in child environments

--- core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/ldap/LdapSSOConfigurationHandler.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/ldap/LdapSSOConfigurationHandler.java
@@ -6,20 +6,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.auth.altus.VirtualGroupRequest;
 import com.sequenceiq.cloudbreak.common.event.Selectable;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.dto.LdapView;
 import com.sequenceiq.cloudbreak.ldap.LdapConfigService;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
-import com.sequenceiq.cloudbreak.auth.altus.VirtualGroupRequest;
-import com.sequenceiq.flow.event.EventSelectorUtil;
 import com.sequenceiq.cloudbreak.reactor.api.event.ldap.LdapSSOConfigurationFailed;
 import com.sequenceiq.cloudbreak.reactor.api.event.ldap.LdapSSOConfigurationRequest;
 import com.sequenceiq.cloudbreak.reactor.api.event.ldap.LdapSSOConfigurationSuccess;
-import com.sequenceiq.flow.reactor.api.handler.EventHandler;
 import com.sequenceiq.cloudbreak.service.GatewayConfigService;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterApiConnectors;
+import com.sequenceiq.cloudbreak.service.environment.EnvironmentConfigProvider;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
+import com.sequenceiq.flow.event.EventSelectorUtil;
+import com.sequenceiq.flow.reactor.api.handler.EventHandler;
 
 import reactor.bus.Event;
 import reactor.bus.EventBus;
@@ -44,6 +45,9 @@ public class LdapSSOConfigurationHandler implements EventHandler<LdapSSOConfigur
     @Inject
     private LdapConfigService ldapConfigService;
 
+    @Inject
+    private EnvironmentConfigProvider environmentConfigProvider;
+
     @Override
     public String selector() {
         return EventSelectorUtil.selector(LdapSSOConfigurationRequest.class);
@@ -57,7 +61,8 @@ public class LdapSSOConfigurationHandler implements EventHandler<LdapSSOConfigur
             Stack stack = stackService.getByIdWithListsInTransaction(stackId);
             GatewayConfig primaryGatewayConfig = gatewayConfigService.getPrimaryGatewayConfig(stack);
             LdapView ldapView = ldapConfigService.get(stack.getEnvironmentCrn(), stack.getName()).orElse(null);
-            VirtualGroupRequest virtualGroupRequest = new VirtualGroupRequest(stack.getEnvironmentCrn(), ldapView != null ? ldapView.getAdminGroup() : "");
+            String environmentCrnForVirtualGroups = environmentConfigProvider.getParentEnvironmentCrn(stack.getEnvironmentCrn());
+            VirtualGroupRequest virtualGroupRequest = new VirtualGroupRequest(environmentCrnForVirtualGroups, ldapView != null ? ldapView.getAdminGroup() : "");
             clusterApiConnectors.getConnector(stack).clusterSecurityService().setupLdapAndSSO(primaryGatewayConfig.getPublicAddress(), ldapView,
                     virtualGroupRequest);
             response = new LdapSSOConfigurationSuccess(stackId);
