98aa43ffd97d2da8243809f134974bbe8956b02d
Tamas Bihari <tbihari@hortonworks.com>
2020-03-04 22:16:58 +0100

    CB-5917 use parent environment virtual groups for workloads in child environments

--- core/src/main/java/com/sequenceiq/cloudbreak/service/environment/EnvironmentConfigProvider.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/environment/EnvironmentConfigProvider.java
@@ -2,6 +2,8 @@ package com.sequenceiq.cloudbreak.service.environment;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.cloudera.cdp.shaded.org.apache.commons.lang3.StringUtils;
@@ -10,15 +12,33 @@ import com.sequenceiq.environment.api.v1.environment.model.response.DetailedEnvi
 
 @Service
 public class EnvironmentConfigProvider {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvironmentConfigProvider.class);
 
     @Inject
     private EnvironmentClientService environmentClientService;
 
     public boolean isChildEnvironment(String environmentCrn) {
+        DetailedEnvironmentResponse environment = getEnvironmentByCrn(environmentCrn);
+        return StringUtils.isNoneEmpty(environment.getParentEnvironmentCrn());
+    }
+
+    public String getParentEnvironmentCrn(String environmentCrn) {
+            String result = environmentCrn;
+            DetailedEnvironmentResponse environmentResponse = getEnvironmentByCrn(environmentCrn);
+            String parentEnvironmentCrn = environmentResponse.getParentEnvironmentCrn();
+            if (StringUtils.isNotBlank(parentEnvironmentCrn)) {
+                LOGGER.debug("The environment('{}') is a child, returning with the crn('{}') of the parent environment", environmentCrn, parentEnvironmentCrn);
+                result = parentEnvironmentCrn;
+            }
+            return result;
+    }
+
+    private DetailedEnvironmentResponse getEnvironmentByCrn(String environmentCrn) {
         try {
-            DetailedEnvironmentResponse environmentResponse = environmentClientService.getByCrn(environmentCrn);
-            return StringUtils.isNoneEmpty(environmentResponse.getParentEnvironmentCrn());
+            LOGGER.info("Fetch environment details by crn:'{}' from Environment service", environmentCrn);
+            return environmentClientService.getByCrn(environmentCrn);
         } catch (Exception ex) {
+            LOGGER.warn(String.format("Failed to get environment by crn:'%s' from Environment service", environmentCrn), ex);
             throw new CloudbreakServiceException("Failed to get environment details from Environment service due to:", ex);
         }
     }
