147599a30af0a529405e53582b732b85e11ccd73
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2020-03-05 11:49:24 +0100

    CB-5900 Remove flow check in case of SDX termination

--- datalake/src/main/java/com/sequenceiq/datalake/service/sdx/ProvisionerService.java
+++ datalake/src/main/java/com/sequenceiq/datalake/service/sdx/ProvisionerService.java
@@ -62,7 +62,6 @@ public class ProvisionerService {
         sdxClusterRepository.findById(id).ifPresentOrElse(sdxCluster -> {
             try {
                 stackV4Endpoint.delete(0L, sdxCluster.getClusterName(), forced);
-                cloudbreakFlowService.getAndSaveLastCloudbreakFlowChainId(sdxCluster);
                 sdxStatusService.setStatusForDatalakeAndNotify(DatalakeStatusEnum.STACK_DELETION_IN_PROGRESS,
                         ResourceEvent.SDX_CLUSTER_DELETION_STARTED, "Datalake stack deletion in progress", sdxCluster);
             } catch (NotFoundException e) {
@@ -88,10 +87,6 @@ public class ProvisionerService {
                     .run(() -> {
                         LOGGER.info("Deletion polling cloudbreak for stack status: '{}' in '{}' env", sdxCluster.getClusterName(), sdxCluster.getEnvName());
                         try {
-                            if (cloudbreakFlowService.isLastKnownFlowRunning(sdxCluster)) {
-                                LOGGER.info("Deletion polling will continue, cluster has an active flow in Cloudbreak, id: {}", sdxCluster.getId());
-                                return AttemptResults.justContinue();
-                            }
                             StackV4Response stackV4Response = stackV4Endpoint.get(0L, sdxCluster.getClusterName(), Collections.emptySet());
                             LOGGER.info("Stack status of SDX {} by response from cloudbreak: {}", sdxCluster.getClusterName(),
                                     stackV4Response.getStatus().name());
