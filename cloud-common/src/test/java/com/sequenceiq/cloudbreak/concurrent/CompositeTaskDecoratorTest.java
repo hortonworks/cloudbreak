d1966afe8b035bafad1a639327a2e66ee402f515
David Han <dhan@cloudera.com>
2020-03-06 10:01:56 -0600

    Revert "Revert "CDPCP-1549. Run user sync internals as internal user""
    An authorization mismatch between the Operation db and the UserSyncStatus
    db resulted in a bug where a user sync operation can be accepted, but
    will fail when saving the user sync status. This commit changes the
    authorization behavior so that authoriation is only performed on acceptance
    of the sync operation. All the user sync internals are run as the internal
    user.
    
    Since UserSyncService internals use an AsyncTaskExecutor, the UserSyncConfig
    has been updated to add a decorator for propagating the user crn to the
    submitted jobs.
    
    This reverts commit 7d32d63d725cdb51e3682a203a8ce2b476da502c.

---
+++ cloud-common/src/test/java/com/sequenceiq/cloudbreak/concurrent/CompositeTaskDecoratorTest.java
@@ -0,0 +1,98 @@
+package com.sequenceiq.cloudbreak.concurrent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+import org.springframework.core.task.TaskDecorator;
+
+class CompositeTaskDecoratorTest {
+
+    @Test
+    void testNullDecorators() {
+        assertThrows(NullPointerException.class, () -> {
+            new CompositeTaskDecorator(null);
+        });
+    }
+
+    @Test
+    void testNoDecorators() {
+        CompositeTaskDecorator underTest = new CompositeTaskDecorator(List.of());
+        Runnable runnable = () -> { };
+        Runnable decoratedRunnable = underTest.decorate(runnable);
+        assertEquals(runnable, decoratedRunnable);
+    }
+
+    @Test
+    void testOneDecorator() {
+        DoSomething before = mock(DoSomething.class);
+        DoSomething after = mock(DoSomething.class);
+        Runnable runnable = mock(Runnable.class);
+        InOrder inOrder = Mockito.inOrder(before, after, runnable);
+
+        CompositeTaskDecorator underTest = new CompositeTaskDecorator(List.of(TestDecorator.around(before, after)));
+        Runnable decoratedRunnable = underTest.decorate(runnable);
+
+        decoratedRunnable.run();
+
+        inOrder.verify(before).doSomething();
+        inOrder.verify(runnable).run();
+        inOrder.verify(after).doSomething();
+    }
+
+    @Test
+    void testMultipleDecorators() {
+        DoSomething before1 = mock(DoSomething.class);
+        DoSomething after1 = mock(DoSomething.class);
+        DoSomething before2 = mock(DoSomething.class);
+        DoSomething after2 = mock(DoSomething.class);
+        Runnable runnable = mock(Runnable.class);
+        InOrder inOrder = Mockito.inOrder(before1, after1, before2, after2, runnable);
+
+        CompositeTaskDecorator underTest = new CompositeTaskDecorator(List.of(
+                TestDecorator.around(before1, after1),
+                TestDecorator.around(before2, after2)));
+        Runnable decoratedRunnable = underTest.decorate(runnable);
+
+        decoratedRunnable.run();
+
+        inOrder.verify(before2).doSomething();
+        inOrder.verify(before1).doSomething();
+        inOrder.verify(runnable).run();
+        inOrder.verify(after1).doSomething();
+        inOrder.verify(after2).doSomething();
+    }
+
+    private static class TestDecorator implements TaskDecorator {
+        private final DoSomething before;
+
+        private final DoSomething after;
+
+        TestDecorator(DoSomething before, DoSomething after) {
+            this.before = before;
+            this.after = after;
+        }
+
+        @Override
+        public Runnable decorate(Runnable runnable) {
+            return () -> {
+                before.doSomething();
+                runnable.run();
+                after.doSomething();
+            };
+        }
+
+        private static TestDecorator around(DoSomething before, DoSomething after) {
+            return new TestDecorator(before, after);
+        }
+    }
+
+    private interface DoSomething {
+        void doSomething();
+    }
+}
