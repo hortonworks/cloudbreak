b438f504500d60531e1794b823d0c9ea5b442891
Peter Kedvessy <9048479+pkedvessy@users.noreply.github.com>
2020-03-06 14:45:03 +0100

    CB-5963 Enable auto TLS support on YARN platform

--- cloud-yarn/src/main/java/com/sequenceiq/cloudbreak/cloud/yarn/YarnPlatformParameters.java
+++ cloud-yarn/src/main/java/com/sequenceiq/cloudbreak/cloud/yarn/YarnPlatformParameters.java
@@ -85,7 +85,7 @@ public class YarnPlatformParameters implements PlatformParameters {
 
     @Override
     public boolean isAutoTlsSupported() {
-        return false;
+        return true;
     }
 
     @Override
