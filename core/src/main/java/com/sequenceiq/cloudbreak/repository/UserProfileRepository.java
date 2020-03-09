20733be7fd91f4141b6efc9c0be35318205806ac
Attila Kanto <attila.kanto@gmail.com>
2020-03-09 11:43:07 +0100

    CB-5955 eliminate UserPrefernces which is a dead code, DB tables are still there due to compatibility issues

--- core/src/main/java/com/sequenceiq/cloudbreak/repository/UserProfileRepository.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/repository/UserProfileRepository.java
@@ -21,7 +21,6 @@ public interface UserProfileRepository extends BaseRepository<UserProfile, Long>
     @Query("SELECT b FROM UserProfile b "
             + "LEFT JOIN FETCH b.user u "
             + "LEFT JOIN FETCH u.tenant "
-            + "LEFT JOIN FETCH u.userPreferences "
             + "WHERE u.id= :userId")
     Optional<UserProfile> findOneByUser(@Param("userId") Long userId);
 
