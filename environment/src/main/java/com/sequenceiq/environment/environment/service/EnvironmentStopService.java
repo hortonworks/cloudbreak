2996162d2dbc7861790dc7fad7407750cc4288b2
David Bajzath <bajzathd@gmail.com>
2020-03-13 17:45:49 +0100

    CB-5723 Deny stop on parent env

--- environment/src/main/java/com/sequenceiq/environment/environment/service/EnvironmentStopService.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/service/EnvironmentStopService.java
@@ -1,8 +1,14 @@
 package com.sequenceiq.environment.environment.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
+import com.sequenceiq.cloudbreak.exception.BadRequestException;
+import com.sequenceiq.environment.environment.EnvironmentStatus;
+import com.sequenceiq.environment.environment.domain.Environment;
 import com.sequenceiq.environment.environment.dto.EnvironmentDto;
 import com.sequenceiq.environment.environment.flow.EnvironmentReactorFlowManager;
 
@@ -21,15 +27,34 @@ public class EnvironmentStopService {
 
     public void stopByCrn(String crn) {
         String accountId = ThreadBasedUserCrnProvider.getAccountId();
-        String userCrn = ThreadBasedUserCrnProvider.getUserCrn();
         EnvironmentDto environment = environmentService.getByCrnAndAccountId(crn, accountId);
-        reactorFlowManager.triggerStopFlow(environment.getId(), environment.getName(), userCrn);
+        stop(environment, accountId);
     }
 
     public void stopByName(String name) {
         String accountId = ThreadBasedUserCrnProvider.getAccountId();
-        String userCrn = ThreadBasedUserCrnProvider.getUserCrn();
         EnvironmentDto environment = environmentService.getByNameAndAccountId(name, accountId);
+        stop(environment, accountId);
+    }
+
+    private void stop(EnvironmentDto environment, String accountId) {
+        validateStoppable(environment, accountId);
+
+        String userCrn = ThreadBasedUserCrnProvider.getUserCrn();
         reactorFlowManager.triggerStopFlow(environment.getId(), environment.getName(), userCrn);
+    }
+
+    private void validateStoppable(EnvironmentDto environment, String accountId) {
+        List<String> runningChildEnvironmentNames = environmentService.findAllByAccountIdAndParentEnvIdAndArchivedIsFalse(accountId, environment.getId())
+                .stream()
+                .filter(childEnvironment -> EnvironmentStatus.ENV_STOPPED != childEnvironment.getStatus())
+                .map(Environment::getName)
+                .collect(Collectors.toList());
+        if (!runningChildEnvironmentNames.isEmpty()) {
+            String message = String.format(
+                    "The following child Environment(s) have to be stopped before Environment stop: [%s]",
+                    String.join(", ", runningChildEnvironmentNames));
+            throw new BadRequestException(message);
+        }
     }
 }
