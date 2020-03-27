dc9a1c2e1576c1bda093729f316b91c629807e3a
Tibor Popovics <tiborpopovics93@gmail.com>
2020-03-27 09:19:03 +0100

    CB-5989: add default value for paywall credentials

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -255,8 +255,8 @@ cloudbreak-conf-defaults() {
     env-import CB_LOG_LEVEL "INFO"
     env-import AMBARI_CLIENT_LOG_LEVEL "ERROR"
     env-import CB_PORT 8080
-    env-import CB_PAYWALL_USERNAME
-    env-import CB_PAYWALL_PASSWORD
+    env-import CB_PAYWALL_USERNAME ""
+    env-import CB_PAYWALL_PASSWORD ""
 
 
     env-import CB_INSTANCE_UUID
