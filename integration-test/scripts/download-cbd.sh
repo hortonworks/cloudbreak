8b6dcdc859872e42880d04014e713bb92e5b916d
doktoric <rdoktorics@cloudera.com>
2020-03-30 08:54:52 +0200

    CB-4344 cb pull request with the new versioning

--- integration-test/scripts/download-cbd.sh
+++ integration-test/scripts/download-cbd.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
 : ${INTEGCB_LOCATION?"integcb location"}
+: ${CB_TARGET_BRANCH?"pull request target branch"}
+: ${CB_VERSION?"version of the latest build"}
+
 cd $INTEGCB_LOCATION
 
 os=$(uname)
-latest_tag=$(git tag --points-at HEAD | sort -n | tail -1)
-if ! [[ $latest_tag ]]; then
-    latest_tag=$(git describe --abbrev=0 --tags)
-fi
-state=$(echo $latest_tag | cut -d '-' -f 2)
-if [[ ! $latest_tag || "$state" = *"dev"* ]]; then
-    branch="master"
-else
-    version=$(echo $latest_tag | cut -d '-' -f 1)
-    branch="rc-$(echo $version | cut -d '.' -f 1,2)"
-fi
-echo -e "\n\033[1;96m--- build latest cbd: $branch for $os\033[0m\n"
+echo -e "\n\033[1;96m--- build latest cbd: $CB_TARGET_BRANCH for $os\033[0m\n"
 rm_flag=""
 if ! [[ "$CIRCLECI" ]]; then
     rm_flag="--rm"
@@ -23,7 +15,7 @@ fi
 
 docker volume rm cbd-source 2>/dev/null || :
 docker volume create --name cbd-source 1>/dev/null
-docker run $rm_flag --user="0" --entrypoint init.sh -v cbd-source:/var/workspace jpco/git:1.0 https://github.com/hortonworks/cloudbreak-deployer.git $branch cloudbreak-deployer
+docker run $rm_flag --user="0" --entrypoint init.sh -v cbd-source:/var/workspace jpco/git:1.0 https://github.com/hortonworks/cloudbreak-deployer.git $CB_TARGET_BRANCH cloudbreak-deployer
 docker run $rm_flag --user="0" -e GOPATH=/usr -v cbd-source:/usr/src/github.com/hortonworks -w /usr/src/github.com/hortonworks/cloudbreak-deployer golang:1.12 make bindata
 docker run $rm_flag --user="0" -e GOPATH=/usr -v cbd-source:/usr/src/github.com/hortonworks -w /usr/src/github.com/hortonworks/cloudbreak-deployer golang:1.12 make build
 docker run $rm_flag --user="0" -v cbd-source:/var/workspace jpco/git:1.0 cat /var/workspace/cloudbreak-deployer/build/${os}/cbd > cbd
@@ -32,7 +24,7 @@ if [[ $rm_flag ]]; then
 fi
 
 chmod +x cbd
-cbd_version=$(TRACE=1 ./cbd --version)
+cbd_version=$(CB_VERSION)
 mkdir etc
 cp ../../mock-caas/src/test/resources/etc/license.txt etc/license.txt
 echo -e "$cbd_version"
