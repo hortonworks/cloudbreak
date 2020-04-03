4998ecea18d2ffa0127dc120e3440dd04180c370
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2020-04-03 11:02:22 +0200

    CB-6404 Secure permissions on cert and private key for user facing TLS

--- orchestrator-salt/src/main/resources/salt/salt/nginx/scripts/create-user-facing-cert.sh
+++ orchestrator-salt/src/main/resources/salt/salt/nginx/scripts/create-user-facing-cert.sh
@@ -4,3 +4,5 @@ CERT_ROOT_PATH=/etc/certs-user-facing
 
 certm -d ${CERT_ROOT_PATH} ca generate -o=gateway --overwrite
 certm -d ${CERT_ROOT_PATH} server generate -o=gateway --host hostname --host {{ salt['pillar.get']('gateway:address') }}
+chown root:root ${CERT_ROOT_PATH}/*
+chmod 600 ${CERT_ROOT_PATH}/*
