be072b027438aa88e159daf9b0fc735427d50b2b
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2020-04-08 20:10:16 +0200

    CB-6514 AWS Datalake deployment on mow-dev is failing with HV000028 Unexpected exception during isValid

--- datalake-api/src/main/java/com/sequenceiq/sdx/api/model/SdxInternalClusterRequest.java
+++ datalake-api/src/main/java/com/sequenceiq/sdx/api/model/SdxInternalClusterRequest.java
@@ -1,12 +1,9 @@
 package com.sequenceiq.sdx.api.model;
 
-import javax.validation.Valid;
-
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.StackV4Request;
 
 public class SdxInternalClusterRequest extends SdxClusterRequest {
 
-    @Valid
     private StackV4Request stackV4Request;
 
     public StackV4Request getStackV4Request() {
