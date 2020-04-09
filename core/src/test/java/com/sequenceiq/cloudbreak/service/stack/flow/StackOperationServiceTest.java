b812d3300d93c94d78eed9d8b7ed901f065de88d
David Bajzath <bajzathd@gmail.com>
2020-04-09 16:26:55 +0200

    CB-6458 Add tests to deny distrox/sdx stop on spot instances

--- core/src/test/java/com/sequenceiq/cloudbreak/service/stack/flow/StackOperationServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stack/flow/StackOperationServiceTest.java
@@ -1,10 +1,18 @@
 package com.sequenceiq.cloudbreak.service.stack.flow;
 
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus.AVAILABLE;
+import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus.STOP_REQUESTED;
 import static com.sequenceiq.cloudbreak.event.ResourceEvent.STACK_START_IGNORED;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -16,12 +24,18 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.sequenceiq.authorization.service.CommonPermissionCheckingUtils;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.StatusRequest;
+import com.sequenceiq.cloudbreak.common.json.Json;
 import com.sequenceiq.cloudbreak.core.flow2.service.ReactorFlowManager;
+import com.sequenceiq.cloudbreak.domain.Template;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.domain.stack.StackStatus;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
+import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceGroup;
 import com.sequenceiq.cloudbreak.exception.BadRequestException;
 import com.sequenceiq.cloudbreak.service.StackUpdater;
+import com.sequenceiq.cloudbreak.service.cluster.flow.ClusterOperationService;
+import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.cloudbreak.structuredevent.event.CloudbreakEventService;
 import com.sequenceiq.cloudbreak.workspace.model.User;
 
@@ -45,6 +59,12 @@ public class StackOperationServiceTest {
 
     @Mock
     private StackUpdater stackUpdater;
+
+    @Mock
+    private ClusterOperationService clusterOperationService;
+
+    @Mock
+    private StackService stackService;
 
     @Test
     public void testStartWhenStackAvailable() {
@@ -105,5 +125,42 @@ public class StackOperationServiceTest {
         underTest.start(stack, cluster, false, new User());
         verify(flowManager, times(1)).triggerStackStart(stack.getId());
         verify(stackUpdater, times(1)).updateStackStatus(stack.getId(),  DetailedStackStatus.START_REQUESTED);
+    }
+
+    @Test
+    public void shouldNotTriggerStopWhenStackRunsOnSpotInstances() {
+        Stack stack = new Stack();
+        stack.setId(9876L);
+        stack.setStackStatus(new StackStatus(stack, AVAILABLE));
+        stack.setInstanceGroups(Set.of(createInstanceGroup(100)));
+
+        when(stackService.getByIdWithLists(stack.getId())).thenReturn(stack);
+
+        Assertions.assertThatThrownBy(() -> underTest.updateStatus(stack.getId(), StatusRequest.STOPPED, true, new User()))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage(String.format("Cannot update the status of stack '%s' to STOPPED, because it runs on spot instances", stack.getName()));
+        verify(stackUpdater, never()).updateStackStatus(any(), any());
+    }
+
+    @Test
+    public void shouldTriggerStopWhenStackRunsOnOnDemandInstances() {
+        Stack stack = new Stack();
+        stack.setId(9876L);
+        stack.setStackStatus(new StackStatus(stack, AVAILABLE));
+        stack.setInstanceGroups(Set.of(createInstanceGroup(0), createInstanceGroup(null)));
+
+        when(stackService.getByIdWithLists(stack.getId())).thenReturn(stack);
+
+        underTest.updateStatus(stack.getId(), StatusRequest.STOPPED, true, new User());
+
+        verify(stackUpdater).updateStackStatus(stack.getId(), STOP_REQUESTED);
+    }
+
+    private InstanceGroup createInstanceGroup(Integer spotPercentage) {
+        Template template = new Template();
+        template.setAttributes(new Json(spotPercentage != null ? Map.of("spotPercentage", spotPercentage) : Map.of()));
+        InstanceGroup instanceGroup = new InstanceGroup();
+        instanceGroup.setTemplate(template);
+        return instanceGroup;
     }
 }
