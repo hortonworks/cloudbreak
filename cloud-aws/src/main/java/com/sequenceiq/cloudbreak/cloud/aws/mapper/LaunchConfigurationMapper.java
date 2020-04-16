9dd5c5d3f11dc5b2661d2dbca54d3c86a5a039c9
Attila Kanto <attila.kanto@gmail.com>
2020-04-16 13:35:01 +0200

    CB-6355 Moving LaunchConfigurationMapperImpl to java folder to avoid the following error message from IDEA:
    Field launchConfigurationMapper in com.sequenceiq.cloudbreak.cloud.aws.LaunchConfigurationHandler required a bean of type 'com.sequenceiq.cloudbreak.cloud.aws.mapper.LaunchConfigurationMapper' that could not be found.
    
    The injection point has the following annotations:
    	- @javax.inject.Inject()

--- cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/mapper/LaunchConfigurationMapper.java
+++ cloud-aws/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/mapper/LaunchConfigurationMapper.java
@@ -1,25 +1,61 @@
 package com.sequenceiq.cloudbreak.cloud.aws.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.autoscaling.model.BlockDeviceMapping;
 import com.amazonaws.services.autoscaling.model.CreateLaunchConfigurationRequest;
 import com.amazonaws.services.autoscaling.model.LaunchConfiguration;
 
-@Mapper(componentModel = "spring", uses = EmptyToNullStringMapper.class)
-public interface LaunchConfigurationMapper {
+@Component
+public class LaunchConfigurationMapper {
 
-    @Mappings({
-            @Mapping(target = "requestCredentials", ignore = true),
-            @Mapping(target = "requestCredentialsProvider", ignore = true),
-            @Mapping(target = "requestMetricCollector", ignore = true),
-            @Mapping(target = "generalProgressListener", ignore = true),
-            @Mapping(target = "sdkRequestTimeout", ignore = true),
-            @Mapping(target = "sdkClientExecutionTimeout", ignore = true),
-            @Mapping(target = "instanceId", ignore = true),
-            @Mapping(target = "customRequestHeaders", ignore = true),
-            @Mapping(target = "customQueryParameters", ignore = true)
-    })
-    CreateLaunchConfigurationRequest mapExistingLaunchConfigToRequest(LaunchConfiguration launchConfiguration);
+    @Autowired
+    private EmptyToNullStringMapper emptyToNullStringMapper;
+
+    public CreateLaunchConfigurationRequest mapExistingLaunchConfigToRequest(LaunchConfiguration launchConfiguration) {
+        if (launchConfiguration == null) {
+            return null;
+        }
+
+        CreateLaunchConfigurationRequest createLaunchConfigurationRequest = new CreateLaunchConfigurationRequest();
+
+        createLaunchConfigurationRequest.setLaunchConfigurationName(emptyToNullStringMapper.map(launchConfiguration.getLaunchConfigurationName()));
+        createLaunchConfigurationRequest.setImageId(emptyToNullStringMapper.map(launchConfiguration.getImageId()));
+        createLaunchConfigurationRequest.setKeyName(emptyToNullStringMapper.map(launchConfiguration.getKeyName()));
+        List<String> list = launchConfiguration.getSecurityGroups();
+        if (list != null) {
+            createLaunchConfigurationRequest.setSecurityGroups(new ArrayList<String>(list));
+        } else {
+            createLaunchConfigurationRequest.setSecurityGroups(null);
+        }
+        createLaunchConfigurationRequest.setClassicLinkVPCId(emptyToNullStringMapper.map(launchConfiguration.getClassicLinkVPCId()));
+        List<String> list1 = launchConfiguration.getClassicLinkVPCSecurityGroups();
+        if (list1 != null) {
+            createLaunchConfigurationRequest.setClassicLinkVPCSecurityGroups(new ArrayList<String>(list1));
+        } else {
+            createLaunchConfigurationRequest.setClassicLinkVPCSecurityGroups(null);
+        }
+        createLaunchConfigurationRequest.setUserData(emptyToNullStringMapper.map(launchConfiguration.getUserData()));
+        createLaunchConfigurationRequest.setInstanceType(emptyToNullStringMapper.map(launchConfiguration.getInstanceType()));
+        createLaunchConfigurationRequest.setKernelId(emptyToNullStringMapper.map(launchConfiguration.getKernelId()));
+        createLaunchConfigurationRequest.setRamdiskId(emptyToNullStringMapper.map(launchConfiguration.getRamdiskId()));
+        List<BlockDeviceMapping> list2 = launchConfiguration.getBlockDeviceMappings();
+        if (list2 != null) {
+            createLaunchConfigurationRequest.setBlockDeviceMappings(new ArrayList<BlockDeviceMapping>(list2));
+        } else {
+            createLaunchConfigurationRequest.setBlockDeviceMappings(null);
+        }
+        createLaunchConfigurationRequest.setInstanceMonitoring(launchConfiguration.getInstanceMonitoring());
+        createLaunchConfigurationRequest.setSpotPrice(emptyToNullStringMapper.map(launchConfiguration.getSpotPrice()));
+        createLaunchConfigurationRequest.setIamInstanceProfile(emptyToNullStringMapper.map(launchConfiguration.getIamInstanceProfile()));
+        createLaunchConfigurationRequest.setEbsOptimized(launchConfiguration.getEbsOptimized());
+        createLaunchConfigurationRequest.setAssociatePublicIpAddress(launchConfiguration.getAssociatePublicIpAddress());
+        createLaunchConfigurationRequest.setPlacementTenancy(emptyToNullStringMapper.map(launchConfiguration.getPlacementTenancy()));
+
+        return createLaunchConfigurationRequest;
+    }
 }
