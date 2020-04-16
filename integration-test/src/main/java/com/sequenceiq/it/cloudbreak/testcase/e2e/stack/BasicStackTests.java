3716da6a5efe3271775b8c76e29f6f64e707dad6
Dénes Berger <dberger@cloudbreak.com>
2020-04-16 10:50:11 +0200

    CB-6611 Rewrite BasicStackTests to use DistroX endpoint instead of raw Cloudbreak stack endpoint

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/stack/BasicStackTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/stack/BasicStackTests.java
@@ -9,18 +9,18 @@ import org.testng.annotations.Test;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.InstanceGroupV4Base;
 import com.sequenceiq.it.cloudbreak.CloudbreakClient;
 import com.sequenceiq.it.cloudbreak.assertion.Assertion;
-import com.sequenceiq.it.cloudbreak.client.StackTestClient;
+import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
-import com.sequenceiq.it.cloudbreak.dto.ClouderaManagerTestDto;
-import com.sequenceiq.it.cloudbreak.dto.ClusterTestDto;
-import com.sequenceiq.it.cloudbreak.dto.stack.StackTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.DistroXClusterTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.clouderamanager.DistroXClouderaManagerTestDto;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.AbstractE2ETest;
 
 public class BasicStackTests extends AbstractE2ETest {
 
     @Inject
-    private StackTestClient stackTestClient;
+    private DistroXTestClient distroXTestClient;
 
     @Override
     protected void setupTest(TestContext testContext) {
@@ -45,35 +45,31 @@ public class BasicStackTests extends AbstractE2ETest {
         Integer downscaleCount = 3;
         String groupToScale = "worker";
 
-        testContext.given(cm, ClouderaManagerTestDto.class)
-                .given(cmcluster, ClusterTestDto.class)
+        testContext.given(cm, DistroXClouderaManagerTestDto.class)
+                .given(cmcluster, DistroXClusterTestDto.class)
                 .withValidateBlueprint(Boolean.FALSE)
                 .withClouderaManager(cm)
-                .given(stack, StackTestDto.class)
+                .given(stack, DistroXTestDto.class)
                 .withCluster(cmcluster)
-                .when(stackTestClient.createV4(), key(stack))
+                .when(distroXTestClient.create(), key(stack))
                 .await(STACK_AVAILABLE, key(stack))
-                .when(stackTestClient.scalePostV4()
-                        .withGroup(groupToScale)
-                        .withDesiredCount(upscaleCount), key(stack))
+                .when(distroXTestClient.scale(groupToScale, upscaleCount), key(stack))
                 .await(STACK_AVAILABLE, key(stack))
-                .then((ctx, stackDto, cloudbreakClient) -> stackTestClient.getV4().action(ctx, stackDto, cloudbreakClient))
+                .then((ctx, stackDto, cloudbreakClient) -> distroXTestClient.get().action(ctx, stackDto, cloudbreakClient))
                 .then(checkWorkerGroupNodeCount(groupToScale, upscaleCount))
-                .when(stackTestClient.scalePostV4()
-                        .withGroup(groupToScale)
-                        .withDesiredCount(downscaleCount), key(stack))
+                .when(distroXTestClient.scale(groupToScale, downscaleCount), key(stack))
                 .await(STACK_AVAILABLE, key(stack))
-                .then((ctx, stackDto, cloudbreakClient) -> stackTestClient.getV4().action(ctx, stackDto, cloudbreakClient))
+                .then((ctx, stackDto, cloudbreakClient) -> distroXTestClient.get().action(ctx, stackDto, cloudbreakClient))
                 .then(checkWorkerGroupNodeCount(groupToScale, downscaleCount))
-                .when(stackTestClient.stopV4(), key(stack))
+                .when(distroXTestClient.stop(), key(stack))
                 .await(STACK_STOPPED)
-                .when(stackTestClient.startV4(), key(stack))
+                .when(distroXTestClient.start(), key(stack))
                 .await(STACK_AVAILABLE, key(stack))
-                .then((tc, testDto, cc) -> stackTestClient.deleteV4().action(tc, testDto, cc))
+                .then((tc, testDto, cc) -> distroXTestClient.delete().action(tc, testDto, cc))
                 .validate();
     }
 
-    private Assertion<StackTestDto, CloudbreakClient> checkWorkerGroupNodeCount(String scaledGroup, int expectedCount) {
+    private Assertion<DistroXTestDto, CloudbreakClient> checkWorkerGroupNodeCount(String scaledGroup, int expectedCount) {
         return (ctx, testDto, cloudbreakClient) -> {
             Integer nodeCount = testDto.getResponse().getInstanceGroups().stream()
                     .filter(group -> scaledGroup.equals(group.getName()))
