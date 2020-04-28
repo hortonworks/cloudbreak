97fd4d2451504c8db77a2f7dc6fab01ce598abce
Bryce Derriso <b.derriso@gmail.com>
2020-04-28 16:16:59 -0500

    Add backup and restore scripts for DL postgres db.

---
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/backup_db.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ $# -ne 6 ]; then
+  echo "Invalid inputs provided"
+  echo "Script accepts five inputs:"
+  echo "  1. Cloud Provider (azure | aws)"
+  echo "  2. PostgreSQL host name."
+  echo "  3. PostgreSQL port."
+  echo "  4. Object Storage Service url to place backups."
+  echo "  5. PostgreSQL user name."
+  echo "  6. PostgreSQL password."
+  exit 1
+fi
+
+CLOUD_PROVIDER="$1"
+HOST="$2"
+PORT="$3"
+BACKUP_LOCATION="$4"
+USERNAME="$5"
+export PGPASSWORD="$6" # We can provide the password to pg_dump through this variable, or in ~/.pgpass
+
+BACKUP_DATE=$(date '+%Y-%m-%dT%H:%M:%SZ')
+LOGFILE=/var/log/dl_postgres_backup.log
+
+echo "Logs at ${LOGFILE}"
+
+doLog() {
+  type_of_msg=$(echo $* | cut -d" " -f1)
+  msg=$(echo "$*" | cut -d" " -f2-)
+  [[ $type_of_msg == INFO ]] && type_of_msg="INFO " # one space for aligning
+  [[ $type_of_msg == WARN ]] && type_of_msg="WARN " # as well
+
+  # print to the terminal if we have one
+  test -t 1 && echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg"
+  echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg" >>$LOGFILE
+}
+
+errorExit() {
+  doLog "ERROR $1"
+  exit 1
+}
+
+dump_to_azure() {
+  SERVICE="$1"
+  doLog "INFO Dumping ${SERVICE}"
+  LOCAL_BACKUP=${DATE_DIR}/${SERVICE}_backup
+  pg_dump --host="$HOST" --port="$PORT" --username="$USERNAME" --dbname="$SERVICE" --format=custom --file="$LOCAL_BACKUP" >>$LOGFILE 2>&1 || errorExit "Unable to dump ${SERVICE}"
+
+  doLog "INFO Uploading to ${BACKUP_LOCATION}"
+  # todo: strip trailing slash from BACKUP_LOCATION if it's there
+  AZURE_LOCATION="${BACKUP_LOCATION}/${BACKUP_DATE}/${SERVICE}_backup"
+  azcopy copy "$LOCAL_BACKUP" "$AZURE_LOCATION" >>$LOGFILE 2>&1 || errorExit "Unable to upload $SERVICE backup"
+  doLog "INFO Completed upload to ${BACKUP_LOCATION}"
+
+  rm -v "$LOCAL_BACKUP" >>$LOGFILE 2>&1
+
+  doLog "INFO ${SERVICE} dumped to "
+}
+run_azure_backup () {
+  BACKUPS_DIR="/var/tmp/" # Is this appropriate for keeping backups that are staged before uploading to Azure Blob Storage?
+  DATE_DIR=${BACKUPS_DIR}/${BACKUP_DATE}
+  mkdir -p "$DATE_DIR" || error_exit "Could not create local directory for backups."
+
+  azcopy login --identity || errorExit "Could not login to Azure"
+  dump_to_azure "hive"
+  dump_to_azure "ranger"
+
+  rmdir -v "$DATE_DIR" >>$LOGFILE 2>&1
+}
+
+dump_to_s3() {
+  SERVICE=$1
+  # todo: strip trailing slash from BACKUP_LOCATION if it's there
+  S3_LOCATION="${BACKUP_LOCATION}/${BACKUP_DATE}/${SERVICE}_backup"
+  doLog "INFO Dumping ${SERVICE} to ${S3_LOCATION}"
+  # todo: Specify a good compression level with `-Z 1...9`
+  pg_dump --host="$HOST" --port="$PORT" --username="$USERNAME" --dbname="${SERVICE}" --format=custom --data-only 2>>$LOGFILE | /usr/bin/aws s3 cp --sse AES256 --no-progress - "${S3_LOCATION}" 2>>$LOGFILE || errorExit "Unable to dump ${SERVICE}."
+  doLog "INFO ${SERVICE} dumped to ${S3_LOCATION}"
+}
+run_aws_backup () {
+  dump_to_s3 "hive"
+  dump_to_s3 "ranger"
+}
+
+doLog "INFO Starting backup ${BACKUP_DATE}"
+
+if [[ "$CLOUD_PROVIDER" = "azure" ]]; then
+  run_azure_backup
+elif [[ "$CLOUD_PROVIDER" = "aws" ]]; then
+  run_aws_backup
+else
+  errorExit "Unknown cloud provider: ${CLOUD_PROVIDER}"
+fi
+
+doLog "INFO Completed backup ${BACKUP_DATE}"
---
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/restore_db.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ $# -ne 6 ]; then
+  echo "Invalid inputs provided"
+  echo "Script accepts five inputs:"
+  echo "  1. Cloud Provider (azure | aws)"
+  echo "  2. PostgreSQL host name."
+  echo "  3. PostgreSQL port."
+  echo "  4. Object Storage Service url to retrieve backups."
+  echo "  5. PostgreSQL user name."
+  echo "  6. PostgreSQL password."
+  exit 1
+fi
+
+CLOUD_PROVIDER="$1"
+HOST="$2"
+PORT="$3"
+CLOUD_LOCATION="$4"
+USERNAME="$5"
+export PGPASSWORD="$6" # We can provide the password to pg_dump through this variable, or in ~/.pgpass
+
+LOGFILE=/var/log/dl_postgres_restore.log
+echo "Logs at ${LOGFILE}"
+
+BACKUPS_DIR="/var/tmp/postgres_restore_staging" # Is this appropriate for keeping backups that are staged before uploading to Azure Blob Storage?
+
+doLog() {
+  type_of_msg=$(echo $* | cut -d" " -f1)
+  msg=$(echo "$*" | cut -d" " -f2-)
+  [[ $type_of_msg == INFO ]] && type_of_msg="INFO " # one space for aligning
+  [[ $type_of_msg == WARN ]] && type_of_msg="WARN " # as well
+
+  # print to the terminal if we have one
+  test -t 1 && echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg"
+  echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg" >>$LOGFILE
+}
+
+errorExit() {
+  doLog "ERROR $1"
+  exit 1
+}
+
+restore_db_from_local() {
+  SERVICE=$1
+  BACKUP="${BACKUPS_DIR}/${SERVICE}_backup"
+  LIST_FILE="${BACKUPS_DIR}/${SERVICE}_list"
+
+  doLog "INFO Restoring $SERVICE"
+  pg_restore -l "$BACKUP" | sed -e "s/\(.*0 0 COMMENT - EXTENSION plpgsql\)/;\1/" >"$LIST_FILE" # We have to drop a problematic COMMENT added by RDS backups.
+  pg_restore --clean --if-exists --host="$HOST" --port="$PORT" --dbname="$SERVICE" --username="$USERNAME" --use-list="$LIST_FILE" "$BACKUP" >>$LOGFILE 2>&1 || errorExit "Unable to restore ${SERVICE}"
+  doLog "INFO Succesfully restored ${SERVICE}"
+}
+
+run_azure_restore() {
+  mkdir -p "$BACKUPS_DIR"
+  azcopy login --identity >>$LOGFILE 2>&1 || errorExit "Could not login to Azure"
+  azcopy copy "$CLOUD_LOCATION"/* "$BACKUPS_DIR" >>$LOGFILE 2>&1 || errorExit "Could not copy backups from Azure."
+  restore_db_from_local "hive"
+  restore_db_from_local "ranger"
+  rm -rfv "$BACKUPS_DIR" >>$LOGFILE 2>&1
+}
+
+restore_db_from_s3() {
+  SERVICE="$1"
+  BACKUP="${BACKUPS_DIR}/${SERVICE}_backup"
+  LIST_FILE="${BACKUPS_DIR}/${SERVICE}_list"
+
+  doLog "INFO Copying ${CLOUD_LOCATION}/${SERVICE}_backup"
+  aws s3 cp "${CLOUD_LOCATION}/${SERVICE}_backup" "$BACKUPS_DIR" || errorExit "Could not copy $SERVICE backup from AWS S3."
+
+  doLog "INFO Restoring $SERVICE"
+  pg_restore -l "$BACKUP" | sed -e "s/\(.*0 0 COMMENT - EXTENSION plpgsql\)/;\1/" >"$LIST_FILE" # We have to drop a problematic COMMENT added by RDS backups.
+  pg_restore --clean --if-exists --host="$HOST" --port="$PORT" --dbname="$SERVICE" --username="$USERNAME" --use-list="$LIST_FILE" "$BACKUP" >>$LOGFILE 2>&1 || errorExit "Unable to restore ${SERVICE}"
+  doLog "INFO Succesfully restored ${SERVICE}"
+}
+
+run_aws_restore () {
+  mkdir -p "$BACKUPS_DIR"
+  restore_db_from_s3 "hive"
+  restore_db_from_s3 "ranger"
+  rm -rfv "$BACKUPS_DIR" >>$LOGFILE 2>&1
+}
+
+if [[ "$CLOUD_PROVIDER" == "azure" ]]; then
+  run_azure_restore
+elif [[ "$CLOUD_PROVIDER" == "aws" ]]; then
+  run_aws_restore
+else
+  errorExit "Unknown cloud provider: ${CLOUD_PROVIDER}"
+fi
+
+doLog "INFO Completed restore."
