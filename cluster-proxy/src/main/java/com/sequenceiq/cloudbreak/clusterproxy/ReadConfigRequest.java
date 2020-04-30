012893a71b7b17bff55066972cd576902e6a0d0a
Jamison Bennett <jamison.bennett@gmail.com>
2020-04-30 11:30:35 -0500

    CB-4377: Use cluster proxy HA for FreeIPA connections
    Use cluster proxy HA for FreeIPA connections. Configure cluster proxy
    to monitor the FreeIPA health using the login page. A better health
    check is being developed, but the login will at least allow cluster
    proxy to determine if FreeIPA is reachable.
    
    Use a sticky session to maintain the connection to a specific FreeIPA
    instance.
    
    When provisioning, wait for the HA endpoint provide a good status
    before continuing to provision.
    
    This was tested manually, by stopping nginx on one node and performing
    FreeIPA management service requests to FreeIPA. Then the first node
    was brought back up and the same thing was done after bringing the
    second node down.

---
+++ cluster-proxy/src/main/java/com/sequenceiq/cloudbreak/clusterproxy/ReadConfigRequest.java
@@ -0,0 +1,40 @@
+package com.sequenceiq.cloudbreak.clusterproxy;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ReadConfigRequest {
+    @JsonProperty
+    private String clusterCrn;
+
+    @JsonCreator
+    public ReadConfigRequest(String clusterCrn) {
+        this.clusterCrn = clusterCrn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ReadConfigRequest that = (ReadConfigRequest) o;
+
+        return Objects.equals(clusterCrn, that.clusterCrn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clusterCrn);
+    }
+
+    @Override
+    public String toString() {
+        return "ReadConfigRequest{clusterCrn='" + clusterCrn + '\'' + '}';
+    }
+}
