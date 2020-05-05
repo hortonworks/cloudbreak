1d4ab7eacd8614d86ed3e37d1e4430ae497d76a8
David Bajzath <bajzathd@gmail.com>
2020-05-05 14:51:18 +0200

    CB-6897 Optimize sql for instance lifecycle column addition

--- core/src/main/resources/schema/app/20200504164000_CB-6897_improve_instance_metadata_with_instance_lifecycle.sql
+++ core/src/main/resources/schema/app/20200504164000_CB-6897_improve_instance_metadata_with_instance_lifecycle.sql
@@ -1,7 +1,13 @@
 -- // CB-6897 Improve InstanceMetadata with instance lifecycle
 -- Migration SQL that makes the change goes here.
 
-ALTER TABLE instancemetadata ADD COLUMN IF NOT EXISTS lifecycle VARCHAR(255) DEFAULT 'NORMAL';
+ALTER TABLE instancemetadata ADD COLUMN IF NOT EXISTS lifecycle VARCHAR(255);
+
+ALTER TABLE instancemetadata ALTER lifecycle SET DEFAULT 'NORMAL';
+
+UPDATE instancemetadata
+  SET lifecycle = 'NORMAL'
+  WHERE lifecycle IS NULL;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
