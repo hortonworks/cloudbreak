bc6d313f54de549a06e3b50863fd3c968c2cdc3b
Peter Erdos <perdos@cloudera.com>
2020-05-06 09:12:37 +0200

    Revert "CB-4377: Use cluster proxy HA for FreeIPA connections"
    This reverts commit 730dfca45202dc11ffd9658a8dadf440a69b11c3.

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/ClusterProxyService.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/stack/ClusterProxyService.java
@@ -11,35 +11,26 @@ import javax.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.clusterproxy.ClientCertificate;
 import com.sequenceiq.cloudbreak.clusterproxy.ClusterProxyConfiguration;
 import com.sequenceiq.cloudbreak.clusterproxy.ClusterProxyRegistrationClient;
 import com.sequenceiq.cloudbreak.clusterproxy.ClusterServiceConfig;
-import com.sequenceiq.cloudbreak.clusterproxy.ClusterServiceHealthCheck;
 import com.sequenceiq.cloudbreak.clusterproxy.ConfigRegistrationRequestBuilder;
 import com.sequenceiq.cloudbreak.clusterproxy.ConfigRegistrationResponse;
 import com.sequenceiq.cloudbreak.clusterproxy.TunnelEntry;
 import com.sequenceiq.cloudbreak.common.json.JsonUtil;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
-import com.sequenceiq.cloudbreak.polling.PollingService;
 import com.sequenceiq.cloudbreak.service.secret.vault.VaultConfigException;
 import com.sequenceiq.cloudbreak.service.secret.vault.VaultSecret;
 import com.sequenceiq.common.api.type.Tunnel;
-import com.sequenceiq.freeipa.entity.FreeIpa;
 import com.sequenceiq.freeipa.entity.SecurityConfig;
 import com.sequenceiq.freeipa.entity.Stack;
-import com.sequenceiq.freeipa.repository.InstanceMetaDataRepository;
 import com.sequenceiq.freeipa.service.GatewayConfigService;
 import com.sequenceiq.freeipa.service.SecurityConfigService;
 import com.sequenceiq.freeipa.service.TlsSecurityService;
-import com.sequenceiq.freeipa.service.polling.clusterproxy.ServiceEndpointHealthListenerTask;
-import com.sequenceiq.freeipa.service.polling.clusterproxy.ServiceEndpointHealthPollerObject;
 import com.sequenceiq.freeipa.util.ClusterProxyServiceAvailabilityChecker;
-import com.sequenceiq.freeipa.service.config.FreeIpaDomainUtils;
-import com.sequenceiq.freeipa.service.freeipa.FreeIpaService;
 import com.sequenceiq.freeipa.vault.FreeIpaCertVaultComponent;
 
 @Service
@@ -58,26 +49,6 @@ public class ClusterProxyService {
     private static final String GATEWAY_SERVICE_TYPE = "GATEWAY";
 
     private static final String NGINX_PROTOCOL = "https";
-
-    private static final int MILLIS_PER_SEC = 1000;
-
-    @Value("${clusterProxy.intervalInSec}")
-    private int intervalInSec;
-
-    @Value("${clusterProxy.healthStatusEndpoint}")
-    private String healthStatusEndpoint;
-
-    @Value("${clusterProxy.timeoutInSec}")
-    private int timeoutInSec;
-
-    @Value("${clusterProxy.healthyStatusCode}")
-    private int healthyStatusCode;
-
-    @Value("${clusterProxy.maxAttempts}")
-    private int maxAttempts;
-
-    @Value("${clusterProxy.maxFailure}")
-    private int maxFailure;
 
     @Inject
     private StackService stackService;
@@ -103,31 +74,19 @@ public class ClusterProxyService {
     @Inject
     private SecurityConfigService securityConfigService;
 
-    @Inject
-    private InstanceMetaDataRepository instanceMetaDataRepository;
-
-    @Inject
-    private FreeIpaService freeIpaService;
-
-    @Inject
-    private PollingService<ServiceEndpointHealthPollerObject> serviceEndpointHealthPollingService;
-
-    @Inject
-    private ServiceEndpointHealthListenerTask serviceEndpointHealthListenerTask;
-
     public Optional<ConfigRegistrationResponse> registerFreeIpa(String accountId, String environmentCrn) {
-        return registerFreeIpa(stackService.getByEnvironmentCrnAndAccountId(environmentCrn, accountId), false, false);
+        return registerFreeIpa(stackService.getByEnvironmentCrnAndAccountId(environmentCrn, accountId), false);
     }
 
-    public Optional<ConfigRegistrationResponse> updateFreeIpaRegistrationAndWait(Long stackId) {
-        return registerFreeIpa(stackService.getStackById(stackId), false, true);
+    public Optional<ConfigRegistrationResponse> updateFreeIpaRegistration(Long stackId) {
+        return registerFreeIpa(stackService.getStackById(stackId), false);
     }
 
     public Optional<ConfigRegistrationResponse> registerBootstrapFreeIpa(Long stackId) {
-        return registerFreeIpa(stackService.getStackById(stackId), true, false);
+        return registerFreeIpa(stackService.getStackById(stackId), true);
     }
 
-    private Optional<ConfigRegistrationResponse> registerFreeIpa(Stack stack, boolean bootstrap, boolean waitForGoodHealth) {
+    private Optional<ConfigRegistrationResponse> registerFreeIpa(Stack stack, boolean bootstrap) {
 
         if (!clusterProxyConfiguration.isClusterProxyIntegrationEnabled()) {
             LOGGER.debug("Cluster Proxy integration disabled. Skipping registering FreeIpa [{}]", stack);
@@ -160,10 +119,6 @@ public class ClusterProxyService {
                 .withTunnelEntries(createTunnelEntries(stack, tunnelGatewayConfigs))
                 .withAccountId(stack.getAccountId());
         ConfigRegistrationResponse response = clusterProxyRegistrationClient.registerConfig(requestBuilder.build());
-
-        if (waitForGoodHealth) {
-            pollForGoodHealth(stack);
-        }
 
         stackUpdater.updateClusterProxyRegisteredFlag(stack, true);
 
@@ -214,21 +169,9 @@ public class ClusterProxyService {
 
     private List<ClusterServiceConfig> createDnsMappedServiceConfigs(Stack stack, List<GatewayConfig> gatewayConfigs, ClientCertificate clientCertificate,
             boolean usePrivateIpToTls) {
-        List<ClusterServiceConfig> serviceConfigs = gatewayConfigs.stream()
+        return gatewayConfigs.stream()
                 .map(gatewayConfig -> createServiceConfig(stack, gatewayConfig.getHostname(), gatewayConfig, clientCertificate, usePrivateIpToTls))
                 .collect(Collectors.toList());
-        List<String> endpoints = gatewayConfigs.stream()
-                .map(gatewayConfig -> getNginxEndpointForRegistration(stack, gatewayConfig, usePrivateIpToTls))
-                .collect(Collectors.toList());
-        FreeIpa freeIpa = freeIpaService.findByStack(stack);
-        serviceConfigs.add(new ClusterServiceConfig(FreeIpaDomainUtils.getFreeIpaFqdn(freeIpa.getDomain()),
-                endpoints,
-                List.of(),
-                clientCertificate,
-                NO_TLS_STRICT_CHECK,
-                new ClusterServiceHealthCheck(intervalInSec, healthStatusEndpoint, timeoutInSec, healthyStatusCode)
-        ));
-        return serviceConfigs;
     }
 
     private List<TunnelEntry> createTunnelEntries(Stack stack, List<GatewayConfig> gatewayConfigs) {
@@ -250,10 +193,12 @@ public class ClusterProxyService {
         return String.format("%s/proxy/%s/%s", clusterProxyConfiguration.getClusterProxyBasePath(), crn, FREEIPA_SERVICE_NAME);
     }
 
-    public String getProxyPath(Stack stack, Optional<String> serviceName) {
-        FreeIpa freeIpa = freeIpaService.findByStack(stack);
-        String proxyServiceName = serviceName.orElse(FreeIpaDomainUtils.getFreeIpaFqdn(freeIpa.getDomain()));
-        return String.format("%s/proxy/%s/%s", clusterProxyConfiguration.getClusterProxyBasePath(), stack.getResourceCrn(), proxyServiceName);
+    public String getProxyPath(Stack stack, String serviceName) {
+        if (ClusterProxyServiceAvailabilityChecker.isDnsBasedServiceNameAvailable(stack)) {
+            return String.format("%s/proxy/%s/%s", clusterProxyConfiguration.getClusterProxyBasePath(), stack.getResourceCrn(), serviceName);
+        } else {
+            return getProxyPath(stack.getResourceCrn());
+        }
     }
 
     private String vaultPath(String vaultSecretJsonString) {
@@ -282,12 +227,6 @@ public class ClusterProxyService {
             ipAddresss = gatewayConfig.getPrivateAddress();
         }
         return String.format("%s://%s:%d", NGINX_PROTOCOL, ipAddresss, stack.getGatewayport());
-    }
-
-    public void pollForGoodHealth(Stack stack) {
-        serviceEndpointHealthPollingService.pollWithTimeout(
-                serviceEndpointHealthListenerTask, new ServiceEndpointHealthPollerObject(stack.getResourceCrn(), clusterProxyRegistrationClient),
-                intervalInSec * MILLIS_PER_SEC, maxAttempts, maxFailure);
     }
 
 }
