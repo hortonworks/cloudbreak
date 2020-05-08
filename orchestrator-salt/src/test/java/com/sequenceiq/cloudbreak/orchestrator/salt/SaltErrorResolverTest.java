6a470281b41f95cf4d909c2d5ca270fe8fa08762
Oliver Szabo <oleewere@gmail.com>
2020-05-08 16:30:06 +0200

    CB-6920. Addendum: map salt script 'Name' to an actual error message

--- orchestrator-salt/src/test/java/com/sequenceiq/cloudbreak/orchestrator/salt/SaltErrorResolverTest.java
+++ orchestrator-salt/src/test/java/com/sequenceiq/cloudbreak/orchestrator/salt/SaltErrorResolverTest.java
@@ -21,13 +21,14 @@ class SaltErrorResolverTest {
         originalErrors.put("node1", "Error4");
         originalErrors.put("node2", "Error5");
         originalErrors.put("node2", "Error6");
-        originalErrors.put("node2", "Name: /usr/local/bin/freeipa_backup");
+        originalErrors.put("node2", "Name: sh /etc/td-agent/check_fluent_plugins.sh");
 
         Multimap<String, String> resolvedMessages = saltErrorResolver.resolveErrorMessages(originalErrors);
         assertThat(resolvedMessages.get("node1"), containsInAnyOrder("Error1", "Failed to upload freeipa backup to object storage, " +
                 "please check the assigned permissions to the instance", "Error3", "Error4"));
-        assertThat(resolvedMessages.get("node2"), containsInAnyOrder("Error5", "Error6", "Failed to upload freeipa backup to object storage, " +
-                "please check the assigned permissions to the instance"));
+        assertThat(resolvedMessages.get("node2"), containsInAnyOrder("Error5", "Error6", "Connection has failed for " +
+                "\"https://repository.cloudera.com/cloudera/api/gems/cloudera-gems/\" or \"https://api.rubygems.org\". " +
+                "If servers are not down or no network: please check your firewall or use a newer OS image with newer fluent plugin versions!"));
     }
 
 }
