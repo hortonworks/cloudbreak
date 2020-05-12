d1f6e1a34ed7463ce2eb1d44c47ab4dcae700661
bbihari <bbihari@hortonworks.com>
2020-05-12 16:06:19 +0200

    CB-6914 Salt error in termination phase

---
+++ orchestrator-salt/src/main/java/com/sequenceiq/cloudbreak/orchestrator/salt/utils/GrainsJsonPropertyUtil.java
@@ -0,0 +1,27 @@
+package com.sequenceiq.cloudbreak.orchestrator.salt.utils;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.IteratorUtils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class GrainsJsonPropertyUtil {
+
+    private GrainsJsonPropertyUtil() {
+    }
+
+    public static Set<String> getPropertySet(JsonNode grainsProperty) {
+        return Optional.ofNullable(grainsProperty)
+                .map(JsonNode::elements)
+                .map(IteratorUtils::toList)
+                .map(jsonNodeList -> (Set<String>) jsonNodeList.stream()
+                        .map(item -> ((JsonNode) item).asText())
+                        .collect(Collectors.toSet())
+                ).orElse(Collections.emptySet());
+    }
+
+}
