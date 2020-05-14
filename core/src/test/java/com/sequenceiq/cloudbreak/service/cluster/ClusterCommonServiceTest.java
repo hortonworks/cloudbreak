3589462e03a708693ef8332bbb7f1c6fc4d08ce8
Attila Kanto <attila.kanto@gmail.com>
2020-05-14 16:12:20 +0200

    CB-7025 Fix ansible to help automation of repair testing

--- core/src/test/java/com/sequenceiq/cloudbreak/service/cluster/ClusterCommonServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/cluster/ClusterCommonServiceTest.java
@@ -1,14 +1,17 @@
 package com.sequenceiq.cloudbreak.service.cluster;
 
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.DetailedStackStatus.AVAILABLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +32,7 @@ import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
 import com.sequenceiq.cloudbreak.exception.NotFoundException;
 import com.sequenceiq.cloudbreak.service.ClusterCommonService;
 import com.sequenceiq.cloudbreak.service.environment.EnvironmentService;
+import com.sequenceiq.cloudbreak.service.stack.InstanceMetaDataService;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.environment.api.v1.environment.model.response.EnvironmentStatus;
 
@@ -44,35 +48,59 @@ public class ClusterCommonServiceTest {
     @Mock
     private EnvironmentService environmentService;
 
+    @Mock
+    private InstanceMetaDataService instanceMetaDataService;
+
     @BeforeEach
     public void setUp() {
     }
 
     @Test
-    public void testIniFileGeneration() {
+    public void testInventoryFileGeneration() {
         // GIVEN
 
         Stack stack = new Stack();
+        stack.setId(1L);
         Cluster cluster = new Cluster();
         cluster.setId(1L);
         cluster.setName("cl1");
         cluster.setClusterManagerIp("gatewayIP");
         stack.setCluster(cluster);
-        stack.setInstanceGroups(generateInstanceMetadata());
+
+        when(instanceMetaDataService.getAllInstanceMetadataByStackId(anyLong())).thenReturn(generateInstanceMetadata());
 
         // WHEN
         String result = underTest.getHostNamesAsIniString(stack, "cloudbreak");
         // THEN
-        assertTrue(result.contains("[server]\ngatewayIP\n\n"));
-        assertTrue(result.contains("[cluster]\nname=cl1\n\n"));
-        assertTrue(result.contains("[master]\nh1\n"));
-        assertTrue(result.contains("[agent]\n"));
-        assertTrue(result.contains("[all:vars]\nansible_ssh_user=cloudbreak\n"));
+
+        assertEquals("[cluster]\n" +
+                "name=cl1\n" +
+                "\n" +
+                "[server]\n" +
+                "gatewayIP\n" +
+                "\n" +
+                "[worker]\n" +
+                "worker-1\n" +
+                "pub-worker-2\n" +
+                "\n" +
+                "[master]\n" +
+                "m1\n" +
+                "\n" +
+                "[agent]\n" +
+                "m1\n" +
+                "worker-1\n" +
+                "pub-worker-2\n" +
+                "\n" +
+                "[all:vars]\n" +
+                "ansible_ssh_user=cloudbreak\n" +
+                "ansible_ssh_common_args='-o StrictHostKeyChecking=no'\n" +
+                "ansible_become=yes\n", result);
     }
 
     @Test
-    public void testIniFileGenerationWithoutAgents() {
+    public void testInventoryFileGenerationWithoutAgents() {
         Stack stack = new Stack();
+        stack.setId(1L);
         Cluster cluster = new Cluster();
         cluster.setId(1L);
         cluster.setName("cl1");
@@ -80,38 +108,43 @@ public class ClusterCommonServiceTest {
         stack.setCluster(cluster);
 
         // WHEN
+        when(instanceMetaDataService.getAllInstanceMetadataByStackId(anyLong())).thenReturn(Collections.emptySet());
         assertThrows(NotFoundException.class, () -> underTest.getHostNamesAsIniString(stack, "cloudbreak"));
     }
 
-    private Set<InstanceGroup> generateInstanceMetadata() {
-        Set<InstanceGroup> instanceGroups = new HashSet<>();
+    private Set<InstanceMetaData> generateInstanceMetadata() {
+        //LinkedHashSet required to keep the order, and have a reliable test run
+        Set<InstanceMetaData> instanceMetaData = new LinkedHashSet<>();
         InstanceGroup master = new InstanceGroup();
         master.setGroupName("master");
         InstanceGroup worker = new InstanceGroup();
         worker.setGroupName("worker");
 
         InstanceMetaData master1 = new InstanceMetaData();
-        master1.setDiscoveryFQDN("h1");
+        master1.setPublicIp("m1");
         master1.setInstanceGroup(master);
         master1.setInstanceStatus(InstanceStatus.SERVICES_HEALTHY);
+        instanceMetaData.add(master1);
 
         master.setInstanceMetaData(new HashSet<>(Arrays.asList(master1)));
 
         InstanceMetaData worker1 = new InstanceMetaData();
-        worker1.setDiscoveryFQDN("worker-1");
+        worker1.setPrivateIp("worker-1");
         worker1.setInstanceGroup(worker);
         worker1.setInstanceStatus(InstanceStatus.SERVICES_HEALTHY);
+        instanceMetaData.add(worker1);
 
         InstanceMetaData worker2 = new InstanceMetaData();
-        worker2.setDiscoveryFQDN("worker-2");
+        // If we have both then we need public ip
+        worker2.setPublicIp("pub-worker-2");
+        worker2.setPrivateIp("worker-2");
         worker2.setInstanceGroup(worker);
         worker2.setInstanceStatus(InstanceStatus.SERVICES_HEALTHY);
 
         worker.setInstanceMetaData(new HashSet<>(Arrays.asList(worker1, worker2)));
+        instanceMetaData.add(worker2);
 
-        instanceGroups.add(master);
-        instanceGroups.add(worker);
-        return instanceGroups;
+        return instanceMetaData;
     }
 
     @Test
