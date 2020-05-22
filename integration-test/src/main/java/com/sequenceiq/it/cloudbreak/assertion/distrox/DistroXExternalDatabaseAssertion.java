2d3e529470ea078693da0e4896bf14d7d04251c7
Meszaros Gergely <grigorij9026@gmail.com>
2020-05-22 12:27:19 +0200

    CB-7143 - DistroX should validated with DB but not in imagevalidator test

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/assertion/distrox/DistroXExternalDatabaseAssertion.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/assertion/distrox/DistroXExternalDatabaseAssertion.java
@@ -1,25 +1,25 @@
 package com.sequenceiq.it.cloudbreak.assertion.distrox;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.sequenceiq.it.cloudbreak.CloudbreakClient;
 import com.sequenceiq.it.cloudbreak.RedbeamsClient;
-import com.sequenceiq.it.cloudbreak.actor.CloudbreakUser;
 import com.sequenceiq.it.cloudbreak.assertion.Assertion;
 import com.sequenceiq.it.cloudbreak.assertion.BaseMicroserviceClientDependentAssertion;
+import com.sequenceiq.it.cloudbreak.context.RunningParameter;
 import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
 import com.sequenceiq.redbeams.api.endpoint.v4.databaseserver.responses.DatabaseServerV4Response;
 
-public class DistroXExternalDatabaseAssertion extends BaseMicroserviceClientDependentAssertion {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class DistroXExternalDatabaseAssertion extends BaseMicroserviceClientDependentAssertion {
 
     private DistroXExternalDatabaseAssertion() {
     }
 
-    public static Assertion<DistroXTestDto, CloudbreakClient> validateTemplateContainsExternalDatabaseHostname(CloudbreakUser actor) {
+    public static Assertion<DistroXTestDto, CloudbreakClient> validateTemplateContainsExternalDatabaseHostname() {
         return (testContext, distroXTestDto, cloudbreakClient) -> {
             String extendedBlueprint = distroXTestDto.getResponse().getCluster().getExtendedBlueprintText();
             String databaseServerCrn = distroXTestDto.getResponse().getCluster().getDatabaseServerCrn();
-            RedbeamsClient redbeamsClient = getClient(testContext, actor, RedbeamsClient.class);
+            RedbeamsClient redbeamsClient = getClient(testContext, testContext.getWho(RunningParameter.emptyRunningParameter()), RedbeamsClient.class);
             DatabaseServerV4Response databaseServer = redbeamsClient.getEndpoints()
                     .databaseServerV4Endpoint()
                     .getByCrn(databaseServerCrn);
--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXEncryptedVolumeTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXEncryptedVolumeTest.java
@@ -1,23 +1,29 @@
 package com.sequenceiq.it.cloudbreak.testcase.e2e.distrox;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.testng.annotations.Test;
-
+import com.sequenceiq.distrox.api.v1.distrox.model.database.DistroXDatabaseAvailabilityType;
+import com.sequenceiq.it.TestParameter;
 import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.externaldatabase.DistroXExternalDatabaseTestDto;
 import com.sequenceiq.it.cloudbreak.dto.distrox.instancegroup.DistroXInstanceGroupTestDto;
 import com.sequenceiq.it.cloudbreak.dto.distrox.instancegroup.DistroXInstanceGroupsBuilder;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.AbstractE2ETest;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static com.sequenceiq.it.cloudbreak.assertion.distrox.DistroXExternalDatabaseAssertion.validateTemplateContainsExternalDatabaseHostname;
 
 public class DistroXEncryptedVolumeTest extends AbstractE2ETest {
 
     @Inject
     private DistroXTestClient distroXTestClient;
+
+    @Inject
+    private TestParameter testParameter;
 
     @Override
     protected void setupTest(TestContext testContext) {
@@ -34,18 +40,24 @@ public class DistroXEncryptedVolumeTest extends AbstractE2ETest {
             when = "a valid DistroX create request is sent with encrypted discs",
             then = "DistroX cluster is created")
     public void testCreateDistroXWithEncryptedVolume(TestContext testContext) {
+        String externalDatabaseName = resourcePropertyProvider().getName();
         List<DistroXInstanceGroupTestDto> distroXInstanceGroupTestDtos = new DistroXInstanceGroupsBuilder(testContext)
                 .defaultHostGroup()
                 .withDiskEncryption()
                 .build();
-        testContext.given(DistroXTestDto.class)
+        testContext.given(externalDatabaseName, DistroXExternalDatabaseTestDto.class)
+                .withAvailabilityType(DistroXDatabaseAvailabilityType.NON_HA)
+                .given(DistroXTestDto.class)
                 .withInstanceGroupsEntity(distroXInstanceGroupTestDtos)
+                .withExternalDatabase(externalDatabaseName)
                 .when(distroXTestClient.create())
                 .await(STACK_AVAILABLE)
+                .then(validateTemplateContainsExternalDatabaseHostname())
                 .then((context, distrox, client) -> {
                     distrox.getResponse();
                     return distrox;
                 })
                 .validate();
     }
+
 }
