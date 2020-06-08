54f84b43012545f70d47d4d835db5bc7020eb6f7
Peter Erdos <perdos@cloudera.com>
2020-06-08 15:41:38 +0200

    CB-7139 failed events "from" methods are incorrect

--- datalake/src/main/java/com/sequenceiq/datalake/flow/create/event/SdxCreateFailedEvent.java
+++ datalake/src/main/java/com/sequenceiq/datalake/flow/create/event/SdxCreateFailedEvent.java
@@ -1,11 +1,16 @@
 package com.sequenceiq.datalake.flow.create.event;
 
+import com.sequenceiq.datalake.flow.SdxEvent;
 import com.sequenceiq.datalake.flow.SdxFailedEvent;
 
 public class SdxCreateFailedEvent extends SdxFailedEvent {
 
     public SdxCreateFailedEvent(Long sdxId, String userId, Exception exception) {
         super(sdxId, userId, exception);
+    }
+
+    public static SdxCreateFailedEvent from(SdxEvent event, Exception exception) {
+        return new SdxCreateFailedEvent(event.getResourceId(), event.getUserId(), exception);
     }
 
     @Override
--- datalake/src/main/java/com/sequenceiq/datalake/flow/stop/event/SdxStopFailedEvent.java
+++ datalake/src/main/java/com/sequenceiq/datalake/flow/stop/event/SdxStopFailedEvent.java
@@ -1,11 +1,16 @@
 package com.sequenceiq.datalake.flow.stop.event;
 
+import com.sequenceiq.datalake.flow.SdxEvent;
 import com.sequenceiq.datalake.flow.SdxFailedEvent;
 
 public class SdxStopFailedEvent extends SdxFailedEvent {
 
     public SdxStopFailedEvent(Long sdxId, String userId, Exception exception) {
         super(sdxId, userId, exception);
+    }
+
+    public static SdxStopFailedEvent from(SdxEvent event, Exception exception) {
+        return new SdxStopFailedEvent(event.getResourceId(), event.getUserId(), exception);
     }
 
     @Override
