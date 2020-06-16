6cb54a29f682efaf6d39dd7a57c77b06a088db7f
ganeshk <ganeshk@cloudera.com>
2020-06-16 16:05:42 -0700

    DISTX-421 Separate endpoint for autoscale and resize recommendation
    1. Platform recommendation endpoint has lots of details.
    2. So creating a new api endpoint just to deal with scaling recommendations.
    
    ./gradlew build

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/BlueprintUtilV4Endpoint.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/blueprint/BlueprintUtilV4Endpoint.java
@@ -2,6 +2,8 @@ package com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint;
 
 import java.util.Set;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,6 +17,7 @@ import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.GeneratedCm
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.RecommendationV4Response;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.ServiceDependencyMatrixV4Response;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.blueprint.responses.SupportedVersionsV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.connector.responses.ScaleRecommendationV4Response;
 import com.sequenceiq.cloudbreak.doc.ControllerDescription;
 import com.sequenceiq.cloudbreak.doc.Notes;
 import com.sequenceiq.cloudbreak.doc.OperationDescriptions.ConnectorOpDescription;
@@ -66,6 +69,15 @@ public interface BlueprintUtilV4Endpoint {
             @QueryParam("platformVariant") String platformVariant,
             @QueryParam("availabilityZone") String availabilityZone,
             @QueryParam("resourceType") CdpResourceType resourceType);
+
+    @GET
+    @Path("scalerecommendation")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = ConnectorOpDescription.GET_RECOMMENDATION, produces = MediaType.APPLICATION_JSON, notes = Notes.CONNECTOR_NOTES,
+            nickname = "createScaleRecommendationForWorkspace")
+    ScaleRecommendationV4Response createRecommendation(
+            @PathParam("workspaceId") @Valid Long workspaceId,
+            @QueryParam("blueprintName") @NotEmpty String blueprintName);
 
     @GET
     @Path("service_versions")
