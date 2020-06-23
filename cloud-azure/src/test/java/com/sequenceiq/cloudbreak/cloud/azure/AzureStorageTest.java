44e220f247c787b0b7c05ac1ccd83bd6714d7fc2
Peter Darvasi <pdarvasi@hortonworks.com>
2020-06-23 17:28:14 +0200

    CB-7632 - Make sure Storage Account name does not change prior to CB 2.20

--- cloud-azure/src/test/java/com/sequenceiq/cloudbreak/cloud/azure/AzureStorageTest.java
+++ cloud-azure/src/test/java/com/sequenceiq/cloudbreak/cloud/azure/AzureStorageTest.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -15,6 +17,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.microsoft.azure.PagedList;
 import com.microsoft.azure.management.resources.Subscription;
@@ -23,6 +26,13 @@ import com.microsoft.azure.management.storage.StorageAccount;
 import com.microsoft.azure.management.storage.implementation.StorageAccountInner;
 import com.sequenceiq.cloudbreak.cloud.azure.client.AzureClient;
 import com.sequenceiq.cloudbreak.cloud.azure.storage.SkuTypeResolver;
+import com.sequenceiq.cloudbreak.cloud.azure.view.AzureCredentialView;
+import com.sequenceiq.cloudbreak.cloud.context.CloudContext;
+import com.sequenceiq.cloudbreak.cloud.model.AvailabilityZone;
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
+import com.sequenceiq.cloudbreak.cloud.model.CloudStack;
+import com.sequenceiq.cloudbreak.cloud.model.Location;
+import com.sequenceiq.cloudbreak.cloud.model.Region;
 
 public class AzureStorageTest {
 
@@ -34,11 +44,23 @@ public class AzureStorageTest {
 
     private static final String SUBSCRIPTION_ANOTHER = "subscriptionAnother";
 
+    private static final String STACK_NAME = "Test Cluster";
+
+    private static final Region REGION = Region.region("westus2");
+
+    private static final String RESOURCE_GROUP = "resource-group";
+
     @Mock
     private AzureUtils armUtils;
 
     @Mock
     private SkuTypeResolver skuTypeResolver;
+
+    @Mock
+    private CloudStack cloudStack;
+
+    @Mock
+    private AzureResourceGroupMetadataProvider azureResourceGroupMetadataProvider;
 
     @InjectMocks
     private AzureStorage underTest;
@@ -83,6 +105,34 @@ public class AzureStorageTest {
         verify(client).listSubscriptions();
     }
 
+    @Test
+    public void testImageStorageNameForSingleRG() {
+        ReflectionTestUtils.setField(underTest, "imageStorePrefix", "cbimg");
+
+        CloudCredential cloudCredential = createCloudCredential();
+        AzureCredentialView azureCredentialView = new AzureCredentialView(cloudCredential);
+        CloudContext cloudContext = createCloudContext();
+
+        when(azureResourceGroupMetadataProvider.useSingleResourceGroup(cloudStack)).thenReturn(true);
+        when(azureResourceGroupMetadataProvider.getResourceGroupName(cloudContext, cloudStack)).thenReturn(RESOURCE_GROUP);
+
+        String imageStorageName = underTest.getImageStorageName(azureCredentialView, cloudContext, cloudStack);
+        assertEquals("cbimgwu2338906682b8305c3", imageStorageName);
+    }
+
+    @Test
+    public void testImageStorageNameForNonSingleRG() {
+        ReflectionTestUtils.setField(underTest, "imageStorePrefix", "cbimg");
+        CloudCredential cloudCredential = createCloudCredential();
+        AzureCredentialView azureCredentialView = new AzureCredentialView(cloudCredential);
+        CloudContext cloudContext = createCloudContext();
+
+        when(azureResourceGroupMetadataProvider.useSingleResourceGroup(cloudStack)).thenReturn(false);
+
+        String imageStorageName = underTest.getImageStorageName(azureCredentialView, cloudContext, cloudStack);
+        assertEquals("cbimgwu2subscriptionone", imageStorageName);
+    }
+
     private void setupSubscriptions(AzureClient client) {
         PagedList<Subscription> subscriptionPagedList = Mockito.spy(PagedList.class);
 
@@ -95,5 +145,18 @@ public class AzureStorageTest {
         subscriptionPagedList.add(subscriptionTwo);
 
         when(client.listSubscriptions()).thenReturn(subscriptionPagedList);
+    }
+
+    private CloudCredential createCloudCredential() {
+        Map<String, String> azureParameters = new HashMap<>();
+        azureParameters.put("subscriptionId", SUBSCRIPTION_ONE);
+        CloudCredential cloudCredential = new CloudCredential("id", "name");
+        cloudCredential.putParameter("azure", azureParameters);
+        return cloudCredential;
+    }
+
+    private CloudContext createCloudContext() {
+        Location location = Location.location(REGION, AvailabilityZone.availabilityZone("westus2"));
+        return new CloudContext(null, STACK_NAME, null, null, location, null, null, "");
     }
 }
