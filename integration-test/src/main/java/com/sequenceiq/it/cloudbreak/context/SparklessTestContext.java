b39e0ed7c1c632e1ecd688f191befd54332898c1
David Bajzath <bajzathd@gmail.com>
2020-06-29 15:15:41 +0200

    CB-7517 Test name tag only matters in non-mock e2e tests

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/context/SparklessTestContext.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/context/SparklessTestContext.java
@@ -1,10 +1,23 @@
 package com.sequenceiq.it.cloudbreak.context;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Primary;
 
 import com.sequenceiq.it.cloudbreak.Prototype;
+import com.sequenceiq.it.cloudbreak.exception.TestMethodNameMissingException;
 
 @Prototype
 @Primary
 public class SparklessTestContext extends TestContext {
+
+    /**
+     * In non-mock tests we use the test method name as a tag, so we must be sure that is is there
+     */
+    @Override
+    public Optional<String> getTestMethodName() {
+        Optional<String> testMethodName = super.getTestMethodName();
+        testMethodName.orElseThrow(TestMethodNameMissingException::new);
+        return testMethodName;
+    }
 }
