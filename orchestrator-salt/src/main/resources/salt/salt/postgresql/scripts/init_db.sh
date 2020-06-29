63141c853f64aa329192a1d20b17859d43166094
Krisztian Horvath <keyki.kk@gmail.com>
2020-06-29 18:30:36 +0200

    CB-7733 allow remote connections for postgres user

--- orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/init_db.sh
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/scripts/init_db.sh
@@ -14,13 +14,17 @@ echo "CREATE USER {{ values['user'] }} WITH PASSWORD '{{ values['password'] }}';
 echo "GRANT ALL PRIVILEGES ON DATABASE {{ values['database'] }} TO {{ values['user'] }};" | psql -U postgres -v "ON_ERROR_STOP=1"
 echo "ALTER SCHEMA public OWNER TO {{ values['user'] }};" | psql -U postgres -d {{ values['database'] }} -v "ON_ERROR_STOP=1"
 
-echo "Add access to pg_hba.conf"
-sed -i '1ihost {{ values['database'] }} {{ values['user'] }} 0.0.0.0/0 md5' $CONFIG_DIR/pg_hba.conf
-sed -i '1ilocal {{ values['database'] }} {{ values['user'] }} md5' $CONFIG_DIR/pg_hba.conf
 echo $(date +%Y-%m-%d:%H:%M:%S) >> $CONFIG_DIR/init_{{ service }}_db_executed
 
 {% endif %}
 
 {% endfor %}
+
+# Allow connections with all user remotely
+sed -i '1ihost all all 0.0.0.0/0 md5' $CONFIG_DIR/pg_hba.conf
+sed -i '1ilocal all all md5' $CONFIG_DIR/pg_hba.conf
+
+# Set postgres password
+echo "ALTER USER postgres WITH PASSWORD 'cloudera'" | psql -U postgres -v "ON_ERROR_STOP=1"
 
 set +e
