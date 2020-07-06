333c202c7980f721e5c13677e0cffe68ec17bc57
David Bajzath <bajzathd@gmail.com>
2020-07-06 11:18:30 +0200

    CB-7803 Disable retry of failed test cases on spot instances

---
+++ integration-test/src/test/java/com/sequenceiq/it/cloudbreak/util/spot/SpotRetryUtilTest.java
@@ -0,0 +1,70 @@
+package com.sequenceiq.it.cloudbreak.util.spot;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.lang.reflect.Method;
+
+import org.mockito.Mockito;
+import org.testng.ITestNGMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.internal.ConstructorOrMethod;
+
+public class SpotRetryUtilTest {
+
+    private Method method;
+
+    private SpotUtil spotUtil;
+
+    private ITestNGMethod testMethod;
+
+    private SpotRetryUtil underTest;
+
+    @BeforeMethod
+    public void setUp() throws NoSuchMethodException {
+        ConstructorOrMethod constructorOrMethod = Mockito.mock(ConstructorOrMethod.class);
+        method = ExampleTest.class.getDeclaredMethod("exampleMethod");
+        when(constructorOrMethod.getMethod()).thenReturn(method);
+        testMethod = Mockito.mock(ITestNGMethod.class);
+        when(testMethod.getConstructorOrMethod()).thenReturn(constructorOrMethod);
+
+        spotUtil = Mockito.mock(SpotUtil.class);
+        underTest = new SpotRetryUtil(spotUtil, true);
+    }
+
+    @Test
+    void shouldRetryOnFirstTryButNotOnSecondWhenShouldUseSpotInstances() {
+        when(spotUtil.shouldUseSpotInstancesForTest(method)).thenReturn(true);
+
+        // first try - is not retried, and it will retry
+        assertFalse(underTest.isRetried(testMethod));
+        assertTrue(underTest.willRetry(testMethod));
+
+        // second try - retried, and it will not retry
+        assertTrue(underTest.isRetried(testMethod));
+        assertFalse(underTest.willRetry(testMethod));
+    }
+
+    @Test
+    void shouldNotRetryWhenShouldUseSpotInstancesIsFalse() {
+        when(spotUtil.shouldUseSpotInstancesForTest(method)).thenReturn(false);
+
+        boolean result = underTest.willRetry(testMethod);
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldNotRetryWhenRetryEnabledIsFalse() {
+        underTest = new SpotRetryUtil(spotUtil, true);
+
+        boolean result = underTest.willRetry(testMethod);
+        assertFalse(result);
+    }
+
+    private static class ExampleTest {
+        private void exampleMethod() {
+        }
+    }
+}
