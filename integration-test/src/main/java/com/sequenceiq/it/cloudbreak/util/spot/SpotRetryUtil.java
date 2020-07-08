e28213b5791e0819f4d654985b140da8662af88f
Bajzáth Dávid <Bajzathd@users.noreply.github.com>
2020-07-08 15:00:30 +0200

    CB-7803 Disable retry of failed test cases on spot instances (#8505)

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/spot/SpotRetryUtil.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/spot/SpotRetryUtil.java
@@ -3,6 +3,7 @@ package com.sequenceiq.it.cloudbreak.util.spot;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.testng.ITestNGMethod;
 
@@ -13,12 +14,16 @@ public class SpotRetryUtil {
 
     private final SpotUtil spotUtil;
 
-    public SpotRetryUtil(SpotUtil spotUtil) {
+    private final boolean retryEnabled;
+
+    public SpotRetryUtil(SpotUtil spotUtil, @Value("${integrationtest.spot.retryEnabled:false}") boolean retryEnabled) {
         this.spotUtil = spotUtil;
+        this.retryEnabled = retryEnabled;
     }
 
     public boolean willRetry(ITestNGMethod testMethod) {
-        return spotUtil.shouldUseSpotInstancesForTest(testMethod.getConstructorOrMethod().getMethod())
+        return retryEnabled
+                && spotUtil.shouldUseSpotInstancesForTest(testMethod.getConstructorOrMethod().getMethod())
                 && RETRIED_TEST_METHODS.add(testMethod);
     }
 
