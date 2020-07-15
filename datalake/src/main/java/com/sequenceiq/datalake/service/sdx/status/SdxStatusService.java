42573207056bb9d29c0aa1117d2c7978ba15df8a
Peter Erdos <perdos@cloudera.com>
2020-07-15 19:15:23 +0200

    CB-7933 Do not fail on sdx status update in Create and Delete action failurehandler
    On prod I realized lot of flows are running and most of them are very old. These flows stuck, in SdxCreateActions and SdxDeleteActions the failure handlers could throw exception if sdx cluster is terminated at status save. This problem now handled and I introduced some improvement in flow error handling. It should terminate flow fails like these.

--- datalake/src/main/java/com/sequenceiq/datalake/service/sdx/status/SdxStatusService.java
+++ datalake/src/main/java/com/sequenceiq/datalake/service/sdx/status/SdxStatusService.java
@@ -1,6 +1,6 @@
 package com.sequenceiq.datalake.service.sdx.status;
 
-import static com.sequenceiq.cloudbreak.exception.NotFoundException.notFoundException;
+import static com.sequenceiq.cloudbreak.common.exception.NotFoundException.notFoundException;
 import static com.sequenceiq.datalake.entity.DatalakeStatusEnum.DELETED;
 
 import java.util.Collection;
@@ -15,10 +15,10 @@ import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.StackV4Response;
 import com.sequenceiq.cloudbreak.cloud.scheduler.PollGroup;
-import com.sequenceiq.cloudbreak.common.exception.NotFoundException;
 import com.sequenceiq.cloudbreak.common.service.Clock;
 import com.sequenceiq.cloudbreak.common.service.TransactionService;
 import com.sequenceiq.cloudbreak.event.ResourceEvent;
+import com.sequenceiq.cloudbreak.common.exception.NotFoundException;
 import com.sequenceiq.datalake.entity.DatalakeStatusEnum;
 import com.sequenceiq.datalake.entity.SdxCluster;
 import com.sequenceiq.datalake.entity.SdxStatusEntity;
