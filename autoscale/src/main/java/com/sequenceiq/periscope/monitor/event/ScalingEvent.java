d63d00754909cd36e2c062add676626216b7e236
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-07-20 15:03:59 +0200

    CB-7923 MetricEvaluator should send alerts in batch

--- autoscale/src/main/java/com/sequenceiq/periscope/monitor/event/ScalingEvent.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/monitor/event/ScalingEvent.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.periscope.monitor.event;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationEvent;
 
 import com.sequenceiq.periscope.domain.BaseAlert;
@@ -7,11 +9,15 @@ import com.sequenceiq.periscope.domain.BaseAlert;
 public class ScalingEvent extends ApplicationEvent {
 
     public ScalingEvent(BaseAlert alert) {
-        super(alert);
+        this(List.of(alert));
     }
 
-    public BaseAlert getAlert() {
-        return (BaseAlert) getSource();
+    public ScalingEvent(List<? extends BaseAlert> alerts) {
+        super(alerts);
+    }
+
+    public List<BaseAlert> getAlerts() {
+        return (List<BaseAlert>) getSource();
     }
 
 }
