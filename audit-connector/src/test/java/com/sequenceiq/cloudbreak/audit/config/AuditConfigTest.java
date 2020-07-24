a41b1c64157fd30a49ee845c78f83a068e2d6391
Dénes Berger <dberger@cloudbreak.com>
2020-07-24 09:07:19 +0200

    CB-8126 Hard fail if audit endpoint configuration is missing
    This configuration item has no default value and it was not asserted before.
    Upon audit service call, the endpoint and port was always used and in case it was null, an NPE
    was raised by the GRPC client library.
    Now a check is implemented so if the auditig is enabled but the endpoint is missing,
    an IllegalStateException is thrown.

--- audit-connector/src/test/java/com/sequenceiq/cloudbreak/audit/config/AuditConfigTest.java
+++ audit-connector/src/test/java/com/sequenceiq/cloudbreak/audit/config/AuditConfigTest.java
@@ -2,25 +2,43 @@ package com.sequenceiq.cloudbreak.audit.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.util.ReflectionUtils;
 
+import com.sequenceiq.cloudbreak.structuredevent.conf.StructuredEventEnablementConfig;
+
+@ExtendWith(MockitoExtension.class)
 class AuditConfigTest {
+
+    @Mock
+    private StructuredEventEnablementConfig structuredEventEnablementConfig;
+
+    @InjectMocks
+    private AuditConfig underTest;
 
     // @formatter:off
     // CHECKSTYLE:OFF
     static Object[][] scenarios() {
         return new Object[][]{
-                // testCaseName      endpoint                  valid  configured   host         port  exception
-                { "null endpoint",   null,                     true,  false,       null,        null, null },
-                { "blank endpoint",  "   ",                    true,  false,       null,        null, null },
-                { "host only",       "hostname",               true,  true,        "hostname",  8982,   null },
-                { "host:port",       "hostname:1234",          true,  true,        "hostname",  1234, null },
-                { "host:port:extra", "hostname:1234:whatelse", false, false,       null,        null, IllegalArgumentException.class },
+                // testCaseName      endpoint                            valid  enabled  configured   host         port  exception
+                { "null endpoint, disabled",   null,                     true,  false,   false,       null,        null, null },
+                { "blank endpoint, disabled",  "   ",                    true,  false,   false,       null,        null, null },
+                { "null endpoint, enabled",    null,                     false, true,    false,       null,        null, IllegalStateException.class },
+                { "blank endpoint, enabled",   "   ",                    false, true,    false,       null,        null, IllegalStateException.class },
+                { "host only",                 "hostname",               true,  true,    true,        "hostname",  8982, null },
+                { "host:port",                 "hostname:1234",          true,  true,    true,        "hostname",  1234, null },
+                { "host:port:extra",           "hostname:1234:whatelse", false, true,    false,       null,        null, IllegalArgumentException.class },
         };
     }
     // CHECKSTYLE:ON
@@ -28,11 +46,15 @@ class AuditConfigTest {
 
     @ParameterizedTest(name = "{0}")
     @MethodSource("scenarios")
-    void init(String testName, String endpoint, boolean valid, boolean configured, String host, Integer port, Class<? extends Exception> exceptionClass) {
-        AuditConfig underTest = new AuditConfig();
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void init(String testName, String endpoint, boolean valid, boolean enabled, boolean configured,
+            String host, Integer port, Class<? extends Exception> exceptionClass) {
+
         Field endpointField = ReflectionUtils.findField(AuditConfig.class, "endpoint");
         ReflectionUtils.makeAccessible(endpointField);
         ReflectionUtils.setField(endpointField, underTest, endpoint);
+        when(structuredEventEnablementConfig.isAuditServiceEnabled()).thenReturn(enabled);
+
         if (valid) {
             underTest.init();
             if (configured) {
