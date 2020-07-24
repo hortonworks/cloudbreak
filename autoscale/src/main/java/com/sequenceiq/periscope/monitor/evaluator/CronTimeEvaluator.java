1df6c744bf8840954f7ceb555b48c9341b3eee99
smaniraju <40003156+smaniraju@users.noreply.github.com>
2020-07-24 05:40:01 +0530

    CB-8088 Schedule-Based downscaling integrated with Yarn Downscale recommendations
    Schedule-Based Downscaling uses YARN downscaling recommendations to decide on which nodes to remove,
    instead of removing a random set of nodes.

--- autoscale/src/main/java/com/sequenceiq/periscope/monitor/evaluator/CronTimeEvaluator.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/monitor/evaluator/CronTimeEvaluator.java
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -13,17 +14,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.StackV4Response;
 import com.sequenceiq.cloudbreak.logger.MDCBuilder;
 import com.sequenceiq.periscope.domain.BaseAlert;
 import com.sequenceiq.periscope.domain.Cluster;
+import com.sequenceiq.periscope.domain.ScalingPolicy;
 import com.sequenceiq.periscope.domain.TimeAlert;
+import com.sequenceiq.periscope.model.yarn.YarnScalingServiceV1Response;
 import com.sequenceiq.periscope.monitor.MonitorUpdateRate;
+import com.sequenceiq.periscope.monitor.client.YarnMetricsClient;
 import com.sequenceiq.periscope.monitor.context.ClusterIdEvaluatorContext;
 import com.sequenceiq.periscope.monitor.context.EvaluatorContext;
+import com.sequenceiq.periscope.monitor.evaluator.load.YarnResponseUtils;
 import com.sequenceiq.periscope.monitor.event.ScalingEvent;
+import com.sequenceiq.periscope.monitor.handler.CloudbreakCommunicator;
 import com.sequenceiq.periscope.repository.TimeAlertRepository;
 import com.sequenceiq.periscope.service.ClusterService;
 import com.sequenceiq.periscope.service.DateService;
+import com.sequenceiq.periscope.utils.StackResponseUtils;
 
 @Component("CronTimeEvaluator")
 @Scope("prototype")
@@ -44,6 +52,21 @@ public class CronTimeEvaluator extends EvaluatorExecutor {
 
     @Inject
     private EventPublisher eventPublisher;
+
+    @Inject
+    private CloudbreakCommunicator cloudbreakCommunicator;
+
+    @Inject
+    private YarnMetricsClient yarnMetricsClient;
+
+    @Inject
+    private YarnResponseUtils yarnResponseUtils;
+
+    @Inject
+    private StackResponseUtils stackResponseUtils;
+
+    @Inject
+    private ScalingPolicyTargetCalculator scalingPolicyTargetCalculator;
 
     private long clusterId;
 
@@ -80,11 +103,11 @@ public class CronTimeEvaluator extends EvaluatorExecutor {
         long start = System.currentTimeMillis();
         Cluster cluster = clusterService.findById(clusterId);
         MDCBuilder.buildMdcContext(cluster);
-        publishIfNeeded(alertRepository.findAllByCluster(clusterId));
+        publishIfNeeded(alertRepository.findAllByClusterIdOrderById(clusterId));
         LOGGER.debug("Finished cronTimeEvaluator for cluster {} in {} ms", cluster.getStackCrn(), System.currentTimeMillis() - start);
     }
 
-    private void publishIfNeeded(List<TimeAlert> alerts) {
+    protected void publishIfNeeded(List<TimeAlert> alerts) {
         for (TimeAlert alert : alerts) {
             if (isPolicyAttached(alert) && isTrigger(alert)) {
                 publish(alert);
@@ -104,7 +127,36 @@ public class CronTimeEvaluator extends EvaluatorExecutor {
     }
 
     private void publish(TimeAlert alert) {
-        eventPublisher.publishEvent(new ScalingEvent(alert));
+        ScalingEvent event = new ScalingEvent(alert);
+
+        StackV4Response stackV4Response = cloudbreakCommunicator.getByCrn(alert.getCluster().getStackCrn());
+        int hostGroupNodeCount = stackResponseUtils.getNodeCountForHostGroup(stackV4Response, alert.getScalingPolicy().getHostGroup());
+        int desiredAbsoluteNodeCount = scalingPolicyTargetCalculator.getDesiredAbsoluteNodeCount(event, hostGroupNodeCount);
+        int targetIncrementNodeCount = desiredAbsoluteNodeCount - hostGroupNodeCount;
+
+        event.setHostGroupNodeCount(hostGroupNodeCount);
+        event.setDesiredAbsoluteHostGroupNodeCount(desiredAbsoluteNodeCount);
+        if (targetIncrementNodeCount < 0) {
+            populateDecommissionCandidates(event, stackV4Response, alert.getCluster(), alert.getScalingPolicy(), -targetIncrementNodeCount);
+        }
+
+        eventPublisher.publishEvent(event);
         LOGGER.debug("Time alert '{}' triggered  for cluster '{}'", alert.getName(), alert.getCluster().getStackCrn());
+    }
+
+    private void populateDecommissionCandidates(ScalingEvent event, StackV4Response stackV4Response, Cluster cluster,
+            ScalingPolicy policy, int mandatoryDownScaleCount) {
+        try {
+            YarnScalingServiceV1Response yarnResponse = yarnMetricsClient.getYarnMetricsForCluster(cluster,
+                    stackV4Response, policy.getHostGroup(), Optional.of(mandatoryDownScaleCount));
+            Map<String, String> hostFqdnsToInstanceId = stackResponseUtils.getCloudInstanceIdsForHostGroup(stackV4Response, policy.getHostGroup());
+
+            List<String> decommissionNodes = yarnResponseUtils.getYarnRecommendedDecommissionHostsForHostGroup(cluster.getStackCrn(), yarnResponse,
+                    hostFqdnsToInstanceId, mandatoryDownScaleCount, Optional.of(mandatoryDownScaleCount));
+            event.setDecommissionNodeIds(decommissionNodes);
+        } catch (Exception ex) {
+            LOGGER.error("Error retrieving decommission candidates for  policy '{}', adjustment type '{}', cluster '{}'",
+                    policy.getName(), policy.getAdjustmentType(), cluster.getStackCrn(), ex);
+        }
     }
 }
--- autoscale/src/main/java/com/sequenceiq/periscope/monitor/evaluator/load/YarnLoadEvaluator.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/monitor/evaluator/load/YarnLoadEvaluator.java
@@ -1,13 +1,8 @@
 package com.sequenceiq.periscope.monitor.evaluator.load;
 
-import static com.sequenceiq.periscope.monitor.evaluator.ScalingConstants.DEFAULT_MAX_SCALE_UP_STEP_SIZE;
-
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -23,8 +18,6 @@ import com.sequenceiq.periscope.domain.Cluster;
 import com.sequenceiq.periscope.domain.LoadAlert;
 import com.sequenceiq.periscope.domain.LoadAlertConfiguration;
 import com.sequenceiq.periscope.model.yarn.YarnScalingServiceV1Response;
-import com.sequenceiq.periscope.model.yarn.YarnScalingServiceV1Response.DecommissionCandidate;
-import com.sequenceiq.periscope.model.yarn.YarnScalingServiceV1Response.NewNodeManagerCandidates;
 import com.sequenceiq.periscope.monitor.client.YarnMetricsClient;
 import com.sequenceiq.periscope.monitor.context.ClusterIdEvaluatorContext;
 import com.sequenceiq.periscope.monitor.context.EvaluatorContext;
@@ -62,6 +55,9 @@ public class YarnLoadEvaluator extends EvaluatorExecutor {
 
     @Inject
     private CloudbreakCommunicator cloudbreakCommunicator;
+
+    @Inject
+    private YarnResponseUtils yarnResponseUtils;
 
     private long clusterId;
 
@@ -124,66 +120,44 @@ public class YarnLoadEvaluator extends EvaluatorExecutor {
         int maxAllowedUpScale = configMaxNodeCount < 0 ? 0 : configMaxNodeCount;
         int maxAllowedDownScale = configMinNodeCount > 0 ? configMinNodeCount : 0;
 
-        YarnScalingServiceV1Response yarnResponse = yarnMetricsClient.getYarnMetricsForCluster(cluster, stackV4Response, policyHostGroup);
-        int yarnRecommendedScaleUpCount =
-                getYarnRecommendedScaleUpCount(yarnResponse, maxAllowedUpScale);
-        List<String> yarnRecommendedDecommissionHosts =
-                getYarnRecommendedDecommissionHostsForHostGroup(yarnResponse, hostFqdnsToInstanceId, maxAllowedDownScale);
+        Optional<Integer> mandatoryUpScaleCount = Optional.of(configMinNodeCount)
+                .filter(mandatoryUpScale -> mandatoryUpScale < 0).map(upscale -> -1 * upscale);
 
-        int targetScaleUpCount = configMinNodeCount < 0 ?
-                Math.max(-configMinNodeCount, yarnRecommendedScaleUpCount) : yarnRecommendedScaleUpCount;
-        int targetScaleDownCount = configMaxNodeCount < 0 ?
-                Math.max(-configMaxNodeCount, yarnRecommendedDecommissionHosts.size()) : yarnRecommendedDecommissionHosts.size();
+        Optional<Integer> mandatoryDownScaleCount = Optional.of(configMaxNodeCount)
+                .filter(mandatoryDownscale -> mandatoryDownscale < 0).map(downscale -> -1 * downscale);
 
-        if (targetScaleUpCount > 0) {
-            sendScaleUpEvent(existingHostGroupSize, targetScaleUpCount);
-        } else if (targetScaleDownCount > 0) {
-            sendScaleDownEvent(targetScaleDownCount, existingHostGroupSize, yarnRecommendedDecommissionHosts);
+        YarnScalingServiceV1Response yarnResponse = yarnMetricsClient
+                .getYarnMetricsForCluster(cluster, stackV4Response, policyHostGroup, mandatoryDownScaleCount);
+
+        int yarnRecommendedScaleUpCount = yarnResponseUtils.
+                getYarnRecommendedScaleUpCount(yarnResponse, policyHostGroup, maxAllowedUpScale, mandatoryUpScaleCount);
+        List<String> yarnRecommendedDecommissionHosts = yarnResponseUtils.
+                getYarnRecommendedDecommissionHostsForHostGroup(cluster.getStackCrn(), yarnResponse,
+                        hostFqdnsToInstanceId, maxAllowedDownScale, mandatoryDownScaleCount);
+
+        if (yarnRecommendedScaleUpCount > 0) {
+            sendScaleUpEvent(existingHostGroupSize, yarnRecommendedScaleUpCount);
+        } else if (!yarnRecommendedDecommissionHosts.isEmpty()) {
+            sendScaleDownEvent(existingHostGroupSize, yarnRecommendedDecommissionHosts);
         }
-    }
-
-    public List<String> getYarnRecommendedDecommissionHostsForHostGroup(YarnScalingServiceV1Response yarnResponse,
-            Map<String, String> hostFqdnsToInstanceId, int maxAllowedDownScale) {
-        return yarnResponse.getScaleDownCandidates().orElse(List.of()).stream()
-                .sorted(Comparator.comparingInt(DecommissionCandidate::getAmCount))
-                .map(DecommissionCandidate::getNodeId)
-                .map(nodeFqdn -> nodeFqdn.split(":")[0])
-                .filter(s -> hostFqdnsToInstanceId.keySet().contains(s))
-                .map(nodeFqdn -> hostFqdnsToInstanceId.get(nodeFqdn))
-                .limit(maxAllowedDownScale)
-                .collect(Collectors.toList());
-    }
-
-    public Integer getYarnRecommendedScaleUpCount(YarnScalingServiceV1Response yarnResponse, Integer maxAllowedUpScale) {
-        return yarnResponse.getScaleUpCandidates()
-                .map(NewNodeManagerCandidates::getCandidates).orElse(List.of()).stream()
-                .filter(candidate -> candidate.getModelName().equalsIgnoreCase(policyHostGroup))
-                .findFirst()
-                .map(NewNodeManagerCandidates.Candidate::getCount)
-                .map(scaleUpCount -> IntStream.of(scaleUpCount, maxAllowedUpScale, DEFAULT_MAX_SCALE_UP_STEP_SIZE).min().getAsInt())
-                .orElse(0);
     }
 
     public void sendScaleUpEvent(Integer existingHostGroupSize, Integer targetScaleUpCount) {
         ScalingEvent scalingEvent = new ScalingEvent(loadAlert);
-        scalingEvent.setHostGroupNodeCount(Optional.of(existingHostGroupSize));
-        scalingEvent.setScalingNodeCount(Optional.of(targetScaleUpCount));
+        scalingEvent.setHostGroupNodeCount(existingHostGroupSize);
+        scalingEvent.setDesiredAbsoluteHostGroupNodeCount(existingHostGroupSize + targetScaleUpCount);
         eventPublisher.publishEvent(scalingEvent);
         LOGGER.info("Triggered ScaleUp for Cluster '{}', NodeCount '{}', HostGroup '{}'",
                 cluster.getStackCrn(), targetScaleUpCount, policyHostGroup);
     }
 
-    public void sendScaleDownEvent(Integer targetScaleDownCount, Integer existingHostGroupSize,
-            List<String> yarnRecommendedDecommissionHosts) {
+    public void sendScaleDownEvent(Integer existingHostGroupSize, List<String> yarnRecommendedDecommissionHosts) {
         ScalingEvent scalingEvent = new ScalingEvent(loadAlert);
-        scalingEvent.setHostGroupNodeCount(Optional.of(existingHostGroupSize));
-        if (!yarnRecommendedDecommissionHosts.isEmpty()) {
-            scalingEvent.setDecommissionNodeIds(yarnRecommendedDecommissionHosts);
-        } else {
-            scalingEvent.setScalingNodeCount(Optional.of(-targetScaleDownCount));
-        }
+        scalingEvent.setHostGroupNodeCount(existingHostGroupSize);
+        scalingEvent.setDesiredAbsoluteHostGroupNodeCount(existingHostGroupSize - yarnRecommendedDecommissionHosts.size());
+        scalingEvent.setDecommissionNodeIds(yarnRecommendedDecommissionHosts);
         eventPublisher.publishEvent(scalingEvent);
         LOGGER.info("Triggered ScaleDown for Cluster '{}', NodeCount '{}', HostGroup '{}', DecommissionNodeIds '{}'",
-                targetScaleDownCount, cluster.getStackCrn(), policyHostGroup, yarnRecommendedDecommissionHosts);
+                yarnRecommendedDecommissionHosts.size(), cluster.getStackCrn(), policyHostGroup, yarnRecommendedDecommissionHosts);
     }
 }
