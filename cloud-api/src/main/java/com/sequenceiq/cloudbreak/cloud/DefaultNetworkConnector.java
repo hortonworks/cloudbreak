ef33919423b9bf16bf9e4c3daf7d7ac8840fd821
doktoric <rdoktorics@cloudera.com>
2020-07-29 22:52:37 +0200

    CB-8204 remove env list if not necessary
    In the old DH list page function was not updated when we introduced the /crn and /name endpoints. Now with this change we will only list environments when required.

--- cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/DefaultNetworkConnector.java
+++ cloud-api/src/main/java/com/sequenceiq/cloudbreak/cloud/DefaultNetworkConnector.java
@@ -25,19 +25,18 @@ public abstract class DefaultNetworkConnector implements NetworkConnector {
 
     @Override
     public SubnetSelectionResult chooseSubnets(Collection<CloudSubnet> subnetMetas, SubnetSelectionParameters subnetSelectionParameters) {
-        LOGGER.info("Trying to choose subnets from: {}.", subnetMetas);
+        LOGGER.debug("Trying to choose subnets from: {}.", subnetMetas);
         SubnetSelectionResult subnetSelectionResult = filterSubnets(subnetMetas, subnetSelectionParameters);
         if (subnetSelectionResult.hasResult()) {
-            LOGGER.info("There are subnets in the subnet selection: {}.", subnetSelectionResult);
+            LOGGER.debug("There are subnets in the subnet selection: {}.", subnetSelectionResult);
             if (subnetSelectionParameters.isHa()) {
-                return selectForHAScenario(subnetSelectionResult.getResult());
+                subnetSelectionResult = selectForHAScenario(subnetSelectionResult.getResult());
             } else {
-                return selectForNonHAScenario(subnetSelectionResult.getResult());
+                subnetSelectionResult = selectForNonHAScenario(subnetSelectionResult.getResult());
             }
-        } else {
-            LOGGER.info("There is no result in the subnet selection: {}.", subnetSelectionResult);
-            return subnetSelectionResult;
         }
+        LOGGER.info("The subnet selection is: {}.", subnetSelectionResult);
+        return subnetSelectionResult;
     }
 
     public abstract SubnetSelectionResult filterSubnets(Collection<CloudSubnet> subnetMetas, SubnetSelectionParameters subnetSelectionParameters);
@@ -48,22 +47,21 @@ public abstract class DefaultNetworkConnector implements NetworkConnector {
 
     public SubnetSelectionResult selectForHAScenario(List<CloudSubnet> subnets) {
         List<CloudSubnet> result = new ArrayList<>();
-        LOGGER.info("The request contains HA option: true");
         Map<String, List<CloudSubnet>> groupedSubnetsByAz = groupSubnetsByAz(subnets);
-        LOGGER.info("The subnets groupped by AZs and the result is: {}.", groupedSubnetsByAz);
+        LOGGER.debug("The subnets groupped by AZs and the result is: {}.", groupedSubnetsByAz);
         // We dont have enough AZ
         if (!groupedSubnetsByAz.isEmpty() && !isDifferentAzCountEnough(groupedSubnetsByAz)) {
-            LOGGER.info("There is not enough different AZ in the subnet setup.");
+            LOGGER.debug("There is not enough different AZ in the subnet setup.");
             return new SubnetSelectionResult(String.format(NOT_ENOUGH_AZ, groupedSubnetsByAz.keySet().size(), subnetCountInDifferentAzMin()));
         }
         // We have enough AZ
         for (Map.Entry<String, List<CloudSubnet>> entry : groupedSubnetsByAz.entrySet()) {
             int random = new Random().nextInt(entry.getValue().size());
             CloudSubnet cloudSubnet = entry.getValue().get(random);
-            LOGGER.info("The selected subnet is: {} by random id {}.", cloudSubnet, random);
+            LOGGER.debug("The selected subnet is: {} by random id {}.", cloudSubnet, random);
             result.add(cloudSubnet);
             if (result.size() == subnetCountInDifferentAzMax()) {
-                LOGGER.info("The selection logic already selected enough subnet which is {}.", result.size());
+                LOGGER.debug("The selection logic already selected enough subnet which is {}.", result.size());
                 break;
             }
         }
