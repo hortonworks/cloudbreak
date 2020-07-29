d0db413523b3c6e785dd3e80f3d46da679a31c27
Peter Erdos <perdos@cloudera.com>
2020-07-29 15:47:25 +0200

    CB-8201 Missing cache for GrpcUmsClient.isAuthorizationEntitlementRegistered

--- auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/GrpcUmsClient.java
+++ auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/GrpcUmsClient.java
@@ -419,6 +419,7 @@ public class GrpcUmsClient {
         }
     }
 
+    @Cacheable(cacheNames = "umsAuthorizationEntitlementRegisteredCache", key = "{ #actorCrn, #accountId }")
     public boolean isAuthorizationEntitlementRegistered(String actorCrn, String accountId) {
         if (StringUtils.equals(accountId, InternalCrnBuilder.INTERNAL_ACCOUNT)) {
             return false;
