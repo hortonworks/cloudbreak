c6ec70db7d9bcde04b1207e879210b23aa6e8854
Bryce Derriso <b.derriso@gmail.com>
2020-08-05 10:08:29 -0500

    Backup and restore using HDFS cli agains object store.
    * Restore db using HDFS cli to transfer backups.
    * Hack together a fix for embedded database backups.
    * Adjust url construction and region passing to account for HDFS cli usage.
    * Use a new kerberos credential cache when kiniting.
    * Remove unused `region` pillar value.
    * Clean up after successful Ranger admin group substitution.

--- orchestrator-salt/src/main/resources/salt/salt/postgresql/disaster_recovery/backup.sls
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/disaster_recovery/backup.sls
@@ -7,15 +7,24 @@ include:
 {% if 'None' != configure_remote_db %}
 backup_postgresql_db:
   cmd.run:
-    - name: /opt/salt/scripts/backup_db.sh {{salt['pillar.get']('platform')}} {{salt['pillar.get']('disaster_recovery:object_storage_url')}} {{salt['pillar.get']('postgres:remote_db_url')}} {{salt['pillar.get']('postgres:remote_db_port')}} {{salt['pillar.get']('postgres:remote_admin')}} {{salt['pillar.get']('disaster_recovery:aws_region')}} {{salt['pillar.get']('disaster_recovery:ranger_admin_group')}}
+    - name: /opt/salt/scripts/backup_db.sh {{salt['pillar.get']('disaster_recovery:object_storage_url')}} {{salt['pillar.get']('postgres:remote_db_url')}} {{salt['pillar.get']('postgres:remote_db_port')}} {{salt['pillar.get']('postgres:remote_admin')}} {{salt['pillar.get']('disaster_recovery:ranger_admin_group')}}
     - require:
       - sls: postgresql.disaster_recovery
 
 {%- else %}
+add_root_role_to_database:
+  cmd.run:
+    - name: createuser root --superuser --login
+    - runas: postgres
+    # counting failure as a success because if `root` is already there, this command will fail.
+    # whether or not `backup_postgresql_db` succeeds is all we really care about.
+    - success_retcodes: 1
+    - require:
+        - sls: postgresql.disaster_recovery
+
 backup_postgresql_db:
   cmd.run:
-    - name: /opt/salt/scripts/backup_db.sh {{salt['pillar.get']('platform')}} {{salt['pillar.get']('disaster_recovery:object_storage_url')}} "" "" "" "" "/var/lib/pgsql"
-    - runas: postgres
+    - name: /opt/salt/scripts/backup_db.sh {{salt['pillar.get']('disaster_recovery:object_storage_url')}} "" "" "" {{salt['pillar.get']('disaster_recovery:ranger_admin_group')}} "/var/lib/pgsql"
     - require:
-      - sls: postgresql.disaster_recovery
+      - cmd: add_root_role_to_database
 {% endif %}
--- orchestrator-salt/src/main/resources/salt/salt/postgresql/disaster_recovery/scripts/restore_db.sh
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/disaster_recovery/scripts/restore_db.sh
@@ -1,47 +1,37 @@
 #!/bin/bash
 # restore_db.sh
 # This script uses the 'psql' cli to drop hive and ranger databases, create them, then read in a plain SQL file to restore data.
-# We use the `azcopy copy` and `aws s3 cp` commands to copy backups from Azure or AWS respectively.
-# When using either cloud provider, the plaintext SQL files are first copied to the local filesystem, then fed into psql.
+# We retrieve the SQL files from a valid URL, which should be `s3a://` or `abfs://` for AWS or Azure clouds respectively.
 
 set -o errexit
 set -o nounset
 set -o pipefail
 set -o xtrace
 
-if [[ $# -ne 6 && $# -ne 7 ]]; then
+if [[ $# -ne 5 && $# -ne 6 ]]; then
   echo "Invalid inputs provided"
   echo "Script accepts 5 inputs:"
-  echo "  1. Cloud Provider (azure | aws)"
-  echo "  2. Object Storage Service url to retrieve backups."
-  echo "  3. PostgreSQL host name."
-  echo "  4. PostgreSQL port."
-  echo "  5. PostgreSQL user name."
-  echo "  6. AWS Region option."
-  echo "  7. ranger admin group"
-  echo "  8. (optional) Log file location."
+  echo "  1. Object Storage Service url to retrieve backups."
+  echo "  2. PostgreSQL host name."
+  echo "  3. PostgreSQL port."
+  echo "  4. PostgreSQL user name."
+  echo "  5. Ranger admin group."
+  echo "  6. (optional) Log file location."
   exit 1
 fi
 
-CLOUD_PROVIDER=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-CLOUD_LOCATION=$(echo "$2" | sed 's/\/\+$//g') # Clear trailng '/' (if present) for later path joining.
-HOST="$3"
-PORT="$4"
-USERNAME="$5"
-
-REGION_OPTION=${6:-''}
-if [[ -n $REGION_OPTION ]]; then
-  # todo: This only works with AWS!
-  REGION_OPTION="--region ${REGION_OPTION}"
-fi
-
-RANGERGROUP="$7"
-LOGFILE=${8:-/var/log/}/dl_postgres_restore.log
+BACKUP_LOCATION="$1/*" # Trailing slash and glob so we copy the _items_ in the directory not the directory itself.
+HOST="$2"
+PORT="$3"
+USERNAME="$4"
+RANGERGROUP="$5"
+LOGFILE=${6:-/var/log/}/dl_postgres_restore.log
 echo "Logs at ${LOGFILE}"
 
 BACKUPS_DIR="/var/tmp/postgres_restore_staging"
 
 doLog() {
+  set +x
   type_of_msg=$(echo "$@" | cut -d" " -f1)
   msg=$(echo "$*" | cut -d" " -f2-)
   [[ $type_of_msg == INFO ]] && type_of_msg="INFO " # one space for aligning
@@ -50,11 +40,24 @@ doLog() {
   # print to the terminal if we have one
   test -t 1 && echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg"
   echo "$(date "+%Y-%m-%dT%H:%M:%SZ") $type_of_msg ""$msg" >>$LOGFILE
+  set -x
 }
 
 errorExit() {
   doLog "ERROR $1"
   exit 1
+}
+
+
+kinit_as_hdfs() {
+  # Use a new Kerberos credential cache, to keep from clobbering the default.
+  export KRB5CCNAME=/tmp/krb5cc_cloudbreak_$EUID
+  HDFS_KEYTAB=/run/cloudera-scm-agent/process/$(ls -t /run/cloudera-scm-agent/process/ | grep hdfs-NAMENODE$ | head -n 1)/hdfs.keytab
+  doLog "kinit as hdfs using Keytab: $HDFS_KEYTAB"
+  kinit -kt "$HDFS_KEYTAB" "hdfs/$(hostname -f)"
+  if [[ $? -ne 0 ]]; then
+    errorExit "Couldn't kinit as hdfs"
+  fi
 }
 
 replace_ranger_group_before_import() {
@@ -71,42 +74,27 @@ restore_db_from_local() {
 
   if [[ "$SERVICE" == "ranger" ]]; then
     replace_ranger_group_before_import $RANGERGROUP $BACKUP
-  fi  
+  fi
   doLog "INFO Restoring $SERVICE"
 
   psql --host="$HOST" --port="$PORT" --dbname="postgres" --username="$USERNAME" -c "drop database ${SERVICE};" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Unable to drop database ${SERVICE}"
   psql --host="$HOST" --port="$PORT" --dbname="postgres" --username="$USERNAME" -c "create database ${SERVICE};" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Unable to re-create database ${SERVICE}"
-  psql --host="$HOST" --port="$PORT" --dbname="$SERVICE" --username="$USERNAME" < "$BACKUP" > $LOGFILE 2> >(tee -a $LOGFILE >&2) || errorExit "Unable to restore ${SERVICE}"
+  psql --host="$HOST" --port="$PORT" --dbname="$SERVICE" --username="$USERNAME" <"$BACKUP" >$LOGFILE 2> >(tee -a $LOGFILE >&2) || errorExit "Unable to restore ${SERVICE}"
   doLog "INFO Succesfully restored ${SERVICE}"
 }
 
-run_azure_restore() {
+run_restore() {
   mkdir -p "$BACKUPS_DIR"
-  azcopy login --identity > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Could not login to Azure"
-  azcopy copy "$CLOUD_LOCATION"/* "$BACKUPS_DIR" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Could not copy backups from Azure."
+  kinit_as_hdfs
+  hdfs dfs -copyToLocal -f "$BACKUP_LOCATION" "$BACKUPS_DIR" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Could not copy backups from ${BACKUP_LOCATION}."
+
   restore_db_from_local "hive"
   restore_db_from_local "ranger"
+
   rm -rfv "$BACKUPS_DIR" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2)
 }
 
-run_aws_restore () {
-  mkdir -p "$BACKUPS_DIR"
-  # shellcheck disable=SC2086
-  # We want to allow word splitting for the REGION_OPTION
-  aws ${REGION_OPTION} s3 cp "${CLOUD_LOCATION}/" "$BACKUPS_DIR" --recursive > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2) || errorExit "Could not copy backups from AWS S3."
-  restore_db_from_local "hive"
-  restore_db_from_local "ranger"
-  rm -rfv "$BACKUPS_DIR" > >(tee -a $LOGFILE) 2> >(tee -a $LOGFILE >&2)
-}
-
-doLog "Info Ranger admin group is $7"
+doLog "Info Ranger admin group is $RANGERGROUP"
 doLog "INFO Initiating restore"
-if [[ "$CLOUD_PROVIDER" == "azure" ]]; then
-  run_azure_restore
-elif [[ "$CLOUD_PROVIDER" == "aws" ]]; then
-  run_aws_restore
-else
-  errorExit "Unknown cloud provider: ${CLOUD_PROVIDER}"
-fi
-
+run_restore
 doLog "INFO Completed restore."
