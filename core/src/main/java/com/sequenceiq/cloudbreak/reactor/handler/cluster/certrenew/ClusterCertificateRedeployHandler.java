7cf978c3b9b55bf42ad93f755f648f12cd6e098b
Tamas Bihari <tbihari@hortonworks.com>
2020-08-11 13:54:36 +0200

    CB-7812 decrease the error count of cert renewal poller and improve error status reasons

--- core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/cluster/certrenew/ClusterCertificateRedeployHandler.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/cluster/certrenew/ClusterCertificateRedeployHandler.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.common.event.Selectable;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.ClusterServiceRunner;
+import com.sequenceiq.cloudbreak.orchestrator.exception.CloudbreakOrchestratorFailedException;
 import com.sequenceiq.cloudbreak.reactor.api.event.cluster.certrenew.ClusterCertificateRedeployRequest;
 import com.sequenceiq.cloudbreak.reactor.api.event.cluster.certrenew.ClusterCertificateRedeploySuccess;
 import com.sequenceiq.cloudbreak.reactor.api.event.cluster.certrenew.ClusterCertificateRenewFailed;
@@ -40,12 +41,13 @@ public class ClusterCertificateRedeployHandler implements EventHandler<ClusterCe
         LOGGER.debug("Redeploy certificate for stack 'id:{}'", stackId);
         Selectable response;
         try {
-            clusterServiceRunner.updateSaltState(stackId);
+            clusterServiceRunner.redeployGatewayCertificate(stackId);
             LOGGER.info("Certificate of the cluster has been redeployed successfully.");
             response = new ClusterCertificateRedeploySuccess(stackId);
         } catch (Exception ex) {
-            LOGGER.warn("Redeploy of certificate has been failed for cluster", ex);
-            response = new ClusterCertificateRenewFailed(stackId, ex);
+            String msg = "Certificate couldn't be redeployed to the cluster: ";
+            LOGGER.warn(msg, ex);
+            response = new ClusterCertificateRenewFailed(stackId, new CloudbreakOrchestratorFailedException(msg + ex.getMessage(), ex));
         }
         eventBus.notify(response.selector(), new Event<>(event.getHeaders(), response));
     }
