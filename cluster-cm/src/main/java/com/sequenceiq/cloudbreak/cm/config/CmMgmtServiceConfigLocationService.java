86f9c677e51e56d020f24a40e454cdb60418d774
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-08-11 14:23:25 +0200

    CB-8363 The MGMT services (like SERVICEMONITOR) only runs on the gateway node and the gateway node minimum value of volume is 1. Therefor we assign the dirs for the first attached disk by default. I removed the determinatin of volume counts from the code.

--- cluster-cm/src/main/java/com/sequenceiq/cloudbreak/cm/config/CmMgmtServiceConfigLocationService.java
+++ cluster-cm/src/main/java/com/sequenceiq/cloudbreak/cm/config/CmMgmtServiceConfigLocationService.java
@@ -1,14 +1,13 @@
 package com.sequenceiq.cloudbreak.cm.config;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import com.cloudera.api.swagger.model.ApiConfig;
 import com.cloudera.api.swagger.model.ApiRole;
 import com.cloudera.api.swagger.model.ApiRoleList;
-import com.sequenceiq.cloudbreak.domain.Resource;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.template.VolumeUtils;
 
@@ -17,32 +16,32 @@ class CmMgmtServiceConfigLocationService implements CmConfigServiceDelegate {
 
     @Override
     public void setConfigs(Stack stack, ApiRoleList apiRoleList) {
-        List<Resource> attachedDisks = stack.getDiskResources();
-        if (!attachedDisks.isEmpty()) {
-            Optional<ApiRole> eventServer = getApiRole("EVENTSERVER", apiRoleList);
-            eventServer.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("eventserver_index_dir", "cloudera-scm-eventserver", attachedDisks.size())));
+        Optional<ApiRole> eventServer = getApiRole("EVENTSERVER", apiRoleList);
+        eventServer.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("eventserver_index_dir", "cloudera-scm-eventserver")));
 
-            Optional<ApiRole> hostMonitor = getApiRole("HOSTMONITOR", apiRoleList);
-            hostMonitor.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("firehose_storage_dir", "cloudera-host-monitor", attachedDisks.size())));
+        Optional<ApiRole> hostMonitor = getApiRole("HOSTMONITOR", apiRoleList);
+        hostMonitor.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("firehose_storage_dir", "cloudera-host-monitor")));
 
-            Optional<ApiRole> reportsManager = getApiRole("REPORTSMANAGER", apiRoleList);
-            reportsManager.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("headlamp_scratch_dir", "cloudera-scm-headlamp", attachedDisks.size())));
+        Optional<ApiRole> reportsManager = getApiRole("REPORTSMANAGER", apiRoleList);
+        reportsManager.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("headlamp_scratch_dir", "cloudera-scm-headlamp")));
 
-            Optional<ApiRole> serviceMonitor = getApiRole("SERVICEMONITOR", apiRoleList);
-            serviceMonitor.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("firehose_storage_dir", "cloudera-service-monitor", attachedDisks.size())));
-        }
+        Optional<ApiRole> serviceMonitor = getApiRole("SERVICEMONITOR", apiRoleList);
+        serviceMonitor.ifPresent(apiRole -> addConfig(apiRole, createApiConfig("firehose_storage_dir", "cloudera-service-monitor")));
     }
 
     private Optional<ApiRole> getApiRole(String roleName, ApiRoleList apiRoleList) {
+        if (CollectionUtils.isEmpty(apiRoleList.getItems())) {
+            return Optional.empty();
+        }
         return apiRoleList.getItems().stream()
-                .filter(apiRole -> apiRole.getName().equals(roleName))
+                .filter(apiRole -> apiRole != null && apiRole.getName().equals(roleName))
                 .findFirst();
     }
 
-    private ApiConfig createApiConfig(String name, String configDir, int numberOfVolumes) {
+    private ApiConfig createApiConfig(String name, String configDir) {
         ApiConfig apiConfig = new ApiConfig();
         apiConfig.setName(name);
-        apiConfig.setValue(VolumeUtils.buildSingleVolumePath(numberOfVolumes, configDir));
+        apiConfig.setValue(VolumeUtils.buildSingleVolumePath(1, configDir));
         apiConfig.setSensitive(false);
         return apiConfig;
     }
