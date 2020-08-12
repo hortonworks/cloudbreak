65cfd7485c2987711e5492dcd53f937e17c07bf4
Tamas Bihari <tbihari@hortonworks.com>
2020-08-12 15:26:23 +0200

    fix unupdated import

--- structuredevent-service-cdp/src/main/java/com/sequenceiq/cloudbreak/structuredevent/config/CDPStructuredEventConfig.java
+++ structuredevent-service-cdp/src/main/java/com/sequenceiq/cloudbreak/structuredevent/config/CDPStructuredEventConfig.java
@@ -3,7 +3,7 @@ package com.sequenceiq.cloudbreak.structuredevent.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.sequenceiq.cloudbreak.CDPDefaultStructuredEventClient;
+import com.sequenceiq.cloudbreak.structuredevent.service.CDPDefaultStructuredEventClient;
 import com.sequenceiq.cloudbreak.structuredevent.service.CDPStructuredEventClient;
 
 @Configuration
