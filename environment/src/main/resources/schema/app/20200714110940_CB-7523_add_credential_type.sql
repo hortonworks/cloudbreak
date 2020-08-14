cc7bff8cae31a9bfafabeec11c73c7aa1c52f76e
doktoric <rdoktorics@cloudera.com>
2020-08-14 11:07:00 +0200

    CB-8421 missing default value for credential type

--- environment/src/main/resources/schema/app/20200714110940_CB-7523_add_credential_type.sql
+++ environment/src/main/resources/schema/app/20200714110940_CB-7523_add_credential_type.sql
@@ -2,7 +2,7 @@
 -- Migration SQL that makes the change goes here.
 
 ALTER TABLE credential ADD COLUMN IF NOT EXISTS type VARCHAR(255);
-UPDATE credential SET type='ENVIRONMENT';
+UPDATE credential SET type='ENVIRONMENT' WHERE type IS NULL;
 -- //@UNDO
 -- SQL to undo the change goes here.
 
