eff6287b3c8d5d4708fc6087177c2b3a8a37f620
smaniraju <smaniraju@cloudera.com>
2020-08-19 21:17:03 +0530

    CB-8505-AssociatePeriscopeRepositoryWithTransaction
    Periscope Repository interfaces should be annotated with Transactional attribute, since without that Query Results will not be pre-flushed and stale data will be returned in certain scenarios.

--- autoscale/src/main/java/com/sequenceiq/periscope/repository/SecurityConfigRepository.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/repository/SecurityConfigRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.repository.query.Param;
 import com.sequenceiq.cloudbreak.workspace.repository.EntityType;
 import com.sequenceiq.periscope.domain.SecurityConfig;
 
+import javax.transaction.Transactional;
+
 @EntityType(entityClass = SecurityConfig.class)
+@Transactional(Transactional.TxType.REQUIRED)
 public interface SecurityConfigRepository extends CrudRepository<SecurityConfig, Long> {
 
     @Query("SELECT sc FROM SecurityConfig sc WHERE sc.cluster.id = :clusterId")
--- autoscale/src/main/java/com/sequenceiq/periscope/repository/TimeAlertRepository.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/repository/TimeAlertRepository.java
@@ -1,14 +1,15 @@
 package com.sequenceiq.periscope.repository;
 
-import java.util.List;
-
+import com.sequenceiq.cloudbreak.workspace.repository.EntityType;
+import com.sequenceiq.periscope.domain.TimeAlert;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import com.sequenceiq.cloudbreak.workspace.repository.EntityType;
-import com.sequenceiq.periscope.domain.TimeAlert;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @EntityType(entityClass = TimeAlert.class)
+@Transactional(Transactional.TxType.REQUIRED)
 public interface TimeAlertRepository extends CrudRepository<TimeAlert, Long> {
 
     TimeAlert findByCluster(@Param("alertId") Long alertId, @Param("clusterId") Long clusterId);
