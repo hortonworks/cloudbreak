4b052a903e86dc8f54fe739ee2ab37e11a794cf0
doktoric <rdoktorics@cloudera.com>
2020-08-20 23:55:08 +0200

    GCP support

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/cloud/PlatformParameterService.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/cloud/PlatformParameterService.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.freeipa.service.cloud;
 
+import static com.sequenceiq.cloudbreak.cloud.model.AvailabilityZone.availabilityZone;
 import static com.sequenceiq.cloudbreak.cloud.model.Location.location;
 import static com.sequenceiq.cloudbreak.cloud.model.Region.region;
 
@@ -44,7 +45,7 @@ public class PlatformParameterService {
     public PlatformParameters getPlatformParameters(Optional<String> requestId, Stack stack, Credential credential) {
         MDCBuilder.addRequestId(requestId.orElse(UUID.randomUUID().toString()));
         LOGGER.debug("Get platform parameters for: {}", stack);
-        Location location = location(region(stack.getRegion()));
+        Location location = location(region(stack.getRegion()), availabilityZone(stack.getAvailabilityZone()));
         CloudContext cloudContext = new CloudContext(stack.getId(), stack.getName(), stack.getCloudPlatform(), stack.getCloudPlatform(),
                 location, stack.getOwner(), stack.getAccountId());
         CloudCredential cloudCredential = credentialConverter.convert(credential);
