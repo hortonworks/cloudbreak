129519f9778a379d841bbf5b5d0073ce5d309fdf
ganeshk <ganeshk@cloudera.com>
2020-08-24 15:18:35 -0700

    GCP support - support for freeipa install in GCS removing Jamison's auto-forward removal feature.

--- freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_install.sh
+++ freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_install.sh
@@ -5,11 +5,6 @@ set -e
 FQDN=$(hostname -f)
 IPADDR=$(hostname -i)
 
-if [ ! -f /etc/resolv.conf.orig ]; then
-  cp /etc/resolv.conf /etc/resolv.conf.orig
-fi
-FORWARDERS=$(grep -Ev '^#|^;' /etc/resolv.conf.orig | grep nameserver | awk '{print "--forwarder " $2}');
-
 ipa-server-install --unattended --uninstall
 
 ipa-server-install \
@@ -26,8 +21,8 @@ ipa-server-install \
           --allow-zone-overlap \
           --ssh-trust-dns \
           --mkhomedir \
-          --ip-address "$IPADDR" \
-          $FORWARDERS \
+          --ip-address $IPADDR \
+          --auto-forwarders \
 {%- if not salt['pillar.get']('freeipa:dnssecValidationEnabled') %}
           --no-dnssec-validation \
 {%- endif %}
