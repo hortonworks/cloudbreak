8ab2679b379ee6a3358b6ea7f620bfc438ec47b8
lnardai <laci.nardai@gmail.com>
2020-09-01 11:54:59 +0200

    CB-8313 making createMachineUser an internalCall, enhanced ThreadBasedUserCrnProvier so internal calls can function well

--- auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/service/AltusIAMService.java
+++ auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/service/AltusIAMService.java
@@ -3,13 +3,11 @@ package com.sequenceiq.cloudbreak.auth.altus.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.cloudera.thunderhead.service.usermanagement.UserManagementProto;
-import com.sequenceiq.cloudbreak.altus.AltusDatabusConfiguration;
 import com.sequenceiq.cloudbreak.auth.altus.GrpcUmsClient;
 import com.sequenceiq.cloudbreak.auth.altus.model.AltusCredential;
 import com.sequenceiq.common.api.telemetry.model.AnonymizationRule;
@@ -19,56 +17,38 @@ public class AltusIAMService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AltusIAMService.class);
 
-    private final boolean useSharedAltusCredential;
-
-    private final String sharedAltusAccessKey;
-
-    private final char[] sharedAltusSecretKey;
-
     private final GrpcUmsClient umsClient;
 
+    private final SharedAltusCredentialProvider sharedAltusCredentialProvider;
+
     public AltusIAMService(GrpcUmsClient umsClient,
-            AltusDatabusConfiguration altusDatabusConfiguration) {
+            SharedAltusCredentialProvider sharedAltusCredentialProvider) {
         this.umsClient = umsClient;
-        this.useSharedAltusCredential = altusDatabusConfiguration.isUseSharedAltusCredential();
-        this.sharedAltusAccessKey = altusDatabusConfiguration.getSharedAccessKey();
-        this.sharedAltusSecretKey = altusDatabusConfiguration.getSharedSecretKey();
+        this.sharedAltusCredentialProvider = sharedAltusCredentialProvider;
     }
 
     /**
      * Generate machine user with access keys
      */
-    public Optional<AltusCredential> generateMachineUserWithAccessKey(String machineUserName, String actorCrn, boolean useSharedCredential) {
-        if (useSharedAltusCredential) {
-            LOGGER.debug("Use shared altus credential is turned on for generating altus credential and access keys.");
-            if (areDatabusCredentialsFilled(useSharedCredential)) {
-                LOGGER.debug("Access and secret keys are set manually application wide for Databus, skip machine user and access key generation");
-                return Optional.of(new AltusCredential(sharedAltusAccessKey, sharedAltusSecretKey));
-            } else {
-                LOGGER.debug("Use shared credential global config credential is set, but no shared access/secret keypair is used.");
-            }
-        }
-
-        return Optional.of(umsClient.createMachineUserAndGenerateKeys(
-                machineUserName,
-                actorCrn,
-                umsClient.getBuiltInDatabusRoleCrn(),
-                UserManagementProto.AccessKeyType.Value.ED25519));
-    }
-
-    public Optional<AltusCredential> generateMachineUserWithAccessKey(String machineUserName, String actorCrn) {
-        return generateMachineUserWithAccessKey(machineUserName, actorCrn, false);
+    public Optional<AltusCredential> generateMachineUserWithAccessKey(String machineUserName, String actorCrn, String accountId, boolean useSharedCredential) {
+        return Optional.ofNullable(sharedAltusCredentialProvider.getSharedCredentialIfConfigured(useSharedCredential)
+                .orElse(umsClient.createMachineUserAndGenerateKeys(
+                        machineUserName,
+                        actorCrn,
+                        accountId,
+                        umsClient.getBuiltInDatabusRoleCrn(),
+                        UserManagementProto.AccessKeyType.Value.ED25519)));
     }
 
     /**
      * Delete machine user with its access keys (and unassign databus role if required)
      */
-    public void clearMachineUser(String machineUserName, String actorCrn, boolean useSharedCredential) {
+    public void clearMachineUser(String machineUserName, String actorCrn, String accountId, boolean useSharedCredential) {
         try {
-            if (useSharedAltusCredential && areDatabusCredentialsFilled(useSharedCredential)) {
+            if (sharedAltusCredentialProvider.isSharedAltusCredentialInUse(useSharedCredential)) {
                 LOGGER.debug("Access and secret keys are set manually application wide for Databus, skip machine user cleanup.");
             } else {
-                umsClient.clearMachineUserWithAccessKeysAndRole(machineUserName, actorCrn, umsClient.getBuiltInDatabusRoleCrn());
+                umsClient.clearMachineUserWithAccessKeysAndRole(machineUserName, actorCrn, accountId, umsClient.getBuiltInDatabusRoleCrn());
             }
         } catch (Exception e) {
             LOGGER.warn("Cluster Databus resource cleanup failed (fluent - databus user). It is not a fatal issue, "
@@ -80,12 +60,7 @@ public class AltusIAMService {
         return umsClient.getAnonymizationRules(accountId, actorCrn);
     }
 
-    public void clearMachineUser(String machineUserName, String actorCrn) {
-        clearMachineUser(machineUserName, actorCrn, false);
+    public void clearMachineUser(String machineUserName, String actorCrn, String accountId) {
+        clearMachineUser(machineUserName, actorCrn, accountId, false);
     }
-
-    private boolean areDatabusCredentialsFilled(boolean useSharedCredential) {
-        return useSharedCredential && sharedAltusSecretKey != null && sharedAltusSecretKey.length > 0 && StringUtils.isNotBlank(sharedAltusAccessKey);
-    }
-
 }
