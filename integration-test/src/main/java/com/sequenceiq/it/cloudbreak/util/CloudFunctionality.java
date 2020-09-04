6acd5c52fe06d4a8ca565d89b11633be9e53042c
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-09-04 13:59:43 +0200

    CB-8647 We should retry all cloud functionalty in IT, AWS and Azure as well. The @Retryable is configured and annotation added.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/CloudFunctionality.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/CloudFunctionality.java
@@ -3,24 +3,76 @@ package com.sequenceiq.it.cloudbreak.util;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
 public interface CloudFunctionality {
+
+    int MAX_DELAY = 5000;
+
+    int DELAY = 2000;
+
+    int MULTIPLIER = 2;
+
+    int ATTEMPTS = 5;
+
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     List<String> listInstanceVolumeIds(List<String> instanceIds);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     Map<String, Map<String, String>> listTagsByInstanceId(List<String> instanceIds);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void deleteInstances(List<String> instanceIds);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void stopInstances(List<String> instanceIds);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void cloudStorageInitialize();
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void cloudStorageListContainer(String baseLocation);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void cloudStorageListContainerFreeIpa(String baseLocation, String clusterName, String crn);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void cloudStorageListContainerDataLake(String baseLocation, String clusterName, String crn);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     void cloudStorageDeleteContainer(String baseLocation);
 
+    @Retryable(
+            maxAttempts = ATTEMPTS,
+            backoff = @Backoff(delay = DELAY, multiplier = MULTIPLIER, maxDelay = MAX_DELAY)
+    )
     Map<String, Boolean> enaSupport(List<String> instanceIds);
 }
