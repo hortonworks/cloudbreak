048497f6143c64fa8739c2d8be21ee44d38cff3e
Jamison Bennett <jamison.bennett@gmail.com>
2020-09-22 21:29:17 -0500

    CB-8137: Fix FreeIPA status for deleted, stopped, available, unreac... ...hable, and unhealthy
    Fixed FreeIPA status for HA clusters.
     * Stopped + Running = Unhealthy
     * Stopped + Terminated = Unhealthy
     * Unreachable + Unreachable = Unreachable
     * Unreahable + Running = Unhealthy
     * Running + Deleted = Unhealthy
    
    This was tested with unit tests and also manually with a local
    deployment of cloudbreak.

--- freeipa/src/main/java/com/sequenceiq/freeipa/sync/FreeipaChecker.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/sync/FreeipaChecker.java
@@ -5,24 +5,22 @@ import static com.sequenceiq.cloudbreak.util.Benchmark.checkedMeasure;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.DetailedStackStatus;
 import com.sequenceiq.freeipa.client.FreeIpaClient;
-import com.sequenceiq.freeipa.client.FreeIpaClientException;
-import com.sequenceiq.freeipa.client.FreeIpaClientExceptionUtil;
-import com.sequenceiq.freeipa.client.FreeIpaHostNotAvailableException;
 import com.sequenceiq.freeipa.client.model.RPCResponse;
 import com.sequenceiq.freeipa.entity.InstanceMetaData;
 import com.sequenceiq.freeipa.entity.Stack;
 import com.sequenceiq.freeipa.service.freeipa.FreeIpaClientFactory;
-import com.sequenceiq.freeipa.service.stack.instance.InstanceMetaDataService;
 
 @Component
 public class FreeipaChecker {
@@ -32,48 +30,66 @@ public class FreeipaChecker {
     @Inject
     private FreeIpaClientFactory freeIpaClientFactory;
 
-    @Inject
-    private InstanceMetaDataService instanceMetaDataService;
-
-    private List<RPCResponse<Boolean>> checkStatus(Stack stack, Set<InstanceMetaData> checkableInstances) throws Exception {
+    private Pair<List<DetailedStackStatus>, String> checkStatus(Stack stack, Set<InstanceMetaData> checkableInstances) throws Exception {
         return checkedMeasure(() -> {
-            List<RPCResponse<Boolean>> statuses = new LinkedList<>();
+            List<DetailedStackStatus> statuses = new LinkedList<>();
+            List<RPCResponse<Boolean>> responses = new LinkedList<>();
             for (InstanceMetaData instanceMetaData : checkableInstances) {
                 String hostname = instanceMetaData.getDiscoveryFQDN();
-                FreeIpaClient freeIpaClient = checkedMeasure(() -> freeIpaClientFactory.getFreeIpaClientForStackWithPing(stack, hostname), LOGGER,
-                        ":::Auto sync::: freeipa client is created in {}ms");
-                statuses.add(checkedMeasure(() -> freeIpaClient.serverConnCheck(freeIpaClient.getHostname(), hostname), LOGGER,
-                        ":::Auto sync::: freeipa server_conncheck ran in {}ms"));
+                try {
+                    FreeIpaClient freeIpaClient = checkedMeasure(() -> freeIpaClientFactory.getFreeIpaClientForStackWithPing(stack, hostname), LOGGER,
+                            ":::Auto sync::: freeipa client is created in {}ms");
+                    RPCResponse<Boolean> response = checkedMeasure(() -> freeIpaClient.serverConnCheck(freeIpaClient.getHostname(), hostname), LOGGER,
+                            ":::Auto sync::: freeipa server_conncheck ran in {}ms");
+                    responses.add(response);
+                    if (response.getResult()) {
+                        statuses.add(DetailedStackStatus.AVAILABLE);
+                    } else {
+                        statuses.add(DetailedStackStatus.UNHEALTHY);
+                    }
+                } catch (Exception e) {
+                    LOGGER.info("FreeIpaClientException occurred during status fetch: " + e.getMessage(), e);
+                    statuses.add(DetailedStackStatus.UNREACHABLE);
+                }
             }
-            return statuses;
+            String message = getMessages(responses);
+            return Pair.of(statuses, message);
         }, LOGGER, ":::Auto sync::: freeipa server status is checked in {}ms");
     }
 
     public SyncResult getStatus(Stack stack, Set<InstanceMetaData> checkableInstances) {
         try {
-            Set<InstanceMetaData> notTermiatedStackInstances = instanceMetaDataService.findNotTerminatedForStack(stack.getId());
-            List<RPCResponse<Boolean>> responses = checkStatus(stack, checkableInstances);
+            if (checkableInstances.isEmpty()) {
+                throw new UnsupportedOperationException("There are no instances of FreeIPA to check the status of");
+            }
+
+            // Exclude terminated but include deleted
+            Set<InstanceMetaData> notTermiatedStackInstances = stack.getAllInstanceMetaDataList().stream()
+                    .filter(Predicate.not(InstanceMetaData::isTerminated))
+                    .collect(Collectors.toSet());
+            Pair<List<DetailedStackStatus>, String> statusCheckPair = checkStatus(stack, checkableInstances);
+            List<DetailedStackStatus> responses = statusCheckPair.getFirst();
             DetailedStackStatus status;
-            String postFix = "";
-            Boolean result = !responses.isEmpty() && responses.stream().allMatch(RPCResponse::getResult);
-            if (result && responses.size() == notTermiatedStackInstances.size()) {
-                status = DetailedStackStatus.PROVISIONED;
+            if (areAllStatusTheSame(responses) && !hasMissingStatus(responses, notTermiatedStackInstances)) {
+                status = responses.get(0);
             } else {
                 status = DetailedStackStatus.UNHEALTHY;
-                postFix = "Freeipa is unhealthy, ";
             }
-            return new SyncResult(postFix + getMessages(responses), status, result);
-        } catch (FreeIpaClientException e) {
-            LOGGER.info("FreeIpaClientException occurred during status fetch: " + e.getMessage(), e);
-            Throwable t = FreeIpaClientExceptionUtil.getAncestorCauseBeforeFreeIpaClientExceptions(e);
-            if (t instanceof FreeIpaHostNotAvailableException) {
-                return new SyncResult("Freeipa is unreachable: " + t.getMessage(), DetailedStackStatus.UNREACHABLE, false);
-            }
-            return new SyncResult("Freeipa is unhealthy, error occurred: " + e.getMessage(), DetailedStackStatus.UNHEALTHY, false);
+
+            return new SyncResult("FreeIpa is " + status + ", " + statusCheckPair.getSecond(), status);
         } catch (Exception e) {
             LOGGER.info("Error occurred during status fetch: " + e.getMessage(), e);
-            return new SyncResult("Freeipa is unreachable, because error occurred: " + e.getMessage(), DetailedStackStatus.UNREACHABLE, false);
+            return new SyncResult("FreeIpa is unreachable, because error occurred: " + e.getMessage(), DetailedStackStatus.UNREACHABLE);
         }
+    }
+
+    private boolean areAllStatusTheSame(List<DetailedStackStatus> response) {
+        DetailedStackStatus first = response.get(0);
+        return response.stream().allMatch(Predicate.isEqual(first));
+    }
+
+    private boolean hasMissingStatus(List<DetailedStackStatus> response, Set<InstanceMetaData> notTermiatedStackInstances) {
+        return response.size() != notTermiatedStackInstances.size();
     }
 
     private String getMessages(List<RPCResponse<Boolean>> responses) {
