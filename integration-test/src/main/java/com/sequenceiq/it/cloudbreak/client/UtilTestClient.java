5483130009d9212f912b5547289ac85c917eeadf
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2020-09-28 19:13:48 +0200

    CB-9000 create mock test for calling checkright util with raw json

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/client/UtilTestClient.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/client/UtilTestClient.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Service;
 
 import com.sequenceiq.it.cloudbreak.CloudbreakClient;
 import com.sequenceiq.it.cloudbreak.action.Action;
+import com.sequenceiq.it.cloudbreak.action.v4.util.CheckRightRawAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.CloudStorageMatrixAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.DeploymentPreferencesAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.RepoConfigValidationAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.SecurityRulesAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.StackMatrixAction;
 import com.sequenceiq.it.cloudbreak.action.v4.util.VersionCheckAction;
+import com.sequenceiq.it.cloudbreak.dto.RawCloudbreakTestDto;
 import com.sequenceiq.it.cloudbreak.dto.securityrule.SecurityRulesTestDto;
 import com.sequenceiq.it.cloudbreak.dto.util.CloudStorageMatrixTestDto;
 import com.sequenceiq.it.cloudbreak.dto.util.DeploymentPreferencesTestDto;
@@ -42,6 +44,10 @@ public class UtilTestClient {
 
     public Action<VersionCheckTestDto, CloudbreakClient> versionChecker() {
         return new VersionCheckAction();
+    }
+
+    public Action<RawCloudbreakTestDto, CloudbreakClient> checkRightRaw() {
+        return new CheckRightRawAction();
     }
 
 }
