b51465e0872972ad7b39765007a0e70a35264700
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-10-02 17:02:47 +0200

    CB-9129 We want to save the structured event if the resource crn is null

--- structuredevent-service-cdp/src/test/java/com/sequenceiq/cloudbreak/structuredevent/service/db/CDPStructuredEventDBServiceTest.java
+++ structuredevent-service-cdp/src/test/java/com/sequenceiq/cloudbreak/structuredevent/service/db/CDPStructuredEventDBServiceTest.java
@@ -4,6 +4,8 @@ import static com.sequenceiq.cloudbreak.structuredevent.event.StructuredEventTyp
 import static com.sequenceiq.cloudbreak.structuredevent.event.StructuredEventType.NOTIFICATION;
 import static com.sequenceiq.cloudbreak.structuredevent.event.StructuredEventType.REST;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
@@ -15,11 +17,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.sequenceiq.cloudbreak.structuredevent.domain.CDPStructuredEventEntity;
+import com.sequenceiq.cloudbreak.structuredevent.event.cdp.CDPOperationDetails;
+import com.sequenceiq.cloudbreak.structuredevent.event.cdp.CDPStructuredEvent;
+import com.sequenceiq.cloudbreak.structuredevent.event.cdp.CDPStructuredRestCallEvent;
 import com.sequenceiq.cloudbreak.structuredevent.repository.CDPPagingStructuredEventRepository;
+import com.sequenceiq.cloudbreak.structuredevent.repository.CDPStructuredEventRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class CDPStructuredEventDBServiceTest {
@@ -29,6 +38,12 @@ public class CDPStructuredEventDBServiceTest {
 
     @Mock
     private CDPPagingStructuredEventRepository pagingStructuredEventRepository;
+
+    @Mock
+    private ConversionService conversionService;
+
+    @Mock
+    private CDPStructuredEventRepository structuredEventRepository;
 
     @Test
     public void testGetPagedEventsOfResourceWhenEventTypesEmpty() {
@@ -52,5 +67,38 @@ public class CDPStructuredEventDBServiceTest {
 
         List actual = eventTypes.getValue();
         Assertions.assertEquals(List.of(NOTIFICATION), actual);
+    }
+
+    @Test
+    public void testCreateWhenResourceCrnIsNull() {
+        CDPStructuredEvent event = new CDPStructuredRestCallEvent();
+        CDPOperationDetails operation = new CDPOperationDetails();
+        operation.setResourceCrn(null);
+        event.setOperation(operation);
+        underTest.create(event);
+        verify(conversionService, never()).convert(event, CDPStructuredEventEntity.class);
+    }
+
+    @Test
+    public void testCreateWhenResourceCrnIsEmpty() {
+        CDPStructuredEvent event = new CDPStructuredRestCallEvent();
+        CDPOperationDetails operation = new CDPOperationDetails();
+        operation.setResourceCrn("");
+        event.setOperation(operation);
+        underTest.create(event);
+        verify(conversionService, never()).convert(event, CDPStructuredEventEntity.class);
+    }
+
+    @Test
+    public void testCreateWhenResourceCrnIsNotEmpty() {
+        CDPStructuredEvent event = new CDPStructuredRestCallEvent();
+        CDPOperationDetails operation = new CDPOperationDetails();
+        operation.setResourceCrn("crn");
+        event.setOperation(operation);
+        CDPStructuredEventEntity entity = new CDPStructuredEventEntity();
+        when(conversionService.convert(event, CDPStructuredEventEntity.class)).thenReturn(entity);
+        underTest.create(event);
+        verify(conversionService, Mockito.times(1)).convert(event, CDPStructuredEventEntity.class);
+        verify(structuredEventRepository, Mockito.times(1)).save(entity);
     }
 }
