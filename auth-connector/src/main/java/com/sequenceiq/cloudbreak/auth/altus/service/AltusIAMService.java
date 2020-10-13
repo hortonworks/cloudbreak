a4158d9a88dbf3cd06c07d12e06c5d7748e5080d
Oliver Szabo <oleewere@gmail.com>
2020-10-13 09:29:34 +0200

    CB-9136. Create scheduled job for machine user cleanup for FreeIPA/DL+DH details: We should clean up generated machine users if those were not delete during cluster destroy (no significant increase with those users, but it's useful if we will have some cleanup scripts) that should be done for 2 components: - freeipa service - cloudbreak (core) service (against datalake/datahub) machine users: - 1 fluent user for freeipa (if cluster logs collection enabled) - 1 fluent user for datalake (if cluster logs collection enabled) - 1 fluent user for datahub (if cluster logs collection or metering enabled) and 1 TP user if WA enabled (overall freeipa/datalake: max. 1-1, datahub: max 2.) (all users have specific patterns) -> Using quartz job scheduler to do the cleanup on every sunday (0:00), clean users that has no attached clusters + user creation date is older than 3 days

--- auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/service/AltusIAMService.java
+++ auth-connector/src/main/java/com/sequenceiq/cloudbreak/auth/altus/service/AltusIAMService.java
@@ -102,4 +102,8 @@ public class AltusIAMService {
     public void clearMachineUser(String machineUserName, String actorCrn, String accountId) {
         clearMachineUser(machineUserName, actorCrn, accountId, false);
     }
+
+    public List<UserManagementProto.MachineUser> getAllMachineUsersForAccount(String actorCrn, String accountId) {
+        return umsClient.listAllMachineUsers(actorCrn, accountId, true, true, Optional.empty());
+    }
 }
---
+++ core/src/main/java/com/sequenceiq/cloudbreak/job/altus/CloudbreakUMSCleanupJob.java
@@ -0,0 +1,139 @@
+package com.sequenceiq.cloudbreak.job.altus;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.cloudera.thunderhead.service.usermanagement.UserManagementProto;
+import com.sequenceiq.cloudbreak.auth.altus.Crn;
+import com.sequenceiq.cloudbreak.quartz.cleanup.UMSCleanupConfig;
+import com.sequenceiq.cloudbreak.quartz.cleanup.job.UMSCleanupJob;
+import com.sequenceiq.cloudbreak.service.altus.AltusMachineUserService;
+import com.sequenceiq.cloudbreak.service.stack.StackService;
+
+@Component
+public class CloudbreakUMSCleanupJob extends UMSCleanupJob {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CloudbreakUMSCleanupJob.class);
+
+    private static final String DATABUS_TP_MACHINUE_USER_PATTERN = "datahub-wa-publisher-%s";
+
+    private final UMSCleanupConfig umsCleanupConfig;
+
+    private final AltusMachineUserService altusMachineUserService;
+
+    private final StackService stackService;
+
+    public CloudbreakUMSCleanupJob(UMSCleanupConfig umsCleanupConfig,
+            AltusMachineUserService altusMachineUserService, StackService stackService) {
+        this.umsCleanupConfig = umsCleanupConfig;
+        this.altusMachineUserService = altusMachineUserService;
+        this.stackService = stackService;
+    }
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        LOGGER.debug("Cleaning up unused UMS resources (machine users) has started.");
+        List<Crn> allStackCrns = getAllStackCrns();
+        String datalakeName = Crn.Service.DATALAKE.getName();
+        Map<String, Set<String>> datalakeMachineUsers = getPossibleDatalakeMachineUsers(allStackCrns, datalakeName);
+        String datahubName = Crn.Service.DATAHUB.getName();
+        List<DatahubMachineUsers> datahubMachineUsers = getPossibleDatahubMachineUsers(allStackCrns, datahubName);
+        Map<String, Set<String>> datahubFluentUsers = getPossibleDatahubFluentUsers(datahubMachineUsers);
+        Map<String, Set<String>> datahubTpUsers = getPossibleDatahubTpUsers(datahubMachineUsers);
+        for (Map.Entry<String, Set<String>> entry : datalakeMachineUsers.entrySet()) {
+            String accountId = entry.getKey();
+            List<UserManagementProto.MachineUser> machineUsers =
+                    altusMachineUserService.getAllInternalMachineUsers(accountId);
+            Set<String> datalakeMachineUsersForAccount = entry.getValue();
+            Set<String> datahubFluentMachineForAccount = datahubFluentUsers.getOrDefault(accountId, new HashSet<>());
+            Set<String> datahubTPMachineForAccount = datahubTpUsers.getOrDefault(accountId, new HashSet<>());
+            for (UserManagementProto.MachineUser machineUser : machineUsers) {
+                String name = machineUser.getMachineUserName();
+                if (name.startsWith("datalake-fluent") && !datalakeMachineUsersForAccount.contains(name)) {
+                    cleanupMachineUser(accountId, machineUser, name);
+                } else if (name.startsWith("datahub-fluent") && !datahubFluentMachineForAccount.contains(name)) {
+                    cleanupMachineUser(accountId, machineUser, name);
+                } else if (name.startsWith("datahub-wa-publisher") && !datahubTPMachineForAccount.contains(name)) {
+                    cleanupMachineUser(accountId, machineUser, name);
+                }
+            }
+        }
+        LOGGER.debug("Cleaning up unused UMS resources (machine users) has finished.");
+    }
+
+    private Map<String, Set<String>> getPossibleDatahubTpUsers(List<DatahubMachineUsers> datahubMachineUsers) {
+        return datahubMachineUsers
+                .stream()
+                .collect(Collectors.groupingBy(DatahubMachineUsers::getAccountId,
+                        Collectors.mapping(
+                                DatahubMachineUsers::getTelemetryPublisherUser,
+                                Collectors.toSet()
+                        )));
+    }
+
+    private Map<String, Set<String>> getPossibleDatahubFluentUsers(List<DatahubMachineUsers> datahubMachineUsers) {
+        return datahubMachineUsers
+                .stream()
+                .collect(Collectors.groupingBy(DatahubMachineUsers::getAccountId,
+                Collectors.mapping(
+                        DatahubMachineUsers::getFluentUser,
+                        Collectors.toSet()
+                )));
+    }
+
+    private List<DatahubMachineUsers> getPossibleDatahubMachineUsers(List<Crn> allStackCrns, String datahubName) {
+        return allStackCrns
+                .stream()
+                .filter(crn -> datahubName.equals(crn.getService().getName()))
+                .map(crn -> {
+                    String fluentUserName = altusMachineUserService.getFluentDatabusMachineUserName(datahubName, crn.getResource());
+                    String tpUserName = String.format(DATABUS_TP_MACHINUE_USER_PATTERN, crn.getResource());
+                    return new DatahubMachineUsers(crn.getAccountId(), fluentUserName, tpUserName);
+                })
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Set<String>> getPossibleDatalakeMachineUsers(List<Crn> allStackCrns, String datalakeName) {
+        return allStackCrns
+                .stream()
+                .filter(crn -> datalakeName.equals(crn.getService().getName()))
+                .collect(Collectors.groupingBy(Crn::getAccountId,
+                        Collectors.mapping(
+                                s -> altusMachineUserService.getFluentDatabusMachineUserName(datalakeName, s.getResource()),
+                                Collectors.toSet()
+                        )));
+    }
+
+    private List<Crn> getAllStackCrns() {
+        return stackService
+                .getAllAlive()
+                .stream()
+                .map(s -> Crn.fromString(s.getCrn()))
+                .collect(Collectors.toList());
+    }
+
+    private void cleanupMachineUser(String accountId, UserManagementProto.MachineUser machineUser, String name) {
+        LOGGER.debug("Cannot found stack for machine user {} (account: {}). Checks that if it can be deleted.",
+                name, accountId);
+        Instant beforeTime = Instant.now().minus(umsCleanupConfig.getMaxAgeDays(), ChronoUnit.DAYS);
+        Instant creationTime = new Date(machineUser.getCreationDateMs()).toInstant();
+        if (creationTime.isBefore(beforeTime)) {
+            altusMachineUserService.cleanupMachineUser(name, accountId);
+        } else {
+            LOGGER.debug("Machine user with name {} is not old enough yet, skipping cleanup. (account id: {})",
+                    name, accountId);
+        }
+    }
+}
