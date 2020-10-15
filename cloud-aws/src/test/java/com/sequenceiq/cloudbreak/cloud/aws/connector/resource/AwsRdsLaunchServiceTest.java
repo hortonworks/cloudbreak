0675b84ea506e8e1cf7d6686ad58f52616a8a0f3
Lajos Rodek <15786876+lajosrodek@users.noreply.github.com>
2020-10-15 17:32:56 +0200

    CB-8107 CB-8108 Enable SSL enforcement for AWS & Azure DL external DB
    * Rebased onto #9215
    * Enable SSL enforcement for the external DB of data lakes. The list of
      cloud platforms where this is applied is controlled by the new
      Datalake service property
      datalake.supported.externaldb.sslenforcement.platform (see
      PlatformConfig.dbServiceSslEnforcementSupportedPlatforms).
      * In AWS, the CF stack is extended with a new
        AWS::RDS::DBParameterGroup resource that has rds.force_ssl=1.
        Supported engine versions: 9.5, 9.6, 10.x, 11.x, 12.x, 13.x.
      * In Azure, the Microsoft.DBforPostgreSQL/servers resource will
        have sslEnforcement=Enabled and minimalTlsVersion=TLS1_2.
    * Data hub DBs and embedded DBs of any cluster type are not affected.
    * Added new unit tests and updated existing ones to use JUnit 5 and
      AssertJ.

---
+++ cloud-aws/src/test/java/com/sequenceiq/cloudbreak/cloud/aws/connector/resource/AwsRdsLaunchServiceTest.java
@@ -0,0 +1,222 @@
+package com.sequenceiq.cloudbreak.cloud.aws.connector.resource;
+
+import static com.sequenceiq.cloudbreak.cloud.aws.connector.resource.AwsRdsLaunchService.CREATED_DB_INSTANCE;
+import static com.sequenceiq.cloudbreak.cloud.aws.connector.resource.AwsRdsLaunchService.CREATED_DB_PARAMETER_GROUP;
+import static com.sequenceiq.cloudbreak.cloud.aws.connector.resource.AwsRdsLaunchService.CREATED_DB_SUBNET_GROUP;
+import static com.sequenceiq.cloudbreak.cloud.aws.connector.resource.AwsRdsLaunchService.HOSTNAME;
+import static com.sequenceiq.cloudbreak.cloud.aws.connector.resource.AwsRdsLaunchService.PORT;
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
+import com.amazonaws.services.cloudformation.model.DescribeStacksRequest;
+import com.amazonaws.services.cloudformation.waiters.AmazonCloudFormationWaiters;
+import com.amazonaws.waiters.Waiter;
+import com.sequenceiq.cloudbreak.cloud.aws.AwsClient;
+import com.sequenceiq.cloudbreak.cloud.aws.AwsStackRequestHelper;
+import com.sequenceiq.cloudbreak.cloud.aws.CloudFormationStackUtil;
+import com.sequenceiq.cloudbreak.cloud.aws.CloudFormationTemplateBuilder;
+import com.sequenceiq.cloudbreak.cloud.aws.CloudFormationTemplateBuilder.RDSModelContext;
+import com.sequenceiq.cloudbreak.cloud.aws.client.AmazonCloudFormationRetryClient;
+import com.sequenceiq.cloudbreak.cloud.aws.view.AwsCredentialView;
+import com.sequenceiq.cloudbreak.cloud.context.AuthenticatedContext;
+import com.sequenceiq.cloudbreak.cloud.context.CloudContext;
+import com.sequenceiq.cloudbreak.cloud.model.AvailabilityZone;
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
+import com.sequenceiq.cloudbreak.cloud.model.CloudResourceStatus;
+import com.sequenceiq.cloudbreak.cloud.model.DatabaseServer;
+import com.sequenceiq.cloudbreak.cloud.model.DatabaseStack;
+import com.sequenceiq.cloudbreak.cloud.model.Location;
+import com.sequenceiq.cloudbreak.cloud.model.Network;
+import com.sequenceiq.cloudbreak.cloud.model.Region;
+import com.sequenceiq.cloudbreak.cloud.model.ResourceStatus;
+import com.sequenceiq.cloudbreak.cloud.model.Security;
+import com.sequenceiq.cloudbreak.cloud.model.Subnet;
+import com.sequenceiq.cloudbreak.cloud.notification.PersistenceNotifier;
+import com.sequenceiq.common.api.type.OutboundInternetTraffic;
+import com.sequenceiq.common.api.type.ResourceType;
+
+@ExtendWith(MockitoExtension.class)
+class AwsRdsLaunchServiceTest {
+
+    private static final Long STACK_ID = 1234L;
+
+    private static final String STACK_NAME = "myStack";
+
+    private static final String STACK_NAME_CF = STACK_NAME + "-" + STACK_ID;
+
+    private static final String PLATFORM = "AWS";
+
+    private static final String VARIANT = "";
+
+    private static final String REGION = "eu-central-1";
+
+    private static final String AVAILABILITY_ZONE = "eu-central-1b";
+
+    private static final String USER_ID = UUID.randomUUID().toString();
+
+    private static final String ACCOUNT_ID = UUID.randomUUID().toString();
+
+    private static final String SUBNET_CIDR = "0.0.0.0/0";
+
+    private static final String SUBNET_ID = "subnet-lkewflerwkj";
+
+    private static final String NETWORK_CIDR = "127.0.0.1/32";
+
+    private static final String SECURITY_GROUP_ID = "sg-98e47c";
+
+    private static final String OUT_DB_INSTANCE = "db-" + UUID.randomUUID().toString();
+
+    private static final String OUT_HOSTNAME = OUT_DB_INSTANCE + ".oijeojwodihfoih." + REGION + ".rds.amazonaws.com";
+
+    private static final String OUT_PORT = "23456";
+
+    private static final String OUT_DB_SUBNET_GROUP = "dsg-kfhoiehjoehij";
+
+    private static final String OUT_DB_PARAMETER_GROUP = "dpg-lojdfoiwejpoekf";
+
+    private static final String CF_TEMPLATE = "{\"template\" : \"foo\"}";
+
+    @Mock
+    private CloudFormationStackUtil cfStackUtil;
+
+    @Mock
+    private AwsClient awsClient;
+
+    @Mock
+    private CloudFormationTemplateBuilder cloudFormationTemplateBuilder;
+
+    @Mock
+    private AwsStackRequestHelper awsStackRequestHelper;
+
+    @Mock
+    private PersistenceNotifier resourceNotifier;
+
+    @Mock
+    private AmazonCloudFormationRetryClient cfRetryClient;
+
+    @Mock
+    private AmazonCloudFormationClient cfClient;
+
+    @Mock
+    private AmazonCloudFormationWaiters cfWaiters;
+
+    @Mock
+    private Waiter<DescribeStacksRequest> creationWaiter;
+
+    @Captor
+    private ArgumentCaptor<RDSModelContext> rdsModelContextCaptor;
+
+    @InjectMocks
+    private AwsRdsLaunchService underTest;
+
+    private AuthenticatedContext authenticatedContext;
+
+    @BeforeEach
+    void setUp() {
+        Region region = Region.region(REGION);
+        AvailabilityZone availabilityZone = AvailabilityZone.availabilityZone(AVAILABILITY_ZONE);
+        Location location = Location.location(region, availabilityZone);
+        CloudContext cloudContext = new CloudContext(STACK_ID, STACK_NAME, PLATFORM, VARIANT, location, USER_ID, ACCOUNT_ID);
+
+        CloudCredential cloudCredential = new CloudCredential();
+        authenticatedContext = new AuthenticatedContext(cloudContext, cloudCredential);
+
+        when(cfStackUtil.getCfStackName(authenticatedContext)).thenReturn(STACK_NAME_CF);
+
+        when(awsClient.createCloudFormationRetryClient(isA(AwsCredentialView.class), eq(REGION))).thenReturn(cfRetryClient);
+        when(awsClient.createCloudFormationClient(isA(AwsCredentialView.class), eq(REGION))).thenReturn(cfClient);
+
+        when(cfRetryClient.describeStacks(isA(DescribeStacksRequest.class))).thenThrow(new AmazonServiceException("Stack not found"));
+
+        when(cfClient.waiters()).thenReturn(cfWaiters);
+        when(cfWaiters.stackCreateComplete()).thenReturn(creationWaiter);
+
+        Map<String, String> cfOutputs = Map.ofEntries(
+                entry(HOSTNAME, OUT_HOSTNAME),
+                entry(PORT, OUT_PORT),
+                entry(CREATED_DB_INSTANCE, OUT_DB_INSTANCE),
+                entry(CREATED_DB_SUBNET_GROUP, OUT_DB_SUBNET_GROUP),
+                entry(CREATED_DB_PARAMETER_GROUP, OUT_DB_PARAMETER_GROUP));
+        when(cfStackUtil.getOutputs(STACK_NAME_CF, cfRetryClient)).thenReturn(cfOutputs);
+
+        when(cloudFormationTemplateBuilder.build(isA(RDSModelContext.class))).thenReturn(CF_TEMPLATE);
+    }
+
+    @Test
+    void launchTestUseSslEnforcementWhenFalse() throws Exception {
+        launchTestUseSslEnforcementInternal(false);
+    }
+
+    @Test
+    void launchTestUseSslEnforcementWhenTrue() throws Exception {
+        launchTestUseSslEnforcementInternal(true);
+    }
+
+    private void launchTestUseSslEnforcementInternal(boolean useSslEnforcement) throws Exception {
+        List<CloudResourceStatus> cloudResourceStatuses = underTest.launch(authenticatedContext, createDatabaseStack(useSslEnforcement), resourceNotifier);
+
+        assertThat(cloudResourceStatuses).isNotNull();
+        assertThat(cloudResourceStatuses).hasSize(6);
+
+        cloudResourceStatuses.forEach(status -> assertThat(status).isNotNull());
+        cloudResourceStatuses.forEach(status -> assertThat(status.getCloudResource()).isNotNull());
+
+        checkOutputResource(cloudResourceStatuses, ResourceType.RDS_HOSTNAME, OUT_HOSTNAME);
+        checkOutputResource(cloudResourceStatuses, ResourceType.RDS_PORT, OUT_PORT);
+        checkOutputResource(cloudResourceStatuses, ResourceType.RDS_INSTANCE, OUT_DB_INSTANCE);
+        checkOutputResource(cloudResourceStatuses, ResourceType.RDS_DB_SUBNET_GROUP, OUT_DB_SUBNET_GROUP);
+        checkOutputResource(cloudResourceStatuses, ResourceType.RDS_DB_PARAMETER_GROUP, OUT_DB_PARAMETER_GROUP);
+        checkOutputResource(cloudResourceStatuses, ResourceType.CLOUDFORMATION_STACK, STACK_NAME_CF);
+
+        verify(cloudFormationTemplateBuilder).build(rdsModelContextCaptor.capture());
+        RDSModelContext rdsModelContext = rdsModelContextCaptor.getValue();
+        assertThat(rdsModelContext).isNotNull();
+        Boolean useSslEnforcementResult = (Boolean) ReflectionTestUtils.getField(rdsModelContext, "useSslEnforcement");
+        assertThat(useSslEnforcementResult).isNotNull();
+        assertThat(useSslEnforcementResult.booleanValue()).isEqualTo(useSslEnforcement);
+    }
+
+    private DatabaseStack createDatabaseStack(boolean useSslEnforcement) {
+        Subnet subnet = new Subnet(SUBNET_CIDR);
+        Network network = new Network(subnet, List.of(NETWORK_CIDR), OutboundInternetTraffic.ENABLED);
+        network.putParameter("subnetId", SUBNET_ID);
+        network.putParameter("vpcCidr", NETWORK_CIDR);
+
+        Security security = new Security(Collections.emptyList(), List.of(SECURITY_GROUP_ID));
+        DatabaseServer databaseServer = DatabaseServer.builder().useSslEnforcement(useSslEnforcement).security(security).build();
+
+        return new DatabaseStack(network, databaseServer, Collections.emptyMap(), null);
+    }
+
+    private void checkOutputResource(List<CloudResourceStatus> cloudResourceStatuses, ResourceType type, String expectedName) {
+        Optional<CloudResourceStatus> cloudResourceStatusOptional =
+                cloudResourceStatuses.stream().filter(resource -> resource.getCloudResource().getType() == type).findFirst();
+        assertThat(cloudResourceStatusOptional.isPresent()).isTrue();
+        CloudResourceStatus cloudResourceStatus = cloudResourceStatusOptional.get();
+        assertThat(cloudResourceStatus.getStatus()).isEqualTo(ResourceStatus.CREATED);
+        assertThat(cloudResourceStatus.getCloudResource().getName()).isEqualTo(expectedName);
+    }
+
+}
---
+++ cloud-aws/src/test/java/com/sequenceiq/cloudbreak/cloud/aws/view/AwsRdsDbParameterGroupViewTest.java
@@ -0,0 +1,134 @@
+package com.sequenceiq.cloudbreak.cloud.aws.view;
+
+import static com.sequenceiq.cloudbreak.cloud.aws.view.AwsRdsDbParameterGroupView.ENGINE_VERSION;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.sequenceiq.cloudbreak.cloud.model.DatabaseEngine;
+import com.sequenceiq.cloudbreak.cloud.model.DatabaseServer;
+
+class AwsRdsDbParameterGroupViewTest {
+
+    @Test
+    void getDBParameterGroupNameTestWhenNullServerId() {
+        DatabaseServer databaseServer = DatabaseServer.builder().build();
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThat(underTest.getDBParameterGroupName()).isNull();
+    }
+
+    @Test
+    void getDBParameterGroupNameTestWhenHasServerId() {
+        DatabaseServer databaseServer = DatabaseServer.builder().serverId("myserver").build();
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThat(underTest.getDBParameterGroupName()).isEqualTo("dpg-myserver");
+    }
+
+    @Test
+    void getDBParameterGroupFamilyTestWhenNullEngine() {
+        DatabaseServer databaseServer = DatabaseServer.builder().build();
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThat(underTest.getDBParameterGroupFamily()).isNull();
+    }
+
+    // Note: There is no easy way to test the "bad engine variant" case as enum classes are final and hard to mock.
+
+    @Test
+    void getDBParameterGroupFamilyTestWhenPgSqlAndNullVersion() {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThatCode(underTest::getDBParameterGroupFamily).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void getDBParameterGroupFamilyTestWhenPgSqlAndBadVersionFormat() {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+        databaseServer.putParameter(ENGINE_VERSION, "latest");
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThatCode(underTest::getDBParameterGroupFamily).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void getDBParameterGroupFamilyTestWhenPgSqlAndMissingMinorVersion() {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+        databaseServer.putParameter(ENGINE_VERSION, "10");
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThatCode(underTest::getDBParameterGroupFamily).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void getDBParameterGroupFamilyTestWhenPgSqlAndMajorVersionNumericOverflow() {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+        databaseServer.putParameter(ENGINE_VERSION, "12345678901234567890.1");
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThatCode(underTest::getDBParameterGroupFamily).isInstanceOf(NumberFormatException.class);
+    }
+
+    static Object[][] unsupportedMajorVersionDataProvider() {
+        return new Object[][]{
+                // testCaseName version
+                {"version 0.1", "0.1"},
+                {"version 1.1", "1.1"},
+                {"version 2.1", "2.1"},
+                {"version 3.1", "3.1"},
+                {"version 4.1", "4.1"},
+                {"version 5.1", "5.1"},
+                {"version 6.1", "6.1"},
+                {"version 7.1", "7.1"},
+                {"version 8.1", "8.1"},
+                {"version 123.1", "123.1"},
+        };
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("unsupportedMajorVersionDataProvider")
+    void getDBParameterGroupFamilyTestWhenPgSqlAndUnsupportedMajorVersion(String testCaseName, String version) {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+        databaseServer.putParameter(ENGINE_VERSION, version);
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThatCode(underTest::getDBParameterGroupFamily).isInstanceOf(IllegalStateException.class);
+    }
+
+    static Object[][] validVersionDataProvider() {
+        return new Object[][]{
+                // testCaseName version familyExpected
+                {"version 9.5", "9.5", "postgres9.5"},
+                {"version 9.6", "9.6", "postgres9.6"},
+                {"version 10.1", "10.1", "postgres10"},
+                {"version 11.1", "11.1", "postgres11"},
+                {"version 12.1", "12.1", "postgres12"},
+                {"version 13.1", "13.1", "postgres13"},
+        };
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("validVersionDataProvider")
+    void getDBParameterGroupFamilyTestWhenPgSqlAndValidVersion(String testCaseName, String version, String familyExpected) {
+        DatabaseServer databaseServer = DatabaseServer.builder().engine(DatabaseEngine.POSTGRESQL).build();
+        databaseServer.putParameter(ENGINE_VERSION, version);
+
+        AwsRdsDbParameterGroupView underTest = new AwsRdsDbParameterGroupView(databaseServer);
+
+        assertThat(underTest.getDBParameterGroupFamily()).isEqualTo(familyExpected);
+    }
+
+}
