477127c0df166c8f05ae1f6a8cb283b788ccf2cc
lnardai <laci.nardai@gmail.com>
2020-10-20 13:49:25 +0200

    CB-9382 make sure getResourceCrnByResourceName for BlueprintService is not returning null and framework also responds with 404 if resourceCrn cannot be found

--- authorization-common/src/main/java/com/sequenceiq/authorization/service/ResourceNamePermissionChecker.java
+++ authorization-common/src/main/java/com/sequenceiq/authorization/service/ResourceNamePermissionChecker.java
@@ -3,6 +3,7 @@ package com.sequenceiq.authorization.service;
 import java.lang.annotation.Annotation;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.authorization.annotation.CheckPermissionByResourceName;
 import com.sequenceiq.authorization.annotation.ResourceName;
 import com.sequenceiq.authorization.resource.AuthorizationResourceAction;
+import com.sequenceiq.cloudbreak.common.exception.NotFoundException;
 
 @Component
 public class ResourceNamePermissionChecker extends ResourcePermissionChecker<CheckPermissionByResourceName> {
@@ -26,6 +28,9 @@ public class ResourceNamePermissionChecker extends ResourcePermissionChecker<Che
         String resourceName = getCommonPermissionCheckingUtils().getParameter(proceedingJoinPoint, methodSignature, ResourceName.class, String.class);
         ResourceBasedCrnProvider resourceBasedCrnProvider = getCommonPermissionCheckingUtils().getResourceBasedCrnProvider(action);
         String resourceCrn = resourceBasedCrnProvider.getResourceCrnByResourceName(resourceName);
+        if (StringUtils.isEmpty(resourceCrn)) {
+            throw new NotFoundException(String.format("Could not find resourceCrn for resource by name: %s", resourceName));
+        }
         if (getCommonPermissionCheckingUtils().legacyAuthorizationNeeded()) {
             getCommonPermissionCheckingUtils().checkPermissionForUserOnResource(action, userCrn, resourceCrn);
         } else {
