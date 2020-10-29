52b94ee338e01360b61a0725c67299a987fc2cae
Attila Kanto <attila.kanto@gmail.com>
2020-10-29 12:39:39 +0100

    CB-9527 Clean up the sensitive details from Audit API and from OperationDetails. It breaks swagger compatibility, but that data shouldn't be there.

--- core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/audits/responses/AuditEventV4Response.java
+++ core-api/src/main/java/com/sequenceiq/cloudbreak/api/endpoint/v4/audits/responses/AuditEventV4Response.java
@@ -1,13 +1,9 @@
 package com.sequenceiq.cloudbreak.api.endpoint.v4.audits.responses;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEvent;
 import com.sequenceiq.common.model.annotations.Immutable;
 import com.sequenceiq.common.model.annotations.TransformGetterType;
-import com.sequenceiq.cloudbreak.structuredevent.event.legacy.OperationDetails;
-import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEvent;
-import com.sequenceiq.cloudbreak.structuredevent.event.StructuredFlowEvent;
-import com.sequenceiq.cloudbreak.structuredevent.event.StructuredNotificationEvent;
-import com.sequenceiq.cloudbreak.structuredevent.event.StructuredRestCallEvent;
 
 @Immutable
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -26,35 +22,12 @@ public class AuditEventV4Response {
         this.structuredEvent = structuredEvent;
     }
 
-    public OperationDetails getOperation() {
-        return structuredEvent == null ? null : structuredEvent.getOperation();
-    }
-
     public Long getAuditId() {
         return auditId;
-    }
-
-    public String getStatus() {
-        return structuredEvent == null ? null : structuredEvent.getStatus();
-    }
-
-    public Long getDuration() {
-        return structuredEvent == null ? null : structuredEvent.getDuration();
     }
 
     public StructuredEvent getStructuredEvent() {
         return structuredEvent;
     }
 
-    public StructuredFlowEvent getRawFlowEvent() {
-        return structuredEvent instanceof StructuredFlowEvent ? (StructuredFlowEvent) structuredEvent : null;
-    }
-
-    public StructuredRestCallEvent getRawRestEvent() {
-        return structuredEvent instanceof StructuredRestCallEvent ? (StructuredRestCallEvent) structuredEvent : null;
-    }
-
-    public StructuredNotificationEvent getRawNotification() {
-        return structuredEvent instanceof StructuredNotificationEvent ? (StructuredNotificationEvent) structuredEvent : null;
-    }
 }
