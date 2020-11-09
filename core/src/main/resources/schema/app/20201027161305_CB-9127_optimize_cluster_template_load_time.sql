671c56af2890941d4d5c592f92712798416169e1
doktoric <rdoktorics@cloudera.com>
2020-11-09 14:31:52 +0100

    CB-9647 initial nodecount default now is 0 to avoid rolling upgrade issues

--- core/src/main/resources/schema/app/20201027161305_CB-9127_optimize_cluster_template_load_time.sql
+++ core/src/main/resources/schema/app/20201027161305_CB-9127_optimize_cluster_template_load_time.sql
@@ -5,7 +5,9 @@ DELETE FROM clustertemplate WHERE status='DEFAULT';
 ALTER TABLE clustertemplate ADD COLUMN IF NOT EXISTS clouderaRuntimeVersion VARCHAR(255);
 
 ALTER TABLE instancegroup ADD COLUMN IF NOT EXISTS initialNodeCount INTEGER;
+ALTER TABLE instancegroup ALTER COLUMN initialNodeCount SET DEFAULT 0;
 UPDATE instancegroup SET initialNodeCount=(SELECT COUNT(*) FROM instancemetadata WHERE instancegroup_id = instancegroup.id);
+
 -- //@UNDO
 -- SQL to undo the change goes here.
 
