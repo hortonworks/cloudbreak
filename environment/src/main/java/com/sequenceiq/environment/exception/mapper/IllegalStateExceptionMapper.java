3b0378bc7fe230ab2be8a4f28fd4df9f5c75b5d7
Gergely Topolyai <gtopolyai@hortonworks.com>
2020-11-20 15:18:13 +0100

    CB-9735 Internal server error when flow under operation occurring. In case of IllegalStateException we will transform the status code to a BAD_REQUEST in all microserices

---
+++ environment/src/main/java/com/sequenceiq/environment/exception/mapper/IllegalStateExceptionMapper.java
@@ -0,0 +1,19 @@
+package com.sequenceiq.environment.exception.mapper;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IllegalStateExceptionMapper extends BaseExceptionMapper<IllegalStateException> {
+
+    @Override
+    Status getResponseStatus() {
+        return Status.BAD_REQUEST;
+    }
+
+    @Override
+    Class<IllegalStateException> getExceptionType() {
+        return IllegalStateException.class;
+    }
+}
