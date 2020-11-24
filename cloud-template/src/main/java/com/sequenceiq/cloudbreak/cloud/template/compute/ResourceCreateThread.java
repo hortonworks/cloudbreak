c7890e30d33adc5204feff7194a5c7973d3242fc
Dénes Berger <dberger@cloudbreak.com>
2020-11-24 20:31:12 +0100

    CB-9502 Add logging to resource creation and adjust timeout for volume set creation

--- cloud-template/src/main/java/com/sequenceiq/cloudbreak/cloud/template/compute/ResourceCreateThread.java
+++ cloud-template/src/main/java/com/sequenceiq/cloudbreak/cloud/template/compute/ResourceCreateThread.java
@@ -43,6 +43,11 @@ public class ResourceCreateThread implements Callable<ResourceRequestResult<List
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceCreateThread.class);
 
+    private static final List<ResourceType> VOLUME_SET_RESOURCE_TYPES = List.of(ResourceType.AWS_VOLUMESET, ResourceType.AZURE_VOLUMESET);
+
+    // half an hour with 1 sec delays
+    private static final int VOLUME_SET_POLLING_ATTEMPTS = 1800;
+
     @Inject
     private ResourceBuilders resourceBuilders;
 
@@ -106,7 +111,7 @@ public class ResourceCreateThread implements Callable<ResourceRequestResult<List
                             resources.stream().map(resource -> new CloudResourceStatus(resource, ResourceStatus.IN_PROGRESS, privateId)).forEach(results::add);
                         } else {
                             PollTask<List<CloudResourceStatus>> task = resourcePollTaskFactory.newPollResourceTask(builder, auth, resources, context, true);
-                            List<CloudResourceStatus> pollerResult = syncPollingScheduler.schedule(task);
+                            List<CloudResourceStatus> pollerResult = scheduleTask(task, resources);
                             for (CloudResourceStatus resourceStatus : pollerResult) {
                                 resourceStatus.setPrivateId(privateId);
                             }
@@ -129,6 +134,18 @@ public class ResourceCreateThread implements Callable<ResourceRequestResult<List
         }
 
         return new ResourceRequestResult<>(FutureResult.SUCCESS, results);
+    }
+
+    private List<CloudResourceStatus> scheduleTask(PollTask<List<CloudResourceStatus>> task, List<CloudResource> resources) throws Exception {
+        boolean volumeSet = resources != null && resources.stream().allMatch(r -> VOLUME_SET_RESOURCE_TYPES.contains(r.getType()));
+        if (volumeSet) {
+            LOGGER.debug("Scheduling volume set poller with shorter timeout.");
+            return syncPollingScheduler.schedule(task, SyncPollingScheduler.POLLING_INTERVAL, VOLUME_SET_POLLING_ATTEMPTS,
+                    SyncPollingScheduler.FAILURE_TOLERANT_ATTEMPT);
+        } else {
+            LOGGER.debug("Scheduling resource poller with default timeout.");
+            return syncPollingScheduler.schedule(task);
+        }
     }
 
     private void persistResources(AuthenticatedContext auth, Iterable<CloudResource> cloudResources) {
