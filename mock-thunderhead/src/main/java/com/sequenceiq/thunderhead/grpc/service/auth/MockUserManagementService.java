75f5962be045ece3d19194d5ea6e209d2599d70c
Jamison Bennett <jamison.bennett@gmail.com>
2020-12-08 11:25:19 -0600

    CB-9931: Remove the CDP_FREEIPA_HEALTH_CHECK entitlement
    Remove the CDP_FREEIPA_HEALTH_CHECK entitlement. The feature has been
    validated.

--- mock-thunderhead/src/main/java/com/sequenceiq/thunderhead/grpc/service/auth/MockUserManagementService.java
+++ mock-thunderhead/src/main/java/com/sequenceiq/thunderhead/grpc/service/auth/MockUserManagementService.java
@@ -190,8 +190,6 @@ public class MockUserManagementService extends UserManagementImplBase {
 
     private static final String CDP_FREEIPA_HA_REPAIR = "CDP_FREEIPA_HA_REPAIR";
 
-    private static final String CDP_FREEIPA_HEALTH_CHECK = "CDP_FREEIPA_HEALTH_CHECK";
-
     private static final String CDP_CLOUD_STORAGE_VALIDATION = "CDP_CLOUD_STORAGE_VALIDATION";
 
     private static final String CDP_RUNTIME_UPGRADE = "CDP_RUNTIME_UPGRADE";
@@ -279,9 +277,6 @@ public class MockUserManagementService extends UserManagementImplBase {
 
     @Value("${auth.mock.freeipa.ha.repair.enable}")
     private boolean enableFreeIpaHaRepair;
-
-    @Value("${auth.mock.freeipa.healthcheck.enable}")
-    private boolean enableFreeIpaHealthCheck;
 
     @Value("${auth.mock.cloudstoragevalidation.enable}")
     private boolean enableCloudStorageValidation;
@@ -592,9 +587,6 @@ public class MockUserManagementService extends UserManagementImplBase {
         }
         if (enableFreeIpaHaRepair) {
             builder.addEntitlements(createEntitlement(CDP_FREEIPA_HA_REPAIR));
-        }
-        if (enableFreeIpaHealthCheck) {
-            builder.addEntitlements(createEntitlement(CDP_FREEIPA_HEALTH_CHECK));
         }
         if (enableCloudStorageValidation) {
             builder.addEntitlements(createEntitlement(CDP_CLOUD_STORAGE_VALIDATION));
