25c0388eca196436d5d493dabdc4fc8d331a3d8c
Tamas Bihari <tbihari@hortonworks.com>
2020-12-16 12:34:10 +0100

    CB-10299 limit the length of the GCP buckets as it could not be longer than 63 characters

--- cloud-gcp/src/test/java/com/sequenceiq/cloudbreak/cloud/gcp/util/GcpLabelUtilTest.java
+++ cloud-gcp/src/test/java/com/sequenceiq/cloudbreak/cloud/gcp/util/GcpLabelUtilTest.java
@@ -1,6 +1,7 @@
 package com.sequenceiq.cloudbreak.cloud.gcp.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -31,5 +32,34 @@ class GcpLabelUtilTest {
         assertEquals("12474ddc-6e44-4f4c-806a-b197ef12cbb8", result.get(ENVIRONMENT_CRN.toLowerCase()));
         assertEquals("5d7-b645-7ccf9edbb73d-user-05ca1026-c028-466b-8943-b04f765fa3f6", result.get(CREATOR_CRN.toLowerCase()));
         assertEquals("-b645-7ccf9edbb73d-freeipa-8111d534-8c7e-4a68-a8ba-7ebb389a3a20", result.get(RESOURCE_CRN.toLowerCase()));
+    }
+
+    @Test
+    void transformValueWhenItsLengthLessThan63Chars() {
+        String originalValue = "gpc-test-value-which-is-short";
+
+        String result = GcpLabelUtil.transformValue(originalValue);
+
+        assertEquals(originalValue, result);
+    }
+
+    @Test
+    void transformValueWhenItsLengthMoreThan63Chars() {
+        String originalValue = "gcp-dev-cloudbreak-gcp-test-9257bdae358342cca05e674b3893563-387";
+
+        String result = GcpLabelUtil.transformValue(originalValue);
+
+        assertEquals(GcpLabelUtil.GCP_MAX_TAG_LEN, result.length());
+        assertEquals(originalValue.substring(originalValue.length() - GcpLabelUtil.GCP_MAX_TAG_LEN), result);
+    }
+
+    @Test
+    void transformValueWhenItsLengthMoreThan63CharsAndItIsACrn() {
+        String originalValue = "crn:cdp:environments:us-west-1:9d74eee4-1cad-45d7-b645-7ccf9edbb73d:environment:12474ddc-6e44-4f4c-806a-b197ef12cbb8";
+
+        String result = GcpLabelUtil.transformValue(originalValue);
+
+        assertTrue(result.length() < GcpLabelUtil.GCP_MAX_TAG_LEN);
+        assertEquals("12474ddc-6e44-4f4c-806a-b197ef12cbb8", result);
     }
 }
