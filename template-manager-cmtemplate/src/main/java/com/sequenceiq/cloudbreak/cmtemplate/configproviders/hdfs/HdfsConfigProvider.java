316bc4e05c8c8f3b5dbb91ad1a7b567f169b6487
Bryce Derriso <b.derriso@gmail.com>
2021-01-05 14:26:25 -0600

    Manually revert CB-7306.
    * Remove code that added dfs_replication to hdfs service configs
    * Add `dfs_replication` values to templates:
      * 7.2.0
      * 7.2.1
      * 7.2.2
      * 7.2.6
      * 7.2.7
      * 7.2.7 HA
      * 7.2.8
      * 7.2.8 HA
    
    Remove dead imports.
    
    Trailing comma, which is not valid JSON.

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hdfs/HdfsConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hdfs/HdfsConfigProvider.java
@@ -1,20 +1,14 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.hdfs;
 
-import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.CLOUDERAMANAGER_VERSION_7_2_0;
-import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.isVersionNewerOrEqualThanLimited;
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
-import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateComponentConfigProvider;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.s3guard.S3GuardConfigProvider;
@@ -34,20 +28,8 @@ public class HdfsConfigProvider implements CmTemplateComponentConfigProvider {
 
         s3GuardConfigProvider.getServiceConfigs(templatePreparationObject, hdfsCoreSiteSafetyValveValue);
 
-        List<ApiClusterTemplateConfig> list = new ArrayList<>();
-        if (StringUtils.isNotEmpty(hdfsCoreSiteSafetyValveValue.toString())) {
-            list.add(config(CORE_SITE_SAFETY_VALVE, hdfsCoreSiteSafetyValveValue.toString()));
-        }
-        if (templatePreparationObject.getProductDetailsView() != null &&
-                templatePreparationObject.getProductDetailsView().getCm() != null &&
-                templatePreparationObject.getProductDetailsView().getCm().getVersion() != null) {
-            String cmVersion = templatePreparationObject.getProductDetailsView().getCm().getVersion();
-            if (templatePreparationObject.getStackType() == StackType.DATALAKE && isVersionNewerOrEqualThanLimited(cmVersion, CLOUDERAMANAGER_VERSION_7_2_0)) {
-                list.add(config("dfs_replication", "1"));
-                list.add(config("service_health_suppression_hdfs_verify_ec_with_topology", "true"));
-            }
-        }
-        return Collections.unmodifiableList(list);
+        return hdfsCoreSiteSafetyValveValue.toString().isEmpty() ? List.of()
+                : List.of(config(CORE_SITE_SAFETY_VALVE, hdfsCoreSiteSafetyValveValue.toString()));
     }
 
     @Override
