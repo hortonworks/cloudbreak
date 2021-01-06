9f5dbdfa0849d160699f735697c4b06246382da0
Arpad Farsang <afarsang@gmail.com>
2021-01-06 13:58:22 +0100

    try

--- core/src/main/resources/schema/app/20201210210553_CB-10095_Add_multiple_load_balancers_to_target_group.sql
+++ core/src/main/resources/schema/app/20201210210553_CB-10095_Add_multiple_load_balancers_to_target_group.sql
@@ -1,10 +1,6 @@
 -- // CB-10095 Add multiple load balancers to target group
 -- Migration SQL that makes the change goes here.
 
-CREATE TABLE IF NOT EXISTS targetgroup_loadbalancer (
-    targetgroupset_id     bigint NOT NULL,
-    loadbalancerset_id    bigint NOT NULL
-);
 
 
 -- //@UNDO
