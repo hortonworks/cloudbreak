132597886090d43cc47d3c187a526cf9a7566432
balazs.bihari <balazs.bihari@gmail.com>
2021-01-14 16:11:36 +0100

    CB-10630 Autoscale always authorize with owner instead of the logged in user

--- autoscale/src/main/java/com/sequenceiq/periscope/repository/HistoryRepository.java
+++ autoscale/src/main/java/com/sequenceiq/periscope/repository/HistoryRepository.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.repository.query.Param;
 
+import com.sequenceiq.cloudbreak.aspect.DisableHasPermission;
 import com.sequenceiq.cloudbreak.repository.BaseRepository;
-import com.sequenceiq.cloudbreak.aspect.HasPermission;
 import com.sequenceiq.cloudbreak.service.EntityType;
 import com.sequenceiq.periscope.domain.History;
 
-@HasPermission
+@DisableHasPermission
 @EntityType(entityClass = History.class)
 public interface HistoryRepository extends BaseRepository<History, Long> {
 
