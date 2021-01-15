631f11cacf0b7601fccc288a19a501eee52544d1
Dénes Berger <dberger@cloudera.com>
2021-01-15 11:00:35 +0100

    CB-10254 Add delete cluster for liftie connector
    And do a mass amount of refactoring.

--- environment/src/main/java/com/sequenceiq/environment/experience/liftie/ExperiencesByLiftie.java
+++ environment/src/main/java/com/sequenceiq/environment/experience/liftie/ExperiencesByLiftie.java
@@ -3,7 +3,8 @@ package com.sequenceiq.environment.experience.liftie;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.commons.lang3.NotImplementedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.environment.environment.dto.EnvironmentExperienceDto;
@@ -15,6 +16,8 @@ import com.sequenceiq.environment.experience.liftie.responses.ListClustersRespon
 @Component
 public class ExperiencesByLiftie implements Experience {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExperiencesByLiftie.class);
+
     private final LiftieApi liftieApi;
 
     public ExperiencesByLiftie(LiftieApi liftieApi) {
@@ -22,14 +25,20 @@ public class ExperiencesByLiftie implements Experience {
     }
 
     @Override
-    public boolean hasExistingClusterForEnvironment(EnvironmentExperienceDto environment) {
-        List<ClusterView> clusterViews = getClusterViews(environment.getCrn(), environment.getAccountId());
-        return countNotDeletedClusters(clusterViews) > 0;
+    public int clusterCountForEnvironment(EnvironmentExperienceDto environment) {
+        List<ClusterView> clusterViews = getClusterViews(environment.getName(), environment.getAccountId());
+        return countNotDeletedClusters(clusterViews);
     }
 
     @Override
-    public void deleteConnectedExperiences(EnvironmentExperienceDto dto) {
-        throw new NotImplementedException("deleteConnectedExperiences is not implemented yet");
+    public void deleteConnectedExperiences(EnvironmentExperienceDto environment) {
+        LOGGER.debug("Getting Liftie cluster list for environment '{}'", environment.getName());
+        List<ClusterView> clusterViews = getClusterViews(environment.getName(), environment.getAccountId());
+        LOGGER.debug("Starting Liftie clusters deletion for environment '{}'", environment.getName());
+        clusterViews.stream()
+                .filter(cv -> !"DELETED".equals(cv.getClusterStatus().getStatus()))
+                .forEach(cv -> liftieApi.deleteCluster(cv.getClusterId()));
+        LOGGER.debug("Liftie clusters delete requests submitted for environment '{}'", environment.getName());
     }
 
     @Override
@@ -37,28 +46,26 @@ public class ExperiencesByLiftie implements Experience {
         return ExperienceSource.LIFTIE;
     }
 
-    private List<ClusterView> getClusterViews(String environmentCrn, String tenant) {
+    private List<ClusterView> getClusterViews(String environmentName, String tenant) {
         List<ClusterView> clusterViews = new LinkedList<>();
-        ListClustersResponse first = liftieApi.listClusters(environmentCrn, tenant, null, null);
+        List<ListClustersResponse> clustersResponses = new LinkedList<>();
+        ListClustersResponse first = liftieApi.listClusters(environmentName, tenant, null);
+        clustersResponses.add(first);
         if (first.getPage().getTotalPages() > 1) {
-            List<ListClustersResponse> clustersResponses = new LinkedList<>();
-            clustersResponses.add(first);
             int currentPage = first.getPage().getNumber() + 1;
             while (currentPage < first.getPage().getTotalPages()) {
-                clustersResponses.add(liftieApi.listClusters(environmentCrn, tenant, null, currentPage));
+                clustersResponses.add(liftieApi.listClusters(environmentName, tenant, currentPage));
                 currentPage++;
             }
-            for (ListClustersResponse clustersResponse : clustersResponses) {
-                clusterViews.addAll(clustersResponse.getClusters().values());
-            }
-        } else {
-            clusterViews.addAll(first.getClusters().values());
+        }
+        for (ListClustersResponse clustersResponse : clustersResponses) {
+            clusterViews.addAll(clustersResponse.getClusters().values());
         }
         return clusterViews;
     }
 
-    private long countNotDeletedClusters(List<ClusterView> clusterViews) {
-        return clusterViews.stream().filter(clusterView -> !"DELETED".equals(clusterView.getClusterStatus().getStatus())).count();
+    private int countNotDeletedClusters(List<ClusterView> clusterViews) {
+        return Math.toIntExact(clusterViews.stream().filter(clusterView -> !"DELETED".equals(clusterView.getClusterStatus().getStatus())).count());
     }
 
 }
--- environment/src/test/java/com/sequenceiq/environment/experience/common/CommonExperienceValidatorTest.java
+++ environment/src/test/java/com/sequenceiq/environment/experience/common/CommonExperienceValidatorTest.java
@@ -2,14 +2,11 @@ package com.sequenceiq.environment.experience.common;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 class CommonExperienceValidatorTest {
 
@@ -19,18 +16,10 @@ class CommonExperienceValidatorTest {
 
     private static final String XP_ENV_ENDPOINT = "someInfixValue";
 
-    @Mock
-    private CommonExperience commonExperience;
-
     private CommonExperienceValidator underTest;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
-        when(commonExperience.getPort()).thenReturn(XP_PORT);
-        when(commonExperience.getInternalEnvEndpoint()).thenReturn(XP_ENV_ENDPOINT);
-        when(commonExperience.getHostAddress()).thenReturn(XP_HOST_ADDRESS);
-
         underTest = new CommonExperienceValidator();
     }
 
