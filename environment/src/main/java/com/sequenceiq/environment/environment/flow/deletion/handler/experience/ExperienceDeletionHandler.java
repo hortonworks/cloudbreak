4a280958953b6d35a6a0a336fe560fe65704a54a
Mészáros Gergely <grigorijat9026@gmail.com>
2021-01-18 14:44:17 +0100

    massive refactor

--- environment/src/main/java/com/sequenceiq/environment/environment/flow/deletion/handler/experience/ExperienceDeletionHandler.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/flow/deletion/handler/experience/ExperienceDeletionHandler.java
@@ -1,25 +1,16 @@
 package com.sequenceiq.environment.environment.flow.deletion.handler.experience;
 
-import static com.sequenceiq.cloudbreak.polling.PollingResult.isSuccess;
-import static com.sequenceiq.cloudbreak.util.NullUtil.getIfNotNull;
 import static com.sequenceiq.environment.environment.flow.deletion.event.EnvDeleteHandlerSelectors.DELETE_EXPERIENCE_EVENT;
 import static com.sequenceiq.environment.environment.flow.deletion.event.EnvDeleteStateSelectors.START_FREEIPA_DELETE_EVENT;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.auth.altus.EntitlementService;
-import com.sequenceiq.cloudbreak.polling.PollingResult;
-import com.sequenceiq.cloudbreak.polling.PollingService;
-import com.sequenceiq.environment.environment.domain.Environment;
 import com.sequenceiq.environment.environment.dto.EnvironmentDeletionDto;
 import com.sequenceiq.environment.environment.dto.EnvironmentDto;
-import com.sequenceiq.environment.environment.dto.EnvironmentExperienceDto;
 import com.sequenceiq.environment.environment.flow.deletion.event.EnvDeleteEvent;
 import com.sequenceiq.environment.environment.flow.deletion.event.EnvDeleteFailedEvent;
 import com.sequenceiq.environment.environment.service.EnvironmentService;
-import com.sequenceiq.environment.exception.ExperienceOperationFailedException;
-import com.sequenceiq.environment.experience.ExperienceConnectorService;
 import com.sequenceiq.flow.reactor.api.event.EventSender;
 import com.sequenceiq.flow.reactor.api.handler.EventSenderAwareHandler;
 
@@ -28,23 +19,18 @@ import reactor.bus.Event;
 @Component
 public class ExperienceDeletionHandler extends EventSenderAwareHandler<EnvironmentDeletionDto> {
 
-    private static final int SINGLE_FAILURE = 1;
-
     private final EntitlementService entitlementService;
-
-    private final PollingService<ExperiencePollerObject> experiencePollingService;
 
     private final EnvironmentService environmentService;
 
-    private final ExperienceConnectorService experienceConnectorService;
+    private final EnvironmentExperienceDeletionAction environmentExperienceDeletionAction;
 
-    protected ExperienceDeletionHandler(EventSender eventSender, EntitlementService entitlementService, PollingService<ExperiencePollerObject> experiencePollingService,
-            EnvironmentService environmentService, ExperienceConnectorService experienceConnectorService) {
+    protected ExperienceDeletionHandler(EventSender eventSender, EntitlementService entitlementService, EnvironmentService environmentService,
+            EnvironmentExperienceDeletionAction environmentExperienceDeletionAction) {
         super(eventSender);
         this.entitlementService = entitlementService;
-        this.experiencePollingService = experiencePollingService;
         this.environmentService = environmentService;
-        this.experienceConnectorService = experienceConnectorService;
+        this.environmentExperienceDeletionAction = environmentExperienceDeletionAction;
     }
 
     @Override
@@ -60,7 +46,7 @@ public class ExperienceDeletionHandler extends EventSenderAwareHandler<Environme
         try {
             if (entitlementService.isExperienceDeletionEnabled(envDto.getAccountId())) {
                 environmentService.findEnvironmentById(envDto.getId())
-                        .ifPresent(this::deleteExperiences);
+                        .ifPresent(environmentExperienceDeletionAction::execute);
             }
             EnvDeleteEvent envDeleteEvent = getEnvDeleteEvent(environmentDeletionDto);
             eventSender().sendEvent(envDeleteEvent, environmentDeletionDtoEvent.getHeaders());
@@ -86,21 +72,4 @@ public class ExperienceDeletionHandler extends EventSenderAwareHandler<Environme
                 .build();
     }
 
-    private void deleteExperiences(Environment environment) {
-        EnvironmentExperienceDto environmentExperienceDto = new EnvironmentExperienceDto.Builder()
-                .withName(environment.getName())
-                .withCrn(environment.getResourceCrn())
-                .withAccountId(environment.getAccountId())
-                .build();
-        experienceConnectorService.deleteConnectedExperiences(environmentExperienceDto);
-        Pair<PollingResult, Exception> result = experiencePollingService.pollWithTimeout(
-                new ExperienceDeletionRetrievalTask(experienceConnectorService),
-                new ExperiencePollerObject(environment.getResourceCrn(), environment.getName(), environment.getAccountId()),
-                ExperienceDeletionRetrievalTask.EXPERIENCE_RETRYING_INTERVAL,
-                ExperienceDeletionRetrievalTask.EXPERIENCE_RETRYING_COUNT,
-                SINGLE_FAILURE);
-        if (!isSuccess(result.getLeft())) {
-            throw new ExperienceOperationFailedException("Failed to delete Experience! " + getIfNotNull(result.getRight(), Throwable::getMessage));
-        }
-    }
 }
