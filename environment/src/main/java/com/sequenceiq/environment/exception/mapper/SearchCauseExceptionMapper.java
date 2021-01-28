029467f6157fb602506265b8b693f772504b42a2
Gergely Topolyai <gtopolyai@hortonworks.com>
2021-01-28 18:13:04 +0100

    CB-10867 While we try to validate the Azure credential, we get a RuntimeException. However, the AuthenticatedException can be found in the tree of causes. Now we search recursively the cause exception. Rules: * if cause is null, get the closestmapper for the exception if cannot find a mapper or the mapper is the default, we search the next cause mapper any other case, use the found mapper

--- environment/src/main/java/com/sequenceiq/environment/exception/mapper/SearchCauseExceptionMapper.java
+++ environment/src/main/java/com/sequenceiq/environment/exception/mapper/SearchCauseExceptionMapper.java
@@ -4,6 +4,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Providers;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 public abstract class SearchCauseExceptionMapper<T extends Throwable> extends BaseExceptionMapper<T> {
 
     @Context
@@ -12,13 +14,28 @@ public abstract class SearchCauseExceptionMapper<T extends Throwable> extends Ba
     @Override
     Response.Status getResponseStatus(T exception) {
         Response.Status defaultResponse = Response.Status.BAD_REQUEST;
-        if (exception.getCause() != null && Exception.class == exception.getCause().getClass()) {
-            return defaultResponse;
+        if (exception != null) {
+            Pair<BaseExceptionMapper, ? extends Throwable> pair = searchRecursively(exception);
+            if (pair.getKey() != null && pair.getKey() != this && !(pair.getKey() instanceof DefaultExceptionMapper)) {
+                defaultResponse = pair.getKey().getResponseStatus(pair.getValue());
+            }
         }
-        BaseExceptionMapper exceptionMapper = (BaseExceptionMapper<? extends Throwable>) providers.getExceptionMapper(exception.getCause().getClass());
-        if (exceptionMapper == null) {
-            return defaultResponse;
+        return defaultResponse;
+    }
+
+    private Pair<BaseExceptionMapper, Throwable> searchRecursively(Throwable exception) {
+        if (exception.getCause() == null) {
+            BaseExceptionMapper exceptionMapper = getExceptionMapper(exception);
+            return Pair.of(exceptionMapper, exception);
         }
-        return exceptionMapper.getResponseStatus(exception.getCause());
+        BaseExceptionMapper<? extends Throwable> exceptionMapper = getExceptionMapper(exception.getCause());
+        if (exceptionMapper == null || exceptionMapper instanceof DefaultExceptionMapper) {
+            return searchRecursively(exception.getCause());
+        }
+        return Pair.of(exceptionMapper, exception.getCause());
+    }
+
+    private BaseExceptionMapper<? extends Throwable> getExceptionMapper(Throwable exception) {
+        return (BaseExceptionMapper<? extends Throwable>) providers.getExceptionMapper(exception.getClass());
     }
 }
---
+++ environment/src/test/java/com/sequenceiq/environment/exception/mapper/TestExceptionMapperConfiguration.java
@@ -0,0 +1,10 @@
+package com.sequenceiq.environment.exception.mapper;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.sequenceiq.environment.exception"})
+public class TestExceptionMapperConfiguration {
+
+}
