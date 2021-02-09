9e4d36b4062d8e02826eaa45db74af913f92d4fa
Mészáros Gergely <grigorijat9026@gmail.com>
2021-02-09 10:44:32 +0100

    CB-11108 - rename experience deletion references from XP_* to EXPERIENCE_*

--- environment/src/main/java/com/sequenceiq/environment/environment/flow/deletion/event/EnvClustersDeleteStateSelectors.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/flow/deletion/event/EnvClustersDeleteStateSelectors.java
@@ -5,7 +5,7 @@ import com.sequenceiq.flow.core.FlowEvent;
 public enum EnvClustersDeleteStateSelectors implements FlowEvent {
 
     START_DATAHUB_CLUSTERS_DELETE_EVENT,
-    START_XP_DELETE_EVENT,
+    START_EXPERIENCE_DELETE_EVENT,
     START_DATALAKE_CLUSTERS_DELETE_EVENT,
     FINISH_ENV_CLUSTERS_DELETE_EVENT,
     FAILED_ENV_CLUSTERS_DELETE_EVENT,
