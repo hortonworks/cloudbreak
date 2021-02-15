58f2f6846c775961466193479ee229b6c2a599fd
Annamaria Szegedi <aszegedi@cloudera.com>
2021-02-15 13:40:36 +0100

    CB-11165 Remove test containers for services not defined in the IT Compose file

--- integration-test/scripts/docker-compose.sh
+++ integration-test/scripts/docker-compose.sh
@@ -18,7 +18,7 @@ cd ..
 
 date
 echo -e "\n\033[1;96m--- Kill running test container\033[0m\n"
-$INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility down
+$INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility down --remove-orphans
 
 date
 echo -e "\n\033[1;96m--- Create docker network\033[0m\n"
@@ -26,7 +26,7 @@ docker network create cbreak_default || true
 
 date
 echo -e "\n\033[1;96m--- Start thunderhead mock\033[0m\n"
-$INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility up -d thunderhead-mock
+$INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility up --remove-orphans --detach thunderhead-mock
 
 date
 echo -e "\n\033[1;96m--- Copy mock infrastructure infrastructure-mock.p12 cert to certs dir\033[0m\n"
@@ -47,9 +47,9 @@ docker ps --format ‘{{.Image}}’
 
 date
 if [ $? -ne 0 ]; then
-    echo ERROR: Failed to bring up all the necessary services! Process is about to terminate.
+    echo -e "\n\033[1;96m--- ERROR: Failed to bring up all the necessary services! Process is about to terminate!\033[0m\n"
     ./cbd kill
-    .deps/bin/docker-compose --compatibility down
+    .deps/bin/docker-compose --compatibility down --remove-orphans
     exit 1
 fi
 
@@ -121,7 +121,7 @@ if [[ "$CIRCLECI" ]]; then
     export DOCKER_CLIENT_TIMEOUT=120
     export COMPOSE_HTTP_TIMEOUT=120
 
-    $INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility up test | tee test.out
+    $INTEGCB_LOCATION/.deps/bin/docker-compose --compatibility up --remove-orphans test | tee test.out
     echo -e "\n\033[1;96m--- Test finished\033[0m\n"
 
     echo -e "\n\033[1;96m--- Collect docker stats:\033[0m\n"
