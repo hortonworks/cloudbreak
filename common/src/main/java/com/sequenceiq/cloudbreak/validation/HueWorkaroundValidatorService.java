a89cca47cb0819e192783e579d9ba3aa1e865638
doktoric <rdoktorics@cloudera.com>
2021-02-15 09:15:54 +0100

    CB-11158 Increase hostgroup validation for HUE from 6 to 7 to support 'gateway' hostgroup names in QA scenarios.

--- common/src/main/java/com/sequenceiq/cloudbreak/validation/HueWorkaroundValidatorService.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/validation/HueWorkaroundValidatorService.java
@@ -14,7 +14,7 @@ public class HueWorkaroundValidatorService {
 
     private static final int MAX_STACK_NAME_LENGTH = 20;
 
-    private static final int MAX_HOST_GROUP_NAME = 6;
+    private static final int MAX_HOST_GROUP_NAME = 7;
 
     public void validateForStackRequest(Set<String> hueHostGroups, String stackName) {
         if (isHuePresented(hueHostGroups)) {
@@ -36,7 +36,7 @@ public class HueWorkaroundValidatorService {
                 throw new BadRequestException(String.format("Your Data Hub contains Hue. Hue does not support CDP Data "
                         + "Hubs where the fully qualified domain name of a VM is longer than 63 characters. "
                         + "Generated hostname: %s. Please retry creating Data Hub using a Data Hub name that is shorter than "
-                        + "20 characters and a hostgroup name that is shorter than 6 characters.", endpointName));
+                        + "20 characters and a hostgroup name that is shorter than 7 characters.", endpointName));
             }
         }
     }
@@ -52,8 +52,8 @@ public class HueWorkaroundValidatorService {
                     .filter(e -> e.length() > MAX_HOST_GROUP_NAME)
                     .collect(Collectors.toSet());
             if (!hostGroupsWhichAreLongerThanSixCharacter.isEmpty()) {
-                throw new BadRequestException(String.format("Hue does not support CDP Data Hubs where hostgroup name is longer than 6 characters. "
-                                + "Please retry creating the template by shortening the hostgroup name: %s  under 6 characters.",
+                throw new BadRequestException(String.format("Hue does not support CDP Data Hubs where hostgroup name is longer than 7 characters. "
+                                + "Please retry creating the template by shortening the hostgroup name: %s  under 7 characters.",
                         String.join(", ", hostGroupsWhichAreLongerThanSixCharacter)));
             }
         }
