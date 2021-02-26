75dcecab5d43557992ad269213c84096b745dd67
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2021-02-26 12:04:56 +0100

    CB-11380 Add more meaningful message in case saltbootstrap is not reachable
    If the instance don't start properly or CCM can't connect we will show a more helpful message.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/HostBootstrapApiCheckerTask.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/HostBootstrapApiCheckerTask.java
@@ -16,7 +16,11 @@ public class HostBootstrapApiCheckerTask extends StackBasedStatusCheckerTask<Hos
 
     @Override
     public void handleTimeout(HostBootstrapApiContext t) {
-        throw new CloudbreakServiceException("Operation timed out. Could not reach bootstrap API in time.");
+        throw new CloudbreakServiceException("Operation timed out. Could not reach bootstrap API in time. "
+                + "The Control Plane was not able to establish the connection with the gateway instance. "
+                + "This could be caused by the reverse SSH tunnel (autossh process) running on this instance could not connect to the Cloudera server. "
+                + "Please check your connection and proxy settings and make sure the instance can reach *.ccm.cdp.cloudera.com "
+                + "Please check your instance on the cloud provider side if it's up and running. Restart it if it couldn't start up properly.");
     }
 
     @Override
