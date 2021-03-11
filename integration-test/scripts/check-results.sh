7e313c7ed960c3989b17472e99a24e33f2b15bd6
Annamaria Szegedi <aszegedi@cloudera.com>
2021-03-11 19:13:19 +0100

    CB-11626 Fix integration-test project check-results failure conditions

--- integration-test/scripts/check-results.sh
+++ integration-test/scripts/check-results.sh
@@ -4,19 +4,29 @@
 
 status_code=0
 
+set -ex
+
 if [[ "$CIRCLECI" ]]; then
 
     # Check integration test results
     date
     if [[ ! -d test-output ]]; then
-        echo -e "\033[0;91m--- !!! NO test-output DIRECTORY !!! ---\n"; status_code=1;
+        echo -e "\033[0;91m--- !!! NO test-output DIRECTORY !!! ---\n";
+        status_code=1;
     else
         if grep -r '<failure ' test-output; then
-          echo -e "\033[0;91m--- !!! INTEGRATION TEST FAILED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n"; status_code=1;
+          echo -e "\033[0;91m--- !!! INTEGRATION TEST FAILED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n";
+          status_code=1;
         fi
         if grep -r '<skipped' test-output; then
-          echo -e "\033[0;91m--- !!! INTEGRATION TEST SKIPPED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n"; status_code=1;
+          echo -e "\033[0;91m--- !!! INTEGRATION TEST SKIPPED, CHECK \033[1;93mtest-output\033[0;91m DIR FOR RESULTS !!! ---\n";
+          status_code=1;
         fi
+    fi
+
+    if [[ $(find suites_log -maxdepth 1 -name '*.log' | wc -l) -lt 3 ]]; then
+        echo -e "\033[0;91m--- !!! NO TEST HAS BEEN LAUNCHED !!! ---\n";
+        status_code=1;
     fi
 
     if [[ -z "${INTEGRATIONTEST_YARN_QUEUE}" ]] && [[ "$AWS" != true ]]; then
@@ -29,15 +39,17 @@ if [[ "$CIRCLECI" ]]; then
 
       pg_res=$(cat ./test-output/docker_stats/pg_stat_network_io_analysed.result);
       if [[ "$pg_res" == "POSTGRES>> OK" ]]; then
-        echo -e "\n\033[0;92m+++ Postgres traffic is below ${max_pg_network_output} limit.";
+        echo -e "\n\033[0;92m+++ POSTGRES TRAFFIC IS BELOW ${max_pg_network_output} LIMIT. +++\n";
       else
-        echo -e "\033[0;91m--- !!! Postgres trafic check failed: ${pg_res}"; status_code=1;
+        echo -e "\033[0;91m--- !!! POSTGRES TRAFIC CHECK FAILED: ${pg_res} !!! ---\n";
+        status_code=1;
       fi
     fi
 
     # Exit if there are failed tests
-    if [[ status_code -eq 1 ]];
-        then exit 1;
+    if [[ status_code -eq 1 ]]; then
+      echo -e "\033[0;91m--- !!! INTEGRATION TEST HAS BEEN FAILED !!! ---\n";
+      exit 1;
     fi
 
     echo -e "\n\033[0;92m+++ INTEGRATION TEST SUCCESSFULLY FINISHED +++\n";
