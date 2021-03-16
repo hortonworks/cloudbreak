6d15efe91c741c4f531520fed87d728248de9e73
Gergely Papp <gpapp@hortonworks.com>
2021-03-16 08:20:33 +0100

    CB-11415 Replace DefaultRestartAction for flows in redbeams service
    Upon restart the DBStack of running flows is not loaded into the in memory store, causing possible problems in the heartbeatService when all deleting flows are queried.
    A restart action was added that fills the in memory store with currently executing flow's DBStack, if present (at the final step of the delete flow DBStack is deleted, thus after a restart at that place there is be no more DBStack).

--- redbeams/src/main/java/com/sequenceiq/redbeams/flow/redbeams/provision/RedbeamsProvisionState.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/flow/redbeams/provision/RedbeamsProvisionState.java
@@ -1,6 +1,9 @@
 package com.sequenceiq.redbeams.flow.redbeams.provision;
 
 import com.sequenceiq.flow.core.FlowState;
+import com.sequenceiq.flow.core.RestartAction;
+import com.sequenceiq.flow.core.restart.DefaultRestartAction;
+import com.sequenceiq.redbeams.flow.redbeams.common.FillInMemoryStateStoreRestartAction;
 
 public enum RedbeamsProvisionState implements FlowState {
     INIT_STATE,
@@ -8,5 +11,13 @@ public enum RedbeamsProvisionState implements FlowState {
     ALLOCATE_DATABASE_SERVER_STATE,
     UPDATE_DATABASE_SERVER_REGISTRATION_STATE,
     REDBEAMS_PROVISION_FINISHED_STATE,
-    FINAL_STATE
+    FINAL_STATE;
+
+    private Class<? extends DefaultRestartAction> restartAction = FillInMemoryStateStoreRestartAction.class;
+
+    @Override
+    public Class<? extends RestartAction> restartAction() {
+        return restartAction;
+    }
+
 }
--- redbeams/src/main/java/com/sequenceiq/redbeams/flow/redbeams/termination/RedbeamsTerminationState.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/flow/redbeams/termination/RedbeamsTerminationState.java
@@ -1,6 +1,9 @@
 package com.sequenceiq.redbeams.flow.redbeams.termination;
 
 import com.sequenceiq.flow.core.FlowState;
+import com.sequenceiq.flow.core.RestartAction;
+import com.sequenceiq.flow.core.restart.DefaultRestartAction;
+import com.sequenceiq.redbeams.flow.redbeams.common.FillInMemoryStateStoreRestartAction;
 
 public enum RedbeamsTerminationState implements FlowState {
     INIT_STATE,
@@ -8,5 +11,13 @@ public enum RedbeamsTerminationState implements FlowState {
     TERMINATE_DATABASE_SERVER_STATE,
     DEREGISTER_DATABASE_SERVER_STATE,
     REDBEAMS_TERMINATION_FINISHED_STATE,
-    FINAL_STATE
+    FINAL_STATE;
+
+    private Class<? extends DefaultRestartAction> restartAction = FillInMemoryStateStoreRestartAction.class;
+
+    @Override
+    public Class<? extends RestartAction> restartAction() {
+        return restartAction;
+    }
+
 }
