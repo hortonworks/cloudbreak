9628c55c4abb9d11bdc633f59146e469fb9b1243
Gergely Topolyai <gtopolyai@hortonworks.com>
2021-03-16 16:36:06 +0100

    CB-11529 credential verification will fail only if the sattus is FAILED.

---
+++ cloud-reactor/src/test/java/com/sequenceiq/cloudbreak/cloud/handler/CredentialVerificationHandlerTest.java
@@ -0,0 +1,58 @@
+package com.sequenceiq.cloudbreak.cloud.handler;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sequenceiq.cloudbreak.cloud.event.credential.CredentialVerificationRequest;
+import com.sequenceiq.cloudbreak.cloud.event.credential.CredentialVerificationResult;
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
+import com.sequenceiq.cloudbreak.cloud.model.CloudCredentialStatus;
+import com.sequenceiq.cloudbreak.cloud.model.CredentialStatus;
+
+@ExtendWith(MockitoExtension.class)
+public class CredentialVerificationHandlerTest {
+
+    @InjectMocks
+    private CredentialVerificationHandler underTest;
+
+    @ParameterizedTest
+    @EnumSource(value = CredentialStatus.class, names = "FAILED", mode = EnumSource.Mode.EXCLUDE)
+    public void testCreateCredentialVerificationResultWhenMissingPermissionAndExceptionNotNull(CredentialStatus status) {
+        long resourceId = 123L;
+
+        CloudCredential cloudResource = mock(CloudCredential.class);
+        CloudCredentialStatus credentialStatus = new CloudCredentialStatus(cloudResource, status, new Exception(), "reason");
+        CredentialVerificationRequest request = mock(CredentialVerificationRequest.class);
+
+        when(request.getResourceId()).thenReturn(resourceId);
+
+        CredentialVerificationResult actual = underTest.createCredentialVerificationResult(request, credentialStatus);
+
+        Assertions.assertEquals(credentialStatus, actual.getCloudCredentialStatus());
+        Assertions.assertNull(actual.getErrorDetails());
+    }
+
+    @Test
+    public void testCreateCredentialVerificationResultWhenFailedAndExceptionNotNull() {
+        long resourceId = 123L;
+
+        CloudCredential cloudResource = mock(CloudCredential.class);
+        CloudCredentialStatus credentialStatus = new CloudCredentialStatus(cloudResource, CredentialStatus.FAILED, new Exception(), "reason");
+        CredentialVerificationRequest request = mock(CredentialVerificationRequest.class);
+
+        when(request.getResourceId()).thenReturn(resourceId);
+
+        CredentialVerificationResult actual = underTest.createCredentialVerificationResult(request, credentialStatus);
+
+        Assertions.assertEquals(credentialStatus, actual.getCloudCredentialStatus());
+        Assertions.assertNotNull(actual.getErrorDetails());
+    }
+}
