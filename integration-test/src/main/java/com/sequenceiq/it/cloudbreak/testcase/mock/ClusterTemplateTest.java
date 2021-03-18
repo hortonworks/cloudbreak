e6d6675d6a5d33b655faa47812889c732ea468fa
David Rorke <drorke@cloudera.com>
2021-03-17 17:43:01 -0700

    CB-10926: Use larger instance types for Datamart and Realtime Datamart templates on GCP
        Aligns GCP instance type sizes with the sizes used on AWS and Azure
        Added Datamart template for 7.2.10

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
@@ -362,7 +362,7 @@ public class ClusterTemplateTest extends AbstractMockTest {
             assertNotNull(entity);
             assertNotNull(entity.getResponses());
             long defaultCount = entity.getResponses().stream().filter(template -> ResourceStatus.DEFAULT.equals(template.getStatus())).count();
-            long expectedCount = 393;
+            long expectedCount = 394;
             assertEquals("Should have " + expectedCount + " of default cluster templates.", expectedCount, defaultCount);
         } catch (Exception e) {
             throw new TestFailException(String.format("Failed to validate default count of cluster templates: %s", e.getMessage()), e);
