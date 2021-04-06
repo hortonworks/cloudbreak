c3ccd788007b824ec78ddd0f7f2187d0a17a7b6b
Dénes Berger <dberger@cloudera.com>
2021-04-06 19:30:34 +0200

    CB-11711 Open NGINX for VPC CIDR with CCMv2 for FreeIPA
    This way all ingress traffic is enabled from within the VPC towards
    NGINX port 9443 if CCMv2 is enabled.
    This is achieved by adding CIDR list to gateway pillar and using it
    with NGINX SSL config.
    There was a missing code part which did not fill the CIDR blocks in case
    of GCP and AZURE providers, this is fixed too.
    To avoid null problems in the DB the ssl.conf templates were modified
    accordingly.

--- environment/src/main/java/com/sequenceiq/environment/environment/flow/creation/handler/freeipa/gcp/FreeIpaGcpNetworkProvider.java
+++ environment/src/main/java/com/sequenceiq/environment/environment/flow/creation/handler/freeipa/gcp/FreeIpaGcpNetworkProvider.java
@@ -1,7 +1,10 @@
 package com.sequenceiq.environment.environment.flow.creation.handler.freeipa.gcp;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Sets;
@@ -9,6 +12,7 @@ import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.environment.environment.dto.EnvironmentDto;
 import com.sequenceiq.environment.environment.flow.creation.handler.freeipa.FreeIpaNetworkProvider;
 import com.sequenceiq.environment.network.dto.GcpParams;
+import com.sequenceiq.environment.network.dto.NetworkDto;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.network.GcpNetworkParameters;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.network.NetworkRequest;
 
@@ -18,14 +22,16 @@ public class FreeIpaGcpNetworkProvider implements FreeIpaNetworkProvider {
     @Override
     public NetworkRequest provider(EnvironmentDto environment) {
         NetworkRequest networkRequest = new NetworkRequest();
-        GcpParams gcpParams = environment.getNetwork().getGcp();
+        NetworkDto network = environment.getNetwork();
+        GcpParams gcpParams = network.getGcp();
         GcpNetworkParameters gcpNetworkParameters = new GcpNetworkParameters();
         gcpNetworkParameters.setNetworkId(gcpParams.getNetworkId());
         gcpNetworkParameters.setNoFirewallRules(gcpParams.getNoFirewallRules());
         gcpNetworkParameters.setNoPublicIp(gcpParams.getNoPublicIp());
         gcpNetworkParameters.setSharedProjectId(gcpParams.getSharedProjectId());
-        gcpNetworkParameters.setSubnetId(environment.getNetwork().getSubnetIds().iterator().next());
+        gcpNetworkParameters.setSubnetId(network.getSubnetIds().iterator().next());
         networkRequest.setGcp(gcpNetworkParameters);
+        networkRequest.setNetworkCidrs(collectNetworkCidrs(network));
         return networkRequest;
     }
 
@@ -43,5 +49,9 @@ public class FreeIpaGcpNetworkProvider implements FreeIpaNetworkProvider {
     @Override
     public CloudPlatform cloudPlatform() {
         return CloudPlatform.GCP;
+    }
+
+    private List<String> collectNetworkCidrs(NetworkDto network) {
+        return CollectionUtils.isNotEmpty(network.getNetworkCidrs()) ? new ArrayList<>(network.getNetworkCidrs()) : List.of();
     }
 }
