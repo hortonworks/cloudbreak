2b368b51a3fd2e72c40b9a07b3895a0e80eef9bf
ganeshk <ganeshk@cloudera.com>
2021-04-16 14:56:27 -0700

    Revert : DISTX-595 Release a first version of Oozie HA without the internal load-balancer
    1. In public cloud all the requests go through Knox, so we do not need a load-balancer for external clients.
    2. CM configuration for multiple Oozie hosts demands a load-balancer to be configured.
    3. This is for Hue to access the Oozie in HA fashion.
    4. Hue can not use Knox for this because the URL is different from what Hue expects it to be.
    5. As a workaround use the first FQDN in the master hosts to configure it as a load-balancer to itself.
    6. This means Oozie will be HA for the schedules, but can not accept modifications from Hue when the first FQDN in the master host is down. It still can accept modifications from external clients via Knox.
    7. Added a TODO pointing to the parent task to add an internal load-balancer for Hue to work with Oozie in HA fashion.

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/oozie/OozieRoleConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/oozie/OozieRoleConfigProvider.java
@@ -2,7 +2,6 @@ package com.sequenceiq.cloudbreak.cmtemplate.configproviders.oozie;
 
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRdsRoleConfigProvider;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
-import com.sequenceiq.cloudbreak.template.views.HostgroupView;
 import com.sequenceiq.cloudbreak.template.views.RdsView;
 
 @Component
@@ -27,28 +25,18 @@ public class OozieRoleConfigProvider extends AbstractRdsRoleConfigProvider {
 
     private static final String OOZIE_DATABASE_PASSWORD = "oozie_database_password";
 
-    private static final String OOZIE_PLUGINS_LIST = "oozie_plugins_list";
-
-    private static final String OOZIE_HA_PLUGINS = "org.apache.oozie.service.ZKUUIDService,"
-        + "org.apache.oozie.service.ZKLocksService,"
-        + "org.apache.oozie.service.ZKXLogStreamingService,"
-        + "org.apache.oozie.service.ZKJobsConcurrencyService";
-
     @Override
     protected List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, TemplatePreparationObject source) {
         switch (roleType) {
             case OozieRoles.OOZIE_SERVER:
                 RdsView oozieRdsView = getRdsView(source);
-                List<ApiClusterTemplateConfig> config = new ArrayList<>(List.of(
-                    config(OOZIE_DATABASE_HOST, oozieRdsView.getHost()),
-                    config(OOZIE_DATABASE_NAME, oozieRdsView.getDatabaseName()),
-                    config(OOZIE_DATABASE_TYPE, oozieRdsView.getSubprotocol()),
-                    config(OOZIE_DATABASE_USER, oozieRdsView.getConnectionUserName()),
-                    config(OOZIE_DATABASE_PASSWORD, oozieRdsView.getConnectionPassword())));
-                if (isOozieHA(source)) {
-                    config.add(config(OOZIE_PLUGINS_LIST, OOZIE_HA_PLUGINS));
-                }
-                return config;
+                return List.of(
+                        config(OOZIE_DATABASE_HOST, oozieRdsView.getHost()),
+                        config(OOZIE_DATABASE_NAME, oozieRdsView.getDatabaseName()),
+                        config(OOZIE_DATABASE_TYPE, oozieRdsView.getSubprotocol()),
+                        config(OOZIE_DATABASE_USER, oozieRdsView.getConnectionUserName()),
+                        config(OOZIE_DATABASE_PASSWORD, oozieRdsView.getConnectionPassword())
+                );
             default:
                 return List.of();
         }
@@ -67,11 +55,5 @@ public class OozieRoleConfigProvider extends AbstractRdsRoleConfigProvider {
     @Override
     protected DatabaseType dbType() {
         return DatabaseType.OOZIE;
-    }
-
-    public static boolean isOozieHA(TemplatePreparationObject source) {
-        return source.getHostGroupsWithComponent(OozieRoles.OOZIE_SERVER)
-            .mapToInt(HostgroupView::getNodeCount)
-            .sum() > 1;
     }
 }
