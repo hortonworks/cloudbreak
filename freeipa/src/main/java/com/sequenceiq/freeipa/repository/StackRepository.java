aeaecad98c2b87b654e768e3b2cef422946680f9
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2021-05-03 11:41:47 +0200

    CB-12315 N+1 query in user sync

--- freeipa/src/main/java/com/sequenceiq/freeipa/repository/StackRepository.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/repository/StackRepository.java
@@ -38,7 +38,10 @@ public interface StackRepository extends AccountAwareResourceRepository<Stack, L
             @Param("environmentCrn") String environmentCrn,
             @Param("name") String name);
 
-    @Query("SELECT s FROM Stack s WHERE s.accountId = :accountId AND s.terminated = -1")
+    @Query("SELECT s FROM Stack s " +
+            "LEFT JOIN FETCH s.instanceGroups ig " +
+            "LEFT JOIN FETCH ig.instanceMetaData " +
+            "WHERE s.accountId = :accountId AND s.terminated = -1")
     List<Stack> findByAccountId(@Param("accountId") String accountId);
 
     @Query("SELECT s FROM Stack s WHERE s.accountId = :accountId AND s.environmentCrn = :environmentCrn AND s.terminated = -1")
