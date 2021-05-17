61d43bcc3255429f2ce9cbc781900123eb267bf8
vbrodetskyi <vbrodetskyi@cloudera.com>
2021-05-17 15:39:15 +0300

    CFM-1975 7.2.9 Flow Management fails to launch in mow-dev. Added condition to update working directories only if stack version equals or greater than 7.2.10. Also added unit tests for related classes.

---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/nifi/NifiRoles.java
@@ -0,0 +1,11 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.nifi;
+
+public class NifiRoles {
+
+    public static final String NIFI = "NIFI";
+
+    public static final String NIFI_NODE = "NIFI_NODE";
+
+    private NifiRoles() { }
+
+}
