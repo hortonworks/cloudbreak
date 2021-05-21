08768904335f8722058f448adcc9676efd687e2f
Haley Reeve <hreeve@cloudera.com>
2021-05-21 15:19:51 -0500

    CB-10773 [HA LB] Datalake create flow timed out instead of terminating gracefully
    Fixes an error in the StackCreation flow so that it handles the COLLECT_LOADBALANCER_METADATA_FAILED_EVENT
    correctly. Tested by running CB locally and inducing a LB metadata collection failure.

--- cloud-reactor/src/main/java/com/sequenceiq/cloudbreak/cloud/handler/CollectLoadBalancerMetadataHandler.java
+++ cloud-reactor/src/main/java/com/sequenceiq/cloudbreak/cloud/handler/CollectLoadBalancerMetadataHandler.java
@@ -1,17 +1,20 @@
 package com.sequenceiq.cloudbreak.cloud.handler;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import reactor.bus.Event;
+import reactor.bus.EventBus;
+
 import com.sequenceiq.cloudbreak.cloud.event.loadbalancer.CollectLoadBalancerMetadataRequest;
 import com.sequenceiq.cloudbreak.cloud.event.loadbalancer.CollectLoadBalancerMetadataResult;
 import com.sequenceiq.cloudbreak.cloud.handler.service.LoadBalancerMetadataService;
 import com.sequenceiq.cloudbreak.cloud.model.CloudLoadBalancerMetadata;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import reactor.bus.Event;
-import reactor.bus.EventBus;
-
-import javax.inject.Inject;
-import java.util.List;
 
 @Component
 public class CollectLoadBalancerMetadataHandler implements CloudPlatformEventHandler<CollectLoadBalancerMetadataRequest> {
@@ -43,7 +46,7 @@ public class CollectLoadBalancerMetadataHandler implements CloudPlatformEventHan
             eventBus.notify(collectLBMetadataResult.selector(), new Event<>(collectLBMetadataRequestEvent.getHeaders(), collectLBMetadataResult));
             LOGGER.debug("Load balancer metadata collection successfully finished");
         } catch (RuntimeException e) {
-            LOGGER.error("Collecting metadata failed", e);
+            LOGGER.error("Collecting load balancer metadata failed", e);
             CollectLoadBalancerMetadataResult failure = new CollectLoadBalancerMetadataResult(e, request.getResourceId());
             request.getResult().onNext(failure);
             eventBus.notify(failure.selector(), new Event<>(collectLBMetadataRequestEvent.getHeaders(), failure));
