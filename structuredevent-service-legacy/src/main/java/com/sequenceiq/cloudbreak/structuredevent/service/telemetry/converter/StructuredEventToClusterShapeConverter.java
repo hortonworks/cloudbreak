bf7799815a6300e98530006874d5da4914c2d57b
anagy <anagy@cloudera.com>
2021-05-27 14:10:05 +0200

    CB-12635 TemporalStorage feature tracking in EDH

--- structuredevent-service-legacy/src/main/java/com/sequenceiq/cloudbreak/structuredevent/service/telemetry/converter/StructuredEventToClusterShapeConverter.java
+++ structuredevent-service-legacy/src/main/java/com/sequenceiq/cloudbreak/structuredevent/service/telemetry/converter/StructuredEventToClusterShapeConverter.java
@@ -4,12 +4,15 @@ import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
 import com.cloudera.thunderhead.service.common.usage.UsageProto;
 import com.sequenceiq.cloudbreak.common.json.JsonUtil;
+import com.sequenceiq.cloudbreak.common.type.TemporaryStorage;
 import com.sequenceiq.cloudbreak.structuredevent.event.BlueprintDetails;
 import com.sequenceiq.cloudbreak.structuredevent.event.InstanceGroupDetails;
 import com.sequenceiq.cloudbreak.structuredevent.event.StackDetails;
@@ -54,15 +57,18 @@ public class StructuredEventToClusterShapeConverter {
 
         if (stackDetails != null) {
             List<String> hostGroupNodeCount = new ArrayList<>();
+            Set<String> hostGroupTemporaryStorage = new HashSet<>();
             int nodeCnt = 0;
             for (InstanceGroupDetails instanceGroupDetails : stackDetails.getInstanceGroups()) {
                 nodeCnt += instanceGroupDetails.getNodeCount();
                 hostGroupNodeCount.add(String.format("%s=%d", instanceGroupDetails.getGroupName(), instanceGroupDetails.getNodeCount()));
+                hostGroupTemporaryStorage.add(instanceGroupDetails.getTemporaryStorage());
             }
 
             cdpClusterShape.setNodes(nodeCnt);
             Collections.sort(hostGroupNodeCount);
             cdpClusterShape.setHostGroupNodeCount(String.join(", ", hostGroupNodeCount));
+            cdpClusterShape.setTemporaryStorageUsed(hostGroupTemporaryStorage.contains(TemporaryStorage.EPHEMERAL_VOLUMES.name()));
             cdpClusterShape.setDefinitionDetails(JsonUtil.writeValueAsStringSilentSafe(stackDetails.getInstanceGroups()));
         }
 
