b67202114f9a9f93876cee235a5ef6a02f5e9fdd
doktoric <rdoktorics@cloudera.com>
2021-06-01 17:30:14 +0200

    CB-12433 Currently, in RAZ-S3 clusters we see ADLS configurations as well and vice versa and causing this issue https://jira.cloudera.com/browse/CB-12251 Ideally each cloud specific cluster should only contain those cloud specific configs. Adding anger.raz.bootstrap.servicetypes property which is s3 in case of aws and adls in case of azure

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/raz/RangerRazBaseConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/raz/RangerRazBaseConfigProvider.java
@@ -1,5 +1,8 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.raz;
 
+import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.isRazTokenConfigurationSupported;
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.getSafetyValveProperty;
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.raz.RangerRazRoles.RANGER_RAZ;
 import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.raz.RangerRazRoles.RANGER_RAZ_SERVER;
 
@@ -8,11 +11,17 @@ import java.util.List;
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
 import com.cloudera.api.swagger.model.ApiClusterTemplateRoleConfigGroup;
 import com.cloudera.api.swagger.model.ApiClusterTemplateService;
+import com.google.common.base.Strings;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRoleConfigProvider;
+import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 
 public abstract class RangerRazBaseConfigProvider extends AbstractRoleConfigProvider {
+
+    private static final String RANGER_RAZ_SITE_XML_ROLE_SAFETY_VALVE = "ranger-raz-conf/ranger-raz-site.xml_role_safety_valve";
+
+    private static final String RANGER_RAZ_BOOTSTRAP_SERVICETYPES = "ranger.raz.bootstrap.servicetypes";
 
     @Override
     public List<ApiClusterTemplateConfig> getServiceConfigs(CmTemplateProcessor templateProcessor, TemplatePreparationObject source) {
@@ -31,7 +40,24 @@ public abstract class RangerRazBaseConfigProvider extends AbstractRoleConfigProv
 
     @Override
     protected List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, TemplatePreparationObject source) {
+        String cdhVersion = source.getBlueprintView().getProcessor().getVersion().orElse("");
+        CloudPlatform cloudPlatform = source.getCloudPlatform();
+        if (!Strings.isNullOrEmpty(cdhVersion) && isRazTokenConfigurationSupported(cdhVersion, cloudPlatform)) {
+            String safetyValveValue = getSafetyValveProperty(RANGER_RAZ_BOOTSTRAP_SERVICETYPES, getServiceType(cloudPlatform));
+            return List.of(config(RANGER_RAZ_SITE_XML_ROLE_SAFETY_VALVE, safetyValveValue));
+        }
         return List.of();
+    }
+
+    private String getServiceType(CloudPlatform cloudPlatform) {
+        switch (cloudPlatform) {
+            case AZURE:
+                return "adls";
+            case AWS:
+                return "s3";
+            default:
+                return null;
+        }
     }
 
     protected ApiClusterTemplateService createTemplate() {
