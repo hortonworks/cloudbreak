161d85960a2c1b964ec46fa18167abfbd9abea15
Oliver Szabo <oleewere@gmail.com>
2021-06-07 19:16:33 +0200

    CB-12952. Progress API should not cause 404 error by DL API if the stack does not exists.

--- datalake/src/main/java/com/sequenceiq/datalake/service/sdx/progress/ProgressService.java
+++ datalake/src/main/java/com/sequenceiq/datalake/service/sdx/progress/ProgressService.java
@@ -2,15 +2,20 @@ package com.sequenceiq.datalake.service.sdx.progress;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.progress.ProgressV4Endpoint;
+import com.sequenceiq.cloudbreak.common.exception.NotFoundException;
 import com.sequenceiq.flow.service.FlowService;
 import com.sequenceiq.sdx.api.model.SdxProgressListResponse;
 import com.sequenceiq.sdx.api.model.SdxProgressResponse;
 
 @Component
 public class ProgressService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProgressService.class);
 
     @Inject
     private ProgressV4Endpoint progressV4Endpoint;
@@ -21,14 +26,22 @@ public class ProgressService {
     public SdxProgressResponse getLastFlowProgressByResourceCrn(String resourceCrn) {
         SdxProgressResponse response = new SdxProgressResponse();
         response.setLastFlowOperation(flowService.getLastFlowProgressByResourceCrn(resourceCrn));
-        response.setLastInternalFlowOperation(progressV4Endpoint.getLastFlowLogProgressByResourceCrn(resourceCrn));
+        try {
+            response.setLastInternalFlowOperation(progressV4Endpoint.getLastFlowLogProgressByResourceCrn(resourceCrn));
+        } catch (NotFoundException notFoundException) {
+            LOGGER.debug("Stack for datalake '{}' has not found yet. It is acceptable for progress response.", resourceCrn);
+        }
         return response;
     }
 
     public SdxProgressListResponse getFlowProgressListByResourceCrn(String resourceCrn) {
         SdxProgressListResponse response = new SdxProgressListResponse();
         response.setRecentFlowOperations(flowService.getFlowProgressListByResourceCrn(resourceCrn));
-        response.setRecentInternalFlowOperations(progressV4Endpoint.getFlowLogsProgressByResourceCrn(resourceCrn));
+        try {
+            response.setRecentInternalFlowOperations(progressV4Endpoint.getFlowLogsProgressByResourceCrn(resourceCrn));
+        } catch (NotFoundException notFoundException) {
+            LOGGER.debug("Stack for datalake '{}' has not found yet. It is acceptable for progress response.", resourceCrn);
+        }
         return response;
     }
 }
