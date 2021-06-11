07b5727b10abc5ae731bbaa97e6f51c2753c93b8
Tibor Popovics <tiborpopovics93@gmail.com>
2021-06-11 10:15:11 +0200

    CB-12991: Improve upgrade matrix test to read the matrix definition from JSON and add more test case.

--- core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/matrix/UpgradeMatrixServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/matrix/UpgradeMatrixServiceTest.java
@@ -1,68 +1,143 @@
 package com.sequenceiq.cloudbreak.service.upgrade.matrix;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.runners.Parameterized.Parameters;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-@RunWith(MockitoJUnitRunner.class)
+import com.sequenceiq.cloudbreak.common.json.JsonUtil;
+import com.sequenceiq.cloudbreak.util.FileReaderUtils;
+
+@RunWith(Parameterized.class)
 public class UpgradeMatrixServiceTest {
 
     @InjectMocks
     private UpgradeMatrixService underTest;
 
-    @Mock
-    private UpgradeMatrixDefinition upgradeMatrixDefinition;
+    private final String currentVersion;
 
-    @Test
-    public void testIsPermittedByUpgradeMatrixShouldReturnTrueWhenTheUpgradeIsEnabledForThatVersion() {
-        when(upgradeMatrixDefinition.getRuntimeUpgradeMatrix()).thenReturn(createRuntimeUpgradeMatrix());
+    private final String targetVersion;
 
-        assertTrue(underTest.permitByUpgradeMatrix("7.1.0", "7.2.2"));
-        assertTrue(underTest.permitByUpgradeMatrix("7.2.1", "7.2.2"));
-        assertTrue(underTest.permitByUpgradeMatrix("7.2.9", "7.2.3"));
-        assertTrue(underTest.permitByUpgradeMatrix("7.2.1", "7.2.4"));
-        assertTrue(underTest.permitByUpgradeMatrix("7.2.5", "7.3.5"));
+    private final boolean expectedResult;
+
+    public UpgradeMatrixServiceTest(String currentVersion, String targetVersion, boolean expectedResult) {
+        this.currentVersion = currentVersion;
+        this.targetVersion = targetVersion;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void before() throws IOException {
+        underTest = new UpgradeMatrixService();
+        ReflectionTestUtils.setField(underTest, "upgradeMatrixDefinition", getUpgradeMatrixDefinition());
+    }
+
+    @Parameters(name = "{index}: Upgrade from {0} to {1} is permitted: {2}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"7a.2b.2c", "7.2.2", false},
+                {"17.12.12", "7.2.2", false},
+                {"a.b.c", "7.2.2", false},
+                {"7a0b1", "7.2.2", false},
+                {"7.1.0", "7.2.01", false},
+
+                {"7.0.1", "7.2.2", false},
+                {"7.0.2", "7.2.2", true},
+                {"7.1.0", "7.2.2", true},
+                {"7.1.1", "7.2.2", false},
+                {"7.2.0", "7.2.2", true},
+                {"7.2.1", "7.2.2", true},
+                {"7.2.2", "7.2.2", false},
+
+                {"7.0.2", "7.2.6", false},
+                {"7.1.0", "7.2.6", true},
+                {"7.1.1", "7.2.6", false},
+                {"7.2.0", "7.2.6", true},
+                {"7.2.1", "7.2.6", true},
+                {"7.2.2", "7.2.6", true},
+                {"7.2.3", "7.2.6", false},
+                {"7.2.6", "7.2.6", false},
+
+                {"7.0.2", "7.2.7", false},
+                {"7.1.0", "7.2.7", true},
+                {"7.1.1", "7.2.7", false},
+                {"7.2.0", "7.2.7", true},
+                {"7.2.1", "7.2.7", true},
+                {"7.2.2", "7.2.7", true},
+                {"7.2.3", "7.2.7", false},
+                {"7.2.6", "7.2.7", true},
+                {"7.2.7", "7.2.7", false},
+
+                {"7.0.2", "7.2.8", false},
+                {"7.1.0", "7.2.8", true},
+                {"7.1.1", "7.2.8", false},
+                {"7.2.0", "7.2.8", true},
+                {"7.2.1", "7.2.8", true},
+                {"7.2.2", "7.2.8", true},
+                {"7.2.3", "7.2.8", false},
+                {"7.2.6", "7.2.8", true},
+                {"7.2.7", "7.2.8", true},
+                {"7.2.8", "7.2.8", false},
+
+                {"7.0.2", "7.2.9", false},
+                {"7.1.0", "7.2.9", true},
+                {"7.1.1", "7.2.9", false},
+                {"7.2.0", "7.2.9", true},
+                {"7.2.1", "7.2.9", true},
+                {"7.2.2", "7.2.9", true},
+                {"7.2.3", "7.2.9", false},
+                {"7.2.6", "7.2.9", true},
+                {"7.2.7", "7.2.9", true},
+                {"7.2.8", "7.2.9", true},
+                {"7.2.9", "7.2.9", false},
+
+                {"7.0.2", "7.2.10", false},
+                {"7.1.0", "7.2.10", true},
+                {"7.1.1", "7.2.10", false},
+                {"7.2.0", "7.2.10", true},
+                {"7.2.1", "7.2.10", true},
+                {"7.2.2", "7.2.10", true},
+                {"7.2.3", "7.2.10", false},
+                {"7.2.6", "7.2.10", true},
+                {"7.2.7", "7.2.10", true},
+                {"7.2.8", "7.2.10", true},
+                {"7.2.9", "7.2.10", true},
+                {"7.2.10", "7.2.10", false},
+
+                {"7.0.2", "7.2.11", false},
+                {"7.1.0", "7.2.11", true},
+                {"7.1.1", "7.2.11", false},
+                {"7.2.0", "7.2.11", true},
+                {"7.2.1", "7.2.11", true},
+                {"7.2.2", "7.2.11", true},
+                {"7.2.3", "7.2.11", false},
+                {"7.2.6", "7.2.11", true},
+                {"7.2.7", "7.2.11", true},
+                {"7.2.8", "7.2.11", true},
+                {"7.2.9", "7.2.11", true},
+                {"7.2.10", "7.2.11", true},
+                {"7.2.11", "7.2.11", false},
+        });
     }
 
     @Test
-    public void testIsPermittedByUpgradeMatrixShouldReturnFalseWhenTheUpgradeIsNotEnabledForThatVersion() {
-        when(upgradeMatrixDefinition.getRuntimeUpgradeMatrix()).thenReturn(createRuntimeUpgradeMatrix());
-
-        assertFalse(underTest.permitByUpgradeMatrix("7.1.1", "7.2.2"));
-        assertFalse(underTest.permitByUpgradeMatrix("7.0.1", "7.2.3"));
-        assertFalse(underTest.permitByUpgradeMatrix("7.3.9", "7.2.4"));
-        assertFalse(underTest.permitByUpgradeMatrix("7.2.6", "7.3.5"));
-        assertFalse(underTest.permitByUpgradeMatrix("8.5.0", "8.5.5"));
+    public void testIsPermittedByUpgradeMatrix() {
+        assertEquals(expectedResult, underTest.permitByUpgradeMatrix(currentVersion, targetVersion));
     }
 
-    @Test
-    public void testIsPermittedByUpgradeMatrixShouldReturnFalseTheExpressionInTheMatrixIsNotValid() {
-        when(upgradeMatrixDefinition.getRuntimeUpgradeMatrix()).thenReturn(createInvalidRuntimeUpgradeMatrix());
-
-        assertFalse(underTest.permitByUpgradeMatrix("7.1.0", "7.2.2"));
+    private UpgradeMatrixDefinition getUpgradeMatrixDefinition() throws IOException {
+        String upgradeMatrixJson = FileReaderUtils.readFileFromPath(Path.of("src/main/resources/definitions/upgrade-matrix-definition.json"));
+        return JsonUtil.readValue(upgradeMatrixJson, UpgradeMatrixDefinition.class);
     }
 
-    private Set<RuntimeUpgradeMatrix> createRuntimeUpgradeMatrix() {
-        return Set.of(
-                createUpgradeMatrix("7.2.2", Set.of("7.1.0", "7.2.*")),
-                createUpgradeMatrix("7.2.[3,4]", Set.of("7.2.*")),
-                createUpgradeMatrix("7.3.*", Set.of("7.2.5")));
-    }
-
-    private Set<RuntimeUpgradeMatrix> createInvalidRuntimeUpgradeMatrix() {
-        return Set.of(createUpgradeMatrix("7.2.2", Set.of("{7{.1.[3,4]")));
-    }
-
-    private RuntimeUpgradeMatrix createUpgradeMatrix(String target, Set<String> source) {
-        return new RuntimeUpgradeMatrix(new Runtime(target), source.stream().map(Runtime::new).collect(Collectors.toSet()));
-    }
 }
