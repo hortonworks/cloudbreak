9366ff042414836277ca0ee562c47eb02e086222
Bryce Derriso <b.derriso@gmail.com>
2021-06-14 11:29:33 -0500

    Fix Azure with SPN issue.
    * Add tests for implementations of EnvironmentBaseNetworkConverter
    * Add tests for GcpEnvironmentNetworkConverterTest.
    * Add tests for AwsEnvironmentNetworkConverterTest.
    * Add tests for AzureEnvironmentNetworkConverter.
    * Extract getCloudSubnet method.
    * Extract buildAttributes and attachEndpointGatewaySubnet methods.
    * Override attachEndpointGatewaySubnet for Azure case.
    * Add comments to explain the AzureEnvironmentNetworkConverter

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/environment/network/AzureEnvironmentNetworkConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/environment/network/AzureEnvironmentNetworkConverter.java
@@ -4,12 +4,18 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.cloud.model.CloudSubnet;
 import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.environment.api.v1.environment.model.EnvironmentNetworkAzureParams;
 import com.sequenceiq.environment.api.v1.environment.model.response.EnvironmentNetworkResponse;
 
 @Component
 public class AzureEnvironmentNetworkConverter extends EnvironmentBaseNetworkConverter {
+
+    @Override
+    protected void attachEndpointGatewaySubnet(EnvironmentNetworkResponse source, Map<String, Object> attributes, CloudSubnet cloudSubnet) {
+        // this is intentionally left as a no-op since Azure does not need to attach an endpoint gateway subnet.
+    }
 
     @Override
     Map<String, Object> getAttributesForLegacyNetwork(EnvironmentNetworkResponse source) {
