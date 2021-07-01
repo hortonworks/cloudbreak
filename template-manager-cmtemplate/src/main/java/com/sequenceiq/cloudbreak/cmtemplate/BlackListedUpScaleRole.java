83ec57e644ea7989c412fa08222c8078f4f88aac
doktoric <rdoktorics@cloudera.com>
2021-07-01 08:49:05 +0200

    CB-10915 Enable scaling for Kafka broker type nodes

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/BlackListedUpScaleRole.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/BlackListedUpScaleRole.java
@@ -7,7 +7,7 @@ import com.sequenceiq.cloudbreak.auth.altus.model.Entitlement;
 import com.sequenceiq.cloudbreak.common.type.Versioned;
 
 public enum BlackListedUpScaleRole implements EntitledForServiceScale {
-    KAFKA_BROKER(Entitlement.DATAHUB_STREAMING_SCALING),
+    KAFKA_BROKER(Entitlement.DATAHUB_STREAMING_SCALING, "7.2.12"),
     NIFI_NODE(Entitlement.DATAHUB_FLOW_SCALING, "7.2.8"),
     ZEPPELIN_SERVER(Entitlement.DATAHUB_DEFAULT_SCALING),
     NAMENODE(Entitlement.DATAHUB_DEFAULT_SCALING);
