4e7e7ad526a9524c45cbfba504536eafcf44e8bf
Mészáros Gergely <grigorijat9026@gmail.com>
2021-07-06 23:54:30 +0200

    CB-13007 - missing "ccm_version" argument for create environment

--- environment-api/src/main/java/com/sequenceiq/environment/api/doc/environment/EnvironmentModelDescription.java
+++ environment-api/src/main/java/com/sequenceiq/environment/api/doc/environment/EnvironmentModelDescription.java
@@ -78,6 +78,8 @@ public class EnvironmentModelDescription {
     public static final String SECURITY_CIDR = "CIDR range which is allowed for inbound traffic. Either IPv4 or IPv6 is allowed.";
 
     public static final String TUNNEL = "Configuration that the connection going directly or with cluster proxy or with ccm and cluster proxy.";
+    public static final String OVERRIDE_TUNNEL = "Flag that marks that the request was intented to set the tunnel version by hand and it will not be " +
+            "overwritten by Cloudbreak";
 
     public static final String LOG_CLOUD_STORAGE = "Cloud storage configuration for this environment. Service logs will be stored in the defined location.";
 
