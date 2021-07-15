6844fd1c29ee795338610ab7c757cb7821286151
Oliver Szabo <oleewere@gmail.com>
2021-07-15 18:45:34 +0200

    Optimize cluster telemetry cleanup on termination.

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/ClusterTerminationService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cluster/flow/ClusterTerminationService.java
@@ -70,7 +70,7 @@ public class ClusterTerminationService {
         Long stackId = cluster.getStack().getId();
         String terminatedName = cluster.getName() + DELIMITER + new Date().getTime();
         cluster.setName(terminatedName);
-        clusterService.cleanupCluster(stackId);
+        clusterService.cleanupCluster(cluster);
         FileSystem fs = cluster.getFileSystem();
         if (fs != null) {
             deleteFileSystemResources(stackId, fs, force);
