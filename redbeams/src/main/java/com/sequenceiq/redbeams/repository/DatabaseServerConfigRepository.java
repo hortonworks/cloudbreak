2c3111c4317751fa50364b4f3960258ffc77194c
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2021-07-19 12:15:34 +0200

    CB-13386 Redbeams getNamesByCrn: Parameter value element did not match expected type

--- redbeams/src/main/java/com/sequenceiq/redbeams/repository/DatabaseServerConfigRepository.java
+++ redbeams/src/main/java/com/sequenceiq/redbeams/repository/DatabaseServerConfigRepository.java
@@ -12,10 +12,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.sequenceiq.authorization.service.model.projection.ResourceCrnAndNameView;
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
 import com.sequenceiq.cloudbreak.workspace.repository.EntityType;
 import com.sequenceiq.redbeams.domain.DatabaseServerConfig;
+import com.sequenceiq.redbeams.domain.RedbeamsResourceCrnAndNameView;
 
 @EntityType(entityClass = DatabaseServerConfig.class)
 @Transactional(TxType.REQUIRED)
@@ -31,21 +31,10 @@ public interface DatabaseServerConfigRepository extends JpaRepository<DatabaseSe
 
     Optional<DatabaseServerConfig> findByName(String name);
 
-    @Query("SELECT s FROM DatabaseServerConfig s WHERE s.workspaceId = :workspaceId AND s.environmentId = :environmentId "
-            + "AND (s.name IN :names OR s.resourceCrn IN :names)")
-    Set<DatabaseServerConfig> findByNameInAndWorkspaceIdAndEnvironmentId(
-            @Param("names") Set<String> names,
-            @Param("workspaceId") Long workspaceId,
-            @Param("environmentId") String environmentId);
-
-    @Query("SELECT c.resourceCrn FROM DatabaseServerConfig c WHERE c.accountId = :accountId")
-    List<Crn> findAllResourceCrnsByAccountId(@Param("accountId") String accountId);
-
     @Query("SELECT c.resourceCrn FROM DatabaseServerConfig c WHERE c.name = :name")
     Optional<Crn> findResourceCrnByName(@Param("name") String name);
 
-    @Query("SELECT c.name as name, c.resourceCrn as crn FROM DatabaseServerConfig c WHERE c.resourceCrn IN (:resourceCrns)")
-    List<ResourceCrnAndNameView> findResourceNamesByCrn(@Param("resourceCrns") Collection<String> resourceCrns);
+    List<RedbeamsResourceCrnAndNameView> findByResourceCrnIn(Collection<Crn> resourceCrns);
 
     @Query("SELECT c.resourceCrn FROM DatabaseServerConfig c WHERE c.name IN (:names)")
     List<Crn> findResourceCrnsByNames(@Param("names") Collection<String> names);
