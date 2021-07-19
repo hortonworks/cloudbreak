7180c3d94274274441cb5f28e6735ab0523f417c
doktoric <rdoktorics@cloudera.com>
2021-07-19 09:54:07 +0200

    CB-13438 instance group converter fails in case of mock (at least for authz tests)

--- core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/InstanceGroupV1ToInstanceGroupV4Converter.java
+++ core/src/main/java/com/sequenceiq/distrox/v1/distrox/converter/InstanceGroupV1ToInstanceGroupV4Converter.java
@@ -194,7 +194,7 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
                 if (distroxNetwork.getAws() != null) {
                     request = new InstanceGroupNetworkV1Request();
                     InstanceGroupAwsNetworkV1Parameters aws = new InstanceGroupAwsNetworkV1Parameters();
-                    aws.setSubnetIds(List.of(distroxNetwork.getAws().getSubnetId()));
+                    aws.setSubnetIds(getSubnetIds(distroxNetwork.getAws().getSubnetId()));
                     request.setAws(aws);
                 }
                 break;
@@ -202,7 +202,7 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
                 if (distroxNetwork.getAzure() != null) {
                     request = new InstanceGroupNetworkV1Request();
                     InstanceGroupAzureNetworkV1Parameters azure = new InstanceGroupAzureNetworkV1Parameters();
-                    azure.setSubnetIds(List.of(distroxNetwork.getAzure().getSubnetId()));
+                    azure.setSubnetIds(getSubnetIds(distroxNetwork.getAzure().getSubnetId()));
                     request.setAzure(azure);
                 }
                 break;
@@ -210,7 +210,7 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
                 if (distroxNetwork.getMock() != null) {
                     request = new InstanceGroupNetworkV1Request();
                     InstanceGroupMockNetworkV1Parameters mock = new InstanceGroupMockNetworkV1Parameters();
-                    mock.setSubnetIds(List.of(distroxNetwork.getMock().getSubnetId()));
+                    mock.setSubnetIds(getSubnetIds(distroxNetwork.getMock().getSubnetId()));
                     request.setMock(mock);
                 }
                 break;
@@ -218,13 +218,21 @@ public class InstanceGroupV1ToInstanceGroupV4Converter {
                 if (distroxNetwork.getGcp() != null) {
                     request = new InstanceGroupNetworkV1Request();
                     InstanceGroupGcpNetworkV1Parameters gcp = new InstanceGroupGcpNetworkV1Parameters();
-                    gcp.setSubnetIds(List.of(distroxNetwork.getGcp().getSubnetId()));
+                    gcp.setSubnetIds(getSubnetIds(distroxNetwork.getGcp().getSubnetId()));
                     request.setGcp(gcp);
                 }
                 break;
             default:
         }
         return request;
+    }
+
+    private List<String> getSubnetIds(String subnet) {
+        if (Strings.isNullOrEmpty(subnet)) {
+            return List.of();
+        } else {
+            return List.of(subnet);
+        }
     }
 
     private boolean instanceGroupRequestHasSubnet(InstanceGroupV1Request instanceGroupV1Request, DetailedEnvironmentResponse environment) {
