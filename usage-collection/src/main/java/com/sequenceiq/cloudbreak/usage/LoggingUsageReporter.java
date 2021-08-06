0db5779fe7bcd50f0cdd634c5decd09f9da05b1b
anagy <anagy@cloudera.com>
2021-08-06 12:56:08 +0200

    CB-13684 Add error handling and additional logging to LoggingUsageReporter

--- usage-collection/src/main/java/com/sequenceiq/cloudbreak/usage/LoggingUsageReporter.java
+++ usage-collection/src/main/java/com/sequenceiq/cloudbreak/usage/LoggingUsageReporter.java
@@ -18,106 +18,169 @@ import com.sequenceiq.cloudbreak.logger.MDCBuilder;
  */
 @Service
 public class LoggingUsageReporter implements UsageReporter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingUsageReporter.class);
+
     private static final Logger BINARY_EVENT_LOGGER = LoggerFactory.getLogger("CDP_BINARY_USAGE_EVENT");
 
     private static final String USAGE_EVENT_MDC_NAME = "binaryUsageEvent";
 
     @Override
     public void cdpDatahubClusterRequested(long timestamp, UsageProto.CDPDatahubClusterRequested details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setTimestamp(timestamp)
-                .setCdpDatahubClusterRequested(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setTimestamp(timestamp)
+                    .setCdpDatahubClusterRequested(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatahubClusterStatusChanged(UsageProto.CDPDatahubClusterStatusChanged details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatahubClusterStatusChanged(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatahubClusterStatusChanged(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatalakeClusterRequested(long timestamp, UsageProto.CDPDatalakeClusterRequested details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setTimestamp(timestamp)
-                .setCdpDatalakeClusterRequested(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setTimestamp(timestamp)
+                    .setCdpDatalakeClusterRequested(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatalakeClusterStatusChanged(UsageProto.CDPDatalakeClusterStatusChanged details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatalakeClusterStatusChanged(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatalakeClusterStatusChanged(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpEnvironmentRequested(UsageProto.CDPEnvironmentRequested details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpEnvironmentRequested(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpEnvironmentRequested(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpEnvironmentStatusChanged(UsageProto.CDPEnvironmentStatusChanged details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpEnvironmentStatusChanged(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpEnvironmentStatusChanged(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatalakeRequested(UsageProto.CDPDatalakeRequested details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatalakeRequested(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatalakeRequested(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatalakeStatusChanged(UsageProto.CDPDatalakeStatusChanged details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatalakeStatusChanged(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatalakeStatusChanged(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatahubRequested(UsageProto.CDPDatahubRequested details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatahubRequested(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatahubRequested(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatahubStatusChanged(UsageProto.CDPDatahubStatusChanged details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatahubStatusChanged(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatahubStatusChanged(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatalakeSync(UsageProto.CDPDatalakeSync details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatalakeSync(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatalakeSync(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     @Override
     public void cdpDatahubSync(UsageProto.CDPDatahubSync details) {
-        checkNotNull(details);
-        log(eventBuilder()
-                .setCdpDatahubSync(details)
-                .build());
+        try {
+            checkNotNull(details);
+            log(eventBuilder()
+                    .setCdpDatahubSync(details)
+                    .build());
+            LOGGER.info("Logged binary format for the following usage event: {}", details);
+        } catch (Exception e) {
+            LOGGER.warn("Could not log binary format for the following usage event: {}! Cause: {}", details, e.getMessage());
+        }
     }
 
     private UsageProto.Event.Builder eventBuilder() {
@@ -128,6 +191,7 @@ public class LoggingUsageReporter implements UsageReporter {
     }
 
     void log(UsageProto.Event event) {
+        LOGGER.info("Logging binary format for the following usage event: {}", event);
         String binaryUsageEvent = BaseEncoding.base64().encode(event.toByteArray());
         MDCBuilder.addMdcField(USAGE_EVENT_MDC_NAME, binaryUsageEvent);
         BINARY_EVENT_LOGGER.info(binaryUsageEvent);
