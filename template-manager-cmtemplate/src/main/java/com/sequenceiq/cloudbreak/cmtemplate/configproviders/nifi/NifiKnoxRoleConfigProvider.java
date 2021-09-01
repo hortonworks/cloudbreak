6fc5e0a761187da30d179416dd64321370ff7290
vbrodetskyi <vbrodetskyi@cloudera.com>
2021-09-01 14:09:06 +0300

    CB-13983 Do not set nifi.ui.knox.url property automatically. Removed all properties that were set for NiFi Knox automatically, but didn't remove a class, because in future it should be useful.

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/nifi/NifiKnoxRoleConfigProvider.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/nifi/NifiKnoxRoleConfigProvider.java
@@ -1,15 +1,8 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.nifi;
 
-import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.CFM_VERSION_2_0_0_0;
-import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.isVersionNewerOrEqualThanLimited;
-import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -19,22 +12,14 @@ import org.springframework.stereotype.Component;
 
 import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
 import com.sequenceiq.cloudbreak.api.service.ExposedServiceCollector;
-import com.sequenceiq.cloudbreak.cloud.model.ClouderaManagerProduct;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRoleConfigProvider;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
-import com.sequenceiq.cloudbreak.template.views.ClusterExposedServiceView;
 
 @Component
 public class NifiKnoxRoleConfigProvider extends AbstractRoleConfigProvider {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NifiKnoxRoleConfigProvider.class);
-
-    private static final String PROXY_CONTEXT_PATH = "nifi.web.proxy.context.path";
-
-    private static final String NIFI_UI_KNOX_URL = "nifi.ui.knox.url";
-
-    private static final String CFM = "CFM";
 
     @Inject
     private ExposedServiceCollector exposedServiceCollector;
@@ -43,58 +28,7 @@ public class NifiKnoxRoleConfigProvider extends AbstractRoleConfigProvider {
     public List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, TemplatePreparationObject source) {
         LOGGER.info("add property values for NifiKnoxRoleConfigProvider");
         List<ApiClusterTemplateConfig> configs = new ArrayList<>();
-
-        Set<String> topologyNames = source.getGatewayView().getGatewayTopologies().keySet();
-        if (!topologyNames.isEmpty()) {
-            String clusterName = source.getGeneralClusterConfigs().getClusterName();
-            String proxyContextPath = topologyNames.stream()
-                    .map(topologyName -> String.format("%s/%s/nifi-app,%s/%s/nifi-app",
-                            clusterName, topologyName,
-                            clusterName, topologyName + "-api"))
-                    .collect(Collectors.joining(","));
-            LOGGER.debug("{} = {} added to template", PROXY_CONTEXT_PATH, proxyContextPath);
-            configs.add(config(PROXY_CONTEXT_PATH, proxyContextPath));
-        }
-
-        Optional<ClouderaManagerProduct> cfm = getClouderaManagerProduct(source);
-        Optional<ClusterExposedServiceView> nifi = getNifi(source);
-
-        if (cfm.isPresent() && isVersionNewerOrEqualThanLimited(cfm.get().getVersion(), CFM_VERSION_2_0_0_0) && nifi.isPresent()) {
-            String nifiUiKnoxURL = nifi.get().getServiceUrl();
-            LOGGER.debug("{} = {} added to template", NIFI_UI_KNOX_URL, nifiUiKnoxURL);
-            configs.add(config(NIFI_UI_KNOX_URL, nifiUiKnoxURL));
-        }
         return configs;
-    }
-
-    private Optional<ClusterExposedServiceView> getNifi(TemplatePreparationObject source) {
-        if (source.getExposedServices() != null && !source.getExposedServices().isEmpty()) {
-            Optional<ClusterExposedServiceView> nifi = source.getExposedServices().get("cdp-proxy")
-                    .stream()
-                    .filter(e -> e.getKnoxService().equalsIgnoreCase(exposedServiceCollector.getNiFiService().getName()))
-                    .findFirst();
-            if (nifi.isEmpty()) {
-                LOGGER.info("Nifi is not presented as an Exposed service");
-            }
-            return nifi;
-        }
-        LOGGER.info("Exposed service null or empty for NifiKnoxRoleConfigProvider");
-        return Optional.empty();
-    }
-
-    private Optional<ClouderaManagerProduct> getClouderaManagerProduct(TemplatePreparationObject source) {
-        if (source.getProductDetailsView() != null) {
-            Optional<ClouderaManagerProduct> cfm = source.getProductDetailsView().getProducts()
-                    .stream()
-                    .filter(e -> e.getName().equalsIgnoreCase(CFM))
-                    .findFirst();
-            if (cfm.isEmpty()) {
-                LOGGER.info("CFM is not presented as a Production");
-            }
-            return cfm;
-        }
-        LOGGER.info("Product null for NifiKnoxRoleConfigProvider");
-        return Optional.empty();
     }
 
     @Override
