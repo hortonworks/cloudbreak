3ec805be502e89e44d6d218e699853fc93288472
Tamas Bihari <tbihari@hortonworks.com>
2021-09-06 17:42:25 +0200

    CB-13711 create index on instancegroup_availabilityzones by instancegroup_id to mitigate database CPU usage

---
+++ freeipa/src/main/resources/schema/app/20210906145828_CB-13711_create_index_on_instancegroup_availabilityzones_by_instancegroup_id_to_mitigate_database_CPU_usage.sql
@@ -0,0 +1,11 @@
+-- // CB-13711 create index on instancegroup_availabilityzones by instancegroup_id to mitigate database CPU usage
+-- Migration SQL that makes the change goes here.
+CREATE INDEX IF NOT EXISTS idx_instancegroup_availabilityzones_instancegroupid
+    ON instancegroup_availabilityzones (instancegroup_id);
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+DROP INDEX IF EXISTS idx_instancegroup_availabilityzones_instancegroupid;
+
+
