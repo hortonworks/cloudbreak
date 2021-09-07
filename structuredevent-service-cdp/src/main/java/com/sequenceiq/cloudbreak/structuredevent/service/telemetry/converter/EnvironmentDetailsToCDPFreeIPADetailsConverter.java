934661e1e238e5e318f5478b66416d66bd829f61
doktoric <rdoktorics@cloudera.com>
2021-09-07 15:27:38 +0200

    CB-13510 adding multiaz to telemetry tracking. Now it is presented on Datalake, DataHub, FreeIPA.

--- structuredevent-service-cdp/src/main/java/com/sequenceiq/cloudbreak/structuredevent/service/telemetry/converter/EnvironmentDetailsToCDPFreeIPADetailsConverter.java
+++ structuredevent-service-cdp/src/main/java/com/sequenceiq/cloudbreak/structuredevent/service/telemetry/converter/EnvironmentDetailsToCDPFreeIPADetailsConverter.java
@@ -26,6 +26,7 @@ public class EnvironmentDetailsToCDPFreeIPADetailsConverter {
 
             if (freeIpaCreationDto != null) {
                 cdpFreeIPADetails.setNodes(freeIpaCreationDto.getInstanceCountByGroup());
+                cdpFreeIPADetails.setMultiAz(freeIpaCreationDto.isEnableMultiAz());
 
                 FreeIpaCreationAwsParametersDto aws = freeIpaCreationDto.getAws();
 
