d588d11fcd1635e9f77bf8c7e9bd6e4a05abfbe9
Peter Darvasi <pdarvasi@hortonworks.com>
2021-09-20 12:36:12 +0200

    CB-14179 - Remove EphemeralClusterUpdate logic

--- core/src/main/java/com/sequenceiq/cloudbreak/reactor/api/event/orchestration/EphemeralClusterUpdateTriggerEvent.java
+++
@@ -1,9 +0,0 @@
-package com.sequenceiq.cloudbreak.reactor.api.event.orchestration;
-
-import com.sequenceiq.cloudbreak.reactor.api.event.StackEvent;
-
-public class EphemeralClusterUpdateTriggerEvent extends StackEvent {
-    public EphemeralClusterUpdateTriggerEvent(String selector, Long stackId) {
-        super(selector, stackId);
-    }
-}
