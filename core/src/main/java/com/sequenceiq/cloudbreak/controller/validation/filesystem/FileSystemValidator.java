b4ba6d99cb7067075a898e6e70b57e5bfe6c5fc9
Gergely Topolyai <gtopolyai@hortonworks.com>
2021-09-22 14:40:01 +0200

    CB-14219 Removes StackValidationV4Request and related codes

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/filesystem/FileSystemValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/validation/filesystem/FileSystemValidator.java
@@ -6,15 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.FileSystemValidationV4Request;
-import com.sequenceiq.cloudbreak.aspect.Measure;
 import com.sequenceiq.cloudbreak.cloud.context.CloudContext;
 import com.sequenceiq.cloudbreak.cloud.event.validation.FileSystemValidationRequest;
 import com.sequenceiq.cloudbreak.cloud.event.validation.FileSystemValidationResult;
 import com.sequenceiq.cloudbreak.cloud.model.CloudCredential;
 import com.sequenceiq.cloudbreak.cloud.model.SpiFileSystem;
 import com.sequenceiq.cloudbreak.common.exception.BadRequestException;
-import com.sequenceiq.cloudbreak.converter.spi.FileSystemValidationV4RequestToSpiFileSystemConverter;
 import com.sequenceiq.cloudbreak.converter.v4.stacks.cluster.CloudStorageConverter;
 import com.sequenceiq.cloudbreak.service.OperationException;
 import com.sequenceiq.common.api.cloudstorage.CloudStorageBase;
@@ -36,9 +33,6 @@ public class FileSystemValidator {
     @Inject
     private CloudStorageConverter cloudStorageConverter;
 
-    @Inject
-    private FileSystemValidationV4RequestToSpiFileSystemConverter fileSystemValidationV4RequestToSpiFileSystemConverter;
-
     public void validate(String platform, CloudCredential cloudCredential, CloudStorageBase cloudStorageRequest,
             Long workspaceId) {
         if (cloudStorageRequest == null) {
@@ -50,33 +44,6 @@ public class FileSystemValidator {
                 .withWorkspaceId(workspaceId)
                 .build();
         SpiFileSystem spiFileSystem = cloudStorageConverter.requestToSpiFileSystem(cloudStorageRequest);
-        FileSystemValidationRequest request = new FileSystemValidationRequest(spiFileSystem, cloudCredential, cloudContext);
-        eventBus.notify(request.selector(), eventFactory.createEvent(request));
-        try {
-            FileSystemValidationResult result = request.await();
-            LOGGER.info("File system validation result: {}", result);
-            Exception exception = result.getErrorDetails();
-            if (exception != null) {
-                throw new BadRequestException(result.getStatusReason(), exception);
-            }
-        } catch (InterruptedException e) {
-            LOGGER.error("Error while sending the file system validation request", e);
-            throw new OperationException(e);
-        }
-    }
-
-    @Measure(FileSystemValidator.class)
-    public void validateFileSystem(String platform, CloudCredential cloudCredential, FileSystemValidationV4Request fileSystemValidationV4Request,
-            Long workspaceId) {
-        if (fileSystemValidationV4Request == null) {
-            return;
-        }
-        LOGGER.info("Sending fileSystemRequest to {} to validate the file system", platform);
-        CloudContext cloudContext = CloudContext.Builder.builder()
-                .withPlatform(platform)
-                .withWorkspaceId(workspaceId)
-                .build();
-        SpiFileSystem spiFileSystem = fileSystemValidationV4RequestToSpiFileSystemConverter.convert(fileSystemValidationV4Request);
         FileSystemValidationRequest request = new FileSystemValidationRequest(spiFileSystem, cloudCredential, cloudContext);
         eventBus.notify(request.selector(), eventFactory.createEvent(request));
         try {
