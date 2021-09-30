96314a3e698c0547e214ae277144b1b97beb5c8f
Peter Erdos <perdos@cloudera.com>
2021-09-30 20:04:28 +0200

    CB-14322 Add missing index on sdxcluster which is missing since detached flag was introduced

---
+++ datalake/src/main/resources/schema/app/20210930144636_CB-14322_add_missing_indexes_on_sdxcluster.sql
@@ -0,0 +1,28 @@
+-- // CB-14322
+-- Migration SQL that makes the change goes here.
+
+CREATE UNIQUE INDEX unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null
+    ON sdxcluster (accountid, envCrn, deleted, detached)
+    WHERE deleted IS NULL;
+
+CREATE UNIQUE INDEX unq_index_sdxcluster_accid_crn_detached_deleted_is_null
+    ON sdxcluster (accountid, crn, deleted, detached)
+    WHERE deleted IS NULL;
+
+CREATE UNIQUE INDEX unq_index_sdxcluster_accid_envcrn_detached_deleted_is_not_null
+    ON sdxcluster (accountid, envCrn, deleted, detached)
+    WHERE deleted IS NOT NULL;
+
+CREATE UNIQUE INDEX unq_index_sdxcluster_accid_crn_detached_deleted_is_not_null
+    ON sdxcluster (accountid, crn, deleted, detached)
+    WHERE deleted IS NOT NULL;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP INDEX IF EXISTS unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null;
+DROP INDEX IF EXISTS unq_index_sdxcluster_accid_crn_detached_deleted_is_null;
+DROP INDEX IF EXISTS unq_index_sdxcluster_accid_envcrn_detached_deleted_is_not_null;
+DROP INDEX IF EXISTS unq_index_sdxcluster_accid_crn_detached_deleted_is_not_null;
+
+
