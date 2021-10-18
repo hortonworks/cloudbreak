4571ff6725e7c48f0d8b2947fd4353417c130ea2
Peter Erdos <perdos@cloudera.com>
2021-10-18 14:29:05 +0200

    CB-14322 add missed IF NOT EXISTS for index sqls

--- datalake/src/main/resources/schema/app/20210930144636_CB-14322_add_missing_indexes_on_sdxcluster.sql
+++ datalake/src/main/resources/schema/app/20210930144636_CB-14322_add_missing_indexes_on_sdxcluster.sql
@@ -1,19 +1,19 @@
 -- // CB-14322
 -- Migration SQL that makes the change goes here.
 
-CREATE UNIQUE INDEX unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null
+CREATE UNIQUE INDEX IF NOT EXISTS unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null
     ON sdxcluster (accountid, envCrn, deleted, detached)
     WHERE deleted IS NULL;
 
-CREATE UNIQUE INDEX unq_index_sdxcluster_accid_crn_detached_deleted_is_null
+CREATE UNIQUE INDEX IF NOT EXISTS unq_index_sdxcluster_accid_crn_detached_deleted_is_null
     ON sdxcluster (accountid, crn, deleted, detached)
     WHERE deleted IS NULL;
 
-CREATE UNIQUE INDEX unq_index_sdxcluster_accid_envcrn_detached_deleted_is_not_null
+CREATE UNIQUE INDEX IF NOT EXISTS unq_index_sdxcluster_accid_envcrn_detached_deleted_is_not_null
     ON sdxcluster (accountid, envCrn, deleted, detached)
     WHERE deleted IS NOT NULL;
 
-CREATE UNIQUE INDEX unq_index_sdxcluster_accid_crn_detached_deleted_is_not_null
+CREATE UNIQUE INDEX IF NOT EXISTS unq_index_sdxcluster_accid_crn_detached_deleted_is_not_null
     ON sdxcluster (accountid, crn, deleted, detached)
     WHERE deleted IS NOT NULL;
 
