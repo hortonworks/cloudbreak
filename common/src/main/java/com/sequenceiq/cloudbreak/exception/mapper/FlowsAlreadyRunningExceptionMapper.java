4ebe3e2608b7d10c93db58df988cba08a2a9da91
Gergely Topolyai <gtopolyai@hortonworks.com>
2021-11-04 15:50:56 +0100

    CB-14297 FlowsAlreadyRunningExceptionMapper moved to common module

--- common/src/main/java/com/sequenceiq/cloudbreak/exception/mapper/FlowsAlreadyRunningExceptionMapper.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/exception/mapper/FlowsAlreadyRunningExceptionMapper.java
@@ -1,11 +1,10 @@
-package com.sequenceiq.freeipa.controller.mapper;
+package com.sequenceiq.cloudbreak.exception.mapper;
 
 import javax.ws.rs.core.Response.Status;
 
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.exception.FlowsAlreadyRunningException;
-import com.sequenceiq.cloudbreak.exception.mapper.BaseExceptionMapper;
 
 @Component
 public class FlowsAlreadyRunningExceptionMapper extends BaseExceptionMapper<FlowsAlreadyRunningException> {
