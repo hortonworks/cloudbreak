4d3c5d754dd47f7dd94c405b537d075c8399a36b
Attila Kristof Foldi <foldik@cloudera.com>
2021-11-09 12:34:39 +0100

    CB-14607 Throw more usefull message for auto scaling failure than default aws waiter's message.

--- cloud-aws-cloudformation/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/scheduler/StackCancellationCheck.java
+++ cloud-aws-cloudformation/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/scheduler/StackCancellationCheck.java
@@ -2,14 +2,10 @@ package com.sequenceiq.cloudbreak.cloud.aws.scheduler;
 
 import static com.sequenceiq.cloudbreak.cloud.scheduler.PollGroup.CANCELLED;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.sequenceiq.cloudbreak.cloud.scheduler.PollGroup;
 import com.sequenceiq.cloudbreak.cloud.store.InMemoryStateStore;
 
 public class StackCancellationCheck implements CancellationCheck {
-    private static final Logger LOGGER = LoggerFactory.getLogger(StackCancellationCheck.class);
 
     private final Long stackId;
 
