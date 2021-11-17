62c4ceb32d20f88bd5cdd4f3315a797fa5c57385
Attila Kristof Foldi <foldik@cloudera.com>
2021-11-17 08:40:31 +0100

    CB-14896 Fix StackBasedStatusCheckerTask to query less data from the database for exit criteria. Deleted unused methods from StackService. Fixed NginxCertListenerTask in core to check if the stack is getting terminated.

--- core/src/main/java/com/sequenceiq/cloudbreak/service/StackBasedStatusCheckerTask.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/StackBasedStatusCheckerTask.java
@@ -1,13 +1,10 @@
 package com.sequenceiq.cloudbreak.service;
 
-import java.util.Optional;
-
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.polling.SimpleStatusCheckerTask;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
 
@@ -21,10 +18,9 @@ public abstract class StackBasedStatusCheckerTask<T extends StackContext> extend
     @Override
     public boolean exitPolling(T t) {
         try {
-            Optional<Stack> stack = stackService.findById(t.getStack().getId());
-            return !stack.isPresent() || stack.get().isDeleteInProgress() || stack.get().isDeleteCompleted();
+            return !stackService.stackExistsAndNotDeleting(t.getStack().getId());
         } catch (Exception ex) {
-            LOGGER.debug("Error occurred when check status checker exit criteria: ");
+            LOGGER.debug("Error occurred when check status checker exit criteria: ", ex);
             return true;
         }
     }
