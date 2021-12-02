112d04b80eb4aeb8b6198a5bcde4537bc539a015
Egyed Tamas <egyed.t@cloudera.com>
2021-12-02 15:21:27 +0100

    CB-15334 Add missing goal lists to Cruise Control config provider
    Starting from CDH>=7.2.14 Cruise Control config provider will set the supported goals list too beside the other goal sets.
    
    Tested:
    - with unit tests,
    - manually on a provisioned cluster.

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/cruisecontrol/CruiseControlGoalConfigs.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/cruisecontrol/CruiseControlGoalConfigs.java
@@ -2,6 +2,25 @@ package com.sequenceiq.cloudbreak.cmtemplate.configproviders.cruisecontrol;
 
 public class CruiseControlGoalConfigs {
 
+    public static final String COMMON_SUPPORTED_GOALS = "com.linkedin.kafka.cruisecontrol.analyzer.goals.RackAwareDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.ReplicaCapacityGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.DiskCapacityGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.NetworkInboundCapacityGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.NetworkOutboundCapacityGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.CpuCapacityGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.ReplicaDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.PotentialNwOutGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.DiskUsageDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.NetworkInboundUsageDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.NetworkOutboundUsageDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.CpuUsageDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.TopicReplicaDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.LeaderReplicaDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.LeaderBytesInDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.goals.PreferredLeaderElectionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.kafkaassigner.KafkaAssignerDiskUsageDistributionGoal," +
+            "com.linkedin.kafka.cruisecontrol.analyzer.kafkaassigner.KafkaAssignerEvenRackAwareGoal";
+
     public static final String COMMON_DEFAULT_GOALS = "com.linkedin.kafka.cruisecontrol.analyzer.goals.ReplicaCapacityGoal," +
             "com.linkedin.kafka.cruisecontrol.analyzer.goals.DiskCapacityGoal," +
             "com.linkedin.kafka.cruisecontrol.analyzer.goals.NetworkInboundCapacityGoal," +
