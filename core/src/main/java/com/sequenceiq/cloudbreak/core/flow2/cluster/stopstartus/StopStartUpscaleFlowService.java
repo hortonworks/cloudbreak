988b653d4d862b152e06220fb661592dca961b1f
Siddharth Seth <sseth@cloudera.com>
2021-12-02 13:49:58 -0800

    Additional changes after rebase to work with Stack/Cluster Status consolidation.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/stopstartus/StopStartUpscaleFlowService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/stopstartus/StopStartUpscaleFlowService.java
@@ -3,11 +3,10 @@ package com.sequenceiq.cloudbreak.core.flow2.cluster.stopstartus;
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.AVAILABLE;
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_FAILED;
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_IN_PROGRESS;
-import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_RE_REGISTER_WITH_CLUSTER_PROXY;
-import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_FINISHED;
 import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_FAILED;
-import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_INIT;
 import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_COMMISSIONING;
+import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_FINISHED;
+import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_INIT;
 import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_NODES_STARTED;
 
 import java.util.Collection;
@@ -30,7 +29,6 @@ import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.host.HostGroup;
 import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceMetaData;
 import com.sequenceiq.cloudbreak.domain.view.StackView;
-import com.sequenceiq.cloudbreak.event.ResourceEvent;
 import com.sequenceiq.cloudbreak.service.StackUpdater;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterService;
 import com.sequenceiq.cloudbreak.service.hostgroup.HostGroupService;
@@ -63,7 +61,7 @@ class StopStartUpscaleFlowService {
     //  What inside vs outside a transaction. Forcing a persist?, etc
 
     void startingInstances(long stackId, String hostGroupName, int nodeCount) {
-        clusterService.updateClusterStatusByStackId(stackId, UPDATE_IN_PROGRESS,
+        clusterService.updateClusterStatusByStackId(stackId, DetailedStackStatus.UPSCALE_IN_PROGRESS,
                 String.format("Scaling up (stopstart) host  group: %s", hostGroupName));
         flowMessageService.fireEventAndLog(stackId, UPDATE_IN_PROGRESS.name(), CLUSTER_SCALING_STOPSTART_UPSCALE_INIT, hostGroupName, String.valueOf(nodeCount));
     }
@@ -73,27 +71,18 @@ class StopStartUpscaleFlowService {
         // TODO CB-14929: Introduce a new state when STARTING. CREATED is not very useful in the context of START/STOP.
         //  Likely a mirror of the CREATED state in terms of functionality.
         instancesStarted.stream().forEach(x -> instanceMetaDataService.updateInstanceStatus(x, InstanceStatus.CREATED));
-        stackUpdater.updateStackStatus(stack.getId(), DetailedStackStatus.STARTED, "Instances: " + instancesStarted.size() + " started successfully.");
+        stackUpdater.updateStackStatus(stack.getId(), DetailedStackStatus.STARTING_CLUSTER_MANAGER_SERVICES,
+                "Instances: " + instancesStarted.size() + " started successfully.");
         flowMessageService.fireEventAndLog(stack.getId(), AVAILABLE.name(), CLUSTER_SCALING_STOPSTART_UPSCALE_NODES_STARTED,
                 String.valueOf(instancesStarted.size()), instancesStarted.stream().map(x -> x.getInstanceId()).collect(Collectors.toList()).toString());
     }
 
     void upscaleCommissionNewNodes(long stackId, String hostGroupName, List<String> instanceIds) {
         // TODO CB-14929: Update instance state to SERVICES_RUNNING? (or introduce a new state) at this point, rather than when the instances start up
-        clusterService.updateClusterStatusByStackId(stackId, UPDATE_IN_PROGRESS,
+        clusterService.updateClusterStatusByStackId(stackId, DetailedStackStatus.STARTING_CLUSTER_MANAGER_SERVICES,
                 String.format("Commissioning via CM: %s", hostGroupName));
         flowMessageService.fireEventAndLog(stackId, UPDATE_IN_PROGRESS.name(), CLUSTER_SCALING_STOPSTART_UPSCALE_COMMISSIONING,
                 hostGroupName, String.valueOf(instanceIds.size()), instanceIds.toString());
-    }
-
-    void reRegisterWithClusterProxy(long stackId) {
-        clusterService.updateClusterStatusByStackId(stackId, UPDATE_IN_PROGRESS, "Re-registering with Cluster Proxy service.");
-        flowMessageService.fireEventAndLog(stackId, UPDATE_IN_PROGRESS.name(), CLUSTER_RE_REGISTER_WITH_CLUSTER_PROXY);
-    }
-
-    private void sendMessage(long stackId, ResourceEvent resourceEvent, String statusReason) {
-        clusterService.updateClusterStatusByStackId(stackId, UPDATE_IN_PROGRESS, statusReason);
-        flowMessageService.fireEventAndLog(stackId, UPDATE_IN_PROGRESS.name(), resourceEvent);
     }
 
     void clusterUpscaleFinished(StackView stackView, String hostgroupName, List<InstanceMetaData> commissioned) {
@@ -101,13 +90,12 @@ class StopStartUpscaleFlowService {
         boolean success = numOfFailedHosts == 0;
         if (success) {
             LOGGER.debug("stopstart upscaled successfully");
-            clusterService.updateClusterStatusByStackId(stackView.getId(), AVAILABLE);
-            commissioned.stream().forEach(x -> instanceMetaDataService.updateInstanceStatus(x, InstanceStatus.SERVICES_RUNNING));
-            stackUpdater.updateStackStatus(stackView.getId(), DetailedStackStatus.PROVISIONED, String.format("will this update the final state to available?"));
+            commissioned.stream().forEach(x -> instanceMetaDataService.updateInstanceStatus(x, InstanceStatus.SERVICES_HEALTHY));
+            stackUpdater.updateStackStatus(stackView.getId(), DetailedStackStatus.AVAILABLE, String.format("finished starting nodes"));
             flowMessageService.fireEventAndLog(stackView.getId(), AVAILABLE.name(), CLUSTER_SCALING_STOPSTART_UPSCALE_FINISHED, hostgroupName);
         } else {
             LOGGER.debug("stopstart upscale failed. {} hosts failed to upscale", numOfFailedHosts);
-            clusterService.updateClusterStatusByStackId(stackView.getId(), UPDATE_FAILED);
+            clusterService.updateClusterStatusByStackId(stackView.getId(), DetailedStackStatus.UPSCALE_FAILED);
             flowMessageService.fireEventAndLog(stackView.getId(), UPDATE_FAILED.name(), CLUSTER_SCALING_FAILED, "added to",
                     String.format("Cluster upscale operation failed on %d node(s).", numOfFailedHosts));
         }
@@ -115,8 +103,8 @@ class StopStartUpscaleFlowService {
 
     void clusterUpscaleFailed(long stackId, Exception errorDetails) {
         LOGGER.info("Error during stopstart upscale flow: " + errorDetails.getMessage(), errorDetails);
-        clusterService.updateClusterStatusByStackId(stackId, UPDATE_FAILED, errorDetails.getMessage());
-        stackUpdater.updateStackStatus(stackId, DetailedStackStatus.PROVISIONED,
+        clusterService.updateClusterStatusByStackId(stackId, DetailedStackStatus.UPSCALE_FAILED, errorDetails.getMessage());
+        stackUpdater.updateStackStatus(stackId, DetailedStackStatus.UPSCALE_FAILED,
                 String.format("New node(s) could not be added to the cluster: %s", errorDetails));
         flowMessageService.fireEventAndLog(stackId, UPDATE_FAILED.name(), CLUSTER_SCALING_FAILED, "added to", errorDetails.getMessage());
     }
--- core/src/main/java/com/sequenceiq/cloudbreak/service/StackCommonService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/StackCommonService.java
@@ -223,7 +223,6 @@ public class StackCommonService {
         }
         validateStackIsNotDataLake(stack.get(), instanceIds);
         return stackOperationService.removeInstances(stack.get(), instanceIds, forced, scalingStrategy);
-        return stackOperationService.removeInstances(stack.get(), instanceIds, forced);
     }
 
     public FlowIdentifier deleteMultipleInstancesInWorkspace(NameOrCrn nameOrCrn, Long workspaceId, Set<String> instanceIds, boolean forced) {
@@ -379,7 +378,7 @@ public class StackCommonService {
 
     private FlowIdentifier put(Stack stack, UpdateStackV4Request updateRequest) {
         MDCBuilder.buildMdcContext(stack);
-        if (updatCommeRequest.getStatus() != null) {
+        if (updateRequest.getStatus() != null) {
             return stackOperationService.updateStatus(stack.getId(), updateRequest.getStatus(), updateRequest.getWithClusterEvent());
         } else {
             Integer scalingAdjustment = updateRequest.getInstanceGroupAdjustment().getScalingAdjustment();
