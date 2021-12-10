70c395ea1d417fe583acfd5fb91268795cdafc95
ganeshk <ganeshk@cloudera.com>
2021-12-09 18:26:08 -0800

    CB-15154 Validation checks for stop/start scaling
    1. Cleaned up validtion for downscale.
    2. Need to add more tests for downscale especially in stack common service test.
    3. End to end test pending.

--- core/src/test/java/com/sequenceiq/cloudbreak/service/stack/flow/UpdateNodeCountValidatorTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stack/flow/UpdateNodeCountValidatorTest.java
@@ -1,5 +1,8 @@
 package com.sequenceiq.cloudbreak.service.stack.flow;
 
+import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.AVAILABLE;
+import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.DELETE_IN_PROGRESS;
+import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.NODE_FAILURE;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
@@ -10,6 +13,7 @@ import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,8 +23,10 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.autoscales.request.InstanceGroupAdjustmentV4Request;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
 import com.sequenceiq.cloudbreak.common.exception.BadRequestException;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.domain.stack.instance.InstanceGroup;
 import com.sequenceiq.common.api.type.ScalabilityOption;
 
@@ -96,4 +102,49 @@ public class UpdateNodeCountValidatorTest {
         );
     }
 
+    @ParameterizedTest(name = "The stack status is {0}.")
+    @MethodSource("testValidateStatusForStartHostGroupData")
+    public void testValidateStatusForStartHostGroup(
+            Status status,
+            Optional<String> errorMessageSegment) {
+        Stack stack = mock(Stack.class);
+        Cluster cluster = mock(Cluster.class);
+        when(stack.getName()).thenReturn("master-stack");
+        when(stack.getStatus()).thenReturn(status);
+        when(stack.getCluster()).thenReturn(cluster);
+        when(cluster.getName()).thenReturn("master-stack");
+        if (status == AVAILABLE) {
+            when(stack.isAvailable()).thenReturn(true);
+            when(stack.hasNodeFailure()).thenReturn(false);
+        } else if (status == NODE_FAILURE) {
+            when(stack.isAvailable()).thenReturn(false);
+            when(stack.hasNodeFailure()).thenReturn(true);
+        } else {
+            when(stack.isAvailable()).thenReturn(false);
+            when(stack.hasNodeFailure()).thenReturn(false);
+        }
+
+        if (errorMessageSegment.isPresent()) {
+            checkExecutableThrowsException(errorMessageSegment, stack, () -> underTest.validateStackStatusForStartHostGroup(stack));
+            checkExecutableThrowsException(errorMessageSegment, stack, () -> underTest.validateClusterStatusForStartHostGroup(stack));
+        } else {
+            assertDoesNotThrow(() -> underTest.validateStackStatusForStartHostGroup(stack));
+            assertDoesNotThrow(() -> underTest.validateClusterStatusForStartHostGroup(stack));
+        }
+    }
+
+    private void checkExecutableThrowsException(Optional<String> errorMessageSegment, Stack stack, Executable executable) {
+        BadRequestException badRequestException = assertThrows(BadRequestException.class, executable);
+        Assert.assertEquals(errorMessageSegment.get(), badRequestException.getMessage());
+    }
+
+    private static Stream<Arguments> testValidateStatusForStartHostGroupData() {
+        return Stream.of(
+                Arguments.of(AVAILABLE, NO_ERROR),
+                Arguments.of(NODE_FAILURE, NO_ERROR),
+                Arguments.of(DELETE_IN_PROGRESS,
+                        Optional.of("Data Hub 'master-stack' has 'DELETE_IN_PROGRESS' state." +
+                                " Node group start operation is not allowed for this state."))
+        );
+    }
 }
