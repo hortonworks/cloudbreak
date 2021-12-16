c2871cf5a570f708b54e12773143d28eb59f1d42
Oliver Szabo <oleewere@gmail.com>
2021-12-16 19:18:19 +0100

    CB-15515. Remove binaryUsageEvent field from CB log output (to decrease usage log size by ~1/3)

--- usage-collection/src/main/java/com/sequenceiq/cloudbreak/usage/strategy/LoggingUsageProcessingStrategy.java
+++ usage-collection/src/main/java/com/sequenceiq/cloudbreak/usage/strategy/LoggingUsageProcessingStrategy.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import com.cloudera.thunderhead.service.common.usage.UsageProto;
 import com.google.common.io.BaseEncoding;
-import com.sequenceiq.cloudbreak.logger.MDCBuilder;
 
 /**
  * A usage reporter strategy class that logs usage events.
@@ -18,14 +17,10 @@ public class LoggingUsageProcessingStrategy implements UsageProcessingStrategy {
 
     private static final Logger BINARY_EVENT_LOGGER = LoggerFactory.getLogger("CDP_BINARY_USAGE_EVENT");
 
-    private static final String USAGE_EVENT_MDC_NAME = "binaryUsageEvent";
-
     @Override
     public void processUsage(UsageProto.Event event) {
         LOGGER.info("Logging binary format for the following usage event: {}", event);
         String binaryUsageEvent = BaseEncoding.base64().encode(event.toByteArray());
-        MDCBuilder.addMdcField(USAGE_EVENT_MDC_NAME, binaryUsageEvent);
         BINARY_EVENT_LOGGER.info(binaryUsageEvent);
-        MDCBuilder.removeMdcField(USAGE_EVENT_MDC_NAME);
     }
 }
