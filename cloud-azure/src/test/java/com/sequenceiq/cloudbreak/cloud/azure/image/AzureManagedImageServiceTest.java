b85cebe481abba2e1dbae513b1bfa2dc45437463
Peter Darvasi <pdarvasi@hortonworks.com>
2021-12-30 12:38:26 +0100

    CB-15552 - Environment Provision Failed - resource cannot be created from Image
    The symptom:
     `The resource 'freeipa51002m0' cannot be created from Image '/subscriptions/[sub-id]/resourceGroups/cloudbreak-images/providers/Microsoft.Compute/images/freeipa-cdh-.vhd-westus2' until Image has been successfully created`
    
    The issue:
     - Even though we poll for the image not being null before submitting the ARM template, it seems there might be an edge case where it is still not successfully provisioned.
     - ARM template cannot refer an entity in `dependsOn` section of the VM-s created out of the ARM template's scope
    
    This commit:
     - adds extra validation for managed image provisioning status

--- cloud-azure/src/test/java/com/sequenceiq/cloudbreak/cloud/azure/image/AzureManagedImageServiceTest.java
+++ cloud-azure/src/test/java/com/sequenceiq/cloudbreak/cloud/azure/image/AzureManagedImageServiceTest.java
@@ -2,22 +2,25 @@ package com.sequenceiq.cloudbreak.cloud.azure.image;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.microsoft.azure.management.compute.VirtualMachineCustomImage;
+import com.microsoft.azure.management.compute.implementation.ImageInner;
 import com.sequenceiq.cloudbreak.cloud.azure.client.AzureClient;
 import com.sequenceiq.cloudbreak.cloud.azure.util.AzureAuthExceptionHandler;
+import com.sequenceiq.cloudbreak.cloud.exception.CloudConnectorException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AzureManagedImageServiceTest {
@@ -38,15 +41,49 @@ public class AzureManagedImageServiceTest {
     private final AzureAuthExceptionHandler azureAuthExceptionHandler = new AzureAuthExceptionHandler();
 
     @Test
-    public void testGetVirtualMachineCustomImageShouldReturnTheImageWhenExistsOnProviderSide() {
+    public void testGetVirtualMachineCustomImageShouldReturnTheImageWhenSucceededOnProviderSide() {
         AzureImageInfo azureImageInfo = new AzureImageInfo(IMAGE_NAME_WITH_REGION, IMAGE_NAME, "imageId", "region", RESOURCE_GROUP);
-        VirtualMachineCustomImage image = Mockito.mock(VirtualMachineCustomImage.class);
+        VirtualMachineCustomImage image = mock(VirtualMachineCustomImage.class);
         when(azureClient.findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION)).thenReturn(image);
+        ImageInner imageInner = mock(ImageInner.class);
+        when(image.inner()).thenReturn(imageInner);
+        when(imageInner.provisioningState()).thenReturn("Succeeded");
 
         Optional<VirtualMachineCustomImage> actual = underTest.findVirtualMachineCustomImage(azureImageInfo, azureClient);
 
         assertTrue(actual.isPresent());
         assertEquals(image, actual.get());
+        verify(azureClient).findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION);
+    }
+
+    @Test
+    public void testGetVirtualMachineCustomImageShouldReturnTheImageWhenUnknownOnProviderSide() {
+        AzureImageInfo azureImageInfo = new AzureImageInfo(IMAGE_NAME_WITH_REGION, IMAGE_NAME, "imageId", "region", RESOURCE_GROUP);
+        VirtualMachineCustomImage image = mock(VirtualMachineCustomImage.class);
+        when(azureClient.findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION)).thenReturn(image);
+        ImageInner imageInner = mock(ImageInner.class);
+        when(image.inner()).thenReturn(imageInner);
+        when(imageInner.provisioningState()).thenReturn("Unknown");
+
+        Optional<VirtualMachineCustomImage> actual = underTest.findVirtualMachineCustomImage(azureImageInfo, azureClient);
+
+        assertTrue(actual.isEmpty());
+        verify(azureClient).findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION);
+    }
+
+    @Test
+    public void testGetVirtualMachineCustomImageShouldReturnTheImageWhenFailedOnProviderSide() {
+        AzureImageInfo azureImageInfo = new AzureImageInfo(IMAGE_NAME_WITH_REGION, IMAGE_NAME, "imageId", "region", RESOURCE_GROUP);
+        VirtualMachineCustomImage image = mock(VirtualMachineCustomImage.class);
+        when(azureClient.findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION)).thenReturn(image);
+        ImageInner imageInner = mock(ImageInner.class);
+        when(image.inner()).thenReturn(imageInner);
+        when(imageInner.provisioningState()).thenReturn("Failed");
+
+        CloudConnectorException actual = Assertions.assertThrows(CloudConnectorException.class, () ->
+                underTest.findVirtualMachineCustomImage(azureImageInfo, azureClient));
+
+        assertEquals("Image (resource-group/image-name-with-region) creation failed in Azure, please check the reason on Azure Portal!", actual.getMessage());
         verify(azureClient).findImage(RESOURCE_GROUP, IMAGE_NAME_WITH_REGION);
     }
 
