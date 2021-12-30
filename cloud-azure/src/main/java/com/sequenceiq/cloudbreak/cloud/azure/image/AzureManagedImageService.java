097a5ce21bc9c6f2024515c96a050e65afa7ff98
Peter Darvasi <pdarvasi@hortonworks.com>
2021-12-30 12:38:26 +0100

    CB-15552 - Environment Provision Failed - resource cannot be created from Image
    The symptom:
     `The resource 'freeipa51002m0' cannot be created from Image '/subscriptions/[sub-id]/resourceGroups/cloudbreak-images/providers/Microsoft.Compute/images/freeipa-cdh-.vhd-westus2' until Image has been successfully created`
    
    The issue:
     - Even though we poll for the image not being null before submitting the ARM template, it seems there might be an edge case where it is still not successfully provisioned.
     - ARM template cannot refer an entity in `dependsOn` section of the VM-s created out of the ARM template's scope
    
    This commit:
     - adds extra validation for managed image provisioning status

--- cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/image/AzureManagedImageService.java
+++ cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/image/AzureManagedImageService.java
@@ -1,13 +1,18 @@
 package com.sequenceiq.cloudbreak.cloud.azure.image;
 
+import static com.microsoft.azure.management.compute.ExecutionState.FAILED;
+import static com.microsoft.azure.management.compute.ExecutionState.SUCCEEDED;
+
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.microsoft.azure.management.compute.ExecutionState;
 import com.microsoft.azure.management.compute.VirtualMachineCustomImage;
 import com.sequenceiq.cloudbreak.cloud.azure.client.AzureClient;
+import com.sequenceiq.cloudbreak.cloud.exception.CloudConnectorException;
 
 @Service
 public class AzureManagedImageService {
@@ -18,11 +23,24 @@ public class AzureManagedImageService {
         String imageName = azureImageInfo.getImageNameWithRegion();
         String resourceGroup = azureImageInfo.getResourceGroup();
         VirtualMachineCustomImage image = client.findImage(resourceGroup, imageName);
-        if (image != null) {
-            LOGGER.debug("Custom image {} is present in resource group {}", imageName, resourceGroup);
-            return Optional.of(image);
+        if (image != null && image.inner() != null) {
+            return getImageByState(imageName, resourceGroup, image);
         } else {
             LOGGER.debug("Custom image {} is not present in resource group {}", imageName, resourceGroup);
+            return Optional.empty();
+        }
+    }
+
+    private Optional<VirtualMachineCustomImage> getImageByState(String imageName, String resourceGroup, VirtualMachineCustomImage image) {
+        ExecutionState creationState = ExecutionState.fromString(image.inner().provisioningState());
+        if (creationState == SUCCEEDED) {
+            LOGGER.debug("Custom image {} is present in resource group {}", imageName, resourceGroup);
+            return Optional.of(image);
+        } else if (creationState == FAILED) {
+            throw new CloudConnectorException(
+                    String.format("Image (%s/%s) creation failed in Azure, please check the reason on Azure Portal!", resourceGroup, imageName));
+        } else {
+            LOGGER.debug("Custom image {} in resource group {} has non final status {}", imageName, resourceGroup, creationState.toString());
             return Optional.empty();
         }
     }
