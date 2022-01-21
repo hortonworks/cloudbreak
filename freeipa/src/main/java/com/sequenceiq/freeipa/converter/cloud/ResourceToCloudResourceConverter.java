b18011292d19c3c1302b7e929d80a8c2dc2be7b3
Tamas Bihari <tbihari@hortonworks.com>
2022-01-21 17:41:32 +0100

    CB-15765 fix the deletion of EIPs of FreeIPA on AWS_NATIVE variant

--- freeipa/src/main/java/com/sequenceiq/freeipa/converter/cloud/ResourceToCloudResourceConverter.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/converter/cloud/ResourceToCloudResourceConverter.java
@@ -6,26 +6,21 @@ import java.util.Optional;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.cloud.model.CloudResource;
 import com.sequenceiq.cloudbreak.cloud.model.CloudResource.Builder;
-import com.sequenceiq.cloudbreak.cloud.model.VolumeSetAttributes;
 import com.sequenceiq.freeipa.entity.Resource;
 import com.sequenceiq.freeipa.service.resource.ResourceAttributeUtil;
 
 @Component
 public class ResourceToCloudResourceConverter {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceToCloudResourceConverter.class);
-
     @Inject
     private ResourceAttributeUtil resourceAttributeUtil;
 
     public CloudResource convert(Resource resource) {
-        Optional<VolumeSetAttributes> attributes = resourceAttributeUtil.getTypedAttributes(resource, VolumeSetAttributes.class);
+        Optional<Object> attributes = resourceAttributeUtil.getTypedAttributes(resource);
 
         Map<String, Object> paramsMap = new HashMap<>();
         attributes.ifPresent(attr -> paramsMap.put(CloudResource.ATTRIBUTES, attr));
