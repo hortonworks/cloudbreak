07409b1e9d1efa9efa3a8faba03446b851f18f9d
ganeshk <ganeshk@cloudera.com>
2022-01-26 15:19:47 -0800

    CB-15341 Successful scale up final status should be based on the number of instances started
    1. We introduced a new state for stop/start scaling.
    2. This state is used when scale down successfully stops some nodes in the hostgroup.
    3. The same state needs to be used when not all the instances are started during the scale up by start action.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/stopstartus/StopStartUpscaleFlowService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/stopstartus/StopStartUpscaleFlowService.java
@@ -1,6 +1,5 @@
 package com.sequenceiq.cloudbreak.core.flow2.cluster.stopstartus;
 
-import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.AVAILABLE;
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_FAILED;
 import static com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status.UPDATE_IN_PROGRESS;
 import static com.sequenceiq.cloudbreak.event.ResourceEvent.CLUSTER_SCALING_STOPSTART_UPSCALE_COMMISSIONING;
@@ -104,13 +103,11 @@ class StopStartUpscaleFlowService {
                 String.valueOf(notCommissionedFqdns.size()), notCommissionedFqdns.stream().collect(Collectors.joining(", ")));
     }
 
-    void clusterUpscaleFinished(StackView stackView, String hostgroupName, List<InstanceMetaData> commissioned) {
-        // TODO CB-14929: CB-15341 follow up. The DetailedStackStatus here needs to be updated based on the number of instances.
-        //  That determines AVAILABLE vs AVAILABLE_WITH_STOPPED_INSTANCES
+    void clusterUpscaleFinished(StackView stackView, String hostgroupName, List<InstanceMetaData> commissioned, DetailedStackStatus finalStackStatus) {
         LOGGER.debug("StopStart upscale finished successfully. CommissionedCount={}", commissioned.size());
         commissioned.stream().forEach(x -> instanceMetaDataService.updateInstanceStatus(x, InstanceStatus.SERVICES_HEALTHY));
-        stackUpdater.updateStackStatus(stackView.getId(), DetailedStackStatus.AVAILABLE, String.format("finished starting nodes"));
-        flowMessageService.fireEventAndLog(stackView.getId(), AVAILABLE.name(), CLUSTER_SCALING_STOPSTART_UPSCALE_FINISHED,
+        stackUpdater.updateStackStatus(stackView.getId(), finalStackStatus, String.format("finished starting nodes"));
+        flowMessageService.fireEventAndLog(stackView.getId(), finalStackStatus.getStatus().name(), CLUSTER_SCALING_STOPSTART_UPSCALE_FINISHED,
                 hostgroupName, String.valueOf(commissioned.size()),
                 commissioned.stream().map(i -> i.getDiscoveryFQDN()).collect(Collectors.joining(", ")));
     }
