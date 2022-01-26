a7f518520c7bd517e5f5e2d276444801bfe3dbc5
Oliver Szabo <oleewere@gmail.com>
2022-01-26 15:52:35 +0100

    CB-15796. Move flow related migration scripts under flow module. details: - db migration can fail if flowchainlog does not exist at that time when this specific sql is running - order of the migration scripts (per module) is decided based on the order of the SchemaLocationProvider implementations - can differ for different projects + jdks -> can lead to migration failure update UT sql - update sql file: add an extra if exsits

--- flow/src/main/resources/schema/flow/20210217140634_CB-11220_introduce_flowChainType_into_the_flowChainLog_table.sql
+++ flow/src/main/resources/schema/flow/20210217140634_CB-11220_introduce_flowChainType_into_the_flowChainLog_table.sql
@@ -1,7 +1,7 @@
 -- // CB-11220 introduce flowChainType into the flowChainLog table
 -- Migration SQL that makes the change goes here.
 
-ALTER TABLE flowchainlog ADD COLUMN flowchaintype CHARACTER VARYING (255);
+ALTER TABLE flowchainlog ADD COLUMN IF NOT EXISTS flowchaintype CHARACTER VARYING (255);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
