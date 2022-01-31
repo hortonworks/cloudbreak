b3fc1e70749b883ae764aeab84261c64b40196be
lnardai <laci.nardai@gmail.com>
2022-01-31 17:24:03 +0100

    CB-15869 removing cdp-sdk-java dependency

--- environment/src/test/java/com/sequenceiq/environment/service/integration/CredentialExperienceTest.java
+++ environment/src/test/java/com/sequenceiq/environment/service/integration/CredentialExperienceTest.java
@@ -58,7 +58,6 @@ import com.sequenceiq.environment.credential.service.CredentialPrerequisiteServi
 import com.sequenceiq.environment.credential.service.CredentialService;
 import com.sequenceiq.environment.credential.service.ServiceProviderCredentialAdapter;
 import com.sequenceiq.environment.credential.v1.CredentialV1Controller;
-import com.sequenceiq.environment.credential.v1.converter.CredentialRequestToCreateAWSCredentialRequestConverter;
 import com.sequenceiq.environment.credential.v1.converter.CredentialToCredentialV1ResponseConverter;
 import com.sequenceiq.environment.credential.validation.CredentialValidator;
 import com.sequenceiq.environment.environment.flow.EnvEventParameterFactory;
@@ -238,7 +237,6 @@ public class CredentialExperienceTest {
             CredentialPrerequisitesHandler.class,
             CredentialExperiencePolicyHandler.class,
             CloudbreakMessagesService.class,
-            CredentialRequestToCreateAWSCredentialRequestConverter.class,
 
             CloudPlatformInitializer.class,
             ConversionConfig.class,
