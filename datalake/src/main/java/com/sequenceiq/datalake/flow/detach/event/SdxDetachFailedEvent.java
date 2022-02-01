59bdcfbf7cefceda311bab22e5fb6c187983ec8f
Richard Doktorics <rdoktorics@cloudera.com>
2022-02-01 10:57:30 +0100

    Revert "CB-15446: Added logic to SDX resize detach flow so that failures are recovered from."

--- datalake/src/main/java/com/sequenceiq/datalake/flow/detach/event/SdxDetachFailedEvent.java
+++ datalake/src/main/java/com/sequenceiq/datalake/flow/detach/event/SdxDetachFailedEvent.java
@@ -15,6 +15,6 @@ public class SdxDetachFailedEvent extends SdxFailedEvent {
 
     @Override
     public String selector() {
-        return "SdxDetachFailedEvent";
+        return "SdxStartDetachEvent";
     }
 }
