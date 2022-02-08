381d82a512e162695eed5e894e660e3e7eac8a46
kkalyan <kkalyan@cloudera.com>
2022-02-08 05:52:26 -0600

    CB-13414: Add e2e tests for re-sizing of the datalake.
    Original PR was https://github.com/hortonworks/cloudbreak/pull/12011.
    
    As datalake is detached, there will be very small interval before resized Dl is created. During this interval DL with the CRN wil not be found. That is exactly what was happening. I have included some failure handling the flowUtil code to address this situation.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/dto/sdx/SdxResizeTestDto.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/dto/sdx/SdxResizeTestDto.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.it.cloudbreak.dto.sdx;
 
+import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.it.cloudbreak.Prototype;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.AbstractSdxTestDto;
@@ -25,8 +26,13 @@ public class SdxResizeTestDto extends AbstractSdxTestDto<SdxClusterResizeRequest
 
     @Override
     public SdxResizeTestDto valid() {
-        return withEnvironmentName(getTestContext().get(EnvironmentTestDto.class).getResponse().getName())
-                .withClusterShape(getCloudProvider().getClusterShape());
+        if (CloudPlatform.MOCK.equals(getCloudPlatform())) {
+            return withEnvironmentName(getTestContext().get(EnvironmentTestDto.class).getResponse().getName())
+                    .withClusterShape(getCloudProvider().getClusterShape());
+        } else {
+            return withEnvironmentName(getTestContext().get(EnvironmentTestDto.class).getResponse().getName())
+                    .withClusterShape(SdxClusterShape.MEDIUM_DUTY_HA);
+        }
     }
 
     public SdxResizeTestDto withEnvironmentName(String environment) {
