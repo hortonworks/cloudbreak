e16c9bc1c9ff8b9660303b539e9df58f5100c0a4
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-02-21 11:55:36 +0100

    CB-15292 Kinit tests during freeipa upgrade
    - Extend the freeipa upgrade test with SDX cluster creation
    - Ssh into SDX master node and test if kinit is working during the upgrade
    - kinit should work, this would ensure KDC is available, and users are present

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/freeipa/FreeIpaUpgradeTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/freeipa/FreeIpaUpgradeTests.java
@@ -5,15 +5,16 @@ import static com.sequenceiq.freeipa.api.v1.operation.model.OperationState.RUNNI
 import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
 import static com.sequenceiq.it.cloudbreak.context.RunningParameter.waitForFlow;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
 
-import com.sequenceiq.freeipa.api.v1.kerberosmgmt.model.HostKeytabRequest;
 import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.InstanceGroupV4Response;
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
 import com.sequenceiq.cloudbreak.polling.AbsolutTimeBasedTimeoutChecker;
 import com.sequenceiq.freeipa.api.v1.dns.model.AddDnsARecordRequest;
@@ -22,17 +23,26 @@ import com.sequenceiq.freeipa.api.v1.dns.model.AddDnsZoneForSubnetsRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.cleanup.CleanupRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.binduser.BindUserCreateRequest;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.Status;
+import com.sequenceiq.freeipa.api.v1.kerberosmgmt.model.HostKeytabRequest;
 import com.sequenceiq.freeipa.api.v1.operation.model.OperationState;
 import com.sequenceiq.freeipa.api.v1.operation.model.OperationStatus;
 import com.sequenceiq.it.cloudbreak.FreeIpaClient;
+import com.sequenceiq.it.cloudbreak.SdxClient;
 import com.sequenceiq.it.cloudbreak.client.FreeIpaTestClient;
+import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
+import com.sequenceiq.it.cloudbreak.cloud.HostGroupType;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaOperationStatusTestDto;
 import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaTestDto;
+import com.sequenceiq.it.cloudbreak.dto.sdx.SdxTestDto;
 import com.sequenceiq.it.cloudbreak.dto.telemetry.TelemetryTestDto;
 import com.sequenceiq.it.cloudbreak.exception.TestFailException;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.AbstractE2ETest;
+import com.sequenceiq.it.cloudbreak.util.ssh.action.SshJClientActions;
+import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
+import com.sequenceiq.sdx.api.model.SdxDatabaseAvailabilityType;
+import com.sequenceiq.sdx.api.model.SdxDatabaseRequest;
 
 public class FreeIpaUpgradeTests extends AbstractE2ETest {
 
@@ -45,7 +55,13 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
     private static final long FIVE_MINUTES_IN_SEC = 5L * 60;
 
     @Inject
+    private SdxTestClient sdxTestClient;
+
+    @Inject
     private FreeIpaTestClient freeIpaTestClient;
+
+    @Inject
+    private SshJClientActions sshJClientActions;
 
     @Test(dataProvider = TEST_CONTEXT)
     @Description(
@@ -55,6 +71,9 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
             then = "the stack should be available AND deletable")
     public void testSingleFreeIpaInstanceUpgrade(TestContext testContext) {
         String freeIpa = resourcePropertyProvider().getName();
+        SdxDatabaseRequest sdxDatabaseRequest = new SdxDatabaseRequest();
+        sdxDatabaseRequest.setAvailabilityType(SdxDatabaseAvailabilityType.NONE);
+        sdxDatabaseRequest.setCreate(false);
 
         testContext
                 .given("telemetry", TelemetryTestDto.class)
@@ -65,6 +84,12 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
                 .withUpgradeCatalogAndImage()
                 .when(freeIpaTestClient.create(), key(freeIpa))
                 .await(FREEIPA_AVAILABLE)
+                .given(SdxTestDto.class)
+                .withCloudStorage()
+                .withExternalDatabase(sdxDatabaseRequest)
+                .when(sdxTestClient.create())
+                .await(SdxClusterStatusResponse.RUNNING)
+                .given(freeIpa, FreeIpaTestDto.class)
                 .when(freeIpaTestClient.upgrade())
                 .await(Status.UPDATE_IN_PROGRESS, waitForFlow().withWaitForFlow(Boolean.FALSE))
                 .given(FreeIpaOperationStatusTestDto.class)
@@ -86,6 +111,9 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
             then = "the stack should be available AND deletable")
     public void testHAFreeIpaInstanceUpgrade(TestContext testContext) {
         String freeIpa = resourcePropertyProvider().getName();
+        SdxDatabaseRequest sdxDatabaseRequest = new SdxDatabaseRequest();
+        sdxDatabaseRequest.setAvailabilityType(SdxDatabaseAvailabilityType.NONE);
+        sdxDatabaseRequest.setCreate(false);
 
         testContext
                 .given("telemetry", TelemetryTestDto.class)
@@ -97,6 +125,12 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
                 .withUpgradeCatalogAndImage()
                 .when(freeIpaTestClient.create(), key(freeIpa))
                 .await(FREEIPA_AVAILABLE)
+                .given(SdxTestDto.class)
+                .withCloudStorage()
+                .withExternalDatabase(sdxDatabaseRequest)
+                .when(sdxTestClient.create())
+                .await(SdxClusterStatusResponse.RUNNING)
+                .given(freeIpa, FreeIpaTestDto.class)
                 .when(freeIpaTestClient.upgrade())
                 .await(Status.UPDATE_IN_PROGRESS, waitForFlow().withWaitForFlow(Boolean.FALSE))
                 .given(FreeIpaOperationStatusTestDto.class)
@@ -124,6 +158,7 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
                 createBindUser(testContext, ipaClient, environmentCrn, accountId);
                 generateHostKeyTab(ipaClient, environmentCrn);
                 cleanUp(testContext, ipaClient, environmentCrn, accountId);
+                kinit(testContext.given(SdxTestDto.class), testContext.getSdxClient());
             }
         } catch (TestFailException e) {
             throw e;
@@ -233,5 +268,24 @@ public class FreeIpaUpgradeTests extends AbstractE2ETest {
     private boolean isSuccess(OperationStatus operationStatus) {
         OperationState operationState = operationStatus.getStatus();
         return COMPLETED == operationState;
+    }
+
+    private void kinit(SdxTestDto sdxTestDto, SdxClient sdxClient) {
+        try {
+            sshJClientActions.checkKinitDuringFreeipaUpgrade(sdxTestDto, getInstanceGroups(sdxTestDto, sdxClient),
+                    List.of(HostGroupType.MASTER.getName()));
+        } catch (TestFailException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error("FreeIPA upgrade kinit test failed with unexpected error", e);
+            throw new TestFailException("FreeIPA upgrade kinit test failed with unexpected error: " + e.getMessage(), e);
+        }
+    }
+
+    private List<InstanceGroupV4Response> getInstanceGroups(SdxTestDto testDto, SdxClient client) {
+        return client.getDefaultClient()
+                .sdxEndpoint()
+                .getDetailByCrn(testDto.getCrn(), Collections.emptySet())
+                .getStackV4Response().getInstanceGroups();
     }
 }
