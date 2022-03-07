0feef50ddefa0aabf2a3cbbcbf6d4f0b9633d7db
Oliver Szabo <oleewere@gmail.com>
2022-03-07 14:41:28 +0100

    CB-15999. Addendum - use additional fields for http usage.

--- usage-collection/src/test/java/com/sequenceiq/cloudbreak/usage/strategy/HttpUsageProcessingStrategyTest.java
+++ usage-collection/src/test/java/com/sequenceiq/cloudbreak/usage/strategy/HttpUsageProcessingStrategyTest.java
@@ -1,9 +1,12 @@
 package com.sequenceiq.cloudbreak.usage.strategy;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.cloudera.thunderhead.service.common.usage.UsageProto;
+import com.sequenceiq.cloudbreak.usage.http.EdhHttpAdditionalField;
+import com.sequenceiq.cloudbreak.usage.http.EdhHttpConfiguration;
 import com.sequenceiq.cloudbreak.usage.http.UsageHttpRecordProcessor;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,14 +29,22 @@ public class HttpUsageProcessingStrategyTest {
     @Mock
     private UsageHttpRecordProcessor processor;
 
+    @Mock
+    private EdhHttpConfiguration edhHttpConfiguration;
+
     @BeforeEach
     public void setUp() {
-        underTest = new HttpUsageProcessingStrategy(processor);
+        underTest = new HttpUsageProcessingStrategy(processor, edhHttpConfiguration);
     }
 
     @Test
     public void testProcessUsage() {
         // GIVEN
+        EdhHttpAdditionalField field = new EdhHttpAdditionalField();
+        field.setKey("key");
+        field.setValue("val");
+        List<EdhHttpAdditionalField> fields = List.of(field);
+        given(edhHttpConfiguration.getAdditionalFields()).willReturn(fields);
         doNothing().when(processor).processRecord(any());
         UsageProto.Event event = UsageProto.Event.newBuilder()
                 .setCdpEnvironmentRequested(UsageProto.CDPEnvironmentRequested
@@ -41,6 +54,7 @@ public class HttpUsageProcessingStrategyTest {
         // WHEN
         underTest.processUsage(event, null);
         // THEN
+        verify(edhHttpConfiguration, times(1)).getAdditionalFields();
         verify(processor, times(1)).processRecord(any());
     }
 }
