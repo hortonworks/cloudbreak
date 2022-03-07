aaca7e7b8a41e0e6d3eb95241e0d130ce8a905f7
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-03-07 16:37:53 +0100

    CB-13347 Increase sizelimit and timelimit for retrieving users and groups from FreeIPA
    User sync retrieves all the users and groups from IPA for comparison against UMS. We need to increase the nsslapd-sizelimit configuration in preparation for supporting more than 2000 users or groups. If we don't make this change, then user sync will think that it needs to add users and groups that already exist in IPA.
    
    If we don't increases this, then the IPA user-find and group-find will truncate the results to 2000 entries. This sizelimit is always enforced even if we set the sizelimit=0 in the request.
    
    The new limit is 11000 to ensure the customer 10000 users+other stuff can fit into the limit.
    GZIP compression support is turned on for responses, as without it for 10600 user we have seen 11.7MB responses. With GZIP support it's reduced to 0.5MB
    
    Every LDAP modify like stuff is moved into a common `initial-ldap-conf.ldif` instead of using various scripts.
    
    Tested with 10000+ users:
    - DL+DH create/termination with and without cluster proxy
    - with single and multi node FreeIPA
    - `ipa user-find --sizelimit 0 --timelimit 0` is finished under 30 sec, closer to 20 sec

--- freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_healthagent_setup.sh
+++ freeipa/src/main/resources/freeipa-salt/salt/freeipa/scripts/freeipa_healthagent_setup.sh
@@ -5,28 +5,6 @@
 set -x
 set -e
 
-ESCAPED_DOMAIN=$(hostname -d| sed -E 's/^|\./\\,dc\\3D/g' | sed 's/^\\,//g')
-REPLICA_BASE="cn=${ESCAPED_DOMAIN},cn=mapping tree,cn=config"
-
-#
-# Add anonymous access for replication agreements
-#
-set +ex
-echo ldapmodify -x -D "cn=directory manager" -w "****" -h localhost
-ldapmodify -x -D "cn=directory manager" -w "$FPW" -h localhost << EOF
-dn: ${REPLICA_BASE}
-changetype: modify
-add: aci
-aci: (targetattr="cn||objectClass||nsDS5ReplicaHost||nsds5replicaLastUpdateEnd||nsds5replicaLastUpdateStatus")(targetfilter="(|(objectclass=nsds5replicationagreement)(objectclass=nsDSWindowsReplicationAgreement))")(version 3.0; aci "permission:Read Replication Agreements"; allow (read, search, compare) groupdn = "ldap:///anyone";)
-EOF
-LDAPMODIFY_RET=$?
-set -ex
-LDAP_TYPE_OR_VALUE_EXISTS=20
-if [[ $LDAPMODIFY_RET -ne 0 && $LDAPMODIFY_RET -ne $LDAP_TYPE_OR_VALUE_EXISTS ]]; then
-  echo ldapmodify failed
-  false
-fi
-
 #
 # Setup cert copy and service refstart on cert update
 #
