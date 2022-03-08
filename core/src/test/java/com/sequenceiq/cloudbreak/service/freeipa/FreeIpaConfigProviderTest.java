b35ceb817f0b000a6c875fdeecbb0f7b15b29014
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-03-08 14:37:34 +0100

    CB-15455 ipa client should prefer the primary gateway
    - modified FreeIPA instance selection logic to prefer primary gateway
    - `ipa-client-install` command now is using `--server` option to connect to the pgw if possible
    
    Tested by creating a cluster and checking the rendered script on the host.

--- core/src/test/java/com/sequenceiq/cloudbreak/service/freeipa/FreeIpaConfigProviderTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/freeipa/FreeIpaConfigProviderTest.java
@@ -20,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceGroupResponse;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceMetaDataResponse;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceMetadataType;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceStatus;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.describe.DescribeFreeIpaResponse;
 
@@ -94,6 +95,40 @@ class FreeIpaConfigProviderTest {
         Map<String, Object> result = underTest.createFreeIpaConfig(ENVIRONMENT_CRN);
 
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testPgwChosen() {
+        DescribeFreeIpaResponse freeIpaResponse = new DescribeFreeIpaResponse();
+        InstanceGroupResponse instanceGroupResponse = new InstanceGroupResponse();
+        InstanceMetaDataResponse pgw = createInstanceMetadata("d", CREATED);
+        pgw.setInstanceType(InstanceMetadataType.GATEWAY_PRIMARY);
+        instanceGroupResponse.setMetaData(Set.of(createInstanceMetadata("b", TERMINATED), createInstanceMetadata("a", REQUESTED),
+                createInstanceMetadata("f", CREATED), pgw, createInstanceMetadata("c", CREATED)));
+        freeIpaResponse.setInstanceGroups(List.of(instanceGroupResponse));
+        when(freeipaClient.findByEnvironmentCrn(ENVIRONMENT_CRN)).thenReturn(Optional.of(freeIpaResponse));
+
+        Map<String, Object> result = underTest.createFreeIpaConfig(ENVIRONMENT_CRN);
+
+        assertEquals(1, result.size());
+        assertEquals("d", result.get("host"));
+    }
+
+    @Test
+    public void testPgwNotChosenIfNotCreated() {
+        DescribeFreeIpaResponse freeIpaResponse = new DescribeFreeIpaResponse();
+        InstanceGroupResponse instanceGroupResponse = new InstanceGroupResponse();
+        InstanceMetaDataResponse pgw = createInstanceMetadata("d", TERMINATED);
+        pgw.setInstanceType(InstanceMetadataType.GATEWAY_PRIMARY);
+        instanceGroupResponse.setMetaData(Set.of(createInstanceMetadata("b", TERMINATED), createInstanceMetadata("a", REQUESTED),
+                createInstanceMetadata("f", CREATED), pgw, createInstanceMetadata("c", CREATED)));
+        freeIpaResponse.setInstanceGroups(List.of(instanceGroupResponse));
+        when(freeipaClient.findByEnvironmentCrn(ENVIRONMENT_CRN)).thenReturn(Optional.of(freeIpaResponse));
+
+        Map<String, Object> result = underTest.createFreeIpaConfig(ENVIRONMENT_CRN);
+
+        assertEquals(1, result.size());
+        assertEquals("c", result.get("host"));
     }
 
     private InstanceMetaDataResponse createInstanceMetadata(String fqdn, InstanceStatus status) {
