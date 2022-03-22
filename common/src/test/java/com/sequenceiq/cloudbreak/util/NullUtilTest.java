404c3174c56a3f630c482225ece05e6306ac6d7a
gmeszaros <gmeszaros@cloudera.com>
2022-03-22 08:40:33 +0100

    CB-16532 - Custom templates are broken in Cloudbreak mow-dev and mow-int

--- common/src/test/java/com/sequenceiq/cloudbreak/util/NullUtilTest.java
+++ common/src/test/java/com/sequenceiq/cloudbreak/util/NullUtilTest.java
@@ -1,6 +1,8 @@
 package com.sequenceiq.cloudbreak.util;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,26 @@ class NullUtilTest {
     @Test
     void throwIfNullTestWhenNonNull() {
         NullUtil.throwIfNull(12, UnsupportedOperationException::new);
+    }
+
+    @Test
+    void testAllNullWhenInputArrayIsNullThenTrueShouldReturn() {
+        assertTrue(NullUtil.allNull(null));
+    }
+
+    @Test
+    void testAllNullWhenOneInputIsNotNullThenFalseShouldReturn() {
+        assertFalse(NullUtil.allNull("someStuff", null));
+    }
+
+    @Test
+    void testAllNullWhenAllTheInputsAreNotNullThenFalseShouldReturn() {
+        assertFalse(NullUtil.allNull("someStuff", "someOtherStuff"));
+    }
+
+    @Test
+    void testAllNullWhenAllTheMultipleInputsAreNullThenTrueShouldReturn() {
+        assertTrue(NullUtil.allNull(null, null));
     }
 
 }
