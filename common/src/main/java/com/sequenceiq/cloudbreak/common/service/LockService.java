3d7d48d93af6f47a1f166300248379e29b4a87ad
Peter Erdos <perdos@cloudera.com>
2022-03-23 16:43:29 +0100

    CB-16378 quartz init should run by one pod at a time

---
+++ common/src/main/java/com/sequenceiq/cloudbreak/common/service/LockService.java
@@ -0,0 +1,53 @@
+package com.sequenceiq.cloudbreak.common.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LockService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LockService.class);
+
+    @Inject
+    private JdbcTemplate jdbcTemplate;
+
+    public void lockAndRunIfLockWasSuccessful(Runnable runnable, LockNumber lockNumber) {
+        LOGGER.info("Try to get PostgreSQL advisory lock with lock number: {}", lockNumber);
+        boolean lockSuccess = false;
+        try {
+            lockSuccess = lock(lockNumber, jdbcTemplate);
+            if (lockSuccess) {
+                LOGGER.info("PostgreSQL advisory lock was successful with lock number: {}", lockNumber);
+                runnable.run();
+            } else {
+                LOGGER.warn("PostgreSQL advisory lock was unsuccessful with lock number: {}", lockNumber);
+            }
+        } finally {
+            if (lockSuccess) {
+                unlock(lockNumber, jdbcTemplate);
+            }
+        }
+    }
+
+    private void unlock(LockNumber lockNumber, JdbcTemplate jdbcTemplate) {
+        Boolean unlocked = jdbcTemplate.queryForObject("SELECT pg_advisory_unlock(" + lockNumber.getLockNumber() + ")", Boolean.class);
+        if (Boolean.FALSE.equals(unlocked)) {
+            LOGGER.error("Unable to release PostgreSQL advisory lock with lock number: {}", lockNumber);
+        } else {
+            LOGGER.info("PostgreSQL advisory lock was released with lock number: {}", lockNumber);
+        }
+    }
+
+    private boolean lock(LockNumber lockNumber, JdbcTemplate jdbcTemplate) {
+        List<Boolean> results = jdbcTemplate.query("SELECT pg_try_advisory_lock(" + lockNumber.getLockNumber() + ")",
+                (rs, rowNum) -> rs.getBoolean("pg_try_advisory_lock"));
+        return results.size() == 1 && results.get(0);
+    }
+
+}
---
+++ common/src/test/java/com/sequenceiq/cloudbreak/common/service/LockServiceTest.java
@@ -0,0 +1,72 @@
+package com.sequenceiq.cloudbreak.common.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@ExtendWith(MockitoExtension.class)
+class LockServiceTest {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private LockService lockService;
+
+    @Test
+    public void lockAndRunIfLockWasSuccessfulTest() {
+        AtomicBoolean lockHappened = new AtomicBoolean(false);
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(List.of(true));
+        when(jdbcTemplate.queryForObject(anyString(), eq(Boolean.class))).thenReturn(true);
+        lockService.lockAndRunIfLockWasSuccessful(() -> lockHappened.set(true), LockNumber.QUARTZ);
+        verify(jdbcTemplate, times(1)).queryForObject(eq("SELECT pg_advisory_unlock(647)"), eq(Boolean.class));
+        verify(jdbcTemplate, times(1)).query(eq("SELECT pg_try_advisory_lock(647)"), any(RowMapper.class));
+        assertTrue(lockHappened.get());
+    }
+
+    @Test
+    public void lockAndRunIfLockWasSuccessfulButReleaseWasUnsuccessfulTest() {
+        AtomicBoolean lockHappened = new AtomicBoolean(false);
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(List.of(true));
+        when(jdbcTemplate.queryForObject(anyString(), eq(Boolean.class))).thenReturn(false);
+        lockService.lockAndRunIfLockWasSuccessful(() -> lockHappened.set(true), LockNumber.QUARTZ);
+        assertTrue(lockHappened.get());
+    }
+
+    @Test
+    public void lockAndRunIfLockWasUnSuccessfulTest() {
+        AtomicBoolean lockHappened = new AtomicBoolean(false);
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(List.of(false));
+        lockService.lockAndRunIfLockWasSuccessful(() -> lockHappened.set(true), LockNumber.QUARTZ);
+        verify(jdbcTemplate, times(0)).queryForObject(anyString(), eq(Boolean.class));
+        assertFalse(lockHappened.get());
+    }
+
+    @Test
+    public void lockAndRunIfLockWasSuccessfulButRunnableThrowExecptionTest() {
+        when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(List.of(true));
+        when(jdbcTemplate.queryForObject(anyString(), eq(Boolean.class))).thenReturn(true);
+        Assertions.assertThrows(RuntimeException.class, () -> lockService.lockAndRunIfLockWasSuccessful(() -> {
+            throw new RuntimeException("runnable failed");
+        }, LockNumber.QUARTZ));
+        verify(jdbcTemplate, times(1)).queryForObject(eq("SELECT pg_advisory_unlock(647)"), eq(Boolean.class));
+        verify(jdbcTemplate, times(1)).query(eq("SELECT pg_try_advisory_lock(647)"), any(RowMapper.class));
+    }
+}
