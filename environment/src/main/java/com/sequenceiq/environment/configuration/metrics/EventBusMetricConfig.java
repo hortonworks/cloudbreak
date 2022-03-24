36e4ab241b4c88e244e59d91992fcd12cedf68bb
Laszlo Turcsanyi <lturcsanyi@cloudera.com>
2022-03-24 16:23:22 +0100

    CB-16142 Introduce live metrics for thread pools of our services - quartz executor - scheduler executor - reactor executor (event bus)

---
+++ environment/src/main/java/com/sequenceiq/environment/configuration/metrics/EventBusMetricConfig.java
@@ -0,0 +1,30 @@
+package com.sequenceiq.environment.configuration.metrics;
+
+import java.util.concurrent.ExecutorService;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sequenceiq.cloudbreak.logger.concurrent.MDCCleanerThreadPoolExecutor;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
+
+@Configuration
+public class EventBusMetricConfig {
+
+    @Inject
+    private MeterRegistry meterRegistry;
+
+    @Inject
+    @Named("eventBusThreadPoolExecutor")
+    private MDCCleanerThreadPoolExecutor executor;
+
+    @Bean
+    public ExecutorService eventBusThreadPoolExecutorService() {
+        return ExecutorServiceMetrics.monitor(meterRegistry, executor, "eventBusThreadPoolExecutor", "threadpool");
+    }
+}
