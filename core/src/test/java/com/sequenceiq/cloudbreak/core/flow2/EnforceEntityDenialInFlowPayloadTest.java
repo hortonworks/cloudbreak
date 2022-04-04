794d2424191487a3d42bdc4debea7f7b0a754f07
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2022-04-04 17:29:13 +0200

    CB-16708 Create enforce unit test to deny entities in payloads and events

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/core/flow2/EnforceEntityDenialInFlowPayloadTest.java
@@ -0,0 +1,14 @@
+package com.sequenceiq.cloudbreak.core.flow2;
+
+import org.junit.jupiter.api.Test;
+
+public class EnforceEntityDenialInFlowPayloadTest {
+
+    @Test
+    public void enforceDenialOfEntitiesInFlowPayload() {
+        // TODO CB-16709
+        // TODO CB-16714
+        // TODO CB-16727
+        //EnforceEntityDenialUtil.denyEntity();
+    }
+}
