51ad2bab2238d8ca1f088e84ae57b94ae4b912fd
Annamaria Szegedi <aszegedi@cloudera.com>
2022-04-06 10:49:24 +0200

    CB-16780 Set back DistroX Stop Start then Scale test

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/DistroXClusterStopStartTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/DistroXClusterStopStartTest.java
@@ -1,57 +1,37 @@
 package com.sequenceiq.it.cloudbreak.testcase.mock;
 
-import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+import java.util.stream.IntStream;
 
 import javax.inject.Inject;
 
 import org.testng.ITestContext;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import com.sequenceiq.it.cloudbreak.assertion.audit.DatahubAuditGrpcServiceAssertion;
-import com.sequenceiq.it.cloudbreak.client.BlueprintTestClient;
 import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
-import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.MockedTestContext;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
-import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.DistroXClusterTestDto;
-import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.clouderamanager.DistroXClouderaManagerTestDto;
-import com.sequenceiq.it.cloudbreak.dto.distrox.image.DistroXImageTestDto;
-import com.sequenceiq.it.cloudbreak.dto.distrox.instancegroup.DistroXNetworkTestDto;
-import com.sequenceiq.it.cloudbreak.testcase.mock.clouderamanager.AbstractClouderaManagerTest;
+import com.sequenceiq.it.cloudbreak.exception.TestFailException;
 
-public class DistroXClusterStopStartTest extends AbstractClouderaManagerTest {
-
-    private static final String IMAGE_CATALOG_ID = "f6e778fc-7f17-4535-9021-515351df3691";
-
-    private static final String CM_FOR_DISTRO_X = "cm4dstrx";
-
-    private static final String CLUSTER_KEY = "cmdistrox";
-
-    private static final String DIX_IMG_KEY = "dixImg";
-
-    private static final String DIX_NET_KEY = "dixNet";
-
-    private static final Duration POLLING_INTERVAL = Duration.of(3000, ChronoUnit.MILLIS);
+public class DistroXClusterStopStartTest extends AbstractMockTest {
 
     @Inject
-    private BlueprintTestClient blueprintTestClient;
-
-    @Inject
-    private DistroXTestClient distroXClient;
-
-    @Inject
-    private SdxTestClient sdxTestClient;
+    private DistroXTestClient distroXTestClient;
 
     @Inject
     private DatahubAuditGrpcServiceAssertion auditGrpcServiceAssertion;
 
-    //CB-7294
-    @Ignore
+    @Override
+    protected void setupTest(TestContext testContext) {
+        createDefaultUser(testContext);
+        createDefaultCredential(testContext);
+        createDefaultImageCatalog(testContext);
+        initializeDefaultBlueprints(testContext);
+        createDefaultDatahub(testContext);
+    }
+
     @Test(dataProvider = TEST_CONTEXT_WITH_MOCK)
     @Description(
             given = "there is a running DistroX cluster",
@@ -59,43 +39,24 @@ public class DistroXClusterStopStartTest extends AbstractClouderaManagerTest {
             then = "the cluster should be available")
     public void testCreateNewRegularDistroXClusterScaleStartStop(MockedTestContext testContext, ITestContext testNgContext) {
         DistroXStartStopTestParameters params = new DistroXStartStopTestParameters(testNgContext.getCurrentXmlTest().getAllParameters());
-        String stack = resourcePropertyProvider().getName();
-        int step = params.getStep();
-        int current = step;
-        DistroXTestDto currentContext = testContext
-                .given(DIX_NET_KEY, DistroXNetworkTestDto.class)
-                .given(DIX_IMG_KEY, DistroXImageTestDto.class)
-                .withImageCatalog()
-                .withImageId(IMAGE_CATALOG_ID)
-                .given(CM_FOR_DISTRO_X, DistroXClouderaManagerTestDto.class)
-                .given(CLUSTER_KEY, DistroXClusterTestDto.class)
-                .withValidateBlueprint(false)
-                .withClouderaManager(CM_FOR_DISTRO_X)
-                .given(stack, DistroXTestDto.class)
-                .withCluster(CLUSTER_KEY)
-                .withName(stack)
-                .withImageSettings(DIX_IMG_KEY)
-                .withNetwork(DIX_NET_KEY)
-                .when(distroXClient.create(), key(stack))
-                .await(STACK_AVAILABLE, key(stack));
-        for (int i = 0; i < params.getTimes(); i++, current += step) {
-            currentContext = currentContext
-                    .when(distroXClient.stop(), key(stack))
-                    .await(STACK_STOPPED, key(stack))
-                    .then(auditGrpcServiceAssertion::stop)
-                    .when(distroXClient.start(), key(stack))
-                    .await(STACK_AVAILABLE, key(stack))
-                    .then(auditGrpcServiceAssertion::start)
-                    .when(distroXClient.scale(params.getHostgroup(), current))
-                    .await(STACK_AVAILABLE, key(stack).withPollingInterval(POLLING_INTERVAL));
+
+        if (params.getTimes() < 1) {
+            throw new TestFailException("Scaling Configuration ERROR: At least 1 round of scaling should be set" +
+                    " with the [DistroXStartStopTestParameters.times] parameter!");
         }
 
-        currentContext
-                .validate();
-    }
-
-    @Override
-    protected BlueprintTestClient blueprintTestClient() {
-        return blueprintTestClient;
+        IntStream.range(1, params.getTimes()).forEach(i -> testContext.given(DistroXTestDto.class)
+                .when(distroXTestClient.stop())
+                .awaitForFlow()
+                .await(STACK_STOPPED)
+                .then(auditGrpcServiceAssertion::stop)
+                .when(distroXTestClient.start())
+                .awaitForFlow()
+                .await(STACK_AVAILABLE)
+                .then(auditGrpcServiceAssertion::start)
+                .when(distroXTestClient.scale(params.getHostgroup(), params.getStep() + i))
+                .awaitForFlow()
+                .await(STACK_AVAILABLE)
+                .validate());
     }
 }
