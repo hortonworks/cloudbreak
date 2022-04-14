517494ca497226c39b6afbc5ed24d7ce53181b65
Attila Kristof Foldi <foldik@cloudera.com>
2022-04-14 08:12:39 +0200

    CB-16828 Log warning when quartz takes long time. Categorize it to >30s, 15s-30s, 5s-15s.

--- common/src/main/java/com/sequenceiq/cloudbreak/quartz/metric/JobMetricsListener.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/quartz/metric/JobMetricsListener.java
@@ -15,9 +15,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.common.metrics.MetricService;
+import com.sequenceiq.cloudbreak.logger.LoggerContextKey;
 
 @Component
 public class JobMetricsListener extends JobListenerSupport {
+
+    private static final int MILLI_SECONDS_5000 = 5000;
+
+    private static final int MILLI_SECONDS_15000 = 15000;
+
+    private static final int MILLI_SECONDS_30000 = 30000;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JobMetricsListener.class);
 
@@ -46,7 +53,34 @@ public class JobMetricsListener extends JobListenerSupport {
                 jobException == null ? "without exception" : jobException.getMessage());
         QuartzMetricType metricType = jobException == null ? QuartzMetricType.JOB_FINISHED : QuartzMetricType.JOB_FAILED;
         Duration jobRunTime = Duration.ofMillis(context.getJobRunTime());
+        logJobDurationIfCritical(context);
         metricService.recordTimerMetric(metricType, jobRunTime,
                 JOB_GROUP.name(), jobKey.getGroup());
+    }
+
+    private void logJobDurationIfCritical(JobExecutionContext context) {
+        long jobRunTime = context.getJobRunTime();
+        if (isJobDurationIsCritical(jobRunTime)) {
+            LOGGER.warn("Critical quartz job ({}), requestId: {}, duration: {}ms, group: {}, class: {}",
+                    calculateCriticalExecutionTimeCategory(jobRunTime),
+                    context.get(LoggerContextKey.REQUEST_ID.toString()),
+                    jobRunTime,
+                    context.getJobDetail().getKey().getGroup(),
+                    context.getJobDetail().getJobClass().getName());
+        }
+    }
+
+    private boolean isJobDurationIsCritical(long jobRunTime) {
+        return jobRunTime > MILLI_SECONDS_5000;
+    }
+
+    private String calculateCriticalExecutionTimeCategory(long jobRunTime) {
+        if (jobRunTime > MILLI_SECONDS_30000) {
+            return ">30s";
+        } else if (jobRunTime > MILLI_SECONDS_15000) {
+            return "15s-30s";
+        } else {
+            return "5s-15s";
+        }
     }
 }
