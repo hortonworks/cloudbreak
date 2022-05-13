7cf9f6e92afd4c42de9c4cb76f39f0396f9c0e4c
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2022-05-13 15:46:55 +0200

    CB-16391 add more logging regarding users in http request to catch the root cause

--- core/src/test/java/com/sequenceiq/cloudbreak/filter/WorkspaceConfiguratorFilterTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/filter/WorkspaceConfiguratorFilterTest.java
@@ -1,31 +1,33 @@
 package com.sequenceiq.cloudbreak.filter;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
 import com.sequenceiq.cloudbreak.auth.crn.CrnTestUtil;
 import com.sequenceiq.cloudbreak.auth.security.authentication.AuthenticatedUserService;
@@ -36,11 +38,10 @@ import com.sequenceiq.cloudbreak.structuredevent.CloudbreakRestRequestThreadLoca
 import com.sequenceiq.cloudbreak.workspace.model.User;
 import com.sequenceiq.cloudbreak.workspace.model.Workspace;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class WorkspaceConfiguratorFilterTest {
 
-    @Rule
-    public final ExpectedException expectedException = ExpectedException.none();
+    private static final String USER_CRN = "crn:cdp:iam:us-west-1:1234:user:5678";
 
     @Mock
     private CloudbreakRestRequestThreadLocalService cloudbreakRestRequestThreadLocalService;
@@ -66,11 +67,11 @@ public class WorkspaceConfiguratorFilterTest {
     @InjectMocks
     private WorkspaceConfiguratorFilter underTest;
 
-    @Before
+    @BeforeEach
     public void before() throws IOException, ServletException {
-        doNothing().when(filterChain).doFilter(any(), any());
-        doNothing().when(cloudbreakRestRequestThreadLocalService).removeRequestedWorkspaceId();
-        doNothing().when(cloudbreakRestRequestThreadLocalService).setRequestedWorkspaceId(any());
+        lenient().doNothing().when(filterChain).doFilter(any(), any());
+        lenient().doNothing().when(cloudbreakRestRequestThreadLocalService).removeRequestedWorkspaceId();
+        lenient().doNothing().when(cloudbreakRestRequestThreadLocalService).setRequestedWorkspaceId(any());
     }
 
     @Test
@@ -99,16 +100,36 @@ public class WorkspaceConfiguratorFilterTest {
     }
 
     @Test
-    public void filterWhenWorkspaceDoesntExist() throws ServletException, IOException {
+    public void filterWhenWorkspaceDoesNotExist() {
         CloudbreakUser cbUser = createCbUserWithCrn();
         when(authenticatedUserService.getCbUser()).thenReturn(cbUser);
         when(userService.getOrCreate(any())).thenReturn(new User());
         when(workspaceService.getByName(anyString(), any())).thenReturn(Optional.empty());
 
-        expectedException.expect(IllegalStateException.class);
-        expectedException.expectMessage("Tenant default workspace does not exist!");
+        assertThrows(IllegalStateException.class, () -> underTest.doFilterInternal(request, response, filterChain),
+                "Tenant default workspace does not exist!");
 
-        underTest.doFilterInternal(request, response, filterChain);
+        verify(workspaceService, times(1)).getByName(eq(Crn.fromString(cbUser.getUserCrn()).getAccountId()), any());
+        verify(workspaceService, times(0)).getByName(eq(cbUser.getTenant()), any());
+    }
+
+    @Test
+    public void testGettingHeaders() {
+        CloudbreakUser cbUser = createCbUserWithCrn();
+        when(authenticatedUserService.getCbUser()).thenReturn(cbUser);
+        when(userService.getOrCreate(any())).thenReturn(new User());
+        when(workspaceService.getByName(anyString(), any())).thenReturn(Optional.of(createWorkspace()));
+        when(request.getHeaderNames()).thenReturn(Collections.enumeration(Set.of("header1", "header2")));
+        when(request.getHeaders("header1")).thenReturn(Collections.enumeration(Set.of("value1")));
+        when(request.getHeaders("header2")).thenReturn(Collections.enumeration(Set.of("value2", "value3")));
+
+        ThreadBasedUserCrnProvider.doAs(USER_CRN, () -> {
+            try {
+                underTest.doFilterInternal(request, response, filterChain);
+            } catch (Exception e) {
+
+            }
+        });
 
         verify(workspaceService, times(1)).getByName(eq(Crn.fromString(cbUser.getUserCrn()).getAccountId()), any());
         verify(workspaceService, times(0)).getByName(eq(cbUser.getTenant()), any());
