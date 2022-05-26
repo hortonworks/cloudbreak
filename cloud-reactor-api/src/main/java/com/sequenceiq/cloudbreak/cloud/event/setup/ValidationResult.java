64d4bdab93508d2ebdf89590a0852b0a91b3da2a
Tibor Popovics <tiborpopovics93@gmail.com>
2022-05-26 21:36:46 +0200

    CB-17162: Show warning message when we are unable to read details of the Azure Marketplace image.
    At the beginning of the cluster provision, we're validating that the Terms and Conditions are accepted for the Azure Marketplace image.
    To perform this validation we need the Microsoft.MarketplaceOrdering/offertypes/publishers/offers/plans/agreements/read permission.
    Sometimes the customers are not providing this permission. After this change, we're sending an error message to the customer to make sure that the Terms and Conditions are accepted.

--- cloud-reactor-api/src/main/java/com/sequenceiq/cloudbreak/cloud/event/setup/ValidationResult.java
+++ cloud-reactor-api/src/main/java/com/sequenceiq/cloudbreak/cloud/event/setup/ValidationResult.java
@@ -1,11 +1,20 @@
 package com.sequenceiq.cloudbreak.cloud.event.setup;
 
+import java.util.Set;
+
 import com.sequenceiq.cloudbreak.cloud.event.CloudPlatformResult;
 
 public class ValidationResult extends CloudPlatformResult {
 
+    private Set<String> warningMessages;
+
     public ValidationResult(Long resourceId) {
         super(resourceId);
+    }
+
+    public ValidationResult(Long resourceId, Set<String> warningMessages) {
+        super(resourceId);
+        this.warningMessages = warningMessages;
     }
 
     public ValidationResult(Exception errorDetails, Long resourceId) {
@@ -16,4 +25,7 @@ public class ValidationResult extends CloudPlatformResult {
         super(statusReason, errorDetails, resourceId);
     }
 
+    public Set<String> getWarningMessages() {
+        return warningMessages;
+    }
 }
