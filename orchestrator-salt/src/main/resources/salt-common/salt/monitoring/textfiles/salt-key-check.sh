013b3cade324117275e9dab673f85d833897c652
Oliver Szabo <oleewere@gmail.com>
2022-06-09 09:09:44 +0200

    CB-17399. Create simple custom metrics by nodexporter textfiles.
    details:
    - add textfiles support for node exporter
    - follow the same example for any custom metrics
    - the simple example is: gathere salt keys, and put the output numbers for different types to a .prom file

---
+++ orchestrator-salt/src/main/resources/salt-common/salt/monitoring/textfiles/salt-key-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+/opt/salt_*/bin/salt-key --list-all --out=json --out-file=/tmp/salt_keys.txt
+result=$?
+if [[ "$result" == "0" && -f /tmp/salt_keys.txt ]]; then
+  minions=$(jq '.minions | length' /tmp/salt_keys.txt)
+  minions_rejected=$(jq '.minions_rejected | length' /tmp/salt_keys.txt)
+  minions_unaccepted=$(jq '.minions_pre | length' /tmp/salt_keys.txt)
+  minions_denied=$(jq '.minions_denied | length' /tmp/salt_keys.txt)
+  echo "salt_minion_accepted $minions" > /var/lib/node_exporter/files/salt_keys.prom.$$
+  echo "salt_minion_unaccepted $minions_unaccepted" >> /var/lib/node_exporter/files/salt_keys.prom.$$
+  echo "salt_minion_rejected $minions_rejected" >> /var/lib/node_exporter/files/salt_keys.prom.$$
+  echo "salt_minion_denied $minions_denied" >> /var/lib/node_exporter/files/salt_keys.prom.$$
+  mv /var/lib/node_exporter/files/salt_keys.prom.$$ /var/lib/node_exporter/files/salt_keys.prom
+  rm -rf /tmp/salt_keys.txt
+fi
