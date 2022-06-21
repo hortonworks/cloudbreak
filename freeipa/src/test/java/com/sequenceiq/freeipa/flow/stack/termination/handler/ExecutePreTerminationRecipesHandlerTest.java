203b9454474e1bb3fce025709ccff0a23ad31e8e
Peter Erdos <perdos@cloudera.com>
2022-06-21 16:28:06 +0200

    CB-17469 Do not run freeipa pre termination recipe if freeipa is fully deleted or stopped

--- freeipa/src/test/java/com/sequenceiq/freeipa/flow/stack/termination/handler/ExecutePreTerminationRecipesHandlerTest.java
+++ freeipa/src/test/java/com/sequenceiq/freeipa/flow/stack/termination/handler/ExecutePreTerminationRecipesHandlerTest.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.freeipa.flow.stack.termination.handler;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -7,9 +8,11 @@ import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -27,6 +30,7 @@ import com.sequenceiq.cloudbreak.orchestrator.host.HostOrchestrator;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
 import com.sequenceiq.cloudbreak.orchestrator.state.ExitCriteriaModel;
 import com.sequenceiq.flow.reactor.api.handler.HandlerEvent;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceStatus;
 import com.sequenceiq.freeipa.entity.InstanceMetaData;
 import com.sequenceiq.freeipa.entity.Stack;
 import com.sequenceiq.freeipa.flow.stack.StackFailureEvent;
@@ -119,6 +123,70 @@ class ExecutePreTerminationRecipesHandlerTest {
         assertEquals(StackFailureEvent.class, selectable.getClass());
         Exception exception = ((StackFailureEvent) selectable).getException();
         assertEquals("timeoutException", exception.getMessage());
+    }
+
+    @Test
+    public void doAcceptTestButEveryInstanceIsDeleted() throws CloudbreakOrchestratorFailedException, CloudbreakOrchestratorTimeoutException {
+        Stack stack = mock(Stack.class);
+        Set<InstanceMetaData> instanceMetaDataSet = Collections.emptySet();
+        when(stack.getNotDeletedInstanceMetaDataSet()).thenReturn(instanceMetaDataSet);
+        when(stackService.getByIdWithListsInTransaction(1L)).thenReturn(stack);
+        GatewayConfig gatewayConfig = mock(GatewayConfig.class);
+        when(gatewayConfigService.getPrimaryGatewayConfig(stack)).thenReturn(gatewayConfig);
+        ExecutePreTerminationRecipesRequest executePreTerminationRecipesRequest = new ExecutePreTerminationRecipesRequest(1L, true);
+        Selectable selectable = executePreTerminationRecipesHandler.doAccept(new HandlerEvent<>(new Event<>(executePreTerminationRecipesRequest)));
+        verify(hostOrchestrator, times(0)).preTerminationRecipes(any(), any(), any(), anyBoolean());
+        assertEquals(ExecutePreTerminationRecipesFinished.class, selectable.getClass());
+        assertTrue(((ExecutePreTerminationRecipesFinished) selectable).getForced());
+        assertEquals(1L, selectable.getResourceId());
+    }
+
+    @Test
+    public void doAcceptTestButWeHaveDeletedAndStoppedNodesOnly() throws CloudbreakOrchestratorFailedException, CloudbreakOrchestratorTimeoutException {
+        Stack stack = mock(Stack.class);
+        InstanceMetaData instanceMetaData1 = new InstanceMetaData();
+        instanceMetaData1.setInstanceStatus(InstanceStatus.STOPPED);
+        InstanceMetaData instanceMetaData2 = new InstanceMetaData();
+        instanceMetaData2.setInstanceStatus(InstanceStatus.STOPPED);
+        Set<InstanceMetaData> instanceMetaDataSet = Set.of(instanceMetaData1, instanceMetaData2);
+        when(stack.getNotDeletedInstanceMetaDataSet()).thenReturn(instanceMetaDataSet);
+        when(stackService.getByIdWithListsInTransaction(1L)).thenReturn(stack);
+        GatewayConfig gatewayConfig = mock(GatewayConfig.class);
+        when(gatewayConfigService.getPrimaryGatewayConfig(stack)).thenReturn(gatewayConfig);
+        ExecutePreTerminationRecipesRequest executePreTerminationRecipesRequest = new ExecutePreTerminationRecipesRequest(1L, true);
+        Selectable selectable = executePreTerminationRecipesHandler.doAccept(new HandlerEvent<>(new Event<>(executePreTerminationRecipesRequest)));
+        verify(hostOrchestrator, times(0)).preTerminationRecipes(any(), any(), any(), anyBoolean());
+        assertEquals(ExecutePreTerminationRecipesFinished.class, selectable.getClass());
+        assertTrue(((ExecutePreTerminationRecipesFinished) selectable).getForced());
+        assertEquals(1L, selectable.getResourceId());
+    }
+
+    @Test
+    public void doAcceptTestButWeHaveDeleted1StoppedAnd1CreatedNode() throws CloudbreakOrchestratorFailedException, CloudbreakOrchestratorTimeoutException {
+        Stack stack = mock(Stack.class);
+        InstanceMetaData instanceMetaData1 = new InstanceMetaData();
+        instanceMetaData1.setInstanceStatus(InstanceStatus.STOPPED);
+        InstanceMetaData instanceMetaData2 = new InstanceMetaData();
+        instanceMetaData2.setInstanceStatus(InstanceStatus.CREATED);
+        Set<InstanceMetaData> instanceMetaDataSet = Set.of(instanceMetaData1, instanceMetaData2);
+        when(stack.getNotDeletedInstanceMetaDataSet()).thenReturn(instanceMetaDataSet);
+        Set<Node> nodes = Set.of(mock(Node.class));
+        ArgumentCaptor<Set<InstanceMetaData>> instanceMetadatasArgumentCaptor = ArgumentCaptor.forClass(Set.class);
+        when(freeIpaNodeUtilService.mapInstancesToNodes(instanceMetadatasArgumentCaptor.capture())).thenReturn(nodes);
+        when(stackService.getByIdWithListsInTransaction(1L)).thenReturn(stack);
+        GatewayConfig gatewayConfig = mock(GatewayConfig.class);
+        ArgumentCaptor<ExitCriteriaModel> exitCriteriaModelArgumentCaptor = ArgumentCaptor.forClass(ExitCriteriaModel.class);
+        when(gatewayConfigService.getPrimaryGatewayConfig(stack)).thenReturn(gatewayConfig);
+        ExecutePreTerminationRecipesRequest executePreTerminationRecipesRequest = new ExecutePreTerminationRecipesRequest(1L, true);
+        Selectable selectable = executePreTerminationRecipesHandler.doAccept(new HandlerEvent<>(new Event<>(executePreTerminationRecipesRequest)));
+        verify(hostOrchestrator).preTerminationRecipes(eq(gatewayConfig), eq(nodes), exitCriteriaModelArgumentCaptor.capture(), eq(true));
+        Set<InstanceMetaData> instanceMetadatasArgumentCaptorValue = instanceMetadatasArgumentCaptor.getValue();
+        assertThat(instanceMetadatasArgumentCaptorValue).containsExactly(instanceMetaData2);
+        ExitCriteriaModel exitCriteriaModel = exitCriteriaModelArgumentCaptor.getValue();
+        assertTrue(((StackBasedExitCriteriaModel) exitCriteriaModel).getStackId().isEmpty());
+        assertEquals(ExecutePreTerminationRecipesFinished.class, selectable.getClass());
+        assertTrue(((ExecutePreTerminationRecipesFinished) selectable).getForced());
+        assertEquals(1L, selectable.getResourceId());
     }
 
 }
