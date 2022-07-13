6c8da0f11ab55a2cffda9ffaea58be23f9e7b2cb
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-07-13 12:53:02 +0200

    CB-17732 Fix NullPointer in PlatformStringTransformer when region is null
    Handle null region gracefully to avoid alerts for ycloud envs.

--- core/src/test/java/com/sequenceiq/cloudbreak/service/image/PlatformStringTransformerTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/image/PlatformStringTransformerTest.java
@@ -1,25 +1,21 @@
 package com.sequenceiq.cloudbreak.service.image;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.stream.Stream;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sequenceiq.cloudbreak.cloud.aws.common.AwsConstants;
 import com.sequenceiq.cloudbreak.cloud.azure.AzureConstants;
 import com.sequenceiq.cloudbreak.cloud.gcp.GcpConstants;
 import com.sequenceiq.cloudbreak.common.type.CloudConstants;
 
-@ExtendWith(MockitoExtension.class)
 class PlatformStringTransformerTest {
 
-    @InjectMocks
-    private PlatformStringTransformer platformStringTransformer;
+    private final PlatformStringTransformer underTest = new PlatformStringTransformer();
 
     private static Stream<Arguments> variantFlags() {
         return Stream.of(
@@ -47,8 +43,40 @@ class PlatformStringTransformerTest {
 
     @ParameterizedTest
     @MethodSource("variantFlags")
-    public void testUseBaseImageAndDisabledBaseImageShouldReturnError(String platform, String variant, String expected) {
-        Assert.assertEquals(expected.toLowerCase(), platformStringTransformer.getPlatformStringForImageCatalog(platform, variant).nameToLowerCase());
+    public void testGetPlatformStringForImageCatalog(String platform, String variant, String expected) {
+        assertEquals(expected.toLowerCase(), underTest.getPlatformStringForImageCatalog(platform, variant).nameToLowerCase());
+    }
+
+    private static Stream<Arguments> regionSource() {
+        return Stream.of(
+                Arguments.of(CloudConstants.AWS, "us-west", "AWS"),
+                Arguments.of(CloudConstants.AWS, "us-gov-west", "AWS_GOV"),
+                Arguments.of(CloudConstants.AWS_NATIVE_GOV, "us-gov-west", "AWS_NATIVE_GOV"),
+                Arguments.of(CloudConstants.AWS_NATIVE, "us-gov-west", "AWS_NATIVE_GOV"),
+                Arguments.of(CloudConstants.AWS_NATIVE, "us-west", "AWS_NATIVE"),
+                Arguments.of(CloudConstants.AWS, null, "AWS"),
+                Arguments.of(CloudConstants.AWS, "", "AWS"),
+
+                Arguments.of(CloudConstants.GCP, "us-west", "GCP"),
+                Arguments.of(CloudConstants.GCP, null, "GCP"),
+                Arguments.of(CloudConstants.GCP, "", "GCP"),
+
+                Arguments.of(CloudConstants.AZURE, "us-west", "AZURE"),
+                Arguments.of(CloudConstants.AZURE, null, "AZURE"),
+                Arguments.of(CloudConstants.AZURE, "", "AZURE"),
+
+                Arguments.of(CloudConstants.MOCK, null, "MOCK"),
+                Arguments.of(CloudConstants.MOCK, "", "MOCK"),
+
+                Arguments.of(CloudConstants.YARN, null, "YARN"),
+                Arguments.of(CloudConstants.YARN, "", "YARN")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("regionSource")
+    public void testGetPlatformStringForImageCatalogByRegion(String platform, String region, String expected) {
+        assertEquals(expected.toLowerCase(), underTest.getPlatformStringForImageCatalogByRegion(platform, region).nameToLowerCase());
     }
 
 }
