509ed824e02f57a92bf6ca91f8a232c89c2ad7e8
Gergely Topolyai <gtopolyai@hortonworks.com>
2022-07-19 15:32:41 +0200

    CB-17798 Reduce the INTEGRATIONTEST_MAX_PG_NETWORK_OUTPUT property's value to 4.05GB

--- integration-test/scripts/check-results.sh
+++ integration-test/scripts/check-results.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #please don't add this property to Jenkins
-: ${INTEGRATIONTEST_MAX_PG_NETWORK_OUTPUT:="10.5GB"}
+: ${INTEGRATIONTEST_MAX_PG_NETWORK_OUTPUT:="4.05GB"}
 
 status_code=0
 
