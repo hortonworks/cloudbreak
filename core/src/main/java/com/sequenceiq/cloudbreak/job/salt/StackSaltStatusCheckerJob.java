cf8fa2c504855fcc791d3bd1754726d6ed8d154c
David Bajzath <bajzathd@gmail.com>
2022-07-22 15:08:48 +0200

    CB-17180 Detect and fix Datahub saltuser password errors
    Works the same as in casse of FreeIPA: a salt command querying the saltuser password expiry date is sent to all gateway instances of a stack:
    - If the password expires sooner than a set threshold, a salt password rotation is started.
    - If it responds with an unauthorized response, a salt password rotation is started.
    - Other types of errors are only logged.

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/job/salt/StackSaltStatusCheckerJob.java
@@ -0,0 +1,141 @@
+package com.sequenceiq.cloudbreak.job.salt;
+
+import static com.sequenceiq.cloudbreak.util.Benchmark.measure;
+
+import java.util.EnumSet;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.common.Status;
+import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
+import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGenerator;
+import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGeneratorFactory;
+import com.sequenceiq.cloudbreak.common.exception.BadRequestException;
+import com.sequenceiq.cloudbreak.dto.StackDto;
+import com.sequenceiq.cloudbreak.quartz.statuschecker.job.StatusCheckerJob;
+import com.sequenceiq.cloudbreak.reactor.api.event.cluster.RotateSaltPasswordReason;
+import com.sequenceiq.cloudbreak.service.RotateSaltPasswordService;
+import com.sequenceiq.cloudbreak.service.stack.StackDtoService;
+
+import io.opentracing.Tracer;
+
+@DisallowConcurrentExecution
+@Component
+public class StackSaltStatusCheckerJob extends StatusCheckerJob {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StackSaltStatusCheckerJob.class);
+
+    private static final EnumSet<Status> IGNORED_STATES = EnumSet.of(
+            Status.REQUESTED,
+            Status.CREATE_IN_PROGRESS,
+            Status.UPDATE_IN_PROGRESS,
+            Status.UPDATE_REQUESTED,
+            Status.STOP_REQUESTED,
+            Status.START_REQUESTED,
+            Status.STOP_IN_PROGRESS,
+            Status.START_IN_PROGRESS,
+            Status.WAIT_FOR_SYNC,
+            Status.MAINTENANCE_MODE_ENABLED,
+            Status.EXTERNAL_DATABASE_CREATION_IN_PROGRESS,
+            Status.BACKUP_IN_PROGRESS,
+            Status.RESTORE_IN_PROGRESS,
+            Status.LOAD_BALANCER_UPDATE_IN_PROGRESS,
+            Status.RECOVERY_IN_PROGRESS,
+            Status.RECOVERY_REQUESTED
+    );
+
+    private static final EnumSet<Status> SYNCABLE_STATES = EnumSet.of(
+            Status.AVAILABLE,
+            Status.UPDATE_FAILED,
+            Status.ENABLE_SECURITY_FAILED,
+            Status.START_FAILED,
+            Status.STOP_FAILED,
+            Status.AMBIGUOUS,
+            Status.UNREACHABLE,
+            Status.NODE_FAILURE,
+            Status.RESTORE_FAILED,
+            Status.BACKUP_FAILED,
+            Status.BACKUP_FINISHED,
+            Status.RESTORE_FINISHED,
+            Status.EXTERNAL_DATABASE_START_FAILED,
+            Status.EXTERNAL_DATABASE_START_IN_PROGRESS,
+            Status.EXTERNAL_DATABASE_START_FINISHED,
+            Status.EXTERNAL_DATABASE_STOP_FAILED,
+            Status.EXTERNAL_DATABASE_STOP_IN_PROGRESS,
+            Status.EXTERNAL_DATABASE_STOP_FINISHED,
+            Status.RECOVERY_FAILED,
+            Status.UPGRADE_CCM_FAILED,
+            Status.UPGRADE_CCM_FINISHED,
+            Status.UPGRADE_CCM_IN_PROGRESS
+    );
+
+    @Inject
+    private StackSaltStatusCheckerJobService jobService;
+
+    @Inject
+    private StackDtoService stackDtoService;
+
+    @Inject
+    private RegionAwareInternalCrnGeneratorFactory regionAwareInternalCrnGeneratorFactory;
+
+    @Inject
+    private RotateSaltPasswordService rotateSaltPasswordService;
+
+    public StackSaltStatusCheckerJob(Tracer tracer) {
+        super(tracer, "Stack Salt Status Checker Job");
+    }
+
+    @Override
+    protected Object getMdcContextObject() {
+        return stackDtoService.getStackViewById(getStackId());
+    }
+
+    @Override
+    protected void executeTracedJob(JobExecutionContext context) throws JobExecutionException {
+        try {
+            measure(() -> {
+                StackDto stack = stackDtoService.getById(getStackId());
+                rotateSaltPasswordService.validateRotateSaltPassword(stack);
+                Status stackStatus = stack.getStatus();
+                if (Status.getUnschedulableStatuses().contains(stackStatus)) {
+                    LOGGER.debug("Stack salt sync will be unscheduled, stack state is {}", stackStatus);
+                    jobService.unschedule(context.getJobDetail().getKey());
+                } else if (null == stackStatus || IGNORED_STATES.contains(stackStatus)) {
+                    LOGGER.debug("Stack salt sync is skipped, stack state is {}", stackStatus);
+                } else if (SYNCABLE_STATES.contains(stackStatus)) {
+                    RegionAwareInternalCrnGenerator dataHub = regionAwareInternalCrnGeneratorFactory.datahub();
+                    ThreadBasedUserCrnProvider.doAs(dataHub.getInternalCrnForServiceAsString(), () -> rotateSaltPasswordIfNeeded(stack));
+                } else {
+                    LOGGER.warn("Unhandled stack status, {}", stackStatus);
+                }
+            }, LOGGER, "Check salt status took {} ms for stack {}.", getStackId());
+        } catch (BadRequestException e) {
+            LOGGER.info("StackSaltStatusCheckerJob cannot run, because validation failed for stack {} with message: {}", getStackId(), e.getMessage());
+            jobService.unschedule(context.getJobDetail().getKey());
+        } catch (Exception e) {
+            LOGGER.info("Exception during stack salt status check.", e);
+        }
+    }
+
+    private void rotateSaltPasswordIfNeeded(StackDto stack) {
+        try {
+            Optional<RotateSaltPasswordReason> rotateSaltPasswordReason = rotateSaltPasswordService.checkIfSaltPasswordRotationNeeded(stack);
+            rotateSaltPasswordReason.ifPresent(reason -> rotateSaltPasswordService.triggerRotateSaltPassword(stack, reason));
+        } catch (Exception e) {
+            rotateSaltPasswordService.sendFailureUsageReport(stack.getResourceCrn(), RotateSaltPasswordReason.UNSET, e.getMessage());
+            throw e;
+        }
+    }
+
+    private Long getStackId() {
+        return Long.valueOf(getLocalId());
+    }
+}
--- core/src/main/java/com/sequenceiq/cloudbreak/service/RotateSaltPasswordService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/RotateSaltPasswordService.java
@@ -1,7 +1,10 @@
 package com.sequenceiq.cloudbreak.service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -10,23 +13,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.cloudera.thunderhead.service.common.usage.UsageProto;
+import com.sequenceiq.cloudbreak.auth.altus.EntitlementService;
 import com.sequenceiq.cloudbreak.common.exception.BadRequestException;
+import com.sequenceiq.cloudbreak.common.service.Clock;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.SaltBootstrapVersionChecker;
+import com.sequenceiq.cloudbreak.core.flow2.service.ReactorFlowManager;
 import com.sequenceiq.cloudbreak.domain.SecurityConfig;
 import com.sequenceiq.cloudbreak.dto.StackDto;
 import com.sequenceiq.cloudbreak.orchestrator.exception.CloudbreakOrchestratorException;
 import com.sequenceiq.cloudbreak.orchestrator.host.HostOrchestrator;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
+import com.sequenceiq.cloudbreak.quartz.saltstatuschecker.SaltStatusCheckerConfig;
 import com.sequenceiq.cloudbreak.reactor.api.event.cluster.RotateSaltPasswordReason;
 import com.sequenceiq.cloudbreak.service.securityconfig.SecurityConfigService;
 import com.sequenceiq.cloudbreak.usage.UsageReporter;
 import com.sequenceiq.cloudbreak.util.PasswordUtil;
 import com.sequenceiq.cloudbreak.view.InstanceMetadataView;
+import com.sequenceiq.flow.api.model.FlowIdentifier;
 
 @Service
 public class RotateSaltPasswordService {
 
+    protected static final String SALTUSER = "saltuser";
+
+    protected static final String UNAUTHORIZED_RESPONSE = "Status: 401 Unauthorized Response";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(RotateSaltPasswordService.class);
+
+    @Inject
+    private SaltStatusCheckerConfig saltStatusCheckerConfig;
+
+    @Inject
+    private Clock clock;
 
     @Inject
     private HostOrchestrator hostOrchestrator;
@@ -43,9 +61,15 @@ public class RotateSaltPasswordService {
     @Inject
     private UsageReporter usageReporter;
 
+    @Inject
+    private EntitlementService entitlementService;
+
+    @Inject
+    private ReactorFlowManager flowManager;
+
     public void validateRotateSaltPassword(StackDto stack) {
-        if (!stack.isAvailable()) {
-            throw new BadRequestException("Rotating SaltStack user password is only available for stacks in available status");
+        if (!entitlementService.isSaltUserPasswordRotationEnabled(stack.getAccountId())) {
+            throw new BadRequestException("Rotating SaltStack user password is not supported in your account");
         }
         if (!isChangeSaltuserPasswordSupported(stack)) {
             throw new BadRequestException(String.format("Rotating SaltStack user password is not supported with your image version, " +
@@ -58,6 +82,11 @@ public class RotateSaltPasswordService {
         return stack.getNotTerminatedAndNotZombieGatewayInstanceMetadata().stream()
                 .map(InstanceMetadataView::getImage)
                 .allMatch(image -> saltBootstrapVersionChecker.isChangeSaltuserPasswordSupported(image));
+    }
+
+    public FlowIdentifier triggerRotateSaltPassword(StackDto stack, RotateSaltPasswordReason reason) {
+        validateRotateSaltPassword(stack);
+        return flowManager.triggerRotateSaltPassword(stack.getId(), reason);
     }
 
     public void rotateSaltPassword(StackDto stack) throws CloudbreakOrchestratorException {
@@ -94,5 +123,41 @@ public class RotateSaltPasswordService {
             LOGGER.error("Failed to report rotate salt password event with resource crn {}, reason {}, result {} and message {}",
                     resourceCrn, reason, result, message, e);
         }
+    }
+
+    public Optional<RotateSaltPasswordReason> checkIfSaltPasswordRotationNeeded(StackDto stack) {
+        Optional<RotateSaltPasswordReason> result;
+        try {
+            List<GatewayConfig> allGatewayConfigs = gatewayConfigService.getAllGatewayConfigs(stack);
+            LocalDate passwordExpiryDate = hostOrchestrator.getPasswordExpiryDate(allGatewayConfigs, SALTUSER);
+            if (isPasswordExpiresSoon(passwordExpiryDate)) {
+                LOGGER.info("Stack {} user {} password expires at {}, password rotation is needed", stack.getId(), SALTUSER, passwordExpiryDate);
+                result = Optional.of(RotateSaltPasswordReason.EXPIRED);
+            } else {
+                LOGGER.info("Stack {} user {} password expires at {}, nothing to do", stack.getId(), SALTUSER, passwordExpiryDate);
+                result = Optional.empty();
+            }
+        } catch (CloudbreakOrchestratorException e) {
+            if (isUnauthorizedException(e)) {
+                LOGGER.info("Received unauthorized response from salt on stack {}", stack.getId());
+                result = Optional.of(RotateSaltPasswordReason.UNAUTHORIZED);
+            } else {
+                LOGGER.warn("Received error response from salt on stack {}", stack.getId(), e);
+                throw new CloudbreakRuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+    private boolean isPasswordExpiresSoon(LocalDate passwordExpiryDate) {
+        long daysUntilPasswordExpires = ChronoUnit.DAYS.between(clock.getCurrentLocalDateTime(), passwordExpiryDate.atStartOfDay());
+        return daysUntilPasswordExpires <= saltStatusCheckerConfig.getPasswordExpiryThresholdInDays();
+    }
+
+    private static boolean isUnauthorizedException(CloudbreakOrchestratorException e) {
+        return Optional.ofNullable(e.getCause())
+                .map(Throwable::getCause)
+                .filter(ex -> ex.getMessage().startsWith(UNAUTHORIZED_RESPONSE))
+                .isPresent();
     }
 }
