bd0e92e88b1cc32d4cae1e7f02768b3015740b95
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-08-05 15:27:38 +0200

    CB-18058 Ability to define Postgres version from runtime version X
    When the database engine version is not defined in the request, it would be overridden with the defined value if the runtime version is equal or higher than the deifned.
    For now it is set to a not existing high veresion number so it doesn't effect anything.
    
    Logic:
    - engine version empty and runtime is older: engine version is null, current default behavior
    - engine version empty and runtime is equal or new: engine version is overridden with defined version (currently 11)
    - engine version is specified in the request: independently from runtime version it will be honored and won't be overridden

---
+++ common/src/main/java/com/sequenceiq/cloudbreak/service/database/DatabaseDefaultVersionProvider.java
@@ -0,0 +1,38 @@
+package com.sequenceiq.cloudbreak.service.database;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.util.VersionComparator;
+
+@Component
+public class DatabaseDefaultVersionProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseDefaultVersionProvider.class);
+
+    @Value("${cb.db.override.minRuntimeVersion}")
+    private String minRuntimeVersion;
+
+    @Value("${cb.db.override.engineVersion}")
+    private String dbEngineVersion;
+
+    private final VersionComparator versionComparator = new VersionComparator();
+
+    public String calculateDbVersionBasedOnRuntimeIfMissing(String runtime, String requestedDbEngineVersion) {
+        if (StringUtils.isNotBlank(requestedDbEngineVersion)) {
+            LOGGER.debug("DB engine version already requested to be [{}]", requestedDbEngineVersion);
+            return requestedDbEngineVersion;
+        } else {
+            if (0 <= versionComparator.compare(() -> runtime, () -> minRuntimeVersion)) {
+                LOGGER.debug("Setting DB engine version to [{}] for runtime [{}]", dbEngineVersion, runtime);
+                return dbEngineVersion;
+            } else {
+                LOGGER.debug("Setting DB engine version to 'null' for runtime [{}]", runtime);
+                return null;
+            }
+        }
+    }
+}
