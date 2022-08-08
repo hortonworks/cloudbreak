871bfed0aeaad47ca9f20bcf5097856c987d2260
David Bajzath <bajzathd@gmail.com>
2022-08-08 17:29:05 +0200

    CB-17348 Create e2e test for saltuser password rotation
    Verify the changed password and password expiry dates on FreeIPA, Datalake and Datahub clusters with the following steps:
    - Set the password change date to a past date (on new clsuters that would be the current day by default)
    - Get the saltuser line from the shadow file and save it as a reference
    - Call the rotate salt password endpoint
    - Verify that the saltuser's line in the shadow file was changed
    - Verify that the saltuser's password change date is set to today

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/freeipa/FreeipaRotateSaltPasswordAction.java
@@ -0,0 +1,27 @@
+package com.sequenceiq.it.cloudbreak.action.freeipa;
+
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sequenceiq.flow.api.model.FlowIdentifier;
+import com.sequenceiq.it.cloudbreak.FreeIpaClient;
+import com.sequenceiq.it.cloudbreak.action.Action;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaTestDto;
+import com.sequenceiq.it.cloudbreak.log.Log;
+
+public class FreeipaRotateSaltPasswordAction implements Action<FreeIpaTestDto, FreeIpaClient> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FreeIpaDownscaleAction.class);
+
+    @Override
+    public FreeIpaTestDto action(TestContext testContext, FreeIpaTestDto testDto, FreeIpaClient client) throws Exception {
+        Log.whenJson(LOGGER, format(" FreeIPA rotate salt password request for environment %n"), testDto.getEnvironmentCrn());
+        FlowIdentifier flowIdentifier = client.getDefaultClient().getFreeIpaV1Endpoint().rotateSaltPassword(testDto.getEnvironmentCrn());
+        testDto.setFlow("FreeIPA rotate salt password",  flowIdentifier);
+        Log.whenJson(LOGGER, format(" FreeIPA rotate salt password started: %n"), testDto.getEnvironmentCrn());
+        return testDto;
+    }
+}
