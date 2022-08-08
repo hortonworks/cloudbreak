3875ca6c2ccd2a0118a51a11fd6c7ce1810d14e5
Laszlo Turcsanyi <lturcsanyi@cloudera.com>
2022-08-08 15:47:09 +0200

    CB-18105 The zombie status is not allowed in case of repair, when the primary gateway changed or if the CDP_TARGETED_UPSCALE entitlement is disabled
    Fix a bug related to setting the SERVICES_RUNNING instance status.

--- core/src/test/java/com/sequenceiq/cloudbreak/core/cluster/ClusterManagerUpscaleServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/core/cluster/ClusterManagerUpscaleServiceTest.java
@@ -1,7 +1,11 @@
 package com.sequenceiq.cloudbreak.core.cluster;
 
+import static com.sequenceiq.cloudbreak.polling.ExtendedPollingResult.ExtendedPollingResultBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -19,8 +23,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.base.InstanceStatus;
 import com.sequenceiq.cloudbreak.cluster.api.ClusterApi;
 import com.sequenceiq.cloudbreak.cluster.service.ClusterClientInitException;
+import com.sequenceiq.cloudbreak.common.exception.CloudbreakServiceException;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.ClusterServiceRunner;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.host.ClusterHostServiceRunner;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
@@ -28,11 +34,18 @@ import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.orchestrator.model.NodeReachabilityResult;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterApiConnectors;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterService;
+import com.sequenceiq.cloudbreak.service.stack.InstanceMetaDataService;
 import com.sequenceiq.cloudbreak.service.stack.StackService;
 import com.sequenceiq.cloudbreak.service.stack.TargetedUpscaleSupportService;
 
 @ExtendWith(MockitoExtension.class)
 public class ClusterManagerUpscaleServiceTest {
+
+    private static final long STACK_ID = 1L;
+
+    private static final long CLUSTER_ID = 2L;
+
+    private static final long INSTANCE_METADATA_ID = 3L;
 
     @Mock
     private StackService stackService;
@@ -55,26 +68,30 @@ public class ClusterManagerUpscaleServiceTest {
     @Mock
     private TargetedUpscaleSupportService targetedUpscaleSupportService;
 
+    @Mock
+    private InstanceMetaDataService instanceMetaDataService;
+
     @InjectMocks
     private ClusterManagerUpscaleService underTest;
 
     @Test
     public void testUpscaleIfTargetedUpscaleNotSupportedOrPrimaryGatewayChanged() throws ClusterClientInitException {
         Stack stack = getStack();
-        when(stackService.getByIdWithListsInTransaction(any())).thenReturn(stack);
-        when(clusterService.findOneWithLists(1L)).thenReturn(Optional.of(stack.getCluster()));
-        when(clusterHostServiceRunner.addClusterServices(any(), any(), any(), anyBoolean())).thenReturn(new NodeReachabilityResult(Set.of(), Set.of()));
-        doNothing().when(clusterServiceRunner).updateAmbariClientConfig(any(), any());
-        doNothing().when(clusterService).updateInstancesToRunning(any(), any());
-        when(clusterApiConnectors.getConnector(any(Stack.class))).thenReturn(clusterApi);
+        when(stackService.getByIdWithListsInTransaction(eq(STACK_ID))).thenReturn(stack);
+        when(clusterService.findOneWithLists(CLUSTER_ID)).thenReturn(Optional.of(stack.getCluster()));
+        when(clusterHostServiceRunner.addClusterServices(eq(stack), eq(stack.getCluster()), any(), anyBoolean()))
+                .thenReturn(new NodeReachabilityResult(Set.of(), Set.of()));
+        doNothing().when(clusterServiceRunner).updateAmbariClientConfig(eq(stack), eq(stack.getCluster()));
+        doNothing().when(clusterService).updateInstancesToRunning(eq(STACK_ID), any());
+        when(clusterApiConnectors.getConnector(eq(stack))).thenReturn(clusterApi);
 
-        underTest.upscaleClusterManager(1L, Collections.singletonMap("hg", 1), true, false);
+        underTest.upscaleClusterManager(STACK_ID, Collections.singletonMap("hg", 1), true, false);
 
         verifyNoInteractions(targetedUpscaleSupportService);
 
         when(targetedUpscaleSupportService.targetedUpscaleOperationSupported(any())).thenReturn(Boolean.FALSE);
 
-        underTest.upscaleClusterManager(1L, Collections.singletonMap("hg", 1), false, false);
+        underTest.upscaleClusterManager(STACK_ID, Collections.singletonMap("hg", 3), false, false);
 
         verifyNoMoreInteractions(clusterServiceRunner);
         verify(clusterApi, times(2)).waitForHosts(any());
@@ -83,23 +100,47 @@ public class ClusterManagerUpscaleServiceTest {
     @Test
     public void testUpscaleIfTargetedUpscaleSupported() throws ClusterClientInitException {
         Stack stack = getStack();
-        when(stackService.getByIdWithListsInTransaction(any())).thenReturn(stack);
-        when(clusterService.findOneWithLists(1L)).thenReturn(Optional.of(stack.getCluster()));
-        when(clusterHostServiceRunner.addClusterServices(any(), any(), any(), anyBoolean())).thenReturn(new NodeReachabilityResult(Set.of(), Set.of()));
-        doNothing().when(clusterService).updateInstancesToRunning(any(), any());
-        when(clusterApiConnectors.getConnector(any(Stack.class))).thenReturn(clusterApi);
-        when(targetedUpscaleSupportService.targetedUpscaleOperationSupported(any())).thenReturn(Boolean.TRUE);
+        when(stackService.getByIdWithListsInTransaction(eq(STACK_ID))).thenReturn(stack);
+        when(clusterService.findOneWithLists(eq(CLUSTER_ID))).thenReturn(Optional.of(stack.getCluster()));
+        when(clusterHostServiceRunner.addClusterServices(eq(stack), eq(stack.getCluster()), any(), anyBoolean()))
+                .thenReturn(new NodeReachabilityResult(Set.of(), Set.of()));
+        doNothing().when(clusterService).updateInstancesToRunning(eq(STACK_ID), any());
+        when(clusterApiConnectors.getConnector(eq(stack))).thenReturn(clusterApi);
+        when(targetedUpscaleSupportService.targetedUpscaleOperationSupported(eq(stack))).thenReturn(Boolean.TRUE);
 
-        underTest.upscaleClusterManager(1L, Collections.singletonMap("hg", 1), false, false);
+        underTest.upscaleClusterManager(STACK_ID, Collections.singletonMap("hg", 3), false, false);
 
         verifyNoInteractions(clusterServiceRunner);
         verify(clusterApi).waitForHosts(any());
     }
 
+    @Test
+    public void testUpscaleClusterManagerWhenRepairAndWaitForHostsTimedOut() throws ClusterClientInitException {
+        Stack stack = getStack();
+        when(stackService.getByIdWithListsInTransaction(eq(STACK_ID))).thenReturn(stack);
+        when(clusterService.findOneWithLists(eq(CLUSTER_ID))).thenReturn(Optional.of(stack.getCluster()));
+        when(clusterHostServiceRunner.addClusterServices(eq(stack), eq(stack.getCluster()), any(), anyBoolean()))
+                .thenReturn(new NodeReachabilityResult(Set.of(), Set.of()));
+        doNothing().when(clusterService).updateInstancesToRunning(eq(STACK_ID), any());
+        when(clusterApiConnectors.getConnector(eq(stack))).thenReturn(clusterApi);
+        when(clusterApi.waitForHosts(any())).thenReturn(new ExtendedPollingResultBuilder().withPayload(Set.of(INSTANCE_METADATA_ID)).timeout().build());
+
+        CloudbreakServiceException exception = assertThrows(CloudbreakServiceException.class,
+                () -> underTest.upscaleClusterManager(STACK_ID, Collections.singletonMap("hg", 1), false, true));
+
+        verifyNoInteractions(clusterServiceRunner);
+        verify(clusterApi).waitForHosts(any());
+        verify(instanceMetaDataService, times(1)).updateInstanceStatus(
+                eq(Set.of(INSTANCE_METADATA_ID)), eq(InstanceStatus.ORCHESTRATION_FAILED),
+                eq("Upscaling cluster manager were not successful, waiting for hosts timed out"));
+        assertEquals("Upscaling cluster manager were not successful, waiting for hosts timed out for nodes: [3]", exception.getMessage());
+    }
+
     private Stack getStack() {
         Stack stack = new Stack();
+        stack.setId(STACK_ID);
         Cluster cluster = new Cluster();
-        cluster.setId(1L);
+        cluster.setId(CLUSTER_ID);
         stack.setCluster(cluster);
         return stack;
 
