4d38cc5621c3ebf7b909820cfadcaea01737fe10
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2022-08-12 17:33:00 +0200

    CB-18178 Increase SSSD cache timeouts
    SSSD timeouts have been configured to 30sec while the default value is 1.5h. The reasons behind this decision were:
    - altus had this setting
    - testing for QE was easier as they don't have to wait after usersync
    
    This setting in large scale seems to cause issues, especially as we populate SSSD config with {{enumerate = true}} where Ranger or Nifi component is installed. For these instances it looks like SSSD is constantly consuming a lot of processing time when there is a lot of users and/or groups. Also this is putting high pressure on FreeIPA instances.
    We have seen already customers in the need to vertically upscale FreeIPA instances just to be able to handle these loads.
    
    For now we would like to increase this setting from 30sec to 300sec(5min) to see how it effects testing and customers. We expect this to decrease the load.

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/SssdConfigProviderTest.java
@@ -0,0 +1,132 @@
+package com.sequenceiq.cloudbreak.core.bootstrap.service.host;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.sequenceiq.cloudbreak.dto.KerberosConfig;
+import com.sequenceiq.cloudbreak.orchestrator.model.SaltPillarProperties;
+import com.sequenceiq.cloudbreak.service.freeipa.FreeIpaConfigProvider;
+import com.sequenceiq.cloudbreak.template.kerberos.KerberosDetailService;
+
+@ExtendWith(MockitoExtension.class)
+class SssdConfigProviderTest {
+
+    private static final int ENTRY_CACHE_TIMEOUT = 300;
+
+    private static final int MEMCACHE_TIMEOUT = 400;
+
+    private static final int DNS_TTL = 30;
+
+    private static final String ENV_CRN = "envCrn";
+
+    @Mock
+    private FreeIpaConfigProvider freeIpaConfigProvider;
+
+    @Mock
+    private KerberosDetailService kerberosDetailService;
+
+    @InjectMocks
+    private SssdConfigProvider underTest;
+
+    @BeforeEach
+    public void init() {
+        ReflectionTestUtils.setField(underTest, "entryCacheTimeout", ENTRY_CACHE_TIMEOUT);
+        ReflectionTestUtils.setField(underTest, "memcacheTimeout", MEMCACHE_TIMEOUT);
+    }
+
+    @Test
+    public void testCreateIpaPillarNonIpaJoinable() {
+        KerberosConfig kerberosConfig = new KerberosConfig();
+        when(kerberosDetailService.isIpaJoinable(kerberosConfig)).thenReturn(Boolean.FALSE);
+
+        Map<String, SaltPillarProperties> result = underTest.createSssdIpaPillar(kerberosConfig, Map.of(), "");
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testCreateIpaPillarIpaJoinableWOEnumerate() {
+        KerberosConfig kerberosConfig = createKerberosConfig();
+        when(kerberosDetailService.isIpaJoinable(kerberosConfig)).thenReturn(Boolean.TRUE);
+        when(kerberosDetailService.getDnsTtl()).thenReturn(DNS_TTL);
+        Map<String, Object> freeIpaConfig = Map.of("random", "stuff");
+        when(freeIpaConfigProvider.createFreeIpaConfig(ENV_CRN)).thenReturn(freeIpaConfig);
+
+        Map<String, SaltPillarProperties> result = underTest.createSssdIpaPillar(kerberosConfig, Map.of(), ENV_CRN);
+
+        assertGenericConfig(result, kerberosConfig, freeIpaConfig);
+        assertFalse((Boolean) ((Map<String, Object>) result.get("sssd-ipa").getProperties().get("sssd-ipa")).get("enumerate"));
+    }
+
+    static Object[][] scenarios() {
+        return new Object[][]{
+                {"Empty Services", Map.of("RANGER_ADMIN", List.of(), "NIFI_REGISTRY_SERVER", List.of(), "NIFI_NODE", List.of()), false},
+                {"Ranger present", Map.of("RANGER_ADMIN", List.of("asf"), "NIFI_REGISTRY_SERVER", List.of(), "NIFI_NODE", List.of()), true},
+                {"Ranger present alone", Map.of("RANGER_ADMIN", List.of("asf")), true},
+                {"NifiNode present", Map.of("RANGER_ADMIN", List.of(), "NIFI_REGISTRY_SERVER", List.of(), "NIFI_NODE", List.of("asf")), true},
+                {"NifiNode present alone", Map.of("NIFI_NODE", List.of("asf")), true},
+                {"NifiRegistry present", Map.of("RANGER_ADMIN", List.of(), "NIFI_REGISTRY_SERVER", List.of("asf"), "NIFI_NODE", List.of()), true},
+                {"NifiRegistry present alone", Map.of("NIFI_REGISTRY_SERVER", List.of("asf")), true},
+                {"All present", Map.of("RANGER_ADMIN", List.of("asf"), "NIFI_REGISTRY_SERVER", List.of("asf"), "NIFI_NODE", List.of("asf")), true},
+        };
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("scenarios")
+    public void testCreateIpaPillarIpaJoinableWithEnumerateCheck(String name, Map<String, List<String>> serviceLocations, boolean expectedEnumerate) {
+        KerberosConfig kerberosConfig = createKerberosConfig();
+        when(kerberosDetailService.isIpaJoinable(kerberosConfig)).thenReturn(Boolean.TRUE);
+        when(kerberosDetailService.getDnsTtl()).thenReturn(DNS_TTL);
+        Map<String, Object> freeIpaConfig = Map.of("random", "stuff");
+        when(freeIpaConfigProvider.createFreeIpaConfig(ENV_CRN)).thenReturn(freeIpaConfig);
+
+        Map<String, SaltPillarProperties> result = underTest.createSssdIpaPillar(kerberosConfig, serviceLocations, ENV_CRN);
+
+        assertGenericConfig(result, kerberosConfig, freeIpaConfig);
+        assertEquals(expectedEnumerate, ((Map<String, Object>) result.get("sssd-ipa").getProperties().get("sssd-ipa")).get("enumerate"));
+    }
+
+    private void assertGenericConfig(Map<String, SaltPillarProperties> result, KerberosConfig kerberosConfig, Map<String, Object> freeIpaConfig) {
+        SaltPillarProperties pillarProperties = result.get("sssd-ipa");
+        assertEquals("/sssd/ipa.sls", pillarProperties.getPath());
+        Map<String, Object> properties = pillarProperties.getProperties();
+
+        Map<String, Object> sssdConfig = (Map<String, Object>) properties.get("sssd-ipa");
+        assertEquals(kerberosConfig.getPrincipal(), sssdConfig.get("principal"));
+        assertEquals(kerberosConfig.getRealm().toUpperCase(), sssdConfig.get("realm"));
+        assertEquals(kerberosConfig.getDomain(), sssdConfig.get("domain"));
+        assertEquals(kerberosConfig.getPassword(), sssdConfig.get("password"));
+        assertEquals(kerberosConfig.getUrl(), sssdConfig.get("server"));
+        assertEquals(DNS_TTL, sssdConfig.get("dns_ttl"));
+        assertEquals(ENTRY_CACHE_TIMEOUT, sssdConfig.get("entryCacheTimeout"));
+        assertEquals(MEMCACHE_TIMEOUT, sssdConfig.get("memcacheTimeout"));
+
+        Map<String, Object> freeIpaConfigResult = (Map<String, Object>) properties.get("freeipa");
+        assertEquals(freeIpaConfig, freeIpaConfigResult);
+    }
+
+    private KerberosConfig createKerberosConfig() {
+        return KerberosConfig.KerberosConfigBuilder.aKerberosConfig()
+                .withPrincipal("principal")
+                .withRealm("realm")
+                .withDomain("domain")
+                .withPassword("pass")
+                .withUrl("url")
+                .build();
+    }
+}
