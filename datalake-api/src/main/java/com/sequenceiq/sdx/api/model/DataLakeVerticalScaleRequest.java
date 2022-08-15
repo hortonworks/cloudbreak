007b5d038bca26bc2e103253d398b8b04b2a87a3
doktoric <rdoktorics@cloudera.com>
2022-08-15 09:24:53 +0200

    CB-17990 Implement Data Lake API to be able to vertical scale Data Lake

---
+++ datalake-api/src/main/java/com/sequenceiq/sdx/api/model/DataLakeVerticalScaleRequest.java
@@ -0,0 +1,43 @@
+package com.sequenceiq.sdx.api.model;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.instancegroup.template.InstanceTemplateV4Request;
+import com.sequenceiq.common.model.JsonEntity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DataLakeVerticalScaleRequest implements JsonEntity {
+
+    @NotNull
+    @ApiModelProperty(ModelDescriptions.HOST_GROUP_NAME)
+    private String group;
+
+    @Valid
+    @NotNull
+    @ApiModelProperty(ModelDescriptions.CUSTOM_INSTANCE_GROUP_OPTIONS)
+    private InstanceTemplateV4Request template;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public InstanceTemplateV4Request getInstanceTemplateV4Request() {
+        return template;
+    }
+
+    public void setInstanceTemplateV4Request(InstanceTemplateV4Request template) {
+        this.template = template;
+    }
+}
