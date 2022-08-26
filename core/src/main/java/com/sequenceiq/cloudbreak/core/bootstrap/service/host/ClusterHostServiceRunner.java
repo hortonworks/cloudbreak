56f21ac55a7d6a4630f7d0e67d7b036ae6eb39a2
Dénes Berger <dberger@cloudera.com>
2022-08-26 15:16:19 +0200

    CB-18345 Fix Impala URLs in Knox topology
    Because of the assumption that Impala always have a
    dedicated coordinator, the Impala URLs were replaced with
    the coordinator URLs.
    In reality there are custom templates where no dedicated
    Impala coordinator exists, thus the empty list should not be
    placed as the Impala URL list.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/ClusterHostServiceRunner.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/bootstrap/service/host/ClusterHostServiceRunner.java
@@ -867,7 +867,9 @@ public class ClusterHostServiceRunner {
                     List.copyOf(componentLocation.get(impalaService.getServiceName())));
             Map<String, List<String>> impalaLocations = componentLocator.getImpalaCoordinatorLocations(cluster);
             List<String> locations = impalaLocations.values().stream().flatMap(List::stream).collect(Collectors.toList());
-            componentLocation.replace(impalaService.getServiceName(), locations);
+            if (!locations.isEmpty()) {
+                componentLocation.replace(impalaService.getServiceName(), locations);
+            }
         }
         return componentLocation;
     }
