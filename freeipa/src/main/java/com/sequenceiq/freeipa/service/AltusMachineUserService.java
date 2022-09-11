3784ecfe3ca8b60c44f9a38c8dba2e44f5134909
Oliver Szabo <oleewere@gmail.com>
2022-09-11 16:25:22 +0200

    CB-18516. Optimistic locking failure during dbus/monitoring credential creation.
    details:
    During stack save, optimistic locking error could happen in freeipa during altus credential creation. the change that could be related here is that the stack was passed in lambda functions.
    In order to avoid the occurrence of it put the stack save in transaction (also query. it first inside the transaction).

--- freeipa/src/main/java/com/sequenceiq/freeipa/service/AltusMachineUserService.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/service/AltusMachineUserService.java
@@ -3,6 +3,7 @@ package com.sequenceiq.freeipa.service;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,10 @@ import com.sequenceiq.cloudbreak.auth.altus.model.MachineUserRequest;
 import com.sequenceiq.cloudbreak.auth.altus.service.AltusIAMService;
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
 import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGeneratorFactory;
+import com.sequenceiq.cloudbreak.common.exception.CloudbreakServiceException;
 import com.sequenceiq.cloudbreak.common.json.Json;
+import com.sequenceiq.cloudbreak.common.service.TransactionService;
+import com.sequenceiq.common.api.telemetry.model.CdpCredential;
 import com.sequenceiq.common.api.telemetry.model.DataBusCredential;
 import com.sequenceiq.common.api.telemetry.model.MonitoringCredential;
 import com.sequenceiq.common.api.telemetry.model.Telemetry;
@@ -36,12 +40,15 @@ public class AltusMachineUserService {
 
     private final StackService stackService;
 
+    private final TransactionService transactionService;
+
     private final RegionAwareInternalCrnGeneratorFactory regionAwareInternalCrnGeneratorFactory;
 
-    public AltusMachineUserService(AltusIAMService altusIAMService, StackService stackService,
+    public AltusMachineUserService(AltusIAMService altusIAMService, StackService stackService,  TransactionService transactionService,
             RegionAwareInternalCrnGeneratorFactory regionAwareInternalCrnGeneratorFactory) {
         this.altusIAMService = altusIAMService;
         this.stackService = stackService;
+        this.transactionService = transactionService;
         this.regionAwareInternalCrnGeneratorFactory = regionAwareInternalCrnGeneratorFactory;
     }
 
@@ -194,11 +201,7 @@ public class AltusMachineUserService {
             dataBusCredential.setMachineUserName(getFluentMachineUser(stack));
             dataBusCredential.setAccessKey(altusCredential.get().getAccessKey());
             dataBusCredential.setPrivateKey(altusCredential.get().getPrivateKey() != null ? new String(altusCredential.get().getPrivateKey()) : null);
-            dataBusCredential.setAccessKeyType(cdpAccessKeyType.getValue());
-            String databusCredentialJsonString = new Json(dataBusCredential).getValue();
-            stack.setDatabusCredential(databusCredentialJsonString);
-            stackService.save(stack);
-            return dataBusCredential;
+            return storeCdpCredential(dataBusCredential, stack.getId(), Stack::setDatabusCredential);
         }
         return null;
     }
@@ -208,11 +211,22 @@ public class AltusMachineUserService {
         monitoringCredential.setMachineUserName(getMonitoringMachineUser(stack));
         monitoringCredential.setAccessKey(altusCredential.getAccessKey());
         monitoringCredential.setPrivateKey(altusCredential.getPrivateKey() != null ? new String(altusCredential.getPrivateKey()) : null);
-        monitoringCredential.setAccessKeyType(cdpAccessKeyType.getValue());
-        String monitoringCredentialJsonString = new Json(monitoringCredential).getValue();
-        stack.setMonitoringCredential(monitoringCredentialJsonString);
-        stackService.save(stack);
-        return monitoringCredential;
+        return storeCdpCredential(monitoringCredential, stack.getId(), Stack::setMonitoringCredential);
+    }
+
+    private <C extends CdpCredential> C storeCdpCredential(C credential, Long stackId, BiConsumer<Stack, String> func) {
+        try {
+            transactionService.required(() -> {
+                String jsonString = new Json(credential).getValue();
+                Stack stackToSave = stackService.getStackById(stackId);
+                func.accept(stackToSave, jsonString);
+                stackService.save(stackToSave);
+            });
+            return credential;
+        } catch (TransactionService.TransactionExecutionException e) {
+            LOGGER.warn("Error during saving cdp credential in stack.", e);
+            throw new CloudbreakServiceException(e);
+        }
     }
 
     public boolean isCredentialExists(Telemetry telemetry, String machineUser, String accessKey, Stack stack) {
