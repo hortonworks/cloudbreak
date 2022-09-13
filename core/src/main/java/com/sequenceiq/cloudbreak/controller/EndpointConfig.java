41d0527bb593d65515c32e1799440064b064a9cd
Attila Kanto <attila.kanto@gmail.com>
2022-09-13 17:23:19 +0200

    Add datalake cost calculation into the response

--- core/src/main/java/com/sequenceiq/cloudbreak/controller/EndpointConfig.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/controller/EndpointConfig.java
@@ -43,7 +43,7 @@ import com.sequenceiq.cloudbreak.exception.mapper.DefaultExceptionMapper;
 import com.sequenceiq.cloudbreak.structuredevent.rest.LegacyStructuredEventFilter;
 import com.sequenceiq.cloudbreak.structuredevent.rest.filter.CDPRestAuditFilter;
 import com.sequenceiq.cloudbreak.util.FileReaderUtils;
-import com.sequenceiq.distrox.v1.distrox.controller.DistroXCostV1Controller;
+import com.sequenceiq.cloudbreak.controller.v4.ClusterCostV4Controller;
 import com.sequenceiq.distrox.v1.distrox.controller.DistroXDatabaseServerV1Controller;
 import com.sequenceiq.distrox.v1.distrox.controller.DistroXInternalV1Controller;
 import com.sequenceiq.distrox.v1.distrox.controller.DistroXUpgradeV1Controller;
@@ -95,7 +95,7 @@ public class EndpointConfig extends ResourceConfig {
             DistroXUpgradeV1Controller.class,
             DistroXDatabaseServerV1Controller.class,
             AuthorizationUtilEndpoint.class,
-            DistroXCostV1Controller.class
+            ClusterCostV4Controller.class
     );
 
     @Value("${info.app.version:unspecified}")
