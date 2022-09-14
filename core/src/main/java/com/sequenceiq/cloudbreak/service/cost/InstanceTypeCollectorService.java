0ea33393a57e87cb1b2d4bf3e3c665662ca002b8
lnardai <laci.nardai@gmail.com>
2022-09-14 11:40:32 +0200

    extracted dumb InstanceTypeCollector logic to separate service

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cost/InstanceTypeCollectorService.java
@@ -0,0 +1,30 @@
+package com.sequenceiq.cloudbreak.service.cost;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.repository.StackRepository;
+import com.sequenceiq.cloudbreak.service.stack.InstanceGroupService;
+import com.sequenceiq.cloudbreak.view.InstanceGroupView;
+
+public class InstanceTypeCollectorService {
+
+    @Inject
+    private StackRepository stackRepository;
+
+    @Inject
+    private InstanceGroupService instanceGroupService;
+
+    public Map<String, Long> getAllInstanceTypesByCrn(String crn) {
+        Stack stack = stackRepository.findByResourceCrn(crn).get();
+        //get list of all instancetype
+        List<InstanceGroupView> instanceGroupList = instanceGroupService.getInstanceGroupViewByStackId(stack.getId());
+        Map<String, Long> countByInstanceType = instanceGroupList.stream()
+                .collect(Collectors.groupingBy(ig -> ig.getTemplate().getInstanceType(), Collectors.counting()));
+        return countByInstanceType;
+    }
+}
