a6202d09e33e10846d23d90a09b367547013d926
doktoric <rdoktorics@cloudera.com>
2022-09-14 12:51:47 +0200

    fixing the missing annotation

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cost/InstanceTypeCollectorService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cost/InstanceTypeCollectorService.java
@@ -6,11 +6,14 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Service;
+
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.repository.StackRepository;
 import com.sequenceiq.cloudbreak.service.stack.InstanceGroupService;
 import com.sequenceiq.cloudbreak.view.InstanceGroupView;
 
+@Service
 public class InstanceTypeCollectorService {
 
     @Inject
