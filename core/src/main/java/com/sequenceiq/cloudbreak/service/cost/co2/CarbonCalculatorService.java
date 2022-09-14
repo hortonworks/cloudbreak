7a9eed60a81edc5969d165942a7193ef3d35326e
lnardai <laci.nardai@gmail.com>
2022-09-14 11:40:32 +0200

    extracted dumb InstanceTypeCollector logic to separate service

--- core/src/main/java/com/sequenceiq/cloudbreak/service/cost/co2/CarbonCalculatorService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/cost/co2/CarbonCalculatorService.java
@@ -1,8 +1,6 @@
 package com.sequenceiq.cloudbreak.service.cost.co2;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -10,11 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.sequenceiq.cloudbreak.domain.stack.Stack;
-import com.sequenceiq.cloudbreak.repository.InstanceGroupRepository;
-import com.sequenceiq.cloudbreak.repository.StackRepository;
-import com.sequenceiq.cloudbreak.service.stack.InstanceGroupService;
-import com.sequenceiq.cloudbreak.view.InstanceGroupView;
+import com.sequenceiq.cloudbreak.service.cost.InstanceTypeCollectorService;
 
 // CHECKSTYLE:OFF
 @Service
@@ -25,32 +19,18 @@ public class CarbonCalculatorService {
     private static final double AWS_AVG_VCPU = 2.12;
     //g/kWh
     private static final double CO2_RATE_CALIFORNIA = 347;
-    @Inject
-    private StackRepository stackRepository;
 
     @Inject
-    private InstanceGroupService instanceGroupService;
-
-    @Inject
-    private InstanceGroupRepository instanceGroupRepository;
+    private InstanceTypeCollectorService instanceTypeCollectorService;
 
     public double getHourlyCarbonFootPrintByCrn(String crn) {
         LOGGER.info("Calculating CO2FP for resource: {}", crn);
         //filter nodes that are not in available status
-        Map<String, Long> instanceTypeList = getAllInstanceTypesByCrn(crn);
+        Map<String, Long> instanceTypeList = instanceTypeCollectorService.getAllInstanceTypesByCrn(crn);
         LOGGER.info("Collected instnace types: {}", instanceTypeList);
         double summarizedWhConsumption = calculateCpuInWh() + calculateDiskInWh() + calculateMemoryInWh();
         // get cluster proper region for CO2 rate
         return summarizedWhConsumption * getCo2RateForRegion() / 1000;
-    }
-
-    private Map<String, Long> getAllInstanceTypesByCrn(String crn) {
-        Stack stack = stackRepository.findByResourceCrn(crn).get();
-        //get list of all instancetype
-        List<InstanceGroupView> instanceGroupList = instanceGroupService.getInstanceGroupViewByStackId(stack.getId());
-        Map<String, Long> countByInstanceType = instanceGroupList.stream()
-                .collect(Collectors.groupingBy(ig -> ig.getTemplate().getInstanceType(), Collectors.counting()));
-        return countByInstanceType;
     }
 
     private double calculateCpuInWh() {
--- core/src/test/java/com/sequenceiq/cloudbreak/service/cost/co2/CarbonCalculatorServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/cost/co2/CarbonCalculatorServiceTest.java
@@ -1,16 +1,29 @@
 package com.sequenceiq.cloudbreak.service.cost.co2;
 // CHECKSTYLE:OFF
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.service.cost.InstanceTypeCollectorService;
 
 @ExtendWith(MockitoExtension.class)
 class CarbonCalculatorServiceTest {
 
     private static final String TEST_CRN ="crn:cdp:datahub:us-west-1:e7b1345f-4ae1-4594-9113-fc91f22ef8bd:cluster:c7da2918-dd14-49ed-9b43-33ff55bd6309";
+
+    @Mock
+    private InstanceTypeCollectorService instanceTypeCollectorService;
 
     @InjectMocks
     private CarbonCalculatorService underTest;
@@ -18,6 +31,9 @@ class CarbonCalculatorServiceTest {
     @Test
     void getHourlyCarbonFootPrintByCrn() {
         //TODO: add some usecases
+        Stack dummyStack = new Stack();
+        Map<String, Long> instanceTypeCounts = new HashMap<>();
+        when(instanceTypeCollectorService.getAllInstanceTypesByCrn(any())).thenReturn(instanceTypeCounts);
         assertEquals(17.4888, underTest.getHourlyCarbonFootPrintByCrn(TEST_CRN));
     }
 }
