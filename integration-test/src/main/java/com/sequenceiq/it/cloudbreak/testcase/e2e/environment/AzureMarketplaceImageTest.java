598950f58e3bcfa8c691cbff7ea204abd54b1e5e
Annamaria Szegedi <aszegedi@cloudera.com>
2022-09-29 18:36:11 +0200

    CB-18626 Enable dedicated storage account for azure E2E with SINGLE_WITH_DEDICATED_STORAGE_ACCOUNT resource group usage
    We have implemented a special single resource group usage flag for AZURE: `SINGLE_WITH_DEDICATED_STORAGE_ACCOUNT`. This kind of single resource group usage will reduce the execution time of the tests and might add to stability.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/environment/AzureMarketplaceImageTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/environment/AzureMarketplaceImageTest.java
@@ -8,17 +8,20 @@ import javax.inject.Inject;
 import org.testng.annotations.Test;
 
 import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
+import com.sequenceiq.common.api.type.ServiceEndpointCreation;
 import com.sequenceiq.environment.api.v1.environment.model.response.DetailedEnvironmentResponse;
 import com.sequenceiq.environment.api.v1.environment.model.response.EnvironmentStatus;
 import com.sequenceiq.environment.api.v1.environment.model.response.FreeIpaImageResponse;
 import com.sequenceiq.it.TestParameter;
 import com.sequenceiq.it.cloudbreak.EnvironmentClient;
+import com.sequenceiq.it.cloudbreak.ResourceGroupTest;
 import com.sequenceiq.it.cloudbreak.assertion.Assertion;
 import com.sequenceiq.it.cloudbreak.client.CredentialTestClient;
 import com.sequenceiq.it.cloudbreak.client.EnvironmentTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.credential.CredentialTestDto;
+import com.sequenceiq.it.cloudbreak.dto.environment.EnvironmentNetworkTestDto;
 import com.sequenceiq.it.cloudbreak.dto.environment.EnvironmentTestDto;
 import com.sequenceiq.it.cloudbreak.dto.telemetry.TelemetryTestDto;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.AbstractE2ETest;
@@ -50,14 +53,17 @@ public class AzureMarketplaceImageTest extends AbstractE2ETest {
                 .given(CredentialTestDto.class)
                 .when(credentialTestClient.create())
                 .given("telemetry", TelemetryTestDto.class)
-                .withLogging()
-                .withReportClusterLogs()
+                    .withLogging()
+                    .withReportClusterLogs()
+                .given(EnvironmentNetworkTestDto.class)
+                    .withServiceEndpoints(ServiceEndpointCreation.DISABLED)
                 .given(EnvironmentTestDto.class)
-                .withNetwork()
-                .withTelemetry("telemetry")
-                .withCreateFreeIpa(Boolean.TRUE)
-                .withOneFreeIpaNode()
-                .withMarketplaceFreeIpaImage()
+                    .withNetwork()
+                    .withTelemetry("telemetry")
+                    .withCreateFreeIpa(Boolean.TRUE)
+                    .withOneFreeIpaNode()
+                    .withMarketplaceFreeIpaImage()
+                    .withResourceGroup(ResourceGroupTest.AZURE_RESOURCE_GROUP_USAGE_MULTIPLE, "")
                 .when(environmentTestClient.create())
                 .await(EnvironmentStatus.AVAILABLE)
                 .then((tc, testDto, cc) -> environmentTestClient.describe().action(tc, testDto, cc))
