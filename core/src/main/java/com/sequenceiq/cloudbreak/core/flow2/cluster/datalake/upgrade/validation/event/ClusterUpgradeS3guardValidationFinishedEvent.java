bede78e4f542dd8c0252be533bd90a1ed6913842
vivprabu <113479354+vivprabu@users.noreply.github.com>
2022-11-02 15:25:13 -0400

    CB-18823 - final tested commit

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/datalake/upgrade/validation/event/ClusterUpgradeS3guardValidationFinishedEvent.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/datalake/upgrade/validation/event/ClusterUpgradeS3guardValidationFinishedEvent.java
@@ -1,9 +1,13 @@
 package com.sequenceiq.cloudbreak.core.flow2.cluster.datalake.upgrade.validation.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sequenceiq.cloudbreak.reactor.api.event.StackEvent;
 
 public class ClusterUpgradeS3guardValidationFinishedEvent  extends StackEvent {
-    public ClusterUpgradeS3guardValidationFinishedEvent(String selector, Long stackId) {
+    @JsonCreator
+    public ClusterUpgradeS3guardValidationFinishedEvent(@JsonProperty("selector") String selector,
+            @JsonProperty("resourceId") Long stackId) {
         super(selector, stackId);
     }
 }
