8bafe9882b0aab7be22cfa027ebf9c769527d483
Aakash Shankar <aakash.shankar@cloudera.com>
2022-11-04 22:43:15 +0530

    CB-19394 Set fs.azure.enable.readhead to false in core_site_safety_valve for runtimes >= 7.2.12

--- template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hdfs/HdfsConfigProviderTest.java
+++ template-manager-cmtemplate/src/test/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/hdfs/HdfsConfigProviderTest.java
@@ -1,8 +1,12 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.hdfs;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -21,6 +25,7 @@ import com.sequenceiq.cloudbreak.TestUtil;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
 import com.sequenceiq.cloudbreak.cloud.model.ClouderaManagerRepo;
 import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.cmtemplate.configproviders.adls.AdlsGen2ConfigProvider;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.s3.S3ConfigProvider;
 import com.sequenceiq.cloudbreak.domain.StorageLocation;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.gateway.Gateway;
@@ -28,13 +33,13 @@ import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
 import com.sequenceiq.cloudbreak.template.TemplatePreparationObject.Builder;
 import com.sequenceiq.cloudbreak.template.filesystem.BaseFileSystemConfigurationsView;
 import com.sequenceiq.cloudbreak.template.filesystem.StorageLocationView;
-import com.sequenceiq.cloudbreak.template.filesystem.adls.AdlsFileSystemConfigurationsView;
+import com.sequenceiq.cloudbreak.template.filesystem.adlsgen2.AdlsGen2FileSystemConfigurationsView;
 import com.sequenceiq.cloudbreak.template.filesystem.s3.S3FileSystemConfigurationsView;
 import com.sequenceiq.cloudbreak.template.views.ExposedServiceUtil;
 import com.sequenceiq.cloudbreak.template.views.HostgroupView;
 import com.sequenceiq.cloudbreak.template.views.PlacementView;
 import com.sequenceiq.cloudbreak.util.FileReaderUtils;
-import com.sequenceiq.common.api.filesystem.AdlsFileSystem;
+import com.sequenceiq.common.api.filesystem.AdlsGen2FileSystem;
 import com.sequenceiq.common.api.filesystem.S3FileSystem;
 import com.sequenceiq.common.api.type.InstanceGroupType;
 
@@ -46,6 +51,9 @@ public class HdfsConfigProviderTest {
 
     @Mock
     private S3ConfigProvider s3ConfigProvider;
+
+    @Mock
+    private AdlsGen2ConfigProvider adlsConfigProvider;
 
     @Test
     public void testGetHdfsServiceConfigsWithoutS3Guard() {
@@ -73,6 +81,21 @@ public class HdfsConfigProviderTest {
         assertEquals(0, serviceConfigs.size());
     }
 
+    @Test
+    public void testGetHdfsServiceConfigsWithAdlsGen2FileSystem() {
+        doNothing().when(adlsConfigProvider).populateServiceConfigs(any(TemplatePreparationObject.class), any(StringBuilder.class), anyString());
+
+        TemplatePreparationObject preparationObject = getTemplatePreparationObject(false, false, false);
+        String inputJson = getBlueprintText("input/clouderamanager.bp");
+        CmTemplateProcessor cmTemplateProcessor = new CmTemplateProcessor(inputJson);
+
+        List<ApiClusterTemplateConfig> serviceConfigs = underTest.getServiceConfigs(cmTemplateProcessor, preparationObject);
+
+        assertThat(serviceConfigs).isEmpty();
+        verify(adlsConfigProvider, times(1)).populateServiceConfigs(any(TemplatePreparationObject.class), any(StringBuilder.class),
+                anyString());
+    }
+
     private TemplatePreparationObject getTemplatePreparationObject(boolean useS3FileSystem, boolean fillDynamoTableName, boolean includeLocations) {
         HostgroupView master = new HostgroupView("master", 1, InstanceGroupType.GATEWAY, 1);
         HostgroupView worker = new HostgroupView("worker", 2, InstanceGroupType.CORE, 2);
@@ -94,7 +117,7 @@ public class HdfsConfigProviderTest {
                     new S3FileSystemConfigurationsView(s3FileSystem, locations, false);
         } else {
             fileSystemConfigurationsView =
-                    new AdlsFileSystemConfigurationsView(new AdlsFileSystem(), locations, false);
+                    new AdlsGen2FileSystemConfigurationsView(new AdlsGen2FileSystem(), locations, false);
         }
 
         Gateway gateway = TestUtil.gatewayEnabledWithExposedKnoxServices(ExposedServiceUtil.exposedService("NAMENODE").getKnoxService());
