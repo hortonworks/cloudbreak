759091c46688965617966d6ffef5ad75923f5c44
SubhanHakverdiyev <subhan.hakverdiyev@gmail.com>
2022-11-09 14:14:23 +0100

    CB-19554 Fixed mapping error when tag is not well formatted. Now it throws 400 instead of 500 internal server error.

---
+++ redbeams/src/main/java/com/sequenceiq/redbeams/controller/mapper/IllegalArgumentExceptionMapper.java
@@ -0,0 +1,21 @@
+package com.sequenceiq.redbeams.controller.mapper;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.exception.mapper.BaseExceptionMapper;
+
+@Component
+public class IllegalArgumentExceptionMapper extends BaseExceptionMapper<IllegalArgumentException> {
+
+    @Override
+    public Status getResponseStatus(IllegalArgumentException exception) {
+        return Status.BAD_REQUEST;
+    }
+
+    @Override
+    public Class<IllegalArgumentException> getExceptionType() {
+        return IllegalArgumentException.class;
+    }
+}
