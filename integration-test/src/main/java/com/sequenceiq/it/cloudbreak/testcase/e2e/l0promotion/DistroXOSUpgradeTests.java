2f605983f5589337ff3200767e41bddc280622b4
Annamaria Szegedi <aszegedi@cloudera.com>
2022-11-17 14:00:36 +0100

    Rewind changes

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/l0promotion/DistroXOSUpgradeTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/l0promotion/DistroXOSUpgradeTests.java
@@ -4,8 +4,10 @@ import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
 
 import javax.inject.Inject;
 
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import com.sequenceiq.distrox.api.v1.distrox.model.upgrade.DistroXUpgradeReplaceVms;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.Status;
 import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.client.FreeIpaTestClient;
@@ -13,6 +15,8 @@ import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
 import com.sequenceiq.it.cloudbreak.cloud.v4.CommonClusterManagerProperties;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.DistroXUpgradeTestDto;
 import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaTestDto;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxTestDto;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxUpgradeTestDto;
@@ -43,8 +47,8 @@ public class DistroXOSUpgradeTests extends AbstractE2ETest {
         createEnvironmentWithFreeIpa(testContext);
     }
 
-//    @Ignore("This test case should be re-enabled in case of OPSAPS-62124 has been resolved at MOW-Dev")
-    @Test(dataProvider = TEST_CONTEXT, description = "We need to wait the OPSAPS-62124 to be resolved")
+    @Ignore("This test case should be re-enabled in case of CB-14877 has been resolved at MOW-Dev")
+    @Test(dataProvider = TEST_CONTEXT, description = "We need to wait the CB-14877 to be resolved")
     @Description(
             given = "there is a running environment with freeIPA in available state",
             when = "SDX (light duty) and base DistroX (5 nodes with master, compute and workers) should be created successfully",
@@ -52,7 +56,7 @@ public class DistroXOSUpgradeTests extends AbstractE2ETest {
             then = "DistroX upgrade should be successful, the cluster should be up and running")
     public void testBaseDistroXOSUpgrade(TestContext testContext) {
         String sdxName = resourcePropertyProvider().getName();
-//        String distroXName = resourcePropertyProvider().getName();
+        String distroXName = resourcePropertyProvider().getName();
         String currentRuntimeVersion = commonClusterManagerProperties.getUpgrade().getDistroXUpgradeCurrentVersion();
         String targetRuntimeVersion = commonClusterManagerProperties.getUpgrade().getDistroXUpgradeTargetVersion();
 
@@ -66,19 +70,19 @@ public class DistroXOSUpgradeTests extends AbstractE2ETest {
                 .awaitForEntitlementChange(10)
                 .validate();
 
-//        testContext
-//                .given(distroXName, DistroXTestDto.class)
-//                    .withTemplate(String.format(commonClusterManagerProperties.getInternalDistroXBlueprintType(), currentRuntimeVersion))
-//                .when(distroXTestClient.create(), key(distroXName))
-//                .await(STACK_AVAILABLE, key(distroXName))
-//                .awaitForHealthyInstances()
-//                .validate();
-//
-//        testContext
-//                .given(distroXName, DistroXTestDto.class)
-//                .when(distroXTestClient.stop(), key(distroXName))
-//                .await(STACK_STOPPED, key(distroXName))
-//                .validate();
+        testContext
+                .given(distroXName, DistroXTestDto.class)
+                    .withTemplate(String.format(commonClusterManagerProperties.getInternalDistroXBlueprintType(), currentRuntimeVersion))
+                .when(distroXTestClient.create(), key(distroXName))
+                .await(STACK_AVAILABLE, key(distroXName))
+                .awaitForHealthyInstances()
+                .validate();
+
+        testContext
+                .given(distroXName, DistroXTestDto.class)
+                .when(distroXTestClient.stop(), key(distroXName))
+                .await(STACK_STOPPED, key(distroXName))
+                .validate();
         testContext
                 .given(SdxUpgradeTestDto.class)
                     .withReplaceVms(SdxUpgradeReplaceVms.ENABLED)
@@ -91,29 +95,29 @@ public class DistroXOSUpgradeTests extends AbstractE2ETest {
                 .awaitForHealthyInstances()
                 .validate();
 
-//        testContext
-//                .given(distroXName, DistroXTestDto.class)
-//                .when(distroXTestClient.start(), key(distroXName))
-//                .await(STACK_AVAILABLE, key(distroXName))
-//                .validate();
-//        testContext
-//                .given(DistroXUpgradeTestDto.class)
-//                    .withRuntime(targetRuntimeVersion)
-//                .given(distroXName, DistroXTestDto.class)
-//                .when(distroXTestClient.upgrade(), key(distroXName))
-//                .await(STACK_AVAILABLE, key(distroXName))
-//                .awaitForHealthyInstances()
-//                .validate();
-//        testContext
-//                .given(DistroXUpgradeTestDto.class)
-//                    .withLockComponents(Boolean.TRUE)
-//                    .withRuntime(null)
-//                    .withReplaceVms(DistroXUpgradeReplaceVms.ENABLED)
-//                .given(distroXName, DistroXTestDto.class)
-//                .when(distroXTestClient.upgrade(), key(distroXName))
-//                .await(STACK_AVAILABLE, key(distroXName))
-//                .awaitForHealthyInstances()
-//                .validate();
+        testContext
+                .given(distroXName, DistroXTestDto.class)
+                .when(distroXTestClient.start(), key(distroXName))
+                .await(STACK_AVAILABLE, key(distroXName))
+                .validate();
+        testContext
+                .given(DistroXUpgradeTestDto.class)
+                    .withRuntime(targetRuntimeVersion)
+                .given(distroXName, DistroXTestDto.class)
+                .when(distroXTestClient.upgrade(), key(distroXName))
+                .await(STACK_AVAILABLE, key(distroXName))
+                .awaitForHealthyInstances()
+                .validate();
+        testContext
+                .given(DistroXUpgradeTestDto.class)
+                    .withLockComponents(Boolean.TRUE)
+                    .withRuntime(null)
+                    .withReplaceVms(DistroXUpgradeReplaceVms.ENABLED)
+                .given(distroXName, DistroXTestDto.class)
+                .when(distroXTestClient.upgrade(), key(distroXName))
+                .await(STACK_AVAILABLE, key(distroXName))
+                .awaitForHealthyInstances()
+                .validate();
 
         testContext
                 .given(FreeIpaTestDto.class)
@@ -127,11 +131,11 @@ public class DistroXOSUpgradeTests extends AbstractE2ETest {
                 .await(SdxClusterStatusResponse.RUNNING, key(sdxName))
                 .awaitForHealthyInstances()
                 .validate();
-//        testContext
-//                .given(distroXName, DistroXTestDto.class)
-//                .when(distroXTestClient.get(), key(distroXName))
-//                .await(STACK_AVAILABLE, key(distroXName))
-//                .awaitForHealthyInstances()
-//                .validate();
+        testContext
+                .given(distroXName, DistroXTestDto.class)
+                .when(distroXTestClient.get(), key(distroXName))
+                .await(STACK_AVAILABLE, key(distroXName))
+                .awaitForHealthyInstances()
+                .validate();
     }
 }
