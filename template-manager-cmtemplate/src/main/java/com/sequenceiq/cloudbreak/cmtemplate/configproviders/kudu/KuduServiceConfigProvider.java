86fdc8ea397a401e6a36a900a0d3b56f5ddf51b1
Attila Bukor <abukor@apache.org>
2022-11-24 17:16:48 +0100

    CB-18996 Add Kudu config provider for Ranger
    022d165 fixed a bug with Kudu's Ranger policies, but at around the same
    time, the blueprint for 7.2.12 was created. Unfortunately, this change
    wasn't copied over, so this doesn't work in newer CDP versions.
    
    Additionally, the fix was done only in the blueprint level, so Ranger
    integration wouldn't work with custom blueprints uploaded by users,
    unless they copied over this change as well.
    
    This commit removes this extra config from the RTDM blueprints and
    adds it in a ConfigProvider instead that applies to versions 7.2.11 and
    above.

---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/kudu/KuduServiceConfigProvider.java
@@ -0,0 +1,42 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.kudu;
+
+import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.CLOUDERA_STACK_VERSION_7_2_11;
+import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.isVersionNewerOrEqualThanLimited;
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
+import com.google.api.client.util.Lists;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateComponentConfigProvider;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
+
+@Component
+public class KuduServiceConfigProvider implements CmTemplateComponentConfigProvider {
+
+    @Override
+    public List<ApiClusterTemplateConfig> getServiceConfigs(CmTemplateProcessor templateProcessor, TemplatePreparationObject source) {
+        ArrayList<ApiClusterTemplateConfig> configs = Lists.newArrayList();
+
+        String cdhVersion = templateProcessor.getStackVersion();
+        if (isVersionNewerOrEqualThanLimited(cdhVersion, CLOUDERA_STACK_VERSION_7_2_11)) {
+            configs.add(config(KuduConfigs.RANGER_KUDU_PLUGIN_SERVICE_NAME, KuduConfigs.GENERATED_RANGER_SERVICE_NAME));
+        }
+
+        return configs;
+    }
+
+    @Override
+    public String getServiceType() {
+        return KuduRoles.KUDU_SERVICE;
+    }
+
+    @Override
+    public List<String> getRoleTypes() {
+        return List.of(KuduRoles.KUDU_MASTER);
+    }
+}
