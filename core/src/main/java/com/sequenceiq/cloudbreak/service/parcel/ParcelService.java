12e9897bb00e7dc782dcc2f3d71916fe60b0deec
Adarsh Sharma <adsharma@cloudera.com>
2022-12-20 14:45:23 -0500

    CB-18161 Parcel Availability Checker should retry failed requests During upgrade, We have seen cases where Parcel availability check fails but later it is successful. To reduce the failures, we are adding retry policy to check parcel availability.

--- core/src/main/java/com/sequenceiq/cloudbreak/service/parcel/ParcelService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/parcel/ParcelService.java
@@ -3,17 +3,29 @@ package com.sequenceiq.cloudbreak.service.parcel;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetrySynchronizationManager;
 import org.springframework.stereotype.Service;
 
+import com.sequenceiq.cloudbreak.auth.PaywallCredentialPopulator;
+import com.sequenceiq.cloudbreak.client.RestClientFactory;
 import com.sequenceiq.cloudbreak.cloud.model.ClouderaManagerProduct;
 import com.sequenceiq.cloudbreak.cloud.model.catalog.Image;
 import com.sequenceiq.cloudbreak.cloud.model.component.StackType;
@@ -33,9 +45,16 @@ import com.sequenceiq.cloudbreak.view.ClusterView;
 import com.sequenceiq.cloudbreak.view.StackView;
 
 @Service
+@EnableRetry
 public class ParcelService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ParcelService.class);
+
+    @Value("#{'${cb.http.retryableStatusCodes:}'.split(',')}")
+    private List<Integer> retryableHttpCodes;
+
+    @Value("${cb.parcel.retry.maxAttempts:5}")
+    private Integer retryMaxAttempts;
 
     @Inject
     private ClusterComponentConfigProvider clusterComponentConfigProvider;
@@ -54,6 +73,12 @@ public class ParcelService {
 
     @Inject
     private ImageReaderService imageReaderService;
+
+    @Inject
+    private RestClientFactory restClientFactory;
+
+    @Inject
+    private PaywallCredentialPopulator paywallCredentialPopulator;
 
     public Set<ClusterComponentView> getParcelComponentsByBlueprint(StackDtoDelegate stack) {
         return getParcelComponentsByBlueprint(stack.getStack(), stack.getCluster(), stack.getBlueprint());
@@ -156,5 +181,28 @@ public class ParcelService {
                 .map(cmp -> cmProductMap.get(cmp.getName()))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toSet());
+    }
+
+    @Retryable(value = Exception.class, maxAttemptsExpression = "${cb.parcel.retry.maxAttempts:5}",
+            backoff = @Backoff(delayExpression = "${cb.parcel.retry.backOffDelay:2000}",
+                    multiplierExpression = "${cb.parcel.retry.backOffMultiplier:2}"))
+    public Optional<Response> getHeadResponseForParcel(String url) {
+        Client client = restClientFactory.getOrCreateDefault();
+        WebTarget target = client.target(url);
+        paywallCredentialPopulator.populateWebTarget(url, target);
+        Response response = target.request().head();
+        LOGGER.info("Head request for {} status: {}", url, response.getStatus());
+        if (retryableHttpCodes.contains(response.getStatus())) {
+            if (RetrySynchronizationManager.getContext().getRetryCount() < retryMaxAttempts - 1) {
+                LOGGER.info("Retry for Http Status {}", response.getStatus());
+                throw new RuntimeException(String.format("Got Http Status %s", response.getStatus()));
+            } else {
+                /**
+                 * This is to ensure that Http code is returned instead of throwing exception when retry is exhausted.
+                 */
+                LOGGER.info("Retry exhausted");
+            }
+        }
+        return Optional.of(response);
     }
 }
