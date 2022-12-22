65a0d40e9315c1e9984d92bd2e3f42dbb774548d
Peter Erdos <perdos@cloudera.com>
2022-12-22 09:39:17 +0100

    CB-17146 redeployGatewayCertificate should init pillars

--- datalake/src/main/java/com/sequenceiq/datalake/service/sdx/cert/CertRenewalService.java
+++ datalake/src/main/java/com/sequenceiq/datalake/service/sdx/cert/CertRenewalService.java
@@ -4,17 +4,22 @@ import static com.sequenceiq.datalake.service.sdx.SdxService.WORKSPACE_ID_DEFAUL
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 
 import javax.inject.Inject;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.WebApplicationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.StackV4Endpoint;
 import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
 import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGeneratorFactory;
+import com.sequenceiq.cloudbreak.common.exception.BadRequestException;
+import com.sequenceiq.cloudbreak.common.exception.ExceptionResponse;
 import com.sequenceiq.cloudbreak.common.exception.WebApplicationExceptionMessageExtractor;
 import com.sequenceiq.cloudbreak.common.service.TransactionService;
 import com.sequenceiq.cloudbreak.common.service.TransactionService.TransactionExecutionException;
-import com.sequenceiq.cloudbreak.common.service.TransactionService.TransactionRuntimeExecutionException;
 import com.sequenceiq.cloudbreak.event.ResourceEvent;
 import com.sequenceiq.cloudbreak.logger.MDCBuilder;
 import com.sequenceiq.datalake.entity.DatalakeStatusEnum;
@@ -30,6 +35,8 @@ import com.sequenceiq.flow.api.model.FlowIdentifier;
 
 @Service
 public class CertRenewalService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CertRenewalService.class);
 
     @Inject
     private SdxReactorFlowManager sdxReactorFlowManager;
@@ -72,24 +79,42 @@ public class CertRenewalService {
     public void renewCertificate(SdxCluster sdxCluster, String userCrn) {
         try {
             transactionService.required(() -> {
-                FlowIdentifier flowIdentifier = ThreadBasedUserCrnProvider.doAsInternalActor(
-                        regionAwareInternalCrnGeneratorFactory.iam().getInternalCrnForServiceAsString(),
-                        () -> stackV4Endpoint.renewCertificate(WORKSPACE_ID_DEFAULT, sdxCluster.getClusterName(), userCrn));
-                saveChainIdAndStatus(sdxCluster, flowIdentifier);
+                try {
+                    FlowIdentifier flowIdentifier = ThreadBasedUserCrnProvider.doAsInternalActor(
+                            regionAwareInternalCrnGeneratorFactory.iam().getInternalCrnForServiceAsString(),
+                            () -> stackV4Endpoint.renewCertificate(WORKSPACE_ID_DEFAULT, sdxCluster.getClusterName(), userCrn));
+                    saveChainIdAndStatus(sdxCluster, flowIdentifier);
+                } catch (ClientErrorException e) {
+                    try {
+                        throw new BadRequestException("Renew certificate failed: " + e.getResponse().readEntity(ExceptionResponse.class).getMessage(), e);
+                    } catch (ProcessingException processingException) {
+                        LOGGER.warn("Can't read response", processingException);
+                        throw e;
+                    }
+                }
             });
         } catch (TransactionExecutionException e) {
-            throw new TransactionRuntimeExecutionException(e);
+            throw e.getCause();
         }
     }
 
     public void renewInternalCertificate(SdxCluster sdxCluster) {
         try {
-            FlowIdentifier flowIdentifier = ThreadBasedUserCrnProvider.doAsInternalActor(
-                    regionAwareInternalCrnGeneratorFactory.iam().getInternalCrnForServiceAsString(),
-                    () -> stackV4Endpoint.renewInternalCertificate(WORKSPACE_ID_DEFAULT, sdxCluster.getStackCrn()));
-            transactionService.required(() -> saveChainIdAndStatus(sdxCluster, flowIdentifier));
+            try {
+                FlowIdentifier flowIdentifier = ThreadBasedUserCrnProvider.doAsInternalActor(
+                        regionAwareInternalCrnGeneratorFactory.iam().getInternalCrnForServiceAsString(),
+                        () -> stackV4Endpoint.renewInternalCertificate(WORKSPACE_ID_DEFAULT, sdxCluster.getStackCrn()));
+                transactionService.required(() -> saveChainIdAndStatus(sdxCluster, flowIdentifier));
+            } catch (ClientErrorException e) {
+                try {
+                    throw new BadRequestException("Renew internal certificate failed: " + e.getResponse().readEntity(ExceptionResponse.class).getMessage(), e);
+                } catch (ProcessingException processingException) {
+                    LOGGER.warn("Can't read response", processingException);
+                    throw e;
+                }
+            }
         } catch (TransactionExecutionException e) {
-            throw new TransactionRuntimeExecutionException(e);
+            throw e.getCause();
         } catch (WebApplicationException e) {
             String errorMessage = webApplicationExceptionMessageExtractor.getErrorMessage(e);
             throw new RuntimeException(errorMessage);
