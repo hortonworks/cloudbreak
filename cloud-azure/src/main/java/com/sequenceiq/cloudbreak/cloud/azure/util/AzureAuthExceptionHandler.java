4d819e380633a4f176edf4927e37f2632dc9c147
Ferenc Schneider <fschneider@cloudera.com>
2023-01-10 12:21:39 +0100

    CB-18306 Azure error messages are quite ugly due to line breaks and json format
    * AzureExceptionExtractor improvement: extract error and error_uri json parameters from the azure
    exception message next to the error_description.
    * AzureAuthExceptionHandler will throw new ProviderAuthenticationFailedException with the extracted
    error message. No exception chain will be used, as the exception mapper send the root exception's
    message via the API
    * AzureCredentialConnector.verify will throw new CloudConnectorException with the extracted error
    message as well
    * The original exception will be logged before the extraction

--- cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/util/AzureAuthExceptionHandler.java
+++ cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/util/AzureAuthExceptionHandler.java
@@ -2,7 +2,11 @@ package com.sequenceiq.cloudbreak.cloud.azure.util;
 
 import java.util.function.Supplier;
 
+import javax.inject.Inject;
+
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.microsoft.aad.adal4j.AuthenticationException;
@@ -10,13 +14,18 @@ import com.sequenceiq.cloudbreak.client.ProviderAuthenticationFailedException;
 
 @Component
 public class AzureAuthExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureAuthExceptionHandler.class);
+
+    @Inject
+    private AzureExceptionExtractor azureExceptionExtractor;
 
     public <T> T handleAuthException(Supplier<T> function) {
         try {
             return function.get();
         } catch (RuntimeException e) {
             if (ExceptionUtils.indexOfThrowable(e, AuthenticationException.class) != -1) {
-                throw new ProviderAuthenticationFailedException(e);
+                LOGGER.warn("AuthenticationException has thrown during azure operation", e);
+                throw new ProviderAuthenticationFailedException(azureExceptionExtractor.extractErrorMessage(e));
             } else {
                 throw e;
             }
@@ -28,7 +37,8 @@ public class AzureAuthExceptionHandler {
             function.run();
         } catch (RuntimeException e) {
             if (ExceptionUtils.indexOfThrowable(e, AuthenticationException.class) != -1) {
-                throw new ProviderAuthenticationFailedException(e);
+                LOGGER.warn("AuthenticationException has thrown during azure operation", e);
+                throw new ProviderAuthenticationFailedException(azureExceptionExtractor.extractErrorMessage(e));
             } else {
                 throw e;
             }
