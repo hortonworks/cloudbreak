b48c989ecfb747a7f0f03017080d8cd571694d00
David Szabo <dszabo@cloudera.com>
2023-03-09 09:27:08 +0100

    CB-19604 E2E Marketplace image tests for datalake and datahub
    Added new test classes for Azure marketplace image tests for
    data lakes and data hubs. Added a setup function to set the
    automatic terms acceptance setting to true before marketplace
    tests. Fixed the marketplace image terms signer request.

--- environment-api/src/main/java/com/sequenceiq/environment/client/EnvironmentClient.java
+++ environment-api/src/main/java/com/sequenceiq/environment/client/EnvironmentClient.java
@@ -5,6 +5,7 @@ import com.sequenceiq.cloudbreak.structuredevent.rest.endpoint.CDPStructuredEven
 import com.sequenceiq.environment.api.v1.credential.endpoint.AuditCredentialEndpoint;
 import com.sequenceiq.environment.api.v1.credential.endpoint.CredentialEndpoint;
 import com.sequenceiq.environment.api.v1.environment.endpoint.EnvironmentEndpoint;
+import com.sequenceiq.environment.api.v1.marketplace.endpoint.AzureMarketplaceTermsEndpoint;
 import com.sequenceiq.environment.api.v1.proxy.endpoint.ProxyEndpoint;
 import com.sequenceiq.flow.api.FlowEndpoint;
 import com.sequenceiq.flow.api.FlowPublicEndpoint;
@@ -26,4 +27,5 @@ public interface EnvironmentClient {
 
     AuthorizationUtilEndpoint authorizationUtilEndpoint();
 
+    AzureMarketplaceTermsEndpoint azureMarketplaceTermsEndpoint();
 }
