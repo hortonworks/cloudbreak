5f53ba496b39f19c2c650913d8818c2870e2b685
Aakash Shankar <aakash.shankar@cloudera.com>
2023-03-15 20:25:28 +0530

    CB-20723 Conditionally invoke IPA user sync based on CB version associated with cluster, when initializing autoscale machine user  - Added cached API to fetch CB version stored in cluster salt state component Testing  - UTs for new components  - Verified behaviour with real DH cluster

---
+++ autoscale/src/main/java/com/sequenceiq/periscope/service/CloudbreakVersionService.java
@@ -0,0 +1,27 @@
+package com.sequenceiq.periscope.service;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.sequenceiq.periscope.cache.CloudbreakVersionCache;
+import com.sequenceiq.periscope.monitor.handler.CloudbreakCommunicator;
+
+@Service
+public class CloudbreakVersionService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CloudbreakVersionService.class);
+
+    @Inject
+    private CloudbreakCommunicator cloudbreakCommunicator;
+
+    @Cacheable(cacheNames = CloudbreakVersionCache.CB_VERSION_CACHE, key = "{ #stackCrn }")
+    public String getCloudbreakSaltStateVersionByStackCrn(String stackCrn) {
+        LOGGER.info("Fetching cloudbreak version info");
+        return cloudbreakCommunicator.getAutoscaleClusterByCrn(stackCrn).getSaltCbVersion();
+    }
+
+}
--- core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/stacks/StackToAutoscaleStackV4ResponseConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/stacks/StackToAutoscaleStackV4ResponseConverter.java
@@ -1,13 +1,19 @@
 package com.sequenceiq.cloudbreak.converter.v4.stacks;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.AutoscaleStackV4Response;
+import com.sequenceiq.cloudbreak.cluster.service.ClusterComponentConfigProvider;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
 import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 
 @Component
 public class StackToAutoscaleStackV4ResponseConverter {
+
+    @Inject
+    private ClusterComponentConfigProvider componentConfigProvider;
 
     public AutoscaleStackV4Response convert(Stack source) {
         AutoscaleStackV4Response result = new AutoscaleStackV4Response();
@@ -25,6 +31,7 @@ public class StackToAutoscaleStackV4ResponseConverter {
         result.setUserCrn(source.getCreator().getUserCrn());
         result.setStackType(source.getType());
         result.setEnvironmentCrn(source.getEnvironmentCrn());
+        result.setSaltCbVersion(componentConfigProvider.getSaltStateComponentCbVersion(source.getClusterId()));
 
         if (source.getCluster() != null) {
             Cluster cluster = source.getCluster();
