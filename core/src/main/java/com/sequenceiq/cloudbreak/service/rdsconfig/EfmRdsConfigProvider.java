f8133467dfe5f9ac890f671350c0ce5e0273b825
Csaba Bejan <bejan.csaba@gmail.com>
2023-03-20 21:22:48 +0100

    CB-21191 Add DB for EFM

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/rdsconfig/EfmRdsConfigProvider.java
@@ -0,0 +1,64 @@
+package com.sequenceiq.cloudbreak.service.rdsconfig;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessor;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateProcessorFactory;
+import com.sequenceiq.cloudbreak.domain.Blueprint;
+
+@Component
+public class EfmRdsConfigProvider extends AbstractRdsConfigProvider {
+    private static final String PILLAR_KEY = "efm";
+
+    @Value("${cb.efm.database.user:efm}")
+    private String efmDbUser;
+
+    @Value("${cb.efm.database.db:efm}")
+    private String efmDb;
+
+    @Value("${cb.efm.database.port:5432}")
+    private String efmDbPort;
+
+    @Inject
+    private CmTemplateProcessorFactory cmTemplateProcessorFactory;
+
+    private boolean isRdsConfigNeedForEfm(Blueprint blueprint) {
+        String blueprintText = blueprint.getBlueprintText();
+        CmTemplateProcessor cmTemplateProcessor = cmTemplateProcessorFactory.get(blueprintText);
+        return cmTemplateProcessor.isCMComponentExistsInBlueprint("EFM_SERVER");
+    }
+
+    @Override
+    protected String getDbUser() {
+        return efmDbUser;
+    }
+
+    @Override
+    protected String getDb() {
+        return efmDb;
+    }
+
+    @Override
+    protected String getDbPort() {
+        return efmDbPort;
+    }
+
+    @Override
+    protected String getPillarKey() {
+        return PILLAR_KEY;
+    }
+
+    @Override
+    protected DatabaseType getRdsType() {
+        return DatabaseType.EFM;
+    }
+
+    @Override
+    protected boolean isRdsConfigNeeded(Blueprint blueprint, boolean hasGateway) {
+        return isRdsConfigNeedForEfm(blueprint);
+    }
+}
---
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/efm/EfmRoleConfigProvider.java
@@ -0,0 +1,56 @@
+package com.sequenceiq.cloudbreak.cmtemplate.configproviders.efm;
+
+import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.ConfigUtils.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.cloudera.api.swagger.model.ApiClusterTemplateConfig;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRdsRoleConfigProvider;
+import com.sequenceiq.cloudbreak.template.TemplatePreparationObject;
+import com.sequenceiq.cloudbreak.template.views.RdsView;
+
+@Component
+public class EfmRoleConfigProvider extends AbstractRdsRoleConfigProvider {
+
+    static final String DATABASE_USER = "efm.db.username";
+
+    static final String DATABASE_PASSWORD = "efm.db.password";
+
+    static final String DATABASE_URL = "efm.db.url";
+
+    static final String DRIVER_CLASS = "efm.db.driverClass";
+
+    @Override
+    protected List<ApiClusterTemplateConfig> getRoleConfigs(String roleType, TemplatePreparationObject source) {
+        RdsView efmRdsView = getRdsView(source);
+        List<ApiClusterTemplateConfig> configs = new ArrayList<>();
+
+        if (EfmRoles.EFM_SERVER.equals(roleType)) {
+            configs.add(config(DATABASE_URL, efmRdsView.getConnectionURL()));
+            configs.add(config(DRIVER_CLASS, efmRdsView.getConnectionDriver()));
+            configs.add(config(DATABASE_USER, efmRdsView.getConnectionUserName()));
+            configs.add(config(DATABASE_PASSWORD, efmRdsView.getConnectionPassword()));
+        }
+
+        return configs;
+    }
+
+    @Override
+    public String getServiceType() {
+        return EfmRoles.EFM;
+    }
+
+    @Override
+    public List<String> getRoleTypes() {
+        return List.of(EfmRoles.EFM_SERVER);
+    }
+
+    @Override
+    protected DatabaseType dbType() {
+        return DatabaseType.EFM;
+    }
+}
