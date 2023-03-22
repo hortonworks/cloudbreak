df36e8d7069f994ef0d6d9e0123e2adff6bf0c2f
Attila Kristof Foldi <foldik@cloudera.com>
2023-03-22 13:44:10 +0100

    CB-21252 Close flow even if an exception happens during failed flow handling.

--- flow/src/test/java/com/sequenceiq/flow/reactor/config/EventBusConfigTest.java
+++ flow/src/test/java/com/sequenceiq/flow/reactor/config/EventBusConfigTest.java
@@ -2,6 +2,7 @@ package com.sequenceiq.flow.reactor.config;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -73,6 +74,25 @@ class EventBusConfigTest {
         FlowLog flowLog = new FlowLog();
         flowLog.setFlowId("123");
         when(flowLogDBService.findFirstByFlowIdOrderByCreatedDesc("123")).thenReturn(Optional.of(flowLog));
+        Event.Headers headers = new Event.Headers();
+        headers.set("FLOW_ID", "123");
+        eventBus.on("exampleselector", (Consumer<Event<? extends Payload>>) event -> {
+            throw new RuntimeException("uncaught exception");
+        });
+        eventBus.notify("exampleselector", new Event<>(headers, null));
+        verify(flowLogDBService, timeout(2000).times(1)).findFirstByFlowIdOrderByCreatedDesc("123");
+        verify(applicationFlowInformation, timeout(1000).times(1)).handleFlowFail(flowLog);
+        verify(flowLogDBService, timeout(1000).times(1)).updateLastFlowLogStatus(flowLog, true);
+        verify(flowLogDBService, timeout(1000).times(1)).finalize(flowLog.getFlowId());
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void uncaughtErrorButFlowFoundAndHandlingFails() {
+        FlowLog flowLog = new FlowLog();
+        flowLog.setFlowId("123");
+        when(flowLogDBService.findFirstByFlowIdOrderByCreatedDesc("123")).thenReturn(Optional.of(flowLog));
+        doThrow(new RuntimeException("Boom")).when(applicationFlowInformation).handleFlowFail(any());
         Event.Headers headers = new Event.Headers();
         headers.set("FLOW_ID", "123");
         eventBus.on("exampleselector", (Consumer<Event<? extends Payload>>) event -> {
