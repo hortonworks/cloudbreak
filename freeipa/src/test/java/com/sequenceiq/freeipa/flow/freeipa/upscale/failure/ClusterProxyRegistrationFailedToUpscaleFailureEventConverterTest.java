f429a93431f3ed2c8a6ae69953d22d9ddf2e99f5
Gergely Papp <gpapp@hortonworks.com>
2023-04-03 09:54:15 +0200

    CB-21215 Freeipa upscale flow can get stuck with error handler
    In the freeipa upscale flow, the failure event returned from the check new instances health state was different than expected from the flow config. Once the failure event was sent, the flow remained in PENDING state. The current PR fixes this error.
    Also a missing converter for another error is added.

---
+++ freeipa/src/test/java/com/sequenceiq/freeipa/flow/freeipa/upscale/failure/ClusterProxyRegistrationFailedToUpscaleFailureEventConverterTest.java
@@ -0,0 +1,39 @@
+package com.sequenceiq.freeipa.flow.freeipa.upscale.failure;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.sequenceiq.freeipa.flow.freeipa.upscale.event.UpscaleFailureEvent;
+import com.sequenceiq.freeipa.flow.stack.StackFailureEvent;
+import com.sequenceiq.freeipa.flow.stack.provision.event.clusterproxy.ClusterProxyRegistrationFailed;
+
+public class ClusterProxyRegistrationFailedToUpscaleFailureEventConverterTest {
+
+    private static final long STACK_ID = 1L;
+
+    private final ClusterProxyRegistrationFailedToUpscaleFailureEventConverter underTest = new ClusterProxyRegistrationFailedToUpscaleFailureEventConverter();
+
+    @Test
+    void testCanConvert() {
+        assertTrue(underTest.canConvert(ClusterProxyRegistrationFailed.class));
+        assertFalse(underTest.canConvert(StackFailureEvent.class));
+    }
+
+    @Test
+    void testConvert() {
+        RuntimeException caughtException = new RuntimeException("my exception message");
+        ClusterProxyRegistrationFailed clusterProxyRegistrationFailed = new ClusterProxyRegistrationFailed(STACK_ID, caughtException);
+
+        UpscaleFailureEvent upscaleFailureEvent = underTest.convert(clusterProxyRegistrationFailed);
+
+        assertEquals(STACK_ID, upscaleFailureEvent.getResourceId());
+        assertEquals("ClusterProxyRegistration", upscaleFailureEvent.getFailedPhase());
+        assertTrue(upscaleFailureEvent.getFailureDetails().isEmpty());
+        assertTrue(upscaleFailureEvent.getSuccess().isEmpty());
+        assertEquals(caughtException, upscaleFailureEvent.getException());
+    }
+
+}
