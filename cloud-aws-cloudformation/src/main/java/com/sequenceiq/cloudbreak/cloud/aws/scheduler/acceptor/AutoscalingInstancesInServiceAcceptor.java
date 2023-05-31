ba49430e218ab46cdcf892011bf51016b22a5899
Dénes Berger <dberger@cloudera.com>
2023-05-31 16:29:27 +0200

    CB-21996 ASG creation waiter is not prepared for empty list in response
    So add an extra condition.

--- cloud-aws-cloudformation/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/scheduler/acceptor/AutoscalingInstancesInServiceAcceptor.java
+++ cloud-aws-cloudformation/src/main/java/com/sequenceiq/cloudbreak/cloud/aws/scheduler/acceptor/AutoscalingInstancesInServiceAcceptor.java
@@ -2,8 +2,11 @@ package com.sequenceiq.cloudbreak.cloud.aws.scheduler.acceptor;
 
 import static software.amazon.awssdk.services.autoscaling.model.LifecycleState.IN_SERVICE;
 
+import java.util.List;
+
 import software.amazon.awssdk.core.waiters.WaiterAcceptor;
 import software.amazon.awssdk.core.waiters.WaiterState;
+import software.amazon.awssdk.services.autoscaling.model.AutoScalingGroup;
 import software.amazon.awssdk.services.autoscaling.model.DescribeAutoScalingGroupsResponse;
 
 public class AutoscalingInstancesInServiceAcceptor implements WaiterAcceptor<DescribeAutoScalingGroupsResponse> {
@@ -21,7 +24,8 @@ public class AutoscalingInstancesInServiceAcceptor implements WaiterAcceptor<Des
 
     @Override
     public boolean matches(DescribeAutoScalingGroupsResponse response) {
-        return response.autoScalingGroups().get(0).instances().stream()
+        List<AutoScalingGroup> asgs = response.autoScalingGroups();
+        return !asgs.isEmpty() && asgs.get(0).instances().stream()
                 .filter(instance -> IN_SERVICE.equals(instance.lifecycleState())).count() == requiredCount;
     }
 }
