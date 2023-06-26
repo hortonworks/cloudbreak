1f24ea9806823fe9139c92b4b4a16c0f1dea0bab
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2023-06-26 09:03:12 +0000

    CB-21685 Refactor DB related context providers in rotation framework
    - added common implementations for DB secret rotations
    - simplified context provider for CM DB secrets, CM services DB secrets

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/rotation/context/provider/AbstractCMRelatedDatabasePasswordContextProvider.java
@@ -0,0 +1,188 @@
+package com.sequenceiq.cloudbreak.rotation.context.provider;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRdsRoleConfigProvider;
+import com.sequenceiq.cloudbreak.common.exception.CloudbreakServiceException;
+import com.sequenceiq.cloudbreak.core.bootstrap.service.container.postgres.PostgresConfigService;
+import com.sequenceiq.cloudbreak.domain.RDSConfig;
+import com.sequenceiq.cloudbreak.dto.StackDto;
+import com.sequenceiq.cloudbreak.orchestrator.exception.CloudbreakOrchestratorFailedException;
+import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
+import com.sequenceiq.cloudbreak.orchestrator.model.SaltPillarProperties;
+import com.sequenceiq.cloudbreak.rotation.ExitCriteriaProvider;
+import com.sequenceiq.cloudbreak.rotation.context.CMServiceConfigRotationContext;
+import com.sequenceiq.cloudbreak.rotation.context.SaltStateApplyRotationContext;
+import com.sequenceiq.cloudbreak.rotation.secret.vault.VaultRotationContext;
+import com.sequenceiq.cloudbreak.service.GatewayConfigService;
+import com.sequenceiq.cloudbreak.service.rdsconfig.AbstractRdsConfigProvider;
+import com.sequenceiq.cloudbreak.service.rdsconfig.RdsConfigService;
+import com.sequenceiq.cloudbreak.service.secret.service.SecretService;
+import com.sequenceiq.cloudbreak.service.stack.StackDtoService;
+import com.sequenceiq.cloudbreak.util.PasswordUtil;
+import com.sequenceiq.cloudbreak.view.ClusterView;
+
+public abstract class AbstractCMRelatedDatabasePasswordContextProvider {
+
+    private static final Integer SALT_STATE_MAX_RETRY = 3;
+
+    @Inject
+    private StackDtoService stackService;
+
+    @Inject
+    private RdsConfigService rdsConfigService;
+
+    @Inject
+    private SecretService secretService;
+
+    @Inject
+    private List<AbstractRdsConfigProvider> rdsConfigProviders;
+
+    @Inject
+    private List<AbstractRdsRoleConfigProvider> rdsRoleConfigProviders;
+
+    @Inject
+    private GatewayConfigService gatewayConfigService;
+
+    @Inject
+    private PostgresConfigService postgresConfigService;
+
+    @Inject
+    private ExitCriteriaProvider exitCriteriaProvider;
+
+    protected abstract Predicate<RDSConfig> getRDSConfigTypePredicate();
+
+    // if false, then only pillars and cm configs are updated, vault and postgres are already updated, only relevant for shared db secrets
+    protected boolean fullRotation(String resource) {
+        return true;
+    }
+
+    protected Predicate<RDSConfig> getRDSConfigCountPredicate() {
+        return rdsConfig -> rdsConfigService.getClustersUsingResource(rdsConfig).size() == 1;
+    }
+
+    protected Map<String, SaltPillarProperties> getRotationPillarProperties(String resource) {
+        try {
+            StackDto stack = stackService.getByCrn(resource);
+            Map<String, SaltPillarProperties> pillarPropertiesSet = Maps.newHashMap();
+            pillarPropertiesSet.put(PostgresConfigService.POSTGRESQL_SERVER, postgresConfigService.getPostgreSQLServerPropertiesForRotation(stack));
+            if (fullRotation(resource)) {
+                pillarPropertiesSet.put(PostgresConfigService.POSTGRES_ROTATION, postgresConfigService.getPillarPropertiesForRotation(stack));
+            }
+            addPillarPropertiesIfNeeded(pillarPropertiesSet, stack);
+            return pillarPropertiesSet;
+        } catch (Exception e) {
+            throw new CloudbreakServiceException("Failed to generate pillar properties for CM DB password rotation.", e);
+        }
+    }
+
+    protected void addPillarPropertiesIfNeeded(Map<String, SaltPillarProperties> pillarPropertiesSet, StackDto stack)
+            throws CloudbreakOrchestratorFailedException {
+
+    }
+
+    protected CMServiceConfigRotationContext getCMServiceConfigRotationContext(Map<RDSConfig, Pair<String, String>> newUserPassPairs, StackDto stack) {
+        Table<String, String, String> cmServiceConfigTable = HashBasedTable.create();
+        newUserPassPairs.forEach((rdsConfig, userPassPair) -> updateCmServiceConfigToTableIfExists(cmServiceConfigTable, rdsConfig,
+                userPassPair.getKey(), userPassPair.getValue()));
+        return new CMServiceConfigRotationContext(stack.getResourceCrn(), cmServiceConfigTable);
+    }
+
+    private void updateCmServiceConfigToTableIfExists(Table<String, String, String> cmServiceConfigTable, RDSConfig rdsConfig,
+            String newUser, String newPassword) {
+        Optional<AbstractRdsRoleConfigProvider> abstractRdsRoleConfigProvider = rdsRoleConfigProviders.stream()
+                .filter(configProvider -> configProvider.dbType().equals(DatabaseType.valueOf(rdsConfig.getType()))).findFirst();
+        if (abstractRdsRoleConfigProvider.isPresent()) {
+            AbstractRdsRoleConfigProvider roleConfigProvider = abstractRdsRoleConfigProvider.get();
+            cmServiceConfigTable.put(roleConfigProvider.getServiceType(), roleConfigProvider.dbUserKey(), newUser);
+            cmServiceConfigTable.put(roleConfigProvider.getServiceType(), roleConfigProvider.dbPasswordKey(), newPassword);
+        }
+    }
+
+    protected VaultRotationContext getVaultRotationContext(Map<RDSConfig, Pair<String, String>> newUserPassPairs, StackDto stack, String resource) {
+        Map<String, String> secretMap = Maps.newHashMap();
+        newUserPassPairs.forEach((rdsConfig, userPassPair) -> {
+            if (fullRotation(resource)) {
+                secretMap.put(rdsConfig.getConnectionUserNameSecret(), userPassPair.getKey());
+                secretMap.put(rdsConfig.getConnectionPasswordSecret(), userPassPair.getValue());
+            }
+        });
+        return VaultRotationContext.builder()
+                .withVaultPathSecretMap(secretMap)
+                .withResourceCrn(stack.getResourceCrn())
+                .build();
+    }
+
+    protected SaltStateApplyRotationContext.SaltStateApplyRotationContextBuilder getSaltStateApplyRotationContextBuilder(StackDto stack) {
+        GatewayConfig primaryGatewayConfig = gatewayConfigService.getPrimaryGatewayConfig(stack);
+        return SaltStateApplyRotationContext.builder()
+                .withResourceCrn(stack.getResourceCrn())
+                .withGatewayConfig(primaryGatewayConfig)
+                .withTargets(Set.of(primaryGatewayConfig.getHostname()))
+                .withExitCriteriaModel(exitCriteriaProvider.get(stack))
+                .withMaxRetry(SALT_STATE_MAX_RETRY)
+                .withMaxRetryOnError(SALT_STATE_MAX_RETRY);
+    }
+
+    protected Map<RDSConfig, Pair<String, String>> getUserPassPairs(StackDto stack, String resource) {
+        ClusterView cluster = stack.getCluster();
+        return rdsConfigService.findByClusterId(cluster.getId())
+                .stream()
+                .filter(getRDSConfigTypePredicate())
+                .filter(getRDSConfigCountPredicate())
+                .filter(rdsConfig -> rdsConfigProviders.stream().anyMatch(configProvider -> matchRdsTypeWithString(configProvider.getRdsType(), rdsConfig)))
+                .collect(Collectors.toMap(rdsConfig -> rdsConfig, rdsConfig -> getUserPassPairs(rdsConfig, resource)));
+    }
+
+    private Pair<String, String> getUserPassPairs(RDSConfig rdsConfig, String resource) {
+        if (fullRotation(resource)) {
+            return getNewUserPassPair(getDefaultUserName(rdsConfig));
+        } else {
+            return getExistingUserPassPair(rdsConfig);
+        }
+    }
+
+    private boolean matchRdsTypeWithString(DatabaseType rdsType, RDSConfig rdsConfig) {
+        if (Arrays.stream(DatabaseType.values()).anyMatch(value -> StringUtils.equals(value.name(), rdsConfig.getType()))) {
+            return rdsType.equals(DatabaseType.valueOf(rdsConfig.getType()));
+        }
+        return false;
+    }
+
+    private String getDefaultUserName(RDSConfig rdsConfig) {
+        return rdsConfigProviders.stream()
+                .filter(configProvider -> matchRdsTypeWithString(configProvider.getRdsType(), rdsConfig))
+                .map(AbstractRdsConfigProvider::getDbUser)
+                .findFirst()
+                .get();
+    }
+
+    private Pair<String, String> getNewUserPassPair(String defaultUserName) {
+        String newUser = defaultUserName + new SimpleDateFormat("ddMMyyHHmmss").format(new Date());
+        String newPassword = PasswordUtil.generatePassword();
+        return Pair.of(newUser, newPassword);
+    }
+
+    private Pair<String, String> getExistingUserPassPair(RDSConfig rdsConfig) {
+        String user = secretService.get(rdsConfig.getConnectionUserNameSecret());
+        String password = secretService.get(rdsConfig.getConnectionPasswordSecret());
+        return Pair.of(user, password);
+    }
+}
--- core/src/test/java/com/sequenceiq/cloudbreak/rotation/context/provider/CMDBPasswordRotationContextProviderTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/rotation/context/provider/CMDBPasswordRotationContextProviderTest.java
@@ -1,14 +1,17 @@
 package com.sequenceiq.cloudbreak.rotation.context.provider;
 
+import static com.sequenceiq.cloudbreak.rotation.context.provider.CMDBContextProviderTestUtil.mockCluster;
+import static com.sequenceiq.cloudbreak.rotation.context.provider.CMDBContextProviderTestUtil.mockGwConfig;
+import static com.sequenceiq.cloudbreak.rotation.context.provider.CMDBContextProviderTestUtil.mockRdsConfig;
+import static com.sequenceiq.cloudbreak.rotation.context.provider.CMDBContextProviderTestUtil.mockStack;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -21,11 +24,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
-import com.sequenceiq.cloudbreak.common.exception.CloudbreakServiceException;
+import com.sequenceiq.cloudbreak.cmtemplate.configproviders.AbstractRdsRoleConfigProvider;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.ClusterDeletionBasedExitCriteriaModel;
-import com.sequenceiq.cloudbreak.core.bootstrap.service.container.postgres.PostgresConfigService;
-import com.sequenceiq.cloudbreak.core.bootstrap.service.host.ClusterHostServiceRunner;
 import com.sequenceiq.cloudbreak.domain.RDSConfig;
+import com.sequenceiq.cloudbreak.domain.stack.cluster.Cluster;
 import com.sequenceiq.cloudbreak.dto.StackDto;
 import com.sequenceiq.cloudbreak.orchestrator.model.GatewayConfig;
 import com.sequenceiq.cloudbreak.rotation.CloudbreakSecretType;
@@ -33,14 +35,14 @@ import com.sequenceiq.cloudbreak.rotation.ExitCriteriaProvider;
 import com.sequenceiq.cloudbreak.rotation.SecretRotationStep;
 import com.sequenceiq.cloudbreak.rotation.common.RotationContext;
 import com.sequenceiq.cloudbreak.service.GatewayConfigService;
+import com.sequenceiq.cloudbreak.service.rdsconfig.AbstractRdsConfigProvider;
 import com.sequenceiq.cloudbreak.service.rdsconfig.RdsConfigService;
 import com.sequenceiq.cloudbreak.service.stack.StackDtoService;
-import com.sequenceiq.cloudbreak.view.ClusterView;
 
 @ExtendWith(MockitoExtension.class)
 public class CMDBPasswordRotationContextProviderTest {
 
-    private StackDto stackDto;
+    private static final DatabaseType TEST_DB_TYPE = DatabaseType.CLOUDERA_MANAGER;
 
     @Mock
     private StackDtoService stackService;
@@ -52,10 +54,10 @@ public class CMDBPasswordRotationContextProviderTest {
     private GatewayConfigService gatewayConfigService;
 
     @Mock
-    private PostgresConfigService postgresConfigService;
+    private AbstractRdsRoleConfigProvider rdsRoleConfigProvider;
 
     @Mock
-    private ClusterHostServiceRunner clusterHostServiceRunner;
+    private AbstractRdsConfigProvider rdsConfigProvider;
 
     @Mock
     private ExitCriteriaProvider exitCriteriaProvider;
@@ -65,37 +67,26 @@ public class CMDBPasswordRotationContextProviderTest {
 
     @BeforeEach
     public void setup() throws IllegalAccessException {
-        FieldUtils.writeDeclaredField(underTest, "defaultUserName", "cm", true);
-        stackDto = mock(StackDto.class);
-        ClusterView cluster = mock(ClusterView.class);
-        when(stackDto.getCluster()).thenReturn(cluster);
-        when(cluster.getId()).thenReturn(1L);
         lenient().when(exitCriteriaProvider.get(any())).thenReturn(ClusterDeletionBasedExitCriteriaModel.nonCancellableModel());
+        FieldUtils.writeField(underTest, "rdsRoleConfigProviders", List.of(rdsRoleConfigProvider), true);
+        FieldUtils.writeField(underTest, "rdsConfigProviders", List.of(rdsConfigProvider), true);
+        Cluster cluster = mockCluster(1L);
+        StackDto stackDto = mockStack(cluster, "resource");
+        RDSConfig rdsConfig = mockRdsConfig(TEST_DB_TYPE);
+        GatewayConfig gatewayConfig = mockGwConfig();
+        when(rdsConfigProvider.getRdsType()).thenReturn(TEST_DB_TYPE);
+        when(rdsConfigProvider.getDbUser()).thenReturn("user");
+        when(rdsConfigService.findByClusterId(any())).thenReturn(Set.of(rdsConfig));
+        when(rdsConfigService.getClustersUsingResource(any())).thenReturn(Set.of(cluster));
+        when(stackService.getByCrn(anyString())).thenReturn(stackDto);
+        when(gatewayConfigService.getPrimaryGatewayConfig(any())).thenReturn(gatewayConfig);
     }
 
     @Test
     public void testGetContext() {
-        RDSConfig rdsConfig = mock(RDSConfig.class);
-        when(rdsConfig.getConnectionUserNameSecret()).thenReturn("userpath");
-        when(rdsConfig.getConnectionPasswordSecret()).thenReturn("passpath");
-        when(rdsConfig.getType()).thenReturn(DatabaseType.CLOUDERA_MANAGER.name());
-        when(rdsConfigService.findByClusterId(any())).thenReturn(Set.of(rdsConfig));
-        when(stackService.getByCrn(anyString())).thenReturn(stackDto);
-        GatewayConfig gatewayConfig = mock(GatewayConfig.class);
-        when(gatewayConfigService.getPrimaryGatewayConfig(any())).thenReturn(gatewayConfig);
-        when(gatewayConfig.getHostname()).thenReturn("host");
-
         Map<SecretRotationStep, RotationContext> contexts = underTest.getContexts("resource");
 
         assertEquals(4, contexts.size());
         assertTrue(CloudbreakSecretType.CLUSTER_CM_DB_PASSWORD.getSteps().stream().allMatch(contexts::containsKey));
-    }
-
-    @Test
-    public void testGetContextWhenNoDb() {
-        when(rdsConfigService.findByClusterId(any())).thenReturn(Set.of());
-        when(stackService.getByCrn(anyString())).thenReturn(stackDto);
-
-        assertThrows(CloudbreakServiceException.class, () -> underTest.getContexts("resource"));
     }
 }
