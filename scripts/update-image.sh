7e9f5d99f4973927c2cc8d4fa881b0cdf597dc83
doktoric <rdoktorics@cloudera.com>
2023-07-24 13:53:07 +0200

    CB-22588 Update docker base image script does not work

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -15,6 +15,7 @@ done
 if [[ `git status --porcelain` ]]; then
   TODAY=$(date +'%m-%d-%Y')
   BRANCH_NAME=base-image-$TODAY
+  git remote set-url origin https://$GH_TOKEN@github.infra.cloudera.com/cloudbreak/cloudbreak
   git branch -D $BRANCH_NAME | true
   git push origin --delete $BRANCH_NAME | true
   git checkout -b $BRANCH_NAME
@@ -23,7 +24,7 @@ if [[ `git status --porcelain` ]]; then
   git commit -m "[JENKINS-AUTOMATION] Update Base image for $NEW_IMAGE"
   git push origin $BRANCH_NAME
   docker pull ghcr.io/supportpal/github-gh-cli
-  docker run  -e GH_TOKEN=$GH_TOKEN -e NEW_IMAGE=$NEW_IMAGE -e BRANCH_NAME=$BRANCH_NAME --entrypoint /bin/bash ghcr.io/supportpal/github-gh-cli -c 'GH_TOKEN=$GH_TOKEN gh pr create --title "Base image update for the latest RELENG image" --body "Automated Pull request to update the base image and reduce CVEs" --base master --head $BRANCH_NAME --repo hortonworks/cloudbreak'
+  echo $GH_TOKEN | docker run -e GH_TOKEN=$GH_TOKEN -e BRANCH_NAME=$BRANCH_NAME --entrypoint /bin/bash ghcr.io/supportpal/github-gh-cli -c 'echo $GH_TOKEN| gh auth login --hostname github.infra.cloudera.com --with-token && gh pr create --title "Base image update for the latest RELENG image" --body "Automated Pull request to update the base image and reduce CVEs" --base master --head $BRANCH_NAME --repo github.infra.cloudera.com/cloudbreak/cloudbreak'
 else
   echo "No new base image from RE DB"
 fi
