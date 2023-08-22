f352f1c9561905b03d72b2aca12342a6fb7591d6
vivprabu <113479354+vivprabu@users.noreply.github.com>
2023-08-22 17:13:42 -0400

    CB-22414-Prepare multi AZ Azure e2e tests for Enterprise Datalake provisioning

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/sdx/SdxMultiAzProvisioningTest.java
@@ -0,0 +1,92 @@
+package com.sequenceiq.it.cloudbreak.testcase.e2e.sdx;
+
+import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.springframework.util.CollectionUtils;
+import org.testng.annotations.Test;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.InstanceGroupV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.instancemetadata.InstanceMetaDataV4Response;
+import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
+import com.sequenceiq.it.cloudbreak.cloud.v4.CommonClusterManagerProperties;
+import com.sequenceiq.it.cloudbreak.context.Description;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.sdx.SdxTestDto;
+import com.sequenceiq.it.cloudbreak.exception.TestFailException;
+import com.sequenceiq.it.cloudbreak.util.CloudFunctionality;
+import com.sequenceiq.it.cloudbreak.util.spot.UseSpotInstances;
+import com.sequenceiq.sdx.api.model.SdxClusterDetailResponse;
+import com.sequenceiq.sdx.api.model.SdxClusterShape;
+import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
+
+public class SdxMultiAzProvisioningTest extends PreconditionSdxE2ETest {
+
+    @Inject
+    private SdxTestClient sdxTestClient;
+
+    @Inject
+    private CommonClusterManagerProperties commonClusterManagerProperties;
+
+    @Test(dataProvider = TEST_CONTEXT)
+    @UseSpotInstances
+    @Description(
+            given = "there is a running Cloudbreak, and an SDX cluster in available state",
+            when = "a valid MultiAz stack create request is sent (Enterprise shape is only available on CM version greater than or equal to 7.2.17)",
+            then = "the MultiAz stack should be available and the cluster should be up and running"
+    )
+    public void testSDXMultiAz(TestContext testContext) {
+        String sdx = resourcePropertyProvider().getName();
+
+        testContext
+                .given(sdx, SdxTestDto.class)
+                .withCloudStorage()
+                .withClusterShape(SdxClusterShape.ENTERPRISE)
+                .withRuntimeVersion(commonClusterManagerProperties.getRuntimeVersion())
+                .withEnableMultiAz(true)
+                .when(sdxTestClient.create(), key(sdx))
+                .await(SdxClusterStatusResponse.RUNNING, key(sdx))
+                .awaitForHealthyInstances()
+                .given(sdx, SdxTestDto.class)
+                .when(sdxTestClient.describe(), key(sdx))
+                .then((tc, testDto, client) -> {
+                    validateMultiAz(testDto, tc);
+                    return testDto;
+                })
+                .validate();
+    }
+
+    private void validateMultiAz(SdxTestDto sdxTestDto, TestContext tc) {
+        SdxClusterDetailResponse sdxClusterDetailResponse = sdxTestDto.getResponse();
+        if (!sdxClusterDetailResponse.isEnableMultiAz()) {
+            throw new TestFailException(String.format("MultiAz is not enabled for %s", sdxClusterDetailResponse.getName()));
+        }
+        List<String> instanceIds = sdxClusterDetailResponse.getStackV4Response().getInstanceGroups().stream()
+                .map(InstanceGroupV4Response::getMetadata)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .map(InstanceMetaDataV4Response::getInstanceId)
+                .collect(Collectors.toList());
+        String sdxName = sdxClusterDetailResponse.getStackV4Response().getName();
+        Map<String, Set<String>> availabilityZoneForVms = getCloudFunctionality(tc).listAvailabilityZonesForVms(sdxName, instanceIds);
+        List<String> instancesWithNoAz = instanceIds.stream().filter(instance -> CollectionUtils.isEmpty(availabilityZoneForVms.get(instance)))
+                .collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(instancesWithNoAz)) {
+            throw new TestFailException(String.format("Availability Zones is missing for instances %s in %s",
+                    String.join(",", instancesWithNoAz), sdxName));
+        }
+    }
+
+    protected CloudFunctionality getCloudFunctionality(TestContext testContext) {
+        return testContext.getCloudProvider().getCloudFunctionality();
+    }
+
+}
