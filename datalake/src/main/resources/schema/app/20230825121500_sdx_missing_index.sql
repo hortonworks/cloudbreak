7ac1c2594f27af943fcec127b0eeb6a809861ff2
lnardai <laci.nardai@gmail.com>
2023-09-18 21:18:33 +0200

    Revert "CB-22958 two parallel datalake request with the same parameters succeeded and we saved 2 datalakes at the same time. It should not happen, but the unique index was not properly created."
    This reverts commit 2d306361c5cdf1aa699f06a5fd1c7c247c800f1d.

--- datalake/src/main/resources/schema/app/20230825121500_sdx_missing_index.sql
+++
@@ -1,9 +0,0 @@
--- // CB-22958 fix sdx unique index
--- Migration SQL that makes the change goes here
-
-drop index if exists unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null;
-
-create unique index unq_index_sdxcluster_accid_envcrn_detached_deleted_is_null
-    on public.sdxcluster (accountid, envcrn, (deleted IS NULL), detached)
-    where (deleted IS NULL);
-
