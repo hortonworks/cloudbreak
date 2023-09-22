9ed96d73c697cb0370e5007c5959612f432260d4
David Bajzath <dbajzath@cloudera.com>
2023-09-22 22:46:01 +0200

    CB-23311 UserDataMigrationPatchService should run even if the stack has retryable flow

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stackpatch/ExistingStackPatchService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stackpatch/ExistingStackPatchService.java
@@ -39,7 +39,11 @@ public abstract class ExistingStackPatchService {
     }
 
     public Date getFirstStart() {
-        int delayInMinutes = RandomUtil.getInt((int) TimeUnit.HOURS.toMinutes(properties.getMaxInitialStartDelayInHours()));
+        return randomDelayWithMaxHours(properties.getMaxInitialStartDelayInHours());
+    }
+
+    protected Date randomDelayWithMaxHours(int maxStartDelayInHours) {
+        int delayInMinutes = RandomUtil.getInt((int) TimeUnit.HOURS.toMinutes(maxStartDelayInHours));
         return Date.from(ZonedDateTime.now().toInstant().plus(Duration.ofMinutes(delayInMinutes)));
     }
 
@@ -52,31 +56,33 @@ public abstract class ExistingStackPatchService {
         if (flowLogService.isOtherFlowRunning(stack.getId())) {
             LOGGER.info("Another flow is running for stack {}, skipping patch apply to let the flow finish", stack.getResourceCrn());
             return false;
-        } else {
-            Optional<FlowLog> lastRetryableFailedFlow = flowRetryService.getLastRetryableFailedFlow(stack.getId());
-            if (lastRetryableFailedFlow.isEmpty()) {
-                try {
-                    LOGGER.info("Starting stack {} patching for {}", stack.getResourceCrn(), getStackPatchType());
-                    boolean success = checkedMeasure(() -> doApply(stack), LOGGER, "Existing stack patching took {} ms for stack {} and patch {}.",
-                            stack.getResourceCrn(), getStackPatchType());
-                    if (success) {
-                        LOGGER.info("Stack {} was patched successfully for {}", stack.getResourceCrn(), getStackPatchType());
-                    } else {
-                        LOGGER.info("Stack {} was not patched for {}", stack.getResourceCrn(), getStackPatchType());
-                    }
-                    return success;
-                } catch (ExistingStackPatchApplyException e) {
-                    throw e;
-                } catch (Exception e) {
-                    String message = String.format("Something unexpected went wrong with stack %s while applying patch %s: %s",
-                            stack.getResourceCrn(), getStackPatchType(), e.getMessage());
-                    throw new ExistingStackPatchApplyException(message, e);
-                }
-            } else {
-                LOGGER.info("Stack {} has a retryable failed flow, skipping patch apply to preserve possible retry", stack.getResourceCrn());
-                return false;
-            }
         }
+        if (shouldCheckForFailedRetryableFlow() && hasRetryableFailedFlow(stack)) {
+            LOGGER.info("Stack {} has a retryable failed flow, skipping patch apply to preserve possible retry", stack.getResourceCrn());
+            return false;
+        }
+        try {
+            LOGGER.info("Starting stack {} patching for {}", stack.getResourceCrn(), getStackPatchType());
+            boolean success = checkedMeasure(() -> doApply(stack), LOGGER, "Existing stack patching took {} ms for stack {} and patch {}.",
+                    stack.getResourceCrn(), getStackPatchType());
+            if (success) {
+                LOGGER.info("Stack {} was patched successfully for {}", stack.getResourceCrn(), getStackPatchType());
+            } else {
+                LOGGER.info("Stack {} was not patched for {}", stack.getResourceCrn(), getStackPatchType());
+            }
+            return success;
+        } catch (ExistingStackPatchApplyException e) {
+            throw e;
+        } catch (Exception e) {
+            String message = String.format("Something unexpected went wrong with stack %s while applying patch %s: %s",
+                    stack.getResourceCrn(), getStackPatchType(), e.getMessage());
+            throw new ExistingStackPatchApplyException(message, e);
+        }
+    }
+
+    private boolean hasRetryableFailedFlow(Stack stack) {
+        Optional<FlowLog> lastRetryableFailedFlow = flowRetryService.getLastRetryableFailedFlow(stack.getId());
+        return lastRetryableFailedFlow.isPresent();
     }
 
     protected boolean isCmServerReachable(Stack stack) throws ExistingStackPatchApplyException {
@@ -89,6 +95,13 @@ public abstract class ExistingStackPatchService {
         return Optional.ofNullable(stack.getPrimaryGatewayInstance())
                 .orElseThrow(() -> new ExistingStackPatchApplyException("Could not find Primary gateway for stack: " + stack.getResourceCrn()))
                 .isReachable();
+    }
+
+    /**
+     * Override this to false when the stack patcher does not start a flow, or it should start its flow either way
+     */
+    protected boolean shouldCheckForFailedRetryableFlow() {
+        return true;
     }
 
     /**
