e9f3d4b7c7a7b5e369fab3db0fcd3c20c2b88bf2
Peter Kedvessy <pkedvessy@cloudera.com>
2023-10-09 11:11:51 +0200

    CB-23445 Update ClientAliveInterval by sudo command test case as it was removed from CB as a part of CB-22932

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/l0promotion/EnvironmentPrivilegedUserTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/l0promotion/EnvironmentPrivilegedUserTest.java
@@ -3,6 +3,7 @@ package com.sequenceiq.it.cloudbreak.testcase.e2e.l0promotion;
 import static com.sequenceiq.it.cloudbreak.context.RunningParameter.expectedMessage;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
@@ -10,10 +11,12 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.recipes.requests.RecipeV4Type;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.instancemetadata.InstanceMetaDataV4Response;
 import com.sequenceiq.cloudbreak.auth.altus.UmsVirtualGroupRight;
 import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGeneratorFactory;
@@ -21,13 +24,17 @@ import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.freeipa.api.v1.operation.model.OperationState;
 import com.sequenceiq.it.cloudbreak.assertion.ums.VirtualGroupTestAssertion;
 import com.sequenceiq.it.cloudbreak.client.FreeIpaTestClient;
+import com.sequenceiq.it.cloudbreak.client.RecipeTestClient;
 import com.sequenceiq.it.cloudbreak.client.UmsTestClient;
 import com.sequenceiq.it.cloudbreak.config.user.TestUserSelectors;
 import com.sequenceiq.it.cloudbreak.context.Description;
+import com.sequenceiq.it.cloudbreak.context.RunningParameter;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.InstanceGroupTestDto;
 import com.sequenceiq.it.cloudbreak.dto.environment.EnvironmentTestDto;
 import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaTestDto;
 import com.sequenceiq.it.cloudbreak.dto.freeipa.FreeIpaUserSyncTestDto;
+import com.sequenceiq.it.cloudbreak.dto.recipe.RecipeTestDto;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
 import com.sequenceiq.it.cloudbreak.dto.ums.UmsTestDto;
 import com.sequenceiq.it.cloudbreak.exception.TestFailException;
@@ -47,6 +54,16 @@ public class EnvironmentPrivilegedUserTest extends AbstractE2ETest {
 
     private static final String SSSD_STATUS = "systemctl status sssd.service";
 
+    private static final String BASH_SCRIPT = "#!/bin/bash";
+
+    private static final String REPLACE_CLIENT_ALIVE_INTERVAL = "sed -i \"s/ClientAliveInterval.*/ClientAliveInterval 1800/g\" /etc/ssh/sshd_config";
+
+    private static final String SSSD_CONFIG_RECIPE_CONTENT = Base64.encodeBase64String(String.join("\n",
+            BASH_SCRIPT,
+            REPLACE_CLIENT_ALIVE_INTERVAL,
+            SSSD_RESTART
+    ).getBytes());
+
     @Inject
     private FreeIpaTestClient freeIpaTestClient;
 
@@ -62,6 +79,9 @@ public class EnvironmentPrivilegedUserTest extends AbstractE2ETest {
     @Inject
     private EnvironmentUtil environmentUtil;
 
+    @Inject
+    private RecipeTestClient recipeTestClient;
+
     @Override
     protected void setupTest(TestContext testContext) {
         testContext.getTestUsers().setSelector(TestUserSelectors.UMS_ONLY);
@@ -72,6 +92,7 @@ public class EnvironmentPrivilegedUserTest extends AbstractE2ETest {
         initializeDefaultBlueprints(testContext);
         testContext.as(L0UserKeys.ENV_CREATOR_A);
         createDefaultCredential(testContext);
+        createRecipeAndApplyOnFreeIpaAndMasterNodes(testContext);
         createDefaultDatalake(testContext);
     }
 
@@ -155,5 +176,17 @@ public class EnvironmentPrivilegedUserTest extends AbstractE2ETest {
                 .filter(instanceMetaDataV4Response -> !"idbroker".equalsIgnoreCase(instanceMetaDataV4Response.getInstanceGroup()))
                 .map(InstanceMetaDataV4Response::getPrivateIp)
                 .collect(Collectors.toSet());
+    }
+
+    private void createRecipeAndApplyOnFreeIpaAndMasterNodes(TestContext context) {
+        String recipeName = resourcePropertyProvider().getName();
+        context
+                .given(recipeName, RecipeTestDto.class)
+                .withName(recipeName)
+                .withContent(SSSD_CONFIG_RECIPE_CONTENT)
+                .withRecipeType(RecipeV4Type.PRE_SERVICE_DEPLOYMENT)
+                .when(recipeTestClient.createV4(), RunningParameter.key(recipeName));
+        context.given("master", InstanceGroupTestDto.class).withRecipes(recipeName);
+        context.given(EnvironmentTestDto.class).withFreeIpaRecipe(Collections.singleton(recipeName));
     }
 }
