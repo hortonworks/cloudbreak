870bf31b7072886d527f042a316a25a2b8c59984
Tamas Bihari <tbihari@cloudera.com>
2023-11-07 17:01:38 +0100

    CB-23774 create stack patcher to add CDP_API_ENDPOINT_URL env variable to user data script in case of CCMv2
    This is necessary because DL/DH images that have been created between 2023/04/11 and 2023/04/26 contains a bug
    which handles CDP_API_ENDPOINT_URL as a mandatory variable for setting up CCMv2 and our user-data script doesn't contains it by default

---
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stackpatch/UserDataCcmV2SetupPatchServiceTest.java
@@ -0,0 +1,102 @@
+package com.sequenceiq.cloudbreak.service.stackpatch;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sequenceiq.cloudbreak.TestUtil;
+import com.sequenceiq.cloudbreak.domain.stack.Stack;
+import com.sequenceiq.cloudbreak.service.image.userdata.UserDataService;
+import com.sequenceiq.cloudbreak.util.FileReaderUtils;
+import com.sequenceiq.common.api.type.InstanceGroupType;
+import com.sequenceiq.common.api.type.Tunnel;
+
+@ExtendWith(MockitoExtension.class)
+class UserDataCcmV2SetupPatchServiceTest {
+
+    @Mock
+    private UserDataService userDataService;
+
+    @InjectMocks
+    private UserDataCcmV2SetupPatchService underTest;
+
+    static Stream<Arguments> isAffectedParametersSource() {
+        return Stream.of(
+                Arguments.arguments(Tunnel.CCMV2, "", true),
+                Arguments.arguments(Tunnel.CCMV2, "export CDP_API_ENDPOINT_URL=", false),
+                Arguments.arguments(Tunnel.CCMV2_JUMPGATE, "", false),
+                Arguments.arguments(Tunnel.CLUSTER_PROXY, "", false),
+                Arguments.arguments(Tunnel.DIRECT, "", false),
+                Arguments.arguments(Tunnel.CCM, "", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("isAffectedParametersSource")
+    void testIsAffected(Tunnel tunnel, String gatewayUserData, boolean expectedResult) {
+        Stack stack = TestUtil.stack();
+        stack.setTunnel(tunnel);
+        Map<InstanceGroupType, String> userDataMap = Map.of(InstanceGroupType.GATEWAY, gatewayUserData);
+        lenient().when(userDataService.getUserData(stack.getId())).thenReturn(userDataMap);
+
+        boolean actual = underTest.isAffected(stack);
+
+        Assertions.assertEquals(expectedResult, actual);
+    }
+
+    @Test
+    void testDoApplyWhenGatewayUserDataIsEmpty() throws ExistingStackPatchApplyException {
+        Stack stack = TestUtil.stack();
+        String coreUserData = "untouched";
+        Map<InstanceGroupType, String> userDataMap = new HashMap<>();
+        userDataMap.put(InstanceGroupType.GATEWAY, "");
+        userDataMap.put(InstanceGroupType.CORE, coreUserData);
+        lenient().when(userDataService.getUserData(stack.getId())).thenReturn(userDataMap);
+
+        boolean actual = underTest.doApply(stack);
+
+        ArgumentCaptor<Map<InstanceGroupType, String>> userDataMapCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(userDataService, times(1)).createOrUpdateUserData(eq(stack.getId()), userDataMapCaptor.capture());
+        Map<InstanceGroupType, String> capturedUserDataMap = userDataMapCaptor.getValue();
+        Assertions.assertEquals(coreUserData, capturedUserDataMap.get(InstanceGroupType.CORE));
+        Assertions.assertEquals("export CDP_API_ENDPOINT_URL=\"\"\n", capturedUserDataMap.get(InstanceGroupType.GATEWAY));
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void testDoApplyShouldAddAdditionalExportToScriptWhenGatewayUserDataIsABashScript() throws ExistingStackPatchApplyException, IOException {
+        Stack stack = TestUtil.stack();
+        String coreUserData = "untouched";
+        Map<InstanceGroupType, String> userDataMap = new HashMap<>();
+        userDataMap.put(InstanceGroupType.GATEWAY, FileReaderUtils.readFileFromClasspath("/input/userdata/gateway-user-data.sh"));
+        userDataMap.put(InstanceGroupType.CORE, coreUserData);
+        lenient().when(userDataService.getUserData(stack.getId())).thenReturn(userDataMap);
+
+        boolean actual = underTest.doApply(stack);
+
+        ArgumentCaptor<Map<InstanceGroupType, String>> userDataMapCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(userDataService, times(1)).createOrUpdateUserData(eq(stack.getId()), userDataMapCaptor.capture());
+        Map<InstanceGroupType, String> capturedUserDataMap = userDataMapCaptor.getValue();
+        Assertions.assertEquals(coreUserData, capturedUserDataMap.get(InstanceGroupType.CORE));
+        String expectedUserDataScript = FileReaderUtils.readFileFromClasspath("/input/userdata/expected-gateway-user-data.sh");
+        Assertions.assertEquals(expectedUserDataScript, capturedUserDataMap.get(InstanceGroupType.GATEWAY));
+        Assertions.assertTrue(actual);
+    }
+}
---
+++ core/src/test/resources/input/userdata/expected-gateway-user-data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## logging
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+set -x
+
+export CLOUD_PLATFORM="${cloudPlatform}"
+export START_LABEL=${platformDiskStartLabel}
+export PLATFORM_DISK_PREFIX=${platformDiskPrefix}
+export LAZY_FORMAT_DISK_LIMIT=12
+export IS_GATEWAY=${gateway?c}
+export TMP_SSH_KEY="${tmpSshKey}"
+export SSH_USER=${sshUser}
+export SALT_BOOT_PASSWORD=${saltBootPassword}
+export SALT_BOOT_SIGN_KEY=${signaturePublicKey}
+export CB_CERT=${cbCert}
+
+export IS_PROXY_ENABLED=false
+
+export IS_CCM_V2_ENABLED=true
+export IS_CCM_V2_JUMPGATE_ENABLED=false
+export CDP_API_ENDPOINT_URL=""
+
+${customUserData}
+
+/usr/bin/user-data-helper.sh "$@" &> /var/log/user-data.log
+
+chmod o-rwx /var/log/user-data.log
+chmod o-rwx /cdp/bin/*
+chmod o-rwx -R /etc/certs/ || :
