b2fd86a11af881db1ae164e7507c05e8717e11b1
tiborpopovics <tpopovics@cloudera.com>
2023-12-07 15:45:16 +0100

    CB-24002: Fix database server settings migration to not update the connection username in every cases

---
+++ core/src/main/java/com/sequenceiq/cloudbreak/reactor/handler/cluster/upgrade/rds/RdsConfigUpdateService.java
@@ -0,0 +1,65 @@
+package com.sequenceiq.cloudbreak.reactor.handler.cluster.upgrade.rds;
+
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.common.StackType;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
+import com.sequenceiq.cloudbreak.domain.RDSConfig;
+import com.sequenceiq.cloudbreak.dto.StackDto;
+import com.sequenceiq.cloudbreak.service.rdsconfig.AbstractRdsConfigProvider;
+import com.sequenceiq.cloudbreak.service.rdsconfig.RdsConfigService;
+
+@Component
+public class RdsConfigUpdateService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RdsConfigUpdateService.class);
+
+    @Inject
+    private RdsConfigService rdsConfigService;
+
+    @Inject
+    private Set<AbstractRdsConfigProvider> rdsConfigProviders;
+
+    public void updateRdsConnectionUserName(StackDto stackDto) {
+        // TODO Currently we update all the rds configs for the cluster because of customer pressure
+        // TODO In the followup CB-22393 it will be refactored to update only the CM config here and CM services config after the CM is successfully restarted
+        LOGGER.debug("Updating rds config connections to the upgraded database in cloudbreak database.");
+        boolean dataHubCluster = StackType.WORKLOAD.equals(stackDto.getStack().getType());
+        Set<RDSConfig> rdsConfigs = rdsConfigService.findByClusterId(stackDto.getCluster().getId())
+                .stream()
+                .filter(filterRdsConfigsToUpdate(dataHubCluster))
+                .collect(Collectors.toSet());
+        rdsConfigs.forEach(rdsConfig -> {
+            String originalUserName = rdsConfig.getConnectionUserName();
+            String newUserName = originalUserName.split("@")[0];
+            LOGGER.debug("Updating rds config connection {}'s user name from {} to {}", rdsConfig.getName(),
+                    originalUserName, newUserName);
+            rdsConfig.setConnectionUserName(newUserName);
+        });
+        rdsConfigService.pureSaveAll(rdsConfigs);
+    }
+
+    private Predicate<RDSConfig> filterRdsConfigsToUpdate(boolean dataHubCluster) {
+        return rdsCfg -> hasMatchingRdsConfigProvider(rdsCfg) && configUpdateRequired(rdsCfg, dataHubCluster);
+    }
+
+    private boolean configUpdateRequired(RDSConfig rdsCfg, boolean dataHubCluster) {
+        return !(dataHubCluster && rdsConfigService.getClustersUsingResource(rdsCfg).size() > 1);
+    }
+
+    private boolean hasMatchingRdsConfigProvider(RDSConfig rdsCfg) {
+        return rdsConfigProviders.stream().anyMatch(configProvider -> isMatchRdsTypeWithString(configProvider.getRdsType(), rdsCfg));
+    }
+
+    private boolean isMatchRdsTypeWithString(DatabaseType rdsType, RDSConfig rdsConfig) {
+        return rdsType.name().equals(rdsConfig.getType());
+    }
+}
--- core/src/test/java/com/sequenceiq/cloudbreak/reactor/handler/cluster/upgrade/rds/MigrateDatabaseSettingsHandlerTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/reactor/handler/cluster/upgrade/rds/MigrateDatabaseSettingsHandlerTest.java
@@ -1,5 +1,6 @@
 package com.sequenceiq.cloudbreak.reactor.handler.cluster.upgrade.rds;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyLong;
@@ -7,12 +8,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -20,12 +17,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.database.base.DatabaseType;
 import com.sequenceiq.cloudbreak.common.database.TargetMajorVersion;
 import com.sequenceiq.cloudbreak.common.event.Selectable;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.container.postgres.PostgresConfigService;
 import com.sequenceiq.cloudbreak.core.bootstrap.service.host.ClusterHostServiceRunner;
-import com.sequenceiq.cloudbreak.domain.RDSConfig;
 import com.sequenceiq.cloudbreak.dto.StackDto;
 import com.sequenceiq.cloudbreak.eventbus.Event;
 import com.sequenceiq.cloudbreak.orchestrator.host.HostOrchestrator;
@@ -37,8 +32,6 @@ import com.sequenceiq.cloudbreak.reactor.api.event.cluster.upgrade.rds.UpgradeRd
 import com.sequenceiq.cloudbreak.reactor.api.event.cluster.upgrade.rds.UpgradeRdsMigrateDatabaseSettingsResponse;
 import com.sequenceiq.cloudbreak.rotation.ExitCriteriaProvider;
 import com.sequenceiq.cloudbreak.service.GatewayConfigService;
-import com.sequenceiq.cloudbreak.service.rdsconfig.AbstractRdsConfigProvider;
-import com.sequenceiq.cloudbreak.service.rdsconfig.RdsConfigService;
 import com.sequenceiq.cloudbreak.service.salt.SaltStateParamsService;
 import com.sequenceiq.cloudbreak.service.stack.StackDtoService;
 import com.sequenceiq.cloudbreak.view.ClusterView;
@@ -63,9 +56,6 @@ public class MigrateDatabaseSettingsHandlerTest {
     private StackDtoService stackDtoService;
 
     @Mock
-    private RdsConfigService rdsConfigService;
-
-    @Mock
     private ExitCriteriaProvider exitCriteriaProvider;
 
     @Mock
@@ -84,10 +74,7 @@ public class MigrateDatabaseSettingsHandlerTest {
     private GatewayConfig primaryGatewayConfig;
 
     @Mock
-    private AbstractRdsConfigProvider rdsConfigProvider1;
-
-    @Mock
-    private AbstractRdsConfigProvider rdsConfigProvider2;
+    private RdsConfigUpdateService rdsConfigUpdateService;
 
     @Test
     void testDoAccept() throws Exception {
@@ -95,57 +82,35 @@ public class MigrateDatabaseSettingsHandlerTest {
 
         when(stackDto.getCluster()).thenReturn(clusterView);
         when(primaryGatewayConfig.getHostname()).thenReturn("hostname");
-        FieldUtils.writeField(underTest, "rdsConfigProviders", Set.of(rdsConfigProvider1, rdsConfigProvider2), true);
-        when(rdsConfigProvider1.getRdsType()).thenReturn(DatabaseType.CLOUDERA_MANAGER);
-        when(rdsConfigProvider2.getRdsType()).thenReturn(DatabaseType.HIVE);
-
-        RDSConfig rdsConfig1 = new RDSConfig();
-        rdsConfig1.setId(0L);
-        rdsConfig1.setName("testRdsConfig1");
-        rdsConfig1.setConnectionUserName("originalUserName@cuttable");
-        rdsConfig1.setType(DatabaseType.CLOUDERA_MANAGER.name());
-        RDSConfig rdsConfig2 = new RDSConfig();
-        rdsConfig2.setId(1L);
-        rdsConfig2.setName("testRdsConfig1");
-        rdsConfig2.setConnectionUserName("originalUserName");
-        rdsConfig2.setType(DatabaseType.HIVE.name());
-        RDSConfig rdsConfig3 = new RDSConfig();
-        rdsConfig3.setId(2L);
-        rdsConfig3.setType("skipped");
 
         when(stackDtoService.getById(anyLong())).thenReturn(stackDto);
-        when(rdsConfigService.findByClusterId(anyLong())).thenReturn(Set.of(rdsConfig1, rdsConfig2, rdsConfig3));
         when(clusterHostServiceRunner.getClouderaManagerDatabasePillarProperties(any(ClusterView.class))).thenReturn(new SaltPillarProperties("path", Map.of()));
         when(postgresConfigService.getPostgreSQLServerPropertiesForRotation(stackDto)).thenReturn(new SaltPillarProperties("path1", Map.of()));
         when(gatewayConfigService.getPrimaryGatewayConfig(stackDto)).thenReturn(primaryGatewayConfig);
         Selectable result = underTest.doAccept(new HandlerEvent<>(new Event<>(request)));
 
         UpgradeRdsMigrateDatabaseSettingsResponse response = (UpgradeRdsMigrateDatabaseSettingsResponse) result;
-        Assertions.assertEquals(1L, response.getResourceId());
+        assertEquals(1L, response.getResourceId());
 
-        verify(rdsConfigService, times(1)).findByClusterId(anyLong());
-        ArgumentCaptor<Iterable<RDSConfig>> argumentCaptor = ArgumentCaptor.forClass(Iterable.class);
-        verify(rdsConfigService, times(1)).pureSaveAll(argumentCaptor.capture());
-        Iterator<RDSConfig> iterator = argumentCaptor.getValue().iterator();
-        Assertions.assertEquals("originalUserName", iterator.next().getConnectionUserName());
-        Assertions.assertEquals("originalUserName", iterator.next().getConnectionUserName());
-        Assertions.assertFalse(iterator.hasNext());
         verify(stackDtoService, times(1)).getById(anyLong());
+        verify(rdsConfigUpdateService).updateRdsConnectionUserName(stackDto);
         ArgumentCaptor<SaltConfig> saltConfigArgumentCaptor = ArgumentCaptor.forClass(SaltConfig.class);
         verify(hostOrchestrator, times(1)).saveCustomPillars(saltConfigArgumentCaptor.capture(), isNull(), isNull());
         SaltConfig saltConfig = saltConfigArgumentCaptor.getValue();
-        Assertions.assertEquals("path", saltConfig.getServicePillarConfig().get(ClusterHostServiceRunner.CM_DATABASE_PILLAR_KEY).getPath());
-        Assertions.assertEquals("path1", saltConfig.getServicePillarConfig().get(PostgresConfigService.POSTGRESQL_SERVER).getPath());
+        assertEquals("path", saltConfig.getServicePillarConfig().get(ClusterHostServiceRunner.CM_DATABASE_PILLAR_KEY).getPath());
+        assertEquals("path1", saltConfig.getServicePillarConfig().get(PostgresConfigService.POSTGRESQL_SERVER).getPath());
+        verify(gatewayConfigService).getPrimaryGatewayConfig(stackDto);
+
     }
 
     @Test
-    void testDoAcceptFailure() throws Exception {
+    void testDoAcceptFailure() {
         UpgradeRdsMigrateDatabaseSettingsRequest request = new UpgradeRdsMigrateDatabaseSettingsRequest(1L, TargetMajorVersion.VERSION_14);
         Exception expectedException = new RuntimeException();
         when(stackDtoService.getById(anyLong())).thenThrow(expectedException);
 
         Selectable result = underTest.doAccept(new HandlerEvent<>(new Event<>(request)));
         UpgradeRdsFailedEvent response = (UpgradeRdsFailedEvent) result;
-        Assertions.assertEquals(expectedException, response.getException());
+        assertEquals(expectedException, response.getException());
     }
 }
