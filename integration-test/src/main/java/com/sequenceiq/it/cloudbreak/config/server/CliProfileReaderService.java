9b060ad9b0453d8160bdce71c68f6d244cb5b6b9
Arpad Farsang <afarsang@gmail.com>
2023-12-08 15:43:36 +0100

    CB-24041 fixing test user creation based on data form dp cli profile

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/config/server/CliProfileReaderService.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/config/server/CliProfileReaderService.java
@@ -6,12 +6,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
+
+import com.sequenceiq.it.cloudbreak.actor.CloudbreakUser;
 
 @Service
 public class CliProfileReaderService {
@@ -21,7 +24,29 @@ public class CliProfileReaderService {
     @Value("${integrationtest.dp.profile:localhost}")
     private String profile;
 
-    Map<String, String> read() throws IOException {
+    public Optional<CloudbreakUser> getProfileUser() {
+        String accessKey;
+        String secretKey;
+        Map<String, String> profileData;
+
+        try {
+            profileData = read();
+            accessKey = profileData.get("apikeyid");
+            secretKey = profileData.get("privatekey");
+        } catch (Exception e) {
+            LOGGER.warn("There is no credential in dp cli config to the profile {}", profile);
+            return Optional.empty();
+        }
+
+        if (accessKey == null || "".equals(accessKey)) {
+            LOGGER.warn("The profile {} is exist,  but there is no credential set in dp cli", profile);
+            return Optional.empty();
+        }
+
+        return Optional.of(new CloudbreakUser(accessKey, secretKey));
+    }
+
+    public Map<String, String> read() throws IOException {
         String userHome = System.getProperty("user.home");
         Path cbProfileLocation = Paths.get(userHome, ".dp", "config");
         if (!Files.exists(cbProfileLocation)) {
--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/config/user/DefaultUserConfig.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/config/user/DefaultUserConfig.java
@@ -7,18 +7,27 @@ import java.util.Base64;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
 import com.sequenceiq.cloudbreak.util.SanitizerUtil;
 import com.sequenceiq.it.cloudbreak.actor.CloudbreakUser;
+import com.sequenceiq.it.cloudbreak.config.server.CliProfileReaderService;
 
 @Component(DEFAULT_USERS)
 class DefaultUserConfig implements TestUserConfig {
+
     public static final String DEFAULT_USERS = "default-user";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultUserConfig.class);
+
     private static final String MOCK_UMS_PASSWORD = "Password123!";
+
+    @Inject
+    private CliProfileReaderService cliProfileReaderService;
 
     @Value("${integrationtest.user.accesskey:}")
     private String accessKey;
@@ -68,16 +77,22 @@ class DefaultUserConfig implements TestUserConfig {
         String name;
         String workloadName;
 
-        crn = mockCrnOrConfiguredCrn();
-        name = userNameFromCrnOrConfiguredUserName(crn);
-        workloadName = workloadUserNameFromCrnOrConfiguredWorkloadUserName(crn);
+        user = cliProfileReaderService.getProfileUser().orElseGet(() -> new CloudbreakUser(accessKey, secretKey));
 
-        user = new CloudbreakUser(accessKey, secretKey, name, crn, "", true, workloadName);
+        crn = mockCrnOrConfiguredCrn(user.getAccessKey(), this.userCrn);
+        name = userNameFromCrnOrConfiguredUserName(crn,  this.userName);
+        workloadName = workloadUserNameFromCrnOrConfiguredWorkloadUserName(crn, this.workloadUserName);
+
+        user.setAdmin(true);
+        user.setCrn(crn);
+        user.setDisplayName(name);
+        user.setWorkloadUserName(workloadName);
         user.setWorkloadPassword(MOCK_UMS_PASSWORD);
-        mockedUms = mockedUms(accessKey);
+        user.setDescription("");
+        mockedUms = mockedUms(user.getAccessKey());
     }
 
-    private String mockCrnOrConfiguredCrn() {
+    private String mockCrnOrConfiguredCrn(String accessKey, String userCrn) {
         String crn;
         try {
             crn = new String(Base64.getDecoder().decode(accessKey));
@@ -88,7 +103,7 @@ class DefaultUserConfig implements TestUserConfig {
         return crn;
     }
 
-    private String userNameFromCrnOrConfiguredUserName(String crn) {
+    private String userNameFromCrnOrConfiguredUserName(String crn, String userName) {
         String name;
         try {
             name = Crn.fromString(crn).getUserId();
@@ -98,7 +113,7 @@ class DefaultUserConfig implements TestUserConfig {
         return name;
     }
 
-    private String workloadUserNameFromCrnOrConfiguredWorkloadUserName(String crn) {
+    private String workloadUserNameFromCrnOrConfiguredWorkloadUserName(String crn, String workloadUserName) {
         String workloadName;
 
         try {
@@ -113,8 +128,10 @@ class DefaultUserConfig implements TestUserConfig {
     private boolean mockedUms(String accessKey) {
         try {
             Crn.fromString(new String(Base64.getDecoder().decode(accessKey)));
+            LOGGER.info("Test user is for a mocked ums");
             return true;
         } catch (Exception e) {
+            LOGGER.info("Test user is ready to use against ums");
             return false;
         }
     }
