c9cf88a75a82d40f84fc8d015388467eaf75e1d2
David Bajzath <dbajzath@cloudera.com>
2024-02-26 10:45:29 +0100

    CB-24202 Poll for image setup in prewarm image validator
    As image size grew the provisiong started to time out because of the image setup step, so a separate polling with separate timeout was introduced.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/imagevalidation/PrewarmImageValidatorE2ETest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/imagevalidation/PrewarmImageValidatorE2ETest.java
@@ -1,25 +1,45 @@
 package com.sequenceiq.it.cloudbreak.testcase.e2e.imagevalidation;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import jakarta.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.testng.annotations.Test;
 
+import com.dyngr.Polling;
+import com.dyngr.core.AttemptMaker;
+import com.dyngr.core.AttemptResults;
+import com.dyngr.exception.PollerStoppedException;
+import com.sequenceiq.cloudbreak.structuredevent.event.StructuredEventType;
+import com.sequenceiq.cloudbreak.structuredevent.event.cdp.CDPStructuredEvent;
 import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
+import com.sequenceiq.it.cloudbreak.context.RunningParameter;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
 import com.sequenceiq.it.cloudbreak.dto.distrox.image.DistroXImageTestDto;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
 import com.sequenceiq.it.cloudbreak.dto.telemetry.TelemetryTestDto;
 import com.sequenceiq.it.cloudbreak.exception.TestFailException;
+import com.sequenceiq.it.cloudbreak.microservice.SdxClient;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.AbstractE2ETest;
 import com.sequenceiq.it.cloudbreak.util.spot.UseSpotInstances;
 import com.sequenceiq.it.util.imagevalidation.ImageValidatorE2ETest;
 import com.sequenceiq.it.util.imagevalidation.ImageValidatorE2ETestUtil;
+import com.sequenceiq.sdx.api.endpoint.SdxEndpoint;
+import com.sequenceiq.sdx.api.endpoint.SdxEventEndpoint;
+import com.sequenceiq.sdx.api.model.SdxClusterResponse;
 import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
 
 public class PrewarmImageValidatorE2ETest extends AbstractE2ETest implements ImageValidatorE2ETest {
+
+    private static final int IMAGE_SETUP_SLEEP_TIME_IN_SECONDS = 10;
+
+    @Value("${integrationtest.imageValidation.imagesetup.timeoutInMinutes:60}")
+    private int imageSetupTimeoutInMinutes;
 
     @Inject
     private SdxTestClient sdxTestClient;
@@ -56,6 +76,18 @@ public class PrewarmImageValidatorE2ETest extends AbstractE2ETest implements Ima
                         commonCloudProperties().getImageValidation().getImageUuid())
                 .withTelemetry("telemetry")
                 .when(sdxTestClient.createInternal())
+                .await(SdxClusterStatusResponse.STACK_CREATION_IN_PROGRESS, RunningParameter.emptyRunningParameter().withoutWaitForFlow())
+                .then((tc, testDto, client) -> {
+                    try {
+                        Polling.waitPeriodly(IMAGE_SETUP_SLEEP_TIME_IN_SECONDS, TimeUnit.SECONDS)
+                                .stopAfterDelay(imageSetupTimeoutInMinutes, TimeUnit.MINUTES)
+                                .stopIfException(true)
+                                .run(imageSetupResultAttemptMaker(testDto, client));
+                    } catch (PollerStoppedException e) {
+                        throw new TestFailException(String.format("Image setup exceeded %d minutes", imageSetupTimeoutInMinutes), e);
+                    }
+                    return testDto;
+                })
                 .await(SdxClusterStatusResponse.RUNNING)
                 .when(sdxTestClient.describeInternal())
                 .validate();
@@ -82,6 +114,27 @@ public class PrewarmImageValidatorE2ETest extends AbstractE2ETest implements Ima
                     }
                     return sdx;
                 }));
+    }
+
+    private AttemptMaker<Boolean> imageSetupResultAttemptMaker(SdxInternalTestDto testDto, SdxClient client) {
+        SdxEndpoint sdxEndpoint = client.getDefaultClient().sdxEndpoint();
+        SdxEventEndpoint sdxEventEndpoint = client.getDefaultClient().sdxEventEndpoint();
+        String environmentCrn = testDto.getResponse().getEnvironmentCrn();
+        List<StructuredEventType> eventTypes = List.of(StructuredEventType.NOTIFICATION);
+        return () -> {
+            SdxClusterResponse sdxClusterResponse = sdxEndpoint.get(testDto.getName());
+            if (SdxClusterStatusResponse.PROVISIONING_FAILED.equals(sdxClusterResponse.getStatus())) {
+                // provisioning failed, do not also fail for image setup
+                return AttemptResults.finishWith(true);
+            }
+            List<CDPStructuredEvent> auditEvents = sdxEventEndpoint.getAuditEvents(environmentCrn, eventTypes, null, null);
+            boolean imageSetupFinished = auditEvents.subList(1, auditEvents.size()).stream()
+                    .anyMatch(auditEvent -> "Setting up CDP image".equals(auditEvent.getStatusReason()));
+            if (imageSetupFinished) {
+                return AttemptResults.finishWith(true);
+            }
+            return AttemptResults.justContinue();
+        };
     }
 
     @Override
