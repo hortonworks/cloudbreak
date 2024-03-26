169beb3d38ec0a160a253287d94964675722173f
tiborpopovics <tpopovics@cloudera.com>
2024-03-26 22:33:11 +0100

    CB-25247: Rolling upgrade HDFS config validator should only validates the related configs if the HDFS service is present on the cluster

--- core/src/main/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/HdfsConfigurationValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/HdfsConfigurationValidator.java
@@ -5,6 +5,7 @@ import static com.sequenceiq.cloudbreak.cmtemplate.configproviders.hdfs.HdfsRole
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import jakarta.inject.Inject;
 
@@ -15,7 +16,9 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
 import com.sequenceiq.cloudbreak.auth.altus.EntitlementService;
 import com.sequenceiq.cloudbreak.cluster.api.ClusterApi;
+import com.sequenceiq.cloudbreak.cmtemplate.CmTemplateGeneratorService;
 import com.sequenceiq.cloudbreak.cmtemplate.configproviders.hdfs.HdfsRoles;
+import com.sequenceiq.cloudbreak.cmtemplate.generator.support.domain.SupportedService;
 import com.sequenceiq.cloudbreak.common.exception.UpgradeValidationFailedException;
 import com.sequenceiq.cloudbreak.dto.StackDto;
 import com.sequenceiq.cloudbreak.service.cluster.ClusterApiConnectors;
@@ -41,18 +44,33 @@ public class HdfsConfigurationValidator implements ServiceUpgradeValidator {
     @Inject
     private EntitlementService entitlementService;
 
+    @Inject
+    private CmTemplateGeneratorService clusterTemplateGeneratorService;
+
     @Override
     public void validate(ServiceUpgradeValidationRequest validationRequest) {
-        if (validationRequest.rollingUpgradeEnabled() && rollingUpgradeValidationEnabled()) {
-            validateHbaseConfigurationForRollingUpgrade(validationRequest.stack());
+        StackDto stack = validationRequest.stack();
+        if (validationRequest.rollingUpgradeEnabled() && rollingUpgradeValidationEnabled() && hdfsServicePresentOnTheCluster(stack)) {
+            validateHbaseConfigurationForRollingUpgrade(stack);
         } else {
             LOGGER.debug("Skipping HDFS configuration validation because the rolling upgrade is not enabled");
         }
     }
 
+    private boolean hdfsServicePresentOnTheCluster(StackDto stack) {
+        Set<SupportedService> services = clusterTemplateGeneratorService.getServicesByBlueprint(stack.getBlueprint().getBlueprintJsonText()).getServices();
+        if (services.stream().anyMatch(service -> HdfsRoles.HDFS.equalsIgnoreCase(service.getName()))) {
+            return true;
+        } else {
+            LOGGER.debug("The HDFS service is not present on the cluster. Only the following services are available: {}", services);
+            return false;
+        }
+    }
+
     private void validateHbaseConfigurationForRollingUpgrade(StackDto stack) {
         ClusterApi connector = clusterApiConnectors.getConnector(stack);
-        Optional<String> config = connector.getRoleConfigValueByServiceType(stack.getName(), HdfsRoles.GATEWAY, HdfsRoles.HDFS, HDFS_CLIENT_CONFIG_SAFETY_VALVE);
+        Optional<String> config = connector.getRoleConfigValueByServiceType(stack.getName(), HdfsRoles.GATEWAY, HdfsRoles.HDFS,
+                HDFS_CLIENT_CONFIG_SAFETY_VALVE);
         List<String> configurationErrors = new ArrayList<>();
         validateHdfsReplaceDataNodeOnFailurePolicy(config, configurationErrors);
         validateHdfsReplaceDataNodeOnFailureEnabled(config, configurationErrors);
@@ -68,7 +86,7 @@ public class HdfsConfigurationValidator implements ServiceUpgradeValidator {
     private void validateHdfsReplaceDataNodeOnFailurePolicy(Optional<String> config, List<String> configurationErrors) {
         if (config.isEmpty() || !config.get().contains(HDFS_REPLACE_DATANODE_ON_FAILURE_POLICY_PROPERTY)) {
             String errorMessage = String.format("the value of the %s configuration is incorrect,"
-                            + " please set this value to NEVER to enable the rolling upgrade for this cluster", HDFS_REPLACE_DATANODE_ON_FAILURE_POLICY);
+                    + " please set this value to NEVER to enable the rolling upgrade for this cluster", HDFS_REPLACE_DATANODE_ON_FAILURE_POLICY);
             configurationErrors.add(errorMessage);
         }
     }
@@ -76,7 +94,7 @@ public class HdfsConfigurationValidator implements ServiceUpgradeValidator {
     private void validateHdfsReplaceDataNodeOnFailureEnabled(Optional<String> config, List<String> configurationErrors) {
         if (config.isEmpty() || !config.get().contains(HDFS_DATANODE_ON_FAILURE_ENABLED_PROPERTY)) {
             String errorMessage = String.format("the value of the %s configuration is incorrect,"
-                            + " please set this value to FALSE to enable the rolling upgrade for this cluster", HDFS_DATANODE_ON_FAILURE_ENABLED);
+                    + " please set this value to FALSE to enable the rolling upgrade for this cluster", HDFS_DATANODE_ON_FAILURE_ENABLED);
             configurationErrors.add(errorMessage);
         }
     }
