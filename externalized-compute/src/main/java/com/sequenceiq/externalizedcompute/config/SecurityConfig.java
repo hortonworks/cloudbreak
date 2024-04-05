9b312a2b15312029e3cb2738798b4e73aa64e2b3
Laszlo Turcsanyi <lturcsanyi@cloudera.com>
2024-04-05 14:05:48 +0200

    CB-25229 [SEA] X-XSS-Protection Header is deprecated use Content-Security-Policy header instead

--- externalized-compute/src/main/java/com/sequenceiq/externalizedcompute/config/SecurityConfig.java
+++ externalized-compute/src/main/java/com/sequenceiq/externalizedcompute/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sequenceiq.externalizedcompute.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,10 @@ public class SecurityConfig {
         http.authorizeHttpRequests(req -> req.requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll());
         http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.headers(h -> h.contentTypeOptions(t -> { }));
+        http.headers(headers -> headers
+                .contentTypeOptions(Customizer.withDefaults())
+                .xssProtection(xssProtection -> xssProtection.disable())
+                .contentSecurityPolicy(Customizer.withDefaults()));
         return http.build();
     }
 
