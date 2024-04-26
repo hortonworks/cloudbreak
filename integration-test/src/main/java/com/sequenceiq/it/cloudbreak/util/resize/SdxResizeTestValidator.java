8aec4ae782f22dec360c003e9633df5c0a92224b
Rodrigo <ralves@cloudera.com>
2024-04-26 11:21:33 +0100

    CB-25188 e2e test to cover the custom instances for resize. Custom instances allow client to override default values. This tests creates a nww DL, call resize with custom instances and check if the new DL was created with custom instances.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/resize/SdxResizeTestValidator.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/resize/SdxResizeTestValidator.java
@@ -1,11 +1,20 @@
 package com.sequenceiq.it.cloudbreak.util.resize;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.InstanceGroupV4Response;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
 import com.sequenceiq.it.cloudbreak.exception.TestFailException;
+import com.sequenceiq.sdx.api.model.SdxClusterDetailResponse;
 import com.sequenceiq.sdx.api.model.SdxClusterResponse;
 import com.sequenceiq.sdx.api.model.SdxClusterShape;
+import com.sequenceiq.sdx.api.model.SdxInstanceGroupDiskRequest;
+import com.sequenceiq.sdx.api.model.SdxInstanceGroupRequest;
 
 public class SdxResizeTestValidator {
     private final SdxClusterShape expectedShape;
@@ -18,12 +27,18 @@ public class SdxResizeTestValidator {
 
     private final AtomicReference<Long> expectedCreationTimestamp;
 
+    private final AtomicReference<List<SdxInstanceGroupRequest>> expectedCustomInstanceGroups;
+
+    private final AtomicReference<List<SdxInstanceGroupDiskRequest>> expectedSdxInstanceGroupDiskRequest;
+
     public SdxResizeTestValidator(SdxClusterShape expectedShape) {
         this.expectedShape = expectedShape;
         expectedCrn = new AtomicReference<>();
         expectedName = new AtomicReference<>();
         expectedRuntime = new AtomicReference<>();
         expectedCreationTimestamp = new AtomicReference<>();
+        expectedCustomInstanceGroups = new AtomicReference<>();
+        expectedSdxInstanceGroupDiskRequest = new AtomicReference<>();
     }
 
     public void setExpectedCrn(String expectedCrn) {
@@ -42,14 +57,70 @@ public class SdxResizeTestValidator {
         this.expectedCreationTimestamp.set(expectedCreationTimestamp);
     }
 
+    public void setExpectedCustomInstanceGroups(List<SdxInstanceGroupRequest> expectedCustomInstanceGroups) {
+        this.expectedCustomInstanceGroups.set(expectedCustomInstanceGroups);
+    }
+
+    public void setExpectedSdxInstanceGroupDiskRequest(List<SdxInstanceGroupDiskRequest> expectedSdxInstanceGroupDiskRequest) {
+        this.expectedSdxInstanceGroupDiskRequest.set(expectedSdxInstanceGroupDiskRequest);
+    }
+
     public SdxInternalTestDto validateResizedCluster(SdxInternalTestDto dto) {
-        SdxClusterResponse response = dto.getResponse();
+        SdxClusterDetailResponse response = dto.getResponse();
         validateClusterShape(response.getClusterShape());
         validateCrn(response.getCrn());
         validateStackCrn(response.getStackCrn());
         validateName(response.getName());
         validateRuntime(response.getRuntime());
+        validateCustomInstanceGroups(response.getStackV4Response().getInstanceGroups());
+        validateCustomInstanceDiskSize(response.getStackV4Response().getInstanceGroups());
         return dto;
+    }
+
+    private void validateCustomInstanceGroups(List<InstanceGroupV4Response> instanceGroupV4Responses) {
+        List<SdxInstanceGroupRequest> customInstanceGroups = expectedCustomInstanceGroups.get();
+        if (!CollectionUtils.isEmpty(customInstanceGroups)) {
+            customInstanceGroups
+                    .forEach(customInstanceGroup ->
+                            getInstanceGroupByName(customInstanceGroup.getName(), instanceGroupV4Responses)
+                                    .ifPresent(instanceGroup -> {
+                                        if (!customInstanceGroup.getInstanceType().equals(instanceGroup.getTemplate().getInstanceType())) {
+                                            fail("instanceType " + customInstanceGroup.getName(),
+                                                    customInstanceGroup.getInstanceType(), instanceGroup.getTemplate().getInstanceType());
+                                        }
+                                    }));
+        }
+
+    }
+
+    private void validateCustomInstanceDiskSize(List<InstanceGroupV4Response> instanceGroupV4Responses) {
+        List<SdxInstanceGroupDiskRequest> customInstanceGroupDisks = expectedSdxInstanceGroupDiskRequest.get();
+        if (!CollectionUtils.isEmpty(customInstanceGroupDisks)) {
+            customInstanceGroupDisks
+                    .forEach(customInstanceGroupDisk ->
+                            getInstanceGroupByName(customInstanceGroupDisk.getName(), instanceGroupV4Responses)
+                                    .ifPresent(instanceGroup -> {
+                                        Integer actualDiskSize =
+                                                instanceGroup
+                                                        .getTemplate()
+                                                        .getAttachedVolumes()
+                                                        .stream()
+                                                        .findAny()
+                                                        .get()
+                                                        .getSize();
+                                        if (!actualDiskSize.equals(customInstanceGroupDisk.getInstanceDiskSize())) {
+                                            fail("instanceDiskSize " + customInstanceGroupDisk.getName(),
+                                                    customInstanceGroupDisk.getInstanceDiskSize().toString(), actualDiskSize.toString());
+                                        }
+                                    }));
+        }
+    }
+
+    private Optional<InstanceGroupV4Response> getInstanceGroupByName(String name, List<InstanceGroupV4Response> instanceGroupV4Responses) {
+        return instanceGroupV4Responses
+                .stream()
+                .filter(instanceGroup -> StringUtils.equals(instanceGroup.getName(), name))
+                .findAny();
     }
 
     public SdxInternalTestDto validateRecoveredCluster(SdxInternalTestDto dto) {
