cfb5eb5a596190ba6cf2c8d52d73c794f2796365
doktoric <rdoktorics@cloudera.com>
2024-05-10 06:36:14 +0200

    CB-25772 adding mock remote env response

--- mock-thunderhead/src/main/java/com/sequenceiq/thunderhead/controller/remotecluster/MockClusterProxyPvcControlPlaneApiController.java
+++ mock-thunderhead/src/main/java/com/sequenceiq/thunderhead/controller/remotecluster/MockClusterProxyPvcControlPlaneApiController.java
@@ -1,30 +1,47 @@
 package com.sequenceiq.thunderhead.controller.remotecluster;
 
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cloudera.thunderhead.service.environments2api.model.DescribeEnvironmentRequest;
+import com.cloudera.thunderhead.service.environments2api.model.DescribeEnvironmentResponse;
+import com.cloudera.thunderhead.service.environments2api.model.EnvironmentSummary;
+import com.cloudera.thunderhead.service.environments2api.model.ListEnvironmentsResponse;
 import com.sequenceiq.cloudbreak.auth.crn.Crn;
-import com.sequenceiq.thunderhead.controller.remotecluster.domain.MockRemoteEnvironmentResponse;
-import com.sequenceiq.thunderhead.controller.remotecluster.domain.MockRemoteEnvironmentResponses;
+import com.sequenceiq.cloudbreak.common.json.JsonUtil;
+import com.sequenceiq.cloudbreak.util.FileReaderUtils;
 
 @RestController
 public class MockClusterProxyPvcControlPlaneApiController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MockClusterProxyPvcControlPlaneApiController.class);
 
+    private DescribeEnvironmentResponse mockRemoteEnvironmentResponse;
+
+    @PostConstruct
+    public void setup() throws IOException {
+        String describeEnvironment = FileReaderUtils.readFileFromClasspathQuietly("mock-responses/clusterproxy/describe-environment.json");
+        mockRemoteEnvironmentResponse = JsonUtil.readValue(describeEnvironment, DescribeEnvironmentResponse.class);
+    }
+
     @PostMapping("/cluster-proxy/proxy/{crn}/PvcControlPlane/api/v1/environments2/listEnvironments")
-    public ResponseEntity<MockRemoteEnvironmentResponses> describeCustomConfig(@PathVariable("crn") String crn) {
-        LOGGER.info("Describe remote cluster for crn: '{}'", crn);
+    public ResponseEntity<ListEnvironmentsResponse> listEnvironments(@PathVariable("crn") String crn) {
+        LOGGER.info("List remote environments for crn: '{}'", crn);
         try {
-            MockRemoteEnvironmentResponses mockRemoteEnvironmentResponses = new MockRemoteEnvironmentResponses();
+            ListEnvironmentsResponse mockRemoteEnvironmentResponses = new ListEnvironmentsResponse();
             for (int i = 0; i < 10; i++) {
                 Crn crnObject = Crn.fromString(crn);
-                MockRemoteEnvironmentResponse mockRemoteEnvironmentResponse = new MockRemoteEnvironmentResponse();
+                EnvironmentSummary mockRemoteEnvironmentResponse = new EnvironmentSummary();
                 mockRemoteEnvironmentResponse.setEnvironmentName(crnObject.getResource() + "-" + i);
                 mockRemoteEnvironmentResponse.setCrn(crn);
                 mockRemoteEnvironmentResponse.setCloudPlatform("OPENSHIFT");
@@ -33,6 +50,22 @@ public class MockClusterProxyPvcControlPlaneApiController {
             }
             return new ResponseEntity<>(
                     mockRemoteEnvironmentResponses,
+                    HttpStatus.OK);
+        } catch (Exception ex) {
+            String msg = "UH-OH something went wrong!";
+            LOGGER.warn(msg, ex);
+            return new ResponseEntity(msg, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/cluster-proxy/proxy/{crn}/PvcControlPlane/api/v1/environments2/describeEnvironment")
+    public ResponseEntity<DescribeEnvironmentResponse> describeEnvironments(@PathVariable("crn") String crn,
+        @RequestBody DescribeEnvironmentRequest environmentRequest) {
+        LOGGER.info("Describe remote environments for crn: '{}'", crn);
+        try {
+            mockRemoteEnvironmentResponse.getEnvironment().setCrn(environmentRequest.getEnvironmentName());
+            return new ResponseEntity<>(
+                    mockRemoteEnvironmentResponse,
                     HttpStatus.OK);
         } catch (Exception ex) {
             String msg = "UH-OH something went wrong!";
