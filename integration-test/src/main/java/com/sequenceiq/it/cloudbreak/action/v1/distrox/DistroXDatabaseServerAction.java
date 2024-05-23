416a109f59702d39665ad9c17576797cc4072884
vivprabu <113479354+vivprabu@users.noreply.github.com>
2024-05-23 15:05:48 -0400

    CB-25614 - E2E test case for external database encryption for datahub

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/v1/distrox/DistroXDatabaseServerAction.java
@@ -0,0 +1,27 @@
+package com.sequenceiq.it.cloudbreak.action.v1.distrox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.database.StackDatabaseServerResponse;
+import com.sequenceiq.it.cloudbreak.log.Log;
+import com.sequenceiq.it.cloudbreak.microservice.CloudbreakClient;
+
+@Component
+public class DistroXDatabaseServerAction {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DistroXDatabaseServerAction.class);
+
+    public StackDatabaseServerResponse getExternalDatabaseConfigs(String distroxCrn, CloudbreakClient cloudbreakClient) throws Exception {
+        Log.when(LOGGER, "DistroX endpoint: %s" + cloudbreakClient.getDefaultClient().distroXDatabaseServerV1Endpoint());
+        Log.whenJson(LOGGER, "Distrox crn: ", distroxCrn);
+
+        StackDatabaseServerResponse stackDatabaseServerResponse = cloudbreakClient.getDefaultClient().distroXDatabaseServerV1Endpoint()
+                .getDatabaseServerByCrn(distroxCrn);
+
+        Log.whenJson(LOGGER, "Stack Database Server Response: ", stackDatabaseServerResponse);
+
+        return stackDatabaseServerResponse;
+    }
+}
--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/dbserverencryption/GcpExternalDatabaseServerEncryptionTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/dbserverencryption/GcpExternalDatabaseServerEncryptionTests.java
@@ -10,19 +10,27 @@ import jakarta.inject.Inject;
 import org.apache.commons.lang3.tuple.Pair;
 import org.testng.annotations.Test;
 
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.StackV4Response;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.database.DatabaseServerSslMode;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.database.StackDatabaseServerResponse;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.InstanceGroupV4Response;
+import com.sequenceiq.distrox.api.v1.distrox.model.database.DistroXDatabaseAvailabilityType;
+import com.sequenceiq.distrox.api.v1.distrox.model.database.DistroXDatabaseRequest;
 import com.sequenceiq.environment.api.v1.environment.model.response.EnvironmentStatus;
 import com.sequenceiq.it.cloudbreak.action.sdx.SdxDatabaseServerAction;
+import com.sequenceiq.it.cloudbreak.action.v1.distrox.DistroXDatabaseServerAction;
+import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.client.EnvironmentTestClient;
 import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
 import com.sequenceiq.it.cloudbreak.context.Description;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.instancegroup.DistroXInstanceGroupsBuilder;
 import com.sequenceiq.it.cloudbreak.dto.environment.EnvironmentTestDto;
 import com.sequenceiq.it.cloudbreak.dto.sdx.SdxTestDto;
 import com.sequenceiq.it.cloudbreak.dto.telemetry.TelemetryTestDto;
 import com.sequenceiq.it.cloudbreak.exception.TestFailException;
+import com.sequenceiq.it.cloudbreak.microservice.CloudbreakClient;
 import com.sequenceiq.it.cloudbreak.microservice.SdxClient;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.sdx.PreconditionSdxE2ETest;
 import com.sequenceiq.it.cloudbreak.testcase.e2e.sdx.SdxUpgradeDatabaseTestUtil;
@@ -51,6 +59,12 @@ public class GcpExternalDatabaseServerEncryptionTests extends PreconditionSdxE2E
     @Inject
     private SdxDatabaseServerAction sdxDatabaseServerAction;
 
+    @Inject
+    private DistroXDatabaseServerAction distroxDatabaseServerAction;
+
+    @Inject
+    private DistroXTestClient distroXTestClient;
+
     @Override
     protected void setupTest(TestContext testContext) {
         testContext.getCloudProvider().getCloudFunctionality().cloudStorageInitialize();
@@ -63,15 +77,19 @@ public class GcpExternalDatabaseServerEncryptionTests extends PreconditionSdxE2E
     @UseSpotInstances
     @Description(
             given = "there is a running Cloudbreak, and an encrypted environment is created on GCP with external database",
-            when = "SDX is created with external encrypted database",
+            when = "SDX and DistroX is created with external encrypted database",
             then = "SSL encryption is enabled for GCP, the cluster should be up and running"
     )
     public void testExternalDatabaseEncryptionOnGCP(TestContext testContext) {
         String sdx = resourcePropertyProvider().getName();
+        String dx = sdx + "dx";
 
         SdxDatabaseRequest sdxDatabaseRequest = new SdxDatabaseRequest();
         sdxDatabaseRequest.setAvailabilityType(SdxDatabaseAvailabilityType.NON_HA);
         sdxDatabaseRequest.setDatabaseEngineVersion(sdxUpgradeDatabaseTestUtil.getTargetMajorVersion().getMajorVersion());
+
+        DistroXDatabaseRequest distroXDatabaseRequest = new DistroXDatabaseRequest();
+        distroXDatabaseRequest.setAvailabilityType(DistroXDatabaseAvailabilityType.NON_HA);
 
         testContext
                 .given("telemetry", TelemetryTestDto.class)
@@ -102,18 +120,53 @@ public class GcpExternalDatabaseServerEncryptionTests extends PreconditionSdxE2E
                     validateEncryption(testDto, tc, client);
                     return testDto;
                 })
+                .given(dx, DistroXTestDto.class)
+                .withTemplate(commonClusterManagerProperties().getDataMartDistroXBlueprintNameForCurrentRuntime())
+                .withExternalDatabase(distroXDatabaseRequest)
+                .withInstanceGroupsEntity(new DistroXInstanceGroupsBuilder(testContext)
+                    .verticalScaleHostGroup()
+                    .build())
+                .when(distroXTestClient.create(), key("dx"))
+                .await(STACK_AVAILABLE, key("dx"))
+                .awaitForHealthyInstances()
+                .given(dx, DistroXTestDto.class)
+                .when(distroXTestClient.get(), key(dx))
+                .then((tc, testDto, client) -> {
+                    validateDistroXEncryption(testDto, tc, client);
+                    return testDto;
+                })
                 .validate();
     }
 
     private void validateEncryption(SdxTestDto sdxTestDto, TestContext tc, SdxClient sdxClient) throws Exception {
         SdxClusterDetailResponse sdxClusterDetailResponse = sdxTestDto.getResponse();
+        String clusterName = sdxClusterDetailResponse.getName();
         StackDatabaseServerResponse stackDatabaseServerResponse = sdxDatabaseServerAction.getSdxExternalDatabaseConfigs(sdxClusterDetailResponse.getCrn(),
                 sdxClient);
-        if (!DatabaseServerSslMode.ENABLED.equals(stackDatabaseServerResponse.getSslConfig().getSslMode())) {
-            throw new TestFailException("SSL Encryption is not enabled for external database for cluster : " + sdxClusterDetailResponse.getName());
-        }
+        validateDbServer(stackDatabaseServerResponse, clusterName);
 
         List<InstanceGroupV4Response> instanceGroups = sdxClusterDetailResponse.getStackV4Response().getInstanceGroups();
+        validateDbSslMode(instanceGroups);
+    }
+
+    private static void validateDbServer(StackDatabaseServerResponse stackDatabaseServerResponse, String clusterName) {
+        if (!DatabaseServerSslMode.ENABLED.equals(stackDatabaseServerResponse.getSslConfig().getSslMode())) {
+            throw new TestFailException("SSL Encryption is not enabled for external database for cluster : " + clusterName);
+        }
+    }
+
+    private void validateDistroXEncryption(DistroXTestDto distroxTestDto, TestContext tc, CloudbreakClient cloudbreakClient) throws Exception {
+        StackV4Response distroxResponse = distroxTestDto.getResponse();
+        String clusterName = distroxResponse.getName();
+        StackDatabaseServerResponse stackDatabaseServerResponse = distroxDatabaseServerAction.getExternalDatabaseConfigs(distroxTestDto.getCrn(),
+                cloudbreakClient);
+        validateDbServer(stackDatabaseServerResponse, clusterName);
+
+        List<InstanceGroupV4Response> instanceGroups = distroxResponse.getInstanceGroups();
+        validateDbSslMode(instanceGroups);
+    }
+
+    private void validateDbSslMode(List<InstanceGroupV4Response> instanceGroups) {
         Map<String, Pair<Integer, String>> dbSslModeConnectionMap = sshJUtil.getSSLModeForExternalDBByIp(instanceGroups, List.of("master"),
                 commonCloudProperties().getDefaultPrivateKeyFile());
 
