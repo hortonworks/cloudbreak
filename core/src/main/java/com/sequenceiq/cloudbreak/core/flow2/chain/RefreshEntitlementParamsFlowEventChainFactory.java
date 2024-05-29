c028ae6a585e6c4029dd185be3ee93517494fca6
Balazs Bihari <bbihari@cloudera.com>
2024-05-29 10:46:06 +0200

    CB-24912 Create EDH tracking on enablement/disablement of prometheus agent

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/chain/RefreshEntitlementParamsFlowEventChainFactory.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/chain/RefreshEntitlementParamsFlowEventChainFactory.java
@@ -1,5 +1,9 @@
 package com.sequenceiq.cloudbreak.core.flow2.chain;
 
+import static com.cloudera.thunderhead.service.common.usage.UsageProto.CDPClusterStatus.Value.ENTITLEMENT_SYNC_FAILED;
+import static com.cloudera.thunderhead.service.common.usage.UsageProto.CDPClusterStatus.Value.ENTITLEMENT_SYNC_FINISHED;
+import static com.cloudera.thunderhead.service.common.usage.UsageProto.CDPClusterStatus.Value.ENTITLEMENT_SYNC_STARTED;
+import static com.cloudera.thunderhead.service.common.usage.UsageProto.CDPClusterStatus.Value.UNSET;
 import static com.sequenceiq.cloudbreak.core.flow2.cluster.salt.update.SaltUpdateEvent.SALT_UPDATE_EVENT;
 
 import java.util.Queue;
@@ -7,15 +11,21 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.springframework.stereotype.Component;
 
+import com.cloudera.thunderhead.service.common.usage.UsageProto.CDPClusterStatus.Value;
 import com.sequenceiq.cloudbreak.common.event.Selectable;
 import com.sequenceiq.cloudbreak.core.flow2.event.RefreshEntitlementParamsFlowChainTriggerEvent;
 import com.sequenceiq.cloudbreak.reactor.api.event.StackEvent;
 import com.sequenceiq.cloudbreak.reactor.api.event.orchestration.RefreshEntitlementParamsTriggerEvent;
+import com.sequenceiq.cloudbreak.structuredevent.service.telemetry.mapper.ClusterUseCaseAware;
 import com.sequenceiq.flow.core.chain.FlowEventChainFactory;
 import com.sequenceiq.flow.core.chain.config.FlowTriggerEventQueue;
+import com.sequenceiq.flow.core.chain.finalize.config.FlowChainFinalizeState;
+import com.sequenceiq.flow.core.chain.finalize.flowevents.FlowChainFinalizePayload;
+import com.sequenceiq.flow.core.chain.init.config.FlowChainInitState;
+import com.sequenceiq.flow.core.chain.init.flowevents.FlowChainInitPayload;
 
 @Component
-public class RefreshEntitlementParamsFlowEventChainFactory implements FlowEventChainFactory<RefreshEntitlementParamsFlowChainTriggerEvent> {
+public class RefreshEntitlementParamsFlowEventChainFactory implements FlowEventChainFactory<RefreshEntitlementParamsFlowChainTriggerEvent>, ClusterUseCaseAware {
 
     @Override
     public String initEvent() {
@@ -25,10 +35,25 @@ public class RefreshEntitlementParamsFlowEventChainFactory implements FlowEventC
     @Override
     public FlowTriggerEventQueue createFlowTriggerEventQueue(RefreshEntitlementParamsFlowChainTriggerEvent event) {
         Queue<Selectable> flowEventChain = new ConcurrentLinkedQueue<>();
+        flowEventChain.add(new FlowChainInitPayload(getName(), event.getResourceId(), event.accepted()));
         if (event.getSaltRefreshNeeded()) {
             flowEventChain.add(new StackEvent(SALT_UPDATE_EVENT.event(), event.getResourceId(), event.accepted()));
         }
         flowEventChain.add(RefreshEntitlementParamsTriggerEvent.fromChainTrigger(event));
+        flowEventChain.add(new FlowChainFinalizePayload(getName(), event.getResourceId(), event.accepted()));
         return new FlowTriggerEventQueue(getName(), event, flowEventChain);
+    }
+
+    @Override
+    public Value getUseCaseForFlowState(Enum flowState) {
+        if (FlowChainInitState.INIT_STATE.equals(flowState)) {
+            return ENTITLEMENT_SYNC_STARTED;
+        } else if (FlowChainFinalizeState.FLOWCHAIN_FINALIZE_FINISHED_STATE.equals(flowState)) {
+            return ENTITLEMENT_SYNC_FINISHED;
+        } else if (flowState.toString().endsWith("FAILED_STATE")) {
+            return ENTITLEMENT_SYNC_FAILED;
+        } else {
+            return UNSET;
+        }
     }
 }
