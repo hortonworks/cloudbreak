27b49444fc9edc8471cdbbcf97d6a2cf4e6230b9
doktoric <rdoktorics@cloudera.com>
2024-06-04 15:04:55 +0200

    CB-25855 adding n2-highmem-2,n2-highmem-4,n2-highmem-8,n2-highmem-16,n2-highmem-32,n2-highmem-48,n2-highmem-64,n2-highmem-80,n2-highmem-96,n2-highmem-128, ,n4-standard-2,n4-standard-4,n4-standard-8,n4-standard-16,n4-standard-32,n4-standard-48,n4-standard-64,n4-standard-80, ,n4-highmem-2,n4-highmem-4,n4-highmem-8,n4-highmem-16,n4-highmem-32,n4-highmem-48,n4-highmem-64,n4-highmem-80 to google cloud

--- cloud-gcp/src/main/java/com/sequenceiq/cloudbreak/cloud/gcp/DistroxEnabledInstanceTypes.java
+++ cloud-gcp/src/main/java/com/sequenceiq/cloudbreak/cloud/gcp/DistroxEnabledInstanceTypes.java
@@ -1,52 +1,77 @@
 package com.sequenceiq.cloudbreak.cloud.gcp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DistroxEnabledInstanceTypes {
-    private static final String ENABLED_TYPES = " e2-standard-2," +
-            "e2-standard-4," +
-            "e2-standard-8," +
-            "e2-standard-16," +
-            "e2-standard-32," +
-            "n2-standard-2," +
-            "n2-standard-4," +
-            "n2-standard-8," +
-            "n2-standard-16," +
-            "n2-standard-32," +
-            "n2-standard-48," +
-            "n2-standard-64," +
-            "n2-standard-80," +
-            "n1-standard-2," +
-            "n1-standard-4," +
-            "n1-standard-8," +
-            "n1-standard-16," +
-            "n1-standard-32," +
-            "n1-standard-48," +
-            "n1-standard-64," +
-            "n1-standard-96," +
-            "n2d-standard-2," +
-            "n2d-standard-4," +
-            "n2d-standard-8," +
-            "n2d-standard-16," +
-            "n2d-standard-32," +
-            "n2d-standard-48," +
-            "n2d-standard-64," +
-            "n2d-standard-80," +
-            "n2d-highmem-16," +
-            "n2d-highcpu-8," +
-            "n2d-highcpu-16," +
-            "n2d-highcpu-32," +
-            "n2d-highmem-32," +
-            "n2d-highmem-64," +
-            "e2-highmem-8," +
-            "e2-highmem-16," +
-            "e2-highcpu-8," +
-            "e2-highcpu-16," +
-            "e2-highcpu-32";
 
-    public static final List<String> GCP_ENABLED_TYPES_LIST = new ArrayList<String>(Arrays.asList(ENABLED_TYPES.trim().split(",")));
+    public static final List<String> GCP_ENABLED_TYPES_LIST = List.of(
+            "e2-standard-2",
+            "e2-standard-4",
+            "e2-standard-8",
+            "e2-standard-16",
+            "e2-standard-32",
+            "n2-standard-2",
+            "n2-standard-4",
+            "n2-standard-8",
+            "n2-standard-16",
+            "n2-standard-32",
+            "n2-standard-48",
+            "n2-standard-64",
+            "n2-standard-80",
+            "n2-highmem-2",
+            "n2-highmem-4",
+            "n2-highmem-8",
+            "n2-highmem-16",
+            "n2-highmem-32",
+            "n2-highmem-48",
+            "n2-highmem-64",
+            "n2-highmem-80",
+            "n2-highmem-96",
+            "n2-highmem-128",
+            "n1-standard-2",
+            "n1-standard-4",
+            "n1-standard-8",
+            "n1-standard-16",
+            "n1-standard-32",
+            "n1-standard-48",
+            "n1-standard-64",
+            "n1-standard-96",
+            "n2d-standard-2",
+            "n2d-standard-4",
+            "n2d-standard-8",
+            "n2d-standard-16",
+            "n2d-standard-32",
+            "n2d-standard-48",
+            "n2d-standard-64",
+            "n2d-standard-80",
+            "n2d-highmem-16",
+            "n2d-highcpu-8",
+            "n2d-highcpu-16",
+            "n2d-highcpu-32",
+            "n2d-highmem-32",
+            "n2d-highmem-64",
+            "e2-highmem-8",
+            "e2-highmem-16",
+            "e2-highcpu-8",
+            "e2-highcpu-16",
+            "e2-highcpu-32",
+            "n4-standard-2",
+            "n4-standard-4",
+            "n4-standard-8",
+            "n4-standard-16",
+            "n4-standard-32",
+            "n4-standard-48",
+            "n4-standard-64",
+            "n4-standard-80",
+            "n4-highmem-2",
+            "n4-highmem-4",
+            "n4-highmem-8",
+            "n4-highmem-16",
+            "n4-highmem-32",
+            "n4-highmem-48",
+            "n4-highmem-64",
+            "n4-highmem-80"
+    );
 
     private DistroxEnabledInstanceTypes() {
     }
