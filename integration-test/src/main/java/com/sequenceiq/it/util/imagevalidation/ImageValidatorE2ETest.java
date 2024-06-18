a2364d51b92eb9f6a53d01584304110a23e87961
David Bajzath <dbajzath@cloudera.com>
2024-06-18 14:53:03 +0200

    CB-26188 Null-safe get image validation image id

--- integration-test/src/main/java/com/sequenceiq/it/util/imagevalidation/ImageValidatorE2ETest.java
+++ integration-test/src/main/java/com/sequenceiq/it/util/imagevalidation/ImageValidatorE2ETest.java
@@ -10,13 +10,15 @@ public interface ImageValidatorE2ETest {
 
     default String getFreeIpaImageId(TestContext testContext) {
         return Optional.ofNullable(testContext.get(FreeIpaTestDto.class))
-                .map(testDto -> testDto.getResponse().getImage().getId())
+                .flatMap(testDto -> Optional.ofNullable(testDto.getResponse()))
+                .map(response -> response.getImage().getId())
                 .orElse(null);
     }
 
     default String getCbImageId(TestContext testContext) {
         return Optional.ofNullable(testContext.get(SdxInternalTestDto.class))
-                .map(testDto -> testDto.getResponse().getStackV4Response().getImage().getId())
+                .flatMap(testDto -> Optional.ofNullable(testDto.getResponse()))
+                .map(response -> response.getStackV4Response().getImage().getId())
                 .orElse(null);
     }
 
