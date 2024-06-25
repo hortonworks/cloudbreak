706f528d5a59063d705d3cfa0a3b7f239fa3f653
jenkins-cloudbreak <cloudbreak-dev@cloudera.com>
2024-06-25 08:30:27 +0200

    [JENKINS-AUTOMATION] Update Base image for docker-private.infra.cloudera.com/cloudera_base/ubi8/cldr-openjdk-21-runtime-cis:1.20-2-stig-25062024

--- integration-test/scripts/docker-build.sh
+++ integration-test/scripts/docker-build.sh
@@ -2,7 +2,7 @@
 
 : ${ADDITIONAL_DOCKER_BUILD_COMMAND:=""}
 
-docker pull docker-private.infra.cloudera.com/cloudera_base/ubi8/cldr-openjdk-21-runtime-cis:1.19-4-stig-29042024
+docker pull docker-private.infra.cloudera.com/cloudera_base/ubi8/cldr-openjdk-21-runtime-cis:1.20-2-stig-25062024
 date
 echo -e "\n\033[1;96m--- build cloudbreak in docker container\033[0m\n"
 docker run -i --rm \
@@ -10,4 +10,4 @@ docker run -i --rm \
     -v $(pwd)/../:/tmp/prj:rw \
     -e "CM_PRIVATE_REPO_USER=$CM_PRIVATE_REPO_USER" \
     -e "CM_PRIVATE_REPO_PASSWORD=$CM_PRIVATE_REPO_PASSWORD" \
-    docker-private.infra.cloudera.com/cloudera_base/ubi8/cldr-openjdk-21-runtime-cis:1.19-4-stig-29042024 /tmp/prj/gradlew -b /tmp/prj/build.gradle clean build -x test
+    docker-private.infra.cloudera.com/cloudera_base/ubi8/cldr-openjdk-21-runtime-cis:1.20-2-stig-25062024 /tmp/prj/gradlew -b /tmp/prj/build.gradle clean build -x test
