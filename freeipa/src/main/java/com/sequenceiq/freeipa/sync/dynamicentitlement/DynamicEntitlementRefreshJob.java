21c16e868d2e2654db88141a39ee738e36da52d4
Laszlo Turcsanyi <lturcsanyi@cloudera.com>
2024-06-27 12:49:32 +0200

    CB-26320 Run salt update for dynamic entitlement job only when required packages are installed

--- freeipa/src/main/java/com/sequenceiq/freeipa/sync/dynamicentitlement/DynamicEntitlementRefreshJob.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/sync/dynamicentitlement/DynamicEntitlementRefreshJob.java
@@ -15,10 +15,14 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.cloudbreak.auth.ThreadBasedUserCrnProvider;
 import com.sequenceiq.cloudbreak.auth.crn.RegionAwareInternalCrnGeneratorFactory;
 import com.sequenceiq.cloudbreak.auth.security.internal.InternalCrnModifier;
+import com.sequenceiq.cloudbreak.cloud.model.catalog.ImagePackageVersion;
 import com.sequenceiq.cloudbreak.quartz.statuschecker.job.StatusCheckerJob;
 import com.sequenceiq.flow.core.FlowLogService;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.Status;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.image.Image;
 import com.sequenceiq.freeipa.entity.Stack;
+import com.sequenceiq.freeipa.service.image.ImageNotFoundException;
+import com.sequenceiq.freeipa.service.image.ImageService;
 import com.sequenceiq.freeipa.service.stack.StackService;
 
 @DisallowConcurrentExecution
@@ -48,6 +52,9 @@ public class DynamicEntitlementRefreshJob extends StatusCheckerJob {
     @Inject
     private InternalCrnModifier internalCrnModifier;
 
+    @Inject
+    private ImageService imageService;
+
     @Override
     protected Optional<Object> getMdcContextObject() {
         return Optional.ofNullable(stackService.getStackById(getStackId()));
@@ -60,7 +67,7 @@ public class DynamicEntitlementRefreshJob extends StatusCheckerJob {
         Status status = stack.getStackStatus().getStatus();
         if (flowLogService.isOtherFlowRunning(stackId)) {
             LOGGER.debug("DynamicEntitlementRefreshJob cannot run, because flow is running for FreeIPA.");
-        } else if (status.isAvailable() && config.isDynamicEntitlementEnabled()) {
+        } else if (status.isAvailable() && config.isDynamicEntitlementEnabled() && isRequiredPackagesInstalled(stack)) {
             LOGGER.debug("DynamicEntitlementRefreshJob will apply watched entitlement changes for FreeIPA");
             ThreadBasedUserCrnProvider.doAs(
                     internalCrnModifier.changeAccountIdInCrnString(regionAwareInternalCrnGeneratorFactory.iam().getInternalCrnForServiceAsString(),
@@ -73,6 +80,27 @@ public class DynamicEntitlementRefreshJob extends StatusCheckerJob {
             LOGGER.debug("DynamicEntitlementRefreshJob cannot run because of stack state {}.", stack.getStackStatus().getStatus());
             logDynamicEntitlementInfo(stack, status);
         }
+    }
+
+    private boolean isRequiredPackagesInstalled(Stack stack) {
+        try {
+            Image image = imageService.getImageForStack(stack);
+            if (image != null) {
+                Map<String, String> packageVersions = image.getPackageVersions();
+                if (packageVersions != null) {
+                    boolean requiredPackagesInstalled = packageVersions.containsKey(ImagePackageVersion.CDP_PROMETHEUS.getKey());
+                    LOGGER.debug("DynamicEntitlementRefreshJob required packages installed {}", requiredPackagesInstalled);
+                    return requiredPackagesInstalled;
+                } else {
+                    LOGGER.warn("PackageVersions is null in image {}", image.getUuid());
+                }
+            } else {
+                LOGGER.warn("Image not found");
+            }
+        } catch (ImageNotFoundException e) {
+            LOGGER.warn("Image not found: {}", e);
+        }
+        return false;
     }
 
     private void logDynamicEntitlementInfo(Stack stack, Status status) {
