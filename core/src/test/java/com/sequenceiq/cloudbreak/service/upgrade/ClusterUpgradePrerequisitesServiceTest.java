1cd91aa3876f7a21639777f494afbe4b54357948
tiborpopovics <tpopovics@cloudera.com>
2024-09-03 16:03:48 +0200

    CB-27018: Remove Zeppelin from the cluster before runtime upgrade
    In this commit I've added the following changes:
    - Removing Zeppelin service from the cluster before runtime upgrade if the target runtime version is 7.3.1 or higher
    - Removing the Zeppelin link from the UI if the service is not available on the cluster anymore

--- core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/ClusterUpgradePrerequisitesServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/ClusterUpgradePrerequisitesServiceTest.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +25,12 @@ class ClusterUpgradePrerequisitesServiceTest {
 
     private static final String DAS_SERVICE_TYPE = "DAS";
 
+    private static final String ZEPPELIN_SERVICE_TYPE = "ZEPPELIN";
+
+    private static final Map<String, String> SERVICES_TO_REMOVE = Map.of(
+            DAS_SERVICE_TYPE, "7.2.18",
+            ZEPPELIN_SERVICE_TYPE, "7.3.1");
+
     @InjectMocks
     private ClusterUpgradePrerequisitesService underTest;
 
@@ -32,43 +40,69 @@ class ClusterUpgradePrerequisitesServiceTest {
     @Mock
     private ClusterApi clusterApi;
 
+    @Mock
+    private ServicesToRemoveBeforeUpgrade servicesToRemoveBeforeUpgrade;
+
     @BeforeEach
     void before() {
         lenient().when(stackDto.getName()).thenReturn(STACK_NAME);
+        when(servicesToRemoveBeforeUpgrade.getServicesToRemove()).thenReturn(SERVICES_TO_REMOVE);
     }
 
     @Test
-    void testRemoveDasServiceIfNecessary() throws Exception {
+    void testRemoveIncompatibleServices() throws Exception {
+        when(clusterApi.isServicePresent(STACK_NAME, DAS_SERVICE_TYPE)).thenReturn(true);
+        when(clusterApi.isServicePresent(STACK_NAME, ZEPPELIN_SERVICE_TYPE)).thenReturn(true);
+
+        underTest.removeIncompatibleServices(stackDto, clusterApi, "7.3.1");
+
+        verify(clusterApi).isServicePresent(STACK_NAME, DAS_SERVICE_TYPE);
+        verify(clusterApi).isServicePresent(STACK_NAME, ZEPPELIN_SERVICE_TYPE);
+        verify(clusterApi).stopClouderaManagerService(DAS_SERVICE_TYPE, true);
+        verify(clusterApi).stopClouderaManagerService(ZEPPELIN_SERVICE_TYPE, true);
+        verify(clusterApi).deleteClouderaManagerService(DAS_SERVICE_TYPE);
+        verify(clusterApi).deleteClouderaManagerService(ZEPPELIN_SERVICE_TYPE);
+    }
+
+    @Test
+    void testRemoveIncompatibleServicesShouldNotRemoveTheServicesIfTheRuntimeVersionIsLowerThanTheLimited() {
+        underTest.removeIncompatibleServices(stackDto, clusterApi, "7.2.17");
+        verifyNoInteractions(clusterApi);
+    }
+
+    @Test
+    void testRemoveIncompatibleServicesShouldOnlyRemoveTheDasServicesIfTheRuntimeVersionIsLowerThanTheLimited() throws Exception {
         when(clusterApi.isServicePresent(STACK_NAME, DAS_SERVICE_TYPE)).thenReturn(true);
 
-        underTest.removeDasServiceIfNecessary(stackDto, clusterApi, "7.2.18");
+        underTest.removeIncompatibleServices(stackDto, clusterApi, "7.3.0");
 
         verify(clusterApi).isServicePresent(STACK_NAME, DAS_SERVICE_TYPE);
+        verify(clusterApi, times(0)).isServicePresent(STACK_NAME, ZEPPELIN_SERVICE_TYPE);
         verify(clusterApi).stopClouderaManagerService(DAS_SERVICE_TYPE, true);
+        verify(clusterApi, times(0)).stopClouderaManagerService(ZEPPELIN_SERVICE_TYPE, true);
         verify(clusterApi).deleteClouderaManagerService(DAS_SERVICE_TYPE);
+        verify(clusterApi, times(0)).deleteClouderaManagerService(ZEPPELIN_SERVICE_TYPE);
     }
 
     @Test
-    void testRemoveDasServiceIfNecessaryShouldNotRemoveTheServiceIfTheRuntimeVersionIsLowerThanTheLimited() throws Exception {
-        underTest.removeDasServiceIfNecessary(stackDto, clusterApi, "7.2.17");
+    void testRemoveIncompatibleServicesShouldNotRemoveTheServiceIfTheRuntimeVersionIsNull() {
+        underTest.removeIncompatibleServices(stackDto, clusterApi,  null);
         verifyNoInteractions(clusterApi);
     }
 
     @Test
-    void testRemoveDasServiceIfNecessaryShouldNotRemoveTheServiceIfTheRuntimeVersionIsNull() throws Exception {
-        underTest.removeDasServiceIfNecessary(stackDto, clusterApi,  null);
-        verifyNoInteractions(clusterApi);
-    }
-
-    @Test
-    void testRemoveDasServiceIfNecessaryShouldNotRemoveTheServiceIfTheServiceIsNotPresentOnTheCluster() throws Exception {
+    void testRemoveIncompatibleServicesShouldNotRemoveTheServiceIfTheServiceIsNotPresentOnTheCluster() throws Exception {
         when(clusterApi.isServicePresent(STACK_NAME, DAS_SERVICE_TYPE)).thenReturn(false);
+        when(clusterApi.isServicePresent(STACK_NAME, ZEPPELIN_SERVICE_TYPE)).thenReturn(false);
 
-        underTest.removeDasServiceIfNecessary(stackDto, clusterApi, "7.2.18");
+        underTest.removeIncompatibleServices(stackDto, clusterApi, "7.3.1");
 
         verify(clusterApi).isServicePresent(STACK_NAME, DAS_SERVICE_TYPE);
+        verify(clusterApi).isServicePresent(STACK_NAME, ZEPPELIN_SERVICE_TYPE);
         verify(clusterApi, times(0)).stopClouderaManagerService(DAS_SERVICE_TYPE, true);
+        verify(clusterApi, times(0)).stopClouderaManagerService(ZEPPELIN_SERVICE_TYPE, true);
         verify(clusterApi, times(0)).deleteClouderaManagerService(DAS_SERVICE_TYPE);
+        verify(clusterApi, times(0)).deleteClouderaManagerService(ZEPPELIN_SERVICE_TYPE);
     }
 
 }
