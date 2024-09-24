77acce48f5c682252b356fda757a9f3ec376f87e
sodre90 <perdos@cloudera.com>
2024-09-24 10:08:07 +0200

    Revert "CB-27185 - improve set_default_java_version.sh"
    This reverts commit dad08d6a20afc149660b2bb2477190f9a6d99b5d.

--- orchestrator-salt/src/main/resources/salt/salt/java/scripts/set_default_java_version.sh
+++ orchestrator-salt/src/main/resources/salt/salt/java/scripts/set_default_java_version.sh
@@ -2,38 +2,46 @@
 
 set -e
 
-CURRENT_JAVA_VERSION=$(java -version 2>&1 | grep -oP "version [^0-9]?(1\.)?\K\d+" || true)
+if [ -n "$TARGET_JAVA_VERSION" ]; then
+  CURRENT_JAVA_VERSION=$(java -version 2>&1 | grep -oP "version [^0-9]?(1\.)?\K\d+" || true)
 
-if [ -z "$CURRENT_JAVA_VERSION" ]; then
-  echo "Failed to determine the current java version."
-  exit 1;
-elif [ $CURRENT_JAVA_VERSION -eq $TARGET_JAVA_VERSION ]; then
-  echo "Current java version equals to the target version."
-  exit 0;
-elif [ -n "$TARGET_JAVA_VERSION" ]; then
-  echo "Change java version to $TARGET_JAVA_VERSION"
-  ALTERNATIVE_TARGET=$TARGET_JAVA_VERSION
-  if [ "$TARGET_JAVA_VERSION" -eq "8" ]; then
-    ALTERNATIVE_TARGET=1.8.0
-  fi
-  if [[ $CPU_ARCH =~ ^aarch64 ]]; then
-    alternatives --set java "java-$ALTERNATIVE_TARGET-openjdk.aarch64"
+  if [ -z "$CURRENT_JAVA_VERSION" ]; then
+    echo "Failed to determine the current java version."
+    exit 1;
+  elif [ $CURRENT_JAVA_VERSION -eq $TARGET_JAVA_VERSION ]; then
+    echo "Current java version equals to the target version."
+    exit 0;
+  elif [ $CURRENT_JAVA_VERSION -eq 8 ] && [ $TARGET_JAVA_VERSION -eq 11 ]; then
+    echo "Change java version from $CURRENT_JAVA_VERSION to $TARGET_JAVA_VERSION"
+
+    alternatives --set java java-11-openjdk.x86_64
+    ln -sfn /etc/alternatives/java_sdk_11 /usr/lib/jvm/java
+    mkdir -p /etc/alternatives/java_sdk_11/jre/lib/security
+    ln -sfn /etc/alternatives/java_sdk_11/conf/security/java.security /etc/alternatives/java_sdk_11/jre/lib/security/java.security
+    ln -sfn /etc/pki/java/cacerts /etc/alternatives/java_sdk_11/jre/lib/security/cacerts
+  elif [ $CURRENT_JAVA_VERSION -eq 8 ] && [ $TARGET_JAVA_VERSION -eq 17 ]; then
+    echo "Change java version from $CURRENT_JAVA_VERSION to $TARGET_JAVA_VERSION"
+    if [[ $CPU_ARCH =~ ^aarch64 ]]; then
+      alternatives --set java java-17-openjdk.aarch64
+    else
+      alternatives --set java java-17-openjdk.x86_64
+    fi
+    ln -sfn /etc/alternatives/java_sdk_17 /usr/lib/jvm/java
+    mkdir -p /etc/alternatives/java_sdk_17/jre/lib/security
+    ln -sfn /etc/alternatives/java_sdk_17/conf/security/java.security /etc/alternatives/java_sdk_17/jre/lib/security/java.security
+    ln -sfn /etc/pki/java/cacerts /etc/alternatives/java_sdk_17/jre/lib/security/cacerts
   else
-    alternatives --set java "java-$ALTERNATIVE_TARGET-openjdk.x86_64"
+    echo "Changing java version from $CURRENT_JAVA_VERSION to $TARGET_JAVA_VERSION is not supported"
+    exit 1
   fi
-  ln -sfn "/etc/alternatives/java_sdk_$ALTERNATIVE_TARGET" "/usr/lib/jvm/java"
-  mkdir -p "/etc/alternatives/java_sdk_$ALTERNATIVE_TARGET/jre/lib/security"
-  if [ "$TARGET_JAVA_VERSION" -eq "17" ]; then
-    ln -sfn "/etc/alternatives/java_sdk_$ALTERNATIVE_TARGET/conf/security/java.security" "/etc/alternatives/java_sdk_$ALTERNATIVE_TARGET/jre/lib/security/java.security"
-  fi
-  ln -sfn "/etc/pki/java/" "/etc/alternatives/java_sdk_$ALTERNATIVE_TARGET/jre/lib/security/cacerts"
 
   #Finally validate the current java version is the target one
+  PREVIOUS_JAVA_VERSION=$CURRENT_JAVA_VERSION
   CURRENT_JAVA_VERSION=$(java -version 2>&1 | grep -oP "version [^0-9]?(1\.)?\K\d+" || true)
-  if [ "$CURRENT_JAVA_VERSION" -eq "$TARGET_JAVA_VERSION" ]; then
-    echo "Java version successfully changed to $TARGET_JAVA_VERSION."
+  if [ $CURRENT_JAVA_VERSION -eq $TARGET_JAVA_VERSION ]; then
+    echo "Java version successfully changed from $PREVIOUS_JAVA_VERSION to $TARGET_JAVA_VERSION."
   else
-    echo "Failed to change java version to $TARGET_JAVA_VERSION. Java version is $CURRENT_JAVA_VERSION"
+    echo "Failed to change java version from $PREVIOUS_JAVA_VERSION to $TARGET_JAVA_VERSION. Java version is $CURRENT_JAVA_VERSION"
     exit 1
   fi
 fi
