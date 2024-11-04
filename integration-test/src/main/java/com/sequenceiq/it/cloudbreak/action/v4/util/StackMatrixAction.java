6bdac19b3afbe21b004e4bbdf5b6f5fcb1bfb9df
David Bajzath <dbajzath@cloudera.com>
2024-11-04 15:21:21 +0100

    CB-27418 2.90 UtilEndpoint.getStackMatrix merge fix

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/v4/util/StackMatrixAction.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/action/v4/util/StackMatrixAction.java
@@ -3,6 +3,8 @@ package com.sequenceiq.it.cloudbreak.action.v4.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sequenceiq.cloudbreak.util.NullUtil;
+import com.sequenceiq.common.model.Architecture;
 import com.sequenceiq.it.cloudbreak.action.Action;
 import com.sequenceiq.it.cloudbreak.context.TestContext;
 import com.sequenceiq.it.cloudbreak.dto.imagecatalog.ImageCatalogTestDto;
@@ -22,7 +24,7 @@ public class StackMatrixAction implements Action<StackMatrixTestDto, CloudbreakC
                 testDto.getCloudPlatform().name(),
                 testDto.isGovCloud(),
                 testDto.getOs(),
-                testDto.getArchitecture()));
+                NullUtil.getIfNotNull(testDto.getArchitecture(), Architecture::getName)));
         Log.whenJson(LOGGER, "Obtaining stack matrix response:\n", testDto.getResponse());
 
         return testDto;
