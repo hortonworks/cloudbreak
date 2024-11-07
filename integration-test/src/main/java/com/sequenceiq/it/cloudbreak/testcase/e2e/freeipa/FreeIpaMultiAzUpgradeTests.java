bd783a0cbac525c49a6b76468369376efa6a9415
adsharma <adsharma@cloudera.com>
2024-11-07 15:26:08 -0500

    CB-26709 Add E2E tests for upgrading MultiAz FreeIPA on GCP Add E2E tests for upgrading MultiAz FreeIPA on GCP

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/freeipa/FreeIpaMultiAzUpgradeTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/freeipa/FreeIpaMultiAzUpgradeTests.java
@@ -3,18 +3,21 @@ package com.sequenceiq.it.cloudbreak.testcase.e2e.freeipa;
 import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
 import static com.sequenceiq.it.cloudbreak.context.RunningParameter.waitForFlow;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.testng.annotations.Test;
 
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.Status;
+import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceGroupResponse;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.common.instance.InstanceMetaDataResponse;
 import com.sequenceiq.freeipa.api.v1.freeipa.stack.model.describe.DescribeFreeIpaResponse;
 import com.sequenceiq.it.cloudbreak.client.FreeIpaTestClient;
@@ -31,6 +34,8 @@ public class FreeIpaMultiAzUpgradeTests extends AbstractE2ETest {
     protected static final Status FREEIPA_AVAILABLE = Status.AVAILABLE;
 
     protected static final Status FREEIPA_DELETE_COMPLETED = Status.DELETE_COMPLETED;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FreeIpaMultiAzUpgradeTests.class);
 
     @Inject
     private FreeIpaTestClient freeIpaTestClient;
@@ -69,19 +74,54 @@ public class FreeIpaMultiAzUpgradeTests extends AbstractE2ETest {
         if (!freeIpaResponse.isEnableMultiAz()) {
             throw new TestFailException(String.format("MultiAz is not enabled for %s after upgrade", freeIpaResponse.getName()));
         }
-        List<String> instanceIds = freeIpaResponse.getInstanceGroups().stream()
-                .map(ig -> ig.getMetaData())
-                .filter(Objects::nonNull)
-                .flatMap(ins -> ins.stream())
-                .map(InstanceMetaDataResponse::getInstanceId)
-                .collect(Collectors.toList());
-        Map<String, Set<String>> availabilityZoneForVms = getCloudFunctionality(tc).listAvailabilityZonesForVms(freeIpa, instanceIds);
-        List<String> instancesWithNoAz = instanceIds.stream().filter(instance -> CollectionUtils.isEmpty(availabilityZoneForVms.get(instance)))
+        for (InstanceGroupResponse instanceGroup: freeIpaResponse.getInstanceGroups()) {
+            if (!CollectionUtils.isEmpty(instanceGroup.getMetaData())) {
+                Map<String, String> instanceZoneMap = instanceGroup.getMetaData().stream()
+                        .collect(Collectors.toMap(InstanceMetaDataResponse::getInstanceId, InstanceMetaDataResponse::getAvailabilityZone));
+                validateMultiAzDistribution(freeIpa, tc, instanceZoneMap, instanceGroup.getName());
+            }
+        }
+    }
+
+    private void validateMultiAzDistribution(String freeIpa, TestContext tc, Map<String, String> instanceZoneMap,
+            String hostGroup) {
+        Map<String, String> availabilityZoneForVms = getCloudFunctionality(tc).listAvailabilityZonesForVms(freeIpa, instanceZoneMap);
+        LOGGER.info("Availability Zone for Vms {}", availabilityZoneForVms);
+        List<String> instancesWithNoAz = instanceZoneMap.keySet().stream().filter(instance -> StringUtils.isEmpty(availabilityZoneForVms.get(instance)))
                 .collect(Collectors.toList());
         if (!CollectionUtils.isEmpty(instancesWithNoAz)) {
-            throw new TestFailException(String.format("Availability Zones is missing for instances %s in %s after upgrade",
-                    instancesWithNoAz.stream().collect(Collectors.joining(",")), freeIpaResponse.getName()));
+            throw new TestFailException(String.format("Availability Zones is missing for instances %s in %s",
+                    String.join(",", instancesWithNoAz), freeIpa));
         }
+        Map<String, Integer> zoneToNodeCountMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : availabilityZoneForVms.entrySet()) {
+            zoneToNodeCountMap.put(entry.getValue(), zoneToNodeCountMap.getOrDefault(entry.getValue(), 0) + 1);
+        }
+        LOGGER.info("Zone to Node count {} after upgrade", zoneToNodeCountMap);
+        int numInstances = instanceZoneMap.size();
+        int numZones = zoneToNodeCountMap.size();
+        int numZonesWithDesiredNumInstances;
+        if (instanceZoneMap.size() >= zoneToNodeCountMap.size()) {
+            numZonesWithDesiredNumInstances = countZonesWithDesiredNumberOfInstances(zoneToNodeCountMap, numInstances / numZones);
+            if (numZones - numInstances % numZones != numZonesWithDesiredNumInstances) {
+                throw new TestFailException(String.format("Distribution of nodes in AZs is not correct in host group: %s after upgrade for %s." +
+                                "There are %s instance and %s zones.Number of Zones with number of instances %s should be %s but is %s",
+                        hostGroup, freeIpa, numInstances, numZones, numInstances / numZones, numZones - numInstances % numZones,
+                        numZonesWithDesiredNumInstances));
+            }
+        }
+        numZonesWithDesiredNumInstances = countZonesWithDesiredNumberOfInstances(zoneToNodeCountMap, numInstances / numZones + 1);
+        if (numInstances % numZones != numZonesWithDesiredNumInstances) {
+            throw new TestFailException(String.format("Distribution of nodes in AZs is not correct in host group: %s after upgrade for %s." +
+                            "There are %s instance and %s zones.Number of Zones with number of instances %s should be %s but is %s",
+                    hostGroup, freeIpa, numInstances, numZones, numInstances / numZones + 1, numInstances % numZones,
+                    numZonesWithDesiredNumInstances));
+        }
+    }
+
+    private int countZonesWithDesiredNumberOfInstances(Map<String, Integer> zoneToNodeCountMap, int desiredCount) {
+        return (int) zoneToNodeCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() == desiredCount).count();
     }
 
     private CloudFunctionality getCloudFunctionality(TestContext testContext) {
