8e7623548d3c031948e73ffd7bf086b5b4dfe3d2
Lajos Rodek <lrodek@cloudera.com>
2024-11-11 15:15:51 +0100

    CB-27765 Reject blank `userManagedIdentity` in env creation
    * `userManagedIdentity` is now considered invalid if it is set to the
      empty string or all whitespace only.
    * `userManagedIdentity == null` is still a permitted case because this
      setting is meaningless when using Azure DB Single Server (and this
      is yet unknown at the time of env creation).

---
+++ environment/src/test/java/com/sequenceiq/environment/environment/validation/validators/EncryptionRoleValidatorTest.java
@@ -0,0 +1,51 @@
+package com.sequenceiq.environment.environment.validation.validators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.sequenceiq.cloudbreak.validation.ValidationResult;
+
+class EncryptionRoleValidatorTest {
+
+    private static final String DOC_LINK = " Refer to Cloudera documentation at https://docs.cloudera.com/cdp-public-cloud/cloud/requirements-azure/topics/" +
+            "mc-az-minimal-setup-for-cloud-storage.html#mc-az-minimal-setup-for-cloud-storage for the required setup.";
+
+    private EncryptionRoleValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new EncryptionRoleValidator();
+    }
+
+    @Test
+    void validateEncryptionRoleTestWhenNull() {
+        ValidationResult validationResult = underTest.validateEncryptionRole(null);
+
+        assertThat(validationResult.hasError()).isFalse();
+    }
+
+    @ParameterizedTest(name = "encryptionRole=\"{0}\"")
+    @ValueSource(strings = {"", " "})
+    void validateEncryptionRoleTestWhenBlank(String encryptionRole) {
+        ValidationResult validationResult = underTest.validateEncryptionRole(encryptionRole);
+
+        assertThat(validationResult.hasError()).isTrue();
+        assertThat(validationResult.getFormattedErrors()).isEqualTo(
+                "If specified, the managed identity resource ID may not be empty or whitespace only." + DOC_LINK);
+    }
+
+    @Test
+    void validateEncryptionRoleTestWhenMalformed() {
+        ValidationResult validationResult = underTest.validateEncryptionRole("foo");
+
+        assertThat(validationResult.hasError()).isTrue();
+        assertThat(validationResult.getFormattedErrors()).isEqualTo(
+                "Must be a full valid managed identity resource ID in the format of /subscriptions/[your-subscription-id]/resourceGroups/" +
+                        "[your-resource-group]/providers/Microsoft.ManagedIdentity/userAssignedIdentities/[name-of-your-identity]." + DOC_LINK);
+    }
+
+}
