358603926bc7c92adba3aef187a015e5ec099f65
Attila Kristof Foldi <foldik@cloudera.com>
2024-11-28 11:46:46 +0100

    CB-27999 Separate Graviton e2e test into separate class. It fixes the issue when e2e test failed because of skipped tests.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXImagesTests.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXImagesTests.java
@@ -12,9 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.image.StackImageV4Response;
-import com.sequenceiq.cloudbreak.util.VersionComparator;
-import com.sequenceiq.common.model.Architecture;
 import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
 import com.sequenceiq.it.cloudbreak.cloud.v4.CloudProvider;
 import com.sequenceiq.it.cloudbreak.context.Description;
@@ -29,8 +26,6 @@ import com.sequenceiq.it.cloudbreak.util.spot.UseSpotInstances;
 
 public class DistroXImagesTests extends AbstractE2ETest {
     private static final Logger LOGGER = LoggerFactory.getLogger(DistroXImagesTests.class);
-
-    private static final String ARM64_MIN_RUNTIME_VERSION = "7.3.1";
 
     @Inject
     private DistroXTestClient distroXTestClient;
@@ -104,46 +99,6 @@ public class DistroXImagesTests extends AbstractE2ETest {
                     if (!dto.getResponse().getImage().getId().equals(selectedImageID.get())) {
                         throw new TestFailException(" The selected image ID is: " + dto.getResponse().getImage().getId() + " instead of: "
                                 + selectedImageID.get());
-                    }
-                    return dto;
-                })
-                .validate();
-    }
-
-    @Test(dataProvider = TEST_CONTEXT)
-    @UseSpotInstances
-    @Description(
-            given = "there is a running cloudbreak",
-            when = "a valid DistroX create request is sent with arm64 architecture",
-            then = "the DistroX's stack and image should have arm64 architecture")
-    public void testDistroXWithArm64ImageCanBeCreatedSuccessfully(TestContext testContext) {
-        String runtimeVersion = new VersionComparator().compare(() -> commonClusterManagerProperties().getRuntimeVersion(), () -> ARM64_MIN_RUNTIME_VERSION) < 0
-                ? ARM64_MIN_RUNTIME_VERSION
-                : commonClusterManagerProperties().getRuntimeVersion();
-        createDatalakeWithVersion(testContext, runtimeVersion);
-
-        String distrox = resourcePropertyProvider().getName();
-
-        testContext
-                .given(distrox, DistroXTestDto.class)
-                .withTemplate(commonClusterManagerProperties().getDataEngDistroXBlueprintName(runtimeVersion))
-                .withArchitecture(Architecture.ARM64)
-                .when(distroXTestClient.create(), key(distrox))
-                .await(STACK_AVAILABLE)
-                .awaitForHealthyInstances()
-                .then((tc, dto, client) -> {
-                    Architecture stackArchitecture = Architecture.fromStringWithFallback(dto.getResponse().getArchitecture());
-                    if (stackArchitecture != Architecture.ARM64) {
-                        throw new TestFailException(String.format("The stack architecture %s does not match, expected arm64", stackArchitecture));
-                    }
-
-                    StackImageV4Response image = dto.getResponse().getImage();
-                    Log.log(LOGGER, format(" Image Catalog Name: %s ", image.getCatalogName()));
-                    Log.log(LOGGER, format(" Image Catalog URL: %s ", image.getCatalogUrl()));
-                    Log.log(LOGGER, format(" Image ID: %s ", image.getId()));
-
-                    if (Architecture.fromStringWithFallback(image.getArchitecture()) != Architecture.ARM64) {
-                        throw new TestFailException(String.format("The image architecture %s does not match, expected arm64", image.getArchitecture()));
                     }
                     return dto;
                 })
