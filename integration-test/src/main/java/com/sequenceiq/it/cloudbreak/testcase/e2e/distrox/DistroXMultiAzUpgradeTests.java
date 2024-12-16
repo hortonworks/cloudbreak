fbcc45573113a444dc170abdb3c0554a9af8b6f6
adsharma <adsharma@cloudera.com>
2024-12-16 16:24:19 -0500

    CB-26720 Add E2E tests for upgrading MultiAz Datahub on GCP Add E2E tests for upgrading MultiAz Datahub on GCP

---
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXMultiAzUpgradeTests.java
@@ -0,0 +1,181 @@
+package com.sequenceiq.it.cloudbreak.testcase.e2e.distrox;
+
+import static com.sequenceiq.it.cloudbreak.context.RunningParameter.key;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import jakarta.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.testng.annotations.Test;
+
+import com.sequenceiq.cloudbreak.api.endpoint.v4.imagecatalog.responses.ImageV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.StackV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.InstanceGroupV4Response;
+import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.response.instancegroup.instancemetadata.InstanceMetaDataV4Response;
+import com.sequenceiq.it.cloudbreak.client.DistroXTestClient;
+import com.sequenceiq.it.cloudbreak.client.ImageCatalogTestClient;
+import com.sequenceiq.it.cloudbreak.client.SdxTestClient;
+import com.sequenceiq.it.cloudbreak.cloud.v4.CommonClusterManagerProperties;
+import com.sequenceiq.it.cloudbreak.context.Description;
+import com.sequenceiq.it.cloudbreak.context.TestContext;
+import com.sequenceiq.it.cloudbreak.dto.distrox.DistroXTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.cluster.DistroXUpgradeTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.image.DistroXImageTestDto;
+import com.sequenceiq.it.cloudbreak.dto.distrox.instancegroup.DistroXInstanceGroupsBuilder;
+import com.sequenceiq.it.cloudbreak.dto.imagecatalog.ImageCatalogTestDto;
+import com.sequenceiq.it.cloudbreak.dto.sdx.SdxInternalTestDto;
+import com.sequenceiq.it.cloudbreak.exception.TestFailException;
+import com.sequenceiq.it.cloudbreak.testcase.e2e.sdx.PreconditionSdxE2ETest;
+import com.sequenceiq.it.cloudbreak.util.CloudFunctionality;
+import com.sequenceiq.it.cloudbreak.util.spot.UseSpotInstances;
+import com.sequenceiq.sdx.api.model.SdxClusterStatusResponse;
+
+public class DistroXMultiAzUpgradeTests extends PreconditionSdxE2ETest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DistroXMultiAzUpgradeTests.class);
+
+    @Inject
+    private SdxTestClient sdxTestClient;
+
+    @Inject
+    private DistroXTestClient distroXTestClient;
+
+    @Inject
+    private CommonClusterManagerProperties commonClusterManagerProperties;
+
+    @Inject
+    private ImageCatalogTestClient imageCatalogTestClient;
+
+    @Test(dataProvider = TEST_CONTEXT)
+    @UseSpotInstances
+    @Description(
+            given = "there is a running Cloudbreak, and an environment with SDX and DistroX clusters in available state",
+            when = "upgrade called on DistroX cluster",
+            then = "Upgrade should be successful,the clusters should be up and running and nodes should be distributed across multiple AZs")
+    public void testDistroXMultiAzUpgrade(TestContext testContext) {
+        String runTimeVersion = commonClusterManagerProperties.getRuntimeVersion();
+        List<ImageV4Response> cdhImages = new ArrayList<>();
+        testContext
+                .given(ImageCatalogTestDto.class)
+                .when(imageCatalogTestClient.getV4WithAdvertisedImages())
+                .then((testContext1, entity, cloudbreakClient) -> {
+                    List<ImageV4Response> sortedImages = entity.getResponse().getImages().getCdhImages().stream()
+                            .filter(im -> im.getVersion().equals(runTimeVersion)
+                                    && im.getImageSetsByProvider().containsKey(testContext.getCloudPlatform().name().toLowerCase()))
+                            .sorted(Comparator.comparing(ImageV4Response::getCreated)).toList();
+                    cdhImages.addAll(sortedImages);
+                    return entity;
+                })
+                .validate();
+        if (cdhImages.size() > 1) {
+            String dataHubKey = "multiAzDistroxUpgrade";
+            String provisionImageKey = "provisionImageKey";
+            testContext.given(SdxInternalTestDto.class)
+                    .withCloudStorage()
+                    .when(sdxTestClient.createInternal())
+                    .await(SdxClusterStatusResponse.RUNNING)
+                    .awaitForHealthyInstances()
+                    .given(provisionImageKey, DistroXImageTestDto.class)
+                    .withImageId(cdhImages.get(0).getUuid())
+                    .given(dataHubKey, DistroXTestDto.class)
+                    .withTemplate(commonClusterManagerProperties.getDataEngDistroXBlueprintName(runTimeVersion))
+                    .withEnableMultiAz(true)
+                    .withInstanceGroupsEntity(new DistroXInstanceGroupsBuilder(testContext)
+                            .defaultHostGroup()
+                            .withStorageOptimizedInstancetype()
+                            .build())
+                    .withImageSettings(provisionImageKey)
+                    .when(distroXTestClient.create(), key(dataHubKey))
+                    .await(STACK_AVAILABLE, key(dataHubKey))
+                    .awaitForHealthyInstances()
+                    .given(dataHubKey, DistroXTestDto.class)
+                    .when(distroXTestClient.get(), key(dataHubKey))
+                    .then((tc, testDto, client) -> {
+                        validateStackForMultiAz(testDto, tc, "provisioning");
+                        return testDto;
+                    })
+                    .given(DistroXUpgradeTestDto.class)
+                    .withImageId(cdhImages.get(cdhImages.size() - 1).getUuid())
+                    .given(dataHubKey, DistroXTestDto.class)
+                    .when(distroXTestClient.upgrade())
+                    .await(STACK_AVAILABLE, key(dataHubKey))
+                    .awaitForHealthyInstances()
+                    .given(dataHubKey, DistroXTestDto.class)
+                    .when(distroXTestClient.get(), key(dataHubKey))
+                    .then((tc, testDto, client) -> {
+                        validateStackForMultiAz(testDto, tc, "Upgrade");
+                        return testDto;
+                    })
+                    .validate();
+        }
+
+    }
+
+    private void validateStackForMultiAz(DistroXTestDto distroXTestDto, TestContext tc, String operation) {
+        StackV4Response stackV4Response = distroXTestDto.getResponse();
+        if (!stackV4Response.isEnableMultiAz()) {
+            throw new TestFailException(String.format("MultiAz is not enabled for %s", stackV4Response.getName()));
+        }
+        for (InstanceGroupV4Response instanceGroup : stackV4Response.getInstanceGroups()) {
+            if (!CollectionUtils.isEmpty(instanceGroup.getMetadata())) {
+                Map<String, String> instanceZoneMap = instanceGroup.getMetadata().stream()
+                        .collect(Collectors.toMap(InstanceMetaDataV4Response::getInstanceId, InstanceMetaDataV4Response::getAvailabilityZone));
+                validateMultiAzDistribution(distroXTestDto.getName(), tc, operation, instanceZoneMap, instanceGroup.getName());
+            }
+        }
+    }
+
+    private void validateMultiAzDistribution(String dataLakeName, TestContext tc, String operation, Map<String, String> instanceZoneMap,
+            String hostGroup) {
+        Map<String, String> availabilityZoneForVms = getCloudFunctionality(tc).listAvailabilityZonesForVms(dataLakeName, instanceZoneMap);
+        LOGGER.info("Availability Zone for Vms {}", availabilityZoneForVms);
+        List<String> instancesWithNoAz = instanceZoneMap.keySet().stream().filter(instance -> StringUtils.isEmpty(availabilityZoneForVms.get(instance)))
+                .collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(instancesWithNoAz)) {
+            throw new TestFailException(String.format("Availability Zones is missing for instances %s in %s",
+                    String.join(",", instancesWithNoAz), dataLakeName));
+        }
+        Map<String, Integer> zoneToNodeCountMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : availabilityZoneForVms.entrySet()) {
+            zoneToNodeCountMap.put(entry.getValue(), zoneToNodeCountMap.getOrDefault(entry.getValue(), 0) + 1);
+        }
+        LOGGER.info("Zone to Node count {} after  {}", zoneToNodeCountMap, operation);
+        int numInstances = instanceZoneMap.size();
+        int numZones = zoneToNodeCountMap.size();
+        int numZonesWithDesiredNumInstances;
+        if (instanceZoneMap.size() >= zoneToNodeCountMap.size()) {
+            numZonesWithDesiredNumInstances = countZonesWithDesiredNumberOfInstances(zoneToNodeCountMap, numInstances / numZones);
+            if (numZones - numInstances % numZones != numZonesWithDesiredNumInstances) {
+                throw new TestFailException(String.format("Distribution of nodes in AZs is not correct in host group: %s after %s for %s." +
+                                "There are %s instance and %s zones.Number of Zones with number of instances %s should be %s but is %s",
+                        hostGroup, operation, dataLakeName, numInstances, numZones, numInstances / numZones, numZones - numInstances % numZones,
+                        numZonesWithDesiredNumInstances));
+            }
+        }
+        numZonesWithDesiredNumInstances = countZonesWithDesiredNumberOfInstances(zoneToNodeCountMap, numInstances / numZones + 1);
+        if (numInstances % numZones != numZonesWithDesiredNumInstances) {
+            throw new TestFailException(String.format("Distribution of nodes in AZs is not correct in host group: %s after %s for %s." +
+                            "There are %s instance and %s zones.Number of Zones with number of instances %s should be %s but is %s",
+                    hostGroup, operation, dataLakeName, numInstances, numZones, numInstances / numZones + 1, numInstances % numZones,
+                    numZonesWithDesiredNumInstances));
+        }
+    }
+
+    private int countZonesWithDesiredNumberOfInstances(Map<String, Integer> zoneToNodeCountMap, int desiredCount) {
+        return (int) zoneToNodeCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() == desiredCount).count();
+    }
+
+    protected CloudFunctionality getCloudFunctionality(TestContext testContext) {
+        return testContext.getCloudProvider().getCloudFunctionality();
+    }
+}
