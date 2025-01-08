dc97f32fc86fc405176c40bf388d458a6f9f76c3
mmolnar <mmolnar@cloudera.com>
2025-01-08 14:36:47 +0100

    CB-28195 Add `@JsonIgnoreProperties(ignoreUnknown = true)` to `DatabasePropertiesV4Response`

--- redbeams-api/src/main/java/com/sequenceiq/redbeams/api/endpoint/v4/databaseserver/responses/DatabasePropertiesV4Response.java
+++ redbeams-api/src/main/java/com/sequenceiq/redbeams/api/endpoint/v4/databaseserver/responses/DatabasePropertiesV4Response.java
@@ -1,11 +1,13 @@
 package com.sequenceiq.redbeams.api.endpoint.v4.databaseserver.responses;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sequenceiq.redbeams.doc.ModelDescriptions;
 import com.sequenceiq.redbeams.doc.ModelDescriptions.DatabaseServer;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = ModelDescriptions.DATABASE_PROPERTIES_RESPONSE)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DatabasePropertiesV4Response {
     @Schema(description = DatabaseServer.CONNECTION_NAME_FORMAT, required = true)
     private ConnectionNameFormat connectionNameFormat = ConnectionNameFormat.USERNAME_ONLY;
