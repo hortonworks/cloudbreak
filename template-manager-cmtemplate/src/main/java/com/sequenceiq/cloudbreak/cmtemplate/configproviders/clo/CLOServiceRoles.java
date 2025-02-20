8830d330af52766d58519094cc64cfdcbd67830c
doktoric <rdoktorics@cloudera.com>
2025-02-20 11:51:29 +0100

    CB-28680 Rename CLO to LAKEHOUSE_OPTIMIZER

--- template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/clo/CLOServiceRoles.java
+++ template-manager-cmtemplate/src/main/java/com/sequenceiq/cloudbreak/cmtemplate/configproviders/clo/CLOServiceRoles.java
@@ -1,7 +1,7 @@
 package com.sequenceiq.cloudbreak.cmtemplate.configproviders.clo;
 
 public class CLOServiceRoles {
-    public static final String CLO_SERVICE = "CLO";
+    public static final String CLO_SERVICE = "LAKEHOUSE_OPTIMIZER";
 
     public static final String CLO_SERVER = "CLO_SERVER";
 
