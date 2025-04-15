b48bf14c226ff36eebfbc857223f02d3fdcb71db
Attila Kristof Foldi <foldik@cloudera.com>
2025-04-15 19:22:04 +0200

    CB-29245 Do not run status finalizer when the cluster is in termination phase because it would cause cluster delete to fail.

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/StackStatusFinalizer.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/StackStatusFinalizer.java
@@ -28,12 +28,14 @@ public class StackStatusFinalizer extends FlowFinalizerCallback {
                 .filter(stackStatus -> stackStatus.getStatus() != null)
                 .filter(stackStatus -> stackStatus.getStatus().isInProgress())
                 .ifPresent(stackStatus -> {
-                    Status finalStatus = stackStatus.getStatus().mapToFailedIfInProgress();
-                    LOGGER.error("Flow completed with stack in {} status which is an in progress status. Mapping it to {} final state.",
-                            stackStatus.getStatus(), finalStatus);
-                    stackUpdater.updateStackStatusAndSetDetailedStatusToUnknown(resourceId, finalStatus,
-                            String.format("Operation completed and the stack is in '%s' state which is an in progress state. It is mapped to failed state. " +
-                                    "This usually happens during an application error or when the operation is cancelled.", stackStatus.getStatus()));
+                    if (!stackStatus.getStatus().isTerminatedOrDeletionInProgress()) {
+                        Status finalStatus = stackStatus.getStatus().mapToFailedIfInProgress();
+                        LOGGER.error("Flow completed with stack in {} status which is an in progress status. Mapping it to {} final state.",
+                                stackStatus.getStatus(), finalStatus);
+                        stackUpdater.updateStackStatusAndSetDetailedStatusToUnknown(resourceId, finalStatus, String.format("Operation completed and the " +
+                                "stack is in '%s' state which is an in progress state. It is mapped to failed state. This usually happens during " +
+                                "an application error or when the operation is cancelled.", stackStatus.getStatus()));
+                    }
                 });
     }
 }
