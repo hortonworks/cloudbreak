17feb83d3673b625610a433b563e2fbc75033600
Laszlo Turcsanyi <lturcsanyi@cloudera.com>
2025-07-01 16:03:08 +0200

    CB-29746 Generate a FreeIPA admin password that does not contain three consecutive repeating characters

--- common/src/main/java/com/sequenceiq/cloudbreak/util/FreeIpaPasswordUtil.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/util/FreeIpaPasswordUtil.java
@@ -16,18 +16,45 @@ public class FreeIpaPasswordUtil {
 
     private static final String SPECIAL_CHARS = "?.-_+";
 
+    private static final int MAX_ATTEMPTS = 1000;
+
     private FreeIpaPasswordUtil() {
     }
 
     public static String generatePassword() {
         String upperCaseLetters = PasswordUtil.getRandomAlphabetic(PWD_PART_LENGTH).toUpperCase(Locale.ROOT);
         String lowerCaseLetters = PasswordUtil.getRandomAlphabetic(PWD_PART_LENGTH).toLowerCase(Locale.ROOT);
-        String pwdPrefix = PasswordUtil.getRandomAlphabetic(PWD_PREFIX_LENGTH);
+        String pwdPrefix = doNotRepeatCharacters(PasswordUtil.getRandomAlphabetic(PWD_PREFIX_LENGTH));
         String numbers = PasswordUtil.getRandomNumeric(PWD_PART_LENGTH);
         String raw = upperCaseLetters.concat(lowerCaseLetters).concat(numbers).concat(SPECIAL_CHARS);
         List<String> list = Arrays.asList(raw.split(""));
         Collections.shuffle(list, SECURE_RANDOM);
-        return pwdPrefix.concat(String.join("", list));
+        return pwdPrefix.concat(doNotRepeatCharacters(String.join("", list)));
+    }
+
+    public static String doNotRepeatCharacters(String random) {
+        int attempts = 0;
+        String randomToCheck = random;
+        while (hasTripleRepeat(randomToCheck)) {
+            if (attempts > MAX_ATTEMPTS) {
+                throw new IllegalStateException("Cannot generate password without repeating characters");
+            }
+            List<String> list = Arrays.asList(randomToCheck.split(""));
+            Collections.shuffle(list, SECURE_RANDOM);
+            randomToCheck = String.join("", list);
+            attempts++;
+        }
+        return randomToCheck;
+    }
+
+    private static boolean hasTripleRepeat(String random) {
+        for (int i = 0; i < random.length() - 2; i++) {
+            char c = random.charAt(i);
+            if (c == random.charAt(i + 1) && c == random.charAt(i + 2)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- common/src/test/java/com/sequenceiq/cloudbreak/util/FreeIpaPasswordUtilTest.java
+++ common/src/test/java/com/sequenceiq/cloudbreak/util/FreeIpaPasswordUtilTest.java
@@ -1,27 +1,52 @@
 package com.sequenceiq.cloudbreak.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-public class FreeIpaPasswordUtilTest {
+class FreeIpaPasswordUtilTest {
 
     private static final Set<String> PATTERNS = Set.of("(?=.*[a-z]).*$", "(?=.*[A-Z]).*$", "(?=.*[0-9]).*$", ".*\\W+.*");
 
     @Test
-    public void testGeneratePasswordShouldCreatePwdWithCorrectSize() {
+    void testGeneratePasswordShouldCreatePwdWithCorrectSize() {
         String actual = FreeIpaPasswordUtil.generatePassword();
         assertEquals(32, actual.length());
     }
 
     @Test
-    public void testGeneratePassword() {
+    void testGeneratePassword() {
         String actual = FreeIpaPasswordUtil.generatePassword();
         long characterClasses = PATTERNS.stream().filter(actual::matches).count();
 
         assertEquals(4, characterClasses);
     }
 
+    @Test
+    void testDoNotRepeatCharactersWhenStringLenghtIsTooShort() {
+        String result = FreeIpaPasswordUtil.doNotRepeatCharacters("aa");
+        assertEquals("aa", result);
+    }
+
+    @Test
+    void testDoNotRepeatCharactersWhenThreeSameCharacter() {
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> FreeIpaPasswordUtil.doNotRepeatCharacters("aaa"));
+        assertEquals("Cannot generate password without repeating characters", exception.getMessage());
+    }
+
+    @Test
+    void testDoNotRepeatCharactersWhenInputContainsInvalidSequence() {
+        String result = FreeIpaPasswordUtil.doNotRepeatCharacters("aaa1");
+        assertNotEquals("aaa1", result);
+    }
+
+    @Test
+    void testDoNotRepeatCharactersWhenInputDoesNotContainInvalidSequence() {
+        String result = FreeIpaPasswordUtil.doNotRepeatCharacters("abcd");
+        assertEquals("abcd", result);
+    }
 }
