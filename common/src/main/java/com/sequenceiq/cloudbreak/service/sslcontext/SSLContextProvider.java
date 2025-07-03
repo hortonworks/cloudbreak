3585945c8d7fb65ecee3c3a721c9769a1538a99e
perdos <perdos@cloudera.com>
2025-07-03 15:44:58 +0200

    CB-29795 Updated `SSLContextProvider` to handle cases with missing certificates or keys
    Updated `SSLContextProvider` to handle cases with missing certificates or keys by defaulting to `CertificateTrustManager.sslContext()`.

--- common/src/main/java/com/sequenceiq/cloudbreak/service/sslcontext/SSLContextProvider.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/service/sslcontext/SSLContextProvider.java
@@ -1,15 +1,18 @@
 package com.sequenceiq.cloudbreak.service.sslcontext;
 
+import java.security.KeyStore;
 import java.util.Optional;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.sequenceiq.cloudbreak.client.CertificateTrustManager;
 import com.sequenceiq.cloudbreak.client.KeyStoreUtil;
 import com.sequenceiq.cloudbreak.common.exception.CloudbreakServiceException;
 
@@ -21,11 +24,16 @@ public class SSLContextProvider {
     @Cacheable(cacheNames = "sslContextCache", key = "{ #serverCert, #additionalServerCert, #clientCert, #clientKey }")
     public SSLContext getSSLContext(String serverCert, Optional<String> additionalServerCert, String clientCert, String clientKey) {
         try {
-            LOGGER.info("Generating custom SSLContext");
-            return SSLContexts.custom()
-                    .loadTrustMaterial(KeyStoreUtil.createTrustStore(serverCert, additionalServerCert), null)
-                    .loadKeyMaterial(KeyStoreUtil.createKeyStore(clientCert, clientKey), "consul".toCharArray())
-                    .build();
+            if (StringUtils.isNoneBlank(serverCert, clientCert, clientKey)) {
+                LOGGER.info("Generating custom SSLContext");
+                KeyStore trustStore = KeyStoreUtil.createTrustStore(serverCert, additionalServerCert);
+                return SSLContexts.custom()
+                        .loadTrustMaterial(trustStore, null)
+                        .loadKeyMaterial(KeyStoreUtil.createKeyStore(clientCert, clientKey), "consul".toCharArray())
+                        .build();
+            } else {
+                return CertificateTrustManager.sslContext();
+            }
         } catch (Exception e) {
             LOGGER.info("Cannot create SSL context", e);
             throw new CloudbreakServiceException("Cannot create SSL context", e);
