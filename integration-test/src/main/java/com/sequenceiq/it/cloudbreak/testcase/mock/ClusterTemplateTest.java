0687d92be4b13f526b3c5bcde67c9a11dbe027d6
Francis Pang <fpang@cloudera.com>
2025-07-15 14:37:07 -0700

    CB-29624 Add 7.3.2 Iceberg Replication Service template and cluster type.
    Iceberg Replication service will be supported by RT 7.3.2, so adding
    support to cloudbreak.
    
    In the cloudbreak UI, I could see:
    
        o Under Shared Resources -> Cluster Templates -> 7.3.2 - Iceberg
    Replication Service
        o Create Data Hub -> 7.3.2 - Iceberg Replication Service for AWS
    (Preview)
    
    Creation of 7.3.2 Iceberg Replication Service Data Hub was successful.

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
@@ -367,7 +367,7 @@ public class ClusterTemplateTest extends AbstractMockTest {
             assertNotNull(entity);
             assertNotNull(entity.getResponses());
             long defaultCount = entity.getResponses().stream().filter(template -> ResourceStatus.DEFAULT.equals(template.getStatus())).count();
-            long expectedCount = 774;
+            long expectedCount = 775;
             assertEquals("Should have " + expectedCount + " of default cluster templates.", expectedCount, defaultCount);
         } catch (Exception e) {
             throw new TestFailException(String.format("Failed to validate default count of cluster templates: %s", e.getMessage()), e);
