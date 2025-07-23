194d6876048622ab744498057334955beec6d134
Szabolcs Horvath <shorvath@cloudera.com>
2025-07-23 09:53:01 +0200

    CB-1: Increase the max header size of the HTTP response decoder used for Azure in the integration-test module

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/azure/azurevm/client/AzureClientConfiguration.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/util/azure/azurevm/client/AzureClientConfiguration.java
@@ -7,6 +7,8 @@ import jakarta.inject.Inject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.ClientSecretCredential;
@@ -18,22 +20,28 @@ import com.sequenceiq.it.cloudbreak.cloud.v4.azure.AzureProperties.Credential;
 @Configuration
 public class AzureClientConfiguration {
 
+    private static final int MAX_RESPONSE_HEADER_SIZE = 16384;
+
     @Inject
     private AzureProperties azureProperties;
 
     @Bean
     public AzureResourceManager getAzure() {
-            Credential credential = azureProperties.getCredential();
-            ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
-                    .clientId(credential.getAppId())
-                    .tenantId(credential.getTenantId())
-                    .clientSecret(credential.getAppPassword())
-                    .build();
+        HttpClient httpClient = new NettyAsyncHttpClientBuilder(reactor.netty.http.client.HttpClient.create()
+                .httpResponseDecoder(spec -> spec.maxHeaderSize(MAX_RESPONSE_HEADER_SIZE)))
+                .build();
+        Credential credential = azureProperties.getCredential();
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId(credential.getAppId())
+                .tenantId(credential.getTenantId())
+                .clientSecret(credential.getAppPassword())
+                .build();
         AzureProfile azureProfile = new AzureProfile(credential.getTenantId(), credential.getSubscriptionId(), AzureEnvironment.AZURE);
         return AzureResourceManager
-                    .configure()
-                    .withLogLevel(BODY_AND_HEADERS)
-                    .authenticate(clientSecretCredential, azureProfile)
-                    .withSubscription(credential.getSubscriptionId());
+                .configure()
+                .withLogLevel(BODY_AND_HEADERS)
+                .withHttpClient(httpClient)
+                .authenticate(clientSecretCredential, azureProfile)
+                .withSubscription(credential.getSubscriptionId());
     }
 }
