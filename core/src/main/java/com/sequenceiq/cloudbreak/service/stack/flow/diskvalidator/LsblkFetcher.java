5843441fdbc305c0d40489c90a8135c632159e94
perdos <perdos@cloudera.com>
2025-09-22 17:27:02 +0200

    CB-30545 Enhance Disk Validation with improved error handling and lsblk result parsing
    Refactored `DiskValidator` to handle `null` sizes safely using `Objects.equals`. Updated `MountDisks` to log validation errors without halting execution. Improved `LsblkFetcher` to handle missing size values in `lsblk` results more gracefully and added detailed logging for `lsblk` command outputs.

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/diskvalidator/LsblkFetcher.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/flow/diskvalidator/LsblkFetcher.java
@@ -10,6 +10,8 @@ import jakarta.inject.Inject;
 
 import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.sequenceiq.cloudbreak.orchestrator.exception.CloudbreakOrchestratorFailedException;
@@ -22,20 +24,28 @@ public class LsblkFetcher {
     public static final String LSBLK_COMMAND = "lsblk -dn --output NAME,SIZE,MOUNTPOINT --bytes | " +
             "awk '{printf \"%s %s %s\\n\", $1, $2/1024/1024/1024, $3, $4}'";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LsblkFetcher.class);
+
     @Inject
     private HostOrchestrator hostOrchestrator;
 
     public MultiValuedMap<String, LsblkLine> getLsblkResults(List<GatewayConfig> allGatewayConfigs, Set<String> targetFqdns) throws
             CloudbreakOrchestratorFailedException {
         Map<String, String> lsblkResults = hostOrchestrator.runCommandOnHosts(allGatewayConfigs, targetFqdns, LSBLK_COMMAND);
+        LOGGER.info("lsblk command results: {}", lsblkResults);
         MultiValuedMap<String, LsblkLine> lsblkResultMap = new ArrayListValuedHashMap<>();
         for (String hostName : lsblkResults.keySet()) {
             lsblkResults.get(hostName).lines().map(String::trim).filter(not(String::isEmpty)).forEach(lsblkLine -> {
                 String[] lsblkLineParts = lsblkLine.split(" ");
-                String device = lsblkLineParts[0];
-                String size = lsblkLineParts[1];
-                String mountPoint = lsblkLineParts.length > 2 ? lsblkLineParts[2] : null;
-                lsblkResultMap.put(hostName, new LsblkLine(device, size, mountPoint));
+                if (lsblkLineParts.length > 0) {
+                    String device = lsblkLineParts[0];
+                    String size = null;
+                    if (lsblkLineParts.length > 1) {
+                        size = lsblkLineParts[1];
+                    }
+                    String mountPoint = lsblkLineParts.length > 2 ? lsblkLineParts[2] : null;
+                    lsblkResultMap.put(hostName, new LsblkLine(device, size, mountPoint));
+                }
             });
         }
         return lsblkResultMap;
