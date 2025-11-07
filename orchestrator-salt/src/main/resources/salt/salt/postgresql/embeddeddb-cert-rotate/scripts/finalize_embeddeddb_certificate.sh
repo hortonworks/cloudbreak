deb4d9d2c4081dce37c06b96ea691623bacb7604
Adam Laszlo Horvath <adam.horvath@cloudera.com>
2025-11-07 06:23:27 +0100

    CB-8115 Enhance embedded DB SSL finalize script to handle missing old certificate's serial number
    - rotation will query only valid certs, thus old sn file will be empty but we do not need to revoke old cert if it is already expired/invalid

--- orchestrator-salt/src/main/resources/salt/salt/postgresql/embeddeddb-cert-rotate/scripts/finalize_embeddeddb_certificate.sh
+++ orchestrator-salt/src/main/resources/salt/salt/postgresql/embeddeddb-cert-rotate/scripts/finalize_embeddeddb_certificate.sh
@@ -16,10 +16,16 @@ CERTS_DIR={{ postgres_directory }}/certs
 
 if [ -e ${CERTS_DIR}/old_cert_sn ]
 then
-  kinit -kt ${CM_KEYTAB_FILE} ${CM_PRINCIPAL}
-  echo "$(date '+%d/%m/%Y %H:%M:%S') - Revoking cert with following Serial number:"
-  cat ${CERTS_DIR}/old_cert_sn
-  cat ${CERTS_DIR}/old_cert_sn | xargs ipa cert-revoke
+  content=$(<"${CERTS_DIR}/old_cert_sn")
+  if [ -n "$content" ] && [[ "$content" =~ [0-9] ]];
+  then
+    kinit -kt ${CM_KEYTAB_FILE} ${CM_PRINCIPAL}
+    echo "$(date '+%d/%m/%Y %H:%M:%S') - Revoking cert with following Serial number:"
+    cat ${CERTS_DIR}/old_cert_sn
+    cat ${CERTS_DIR}/old_cert_sn | xargs ipa cert-revoke
+  else
+    echo "$(date '+%d/%m/%Y %H:%M:%S') - Old certificate serial number missing, possible reason: missing from FreeIPA server or it is already expired, thus revokal is not necessary."
+  fi
 fi
 
 rm -f ${CERTS_DIR}/postgres_bkp.key
