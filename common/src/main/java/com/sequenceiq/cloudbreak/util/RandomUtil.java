d6ba05849b56a2944abdd73bb8afb6a496fc80f8
Peter Darvasi <pdarvasi@cloudera.com>
2025-11-18 14:05:59 +0100

    CB-31137 - Make long-syncer distribution uniform
    With the little modification, the 2 hour long syncer uniformly distributes
    all the triggers between the 2 hours, making the Quartz jobs' utilization
    more smooth, with less spikes and bursts.

--- common/src/main/java/com/sequenceiq/cloudbreak/util/RandomUtil.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/util/RandomUtil.java
@@ -1,10 +1,15 @@
 package com.sequenceiq.cloudbreak.util;
 
 import java.security.SecureRandom;
+import java.util.random.RandomGenerator;
 
 public class RandomUtil {
 
+    private static final String L_64_X_128_MIX_RANDOM = "L64X128MixRandom";
+
     private static final SecureRandom RANDOM = new SecureRandom();
+
+    private static final RandomGenerator QUICK_RANDOM = RandomGenerator.of(L_64_X_128_MIX_RANDOM);
 
     private RandomUtil() {
     }
@@ -19,5 +24,9 @@ public class RandomUtil {
 
     public static SecureRandom getRandom() {
         return RANDOM;
+    }
+
+    public static int getQuickRandomInt(int maximumNumber) {
+        return QUICK_RANDOM.nextInt(maximumNumber);
     }
 }
