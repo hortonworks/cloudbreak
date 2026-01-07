ac9771a4d1bc8ebef6ac5ad81c0fd2d18ebfb4ad
perdos <perdos@cloudera.com>
2026-01-07 15:01:13 +0100

    CB-31408 Add Java version validation for 7.3.2 upgrade and integrate entitlement check

--- core/src/main/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/JavaVersionUpgradeValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/JavaVersionUpgradeValidator.java
@@ -1,12 +1,16 @@
 package com.sequenceiq.cloudbreak.service.upgrade.validation.service;
 
 import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.CLOUDERA_STACK_VERSION_7_3_1;
+import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.CLOUDERA_STACK_VERSION_7_3_2;
 import static com.sequenceiq.cloudbreak.cmtemplate.CMRepositoryVersionUtil.isVersionNewerOrEqualThanLimited;
+
+import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.sequenceiq.cloudbreak.auth.altus.EntitlementService;
 import com.sequenceiq.cloudbreak.common.exception.UpgradeValidationFailedException;
 
 @Component
@@ -16,20 +20,38 @@ public class JavaVersionUpgradeValidator implements ServiceUpgradeValidator {
 
     private static final Integer NOT_SUPPORTED_JAVA_VERSION = 11;
 
+    private static final Integer POST_732_MIN_JAVA_VERSION = 17;
+
+    @Inject
+    private EntitlementService entitlementService;
+
     @Override
     public void validate(ServiceUpgradeValidationRequest request) {
         String targetRuntime = request.upgradeImageInfo().getTargetStatedImage().getImage().getVersion();
         Integer currentJavaVersion = request.stack().getStack().getJavaVersion();
-        if (isVersionNewerOrEqualThan731(targetRuntime) && NOT_SUPPORTED_JAVA_VERSION.equals(currentJavaVersion)) {
+        if (isVersionNewerOrEqualThan732(targetRuntime)) {
+            if (!entitlementService.isAutoJavaUpgaradeEnabled(request.stack().getAccountId()) && currentJavaVersion < POST_732_MIN_JAVA_VERSION) {
+                String message = String.format("You cannot upgrade to %s because your current cluster uses JDK %d, and upgrading to %s with " +
+                                "JDK %d is not supported. Please upgrade to JDK 17 or higher before upgrading the cluster.",
+                        targetRuntime, currentJavaVersion, targetRuntime, currentJavaVersion);
+                LOGGER.error("Cluster upgrade validation failed for 7.3.2+ cluster. {}", message);
+                throw new UpgradeValidationFailedException(message);
+            }
+        } else if (isVersionNewerOrEqualThan731(targetRuntime) && NOT_SUPPORTED_JAVA_VERSION.equals(currentJavaVersion)) {
             String message = String.format("You cannot upgrade to %s because your current cluster uses JDK %d, and upgrading to %s with "
                     + "JDK %d is not supported. Please downgrade to JDK 8 before upgrading the cluster.",
                     targetRuntime, NOT_SUPPORTED_JAVA_VERSION, targetRuntime, NOT_SUPPORTED_JAVA_VERSION);
-            LOGGER.error("Cluster upgrade validation failed. {}", message);
+            LOGGER.error("Cluster upgrade validation failed because of JDK 11. {}", message);
             throw new UpgradeValidationFailedException(message);
         }
+
     }
 
     private boolean isVersionNewerOrEqualThan731(String targetRuntime) {
         return isVersionNewerOrEqualThanLimited(targetRuntime, CLOUDERA_STACK_VERSION_7_3_1);
+    }
+
+    private boolean isVersionNewerOrEqualThan732(String targetRuntime) {
+        return isVersionNewerOrEqualThanLimited(targetRuntime, CLOUDERA_STACK_VERSION_7_3_2);
     }
 }
--- core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/JavaVersionUpgradeValidatorTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/upgrade/validation/service/JavaVersionUpgradeValidatorTest.java
@@ -1,7 +1,9 @@
 package com.sequenceiq.cloudbreak.service.upgrade.validation.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.util.stream.Stream;
@@ -14,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sequenceiq.cloudbreak.auth.altus.EntitlementService;
 import com.sequenceiq.cloudbreak.cloud.model.catalog.Image;
 import com.sequenceiq.cloudbreak.common.exception.UpgradeValidationFailedException;
 import com.sequenceiq.cloudbreak.domain.stack.Stack;
@@ -28,34 +31,49 @@ class JavaVersionUpgradeValidatorTest {
     private JavaVersionUpgradeValidator underTest;
 
     @Mock
+    private EntitlementService entitlementService;
+
+    @Mock
     private StackDto stackDto;
 
-    @ParameterizedTest(name = "[{index}] Target runtime version: {0} Current Java version: {1} Should permit upgrade: {2}")
+    @ParameterizedTest(name = "[{index}] Target runtime version: {0} Current Java version: {1} Should permit upgrade: {2} Auto java upgrade enabled: {4}")
     @MethodSource("provideTestParameters")
-    public void testValidate(String targetRuntimeVersion, Integer currentJavaVersion, boolean shouldPermitUpgrade) {
+    public void testValidate(String targetRuntimeVersion, Integer currentJavaVersion, boolean autoJavaUpgradeEnabled,
+            boolean shouldPermitUpgrade, String containedMessage) {
         UpgradeImageInfo upgradeImageInfo = createUpgradeImageInfo(targetRuntimeVersion);
         Stack stack = createStack(currentJavaVersion);
         when(stackDto.getStack()).thenReturn(stack);
+        lenient().when(stackDto.getAccountId()).thenReturn("12345678");
+        lenient().when(entitlementService.isAutoJavaUpgaradeEnabled("12345678")).thenReturn(autoJavaUpgradeEnabled);
         ServiceUpgradeValidationRequest request = new ServiceUpgradeValidationRequest(stackDto, true, true, upgradeImageInfo, true);
 
         if (shouldPermitUpgrade) {
             assertDoesNotThrow(() -> underTest.validate(request));
         } else {
-            assertThrows(UpgradeValidationFailedException.class, () -> underTest.validate(request));
+            UpgradeValidationFailedException exception = assertThrows(UpgradeValidationFailedException.class, () -> underTest.validate(request));
+            assertThat(exception.getMessage()).contains(containedMessage);
         }
     }
 
     private static Stream<Arguments> provideTestParameters() {
         return Stream.of(
-                Arguments.of("7.3.0", 8, true),
-                Arguments.of("7.3.0", 11, true),
-                Arguments.of("7.3.0", 17, true),
-                Arguments.of("7.3.1", 8, true),
-                Arguments.of("7.3.1", 11, false),
-                Arguments.of("7.3.1", 17, true),
-                Arguments.of("7.3.2", 8, true),
-                Arguments.of("7.3.2", 11, false),
-                Arguments.of("7.3.2", 17, true)
+                Arguments.of("7.3.0", 8, false, true, null),
+                Arguments.of("7.3.0", 8, true, true, null),
+                Arguments.of("7.3.0", 11, false, true, null),
+                Arguments.of("7.3.0", 11, true, true, null),
+                Arguments.of("7.3.0", 17, false, true, null),
+                Arguments.of("7.3.0", 17, true, true, null),
+                Arguments.of("7.3.1", 8, false, true, null),
+                Arguments.of("7.3.1", 8, true, true, null),
+                Arguments.of("7.3.1", 11, false, false, "You cannot upgrade to 7.3.1 because your current cluster uses JDK 11"),
+                Arguments.of("7.3.1", 17, false, true, null),
+                Arguments.of("7.3.1", 17, true, true, null),
+                Arguments.of("7.3.2", 8, false, false, "You cannot upgrade to 7.3.2 because your current cluster uses JDK 8"),
+                Arguments.of("7.3.2", 8, true, true, null),
+                Arguments.of("7.3.2", 11, false, false, "You cannot upgrade to 7.3.2 because your current cluster uses JDK 11"),
+                Arguments.of("7.3.2", 11, true, true, null),
+                Arguments.of("7.3.2", 17, false, true, null),
+                Arguments.of("7.3.2", 17, true, true, null)
         );
     }
 
