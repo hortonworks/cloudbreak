da590cf904f78e3af45925f67fa6c901b74813b8
David Bajzath <dbajzath@cloudera.com>
2026-02-12 13:05:23 +0100

    CB-31729 Improve Active Directory scripts' logging

--- freeipa/src/test/resources/crossrealmtrust/ad/activedirectory_commands_cleanup.bat
+++ freeipa/src/test/resources/crossrealmtrust/ad/activedirectory_commands_cleanup.bat
@@ -4,13 +4,16 @@ set IPA_DOMAIN=freeipa.org
 :: Clean up of DNS setup and its caches for Active Directory cross-realm trust
 :: More info: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/dnscmd
 dnscmd 127.0.0.1 /zoneinfo %IPA_DOMAIN%. > NUL 2>&1 && (
-  echo IPA zone found, removing it and its caches...
-  dnscmd 127.0.0.1 /ZoneDelete %IPA_DOMAIN% /f || (
+  echo IPA DNS zone found, removing it and its caches...
+  dnscmd 127.0.0.1 /ZoneDelete %IPA_DOMAIN% /f && (
+    dnscmd 127.0.0.1 /ClearCache > NUL 2>&1
+    :: More info: https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc731935(v=ws.11)
+    nltest /dsgetdc:%IPA_DOMAIN% /force > NUL 2>&1 || echo:
+  ) || (
     echo [FAILURE] Failed to remove IPA DNS zone for %IPA_DOMAIN% domain!
   )
-  dnscmd 127.0.0.1 /ClearCache > NUL 2>&1
-  :: More info: https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-R2-and-2012/cc731935(v=ws.11)
-  nltest /dsgetdc:%IPA_DOMAIN% /force > NUL 2>&1 || echo:
+) || (
+  echo IPA DNS zone is already deleted
 )
 
 :: Remove trust from the Active Directory server towards the FreeIPA
@@ -20,4 +23,6 @@ netdom query /Domain:%AD_DOMAIN% /Direct TRUST | findstr %IPA_DOMAIN% > NUL 2>&1
   netdom trust %IPA_DOMAIN% /Domain:%AD_DOMAIN% /Remove /Force || (
     echo [FAILURE] Failed to remove IPA trust for %IPA_DOMAIN% domain!
   )
+) || (
+  echo IPA trust is already deleted
 )
