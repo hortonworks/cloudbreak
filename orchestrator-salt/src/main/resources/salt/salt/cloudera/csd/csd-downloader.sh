55491d8b0222bdf6a0ef3baaf835ae366b7fb76d
tiborpopovics <tpopovics@cloudera.com>
2026-03-06 10:29:26 +0100

    CB-32119 Modify CSD file cleanup logic to only delete unnecessary CSD files
    In this commit, I modified the CSD downloader script so that it no longer deletes the entire contents of the CSD folder. Instead, it removes only the unnecessary files at the end of the process.
    
    I also fixed the logic used to determine the required parcels based on the blueprint and the contents of the parcel manifest files. The logic was broken because the service name was retrieved from the blueprint in the wrong format, and I also made the comparison case-insensitive.

--- orchestrator-salt/src/main/resources/salt/salt/cloudera/csd/csd-downloader.sh
+++ orchestrator-salt/src/main/resources/salt/salt/cloudera/csd/csd-downloader.sh
@@ -26,14 +26,15 @@ CREDENTIAL="{{ salt['pillar.get']('cloudera-manager:paywall_username') }}:{{ sal
 echo "$(date '+%d/%m/%Y %H:%M:%S') - Paywall credential found " |& tee -a /var/log/csd_downloader.log
 {%- endif %}
 
-rm -rf /opt/cloudera/csd
 mkdir -p /opt/cloudera/csd
 cd /opt/cloudera/csd
+declare -a desiredFiles=()
 
 for url in ${csdUrls[@]}
 do
   fileName=$(basename $url)
   echo "$(date '+%d/%m/%Y %H:%M:%S') - Trying to download CSD file name ($fileName) from URL: ($url) " |& tee -a /var/log/csd_downloader.log
+  desiredFiles+=("$fileName")
   if test -f $fileName
   then
     echo "$(date '+%d/%m/%Y %H:%M:%S') - ($fileName) already exists " |& tee -a /var/log/csd_downloader.log
@@ -49,6 +50,21 @@ do
     curl -L -O -R --fail "${PROXY_ARGS[@]}" "${PROXY_AUTH_ARGS[@]}" $AUTH_FLAG $url |& tee -a /var/log/csd_downloader.log
   fi
 done
+
+declare -A desiredMap
+for f in "${desiredFiles[@]}"; do
+  desiredMap["$f"]=1
+done
+
+for file in *; do
+  [[ -f "$file" ]] || continue
+
+  if [[ -z "${desiredMap[$file]}" ]]; then
+    echo "$(date '+%d/%m/%Y %H:%M:%S') - Removing obsolete CSD: $file" |& tee -a /var/log/csd_downloader.log
+    rm -f "$file"
+  fi
+done
+
 {% else %}
 echo "$(date '+%d/%m/%Y %H:%M:%S') - No CSDs to download. " |& tee -a /var/log/csd_downloader.log
 
