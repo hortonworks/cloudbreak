b0ea7b043c10ddc36e58f9db96bb69e294e595e1
Daniel Jobbagy <daniel.jobbagy@cloudera.com>
2026-03-31 13:56:51 +0200

    CB-32450 Don't retry parcel availability check for non-archive URLs

--- core/src/main/java/com/sequenceiq/cloudbreak/service/parcel/ParcelAvailabilityRetrievalService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/parcel/ParcelAvailabilityRetrievalService.java
@@ -1,8 +1,10 @@
 package com.sequenceiq.cloudbreak.service.parcel;
 
-import java.util.List;
+import java.util.Map;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
@@ -11,9 +13,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.retry.support.RetrySynchronizationManager;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
 import com.sequenceiq.cloudbreak.auth.PaywallCredentialPopulator;
@@ -24,11 +26,10 @@ public class ParcelAvailabilityRetrievalService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ParcelAvailabilityRetrievalService.class);
 
-    @Value("#{'${cb.http.retryableStatusCodes:}'.split(',')}")
-    private List<Integer> retryableHttpCodes;
+    private static final int MAX_ATTEMPTS = 3;
 
-    @Value("${cb.parcel.retry.maxAttempts:5}")
-    private Integer retryMaxAttempts;
+    @Value("${cb.parcel.retry.backOffDelay:2000}")
+    private long backOffDelay;
 
     @Inject
     private RestClientFactory restClientFactory;
@@ -36,25 +37,34 @@ public class ParcelAvailabilityRetrievalService {
     @Inject
     private PaywallCredentialPopulator paywallCredentialPopulator;
 
-    @Retryable(retryFor = Exception.class,
-            maxAttemptsExpression = "${cb.parcel.retry.maxAttempts:5}",
-            backoff = @Backoff(delayExpression = "${cb.parcel.retry.backOffDelay:2000}",
-                    multiplierExpression = "${cb.parcel.retry.backOffMultiplier:2}"))
+    private RetryTemplate retryTemplate;
+
+    @PostConstruct
+    void initRetryTemplate() {
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(backOffDelay);
+        RetryTemplate template = new RetryTemplate();
+        template.setBackOffPolicy(backOffPolicy);
+        template.setRetryPolicy(new SimpleRetryPolicy(MAX_ATTEMPTS, Map.<Class<? extends Throwable>, Boolean>of(ProcessingException.class, true)));
+        retryTemplate = template;
+    }
+
     @Cacheable(cacheNames = "parcelAvailabilityCache", key = "#url")
     public Response getHeadResponseForParcel(String url) {
+        if (PaywallCredentialPopulator.ARCHIVE_URL_PATTERN.matcher(url).find()) {
+            LOGGER.info("Archive URL '{}' detected, HEAD request will be attempted up to {} times on ProcessingException", url, MAX_ATTEMPTS);
+            return retryTemplate.execute(ctx -> doHeadRequest(url));
+        }
+        LOGGER.info("Non-archive URL '{}', HEAD request will be sent without retry", url);
+        return doHeadRequest(url);
+    }
+
+    private Response doHeadRequest(String url) {
         Client client = restClientFactory.getOrCreateWithFollowRedirects();
         WebTarget target = client.target(url);
         paywallCredentialPopulator.populateWebTarget(url, target);
         Response response = target.request().head();
-        LOGGER.info("Head request for {} status: {}", url, response.getStatus());
-        if (retryableHttpCodes.contains(response.getStatus())) {
-            if (RetrySynchronizationManager.getContext().getRetryCount() < retryMaxAttempts - 1) {
-                LOGGER.info("Retry for Http Status {}", response.getStatus());
-                throw new RuntimeException(String.format("Got Http Status %s", response.getStatus()));
-            } else {
-                LOGGER.info("Retry exhausted");
-            }
-        }
+        LOGGER.info("HEAD request for {} returned status: {}", url, response.getStatus());
         return response;
     }
 }
