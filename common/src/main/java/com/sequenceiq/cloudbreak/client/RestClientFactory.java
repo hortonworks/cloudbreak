5624560b7a6811f85c2f0993706a805fefd750fa
mmolnar <mmolnar@cloudera.com>
2026-05-27 16:03:20 +0200

    CB-33097 Use secure client for S3-hosted image catalogs
    The changes remove the patched HttpUrlConnector as it not required anymore, as new Jersey contains the fix.
    Also adds a getOrCreateWithFollowRedirectsAndSecureForS3 method to RestClientFactory that enables SNI for S3 URLs
    by disabling custom hostname verifier.
    Then wire all image catalog HTTP calls to use it.

--- common/src/main/java/com/sequenceiq/cloudbreak/client/RestClientFactory.java
+++ common/src/main/java/com/sequenceiq/cloudbreak/client/RestClientFactory.java
@@ -1,5 +1,8 @@
 package com.sequenceiq.cloudbreak.client;
 
+import java.net.URI;
+import java.util.Locale;
+
 import jakarta.ws.rs.client.Client;
 
 import org.springframework.stereotype.Component;
@@ -17,5 +20,14 @@ public class RestClientFactory {
 
     public Client getOrCreateWithFollowRedirects() {
         return getOrCreate(ConfigKey.builder().withFollowRedirects(true).build());
+    }
+
+    public Client getOrCreateWithFollowRedirectsAndSecureForS3(String url) {
+        String host = URI.create(url).getHost();
+        ConfigKey configKey = ConfigKey.builder()
+                .withFollowRedirects(true)
+                .withSecure(host.toLowerCase(Locale.ROOT).endsWith("s3.amazonaws.com"))
+                .build();
+        return getOrCreate(configKey);
     }
 }
