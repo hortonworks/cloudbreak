b954c375d5a2eab302e1d2746f43a5b731aaf659
nkhatri <nkhatri@cloudera.com>
2026-06-08 21:05:20 +0530

    CB-33278 Add 7.3.2 Iceberg Replication Cluster Template for Azure

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/mock/ClusterTemplateTest.java
@@ -367,7 +367,7 @@ public class ClusterTemplateTest extends AbstractMockTest {
             assertNotNull(entity);
             assertNotNull(entity.getResponses());
             long defaultCount = entity.getResponses().stream().filter(template -> ResourceStatus.DEFAULT.equals(template.getStatus())).count();
-            long expectedCount = 782;
+            long expectedCount = 783;
             assertEquals(expectedCount, defaultCount, "Should have " + expectedCount + " of default cluster templates.");
         } catch (Exception e) {
             throw new TestFailException(String.format("Failed to validate default count of cluster templates: %s", e.getMessage()), e);
