7ab2244cb832a89563df84aa2a3b99be7dae2a61
David Bajzath <dbajzath@cloudera.com>
2026-07-06 13:24:25 +0200

    CB-33607 Handle non-PAAS datalakes in SharedServiceValidator

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/SharedServiceValidator.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/SharedServiceValidator.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.StackV4Request;
 import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.cloudbreak.domain.view.StackView;
+import com.sequenceiq.cloudbreak.sdx.TargetPlatform;
+import com.sequenceiq.cloudbreak.sdx.common.PlatformAwareSdxConnector;
+import com.sequenceiq.cloudbreak.sdx.common.model.SdxBasicView;
 import com.sequenceiq.cloudbreak.validation.ValidationResult;
 import com.sequenceiq.cloudbreak.validation.ValidationResult.ValidationResultBuilder;
 import com.sequenceiq.cloudbreak.workspace.model.Workspace;
@@ -21,6 +24,9 @@ public class SharedServiceValidator {
     @Qualifier("stackViewServiceDeprecated")
     private StackViewService stackViewService;
 
+    @Inject
+    private PlatformAwareSdxConnector platformAwareSdxConnector;
+
     public ValidationResult checkSharedServiceStackRequirements(StackV4Request request, Workspace workspace) {
         ValidationResultBuilder resultBuilder = ValidationResult.builder();
         if (request.getSharedService() != null) {
@@ -31,15 +37,20 @@ public class SharedServiceValidator {
     }
 
     private void checkCloudPlatform(StackV4Request request, Long workspaceId, ValidationResultBuilder resultBuilder) {
-        Optional<StackView> datalakeStack = stackViewService.findByName(request.getSharedService().getDatalakeName(), workspaceId);
-        if (datalakeStack.isEmpty()) {
-            resultBuilder.error("Datalake stack with the requested name (in sharedService/sharedClusterName field) was not found.");
-        } else {
-            CloudPlatform requestedCloudPlatform = request.getCloudPlatform();
-            String datalakeCloudPlatform = datalakeStack.get().cloudPlatform();
-            if (!datalakeCloudPlatform.equals(requestedCloudPlatform.name())) {
-                resultBuilder.error(String.format("Requested cloud platform [%s] does not match with the datalake"
-                        + " cluser's cloud platform [%s].", requestedCloudPlatform, datalakeCloudPlatform));
+        Optional<SdxBasicView> sdxBasicView = platformAwareSdxConnector.getSdxBasicViewByEnvironmentCrn(request.getEnvironmentCrn());
+        if (sdxBasicView.isEmpty()) {
+            resultBuilder.error("Datalake stack in the requested environment was not found.");
+        } else if (sdxBasicView.get().platform().equals(TargetPlatform.PAAS)) {
+            Optional<StackView> datalakeStack = stackViewService.findByName(request.getSharedService().getDatalakeName(), workspaceId);
+            if (datalakeStack.isEmpty()) {
+                resultBuilder.error("Datalake stack with the requested name (in sharedService/sharedClusterName field) was not found.");
+            } else {
+                String datalakeCloudPlatform = datalakeStack.get().cloudPlatform();
+                CloudPlatform requestedCloudPlatform = request.getCloudPlatform();
+                if (!datalakeCloudPlatform.equals(requestedCloudPlatform.name())) {
+                    resultBuilder.error(String.format("Requested cloud platform [%s] does not match with the datalake"
+                            + " cluser's cloud platform [%s].", requestedCloudPlatform, datalakeCloudPlatform));
+                }
             }
         }
     }
--- core/src/test/java/com/sequenceiq/cloudbreak/service/stack/SharedServiceValidatorTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/stack/SharedServiceValidatorTest.java
@@ -1,8 +1,11 @@
 package com.sequenceiq.cloudbreak.service.stack;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -20,9 +23,9 @@ import com.sequenceiq.cloudbreak.api.endpoint.v4.stacks.request.cluster.sharedse
 import com.sequenceiq.cloudbreak.common.mappable.CloudPlatform;
 import com.sequenceiq.cloudbreak.domain.RDSConfig;
 import com.sequenceiq.cloudbreak.domain.view.StackView;
-import com.sequenceiq.cloudbreak.ldap.LdapConfigService;
-import com.sequenceiq.cloudbreak.service.blueprint.BlueprintService;
-import com.sequenceiq.cloudbreak.service.rdsconfig.RdsConfigService;
+import com.sequenceiq.cloudbreak.sdx.TargetPlatform;
+import com.sequenceiq.cloudbreak.sdx.common.PlatformAwareSdxConnector;
+import com.sequenceiq.cloudbreak.sdx.common.model.SdxBasicView;
 import com.sequenceiq.cloudbreak.validation.ValidationResult;
 import com.sequenceiq.cloudbreak.workspace.model.Workspace;
 
@@ -31,39 +34,54 @@ class SharedServiceValidatorTest {
 
     private static final String DATALAKE_NAME = "datalake";
 
-    private static final String LDAP_NAME = "ldap";
-
     private static final String RANGER_DB_NAME = "ranger";
 
     private static final String HIVE_DB_NAME = "hivetest";
-
-    private static final String RANGER_TYPE_STRING = "RANGER";
-
-    private static final String HIVE_TYPE_STRING = "HIVE";
-
-    @Mock
-    private RdsConfigService rdsConfigService;
 
     @Mock
     private StackViewService stackViewService;
 
     @Mock
-    private BlueprintService blueprintService;
-
-    @Mock
-    private LdapConfigService ldapConfigService;
+    private PlatformAwareSdxConnector platformAwareSdxConnector;
 
     @InjectMocks
     private SharedServiceValidator underTest;
 
     @Test
-    void testWithValidRequest() {
+    void testWithPdlDatalake() {
         StackV4Request stackRequest = getStackV4Request(CloudPlatform.GCP);
+        when(platformAwareSdxConnector.getSdxBasicViewByEnvironmentCrn(any())).thenReturn(Optional.of(getSdxBasicView(TargetPlatform.PDL)));
+
+        ValidationResult validationResult = underTest.checkSharedServiceStackRequirements(stackRequest, getWorkspace());
+
+        assertFalse(validationResult.hasError());
+        verifyNoInteractions(stackViewService);
+    }
+
+    @Test
+    void testWithValidPaasRequest() {
+        StackV4Request stackRequest = getStackV4Request(CloudPlatform.GCP);
+        when(platformAwareSdxConnector.getSdxBasicViewByEnvironmentCrn(any())).thenReturn(Optional.of(getSdxBasicView(TargetPlatform.PAAS)));
         when(stackViewService.findByName(eq(DATALAKE_NAME), anyLong())).thenReturn(Optional.ofNullable(getStackView()));
 
         ValidationResult validationResult = underTest.checkSharedServiceStackRequirements(stackRequest, getWorkspace());
 
         assertFalse(validationResult.hasError());
+    }
+
+    @Test
+    void testWithInvalidPaasRequest() {
+        StackV4Request stackRequest = getStackV4Request(CloudPlatform.AWS);
+        when(platformAwareSdxConnector.getSdxBasicViewByEnvironmentCrn(any())).thenReturn(Optional.of(getSdxBasicView(TargetPlatform.PAAS)));
+        when(stackViewService.findByName(eq(DATALAKE_NAME), anyLong())).thenReturn(Optional.ofNullable(getStackView()));
+
+        ValidationResult validationResult = underTest.checkSharedServiceStackRequirements(stackRequest, getWorkspace());
+
+        assertTrue(validationResult.hasError());
+    }
+
+    private SdxBasicView getSdxBasicView(TargetPlatform targetPlatform) {
+        return new SdxBasicView(DATALAKE_NAME, "crn", "", false, 0L, "", targetPlatform);
     }
 
     private RDSConfig getDatabase(String type) {
